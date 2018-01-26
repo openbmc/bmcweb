168792a1349438ca10d9e275a41ddb412634c171
Vernon Mauery <vernon.mauery@intel.com>
2018-01-26 13:42:54 -0800

    Allow for systemd socket activation
    If spawned via systemd's socket activation mechanism, use that socket
    instead of opening a new one to listen on.
    
    Change-Id: Ia35110902b30b08355edf2fe4041e8377582e72c
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- src/webserver_main.cpp
+++ src/webserver_main.cpp
@@ -14,7 +14,30 @@
 #include <string>
 #include <crow/app.h>
 #include <boost/asio.hpp>
+#include <systemd/sd-daemon.h>
 #include "redfish.hpp"
+
+constexpr int defaultPort = 18080;
+
+template <typename... Middlewares>
+void setup_socket(crow::Crow<Middlewares...>& app) {
+  int listen_fd = sd_listen_fds(0);
+  if (1 == listen_fd) {
+    CROW_LOG_INFO << "attempting systemd socket activation";
+    if (sd_is_socket_inet(SD_LISTEN_FDS_START, AF_UNSPEC, SOCK_STREAM, 1, 0)) {
+      CROW_LOG_INFO << "Starting webserver on socket handle "
+                    << SD_LISTEN_FDS_START;
+      app.socket(SD_LISTEN_FDS_START);
+    } else {
+      CROW_LOG_INFO << "bad incoming socket, starting webserver on port "
+                << defaultPort;
+      app.port(defaultPort);
+    }
+  } else {
+    CROW_LOG_INFO << "Starting webserver on port " << defaultPort;
+    app.port(defaultPort);
+  }
+}
 
 int main(int argc, char** argv) {
   auto io = std::make_shared<boost::asio::io_service>();
@@ -44,9 +67,9 @@ int main(int argc, char** argv) {
   crow::openbmc_mapper::request_routes(app);
 
   crow::logger::setLogLevel(crow::LogLevel::INFO);
-  int port = 18080;
-  std::cout << "Starting webserver on port " << port << "\n";
-  app.port(port);
+
+  CROW_LOG_INFO << "bmcweb (" << __DATE__ << ": " << __TIME__ << ')';
+  setup_socket(app);
 
   // Start dbus connection
   crow::connections::system_bus =
