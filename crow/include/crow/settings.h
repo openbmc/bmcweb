ac093318ac41bb5306d19d01824f79d0d3488840
Ed Tanous <ed.tanous@intel.com>
2018-03-16 13:53:05 -0700

    Disable logging when in release mode
    This increases the performance of the webserver quite a bit.
    
    Change-Id: I20714d89163cf30c65afd18d16f4fd16c06824d5
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>

--- crow/include/crow/settings.h
+++ crow/include/crow/settings.h
@@ -2,12 +2,6 @@
 // settings for crow
 // TODO - replace with runtime config. libucl?
 
-/* #ifdef - enables debug mode */
-#define CROW_ENABLE_DEBUG
-
-/* #ifdef - enables logging */
-#define CROW_ENABLE_LOGGING
-
 /* #ifdef - enables ssl */
 //#define CROW_ENABLE_SSL
 
