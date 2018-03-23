7d95f5f6db1df37fe6438aa0e87479275c82807a
Ed tanous <ed@tanous.net>
2018-03-23 00:19:20 -0700

    Remove hunter and submodules
    And use external project instead.  This should make it easier to keep
    our systems up to date with yocto.
    
    Change-Id: Id2c9f3e474eebf2194eaf58b0c572f7a1421def6

--- build_x86.sh
+++ build_x86.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
-
-#Currently there are some build errors with the pre-instaleld libgtest-dev, as a workaround, we can build it with these steps(first time only):
-#cd googletest/googletest/
-#cmake ./
-#make
-#sudo make install
-
-cd `dirname $0`
-#update source code
-#git pull
-git submodule init
-git submodule sync
-git submodule update
-
+sudo apt-get install -y libpam0g-dev libssl-dev zlib1g-dev
 mkdir -p build
 cd build
-cmake ..  -DHUNTER_ENABLED=ON
-make  -j$(nproc)
+cmake ..
+cmake --build .
