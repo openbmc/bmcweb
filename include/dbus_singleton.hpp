d28442555ee690fb07428d5b8c8d267e36dfe100
Ed Tanous <ed.tanous@intel.com>
2018-04-12 12:17:20 -0700

    Move bmcweb over to sdbusplus
    Requires patchset from https://gerrit.openbmc-project.xyz/#/c/10278/ to build.
    
    This patchset moves bmcweb from using boost-dbus over entirely to
    sdbusplus.  This has some nice improvements in performance (about 30%
    of CPU cycles saved in dbus transactions), as well as makes this
    project manuver closer to the upstream way of thinking.
    
    Changes to bmcweb are largely ceremonial, and fall into a few
    categories:
    1. Moves async_method_call instances to the new format, and deletes any
    use of the "endpoint" object in leiu of the sdbusplus style interface
    2. sdbus object_path object doesn't allow access to the string
    directly, so code that uses it moves to explicit casts.
    3. The mapbox variant, while attempting to recreate boost::variant,
    misses a T* get<T*>() method implementation, which allows using variant
    without exceptions.  Currently, there is an overload for
    mapbox::get_ptr implementation which replecates the functionality.
    
    Tested by: Booting the bmcweb on a target, iterating through redfish
    basic phosphor-webui usage, and websockets usage
    
    Change-Id: I2d95882908d6eb6dba00b9219a221dd96449ca7b
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>

--- include/dbus_singleton.hpp
+++ include/dbus_singleton.hpp
@@ -1,10 +1,21 @@
 #pragma once
-#include <dbus/connection.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <iostream>
+
+namespace mapbox {
+template <typename T, typename... Types>
+const T* get_ptr(const mapbox::util::variant<Types...>& v) {
+  if (v.template is<std::remove_const_t<T>>()) {
+    return &v.template get_unchecked<std::remove_const_t<T>>();
+  } else {
+    return nullptr;
+  }
+}
+}  // namespace mapbox
 
 namespace crow {
 namespace connections {
+static std::shared_ptr<sdbusplus::asio::connection> system_bus;
 
-static std::shared_ptr<dbus::connection> system_bus;
-
-}  // namespace dbus
+}  // namespace connections
 }  // namespace crow
