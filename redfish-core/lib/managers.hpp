ee5e3a4fd03edb6623b81911915706646787414a
Andrew Geissler <geissonator@yahoo.com>
2018-08-17 07:56:14 -0700

    Initial redfish logging support
    This was imported from a fork:
    https://github.com/ampere-openbmc/bmcweb/commits/ampere-next/redfish-core/lib/logservices.hpp
    
    Which had a series of commits from TungVuX and hyche
    
    The initial patch is that code verbatim.
    Follow up patches on top of this make the necessary changes
    for current bmcweb:
    - Move to sdbusplus
    - C++ naming convention changes
    - Clang format
    - Some refactoring
    
    Change-Id: I422b0d0ec577ea734fecfb6f49101ec5ff45a556
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Signed-off-by: Anthony Wilson <wilsonan@us.ibm.com>

--- redfish-core/lib/managers.hpp
+++ redfish-core/lib/managers.hpp
@@ -725,7 +725,7 @@ class Manager : public Node
         manager_reset["ResetType@Redfish.AllowableValues"] = {
             "GracefulRestart"};
 
-        res.jsonValue["DateTime"] = getDateTime();
+        res.jsonValue["DateTime"] = crow::utility::dateTimeNow();
         std::shared_ptr<AsyncResp> asyncResp = std::make_shared<AsyncResp>(res);
 
         crow::connections::systemBus->async_method_call(
@@ -987,23 +987,6 @@ class Manager : public Node
                 }
             }
         }
-    }
-
-    std::string getDateTime() const
-    {
-        std::array<char, 128> dateTime;
-        std::string redfishDateTime("0000-00-00T00:00:00Z00:00");
-        std::time_t time = std::time(nullptr);
-
-        if (std::strftime(dateTime.begin(), dateTime.size(), "%FT%T%z",
-                          std::localtime(&time)))
-        {
-            // insert the colon required by the ISO 8601 standard
-            redfishDateTime = std::string(dateTime.data());
-            redfishDateTime.insert(redfishDateTime.end() - 2, ':');
-        }
-
-        return redfishDateTime;
     }
 
     std::string uuid;
