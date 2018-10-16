6d79658b5a0f6e2c6aaabb55c901916c90fafa2e
Ed Tanous <ed.tanous@intel.com>
2018-10-16 15:29:46 -0700

    bmcweb: Move boost::optional to std::optional
    This change is largely a sed replace, and gets us closer to standards
    compliance.
    
    Change-Id: Ie60437eff43de830ef19389344a55430dabd9d16

--- redfish-core/lib/ethernet.hpp
+++ redfish-core/lib/ethernet.hpp
@@ -79,7 +79,7 @@ struct EthernetInterfaceData
     std::string hostname;
     std::string default_gateway;
     std::string mac_address;
-    boost::optional<uint32_t> vlan_id;
+    std::optional<uint32_t> vlan_id;
 };
 
 // Helper function that changes bits netmask notation (i.e. /24)
@@ -1061,7 +1061,7 @@ class EthernetInterface : public Node
                 }
             }
 
-            boost::optional<uint8_t> prefixLength;
+            std::optional<uint8_t> prefixLength;
             const std::string *subnetField = nullptr;
             nlohmann::json::const_iterator subnetFieldIt =
                 thisJson.find("SubnetMask");
