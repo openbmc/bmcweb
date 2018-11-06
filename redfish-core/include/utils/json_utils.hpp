6fd2bbee90d3c57b0ada0a9c59c89297d67bf013
Jason M. Bills <jason.m.bills@linux.intel.com>
2018-11-06 15:44:38 -0800

    Add automatic range-checking and type-casting for readJson
    This change allows numerical types to be passed to readJson
    with the desired type.  They will then be automatically
    range-checked and cast to that type.
    
    Tested: Requested variables of type uint8_t, int, and uint64_t
    and confirmed that the range was correctly checked for each.
    
    Change-Id: Ia55cb03ba2c4321a7de35a7c7b566980a72c5449
    Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>

--- redfish-core/include/utils/json_utils.hpp
+++ redfish-core/include/utils/json_utils.hpp
@@ -46,13 +46,49 @@ template <typename Type> struct unpackValue
 {
     using isRequired = std::true_type;
     using JsonType = std::add_const_t<std::add_pointer_t<Type>>;
+    Type value;
+    bool valid = false;
+    unpackValue(nlohmann::json& jsonValue)
+    {
+        if constexpr (std::is_arithmetic_v<Type>)
+        {
+            uint64_t* jsonPtr = jsonValue.get_ptr<uint64_t*>();
+            if (jsonPtr == nullptr)
+            {
+                return;
+            }
+            if (*jsonPtr > std::numeric_limits<Type>::max())
+            {
+                return;
+            }
+            value = static_cast<Type>(*jsonPtr);
+        }
+        else
+        {
+            JsonType jsonPtr = jsonValue.get_ptr<JsonType>();
+            if (jsonPtr == nullptr)
+            {
+                return;
+            }
+            value = std::move(*jsonPtr);
+        }
+
+        valid = true;
+    }
 };
 
 template <typename OptionalType>
 struct unpackValue<boost::optional<OptionalType>>
 {
     using isRequired = std::false_type;
-    using JsonType = std::add_const_t<std::add_pointer_t<OptionalType>>;
+    OptionalType value;
+    bool valid = false;
+    unpackValue(nlohmann::json& jsonValue)
+    {
+        unpackValue<OptionalType> optionalValue(jsonValue);
+        value = std::move(optionalValue.value);
+        valid = optionalValue.valid;
+    }
 };
 
 template <size_t Count, size_t Index>
@@ -78,9 +114,8 @@ void readJsonValues(const std::string& key, nlohmann::json& jsonValue,
 
     handled.set(Index);
 
-    using UnpackType = typename unpackValue<ValueType>::JsonType;
-    UnpackType value = jsonValue.get_ptr<UnpackType>();
-    if (value == nullptr)
+    unpackValue<ValueType> value(jsonValue);
+    if (!value.valid)
     {
         BMCWEB_LOG_DEBUG << "Value for key " << key
                          << " was incorrect type: " << jsonValue.type_name();
@@ -88,7 +123,7 @@ void readJsonValues(const std::string& key, nlohmann::json& jsonValue,
         return;
     }
 
-    valueToFill = *value;
+    valueToFill = std::move(value.value);
 }
 
 template <size_t Index = 0, size_t Count>
