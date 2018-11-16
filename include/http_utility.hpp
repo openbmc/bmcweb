baad780da802ecb97e751d9ff5c39306f55cc588
Ed Tanous <ed.tanous@intel.com>
2018-11-16 14:52:56 -0800

    bmcweb: support next= url for forwarding login
    Related to patchset here.  This patchset implements the bmc side of
    redirecting the users URL after login correctly.
    
    https://gerrit.openbmc-project.xyz/#/c/openbmc/phosphor-webui/+/15925
    
    Change-Id: Idf42f4cecd29fb0c6088721eb5e10fb39e1168e7
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>

--- include/http_utility.hpp
+++ include/http_utility.hpp
@@ -26,4 +26,29 @@ inline bool requestPrefersHtml(const crow::Request& req)
     }
     return false;
 }
+
+inline std::string urlEncode(const boost::string_view value)
+{
+    std::ostringstream escaped;
+    escaped.fill('0');
+    escaped << std::hex;
+
+    for (const char c : value)
+    {
+        // Keep alphanumeric and other accepted characters intact
+        if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
+        {
+            escaped << c;
+            continue;
+        }
+
+        // Any other characters are percent-encoded
+        escaped << std::uppercase;
+        escaped << '%' << std::setw(2)
+                << static_cast<int>(static_cast<unsigned char>(c));
+        escaped << std::nouppercase;
+    }
+
+    return escaped.str();
+}
 } // namespace http_helpers
