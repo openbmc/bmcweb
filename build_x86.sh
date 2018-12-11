8a0f45e33e7b3fe2abe19e8ad8728169005a624b
James Feist <james.feist@linux.intel.com>
2018-12-11 12:58:18 -0800

    build_x86.sh add missing dependencies
    Add autoconf and autoconf-archive as out of tree builds
    have to build autotools projects.
    
    Change-Id: I8daea46e0bf7d9dff493e7360e05b9043a399771
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- build_x86.sh
+++ build_x86.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo apt-get install -y libpam0g-dev libssl-dev zlib1g-dev
+sudo apt-get install -y libpam0g-dev libssl-dev zlib1g-dev \
+	autoconf-archive autoconf
 mkdir -p build
 cd build
 cmake ..
