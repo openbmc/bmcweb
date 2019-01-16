7798a58b906832c456fd02af7c6040424060af7c
Andrew Geissler <geissonator@yahoo.com>
2019-01-16 13:25:59 -0600

    Move REST error response to separate file
    This function will be useful for source files outside of its current
    source file so move to a separate file to prepare for others to use it
    
    Change-Id: I5f5d4d350f89560219818fcd03151e5a70dd4922
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

---
+++ include/openbmc_rest_error.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+#include "crow/common.h"
+
+const std::string notFoundMsg = "404 Not Found";
+const std::string badReqMsg = "400 Bad Request";
+const std::string methodNotAllowedMsg = "405 Method Not Allowed";
+const std::string forbiddenMsg = "403 Forbidden";
+const std::string methodFailedMsg = "500 Method Call Failed";
+
+const std::string notFoundDesc =
+    "org.freedesktop.DBus.Error.FileNotFound: path or object not found";
+const std::string propNotFoundDesc = "The specified property cannot be found";
+const std::string noJsonDesc = "No JSON object could be decoded";
+const std::string methodNotFoundDesc = "The specified method cannot be found";
+const std::string methodNotAllowedDesc = "Method not allowed";
+const std::string forbiddenPropDesc =
+    "The specified property cannot be created";
+const std::string forbiddenResDesc = "The specified resource cannot be created";
+
+void setErrorResponse(crow::Response &res, boost::beast::http::status result,
+                      const std::string &desc, const std::string &msg)
+{
+    res.result(result);
+    res.jsonValue = {{"data", {{"description", desc}}},
+                     {"message", msg},
+                     {"status", "error"}};
+}
