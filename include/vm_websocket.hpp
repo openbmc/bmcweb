d1bb5ddeb9ef3057101c28069115825e7a3152fb
Adriana Kobylak <anoo@us.ibm.com>
2019-01-17 12:08:38 -0600

    vm_websocket: Add websocket handler
    On receiving a websocket request on endpoint /vm/0/0,
    connect to the nbd-proxy app and send/receive stdio.
    
    Tested: Verified that the host could see the virtual
    media usb device, mounted it manually and checked
    the contents of the iso file used for the test were
    there. To test, used the html and js script:
    https://github.com/openbmc/jsnbd/tree/master/web
    and an Ubuntu iso image file.
    Verified that it worked after closing the websocket
    (using the stop function from the html file), to
    check that the processes were cleaned up and freed
    up for a subsequent request.
    
    Change-Id: I0b070310b070c086d67d0ae3e2c165551d6b87cc
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- include/vm_websocket.hpp
+++ include/vm_websocket.hpp
@@ -3,13 +3,143 @@
 #include "filesystem.hpp"
 
 #include <crow/app.h>
+#include <crow/websocket.h>
+#include <signal.h>
 
+#include <boost/process.hpp>
 #include <nlohmann/json.hpp>
+#include <webserver_common.hpp>
 
 namespace crow
 {
 namespace obmc_vm
 {
+
+namespace filesystem = std::filesystem;
+
+static crow::websocket::Connection* session = nullptr;
+
+static std::array<char, 131072> outputBuffer;
+static std::string inputBuffer;
+
+static bool doingWrite = false;
+
+class Handler
+{
+  public:
+    Handler(const std::string& media, boost::asio::io_service& ios) :
+        pipeOut(ios), pipeIn(ios), media(media)
+    {
+    }
+
+    ~Handler()
+    {
+        // boost::process::child::terminate uses SIGKILL, need to send SIGTERM
+        // to allow the proxy to stop nbd-client and the USB device gadget.
+        auto rc = kill(proxy.id(), SIGTERM);
+        if (rc)
+        {
+            return;
+        }
+        proxy.wait();
+    }
+
+    void connect()
+    {
+        std::error_code ec;
+        proxy = boost::process::child("/usr/sbin/nbd-proxy", media,
+                                      boost::process::std_out > pipeOut,
+                                      boost::process::std_in < pipeIn, ec);
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR << "Couldn't connect to nbd-proxy: "
+                             << ec.message();
+            if (session != nullptr)
+            {
+                session->close("Error connecting to nbd-proxy");
+            }
+            return;
+        }
+        doWrite();
+        doRead();
+    }
+
+    void doWrite()
+    {
+        if (doingWrite)
+        {
+            BMCWEB_LOG_DEBUG << "Already writing.  Bailing out";
+            return;
+        }
+
+        if (inputBuffer.empty())
+        {
+            BMCWEB_LOG_DEBUG << "inputBuffer empty.  Bailing out";
+            return;
+        }
+
+        doingWrite = true;
+        pipeIn.async_write_some(
+            boost::asio::buffer(inputBuffer.data(), inputBuffer.size()),
+            [this](boost::beast::error_code ec, std::size_t bytes_written) {
+                BMCWEB_LOG_DEBUG << "Wrote " << bytes_written << "bytes";
+                doingWrite = false;
+                inputBuffer.erase(0, bytes_written);
+
+                if (session == nullptr)
+                {
+                    return;
+                }
+                if (ec == boost::asio::error::eof)
+                {
+                    session->close("VM socket port closed");
+                    return;
+                }
+                if (ec)
+                {
+                    session->close("Error in writing to proxy port");
+                    BMCWEB_LOG_ERROR << "Error in VM socket write " << ec;
+                    return;
+                }
+                doWrite();
+            });
+    }
+
+    void doRead()
+    {
+        pipeOut.async_read_some(
+            boost::asio::buffer(outputBuffer.data(), outputBuffer.size()),
+            [this](const boost::system::error_code& ec, std::size_t bytesRead) {
+                BMCWEB_LOG_DEBUG << "read done.  Read " << bytesRead
+                                 << " bytes";
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "Couldn't read from VM port: " << ec;
+                    if (session != nullptr)
+                    {
+                        session->close("Error in connecting to VM port");
+                    }
+                    return;
+                }
+                if (session == nullptr)
+                {
+                    return;
+                }
+
+                boost::beast::string_view payload(outputBuffer.data(),
+                                                  bytesRead);
+                session->sendBinary(payload);
+                doRead();
+            });
+    }
+
+    boost::process::async_pipe pipeOut;
+    boost::process::async_pipe pipeIn;
+    boost::process::child proxy;
+    std::string media;
+};
+
+std::unique_ptr<Handler> handler;
 
 template <typename... Middlewares> void requestRoutes(Crow<Middlewares...>& app)
 {
@@ -76,6 +206,43 @@ template <typename... Middlewares> void requestRoutes(Crow<Middlewares...>& app)
                              {"message", "200 OK"},
                              {"status", "ok"}};
             res.end();
+        });
+
+    BMCWEB_ROUTE(app, "/vm/0/0")
+        .websocket()
+        .onopen([](crow::websocket::Connection& conn) {
+            BMCWEB_LOG_DEBUG << "Connection " << &conn << " opened";
+
+            if (session != nullptr)
+            {
+                conn.close("Session already connected");
+                return;
+            }
+
+            session = &conn;
+
+            if (handler != nullptr)
+            {
+                conn.close("Handler already running");
+                return;
+            }
+
+            // TODO Support /vm/0/<media> in the endpoint path, hardcode for now
+            auto media = "0"; // <media>
+            handler = std::make_unique<Handler>(media, conn.get_io_context());
+            handler->connect();
+        })
+        .onclose(
+            [](crow::websocket::Connection& conn, const std::string& reason) {
+                session = nullptr;
+                handler = nullptr;
+                inputBuffer.clear();
+                inputBuffer.shrink_to_fit();
+            })
+        .onmessage([](crow::websocket::Connection& conn,
+                      const std::string& data, bool is_binary) {
+            inputBuffer += data;
+            handler->doWrite();
         });
 }
 
