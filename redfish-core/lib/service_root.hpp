d26e2b394df1d9e4e1eb92ba7dd29989179059a8
Marri Devender Rao <devenrao@in.ibm.com>
2019-01-21 10:27:12 -0600

    Redfish: Add certificate service to manage certificates
    Catered for HTTPS and LDAP certificates
    
    1. Tested schema with validator and no issues
    2. GET on /redfish/v1/CertificateService/
      "CertificateService": {
        "@odata.id": "/redfish/v1/CertificateService"
      },
    
    3. GET on /redfish/v1/CertificateService/CertificateLocations/
      "@odata.context":
    "/redfish/v1/$metadata#CertificateLocations.CertificateLocations",
      "@odata.id": "/redfish/v1/CertificateService/CertificateLocations",
      "@odata.type": "#CertificateLocations.v1_0_0.CertificateLocations",
      "Description": "Defines a resource that an administrator can use in order to
    locate all certificates installed on a given service",
      "Id": "CertificateLocations",
      "Name": "Certificate Locations"
    
    Change-Id: I19bd1867e5da22c0c6a5e5219ee97c8cac0b486f
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- redfish-core/lib/service_root.hpp
+++ redfish-core/lib/service_root.hpp
@@ -40,8 +40,8 @@ class ServiceRoot : public Node
     void doGet(crow::Response& res, const crow::Request& req,
                const std::vector<std::string>& params) override
     {
-        res.jsonValue["@odata.type"] = "#ServiceRoot.v1_1_1.ServiceRoot";
-        res.jsonValue["@odata.id"] = "/redfish/v1";
+        res.jsonValue["@odata.type"] = "#ServiceRoot.v1_5_0.ServiceRoot";
+        res.jsonValue["@odata.id"] = "/redfish/v1/";
         res.jsonValue["@odata.context"] =
             "/redfish/v1/$metadata#ServiceRoot.ServiceRoot";
         res.jsonValue["Id"] = "RootService";
@@ -63,7 +63,8 @@ class ServiceRoot : public Node
 
         res.jsonValue["UpdateService"] = {
             {"@odata.id", "/redfish/v1/UpdateService"}};
-
+        res.jsonValue["CertificateService"] = {
+            {"@odata.id", "/redfish/v1/CertificateService"}};
         res.jsonValue["UUID"] = getUuid();
         res.end();
     }
