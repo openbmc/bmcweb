a67c7978a78cf45e606959f825bab8a6d6819320
Marri Devender Rao <devenrao@in.ibm.com>
2019-01-21 10:27:12 -0600

    Redfish: Add Certificate Service, Collection, Location scehma
    Added CertificateService, CertificateLocations schemas
    Added CertificateCollection schema for HTTPS certficates
    
    Tested:
    1.Tested schema with validator and no issues
    2.Privilege map for certificate service is not yet pubished
    3.GET on root service
    curl -b cjar -k https://${bmc}/redfish/v1
    {
      ...
      "CertificateService": {
        "@odata.id": "/redfish/v1/CertificateService"
      },
      ...
    }
    
    3.POST on /redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates
    {
      "@Message.ExtendedInfo": [
        {
          "@odata.type": "/redfish/v1/$metadata#Message.v1_0_0.Message",
          "Message": "Successfully Completed Request",
          "MessageArgs": [],
          "MessageId": "Base.1.4.0.Success",
          "Resolution": "None",
          "Severity": "OK"
        }
      ]
    }
    
    4.GET on /redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/
    {
      "@odata.context":
    "/redfish/v1/$metadata#CertificateCollection.CertificateCollection",
      "@odata.id": "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates",
      "@odata.type": "#CertificateCollection.CertificatesCollection",
      "Description": "A Collection of HTTPS certificate instances",
      "Members": [
        {
          "@odata.id":
          "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/1"
        }
      ],
      "Members@odata.count": 1,
      "Name": "HTTPS Certificate Collection"
    }
    
    5.GET on /redfish/v1/CertificateService/CertificateLocations/
    {
      "@odata.context":
    "/redfish/v1/$metadata#CertificateLocations.CertificateLocations",
      "@odata.id": "/redfish/v1/CertificateService/CertificateLocations",
      "@odata.type": "#CertificateLocations.v1_0_0.CertificateLocations",
      "Description": "Defines a resource that an administrator can use in order to
        locate all certificates installed on a given service",
      "Id": "CertificateLocations",
      "Links": {
        "Certificates": [
          {
            "@odata.id":
            "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/1"
          }
        ],
        "Certificates@odata.count": 1
      },
      "Name": "Certificate Locations"
    }
    
    Change-Id: I19bd1867e5da22c0c6a5e5219ee97c8cac0b486f
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

---
+++ redfish-core/lib/certificate_service.hpp
@@ -0,0 +1,265 @@
+/*
+// Copyright (c) 2018 IBM Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+
+#include "node.hpp"
+
+namespace redfish
+{
+static const char *httpsServiceName =
+    "xyz.openbmc_project.Certs.Manager.Server.Https";
+static const char *httpsObjectPath = "/xyz/openbmc_project/certs/server/https";
+static const char *certInstallIntf = "xyz.openbmc_project.Certs.Install";
+static const char *dbusObjManagerIntf = "org.freedesktop.DBus.ObjectManager";
+
+/**
+ * The Certificate schema defines a Certificate Service which represents the
+ * actions available to manage certificates and links to where certificates
+ * are installed.
+ */
+class CertificateService : public Node
+{
+  public:
+    CertificateService(CrowApp &app) :
+        Node(app, "/redfish/v1/CertificateService/")
+    {
+        // TODO (devenrao) No entries are available for Certificate
+        // sevice at https://www.dmtf.org/standards/redfish 
+        // "redfish standard registries". Need to modify after DMTF
+        // publish Privilege details for certificate service
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::put, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::post, {{"ConfigureComponents"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        res.jsonValue = {
+            {"@odata.type", "#CertificateService.v1_0_0.CertificateService"},
+            {"@odata.id", "/redfish/v1/CertificateService"},
+            {"@odata.context",
+             "/redfish/v1/$metadata#CertificateService.CertificateService"},
+            {"Id", "CertificateService"},
+            {"Name", "Certificate Service"},
+            {"Description", "Actions available to manage certificates"},
+            {"@odata.id",
+             "/redfish/v1/CertificateService/CertificateLocations"}};
+        res.end();
+    }
+}; // CertificateService
+
+/**
+ * @brief Find the ID specified in the URL
+ * Finds the numbers specified after the last "/" in the URL and returns.
+ * @param[in] path URL
+ * @return 0 on failure and number on success
+ */
+int getIDFromURL(const std::string &path)
+{
+    std::size_t found = path.rfind("/");
+    if ((found != std::string::npos) && (found++ <= path.length()))
+    {
+        return atoi(path.substr(found).c_str());
+    }
+    return 0;
+}
+
+/**
+ * Collection of HTTPS certificates
+ */
+class HTTPSCertificateCollection : public Node
+{
+  public:
+    template <typename CrowApp>
+    HTTPSCertificateCollection(CrowApp &app) :
+        Node(app,
+             "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::put, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::post, {{"ConfigureComponents"}}}};
+    }
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        BMCWEB_LOG_DEBUG << "HTTPSCertificateCollection::doGet ";
+        res.jsonValue = {
+            {"@odata.id",
+             "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates"},
+            {"@odata.type", "#CertificateCollection.CertificatesCollection"},
+            {"@odata.context",
+             "/redfish/v1/"
+             "$metadata#CertificateCollection.CertificateCollection"},
+            {"Name", "HTTPS Certificates Collection"},
+            {"Description", "A Collection of HTTPS certificate instances"}};
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        auto getCertificateList = [asyncResp](
+                                      const boost::system::error_code ec,
+                                      const ManagedObjectType &certs) {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            nlohmann::json &members = asyncResp->res.jsonValue["Members"];
+            members = nlohmann::json::array();
+            for (const auto &cert : certs)
+            {
+                auto id = getIDFromURL(cert.first.str);
+                if (id)
+                {
+                    members.push_back(
+                        {{"@odata.id", "/redfish/v1/Managers/bmc/"
+                                       "NetworkProtocol/HTTPS/Certificates/" +
+                                           std::to_string(id)}});
+                }
+            }
+            asyncResp->res.jsonValue["Members@odata.count"] = certs.size();
+        };
+        crow::connections::systemBus->async_method_call(
+            std::move(getCertificateList), httpsServiceName, httpsObjectPath,
+            "org.freedesktop.DBus.ObjectManager", "GetManagedObjects");
+    }
+
+    void doPost(crow::Response &res, const crow::Request &req,
+                const std::vector<std::string> &params) override
+    {
+        BMCWEB_LOG_DEBUG << "HTTPSCertificateCollection::doPost";
+
+        std::string filepath("/tmp/" + boost::uuids::to_string(
+                                           boost::uuids::random_generator()()));
+        std::ofstream out(filepath, std::ofstream::out | std::ofstream::binary |
+                                        std::ofstream::trunc);
+        out << req.body;
+        out.close();
+
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        auto installCertificate =
+            [asyncResp, filepath](const boost::system::error_code ec) {
+                std::remove(filepath.c_str());
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                messages::success(asyncResp->res);
+            };
+        crow::connections::systemBus->async_method_call(
+            std::move(installCertificate), httpsServiceName, httpsObjectPath,
+            certInstallIntf, "Install", filepath);
+    }
+}; // HTTPSCertificateCollection
+
+/**
+ * @brief Retrieve the certificates installed list and append to the response
+ *
+ * @param[in] asyncResp Shared pointer to the response message
+ * @param[in] certURL  Path of the certificate object
+ * @param[in] service  D-Bus service
+ * @param[in] path  Path of the D-Bus service object
+ * @return None
+ */
+void getCertificateLocations(std::shared_ptr<AsyncResp> asyncResp,
+                             const std::string &certURL,
+                             const std::string &service,
+                             const std::string &path)
+{
+    BMCWEB_LOG_DEBUG << "getCertificateLocations URI=" << certURL
+                     << " Service=" << service << " Path=" << path;
+    auto getCertLocations = [asyncResp,
+                             certURL](const boost::system::error_code ec,
+                                      const ManagedObjectType &certs) {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        nlohmann::json &links =
+            asyncResp->res.jsonValue["Links"]["Certificates"];
+        links = nlohmann::json::array();
+        for (auto &cert : certs)
+        {
+            auto id = getIDFromURL(cert.first.str);
+            if (id)
+            {
+                links.push_back({{"@odata.id", certURL + std::to_string(id)}});
+            }
+        }
+        asyncResp->res.jsonValue["Links"]["Certificates@odata.count"] =
+            links.size();
+    };
+    crow::connections::systemBus->async_method_call(
+        std::move(getCertLocations), service, path, dbusObjManagerIntf,
+        "GetManagedObjects");
+}
+
+/**
+ * The certificate location schema defines a resource that an administrator
+ * can use in order to locate all certificates installed on a given service.
+ */
+class CertificateLocations : public Node
+{
+  public:
+    template <typename CrowApp>
+    CertificateLocations(CrowApp &app) :
+        Node(app, "/redfish/v1/CertificateService/CertificateLocations/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::put, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::post, {{"ConfigureComponents"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        res.jsonValue = {
+            {"@odata.id",
+             "/redfish/v1/CertificateService/CertificateLocations"},
+            {"@odata.type",
+             "#CertificateLocations.v1_0_0.CertificateLocations"},
+            {"@odata.context",
+             "/redfish/v1/$metadata#CertificateLocations.CertificateLocations"},
+            {"Name", "Certificate Locations"},
+            {"Id", "CertificateLocations"},
+            {"Description",
+             "Defines a resource that an administrator can use in order to"
+             "locate all certificates installed on a given service"}};
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        getCertificateLocations(
+            asyncResp,
+            "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/",
+            httpsServiceName, httpsObjectPath);
+    }
+}; // CertificateLocations
+} // namespace redfish
