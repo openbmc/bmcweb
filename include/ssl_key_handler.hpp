48578528524c2961ae5c66ced79f96fc696a70ec
Ed Tanous <ed.tanous@intel.com>
2019-02-07 15:27:14 -0800

    bmcweb: update TLS cipher suites to compat "C"
    This is the second attempt to make cipher suites more restrictive.  This
    was previously attempted in patchset
    54fd221a9139f46c7c95b4a22cc09e6e7ce4cbbc
    
    Which caused some compatibility issues.  This is the second attempt,
    which does a better job.
    
    Previously, bmcweb was utilitizing the "mozilla compatibility" cipher
    suites.  This is overly lenient on broken ciphers and can cause some
    issues with security reviews.  In researching this, it looks like we
    never actually documented that we follow Mozilla ciphers, aside from the
    statement "The OpenBMC webserver shall follow the latest OWASP recommendations for
      authentication, session management, and security."  Considering that
    we're moving _to_ OWASP recommendations, this commit is simply making us
    follow the advice we already document, although this commit also updates
    the documentation to be more clear.
    
    This commit also fixes our TLS logic to support _only_ TLS 1.2 and 1.3,
    as only updating the cipher suites caused us to fall into TLS 1.3 only,
    which broke compatibility with a lot of operating systems.
    
    Tested by:
    Ran the testssl.sh script here:
    https://github.com/drwetter/testssl.sh
    
    Observed the following in the output:
     TODO
    
    Change-Id: I92a77807848354379ea2cbaa31767eeceadae295
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>

--- include/ssl_key_handler.hpp
+++ include/ssl_key_handler.hpp
@@ -305,55 +305,30 @@ inline boost::asio::ssl::context getSslContext(const std::string &ssl_pem_file)
         BMCWEB_LOG_ERROR << "Error setting tmp ecdh list\n";
     }
 
-    // From mozilla "compatibility"
-    std::string mozillaCompatibilityCiphers = "ECDHE-ECDSA-CHACHA20-POLY1305:"
-                                              "ECDHE-RSA-CHACHA20-POLY1305:"
-                                              "ECDHE-ECDSA-AES128-GCM-SHA256:"
-                                              "ECDHE-RSA-AES128-GCM-SHA256:"
-                                              "ECDHE-ECDSA-AES256-GCM-SHA384:"
-                                              "ECDHE-RSA-AES256-GCM-SHA384:"
-                                              "DHE-RSA-AES128-GCM-SHA256:"
-                                              "DHE-RSA-AES256-GCM-SHA384:"
-                                              "ECDHE-ECDSA-AES128-SHA256:"
-                                              "ECDHE-RSA-AES128-SHA256:"
-                                              "ECDHE-ECDSA-AES128-SHA:"
-                                              "ECDHE-RSA-AES256-SHA384:"
-                                              "ECDHE-RSA-AES128-SHA:"
-                                              "ECDHE-ECDSA-AES256-SHA384:"
-                                              "ECDHE-ECDSA-AES256-SHA:"
-                                              "ECDHE-RSA-AES256-SHA:"
-                                              "DHE-RSA-AES128-SHA256:"
-                                              "DHE-RSA-AES128-SHA:"
-                                              "DHE-RSA-AES256-SHA256:"
-                                              "DHE-RSA-AES256-SHA:"
-                                              "ECDHE-ECDSA-DES-CBC3-SHA:"
-                                              "ECDHE-RSA-DES-CBC3-SHA:"
-                                              "EDH-RSA-DES-CBC3-SHA:"
-                                              "AES128-GCM-SHA256:"
-                                              "AES256-GCM-SHA384:"
-                                              "AES128-SHA256:"
-                                              "AES256-SHA256:"
-                                              "AES128-SHA:"
-                                              "AES256-SHA:"
-                                              "DES-CBC3-SHA:"
-                                              "!DSS";
+    // From OWASP C recommendations
+    // https://www.owasp.org/index.php/TLS_Cipher_String_Cheat_Sheet
 
-    // From mozilla "modern"
-    std::string mozillaModernCiphers = "ECDHE-ECDSA-AES256-GCM-SHA384:"
-                                       "ECDHE-RSA-AES256-GCM-SHA384:"
-                                       "ECDHE-ECDSA-CHACHA20-POLY1305:"
-                                       "ECDHE-RSA-CHACHA20-POLY1305:"
-                                       "ECDHE-ECDSA-AES128-GCM-SHA256:"
-                                       "ECDHE-RSA-AES128-GCM-SHA256:"
-                                       "ECDHE-ECDSA-AES256-SHA384:"
-                                       "ECDHE-RSA-AES256-SHA384:"
-                                       "ECDHE-ECDSA-AES128-SHA256:"
-                                       "ECDHE-RSA-AES128-SHA256";
+    std::string owaspC = "DHE-RSA-AES256-GCM-SHA384"
+                         "DHE-RSA-AES128-GCM-SHA256"
+                         "ECDHE-RSA-AES256-GCM-SHA384"
+                         "ECDHE-RSA-AES128-GCM-SHA256"
+                         "DHE-RSA-AES256-SHA256"
+                         "DHE-RSA-AES128-SHA256"
+                         "ECDHE-RSA-AES256-SHA384"
+                         "ECDHE-RSA-AES128-SHA256"
+                         "ECDHE-RSA-AES256-SHA"
+                         "ECDHE-RSA-AES128-SHA"
+                         "AES256-GCM-SHA384"
+                         "AES128-GCM-SHA256"
+                         "AES256-SHA256"
+                         "AES128-SHA256"
+                         "AES256-SHA"
+                         "AES128-SHA"
+                         "DHE-RSA-AES256-SHA"
+                         "DHE-RSA-AES128-SHA";
 
-    std::string aesOnlyCiphers = "AES128+EECDH:AES128+EDH:!aNULL:!eNULL";
-
-    if (SSL_CTX_set_cipher_list(mSslContext.native_handle(),
-                                mozillaCompatibilityCiphers.c_str()) != 1)
+    if (SSL_CTX_set_cipher_list(mSslContext.native_handle(), owaspC.c_str()) !=
+        1)
     {
         BMCWEB_LOG_ERROR << "Error setting cipher list\n";
     }
