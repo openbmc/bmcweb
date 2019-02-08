openbmc/bmcweb

commit 54fd221a9139f46c7c95b4a22cc09e6e7ce4cbbc

bmcweb: update SSL cipher suites to OWASP compatB
Previously, bmcweb was utilitizing the "mozilla compatibility" cipher
suites.  This is overly lenient on broken ciphers and can cause some
issues with security reviews.  In researching this, it looks like we
never actually documented that we follow Mozilla ciphers, aside from the
statement "The OpenBMC webserver shall follow the latest OWASP recommendations for
  authentication, session management, and security."  Considering that
we're moving _to_ OWASP recommendations, this commit is simply making us
follow the advice we already document, although this commit also updates
the documentation to be more clear.

Tested By:
Loaded on a BMC, opened web page in browser, and observed phosphor-webui
loaded correctly.

Change-Id: I912b35d378ce955c1472b2d54f1a365f6efea160
Signed-off-by: Ed Tanous <ed.tanous@intel.com>

include/ssl_key_handler.hpp
@@ -352,8 +352,23 @@ inline boost::asio::ssl::context getSslContext(const std::string &ssl_pem_file)
 
     std::string aesOnlyCiphers = "AES128+EECDH:AES128+EDH:!aNULL:!eNULL";
 
+    // OWASP Cipher String 'B' (Broad compatibility to browsers)
+    // https://www.owasp.org/index.php/TLS_Cipher_String_Cheat_Sheet
+    std::string owaspBroadCompatibility = "DHE-RSA-AES256-GCM-SHA384:"
+                                          "DHE-RSA-AES128-GCM-SHA256:"
+                                          "ECDHE-RSA-AES256-GCM-SHA384:"
+                                          "ECDHE-RSA-AES128-GCM-SHA256:"
+                                          "DHE-RSA-AES256-SHA256:"
+                                          "DHE-RSA-AES128-SHA256:"
+                                          "ECDHE-RSA-AES256-SHA384:"
+                                          "ECDHE-RSA-AES128-SHA256:"
+                                          "ECDHE-RSA-AES256-SHA:"
+                                          "ECDHE-RSA-AES128-SHA:"
+                                          "DHE-RSA-AES256-SHA:"
+                                          "DHE-RSA-AES128-SHA";
+
     if (SSL_CTX_set_cipher_list(mSslContext.native_handle(),
-                                mozillaCompatibilityCiphers.c_str()) != 1)
+                                owaspBroadCompatibility.c_str()) != 1)
     {
         BMCWEB_LOG_ERROR << "Error setting cipher list\n";
     }

commit 429fce2a68f7bc13f336da1616bd27ff8499f159

bmcweb: update TLS cipher suites to mozilla modern
This is the second attempt to make cipher suites more restrictive.  This
was previously attempted in patchset
54fd221a9139f46c7c95b4a22cc09e6e7ce4cbbc

Which caused some compatibility issues.  This is the second attempt,
which does a better job.  OWASP does not enable eppiliptic curve TLS 1.2
cipher suites, which at this time are required for bmcweb to function.

Previously, bmcweb was utilitizing the "mozilla compatibility" cipher
suites.  This is overly lenient on broken ciphers and can cause some
issues with security reviews.

This commit also fixes our TLS logic to support _only_ TLS 1.2 and 1.3,
as only updating the cipher suites caused us to fall into TLS 1.3 only,
which broke compatibility with a lot of operating systems.

Tested by:
Ran the testssl.sh script here:
https://github.com/drwetter/testssl.sh

Observed the following in the output:

    testssl.sh       3.0rc3 from https://testssl.sh/dev/
    (977dc1ac4 2019-02-06 22:42:14 -- )

      This program is free software. Distribution and
             modification under GPLv2 permitted.
      USAGE w/o ANY WARRANTY. USE IT AT YOUR OWN RISK!

       Please file bugs @ https://testssl.sh/bugs/

 Using "OpenSSL 1.0.2-chacha (1.0.2k-dev)" [~183 ciphers]
 on Hades:/home/ed/testssl.sh/bin/openssl.Linux.x86_64
 (built: "Jan 18 17:12:17 2019", platform: "linux-x86_64")

 Start 2019-02-08 11:04:50        -->> 10.243.49.64:18080 (10.243.49.64) <<--

 rDNS (10.243.49.64):    wolfpass.jf.intel.com.
 Service detected:       HTTP

 Testing protocols via sockets except NPN+ALPN

 SSLv2      not offered (OK)
 SSLv3      not offered (OK)
 TLS 1      not offered
 TLS 1.1    not offered
 TLS 1.2    offered (OK)
 TLS 1.3    offered (OK): final
 NPN/SPDY   not offered
 ALPN/HTTP2 not offered

 Testing cipher categories

 NULL ciphers (no encryption)                  not offered (OK)
 Anonymous NULL Ciphers (no authentication)    not offered (OK)

crow/include/crow/app.h
@@ -156,7 +156,9 @@ template <typename... Middlewares> class Crow
         sslContext.use_private_key_file(key_filename, ssl_context_t::pem);
         sslContext.set_options(boost::asio::ssl::context::default_workarounds |
                                boost::asio::ssl::context::no_sslv2 |
-                               boost::asio::ssl::context::no_sslv3);
+                               boost::asio::ssl::context::no_sslv3 |
+                               boost::asio::ssl::context::no_tlsv1 |
+                               boost::asio::ssl::context::no_tlsv1_1);
         return *this;
     }
 
@@ -166,7 +168,9 @@ template <typename... Middlewares> class Crow
         sslContext.load_verify_file(pem_filename);
         sslContext.set_options(boost::asio::ssl::context::default_workarounds |
                                boost::asio::ssl::context::no_sslv2 |
-                               boost::asio::ssl::context::no_sslv3);
+                               boost::asio::ssl::context::no_sslv3 |
+                               boost::asio::ssl::context::no_tlsv1 |
+                               boost::asio::ssl::context::no_tlsv1_1);
         return *this;
     }
 
@@ -176,7 +180,7 @@ template <typename... Middlewares> class Crow
         return *this;
     }
 
-    ssl_context_t sslContext{boost::asio::ssl::context::sslv23};
+    ssl_context_t sslContext{boost::asio::ssl::context::tls_server};
 
 #else
     template <typename T, typename... Remain> self_t& ssl_file(T&&, Remain&&...)

include/ssl_key_handler.hpp
@@ -282,7 +282,8 @@ inline void ensureOpensslKeyPresentAndValid(const std::string &filepath)
 
 inline boost::asio::ssl::context getSslContext(const std::string &ssl_pem_file)
 {
-    boost::asio::ssl::context mSslContext{boost::asio::ssl::context::sslv23};
+    boost::asio::ssl::context mSslContext{
+        boost::asio::ssl::context::tls_server};
     mSslContext.set_options(boost::asio::ssl::context::default_workarounds |
                             boost::asio::ssl::context::no_sslv2 |
                             boost::asio::ssl::context::no_sslv3 |
@@ -305,55 +306,19 @@ inline boost::asio::ssl::context getSslContext(const std::string &ssl_pem_file)
         BMCWEB_LOG_ERROR << "Error setting tmp ecdh list\n";
     }
 
-    // From mozilla "compatibility"
-    std::string mozillaCompatibilityCiphers = "ECDHE-ECDSA-CHACHA20-POLY1305:"
-                                              "ECDHE-RSA-CHACHA20-POLY1305:"
-                                              "ECDHE-ECDSA-AES128-GCM-SHA256:"
-                                              "ECDHE-RSA-AES128-GCM-SHA256:"
-                                              "ECDHE-ECDSA-AES256-GCM-SHA384:"
-                                              "ECDHE-RSA-AES256-GCM-SHA384:"
-                                              "DHE-RSA-AES128-GCM-SHA256:"
-                                              "DHE-RSA-AES256-GCM-SHA384:"
-                                              "ECDHE-ECDSA-AES128-SHA256:"
-                                              "ECDHE-RSA-AES128-SHA256:"
-                                              "ECDHE-ECDSA-AES128-SHA:"
-                                              "ECDHE-RSA-AES256-SHA384:"
-                                              "ECDHE-RSA-AES128-SHA:"
-                                              "ECDHE-ECDSA-AES256-SHA384:"
-                                              "ECDHE-ECDSA-AES256-SHA:"
-                                              "ECDHE-RSA-AES256-SHA:"
-                                              "DHE-RSA-AES128-SHA256:"
-                                              "DHE-RSA-AES128-SHA:"
-                                              "DHE-RSA-AES256-SHA256:"
-                                              "DHE-RSA-AES256-SHA:"
-                                              "ECDHE-ECDSA-DES-CBC3-SHA:"
-                                              "ECDHE-RSA-DES-CBC3-SHA:"
-                                              "EDH-RSA-DES-CBC3-SHA:"
-                                              "AES128-GCM-SHA256:"
-                                              "AES256-GCM-SHA384:"
-                                              "AES128-SHA256:"
-                                              "AES256-SHA256:"
-                                              "AES128-SHA:"
-                                              "AES256-SHA:"
-                                              "DES-CBC3-SHA:"
-                                              "!DSS";
-
-    // From mozilla "modern"
-    std::string mozillaModernCiphers = "ECDHE-ECDSA-AES256-GCM-SHA384:"
-                                       "ECDHE-RSA-AES256-GCM-SHA384:"
-                                       "ECDHE-ECDSA-CHACHA20-POLY1305:"
-                                       "ECDHE-RSA-CHACHA20-POLY1305:"
-                                       "ECDHE-ECDSA-AES128-GCM-SHA256:"
-                                       "ECDHE-RSA-AES128-GCM-SHA256:"
-                                       "ECDHE-ECDSA-AES256-SHA384:"
-                                       "ECDHE-RSA-AES256-SHA384:"
-                                       "ECDHE-ECDSA-AES128-SHA256:"
-                                       "ECDHE-RSA-AES128-SHA256";
-
-    std::string aesOnlyCiphers = "AES128+EECDH:AES128+EDH:!aNULL:!eNULL";
+    std::string mozillaModern = "ECDHE-ECDSA-AES256-GCM-SHA384:"
+                                "ECDHE-RSA-AES256-GCM-SHA384:"
+                                "ECDHE-ECDSA-CHACHA20-POLY1305:"
+                                "ECDHE-RSA-CHACHA20-POLY1305:"
+                                "ECDHE-ECDSA-AES128-GCM-SHA256:"
+                                "ECDHE-RSA-AES128-GCM-SHA256:"
+                                "ECDHE-ECDSA-AES256-SHA384:"
+                                "ECDHE-RSA-AES256-SHA384:"
+                                "ECDHE-ECDSA-AES128-SHA256:"
+                                "ECDHE-RSA-AES128-SHA256";
 
     if (SSL_CTX_set_cipher_list(mSslContext.native_handle(),
-                                mozillaCompatibilityCiphers.c_str()) != 1)
+                                mozillaModern.c_str()) != 1)
     {
         BMCWEB_LOG_ERROR << "Error setting cipher list\n";
     }