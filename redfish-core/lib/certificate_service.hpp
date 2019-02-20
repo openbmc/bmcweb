02e9fa19abe80df3a9305c5f53e9d1660c0793a0
Marri Devender Rao <devenrao@in.ibm.com>
2019-02-20 01:07:01 -0600

    Redfish: Add ReplaceCertificate Action of CertificateService
    This change adds ReplaceCertificate Action for replacing
    HTTPS and LDAP certificates
    
    1.Tested schema with validator and no issues
    2.Privilege map for certificate service is not yet pubished
    3.Replace certificate
    curl -c cjar -b cjar -k -H "X-Auth-Token: $bmc_token" -X POST
    https://${bmc}/redfish/v1/CertificateService/Actions/CertificateService.ReplaceCertificate/
    -d @data_https.json
    
    {
      "@Message.ExtendedInfo": [
        {
          "@odata.type": "/redfish/v1/$metadata#Message.v1_0_0.Message",
          "Message": "Successfully Completed Request",
          "MessageArgs": [],
          "MessageId": "Base.1.4.0.Success",
          "Resolution": "None",
          "Severity": "OK"
        }
      ]
    }
    
    Change-Id: I2acbf8afa06bbf7d029d4971f7ab3b3988f5f060
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- redfish-core/lib/certificate_service.hpp
+++ redfish-core/lib/certificate_service.hpp
@@ -69,6 +69,12 @@ class CertificateService : public Node
         res.jsonValue["CertificateLocations"] = {
             {"@odata.id",
              "/redfish/v1/CertificateService/CertificateLocations"}};
+        auto &replaceCert =
+            res.jsonValue["Actions"]["#CertificateService.ReplaceCertificate"];
+        replaceCert["target"] = "/redfish/v1/CertificateService/Actions/"
+                                "CertificateService.ReplaceCertificate";
+        replaceCert["CertificateType@Redfish.AllowableValues"] = {"PEM",
+                                                                  "PKCS7"};
         res.end();
     }
 }; // CertificateService
@@ -89,6 +95,109 @@ int getIDFromURL(const std::string &path)
 }
 using GetObjectType =
     std::vector<std::pair<std::string, std::vector<std::string>>>;
+
+/**
+ * Action to replace an existing certificate
+ */
+class CertificateActionsReplaceCertificate : public Node
+{
+  public:
+    CertificateActionsReplaceCertificate(CrowApp &app) :
+        Node(app, "/redfish/v1/CertificateService/Actions/"
+                  "CertificateService.ReplaceCertificate/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::put, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::post, {{"ConfigureManager"}}}};
+    }
+
+  private:
+    void doPost(crow::Response &res, const crow::Request &req,
+                const std::vector<std::string> &params) override
+    {
+        std::string certificate;
+        std::string certificateType;
+        std::string certificateUri;
+        if (!json_util::readJson(req, res, "CertificateString", certificate,
+                                 "CertificateType", certificateType,
+                                 "CertificateUri", certificateUri))
+        {
+            BMCWEB_LOG_ERROR << "Required parameters are missing";
+            return;
+        }
+        auto id = getIDFromURL(certificateUri);
+        if (!id)
+        {
+            BMCWEB_LOG_ERROR << "Invalid certificate URI";
+            return;
+        }
+
+        std::string filepath("/tmp/" + boost::uuids::to_string(
+                                           boost::uuids::random_generator()()));
+        std::ofstream out(filepath, std::ofstream::out | std::ofstream::binary |
+                                        std::ofstream::trunc);
+        out << certificate;
+        out.close();
+
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        auto replaceCertificate =
+            [asyncResp, filepath](const boost::system::error_code ec) {
+                std::remove(filepath.c_str());
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                messages::success(asyncResp->res);
+            };
+        std::string objecPath;
+        if (boost::starts_with(
+                certificateUri,
+                "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/"))
+        {
+            objecPath = std::string(httpsObjectPath) + "/" + std::to_string(id);
+        }
+        else if (boost::starts_with(
+                     certificateUri,
+                     "/redfish/v1/AccountService/LDAP/Certificates/"))
+        {
+            objecPath = std::string(ldapObjectPath) + "/" + std::to_string(id);
+        }
+        else
+        {
+            BMCWEB_LOG_ERROR << "Unsupported certificate URI" << certificateUri;
+            return;
+        }
+        if (!objecPath.empty())
+        {
+            auto getServiceName = [asyncResp, replaceCertificate, objecPath,
+                                   filepath](const boost::system::error_code ec,
+                                             const GetObjectType &resp) {
+                if (ec || resp.empty())
+                {
+                    BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                std::string service = resp.begin()->first;
+                BMCWEB_LOG_DEBUG << "Replace certificate for service: "
+                                 << service;
+                crow::connections::systemBus->async_method_call(
+                    std::move(replaceCertificate), service, objecPath,
+                    certInstallIntf, "Install", filepath);
+            };
+            crow::connections::systemBus->async_method_call(
+                std::move(getServiceName), mapperBusName, mapperObjectPath,
+                mapperIntf, "GetObject", objecPath,
+                std::array<std::string, 0>());
+        }
+    }
+}; // CertificateActionsReplaceCertificate
 
 /**
  * @brief Converts EPOC time to redfish time format
