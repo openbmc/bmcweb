521df56c7a7a8a64363b3a8094500f224ebdfe04
Marri Devender Rao <devenrao@in.ibm.com>
2019-02-20 01:05:22 -0600

    Redfish: Add LDAP Certificate Collection and LDAP Certificate
    GET on LDAP certificate collection returns links to
    LDAP certificates installed in the system.
    
    POST on LDAP certificate collection installs certificate onto
    the system.
    
    GET on LDAP certificate returns certificate properties.
    
    Tested:
    1. Tested schema with validator and no issues
    2. Privilege map for certificate service is not yet pubished
    3. POST on /redfish/v1/AccountService/LDAP/Certificates
    curl -c cjar -b cjar -k -H "X-Auth-Token: $bmc_token" -H "Content-Type: application/octet-stream" -X POST -T testcert.pem https://${bmc}/redfish/v1/AccountServie/LDAP/Certificates
    {
      "@odata.context": "/redfish/v1/$metadata#Certificate.Certificate",
      "@odata.id": "/redfish/v1/AccountService/LDAP/Certificates/1",
      "@odata.type": "#Certificate.v1_0_0.Certificate",
      "CertificateString": "-----BEGIN CERTIFICATE---------END CERTIFICATE-----\n",
      "Description": "LDAP Certificate",
      "Id": "1",
      "Issuer": {
        "City": "SomeCity",
        "CommonName": "www.company.com",
        "Country": "US",
        "Organization": "MyCompany",
        "State": "VA",
        "organizationUnit": "MyDivision"
      },
      "KeyUsage": [
        "KeyAgreement",
        "ServerAuthentication"
      ],
      "Name": "LDAP Certificate",
      "Subject": {
        "City": "SomeCity",
        "CommonName": "www.company.com",
        "Organization": "MyCompany",
        "State": "VA",
        "organizationUnit": "MyDivision"
      },
      "ValidNotAfter": "2029-03-14T02:11:02+00:00",
      "ValidNotBefore": "2019-03-17T02:11:02+00:00"
    }
    
    4. GET on /redfish/v1/AccountService/LDAP/Certificates/
    {
      "@odata.context":
      "/redfish/v1/$metadata#CertificateCollection.CertificateCollection",
      "@odata.id": "/redfish/v1/AccountService/LDAP/Certificates",
      "@odata.type": "#CertificateCollection.CertificatesCollection",
      "Description": "A Collection of LDAP certificate instances",
      "Members": [
        {
          "@odata.id": "/redfish/v1/AccountService/LDAP/Certificates/1"
        }
      ],
      "Members@odata.count": 1,
      "Name": "LDAP Certificate Collection"
    }
    
    5.GET on /redfish/v1/CertificateService/CertificateLocations/
    {
      "@odata.context":
      "/redfish/v1/$metadata#CertificateLocations.CertificateLocations",
      "@odata.id": "/redfish/v1/CertificateService/CertificateLocations",
      "@odata.type": "#CertificateLocations.v1_0_0.CertificateLocations",
      "Description": "Defines a resource that an administrator can use in order to
       locate all certificates installed on a given service",
      "Id": "CertificateLocations",
      "Links": {
        "Certificates": [
          {
            "@odata.id":
    "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/1"
          },
          {
            "@odata.id": "/redfish/v1/AccountService/LDAP/Certificates/1"
          }
        ],
        "Certificates@odata.count": 2
      },
      "Name": "Certificate Locations"
    }
    
    6.GET on  /redfish/v1/AccountService/LDAP/Certificates/1
    {
      "@odata.context": "/redfish/v1/$metadata#Certificate.Certificate",
      "@odata.id": "/redfish/v1/AccountService/LDAP/Certificates/1",
      "@odata.type": "#Certificate.v1_0_0.Certificate",
      "CertificateString": "-----BEGINCERTIFICATE-----\n ... -----ENDCERTIFICATE-----\n",
      "CertificateType": "PEM",
      "Description": "LDAP Certificate",
      "Id": "1",
      "Issuer": {
        "CommonName": "localhost",
        "Organization": "openbmc-project.xyz"
      },
      "KeyUsage": [],
      "Name": "LDAP Certificate",
      "Subject": {
        "CommonName": "localhost"
    }
    
    Change-Id: I056a4cea8b0377e156b660984857cdfadbfe1b2c
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -119,6 +119,8 @@ class RedfishService
         nodes.emplace_back(std::make_unique<CertificateLocations>(app));
         nodes.emplace_back(std::make_unique<HTTPSCertificateCollection>(app));
         nodes.emplace_back(std::make_unique<HTTPSCertificate>(app));
+        nodes.emplace_back(std::make_unique<LDAPCertificateCollection>(app));
+        nodes.emplace_back(std::make_unique<LDAPCertificate>(app));
         for (const auto& node : nodes)
         {
             node->initPrivileges();
