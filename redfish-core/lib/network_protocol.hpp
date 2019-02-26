f3d073e5a7b2cd82a0c6b788ddbdf6880f8aacc3
Marri Devender Rao <devenrao@in.ibm.com>
2019-02-26 02:00:37 -0600

    Redfish: Add HTTPS CertificateCollection
    This change adds certificate collection for HTTPS certificates
    
    HTTPSCertificateCollection contains links to HTTPS certificates
    installed on the system.
    
    Modified CertificateLocations to include HTTPS certificate links
    
    Tested:
    1. Tested schema with validator and no issues
    2. Privilege map for certificate service is not yet pubished
    3.POST on /redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates
    {
      "@Message.ExtendedInfo": [
        {
          "@odata.type": "/redfish/v1/$metadata#Message.v1_0_0.Message",
          "Message": "Successfully Completed Request",
          "MessageArgs": [],
          "MessageId": "Base.1.4.0.Success",
          "Resolution": "None",
          "Severity": "OK"
        }
      ]
    }
    
    4.GET on /redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/
    {
      "@odata.context":
    "/redfish/v1/$metadata#CertificateCollection.CertificateCollection",
      "@odata.id": "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates",
      "@odata.type": "#CertificateCollection.CertificatesCollection",
      "Description": "A Collection of HTTPS certificate instances",
      "Members": [
        {
          "@odata.id":
          "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/1"
        }
      ],
      "Members@odata.count": 1,
      "Name": "HTTPS Certificate Collection"
    }
    
    5.GET on /redfish/v1/CertificateService/CertificateLocations/
    {
      "@odata.context":
    "/redfish/v1/$metadata#CertificateLocations.CertificateLocations",
      "@odata.id": "/redfish/v1/CertificateService/CertificateLocations",
      "@odata.type": "#CertificateLocations.v1_0_0.CertificateLocations",
      "Description": "Defines a resource that an administrator can use in order to
        locate all certificates installed on a given service",
      "Id": "CertificateLocations",
      "Links": {
        "Certificates": [
          {
            "@odata.id":
            "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/1"
          }
        ],
        "Certificates@odata.count": 1
      },
      "Name": "Certificate Locations"
    }
    
    Change-Id: Ia3c6a3b5d740c94edaa8a8783f1a18ce96d4b49c
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- redfish-core/lib/network_protocol.hpp
+++ redfish-core/lib/network_protocol.hpp
@@ -109,7 +109,7 @@ class NetworkProtocol : public Node
     void getData(const std::shared_ptr<AsyncResp>& asyncResp)
     {
         asyncResp->res.jsonValue["@odata.type"] =
-            "#ManagerNetworkProtocol.v1_1_0.ManagerNetworkProtocol";
+            "#ManagerNetworkProtocol.v1_4_0.ManagerNetworkProtocol";
         asyncResp->res.jsonValue["@odata.id"] =
             "/redfish/v1/Managers/bmc/NetworkProtocol";
         asyncResp->res.jsonValue["@odata.context"] =
@@ -156,7 +156,14 @@ class NetworkProtocol : public Node
                              "running") ||
                             (std::get<NET_PROTO_UNIT_SUB_STATE>(unit) ==
                              "listening");
-
+                        if (service == "HTTPS")
+                        {
+                            asyncResp->res
+                                .jsonValue["service"]["Certificates"] = {
+                                "@odata.id",
+                                "/redfish/v1/Managers/bmc/NetworkProtocol/"
+                                "HTTPS/Certificates/"};
+                        }
                         crow::connections::systemBus->async_method_call(
                             [asyncResp, service{std::string(service)}](
                                 const boost::system::error_code ec,
