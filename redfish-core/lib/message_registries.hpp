eece1bfccfb466b902d80faaeb8e3f54b5f15bc8
Jason M. Bills <jason.m.bills@linux.intel.com>
2019-03-27 13:23:22 -0700

    Add the DMTF Resource Event Message Registry
    This change adds the Resource Event Message Registry so we
    can use the standard threshold exceeded event messages.
    
    Tested:
    Verified that the Resource Event Message Registry shows up
    correctly under the Registries list in redfish.
    
    Change-Id: Ia962772c8e07c9b2e096d7fbd6e228fe8a4eb0cf
    Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>

--- redfish-core/lib/message_registries.hpp
+++ redfish-core/lib/message_registries.hpp
@@ -18,6 +18,7 @@
 #include "base_message_registry.hpp"
 #include "node.hpp"
 #include "registries.hpp"
+#include "resource_event_message_registry.hpp"
 
 namespace redfish
 {
@@ -63,6 +64,8 @@ class MessageRegistryFileCollection : public Node
         messageRegistryFileArray = nlohmann::json::array();
         messageRegistryFileArray.push_back(
             {{"@odata.id", "/redfish/v1/Registries/Base"}});
+        messageRegistryFileArray.push_back(
+            {{"@odata.id", "/redfish/v1/Registries/ResourceEvent"}});
         messageRegistryFileArray.push_back(
             {{"@odata.id", "/redfish/v1/Registries/OpenBMC"}});
         asyncResp->res.jsonValue["Members@odata.count"] =
@@ -168,6 +171,128 @@ class BaseMessageRegistry : public Node
         // Go through the Message Registry and populate each Message
         for (const message_registries::MessageEntry &message :
              message_registries::base::registry)
+        {
+            messageArray.push_back(
+                {{message.first,
+                  {{"Description", message.second.description},
+                   {"Message", message.second.message},
+                   {"Severity", message.second.severity},
+                   {"NumberOfArgs", message.second.numberOfArgs},
+                   {"Resolution", message.second.resolution}}}});
+            if (message.second.numberOfArgs > 0)
+            {
+                nlohmann::json &messageParamArray =
+                    messageArray.back()[message.first]["ParamTypes"];
+                for (int i = 0; i < message.second.numberOfArgs; i++)
+                {
+                    messageParamArray.push_back(message.second.paramTypes[i]);
+                }
+            }
+        }
+    }
+};
+
+class ResourceEventMessageRegistryFile : public Node
+{
+  public:
+    template <typename CrowApp>
+    ResourceEventMessageRegistryFile(CrowApp &app) :
+        Node(app, "/redfish/v1/Registries/ResourceEvent/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::put, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::post, {{"ConfigureManager"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        std::shared_ptr<AsyncResp> asyncResp = std::make_shared<AsyncResp>(res);
+
+        asyncResp->res.jsonValue["@odata.id"] =
+            "/redfish/v1/Registries/ResourceEvent";
+        asyncResp->res.jsonValue["@odata.type"] =
+            "#MessageRegistryFile.v1_1_0.MessageRegistryFile";
+        asyncResp->res.jsonValue["@odata.context"] =
+            "/redfish/v1/$metadata#MessageRegistryFile.MessageRegistryFile";
+        asyncResp->res.jsonValue["Name"] =
+            "Resource Event Message Registry File";
+        asyncResp->res.jsonValue["Description"] =
+            "DMTF Resource Event Message Registry File Location";
+        asyncResp->res.jsonValue["Id"] =
+            message_registries::resource_event::header.registryPrefix;
+        asyncResp->res.jsonValue["Registry"] =
+            message_registries::resource_event::header.id;
+        nlohmann::json &messageRegistryLanguageArray =
+            asyncResp->res.jsonValue["Languages"];
+        messageRegistryLanguageArray = nlohmann::json::array();
+        messageRegistryLanguageArray.push_back({"en"});
+        asyncResp->res.jsonValue["Languages@odata.count"] =
+            messageRegistryLanguageArray.size();
+        nlohmann::json &messageRegistryLocationArray =
+            asyncResp->res.jsonValue["Location"];
+        messageRegistryLocationArray = nlohmann::json::array();
+        messageRegistryLocationArray.push_back(
+            {{"Language", "en"},
+             {"PublicationUri",
+              "https://redfish.dmtf.org/registries/ResourceEvent.1.0.0.json"},
+             {"Uri", "/redfish/v1/Registries/ResourceEvent/ResourceEvent"}});
+        asyncResp->res.jsonValue["Location@odata.count"] =
+            messageRegistryLocationArray.size();
+    }
+};
+
+class ResourceEventMessageRegistry : public Node
+{
+  public:
+    template <typename CrowApp>
+    ResourceEventMessageRegistry(CrowApp &app) :
+        Node(app, "/redfish/v1/Registries/ResourceEvent/ResourceEvent/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::put, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::post, {{"ConfigureManager"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        std::shared_ptr<AsyncResp> asyncResp = std::make_shared<AsyncResp>(res);
+
+        asyncResp->res.jsonValue["@Redfish.Copyright"] =
+            message_registries::resource_event::header.copyright;
+        asyncResp->res.jsonValue["@odata.type"] =
+            message_registries::resource_event::header.type;
+        asyncResp->res.jsonValue["Id"] =
+            message_registries::resource_event::header.id;
+        asyncResp->res.jsonValue["Name"] =
+            message_registries::resource_event::header.name;
+        asyncResp->res.jsonValue["Language"] =
+            message_registries::resource_event::header.language;
+        asyncResp->res.jsonValue["Description"] =
+            message_registries::resource_event::header.description;
+        asyncResp->res.jsonValue["RegistryPrefix"] =
+            message_registries::resource_event::header.registryPrefix;
+        asyncResp->res.jsonValue["RegistryVersion"] =
+            message_registries::resource_event::header.registryVersion;
+        asyncResp->res.jsonValue["OwningEntity"] =
+            message_registries::resource_event::header.owningEntity;
+        nlohmann::json &messageArray = asyncResp->res.jsonValue["Messages"];
+        messageArray = nlohmann::json::array();
+
+        // Go through the Message Registry and populate each Message
+        for (const message_registries::MessageEntry &message :
+             message_registries::resource_event::registry)
         {
             messageArray.push_back(
                 {{message.first,
