c26770bbb224b8abba09a5df47afca0a759fdcdc
Ratan Gupta <ratagupt@linux.vnet.ibm.com>
2019-04-05 16:30:46 +0530

    Send 200 ok http code in case of partial success
    As per the redfish spec PATCH operation should return
    200 OK in the case of partial success.
    
    As per the spec DSP0266_1_7_0
    =========================================================
    Modify several properties where one or more properties can never be
    updated.
    
    The Service Returns:
    => The HTTP 200 status code
    => A resource representation with a message that lists the non-updatable
    properties.
    => The service may update other properties in the
    resource.
    =========================================================
    
    More details can be found at the following thread link
    https://redfishforum.com/thread/177/patch-request-multiple-properties
    
    TestedBy:
              PATCH individual Property
                    Success: 200 OK
                    Failure: 500
              PATCH Multi Property
                    Partial Success: 200 OK
                    All failure: 500
              Run redfish validator
                  Success
    
    Change-Id: I12afba396573609278b08613927fe060abbecc99
    Signed-off-by: Ratan Gupta <ratagupt@linux.vnet.ibm.com>

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -755,6 +755,7 @@ class AccountService : public Node
                                                     "ServiceAddresses",
                                                     serviceAddressList.front());
                 }
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the service address";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -788,6 +789,7 @@ class AccountService : public Node
                 asyncResp->res.jsonValue[ldapServerElementName]
                                         ["Authentication"]["Username"] =
                     username;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the username";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -820,6 +822,7 @@ class AccountService : public Node
                 }
                 asyncResp->res.jsonValue[ldapServerElementName]
                                         ["Authentication"]["Password"] = "";
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the password";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -864,6 +867,7 @@ class AccountService : public Node
                                                     "BaseDistinguishedNames",
                                                     baseDNList.front());
                 }
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the base DN";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -899,6 +903,7 @@ class AccountService : public Node
                 auto& searchSettingsJson =
                     serverTypeJson["LDAPService"]["SearchSettings"];
                 searchSettingsJson["UsernameAttribute"] = userNameAttribute;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the user name attr.";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -934,6 +939,7 @@ class AccountService : public Node
                 auto& searchSettingsJson =
                     serverTypeJson["LDAPService"]["SearchSettings"];
                 searchSettingsJson["GroupsAttribute"] = groupsAttribute;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the groupname attr";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -967,6 +973,7 @@ class AccountService : public Node
                 asyncResp->res
                     .jsonValue[ldapServerElementName]["ServiceEnabled"] =
                     serviceEnabled;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated Service enable = "
                                  << serviceEnabled;
             },
