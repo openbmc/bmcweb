93655838ab4622d048813b5491535d1b2ad6ca80
Ratan Gupta <ratagupt@linux.vnet.ibm.com>
2019-04-05 16:30:46 +0530

    Send 200 ok http code in case of partial success
    As per the redfish spec PATCH operation should return
    200 OK in the case of partial success.
    
    TestedBy:
              PATCH individual Property
                    Success: 200 OK
                    Failure: 500
              PATCH Multi Property
                    Partial Success: 200 OK
                    All failure: 500
              Run redfish validator
                  Success
    
    Change-Id: I12afba396573609278b08613927fe060abbecc99
    Signed-off-by: Ratan Gupta <ratagupt@linux.vnet.ibm.com>

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -411,6 +411,7 @@ class AccountService : public Node
                                                     "ServiceAddresses",
                                                     serviceAddressList.front());
                 }
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the service address";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -444,6 +445,7 @@ class AccountService : public Node
                 asyncResp->res.jsonValue[ldapServerElementName]
                                         ["Authentication"]["Username"] =
                     username;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the username";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -476,6 +478,7 @@ class AccountService : public Node
                 }
                 asyncResp->res.jsonValue[ldapServerElementName]
                                         ["Authentication"]["Password"] = "";
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the password";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -520,6 +523,7 @@ class AccountService : public Node
                                                     "BaseDistinguishedNames",
                                                     baseDNList.front());
                 }
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the base DN";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -555,6 +559,7 @@ class AccountService : public Node
                 auto& searchSettingsJson =
                     serverTypeJson["LDAPService"]["SearchSettings"];
                 searchSettingsJson["UsernameAttribute"] = userNameAttribute;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the user name attr.";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -590,6 +595,7 @@ class AccountService : public Node
                 auto& searchSettingsJson =
                     serverTypeJson["LDAPService"]["SearchSettings"];
                 searchSettingsJson["GroupsAttribute"] = groupsAttribute;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated the groupname attr";
             },
             ldapDbusService, ldapConfigObject, propertyInterface, "Set",
@@ -623,6 +629,7 @@ class AccountService : public Node
                 asyncResp->res
                     .jsonValue[ldapServerElementName]["ServiceEnabled"] =
                     serviceEnabled;
+                asyncResp->isPropertyUpdated = true;
                 BMCWEB_LOG_DEBUG << "Updated Service enable = "
                                  << serviceEnabled;
             },
