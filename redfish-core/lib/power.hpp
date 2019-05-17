811813375b40090b7f0f40cfc075163a746ca89f
Eddie James <eajames@linux.ibm.com>
2019-05-17 21:24:36 +0000

    Add basic PowerControl and PowerLimit properties
    Add code in the power-specific response function to fetch the available
    PowerControl and PowerLimit properties. Add them to the Redfish response.
    
    Output from:
    curl -k https://${bmc}/redfish/v1/Chassis/chassis/Power
    {
      "@odata.context": "/redfish/v1/$metadata#Power.Power",
      "@odata.id": "/redfish/v1/Chassis/chassis/Power",
      "@odata.type": "#Power.v1_5_2.Power",
      "Id": "Power",
      "Name": "Power",
      "PowerControl": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Power#/PowerControl/",
          "MemberId": "total_power",
          "Name": "total power",
          "PowerConsumedWatts": 236000000.0,
          "Status": {
            "Health": "OK",
            "State": "Enabled"
          }
        }
      ],
      "PowerLimit": [
        {
          "LimitInWatts": null
        }
      ],
      "Voltages": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Power#/Voltages/0",
          "@odata.type": "#Power.v1_0_0.Voltage",
          "LowerThresholdCritical": 0.0,
          "LowerThresholdNonCritical": 0.0,
          "MaxReadingRange": 0.0,
          "MemberId": "p0_vcs_voltage",
          "MinReadingRange": 0.0,
          "Name": "p0 vcs voltage",
          "ReadingVolts": 0.9490000000000001,
          "Status": {
            "Health": "OK",
            "State": "Enabled"
          },
          "UpperThresholdCritical": 1.3,
          "UpperThresholdNonCritical": 1.1500000000000001
        },
    
    Signed-off-by: Eddie James <eajames@linux.ibm.com>
    Change-Id: I447de59fb44a4ecbe7b47610d915ac22aef90250

--- redfish-core/lib/power.hpp
+++ redfish-core/lib/power.hpp
@@ -22,6 +22,9 @@
 namespace redfish
 {
 
+using PropertyVariant =
+    sdbusplus::message::variant<std::string, int64_t, double, uint32_t, bool>;
+
 class Power : public Node
 {
   public:
@@ -54,7 +57,268 @@ class Power : public Node
 
         auto sensorAsyncResp = std::make_shared<SensorsAsyncResp>(
             res, chassis_name, typeList, "Power");
-        // TODO Need to retrieve Power Control information.
+
+        struct PowerProperty
+        {
+            PowerProperty(const char* i, const char* n, const char* o,
+                          const char* p, const char* r) :
+                interface(i),
+                name(n), output(o), path(p), redfish(r)
+            {
+            }
+
+            std::string interface;
+            std::string name;
+            std::string output;
+            std::string path;
+            std::string redfish;
+        };
+        std::array<PowerProperty, 2> props = {
+            PowerProperty("xyz.openbmc_project.Sensor.Value", "total_power",
+                          "PowerConsumedWatts", "/xyz/openbmc_project/sensors/",
+                          "PowerControl"),
+            PowerProperty("xyz.openbmc_project.Control.Power.Cap", "power_cap",
+                          "LimitInWatts", "/xyz/openbmc_project/control/",
+                          "PowerLimit")};
+
+        for (const PowerProperty& prop : props)
+        {
+            auto respHandler = [sensorAsyncResp,
+                                prop](const boost::system::error_code ec,
+                                      const GetSubTreeType& subtree) {
+                if (ec)
+                {
+                    messages::resourceNotFound(sensorAsyncResp->res, prop.name,
+                                               sensorAsyncResp->chassisId);
+                    return;
+                }
+
+                for (const std::pair<
+                         std::string,
+                         std::vector<
+                             std::pair<std::string, std::vector<std::string>>>>&
+                         object : subtree)
+                {
+                    if (object.second.empty())
+                    {
+                        continue;
+                    }
+
+                    std::vector<std::string> split;
+
+                    split.reserve(6);
+                    boost::algorithm::split(split, object.first,
+                                            boost::is_any_of("/"));
+                    if (split.size() < 6)
+                    {
+                        continue;
+                    }
+
+                    if (split[5].compare(prop.name))
+                    {
+                        continue;
+                    }
+
+                    if (!prop.redfish.compare("PowerLimit"))
+                    {
+                        auto limitHandler = [sensorAsyncResp, prop](
+                                                const boost::system::error_code
+                                                    ec,
+                                                const std::vector<
+                                                    std::pair<std::string,
+                                                              PropertyVariant>>&
+                                                    properties) {
+                            if (ec)
+                            {
+                                messages::internalError(sensorAsyncResp->res);
+                                BMCWEB_LOG_ERROR
+                                    << "get valueHandler: Dbus error " << ec;
+                                return;
+                            }
+
+                            nlohmann::json& tempArray =
+                                sensorAsyncResp->res.jsonValue[prop.redfish];
+
+                            if (tempArray.empty())
+                            {
+                                tempArray.push_back({});
+                            }
+
+                            nlohmann::json& sensorJson = tempArray.back();
+                            bool enabled = false;
+                            double powerCap = 0.0;
+
+                            for (const std::pair<std::string, PropertyVariant>&
+                                     property : properties)
+                            {
+                                if (!property.first.compare("PowerCap"))
+                                {
+                                    const double* d =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            double>(&property.second);
+                                    const int64_t* i =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            int64_t>(&property.second);
+                                    const uint32_t* u =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            uint32_t>(&property.second);
+
+                                    if (d)
+                                    {
+                                        powerCap = *d;
+                                    }
+                                    else if (i)
+                                    {
+                                        powerCap = *i;
+                                    }
+                                    else if (u)
+                                    {
+                                        powerCap = *u;
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                                else if (property.first.compare(
+                                             "PowerCapEnable"))
+                                {
+                                    const bool* b =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            bool>(&property.second);
+
+                                    if (b)
+                                    {
+                                        enabled = *b;
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                            }
+
+                            nlohmann::json& value = sensorJson[prop.output];
+
+                            if (enabled)
+                            {
+                                value = powerCap;
+                            }
+                        };
+
+                        crow::connections::systemBus->async_method_call(
+                            std::move(limitHandler),
+                            object.second.front().first, object.first,
+                            "org.freedesktop.DBus.Properties", "GetAll",
+                            prop.interface);
+                    }
+                    else
+                    {
+                        auto valueHandler = [sensorAsyncResp, prop](
+                                                const boost::system::error_code
+                                                    ec,
+                                                const std::vector<
+                                                    std::pair<std::string,
+                                                              PropertyVariant>>&
+                                                    properties) {
+                            if (ec)
+                            {
+                                messages::internalError(sensorAsyncResp->res);
+                                BMCWEB_LOG_ERROR
+                                    << "get valueHandler: Dbus error " << ec;
+                                return;
+                            }
+
+                            int64_t scale = 0;
+                            nlohmann::json& tempArray =
+                                sensorAsyncResp->res.jsonValue[prop.redfish];
+
+                            if (tempArray.empty())
+                            {
+                                tempArray.push_back(
+                                    {{"@odata.id",
+                                      "/redfish/v1/Chassis/" +
+                                          sensorAsyncResp->chassisId + "/" +
+                                          sensorAsyncResp->chassisSubNode +
+                                          "#/" + prop.redfish + "/"}});
+                            }
+
+                            nlohmann::json& sensorJson = tempArray.back();
+
+                            sensorJson["MemberId"] = prop.name;
+                            sensorJson["Name"] =
+                                boost::replace_all_copy(prop.name, "_", " ");
+
+                            sensorJson["Status"]["State"] = "Enabled";
+                            sensorJson["Status"]["Health"] = "OK";
+
+                            for (const std::pair<std::string, PropertyVariant>&
+                                     property : properties)
+                            {
+                                if (!property.first.compare("Scale"))
+                                {
+                                    const int64_t* i =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            int64_t>(&property.second);
+
+                                    if (i)
+                                    {
+                                        scale = *i;
+                                    }
+                                }
+                                else if (!property.first.compare("Value"))
+                                {
+                                    const double* d =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            double>(&property.second);
+                                    const int64_t* i =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            int64_t>(&property.second);
+                                    const uint32_t* u =
+                                        sdbusplus::message::variant_ns::get_if<
+                                            uint32_t>(&property.second);
+                                    double val = 0.0;
+                                    nlohmann::json& value =
+                                        sensorJson[prop.output];
+
+                                    if (i)
+                                    {
+                                        val = *i;
+                                    }
+                                    else if (d)
+                                    {
+                                        val = *d;
+                                    }
+                                    else if (u)
+                                    {
+                                        val = *u;
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+
+                                    val = val * std::pow(10, scale);
+                                    value = val;
+                                }
+                            }
+                        };
+                        crow::connections::systemBus->async_method_call(
+                            std::move(valueHandler),
+                            object.second.front().first, object.first,
+                            "org.freedesktop.DBus.Properties", "GetAll",
+                            prop.interface);
+                    }
+                }
+            };
+
+            crow::connections::systemBus->async_method_call(
+                std::move(respHandler), "xyz.openbmc_project.ObjectMapper",
+                "/xyz/openbmc_project/object_mapper",
+                "xyz.openbmc_project.ObjectMapper", "GetSubTree", prop.path, 2,
+                std::array<std::string, 1>{prop.interface});
+        }
+
         getChassisData(sensorAsyncResp);
     }
     void doPatch(crow::Response& res, const crow::Request& req,
