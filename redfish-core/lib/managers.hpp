e5c23bf59d89f18fa1a4fca89144fbfcd2763c18
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2019-07-11 10:16:43 +0200

    Add VirtualMedia to Redfish, with $expand support
    This change adds VirtualMedia scheme to Redfish.
    Implementation is based on input from virtual-media module (wip)
    and nbd proxy which is a bmcweb part (also wip). The code is used
    only in case ndb-proxy is supported in bmcweb
    (BMCWEB_ENABLE_VM_NBDPROXY compilation flag).
    Redfish $expand flag is supported.
    
    Most of the data is static right now, some updates will go as soon as
    dependants are ready.
    
    Tested:
    Tested manually, using Virtual Media application and nbd-proxy.
    
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
    Change-Id: I5415dc0ffe52069fd35bc614b0378bbc4ad41ff6

--- redfish-core/lib/managers.hpp
+++ redfish-core/lib/managers.hpp
@@ -1488,6 +1488,10 @@ class Manager : public Node
 
         res.jsonValue["EthernetInterfaces"] = {
             {"@odata.id", "/redfish/v1/Managers/bmc/EthernetInterfaces"}};
+
+        res.jsonValue["VirtualMedia"] = {
+            {"@odata.id", "/redfish/v1/Managers/bmc/VirtualMedia"}};
+
         // default oem data
         nlohmann::json& oem = res.jsonValue["Oem"];
         nlohmann::json& oemOpenbmc = oem["OpenBmc"];
