c2a8954308c38f34e4c7386fc3ca586d69b5794f
Chicago Duan <duanzhijia01@inspur.com>
2019-07-15 14:22:08 +0800

    Support to Delete a single event log using redfish
    Tested:Use redfish to delete a single event log
    Before:
    curl -k -H "X-Auth-Token: $bmc_token" -X DELETE https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries/2 -d '{"[]"}'
    Method Not Allowed
    
    After:
    curl -k -H "X-Auth-Token: $bmc_token" -X DELETE https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries/2 -d '{"[]"}'
    
    No error response,and the entry 2 is deleted.
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: I31e4470d73b72012b8727d466530202e2609000d

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -1011,6 +1011,40 @@ class DBusEventLogEntry : public Node
             "org.freedesktop.DBus.Properties", "GetAll",
             "xyz.openbmc_project.Logging.Entry");
     }
+
+  private:
+    void doDelete(crow::Response &res, const crow::Request &req,
+                  const std::vector<std::string> &params) override
+    {
+
+        const std::string &entryID = params[0];
+
+        BMCWEB_LOG_DEBUG << "Do delete single event entries.";
+
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        // Process response from Logging service.
+        auto resp_handler = [asyncResp](const boost::system::error_code ec) {
+            BMCWEB_LOG_DEBUG << "EventLogEntry (DBus) doDelete callback: Done";
+            if (ec)
+            {
+                // TODO Handle for specific error code
+                BMCWEB_LOG_ERROR
+                    << "EventLogEntry (DBus) doDelete resp_handler got error "
+                    << ec;
+                asyncResp->res.result(
+                    boost::beast::http::status::internal_server_error);
+                return;
+            }
+
+            asyncResp->res.result(boost::beast::http::status::no_content);
+        };
+
+        // Make call to Logging service to request Delete Log
+        crow::connections::systemBus->async_method_call(
+            resp_handler, "xyz.openbmc_project.Logging",
+            "/xyz/openbmc_project/logging/entry/" + entryID,
+            "xyz.openbmc_project.Object.Delete", "Delete");
+    }
 };
 
 class BMCLogServiceCollection : public Node
