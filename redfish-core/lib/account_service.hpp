c22bd5ccdbf757390135a4deaaa9245ad4f7cb73
jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
2019-08-07 20:26:37 +0000

    account_service: redfish user Patch error handling
    Modified doPatch method to populate redfish user update error codes.
    
    Tested:
    Tested user updates with below scenarios
    1)Provided username is not exist
    2)Replace username already user exists
    3)Replace Username is NULL/Invalid
    4)Replace username is not starting with alphabet
    5)Replace username exceeds more than 16 characters
    6)Password is not valid for Replace/existing username
    
    Signed-off-by: jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
    Change-Id: Ibee448c5d5c4f38c5c4cacda757864593f6001fc

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -111,6 +111,56 @@ inline std::string getPrivilegeFromRoleId(std::string_view role)
     return "";
 }
 
+void errorMessage(const boost::system::error_code ec, const char* errorMessage,
+                  std::shared_ptr<AsyncResp> asyncResp,
+                  const std::string& newUser,
+                  std::optional<std::string> username)
+{
+    if (strcmp(errorMessage,
+               "xyz.openbmc_project.User.Common.Error.UserNameExists") == 0)
+    {
+        messages::resourceAlreadyExists(asyncResp->res,
+                                        "#ManagerAccount.v1_0_3.ManagerAccount",
+                                        "UserName", newUser);
+    }
+    else if (strcmp(errorMessage, "xyz.openbmc_project.User.Common.Error."
+                                  "UserNameDoesNotExist") == 0)
+    {
+        if (username.value().length() > 0)
+        {
+            messages::resourceNotFound(asyncResp->res,
+                                       "#ManagerAccount.v1_0_3.ManagerAccount",
+                                       username.value());
+        }
+        else
+        {
+            messages::resourceNotFound(asyncResp->res,
+                                       "#ManagerAccount.v1_0_3.ManagerAccount",
+                                       newUser);
+        }
+    }
+    else if (strcmp(errorMessage,
+                    "xyz.openbmc_project.Common.Error.InvalidArgument") == 0)
+    {
+        messages::actionParameterUnknown(asyncResp->res, "UserName", newUser);
+    }
+    else if (strcmp(errorMessage,
+                    "xyz.openbmc_project.User.Common.Error.NoResource") == 0)
+    {
+        messages::createLimitReachedForResource(asyncResp->res);
+    }
+    else if (strcmp(errorMessage, "xyz.openbmc_project.User.Common.Error."
+                                  "UserNameGroupFail") == 0)
+    {
+        messages::stringValueTooLong(asyncResp->res, "UserName", 16);
+    }
+    else
+    {
+        messages::internalError(asyncResp->res);
+    }
+    return;
+}
+
 void parseLDAPConfigData(nlohmann::json& json_response,
                          const LDAPConfigData& confData,
                          const std::string& ldapType)
@@ -1298,19 +1348,25 @@ class ManagerAccount : public Node
                                  locked);
             return;
         }
+        else if (newUserName.value() == username)
+        {
+            updateUserProperties(asyncResp, username, password, enabled, roleId,
+                                 locked);
+            return;
+        }
         else
         {
             crow::connections::systemBus->async_method_call(
                 [this, asyncResp, username, password(std::move(password)),
                  roleId(std::move(roleId)), enabled(std::move(enabled)),
-                 newUser{std::string(*newUserName)}, locked(std::move(locked))](
-                    const boost::system::error_code ec) {
+                 newUser{std::string(*newUserName)},
+                 locked(std::move(locked))](const boost::system::error_code ec,
+                                            sdbusplus::message::message& m) {
                     if (ec)
                     {
-                        BMCWEB_LOG_ERROR << "D-Bus responses error: " << ec;
-                        messages::resourceNotFound(
-                            asyncResp->res,
-                            "#ManagerAccount.v1_0_3.ManagerAccount", username);
+                        const sd_bus_error* e = m.get_error();
+                        errorMessage(ec, e->name, asyncResp, newUser, username);
+
                         return;
                     }
 
@@ -1334,8 +1390,36 @@ class ManagerAccount : public Node
         {
             if (!pamUpdatePassword(username, *password))
             {
-                BMCWEB_LOG_ERROR << "pamUpdatePassword Failed";
-                messages::internalError(asyncResp->res);
+                // This is to handle the case where username does not exist
+                crow::connections::systemBus->async_method_call(
+                    [asyncResp, username](const boost::system::error_code ec,
+                                          sdbusplus::message::message& m) {
+                        if (ec)
+                        {
+                            const sd_bus_error* e = m.get_error();
+                            if (strcmp(e->name,
+                                       "xyz.openbmc_project.User.Common.Error."
+                                       "UserNameDoesNotExist") == 0)
+                            {
+                                messages::resourceNotFound(
+                                    asyncResp->res,
+                                    "#ManagerAccount.v1_0_3.ManagerAccount",
+                                    username);
+                            }
+                            return;
+                        }
+                        else
+                        {
+                            // If pasword is invalid
+                            messages::invalidObject(asyncResp->res, "Password");
+                            BMCWEB_LOG_ERROR << "pamUpdatePassword Failed";
+                        }
+                    },
+                    "xyz.openbmc_project.User.Manager",
+                    "/xyz/openbmc_project/user",
+                    "xyz.openbmc_project.User.Manager", "GetUserInfo",
+                    username);
+
                 return;
             }
         }
