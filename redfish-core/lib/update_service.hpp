7e86b6d76f4fdf90b2a79dfa2d30d8c73bdec9a3
Jayashankar Padath <jayashankar.padath@in.ibm.com>
2019-08-23 12:30:57 +0530

    Redfish: ApplyTime property GET support
    This change is to get the ApplyTime property using the UpdateServce
    redfish schema. It can be used to check the value before initiating the
    BMC image upload and activation.
    
    If the ApplyTime value is Immediate, force-reboot.service gets called
    which reboots the BMC. If the ApplyTime value is OnReset, no force reboot
    will be triggered and the new BMC image will be functional till the user
    decideds to reboot the BMC manually.
    
    Tested:
    
    1. If the value of ApplyTime is OnReset
    GET https://$bmc/redfish/v1/UpdateService{
      "@odata.context": "/redfish/v1/$metadata#UpdateService.UpdateService",
      "@odata.id": "/redfish/v1/UpdateService",
      "@odata.type": "#UpdateService.v1_2_0.UpdateService",
      "ApplyTime": "OnReset",
      "Description": "Service for Software Update",
      "FirmwareInventory": {
        "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory"
      },
      "HttpPushUri": "/redfish/v1/UpdateService",
      "Id": "UpdateService",
      "Name": "Update Service",
      "ServiceEnabled": true
    }
    
    2. If the value of ApplyTime is Immediate
    GET https://$bmc/redfish/v1/UpdateService
    {
      "@odata.context": "/redfish/v1/$metadata#UpdateService.UpdateService",
      "@odata.id": "/redfish/v1/UpdateService",
      "@odata.type": "#UpdateService.v1_2_0.UpdateService",
      "ApplyTime": "Immediate",
      "Description": "Service for Software Update",
      "FirmwareInventory": {
        "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory"
      },
      "HttpPushUri": "/redfish/v1/UpdateService",
      "Id": "UpdateService",
      "Name": "Update Service",
      "ServiceEnabled": true
    }
    
    Signed-off-by: Jayashankar Padath <jayashankar.padath@in.ibm.com>
    Change-Id: Icd01bb6c102c0a24285c79ccf4d41fd5fe53f0ed

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -339,7 +339,7 @@ class UpdateService : public Node
     void doGet(crow::Response &res, const crow::Request &req,
                const std::vector<std::string> &params) override
     {
-        res.jsonValue["@odata.type"] = "#UpdateService.v1_2_0.UpdateService";
+        res.jsonValue["@odata.type"] = "#UpdateService.v1_4_0.UpdateService";
         res.jsonValue["@odata.id"] = "/redfish/v1/UpdateService";
         res.jsonValue["@odata.context"] =
             "/redfish/v1/$metadata#UpdateService.UpdateService";
@@ -360,7 +360,37 @@ class UpdateService : public Node
         updateSvcSimpleUpdate["TransferProtocol@Redfish.AllowableValues"] = {
             "TFTP"};
 #endif
-        res.end();
+        // Get the current ApplyTime value
+        std::shared_ptr<AsyncResp> aResp = std::make_shared<AsyncResp>(res);
+        crow::connections::systemBus->async_method_call(
+            [aResp](const boost::system::error_code ec,
+                    const std::variant<std::string> &applyTime) {
+                if (ec)
+                {
+                    BMCWEB_LOG_DEBUG << "DBUS response error " << ec;
+                    messages::internalError(aResp->res);
+                    return;
+                }
+
+                const std::string *s = std::get_if<std::string>(&applyTime);
+                if (s != nullptr)
+                {
+                    // Store the ApplyTime Value
+                    if (*s == "xyz.openbmc_project.Software.ApplyTime."
+                              "RequestedApplyTimes.Immediate")
+                    {
+                        aResp->res.jsonValue["ApplyTime"] = "Immediate";
+                    }
+                    else
+                    {
+                        aResp->res.jsonValue["ApplyTime"] = "OnReset";
+                    }
+                }
+            },
+            "xyz.openbmc_project.Settings",
+            "/xyz/openbmc_project/software/apply_time",
+            "org.freedesktop.DBus.Properties", "Get",
+            "xyz.openbmc_project.Software.ApplyTime", "RequestedApplyTime");
     }
 
     void doPatch(crow::Response &res, const crow::Request &req,
