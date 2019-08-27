9e884bdba4e47ed8920a8567271e5fad9c48ff2e
Cheng C Yang <cheng.c.yang@linux.intel.com>
2019-08-27 08:50:59 +0800

    Add PSU Deassert Event
    Add PSU Deassert Event for AC Lost, Predictive Failure, Failure, Fan Fault.
    
    Tested:
    Create a Fan Fault and then recover the Fan, check redfish log:
    {
        "@odata.context": "/redfish/v1/$metadata#LogEntry.LogEntry",
        "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/
    1566427122_10",
        "@odata.type": "#LogEntry.v1_4_0.LogEntry",
        "Created": "2019-08-21T22:38:42+00:00",
        "EntryType": "Event",
        "Id": "1566427122_10",
        "Message": "Power supply PSU1 fan fan1 recovered.",
        "MessageArgs": [
            "PSU1"
        ],
        "MessageId": "OpenBMC.0.1.PowerSupplyFanRecovered",
        "Name": "System Event Log Entry",
        "Severity": "OK"
    },
    
    Signed-off-by: Cheng C Yang <cheng.c.yang@linux.intel.com>
    Change-Id: I224510c56ad87d1a2fc8e7ae5b3546fc34243d12

--- redfish-core/include/registries/openbmc_message_registry.hpp
+++ redfish-core/include/registries/openbmc_message_registry.hpp
@@ -1023,6 +1023,16 @@ const std::array registry = {
                      .resolution = "None.",
                  }},
     MessageEntry{
+        "PowerSupplyACInserted",
+        {
+            .description = "Indicates that a power supply AC was inserted.",
+            .message = "Power supply %1 AC power inserted.",
+            .severity = "Critical",
+            .numberOfArgs = 1,
+            .paramTypes = {"string"},
+            .resolution = "None.",
+        }},
+    MessageEntry{
         "PowerSupplyACLost",
         {
             .description = "Indicates that a power supply has lost AC power.",
@@ -1053,6 +1063,17 @@ const std::array registry = {
             .paramTypes = {"string", "string"},
             .resolution = "None.",
         }},
+    MessageEntry{
+        "PowerSupplyFanRecovered",
+        {
+            .description =
+                "Indicates that the power supply fan recovered from a failure.",
+            .message = "Power supply %1 fan %2 recovered.",
+            .severity = "OK",
+            .numberOfArgs = 2,
+            .paramTypes = {"string", "string"},
+            .resolution = "None.",
+        }},
     MessageEntry{"PowerSupplyFailed",
                  {
                      .description = "Indicates that a power supply has failed.",
@@ -1077,6 +1098,27 @@ const std::array registry = {
         {
             .description = "Indicates that a power supply has been inserted.",
             .message = "Power supply %1 inserted.",
+            .severity = "OK",
+            .numberOfArgs = 1,
+            .paramTypes = {"string"},
+            .resolution = "None.",
+        }},
+    MessageEntry{"PowerSupplyPredictedFailureRecovered",
+                 {
+                     .description = "Indicates that a power supply recovered "
+                                    "from a predicted failure.",
+                     .message = "Power supply %1 predicted failure recovered.",
+                     .severity = "OK",
+                     .numberOfArgs = 1,
+                     .paramTypes = {"string"},
+                     .resolution = "None.",
+                 }},
+    MessageEntry{
+        "PowerSupplyRecovered",
+        {
+            .description =
+                "Indicates that a power supply recovered from a failure.",
+            .message = "Power supply %1 recovered.",
             .severity = "OK",
             .numberOfArgs = 1,
             .paramTypes = {"string"},
