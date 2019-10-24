c19eba23cab7b4835c35b7870912628a7904bbde
Ed Tanous <ed.tanous@intel.com>
2019-10-24 16:30:02 -0700

    Deprecate the "" operator, and isEqP
    While a cool example of how to do string matching in constexpr space,
    the set of verbs available to HTTP has been fixed for a very long time.
    
    This was ported over to beast a while back, but we kept the API for....
    mediocre reasons of backward compatibility.  Remove that, and delete the
    now unused code.
    
    Tested: Built and loaded on a Witherspoon. Validator passes.
    
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>
    Change-Id: Iaf048e196f9b6e71983189877203bf80390df286
    Signed-off-by: James Feist <james.feist@linux.intel.com>
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- include/authorization.hpp
+++ include/authorization.hpp
@@ -140,7 +140,7 @@ static const std::shared_ptr<crow::persistent_data::UserSession>
     }
 #ifndef BMCWEB_INSECURE_DISABLE_CSRF_PREVENTION
     // RFC7231 defines methods that need csrf protection
-    if (req.method() != "GET"_method)
+    if (req.method() != boost::beast::http::verb::get)
     {
         std::string_view csrf = req.getHeaderValue("X-XSRF-TOKEN");
         // Make sure both tokens are filled
@@ -167,7 +167,7 @@ static const std::shared_ptr<crow::persistent_data::UserSession>
 static bool isOnWhitelist(const crow::Request& req)
 {
     // it's allowed to GET root node without authentication
-    if ("GET"_method == req.method())
+    if (req.method() != boost::beast::http::verb::get)
     {
         if (req.url == "/redfish/v1" || req.url == "/redfish/v1/" ||
             req.url == "/redfish" || req.url == "/redfish/" ||
@@ -184,7 +184,7 @@ static bool isOnWhitelist(const crow::Request& req)
 
     // it's allowed to POST on session collection & login without
     // authentication
-    if ("POST"_method == req.method())
+    if (boost::beast::http::verb::post == req.method())
     {
         if ((req.url == "/redfish/v1/SessionService/Sessions") ||
             (req.url == "/redfish/v1/SessionService/Sessions/") ||
