e59f30f4b97ca694ff12124a103a746864fdb7d1
Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
2019-10-28 13:40:01 +0530

    bmcweb: Message entry for system interface modes
    Message entry added for system interface mode (restriction mode),
    which is part of
    xyz/openbmc_project/Control/Security/RestrictionMode.interface.yaml.
    These message entries are used to indicate system interface
    restricted command excecution after BIOS POST.
    
    Tested:
    1. Redfish validator - passed for this new addition
    2. Log will be as below
    {
        "@odata.context": "/redfish/v1/$metadata#LogEntry.LogEntry",
        "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/184",
        "@odata.type": "#LogEntry.v1_4_0.LogEntry",
        "Created": "1970-01-01T00:03:04+00:00",
        "EntryType": "Event",
        "Id": "184",
        "Message": "The system interface is in unprovisioned state.",
        "MessageArgs": [],
        "MessageId": "OpenBMC.0.1.SystemInterfaceUnprovisioned",
        "Name": "System Event Log Entry",
        "Severity": "Critical"
    },
    {
        "@odata.context": "/redfish/v1/$metadata#LogEntry.LogEntry",
        "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/198",
        "@odata.type": "#LogEntry.v1_4_0.LogEntry",
        "Created": "1970-01-01T00:03:18+00:00",
        "EntryType": "Event",
        "Id": "198",
        "Message": "The system interface is in whitelist provisioned state.",
        "MessageArgs": [],
        "MessageId": "OpenBMC.0.1.SystemInterfaceWhitelistProvisioned",
        "Name": "System Event Log Entry",
        "Severity": "Warning"
    },
    {
        "@odata.context": "/redfish/v1/$metadata#LogEntry.LogEntry",
        "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/204",
        "@odata.type": "#LogEntry.v1_4_0.LogEntry",
        "Created": "1970-01-01T00:03:24+00:00",
        "EntryType": "Event",
        "Id": "204",
        "Message": "The system interface is in disabled provisioned state.",
        "MessageArgs": [],
        "MessageId": "OpenBMC.0.1.SystemInterfaceDisabledProvisioned",
        "Name": "System Event Log Entry",
        "Severity": "OK"
    }
    
    Change-Id: I0e5496c50280f4614d08182b7b13136d254e1d40
    Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>

--- redfish-core/include/registries/openbmc_message_registry.hpp
+++ redfish-core/include/registries/openbmc_message_registry.hpp
@@ -29,7 +29,7 @@ const Header header = {
     "0.1.0",
     "OpenBMC",
 };
-const std::array<MessageEntry, 115> registry = {
+const std::array<MessageEntry, 118> registry = {
     MessageEntry{
         "ADDDCCorrectable",
         {
@@ -1379,6 +1379,39 @@ const std::array<MessageEntry, 115> registry = {
                      "Indicates that an SSB Thermal trip has been asserted.",
                      "SSB Thermal trip.",
                      "Critical",
+                     0,
+                     {},
+                     "None.",
+                 }},
+    MessageEntry{"SystemInterfaceDisabledProvisioned",
+                 {
+                     "Indicates that the system Interface is in disabled "
+                     "provisioned state. All commands are blocked to execute "
+                     "through the system interface.",
+                     "The system interface is in disabled provisioned state.",
+                     "OK",
+                     0,
+                     {},
+                     "None.",
+                 }},
+    MessageEntry{"SystemInterfaceUnprovisioned",
+                 {
+                     "Indicates that the system interface is in unprovisioned "
+                     "state. All commands are permitted to execute through "
+                     "the system interface.",
+                     "The system interface is in unprovisioned state.",
+                     "Critical",
+                     0,
+                     {},
+                     "None.",
+                 }},
+    MessageEntry{"SystemInterfaceWhitelistProvisioned",
+                 {
+                     "Indicates that the system interface is in whitelist "
+                     "provisioned state. Only whitelisted commands "
+                     "are permitted to execute through the system interface.",
+                     "The system interface is in whitelist provisioned state.",
+                     "Warning",
                      0,
                      {},
                      "None.",
