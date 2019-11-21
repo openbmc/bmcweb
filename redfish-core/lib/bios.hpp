c4aa4ae27172f6d0d254cbf8156b8945578544fd
Carol Wang <wangkair@cn.ibm.com>
2019-11-21 13:56:38 +0800

    Redfish: Implement ResetBios action
    This action resets the BIOS attributes to default.
    
    Tested:
    GET test:
    1. $ curl -k -H "X-Auth-Token: $token" https://${bmc}/redfish/v1/Systems/system
    {
      ...
      "Bios": {
        "@odata.id": "/redfish/v1/Systems/system/Bios"
      },
      ...
    }
    2. $ curl -k -H "X-Auth-Token: $token" https://${bmc}/redfish/v1/Systems/system/Bios
    {
      "@odata.id": "/redfish/v1/Systems/system/Bios",
      "@odata.type": "#Bios.v1_1_0.Bios",
      "Actions": {
        "#Bios.ResetBios": {
          "target": "/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios"
        }
      },
      "Description": "BIOS Configuration Service",
      "Id": "BIOS",
      "Name": "BIOS Configuration"
    }
    
    POST test:
    1. Change gard list:
     # ./gard list
     No GARD entries to display
     # ./gard create /Sys0/Node0/Proc1/EQ1/EX1/Core0
     # ./gard list
      ID       | Error    | Type       | Path
     -----------------------------------------------------------------------
      00000001 | 00000000 | Manual     | /Sys0/Node0/Proc1/EQ1/EX1/Core0
     =======================================================================
    2. Reset bios:
     # curl -k -H "X-Auth-Token: $token" -X POST https://${bmc}/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios
    3. Check gard list again:
     # ./gard list
     No GARD entries to display
    
    Validator tool test:
     Counter({'pass': 3001, 'skipOptional': 2475, 'metadataNamespaces': 1605,
     'passGet': 191, 'serviceNamespaces': 72, 'invalidPropertyValue': 10,
     'passAction': 7, 'optionalAction': 6, 'warningPresent': 6, 'warnDeprecated':
     2, 'unverifiedComplexAdditional': 1})
     Validation has succeeded.
    
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>
    Change-Id: I0cba966bfde04566001b6df07ad15217f627c327

---
+++ redfish-core/lib/bios.hpp
@@ -0,0 +1,88 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+
+#include "node.hpp"
+
+namespace redfish
+{
+/**
+ * BiosService class supports handle get method for bios.
+ */
+class BiosService : public Node
+{
+  public:
+    BiosService(CrowApp &app) : Node(app, "/redfish/v1/Systems/system/Bios/")
+    {
+        entityPrivileges = {{boost::beast::http::verb::get, {{"Login"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+
+        asyncResp->res.jsonValue["@odata.id"] =
+            "/redfish/v1/Systems/system/Bios";
+        asyncResp->res.jsonValue["@odata.type"] = "#Bios.v1_1_0.Bios";
+        asyncResp->res.jsonValue["Name"] = "BIOS Configuration";
+        asyncResp->res.jsonValue["Description"] = "BIOS Configuration Service";
+        asyncResp->res.jsonValue["Id"] = "BIOS";
+        asyncResp->res.jsonValue["Actions"]["#Bios.ResetBios"] = {
+            {"target",
+             "/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios"}};
+    }
+};
+/**
+ * BiosReset class supports handle POST method for Reset bios.
+ * The class retrieves and sends data directly to D-Bus.
+ */
+class BiosReset : public Node
+{
+  public:
+    BiosReset(CrowApp &app) :
+        Node(app, "/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::post, {{"ConfigureManager"}}}};
+    }
+
+  private:
+    /**
+     * Function handles POST method request.
+     * Analyzes POST body message before sends Reset request data to D-Bus.
+     */
+    void doPost(crow::Response &res, const crow::Request &req,
+                const std::vector<std::string> &params) override
+    {
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "Failed to reset bios: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+            },
+            "org.open_power.Software.Host.Updater",
+            "/xyz/openbmc_project/software",
+            "xyz.openbmc_project.Common.FactoryReset", "Reset");
+    }
+};
+} // namespace redfish
