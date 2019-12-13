8eef9170fd26a501ea405edc2cb476e996b771dc
manojkiraneda <manojkiran.eda@gmail.com>
2019-12-13 17:10:41 +0530

    Add unit test for the lock management
    - The idea behind this commit is to enable the unit test
    for the lock management algorithm, and below are the unit
    test cases that are written:
    
    Testedby:
    
    [----------] 20 tests from locktest
    [ RUN      ] locktest.ValidationGoodTestCase
    [       OK ] locktest.ValidationGoodTestCase (3 ms)
    [ RUN      ] locktest.ValidationBadTestWithLocktype
    [       OK ] locktest.ValidationBadTestWithLocktype (1 ms)
    [ RUN      ] locktest.ValidationBadTestWithlockFlags
    [       OK ] locktest.ValidationBadTestWithlockFlags (0 ms)
    [ RUN      ] locktest.ValidationBadTestWithSegmentlength
    [       OK ] locktest.ValidationBadTestWithSegmentlength (1 ms)
    [ RUN      ] locktest.MultiRequestWithoutConflict
    [       OK ] locktest.MultiRequestWithoutConflict (3 ms)
    [ RUN      ] locktest.MultiRequestWithConflictduetoSameSegmentLength
    [       OK ] locktest.MultiRequestWithConflictduetoSameSegmentLength (2 ms)
    [ RUN      ] locktest.MultiRequestWithoutConflictduetoDifferentSegmentData
    [       OK ] locktest.MultiRequestWithoutConflictduetoDifferentSegmentData (1 ms)
    [ RUN      ] locktest.MultiRequestWithConflictduetoSameSegmentData
    [       OK ] locktest.MultiRequestWithConflictduetoSameSegmentData (2 ms)
    [ RUN      ] locktest.MultiRequestWithoutConflictduetoDifferentSegmentLength
    [       OK ] locktest.MultiRequestWithoutConflictduetoDifferentSegmentLength (1 ms)
    [ RUN      ] locktest.MultiRequestWithoutConflictduetoReadLocktype
    [       OK ] locktest.MultiRequestWithoutConflictduetoReadLocktype (1 ms)
    [ RUN      ] locktest.MultiRequestWithoutConflictduetoReadLocktypeAndLockall
    [       OK ] locktest.MultiRequestWithoutConflictduetoReadLocktypeAndLockall (2 ms)
    [ RUN      ] locktest.RequestConflictedWithLockTableEntries
    [       OK ] locktest.RequestConflictedWithLockTableEntries (6 ms)
    [ RUN      ] locktest.RequestNotConflictedWithLockTableEntries
    [       OK ] locktest.RequestNotConflictedWithLockTableEntries (3 ms)
    [ RUN      ] locktest.TestGenerateTransactionIDFunction
    [       OK ] locktest.TestGenerateTransactionIDFunction (1 ms)
    [ RUN      ] locktest.ValidateTransactionIDsGoodTestCase
    [       OK ] locktest.ValidateTransactionIDsGoodTestCase (3 ms)
    [ RUN      ] locktest.ValidateTransactionIDsBadTestCase
    [       OK ] locktest.ValidateTransactionIDsBadTestCase (2 ms)
    [ RUN      ] locktest.ValidateisItMyLockGoodTestCase
    [       OK ] locktest.ValidateisItMyLockGoodTestCase (2 ms)
    [ RUN      ] locktest.ValidateisItMyLockBadTestCase
    [       OK ] locktest.ValidateisItMyLockBadTestCase (2 ms)
    [ RUN      ] locktest.ValidateSessionIDForGetlocklistBadTestCase
    [       OK ] locktest.ValidateSessionIDForGetlocklistBadTestCase (3 ms)
    [ RUN      ] locktest.ValidateSessionIDForGetlocklistGoodTestCase
    [       OK ] locktest.ValidateSessionIDForGetlocklistGoodTestCase (3 ms)
    [----------] 20 tests from locktest (82 ms total)
    
    Signed-off-by: manojkiraneda <manojkiran.eda@gmail.com>
    Change-Id: Id274ee356adfa7ba03da02d83b609d37c8c99f8d

--- include/ibm/locks.hpp
+++ include/ibm/locks.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <logging.h>
+
 #include <boost/algorithm/string.hpp>
 #include <boost/container/flat_map.hpp>
 #include <filesystem>
@@ -43,6 +45,7 @@ class Lock
     uint32_t transactionId;
     boost::container::flat_map<uint32_t, LockRequests> lockTable;
 
+  protected:
     Lock()
     {
         loadLocks();
@@ -151,6 +154,7 @@ class Lock
         return true;
     }
 
+  protected:
     /*
      * This function implements the logic of checking the ownership of the
      * lock from the releaselock request.
@@ -159,8 +163,8 @@ class Lock
      * Returns : False (if the request HMC or Session does not own the lock(s))
      */
 
-    RcRelaseLock isItMyLock(const ListOfTransactionIds &refRids,
-                            const SessionFlags &ids)
+    virtual RcRelaseLock isItMyLock(const ListOfTransactionIds &refRids,
+                                    const SessionFlags &ids)
     {
         for (const auto &i : refRids)
         {
@@ -192,7 +196,7 @@ class Lock
      * if the transaction ID is not valid & not present in the lock table
      */
 
-    bool validateRids(const ListOfTransactionIds &refRids)
+    virtual bool validateRids(const ListOfTransactionIds &refRids)
     {
         for (const auto &id : refRids)
         {
@@ -220,7 +224,7 @@ class Lock
      * Returns : False (if not a Valid lock request)
      */
 
-    bool isValidLockRequest(const LockRequest refLockRecord)
+    virtual bool isValidLockRequest(const LockRequest refLockRecord)
     {
 
         // validate the locktype
@@ -295,7 +299,7 @@ class Lock
      *
      */
 
-    Rc isConflictWithTable(const LockRequests refLockRequestStructure)
+    virtual Rc isConflictWithTable(const LockRequests refLockRequestStructure)
     {
 
         uint32_t transactionId;
@@ -365,7 +369,7 @@ class Lock
      * Returns : False (if not conflicting)
      */
 
-    bool isConflictRequest(const LockRequests refLockRequestStructure)
+    virtual bool isConflictRequest(const LockRequests refLockRequestStructure)
     {
         // check for all the locks coming in as a part of single request
         // return conflict if any two lock requests are conflicting
@@ -548,7 +552,7 @@ class Lock
      * the Management Console for debug.
      */
 
-    uint32_t generateTransactionId()
+    virtual uint32_t generateTransactionId()
     {
         ++transactionId;
         return transactionId;
@@ -720,6 +724,10 @@ class Lock
         }
 
         return std::make_pair(true, std::make_pair(true, 1));
+    }
+
+    virtual ~Lock()
+    {
     }
 };
 } // namespace ibm_mc_lock
