04b65c25ce51aa384e4c572883a39a5527308efa
manojkiraneda <manojkiran.eda@gmail.com>
2019-12-13 17:07:09 +0530

    Implement GetLockList function in Lock Service
    - This commit implements the GetLockList rest API, by  which
      any external client(with admin privelege) can get a list of
      locks obtained by providing the list of sessionID's as input.
    
    TestedBy:
    
    1. Get the session ID from the Session Service
    
    curl -k -H "X-Auth-Token: $bmc_tokens" -X GET https://<ip>/redfish/v1/SessionService/Sessions/
    {
      "@odata.context": "/redfish/v1/$metadata#SessionCollection.SessionCollection",
      "@odata.id": "/redfish/v1/SessionService/Sessions/",
      "@odata.type": "#SessionCollection.SessionCollection",
      "Description": "Session Collection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/SessionService/Sessions/qM4D0VfZt3"
        },
        {
          "@odata.id": "/redfish/v1/SessionService/Sessions/bjYCiBSCIh"
        },
        {
          "@odata.id": "/redfish/v1/SessionService/Sessions/LkfHvJFggY"
        },
        {
          "@odata.id": "/redfish/v1/SessionService/Sessions/6RKP0aN5Gi"
        }
      ],
      "Members@odata.count": 4,
      "Name": "Session Collection"
    }
    
    2. Use the GetLockList API to obtain the locks owned by a particular
    session.
    
    curl -k -H "X-Auth-Token:$bmc_tokens" -XPOST -H "Content-type: application/json" -d
    '{"SessionIDs": ["qM4D0VfZt3","bjYCiBSCIh"]}' https://127.0.0.1:2443/ibm/v1/HMC/LockService/Actions/LockService.GetLockList
    {
      "Records": [
        {
          "HMCID": "hmc-id",
          "LockType": "Read",
          "ResourceID": 256,
          "SegmentFlags": [
            {
              "LockFlag": "LockSame",
              "SegmentLength": 3
            },
            {
              "LockFlag": "DontLock",
              "SegmentLength": 4
            }
          ],
          "SessionID": "qM4D0VfZt3",
          "TransactionID": 1
        }
      ]
    }
    
    Signed-off-by: manojkiraneda <manojkiran.eda@gmail.com>
    Change-Id: I9375e2927938ab682df06ef60c823b279a7efead

--- include/ibm/locks.hpp
+++ include/ibm/locks.hpp
@@ -144,6 +144,12 @@ class lock
     rcreleaselockapi Releaselock(const listoftransactionIDs,
                                  std::pair<stype, stype>);
 
+    /*
+     * This function implements the logic for getting the list of locks obtained
+     * by a particular management console.
+     */
+    rcgetlocklist getlocklist(const std::vector<std::string>);
+
     lock()
     {
         transactionID = 0;
@@ -151,7 +157,57 @@ class lock
 
 } lockobject;
 
-rcreleaselockapi lock::Releaselock(listoftransactionIDs p,
+rcgetlocklist lock::getlocklist(const std::vector<std::string> listsessionid)
+{
+
+    // validate the session id
+    std::vector<std::pair<uint32_t, lockrequests>> locklist;
+
+    if (!locktable.empty())
+    {
+        std::vector<std::pair<uint32_t, lockrequests>> templist;
+
+        for (auto i : listsessionid)
+        {
+            auto it = locktable.begin();
+            while (it != locktable.end())
+            {
+                // Check if session id of this entry matches with session id
+                // given
+                if (std::get<0>(it->second[0]) == i)
+                {
+                    BMCWEB_LOG_DEBUG << "Session id is found in the locktable";
+
+                    // Push the whole lock record into a vector for returning
+                    // the json
+                    locklist.push_back(std::make_pair(it->first, it->second));
+                    templist.push_back(std::make_pair(it->first, it->second));
+                }
+                // Go to next entry in map
+                it++;
+            }
+        }
+        if (templist.size() == 0)
+        {
+            // The session id is not found in the lock table
+            // return an empty list
+            return std::make_pair(true, locklist);
+        }
+
+        // we found at least one entry with the given session id
+        // return the json list of lock records pertaining to the
+        // given session id
+        return std::make_pair(true, locklist);
+    }
+    else
+    {
+        // if lock table is empty , the return the empty lock list
+        return std::make_pair(true, locklist);
+    }
+    return std::make_pair(true, listsessionid[0]);
+}
+
+rcreleaselockapi lock::Releaselock(const listoftransactionIDs p,
                                    std::pair<stype, stype> ids)
 {
 
