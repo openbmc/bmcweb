1e7639d4ce600bffb7155070646d657ab49ab4a2
Sunitha Harish <sunithaharish04@gmail.com>
2019-12-13 03:48:09 -0600

    Persist the lock table
    This commit persist the lock table whenever there is
    a change in the lock table which may happen during aquire
    lock/Release lock.
    
    This commit also restores the lock during start of the
    bmcweb as locks are tied up with the session which aquired
    the lock.
    
    TestedBy:
    
    Signed-off-by: Sunitha Harish <sunithaharish04@gmail.com>
    Change-Id: I081f61922e7c0c24db12efc4d446cdd641856279

--- include/openbmc_ibm_mc_rest.hpp
+++ include/openbmc_ibm_mc_rest.hpp
@@ -11,6 +11,7 @@
 #include <regex>
 #include <sdbusplus/message/types.hpp>
 #include <utils/json_utils.hpp>
+#include <persistent_ibm_mc_locks.hpp>
 
 #define MAX_SAVE_AREA_FILESIZE 20000
 using stype = std::string;
@@ -55,6 +56,7 @@ class lock
     lock()
     {
         rid = 0;
+        crow::persistent_ibm_mc_lock::LockPersistence::loadLocks(locktable);
     }
 
     // friend class locktest;
@@ -245,6 +247,13 @@ rc lock::isconflictwithtable(lockrequest *reflockrequeststructure)
         // rid = getmyrequestid();
         locktable.emplace(std::pair<uint32_t, lockrequest>(
             transactionID, *reflockrequeststructure));
+        //save the lock in the persistent file
+        bool isSaved = crow::persistent_ibm_mc_lock::LockPersistence::saveLocks(locktable);
+        if(!isSaved)
+        {
+            BMCWEB_LOG_DEBUG << "Error saving the locks in persistent";
+        }
+
         // vrid.push_back(rid);
         // }
         return std::make_pair(false, transactionID);
@@ -288,6 +297,13 @@ rc lock::isconflictwithtable(lockrequest *reflockrequeststructure)
         transactionID = getmyrequestid();
         locktable.emplace(
             std::make_pair(transactionID, *reflockrequeststructure));
+        //save the lock in the persistent file
+        bool isSaved = crow::persistent_ibm_mc_lock::LockPersistence::saveLocks(locktable);
+        if(!isSaved)
+        {
+            BMCWEB_LOG_DEBUG << "Error saving the locks in persistent";
+        }
+
         // vrid.push_back(rid);
         //}
     }
@@ -967,7 +983,7 @@ template <typename... Middlewares> void requestRoutes(Crow<Middlewares...> &app)
         .requires({"ConfigureComponents", "ConfigureManager"})
         .methods(
             "POST"_method)([](const crow::Request &req, crow::Response &res) {
-            std::vector<string> listSessionIDs;
+            std::vector<std::string> listSessionIDs;
 
             if (!redfish::json_util::readJson(req, res, "SessionIDs",
                                               listSessionIDs))
