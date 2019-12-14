0685cdc126952b5bd6b49d9fe4715acabbc7ddd0
Ratan Gupta <ratagupt@linux.vnet.ibm.com>
2019-12-14 09:39:47 +0530

    Rest service root implementation
    This commit introduces the following
    => Service root implementation
    => compiler option for the IBM management console specific functionalities
    
    TestedBy:
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/ibm/v1
    
    Signed-off-by: Ratan Gupta <ratagupt@linux.vnet.ibm.com>
    Change-Id: I2dcb8eee0b69b1723e0cc3d980a5846b3519e7d9

---
+++ include/openbmc_ibm_mc_rest.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <app.h>
+#include <tinyxml2.h>
+
+#include <async_resp.hpp>
+
+namespace crow
+{
+namespace openbmc_ibm_mc
+{
+
+template <typename... Middlewares> void requestRoutes(Crow<Middlewares...> &app)
+{
+
+    // allowed only for admin
+    BMCWEB_ROUTE(app, "/ibm/v1/")
+        .requires({"ConfigureComponents", "ConfigureManager"})
+        .methods(
+            "GET"_method)([](const crow::Request &req, crow::Response &res) {
+            res.jsonValue["@odata.type"] = "#ServiceRoot.v1_0_0.ServiceRoot";
+            res.jsonValue["@odata.id"] = "/ibm/v1/";
+            res.jsonValue["@odata.context"] =
+                "/ibm/v1/$metadata#ServiceRoot.ServiceRoot";
+            res.jsonValue["Id"] = "IBM Rest RootService";
+            res.jsonValue["Name"] = "IBM Rest Root Service";
+            res.jsonValue["ConfigFiles"] = {
+                {"@odata.id", "/ibm/v1/Host/ConfigFiles"}};
+            res.jsonValue["LockService"] = {
+                {"@odata.id", "/redfish/v1/HMC/LockService"}};
+            res.end();
+        });
+}
+
+} // namespace openbmc_ibm_mc
+} // namespace crow
