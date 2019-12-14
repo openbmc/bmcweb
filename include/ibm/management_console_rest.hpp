ffeec994ad7c7cba03dd76250e69cb094f2b2738
Ratan Gupta <ratagupt@linux.vnet.ibm.com>
2019-12-14 14:06:09 +0530

    Remove the locks associated with the session
    This commit does the following
    => during session timeout erase the locks associated
    with the session.
    => Erase the locks when the session is explicitly deleted
    on a user request.
    
    We need to find a different way of calculating session timeout
    currently session timeout gets calculated when the
    request comes to BMC.
    
    TODO: We need some module which keeps looking at the sessions
    in certain time interval and earse the session if it is
    timeout, It is useful in the case where there is resources
    which gets free after session timeout.
    
    It may happen that client gets the session, obtain cerain resources
    on that session and never sends any request, in that case session timeout
    will never occur for that session.
    
    Signed-off-by: Ratan Gupta <ratagupt@linux.vnet.ibm.com>
    Change-Id: Ic9962f761fc84a03747a90bd951ea36eb8962455

--- include/ibm/management_console_rest.hpp
+++ include/ibm/management_console_rest.hpp
@@ -334,7 +334,7 @@ void handleAcquireLockAPI(const crow::Request &req, crow::Response &res,
 
     const LockRequests &t = lockRequestStructure;
 
-    auto varAcquireLock = crow::ibm_mc_lock::lockObject.acquireLock(t);
+    auto varAcquireLock = crow::ibm_mc_lock::Lock::getInstance().acquireLock(t);
 
     if (varAcquireLock.first)
     {
@@ -420,7 +420,7 @@ void handleReleaseLockAPI(const crow::Request &req, crow::Response &res,
 
     // validate the request ids
 
-    auto varReleaselock = crow::ibm_mc_lock::lockObject.releaseLock(
+    auto varReleaselock = crow::ibm_mc_lock::Lock::getInstance().releaseLock(
         listTransactionIds, std::make_pair(clientId, sessionId));
 
     if (!varReleaselock.first)
@@ -478,7 +478,8 @@ void handleGetLockListAPI(const crow::Request &req, crow::Response &res,
 {
     BMCWEB_LOG_DEBUG << listSessionIds.size();
 
-    auto status = crow::ibm_mc_lock::lockObject.getLockList(listSessionIds);
+    auto status =
+        crow::ibm_mc_lock::Lock::getInstance().getLockList(listSessionIds);
     auto var = std::get<std::vector<std::pair<uint32_t, LockRequests>>>(status);
 
     nlohmann::json lockRecords = nlohmann::json::array();
@@ -567,7 +568,6 @@ template <typename... Middlewares> void requestRoutes(Crow<Middlewares...> &app)
         .methods("POST"_method)(
             [](const crow::Request &req, crow::Response &res) {
                 std::vector<nlohmann::json> body;
-
                 if (!redfish::json_util::readJson(req, res, "Request", body))
                 {
                     BMCWEB_LOG_DEBUG << "Not a Valid JSON";
@@ -577,7 +577,6 @@ template <typename... Middlewares> void requestRoutes(Crow<Middlewares...> &app)
                 }
                 handleAcquireLockAPI(req, res, body);
             });
-
     BMCWEB_ROUTE(app, "/ibm/v1/HMC/LockService/Actions/LockService.ReleaseLock")
         .requires({"ConfigureComponents", "ConfigureManager"})
         .methods("POST"_method)(
@@ -591,7 +590,6 @@ template <typename... Middlewares> void requestRoutes(Crow<Middlewares...> &app)
                     res.end();
                     return;
                 }
-
                 handleReleaseLockAPI(req, res, listTransactionIds);
             });
     BMCWEB_ROUTE(app, "/ibm/v1/HMC/LockService/Actions/LockService.GetLockList")
@@ -607,7 +605,6 @@ template <typename... Middlewares> void requestRoutes(Crow<Middlewares...> &app)
                     res.end();
                     return;
                 }
-
                 handleGetLockListAPI(req, res, listSessionIds);
             });
 }
