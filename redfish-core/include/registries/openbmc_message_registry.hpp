0def8210eca7dc05de92ec7e6ab8dcb4943fb73b
ZhikuiRen <zhikui.ren@intel.com>
2020-01-29 14:58:08 -0800

    Log BIOS POST Codes Through Redfish
    Added PostCodes MessageID, PostCodesLogService, PostCodesClear,
    PostCodesEntry and PostCodesEntryCollection to redfish-core log_services.
    Design document located at openbmc/docs/designs/redfish-postcodes.md:
    
    Tested:
    Build with changes in phosphor-dbus-interfaces
    and phosphor-post-code-manager.
    PostCode Collection and entries passed RedfishServiceValidator test.
    
    Reviewed redfish log contents against
    design document that is mentioned above:
    bmc/redfish/v1/Systems/system/LogServices
    bmc/redfish/v1/Systems/system/LogServices/PostCodes
    bmc/redfish/v1/Systems/system/LogServices/PostCodes/Entries
    
    Send POST clear commands and verified PostCodes log is cleared and
    boot cycle index is reset to 1.
    
    Change-Id: I8de4d4749d1a17d590619d70670d279c01753b03
    Signed-off-by: ZhikuiRen <zhikui.ren@intel.com>

--- redfish-core/include/registries/openbmc_message_registry.hpp
+++ redfish-core/include/registries/openbmc_message_registry.hpp
@@ -29,7 +29,7 @@ const Header header = {
     "0.1.0",
     "OpenBMC",
 };
-constexpr std::array<MessageEntry, 153> registry = {
+constexpr std::array<MessageEntry, 154> registry = {
     MessageEntry{
         "ADDDCCorrectable",
         {
@@ -170,6 +170,15 @@ constexpr std::array<MessageEntry, 153> registry = {
                      {
                          "string",
                      },
+                     "None.",
+                 }},
+    MessageEntry{"BIOSPOSTCode",
+                 {
+                     "BIOS Power-On Self-Test Code received",
+                     "Boot Count: %1: TS Offset: %2; POST Code: %3",
+                     "OK",
+                     3,
+                     {"number", "number", "number"},
                      "None.",
                  }},
     MessageEntry{"BIOSPOSTError",
