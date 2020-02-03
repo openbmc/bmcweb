d1e9d08ad3102e22beb7f59c26f2394e33d79fa8
raviteja-b <raviteja28031990@gmail.com>
2020-02-03 12:23:18 -0600

    Redfish:Dump offload handler implementation using nbd-proxy
    This handler transfers data between nbd-client and nbd-server.
    basically it invokes nbd-proxy and reads data from socket
    and writes on to nbd-client and vice-versa
    
    Change-Id: I429393a5e056647333bf4e148c0df2a5695b2a47
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- http/http_connection.h
+++ http/http_connection.h
@@ -517,6 +517,10 @@ class Connection : public std::enable_shared_from_this<
 
         if (!isInvalidRequest)
         {
+            req->socket = [this, self = shared_from_this()]() -> Adaptor& {
+                return self->socket();
+            };
+
             res.completeRequestHandler = [] {};
             res.isAliveHelper = [this]() -> bool { return isAlive(); };
 
