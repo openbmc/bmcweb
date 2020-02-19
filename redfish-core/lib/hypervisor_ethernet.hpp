46eb8eb2b3d28efe94d77702a899c1026ec74815
Sunitha Harish <sunithaharish04@gmail.com>
2020-02-19 08:34:59 -0600

    Adding Hypervisor system and Hypervisor Interface
    This commit implements the GET commands for Power hypervisor
    Virtual Management Interface's interface collection.
    
    Tested by:
    1. GET https://${bmc}/redfish/v1/Systems
    2. GET https://${bmc}/redfish/v1/Systems/hypervisor
    3. GET https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces
    4. Successfully ran the Redfish Validator
    
    *** /redfish/v1/Systems
             Type (#ComputerSystemCollection.ComputerSystemCollection), GET SUCCESS (time: 0.335171)
             PASS
    
    *** /redfish/v1/Systems/hypervisor
             Type (#ComputerSystem.v1_6_0.ComputerSystem), GET SUCCESS (time: 0.341849)
             PASS
    
    *** /redfish/v1/Systems/hypervisor/EthernetInterfaces
             Type (#EthernetInterfaceCollection.EthernetInterfaceCollection), GET SUCCESS (time: 0.362111)
             PASS
    5. Verified the hypervisor in not listed on BMC which does not implement
       the Hypervisor object
    
    Signed-off-by: Sunitha Harish <sunithaharish04@gmail.com>
    Change-Id: Ie1eb166808473137dd65df54fb4d748cfd1131d2

---
+++ redfish-core/lib/hypervisor_ethernet.hpp
@@ -0,0 +1,138 @@
+#pragma once
+
+#include <boost/container/flat_map.hpp>
+#include <boost/container/flat_set.hpp>
+#include <dbus_singleton.hpp>
+#include <error_messages.hpp>
+#include <node.hpp>
+#include <optional>
+#include <utils/json_utils.hpp>
+#include <variant>
+
+namespace redfish
+{
+
+/**
+ * Hypervisor Systems derived class for delivering Computer Systems Schema.
+ */
+class HypervisorSystem : public Node
+{
+  public:
+    /*
+     * Default Constructor
+     */
+    HypervisorSystem(CrowApp &app) :
+        Node(app, "/redfish/v1/Systems/hypervisor/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureComponents"}}}};
+    }
+
+  private:
+    /**
+     * Functions triggers appropriate requests on DBus
+     */
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        std::shared_ptr<AsyncResp> asyncResp = std::make_shared<AsyncResp>(res);
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec,
+                        const std::variant<std::string> &hostName) {
+                if (ec)
+                {
+                    messages::resourceNotFound(asyncResp->res, "System",
+                                               "hypervisor");
+                    return;
+                }
+                BMCWEB_LOG_DEBUG << "Hypervisor is available";
+
+                asyncResp->res.jsonValue["@odata.type"] =
+                    "#ComputerSystem.v1_6_0.ComputerSystem";
+                asyncResp->res.jsonValue["@odata.id"] =
+                    "/redfish/v1/Systems/hypervisor";
+                asyncResp->res.jsonValue["Description"] = "Hypervisor";
+                asyncResp->res.jsonValue["Name"] = "Hypervisor";
+                asyncResp->res.jsonValue["Id"] = "hypervisor";
+                asyncResp->res.jsonValue["Links"]["ManagedBy"] = {
+                    {{"@odata.id", "/redfish/v1/Managers/bmc"}}};
+                asyncResp->res.jsonValue["EthernetInterfaces"] = {
+                    {"@odata.id", "/redfish/v1/Systems/hypervisor/"
+                                  "EthernetInterfaces"}};
+                // TODO: Add "SystemType" : "hypervisor"
+            },
+            "xyz.openbmc_project.Settings", "/xyz/openbmc_project/network/vmi",
+            "org.freedesktop.DBus.Properties", "Get",
+            "xyz.openbmc_project.Network.SystemConfiguration", "HostName");
+    }
+};
+
+/**
+ * HypervisorInterfaceCollection class to handle the GET and PATCH on Hypervisor
+ * Interface
+ */
+class HypervisorInterfaceCollection : public Node
+{
+  public:
+    template <typename CrowApp>
+    HypervisorInterfaceCollection(CrowApp &app) :
+        Node(app, "/redfish/v1/Systems/hypervisor/EthernetInterfaces/")
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureComponents"}}}};
+    }
+
+  private:
+    /**
+     * Functions triggers appropriate requests on DBus
+     */
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        std::shared_ptr<AsyncResp> asyncResp = std::make_shared<AsyncResp>(res);
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec,
+                        const std::variant<std::string> &hostname) {
+                nlohmann::json &iface_array =
+                    asyncResp->res.jsonValue["Members"];
+                auto &count = asyncResp->res.jsonValue["Members@odata.count"];
+                count = 0;
+
+                if (ec)
+                {
+                    messages::resourceNotFound(asyncResp->res, "System",
+                                               "hypervisor");
+                    return;
+                }
+                asyncResp->res.jsonValue["@odata.type"] =
+                    "#EthernetInterfaceCollection."
+                    "EthernetInterfaceCollection";
+                asyncResp->res.jsonValue["@odata.id"] =
+                    "/redfish/v1/Systems/hypervisor/EthernetInterfaces";
+                asyncResp->res.jsonValue["Name"] =
+                    "Virtual Management Ethernet "
+                    "Network Interface Collection";
+                asyncResp->res.jsonValue["Description"] =
+                    "Collection of Virtual Management "
+                    "Interfaces for the hypervisor";
+                // By default two Ethernet Interface are available for the
+                // Hypervisor
+                iface_array.push_back(
+                    {{"@odata.id", "/redfish/v1/Systems/hypervisor/"
+                                   "EthernetInterfaces/intf0"}});
+                iface_array.push_back(
+                    {{"@odata.id", "/redfish/v1/Systems/hypervisor/"
+                                   "EthernetInterfaces/intf1"}});
+                count = iface_array.size();
+            },
+            "xyz.openbmc_project.Settings", "/xyz/openbmc_project/network/vmi",
+            "org.freedesktop.DBus.Properties", "Get",
+            "xyz.openbmc_project.Network.SystemConfiguration", "HostName");
+    }
+};
+
+} // namespace redfish
