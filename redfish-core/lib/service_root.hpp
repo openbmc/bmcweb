3fc7450a5a156e22c77d9a56c2710bfa694aabb2
James Feist <james.feist@linux.intel.com>
2020-02-19 15:11:58 -0800

    Add TaskService
    This adds tasks service to Redfish and creates an
    example for crashdump. The TaskData object creates
    tasks that can be updated based on d-bus matches. It
    also has a configurable timeout using timers. Task
    Monitor uses these task objects to reply with a 202
    until the async task is done, then a 204 when it is
    either failed or completed.
    
    Messages support will come in future commit.
    
    Tested: Validator passed, wrote script to poll monitor,
    verified that got 202 with location header and retry-after
    set correctly, then 204, then 404.
    
    Change-Id: I109e671baa1c1eeff1a11ae578e7361bf6ef9f14
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- redfish-core/lib/service_root.hpp
+++ redfish-core/lib/service_root.hpp
@@ -66,6 +66,7 @@ class ServiceRoot : public Node
         res.jsonValue["UUID"] = uuid;
         res.jsonValue["CertificateService"] = {
             {"@odata.id", "/redfish/v1/CertificateService"}};
+        res.jsonValue["Tasks"] = {{"@odata.id", "/redfish/v1/TaskService"}};
         res.end();
     }
 
