0b959091db0168e7a0dd21f7a3d387f0646e55a0
Kuiying Wang <kuiying.wang@intel.com>
2020-02-24 19:30:17 +0800

    Define Redfish interface "/Registries/Bios" and enable Attributes property
    1. Define Redfish interface "/Registries/Bios" for BIOS Attribute Registry
       RBC Daemon provide method to get BIOS attribute resgistry.
    2. Eanble Attributes property for BIOS resource
    3. Define Redfish interface "/Systems/system/Bios/Settings" for BIOS
    settings
    
    Tested:
    Use postman (Redfish tool) could get all the attributes as below:
    https://IP_ADDR/redfish/v1/Systems/system/Bios
    {
        "@odata.id": "/redfish/v1/Systems/system/Bios",
        "@odata.type": "#Bios.v1_1_0.Bios",
        "Actions": {
            "#Bios.ResetBios": {
                "target": "/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios"
            }
        },
        "Attributes": {
            "ADDDCEn": "0x00",
            "ADREn": "0x01",
            "AEPErrorInjEn": "0x00",
            "ARIEnable": "0x01",
            "ASPML1Check": "0x00",
            "AcsControl": "0x01",
            "ActiveCoreNum": "0x00",
            "AddinVideoAdapter": "0x02",
            "BootMode": "0x01",
            "BootOrder_0": "0x0004",
            "BootOrder_1": "0x0005",
            "BootOrder_2": "0x0006",
            ...........
        }
    }
    
    Redfish interface: https://BMCIP/redfish/v1/Registries/Bios
    {
        "@odata.context":
    "/redfish/v1/$metadata#AttributeRegistry.AttributeRegistry",
        "@odata.id": "/redfish/v1/Registries/Bios",
        "@odata.type": "#AttributeRegistry.v1_3_2.AttributeRegistry",
        "Id": "BiosAttributeRegistryV1",
        "Language": "en",
        "Name": "Bios Attribute Registry Version 1",
        "OwningEntity": "Intel",
        "RegistryEntries": {
            "Attributes": [
                {
                    "AttributeName": "QuietBoot",
                    "CurrentValue": "0x01",
                    "DefaultValue": "0x01",
                    "Discription": "0x01",
                    "DisplayName": "Quiet Boot",
                    "DisplayOrder": "00",
                    "MenuPath": "Main/Quiet Boot",
                    ......
                }
        }
        "RegistryVersion": "1.0.0",
        "SupportedSystems": []
    }
    
    https://BMC_IPADDR/redfish/v1/Systems/system/Bios/Settings
    {
        "@odata.id": "/redfish/v1/Systems/system/Bios/Settings",
        "@odata.type": "#Bios.v1_1_0.Bios",
        "AttributeRegistry": "BiosAttributeRegistryV1",
        "Attributes": {},
        "Id": "BiosSettingsV1",
        "Name": "Bios Settings Version 1"
    }
    
    Change-Id: Iecc61018c350f0b8c89df59b2864b941508b1916
    Signed-off-by: Kuiying Wang <kuiying.wang@intel.com>

--- redfish-core/lib/bios.hpp
+++ redfish-core/lib/bios.hpp
@@ -2,8 +2,16 @@
 
 #include "node.hpp"
 
+#include <tinyxml2.h>
+
 namespace redfish
 {
+enum class AttributesModel
+{
+    Attributeslist = 0,
+    AttributesRegistry = 1,
+    PendingAttributelist = 2
+};
 /**
  * BiosService class supports handle get method for bios.
  */
@@ -16,6 +24,8 @@ class BiosService : public Node
     }
 
   private:
+    static constexpr const char *biosAttributeFilePath =
+        "/tmp/BIOSConfig/BiosAttribute.json";
     void doGet(crow::Response &res, const crow::Request &req,
                const std::vector<std::string> &params) override
     {
@@ -30,6 +40,40 @@ class BiosService : public Node
         asyncResp->res.jsonValue["Actions"]["#Bios.ResetBios"] = {
             {"target",
              "/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios"}};
+        asyncResp->res.jsonValue["@Redfish.Settings"] = {
+            {"@odata.type", "#Settings.v1_3_0.Settings"},
+            {"SettingsObject",
+             {"@odata.id", "/redfish/v1/Systems/system/Bios/Settings"}}};
+        asyncResp->res.jsonValue["Attributes"] = nlohmann::json::object();
+        getBiosAttributes(asyncResp);
+    }
+
+    void getBiosAttributes(std::shared_ptr<AsyncResp> asyncResp)
+    {
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec) {
+                if (ec)
+                {
+                    BMCWEB_LOG_DEBUG << "getBiosAttributes DBUS error: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                nlohmann::json &attributeArray =
+                    asyncResp->res.jsonValue["Attributes"];
+                if (!std::filesystem::exists(biosAttributeFilePath))
+                {
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                std::ifstream file(biosAttributeFilePath);
+                attributeArray = nlohmann::json::parse(file);
+                file.close();
+                std::remove(biosAttributeFilePath);
+            },
+            "xyz.openbmc_project.biosconfig_manager",
+            "/xyz/openbmc_project/BIOSConfig/BIOSConfigMgr",
+            "xyz.openbmc_project.BIOSConfig.BIOSConfigMgr", "GetAllAttributes",
+            static_cast<uint8_t>(AttributesModel::Attributeslist));
     }
 };
 /**
@@ -68,6 +112,196 @@ class BiosReset : public Node
             "org.open_power.Software.Host.Updater",
             "/xyz/openbmc_project/software",
             "xyz.openbmc_project.Common.FactoryReset", "Reset");
+    }
+};
+
+/**
+ * BiosSettings class supports handle GET/PATCH method for
+ * BIOS configuration pending settings.
+ */
+class BiosSettings : public Node
+{
+  public:
+    static constexpr const char *biosSettingsFilePath =
+        "/tmp/BIOSConfig/BiosSettings.json";
+    BiosSettings(CrowApp &app) :
+        Node(app, "/redfish/v1/Systems/system/Bios/Settings")
+    {
+        entityPrivileges = {{boost::beast::http::verb::get, {{"Login"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        asyncResp->res.jsonValue["@odata.id"] =
+            "/redfish/v1/Systems/system/Bios/Settings";
+        asyncResp->res.jsonValue["@odata.type"] = "#Bios.v1_1_0.Bios";
+        asyncResp->res.jsonValue["Name"] = "Bios Settings Version 1";
+        asyncResp->res.jsonValue["Id"] = "BiosSettingsV1";
+        asyncResp->res.jsonValue["AttributeRegistry"] =
+            "BiosAttributeRegistryV1";
+        asyncResp->res.jsonValue["Attributes"] = nlohmann::json::object();
+
+        getBiosSettings(asyncResp);
+    }
+    void doPatch(crow::Response &res, const crow::Request &req,
+                 const std::vector<std::string> &params) override
+    {
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        nlohmann::json jsonRequest;
+        if (!json_util::processJsonFromRequest(res, req, jsonRequest))
+        {
+            BMCWEB_LOG_ERROR << "processJsonFromRequest error";
+            messages::internalError(res);
+            return;
+        }
+        if (std::filesystem::exists(biosSettingsFilePath))
+        {
+            nlohmann::json biosSettingsJson;
+            std::ifstream ifile;
+            ifile.open(biosSettingsFilePath);
+            biosSettingsJson = nlohmann::json::parse(ifile);
+            biosSettingsJson.merge_patch(jsonRequest);
+            ifile.close();
+
+            std::ofstream ofile;
+            ofile.open(biosSettingsFilePath);
+            ofile << biosSettingsJson.dump() << std::endl;
+            ofile.close();
+        }
+        else
+        {
+            std::ofstream ofile;
+            ofile.open(biosSettingsFilePath);
+            ofile << jsonRequest.dump() << std::endl;
+            ofile.close();
+        }
+
+        patchBiosSettings(asyncResp);
+    }
+    /**
+     * Patch BIOS Settings from d-bus.
+     */
+    void patchBiosSettings(std::shared_ptr<AsyncResp> asyncResp)
+    {
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec, const bool ret) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "patchBiosSettings DBUS error: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                if (ret)
+                {
+                    messages::success(asyncResp->res);
+                    return;
+                }
+                messages::propertyValueNotInList(
+                    asyncResp->res, "Invalid value", "Or invalid key");
+            },
+            "xyz.openbmc_project.biosconfig_manager",
+            "/xyz/openbmc_project/BIOSConfig/BIOSConfigMgr",
+            "xyz.openbmc_project.BIOSConfig.BIOSConfigMgr",
+            "PatchBiosSettings");
+    }
+    /**
+     * Get BIOS Settings from d-bus.
+     */
+    void getBiosSettings(std::shared_ptr<AsyncResp> asyncResp)
+    {
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "getBiosSettings DBUS error: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                nlohmann::json &attributeArray =
+                    asyncResp->res.jsonValue["Attributes"];
+                if (!std::filesystem::exists(biosSettingsFilePath))
+                {
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                std::ifstream file(biosSettingsFilePath);
+                attributeArray = nlohmann::json::parse(file);
+                file.close();
+            },
+            "xyz.openbmc_project.biosconfig_manager",
+            "/xyz/openbmc_project/BIOSConfig/BIOSConfigMgr",
+            "xyz.openbmc_project.BIOSConfig.BIOSConfigMgr", "GetAllAttributes",
+            static_cast<uint8_t>(AttributesModel::PendingAttributelist));
+    }
+};
+/**
+ * BiosAttributeRegistry class supports handle get method for BIOS attribute
+ * registry.
+ */
+class BiosAttributeRegistry : public Node
+{
+  public:
+    static constexpr const char *biosAttributeRegistryFilePath =
+        "/tmp/BIOSConfig/BiosAttributeRegistry.json";
+    BiosAttributeRegistry(CrowApp &app) :
+        Node(app, "/redfish/v1/Registries/Bios/")
+    {
+        entityPrivileges = {{boost::beast::http::verb::get, {{"Login"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        asyncResp->res.jsonValue["@odata.id"] = "/redfish/v1/Registries/Bios";
+        asyncResp->res.jsonValue["@odata.context"] =
+            "/redfish/v1/$metadata#AttributeRegistry.AttributeRegistry";
+        asyncResp->res.jsonValue["@odata.type"] =
+            "#AttributeRegistry.v1_3_2.AttributeRegistry";
+        asyncResp->res.jsonValue["Name"] = "Bios Attribute Registry Version 1";
+        asyncResp->res.jsonValue["Id"] = "BiosAttributeRegistryV1";
+        asyncResp->res.jsonValue["RegistryVersion"] = "1.0.0";
+        asyncResp->res.jsonValue["Language"] = "en";
+        asyncResp->res.jsonValue["OwningEntity"] = "Intel";
+        asyncResp->res.jsonValue["SupportedSystems"] = nlohmann::json::array();
+
+        getBiosAttributeRegistry(asyncResp);
+    }
+
+    /**
+     * Get BIOS attribute registry from d-bus.
+     */
+    void getBiosAttributeRegistry(std::shared_ptr<AsyncResp> asyncResp)
+    {
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec) {
+                if (ec)
+                {
+                    BMCWEB_LOG_DEBUG << "getBiosAttributeRegistry DBUS error: "
+                                     << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                nlohmann::json &attributeArray =
+                    asyncResp->res.jsonValue["RegistryEntries"];
+                if (!std::filesystem::exists(biosAttributeRegistryFilePath))
+                {
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                std::ifstream file(biosAttributeRegistryFilePath);
+                nlohmann::json tempAttrRegJson = nlohmann::json::parse(file);
+                attributeArray = tempAttrRegJson["RegistryEntries"];
+                file.close();
+            },
+            "xyz.openbmc_project.biosconfig_manager",
+            "/xyz/openbmc_project/BIOSConfig/BIOSConfigMgr",
+            "xyz.openbmc_project.BIOSConfig.BIOSConfigMgr", "GetAllAttributes",
+            static_cast<uint8_t>(AttributesModel::AttributesRegistry));
     }
 };
 } // namespace redfish
