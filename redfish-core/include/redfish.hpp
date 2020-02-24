bae0edd743a5c6bc1acb1f6bca656001dc0257a4
Sunitha Harish <sunithaharish04@gmail.com>
2020-02-24 02:40:24 -0600

    Redfish Interface Implementation for Hypervisor-IPv4 Static IP
    This commit implements the GET command for Power hypervisor
    Virtual Management Interface's IPv4 static network.
    
    Tested by:
    1. GET https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/intf0
    2. GET https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/intf1
    3. Successfully ran the Redfish Validator
    
    *** /redfish/v1/Systems/hypervisor/EthernetInterfaces/intf0
             Type (#EthernetInterface.v1_5_1.EthernetInterface), GET SUCCESS (time: 0.400974)
             PASS
    
    *** /redfish/v1/Systems/hypervisor/EthernetInterfaces/intf1
             Type (#EthernetInterface.v1_5_1.EthernetInterface), GET SUCCESS (time: 0.366143)
             PASS
    
    Signed-off-by: Sunitha Harish <sunithaharish04@gmail.com>
    Change-Id: Ie0f6996a01f9772e91dcf6d5045a20f3a257f67c

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -175,6 +175,7 @@ class RedfishService
 
         nodes.emplace_back(
             std::make_unique<HypervisorInterfaceCollection>(app));
+        nodes.emplace_back(std::make_unique<HypervisorInterface>(app));
         nodes.emplace_back(std::make_unique<HypervisorSystem>(app));
 
         for (const auto& node : nodes)
