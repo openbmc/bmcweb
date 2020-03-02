f75627701cf846942281183a9fe2f27be24da5a0
Kuiying Wang <kuiying.wang@intel.com>
2020-03-02 14:55:58 +0800

    Define Redfish interface "/Registries/Bios" for BIOS Attribute Registry
    OOB Daemon provide method to get BIOS attribute resgistry.
    It depends on dbus interfaces define of OOB manager
    https://gerrit.openbmc-project.xyz/#/c/openbmc/phosphor-dbus-interfaces/+/18242/
    
    Tested:
    Redfish interface: https://BMCIP/redfish/v1/Registries/Bios
    
    {
        "@odata.context": "/redfish/v1/$metadata#AttributeRegistry.AttributeRegistry",
        "@odata.id": "/redfish/v1/Registries/Bios",
        "@odata.type": "#AttributeRegistry.v1_3_2.AttributeRegistry",
        "Id": "BiosAttributeRegistryV1",
        "Language": "en",
        "Name": "Bios Attribute Registry Version 1",
        "OwningEntity": "Intel",
        "RegistryEntries": {
            "Attributes": [
                {
                    "AttributeName": "QuietBoot",
                    "CurrentValue": "0x01",
                    "DefaultValue": "0x01",
                    "Discription": "0x01",
                    "DisplayName": "Quiet Boot",
                    "DisplayOrder": "00",
                    "MenuPath": "Main/Quiet Boot",
                    ......
                }
        }
        "RegistryVersion": "1.0.0",
        "SupportedSystems": []
    }
    
    Change-Id: I4d1ca3f14b1640df8da4e66a6c4c6df7c61d2c9c
    Signed-off-by: Kuiying Wang <kuiying.wang@intel.com>

--- redfish-core/lib/bios.hpp
+++ redfish-core/lib/bios.hpp
@@ -2,6 +2,10 @@
 
 #include "node.hpp"
 
+#include <tinyxml2.h>
+
+#include <fstream>
+
 namespace redfish
 {
 /**
@@ -68,6 +72,69 @@ class BiosReset : public Node
             "org.open_power.Software.Host.Updater",
             "/xyz/openbmc_project/software",
             "xyz.openbmc_project.Common.FactoryReset", "Reset");
+    }
+};
+
+/**
+ * BiosAttributeRegistry class supports handle get method for BIOS attribute
+ * registry.
+ */
+class BiosAttributeRegistry : public Node
+{
+  public:
+    BiosAttributeRegistry(CrowApp &app) :
+        Node(app, "/redfish/v1/Registries/Bios/")
+    {
+        entityPrivileges = {{boost::beast::http::verb::get, {{"Login"}}}};
+    }
+
+  private:
+    void doGet(crow::Response &res, const crow::Request &req,
+               const std::vector<std::string> &params) override
+    {
+        auto asyncResp = std::make_shared<AsyncResp>(res);
+        asyncResp->res.jsonValue["@odata.id"] = "/redfish/v1/Registries/Bios";
+        asyncResp->res.jsonValue["@odata.context"] =
+            "/redfish/v1/$metadata#AttributeRegistry.AttributeRegistry";
+        asyncResp->res.jsonValue["@odata.type"] =
+            "#AttributeRegistry.v1_3_2.AttributeRegistry";
+        asyncResp->res.jsonValue["Name"] = "Bios Attribute Registry Version 1";
+        asyncResp->res.jsonValue["Id"] = "BiosAttributeRegistryV1";
+        asyncResp->res.jsonValue["RegistryVersion"] = "1.0.0";
+        asyncResp->res.jsonValue["Language"] = "en";
+        asyncResp->res.jsonValue["OwningEntity"] = "Intel";
+        asyncResp->res.jsonValue["SupportedSystems"] = nlohmann::json::array();
+
+        getBiosAttributeRegistry(asyncResp);
+    }
+
+    /**
+     * Get BIOS attribute registry from d-bus.
+     */
+    void getBiosAttributeRegistry(std::shared_ptr<AsyncResp> asyncResp)
+    {
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec,
+                        const std::variant<std::string> &attributeString) {
+                if (ec)
+                {
+                    BMCWEB_LOG_DEBUG
+                        << "getBiosAttributeRegistry DBUS error: " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                nlohmann::json &attributeArray =
+                    asyncResp->res.jsonValue["RegistryEntries"];
+                const std::string *attrStr =
+                    std::get_if<std::string>(&attributeString);
+                if (attrStr != nullptr)
+                {
+                    attributeArray = nlohmann::json::parse(attrStr->c_str());
+                }
+            },
+            "xyz.openbmc_project.OOB.BiosConfig",
+            "/xyz/openbmc_project/OOB/BiosConfig/Manager",
+            "xyz.openbmc_project.OOB.BiosConfigManager", "GetBiosAttributeRegistry");
     }
 };
 } // namespace redfish
