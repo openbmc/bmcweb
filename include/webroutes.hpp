7272376cd4097ca472124f90cfe89fd95e6b87a9
James Feist <james.feist@linux.intel.com>
2020-04-03 10:58:55 -0700

    Rework Authorization flow
    Currently we parse the whole message before authenticating,
    allowing an attacker the ability to upload a large image,
    or keep a connection open for the max amount of time easier
    than it should be. This moves the authentication to the
    earliest point possible, and restricts unauthenticated users
    timeouts and max upload sizes. It also makes it so that
    unauthenticated users cannot keep the connection alive
    forever by refusing to close the connection.
    
    Tested:
    - login/logout
    - firmware update
    - large POST when unauthenticated
    - timeouts when unauthenticated
    - slowhttptest
    
    Change-Id: Ifa02d8db04eac1821e8950eb85e71634a9e6d265
    Signed-off-by: James Feist <james.feist@linux.intel.com>

---
+++ include/webroutes.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <boost/container/flat_set.hpp>
+namespace crow
+{
+namespace webroutes
+{
+
+static boost::container::flat_set<std::string> routes;
+
+} // namespace webroutes
+
+} // namespace crow
+
