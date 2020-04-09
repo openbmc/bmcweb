c9d272174c80536e8f07711e062ed9804f73437c
AppaRao Puli <apparao.puli@linux.intel.com>
2020-04-09 22:09:03 +0530

    EventService: Add event log support with inotify
    Add event logs support
     - Event log monitor by adding inotify on redfish log file.
     - Read event logs from "/var/log/redfish" file.
     - Filter the event logs using configured settings.
     - Format the event log data as per Events schema.
     - Send event log information to subcribed client.
    
    Tested:
     - Added new event log subscription with filters
       and verified the event logs on event listener.
     - Ran redfish validater successfully.
     - Disabled BMCWEB_ENABLE_REDFISH_DBUS_LOG_ENTRIES
       and build successful.
    
       Event on listener looks as below:
    {
       "@odata.type":"#Event.v1_4_0.Event",
       "Events":[
          {
             "Context":"CustomText",
             "EventId":"94787",
             "EventTimestamp":"1970-01-02T02:19:47+00:00",
             "EventType":"Event",
             "Message":"Memory ThermTrip asserted: .",
             "MessageArgs":[
                ""
             ],
             "MessageId":"OpenBMC.0.1.MemoryThermTrip",
             "Severity":"Critical"
          }
       ],
       "Id":"3",
       "Name":"Event Log"
    }
    
    Change-Id: Ie87322ff59f9f7caa26fb18d2e3b8d7af77ec540
    Signed-off-by: AppaRao Puli <apparao.puli@linux.intel.com>

--- redfish-core/include/event_service_manager.hpp
+++ redfish-core/include/event_service_manager.hpp
@@ -15,6 +15,11 @@
 */
 #pragma once
 #include "node.hpp"
+#include "registries.hpp"
+#include "registries/base_message_registry.hpp"
+#include "registries/openbmc_message_registry.hpp"
+
+#include <sys/inotify.h>
 
 #include <boost/container/flat_map.hpp>
 #include <cstdlib>
@@ -27,6 +32,227 @@
 
 namespace redfish
 {
+#ifndef BMCWEB_ENABLE_REDFISH_DBUS_LOG_ENTRIES
+constexpr const char* redfishEventLogFile = "/var/log/redfish";
+constexpr const uint32_t inotifyFileAction = IN_MODIFY;
+std::shared_ptr<boost::asio::posix::stream_descriptor> inotifyConn = nullptr;
+
+struct EventDeleter
+{
+    void operator()(sd_event* event) const
+    {
+        event = sd_event_unref(event);
+    }
+};
+
+using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
+static std::unique_ptr<sd_event, EventDeleter> eventPtr;
+
+namespace message_registries
+{
+static const Message*
+    getMsgFromRegistry(const std::string& messageKey,
+                       const boost::beast::span<const MessageEntry> registry)
+{
+    boost::beast::span<const MessageEntry>::const_iterator messageIt =
+        std::find_if(registry.cbegin(), registry.cend(),
+                     [&messageKey](const MessageEntry& messageEntry) {
+                         return !std::strcmp(messageEntry.first,
+                                             messageKey.c_str());
+                     });
+    if (messageIt != registry.cend())
+    {
+        return &messageIt->second;
+    }
+
+    return nullptr;
+}
+
+static const Message* formatMessage(const std::string_view& messageID)
+{
+    // Redfish MessageIds are in the form
+    // RegistryName.MajorVersion.MinorVersion.MessageKey, so parse it to find
+    // the right Message
+    std::vector<std::string> fields;
+    fields.reserve(4);
+    boost::split(fields, messageID, boost::is_any_of("."));
+    std::string& registryName = fields[0];
+    std::string& messageKey = fields[3];
+
+    // Find the right registry and check it for the MessageKey
+    if (std::string(base::header.registryPrefix) == registryName)
+    {
+        return getMsgFromRegistry(
+            messageKey, boost::beast::span<const MessageEntry>(base::registry));
+    }
+    if (std::string(openbmc::header.registryPrefix) == registryName)
+    {
+        return getMsgFromRegistry(
+            messageKey,
+            boost::beast::span<const MessageEntry>(openbmc::registry));
+    }
+    return nullptr;
+}
+} // namespace message_registries
+
+namespace event_log
+{
+bool getUniqueEntryID(const std::string& logEntry, std::string& entryID,
+                      const bool firstEntry = true)
+{
+    static time_t prevTs = 0;
+    static int index = 0;
+    if (firstEntry)
+    {
+        prevTs = 0;
+    }
+
+    // Get the entry timestamp
+    std::time_t curTs = 0;
+    std::tm timeStruct = {};
+    std::istringstream entryStream(logEntry);
+    if (entryStream >> std::get_time(&timeStruct, "%Y-%m-%dT%H:%M:%S"))
+    {
+        curTs = std::mktime(&timeStruct);
+    }
+    // If the timestamp isn't unique, increment the index
+    if (curTs == prevTs)
+    {
+        index++;
+    }
+    else
+    {
+        // Otherwise, reset it
+        index = 0;
+    }
+    // Save the timestamp
+    prevTs = curTs;
+
+    entryID = std::to_string(curTs);
+    if (index > 0)
+    {
+        entryID += "_" + std::to_string(index);
+    }
+    return true;
+}
+
+int getEventLogParams(const std::string logEntry, std::string& timestamp,
+                      std::string& messageID,
+                      boost::beast::span<std::string>& messageArgs)
+{
+    // The redfish log format is "<Timestamp> <MessageId>,<MessageArgs>"
+    // First get the Timestamp
+    size_t space = logEntry.find_first_of(" ");
+    if (space == std::string::npos)
+    {
+        return 1;
+    }
+    timestamp = logEntry.substr(0, space);
+    // Then get the log contents
+    size_t entryStart = logEntry.find_first_not_of(" ", space);
+    if (entryStart == std::string::npos)
+    {
+        return 1;
+    }
+    std::string_view entry(logEntry);
+    entry.remove_prefix(entryStart);
+    // Use split to separate the entry into its fields
+    std::vector<std::string> logEntryFields;
+    boost::split(logEntryFields, entry, boost::is_any_of(","),
+                 boost::token_compress_on);
+    // We need at least a MessageId to be valid
+    if (logEntryFields.size() < 1)
+    {
+        return 1;
+    }
+    messageID = logEntryFields[0];
+
+    // Get the MessageArgs from the log if there are any
+    if (logEntryFields.size() > 1)
+    {
+        std::string& messageArgsStart = logEntryFields[1];
+        // If the first string is empty, assume there are no MessageArgs
+        std::size_t messageArgsSize = 0;
+        if (!messageArgsStart.empty())
+        {
+            messageArgsSize = logEntryFields.size() - 1;
+        }
+
+        messageArgs = boost::beast::span(&messageArgsStart, messageArgsSize);
+    }
+
+    return 0;
+}
+
+void getRegistryAndMessageKey(const std::string& messageID,
+                              std::string& registryName,
+                              std::string& messageKey)
+{
+    // Redfish MessageIds are in the form
+    // RegistryName.MajorVersion.MinorVersion.MessageKey, so parse it to find
+    // the right Message
+    std::vector<std::string> fields;
+    fields.reserve(4);
+    boost::split(fields, messageID, boost::is_any_of("."));
+    registryName = fields[0];
+    messageKey = fields[3];
+}
+
+int formatEventLogEntry(const std::string& logEntryID,
+                        const std::string& messageID,
+                        const boost::beast::span<std::string>& messageArgs,
+                        std::string& timestamp, const std::string customText,
+                        nlohmann::json& logEntryJson)
+{
+    // Get the Message from the MessageRegistry
+    const message_registries::Message* message =
+        message_registries::formatMessage(messageID);
+
+    std::string msg;
+    std::string severity;
+    if (message != nullptr)
+    {
+        msg = message->message;
+        severity = message->severity;
+    }
+
+    // Fill the MessageArgs into the Message
+    int i = 0;
+    for (const std::string& messageArg : messageArgs)
+    {
+        std::string argStr = "%" + std::to_string(++i);
+        size_t argPos = msg.find(argStr);
+        if (argPos != std::string::npos)
+        {
+            msg.replace(argPos, argStr.length(), messageArg);
+        }
+    }
+
+    // Get the Created time from the timestamp. The log timestamp is in
+    // RFC3339 format which matches the Redfish format except for the
+    // fractional seconds between the '.' and the '+', so just remove them.
+    std::size_t dot = timestamp.find_first_of(".");
+    std::size_t plus = timestamp.find_first_of("+");
+    if (dot != std::string::npos && plus != std::string::npos)
+    {
+        timestamp.erase(dot, plus - dot);
+    }
+
+    // Fill in the log entry with the gathered data
+    logEntryJson = {{"EventId", logEntryID},
+                    {"EventType", "Event"},
+                    {"Severity", std::move(severity)},
+                    {"Message", std::move(msg)},
+                    {"MessageId", std::move(messageID)},
+                    {"MessageArgs", std::move(messageArgs)},
+                    {"EventTimestamp", std::move(timestamp)},
+                    {"Context", customText}};
+    return 0;
+}
+#endif
+
+} // namespace event_log
+
 class Subscription
 {
   public:
@@ -53,6 +279,7 @@ class Subscription
     {
         conn = std::make_shared<crow::HttpClient>(
             crow::connections::systemBus->get_io_context(), host, port);
+        eventSeqNum = 1;
     }
     ~Subscription()
     {
@@ -74,7 +301,94 @@ class Subscription
         conn->doConnectAndSend(path, msg);
     }
 
+#ifndef BMCWEB_ENABLE_REDFISH_DBUS_LOG_ENTRIES
+    void filterAndSendEventLogs(const std::vector<std::string>& eventRecords)
+    {
+        nlohmann::json logEntryArray;
+
+        bool firstEntry = true;
+        for (const std::string& logEntry : eventRecords)
+        {
+            std::string idStr;
+            if (!event_log::getUniqueEntryID(logEntry, idStr, firstEntry))
+            {
+                continue;
+            }
+            if (firstEntry)
+            {
+                firstEntry = false;
+            }
+
+            std::string timestamp;
+            std::string messageID;
+            boost::beast::span<std::string> messageArgs;
+            if (event_log::getEventLogParams(logEntry, timestamp, messageID,
+                                             messageArgs) != 0)
+            {
+                BMCWEB_LOG_DEBUG << "Read eventLog entry params failed";
+                continue;
+            }
+
+            std::string registryName;
+            std::string messageKey;
+            event_log::getRegistryAndMessageKey(messageID, registryName,
+                                                messageKey);
+
+            // If registryPrefixes list is empty, don't filter events
+            // send everything.
+            if (registryPrefixes.size())
+            {
+                auto obj = std::find(registryPrefixes.begin(),
+                                     registryPrefixes.end(), registryName);
+                if (obj == registryPrefixes.end())
+                {
+                    continue;
+                }
+            }
+
+            // If registryMsgIds list is empty, don't filter events
+            // send everything.
+            if (registryMsgIds.size())
+            {
+                auto obj = std::find(registryMsgIds.begin(),
+                                     registryMsgIds.end(), messageKey);
+                if (obj == registryMsgIds.end())
+                {
+                    continue;
+                }
+            }
+
+            logEntryArray.push_back({});
+            nlohmann::json& bmcLogEntry = logEntryArray.back();
+            if (event_log::formatEventLogEntry(idStr, messageID, messageArgs,
+                                               timestamp, customText,
+                                               bmcLogEntry) != 0)
+            {
+                BMCWEB_LOG_DEBUG << "Read eventLog entry failed";
+                continue;
+            }
+        }
+
+        if (logEntryArray.size() < 1)
+        {
+            BMCWEB_LOG_DEBUG << "No log entries for sending";
+            return;
+        }
+
+        nlohmann::json msg = {{"@odata.type", "#Event.v1_4_0.Event"},
+                              {"Id", std::to_string(eventSeqNum)},
+                              {"Name", "Event Log"},
+                              {"Events", logEntryArray}};
+
+        this->sendEvent(msg.dump());
+        this->eventSeqNum++;
+
+        return;
+    }
+#endif
+
   private:
+    uint64_t eventSeqNum;
     std::string host;
     std::string port;
     std::string path;
@@ -99,6 +413,7 @@ class EventServiceManager
         retryTimeoutInterval = 30; // seconds
     }
 
+    std::string lastEventTStr;
     boost::container::flat_map<std::string, std::shared_ptr<Subscription>>
         subscriptionsMap;
 
@@ -157,6 +472,13 @@ class EventServiceManager
         }
 
         updateSubscriptionData();
+
+#ifndef BMCWEB_ENABLE_REDFISH_DBUS_LOG_ENTRIES
+        if (lastEventTStr.empty())
+        {
+            setLastEventTimestamp();
+        }
+#endif
         return id;
     }
 
@@ -208,6 +530,134 @@ class EventServiceManager
         }
         return false;
     }
+
+#ifndef BMCWEB_ENABLE_REDFISH_DBUS_LOG_ENTRIES
+    void setLastEventTimestamp()
+    {
+        std::ifstream logStream(redfishEventLogFile);
+        if (!logStream.good())
+        {
+            BMCWEB_LOG_ERROR << " Redfish log file open failed \n";
+            return;
+        }
+        std::string logEntry;
+        while (std::getline(logStream, logEntry))
+        {
+            size_t space = logEntry.find_first_of(" ");
+            if (space == std::string::npos)
+            {
+                // Shouldn't enter here but lets skip it.
+                BMCWEB_LOG_DEBUG << "Error format log present";
+                continue;
+            }
+            lastEventTStr = logEntry.substr(0, space);
+        }
+        BMCWEB_LOG_DEBUG << "Last Event time stamp set: " << lastEventTStr;
+    }
+
+    void readEventLogsFromFile()
+    {
+        if (!getNumberOfSubscriptions())
+        {
+            // no subscriptions. Just return.
+            BMCWEB_LOG_DEBUG << "No Subscriptions\n";
+            return;
+        }
+        std::ifstream logStream(redfishEventLogFile);
+        if (!logStream.good())
+        {
+            BMCWEB_LOG_ERROR << " Redfish log file open failed";
+            return;
+        }
+
+        std::vector<std::string> eventRecords;
+
+        bool startLogCollection = false;
+        std::string logEntry;
+        while (std::getline(logStream, logEntry))
+        {
+            if (!startLogCollection)
+            {
+                if (boost::starts_with(logEntry, lastEventTStr))
+                {
+                    startLogCollection = true;
+                }
+                continue;
+            }
+            size_t space = logEntry.find_first_of(" ");
+            if (space == std::string::npos)
+            {
+                // Shouldn't enter here but lets skip it.
+                BMCWEB_LOG_DEBUG << "Error format log present";
+                continue;
+            }
+            lastEventTStr = logEntry.substr(0, space);
+            eventRecords.push_back(logEntry);
+        }
+
+        for (const auto& it : this->subscriptionsMap)
+        {
+            std::shared_ptr<Subscription> entry = it.second;
+            entry->filterAndSendEventLogs(eventRecords);
+        }
+    }
+
+    static void watchRedfishEventLogFile()
+    {
+        std::array<char, 1024> readBuffer;
+        inotifyConn->async_read_some(
+            boost::asio::buffer(readBuffer),
+            [&](const boost::system::error_code& ec,
+                const std::size_t& bytesTransferred) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "Callback Error: " << ec.message();
+                    return;
+                }
+                std::size_t index = 0;
+                while ((index + sizeof(inotify_event)) <= bytesTransferred)
+                {
+                    struct inotify_event event;
+                    std::memcpy(&event, &readBuffer[index],
+                                sizeof(inotify_event));
+                    if (event.mask == inotifyFileAction)
+                    {
+                        EventServiceManager::getInstance()
+                            .readEventLogsFromFile();
+                    }
+                    index += (sizeof(inotify_event) + event.len);
+                }
+
+                watchRedfishEventLogFile();
+            });
+    }
+
+    static int startEventLogMonitor(boost::asio::io_service& ioc)
+    {
+        inotifyConn =
+            std::make_shared<boost::asio::posix::stream_descriptor>(ioc);
+        int fd = inotify_init1(IN_NONBLOCK);
+        if (fd == -1)
+        {
+            BMCWEB_LOG_ERROR << "inotify_init1 failed.";
+            return -1;
+        }
+        auto wd = inotify_add_watch(fd, redfishEventLogFile, inotifyFileAction);
+        if (wd == -1)
+        {
+            BMCWEB_LOG_ERROR
+                << "inotify_add_watch failed for redfish log file.";
+            return -1;
+        }
+
+        // monitor redfish event log file
+        inotifyConn->assign(fd);
+        watchRedfishEventLogFile();
+
+        return 0;
+    }
+
+#endif
 };
 
 } // namespace redfish
