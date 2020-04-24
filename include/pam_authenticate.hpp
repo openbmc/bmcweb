b9d3af06482375e8227528b8837cf04605e874bc
Joseph Reynolds <joseph-reynolds@charter.net>
2020-04-24 17:32:53 -0500

    bmcweb: rate limit password auth failures
    This is an experimental patch to enhance BMCWeb to rate-limit password
    authentication failures.  When there are excessive password auth
    failures within a time period, new authentication attempts are rejected
    with HTTP status 429 "Too Many Requests" and response header "Retry-After"
    until the failures time out.
    
    Rate-limiting is engaged only after exceessing failures are seen.
    Otherwise authentication proceeds at full speed.
    Other types of authorization are not affected, including: mTLS.
    Existing sessions are not affected, including access via Cookie
    or X-Auth-Token.
    
    This affects password-based authentication:
     - login via POST /redfish/v1/AccountService/Accounts/{USER}
     - login via POST /login
     - Basic auth
    
    This is work in progress.  The rate-limiting parameters are constexpr
    when they should be configurable by the admin.
    Ideally the rate-limiting service should be handled by Linux-PAM
    or provided by a BMC service so it can be be applied to authentication
    use cases from outside of the web server.  For example: IPMI or
    access from the console.
    See also: https://github.com/linux-pam/linux-pam/issues/216
    See also: https://redfishforum.com/thread/311/rate-limit-authentication-attempts
    
    Tested:
    Setup: Ensure account lockouts are disabled:
      PATCH /redfish/v1/AccountService/ {"AccountLockoutThreshold": 0}
    
    1. Test that authentication still gives correct results for correct
       and incorrect passwords.
    2. Test rate-limiting, do this in sequence (time critical):
    - Cause excessing auth failures
    - While rate-limiting is not allowing auth attempts:
       + Validate correct auth attempts get HTTP status 429
       + Validate existing established sessions operate normally
    - After rate-limiting times out:
       + Validate correct auth attempts succeed
    
    Validate HTTP response header "Retry-After" is present with the correct
    value whenever 429 is used.
    Validate there is a log entry when rate-limiting was used.  (Not done.)
    
    Signed-off-by: Joseph Reynolds <joseph-reynolds@charter.net>
    Change-Id: I60c8a78251e5355305222ac0f0068b34ec016758

--- include/pam_authenticate.hpp
+++ include/pam_authenticate.hpp
@@ -3,8 +3,10 @@
 #include <security/pam_appl.h>
 
 #include <boost/utility/string_view.hpp>
+#include <chrono>
 #include <cstring>
 #include <memory>
+#include <queue>
 
 // function used to get user input
 inline int pamFunctionConversation(int numMsg, const struct pam_message** msg,
@@ -48,6 +50,62 @@ inline int pamFunctionConversation(int numMsg, const struct pam_message** msg,
 }
 
 /**
+ * @brief Helps you rate-limit events, for example, 1 login per second.
+ * Uses a sliding time window with 1 second granularity.
+ * Usage:
+ *   Create a RateLimiter object.
+ *   Invoke addEvent() every time the event happens.
+ *   Use isEventAllowed() to learn if you should deny due to rate-limiting.
+ **/
+template <unsigned int MaxEvents, unsigned int PeriodInSeconds>
+class RateLimiter
+{
+    std::queue<std::chrono::steady_clock::time_point> eventQueue;
+    void expireOldEvents()
+    {
+        std::chrono::steady_clock::time_point expirationTime =
+            std::chrono::steady_clock::now() -
+            std::chrono::seconds(PeriodInSeconds);
+        while ((!eventQueue.empty()) && (eventQueue.front() < expirationTime))
+        {
+            eventQueue.pop();
+        }
+    }
+
+  public:
+    bool isEventAllowed()
+    {
+        expireOldEvents();
+        return (eventQueue.size() < MaxEvents);
+    }
+    void addEvent()
+    {
+        expireOldEvents();
+        eventQueue.push(std::chrono::steady_clock::now());
+    }
+};
+
+// See https://github.com/linux-pam/linux-pam/issues/216
+#define PSEUDO_PAM_RATE_LIMITING (_PAM_RETURN_VALUES + 1)
+/* Pam authentication unavailable due to rate limiting */
+
+namespace pam_auth
+{
+namespace rate_limiter
+{
+/**
+ * @brief Define parameters of the authentication rate limiter. Apply
+ * rate-limiting when there are EventThreshold events in the past
+ * PeriodInSeconds.
+ * @const EventThreshold The number of events to trigger rate-limiting.
+ * @const PeriodInSeconds The maximum duration to rate-limit.
+ */
+constexpr unsigned int EventThreshold = 5;
+constexpr unsigned int PeriodInSeconds = 30;
+} // namespace rate_limiter
+} // namespace pam_auth
+
+/**
  * @brief Attempt username/password authentication via PAM.
  * @param username The provided username aka account name.
  * @param password The provided password.
@@ -55,6 +113,19 @@ inline int pamFunctionConversation(int numMsg, const struct pam_message** msg,
 inline int pamAuthenticateUser(const std::string_view username,
                                const std::string_view password)
 {
+    // Rate limit authentication failures without regard to account name or
+    // request origin.  Note the tricky usage: This only counts authentication
+    // *failures* but applies the rate-limit to all attempts.  This allows
+    // authentication to run at full speed until there are too many failures.
+    static RateLimiter<pam_auth::rate_limiter::EventThreshold,
+                       pam_auth::rate_limiter::PeriodInSeconds>
+        failedAuthRateLimiter;
+
+    if (!failedAuthRateLimiter.isEventAllowed())
+    {
+        return PSEUDO_PAM_RATE_LIMITING; // PAM_MAXTRIES?
+    }
+
     std::string userStr(username);
     std::string passStr(password);
     const struct pam_conv localConversation = {
@@ -73,10 +144,11 @@ inline int pamAuthenticateUser(const std::string_view username,
     if (retval != PAM_SUCCESS)
     {
         pam_end(localAuthHandle, PAM_SUCCESS); // ignore retval
+        failedAuthRateLimiter.addEvent();
         return retval;
     }
 
-    /* check that the account is healthy */
+    /* Authentication is successful. Check if the account is healthy */
     retval = pam_acct_mgmt(localAuthHandle, PAM_DISALLOW_NULL_AUTHTOK);
     if (retval != PAM_SUCCESS)
     {
