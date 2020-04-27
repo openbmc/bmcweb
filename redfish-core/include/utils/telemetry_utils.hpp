a2284b2cfe9fe6ed3ff0fb7d0c251788332fcebf
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-04-27 17:24:15 +0200

    Redfish TelemetryService schema implementation
    Added TelemetryService, MetricReports, MetricReportCollection,
    MetricReportDefinition and MetricReportDefinitionCollection schemas
    with GET method support. Added TelemetryService URI to root service.
    Implemented communication with backend - MonitoringService.
    Added schemes attributes that are supported by MonitoringService
    design. User is able to fetch basic information about reports if
    MonitoringService is present in OpenBMC.
    Added util function that converts decimal value into duration format
    that is described by ISO 8601 and Redfish specification.
    
    Tested:
     - Succesfully passed RedfishServiceValidator.py
     - Verified DBus method calls to MonitoringService
     - Verified all possible pages that are displayed to user when:
       - Reports are fully defined in MonitoringService
       - Reports are partially available in MonitoringService
       - MonitoringService is disabled
     - Verified time_utils::toDurationString() output
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Signed-off-by: Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: Ie6b0b49f4ef5eeaef07d1209b6c349270c04d570

---
+++ redfish-core/include/utils/telemetry_utils.hpp
@@ -0,0 +1,94 @@
+/*
+// Copyright (c) 2018-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+
+namespace redfish
+{
+
+namespace telemetry
+{
+
+constexpr const char* service = "xyz.openbmc_project.MonitoringService";
+constexpr const char* reportInterface =
+    "xyz.openbmc_project.MonitoringService.Report";
+constexpr const char* metricReportDefinitionUri =
+    "/redfish/v1/TelemetryService/MetricReportDefinitions/";
+constexpr const char* metricReportUri =
+    "/redfish/v1/TelemetryService/MetricReports/";
+
+inline void getReportCollection(const std::shared_ptr<AsyncResp>& asyncResp,
+                                const std::string& uri)
+{
+    const std::array<const char*, 1> interfaces = {reportInterface};
+
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, uri](const boost::system::error_code ec,
+                         const std::vector<std::string>& reportPaths) {
+            if (ec == boost::system::errc::no_such_file_or_directory)
+            {
+                asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+                asyncResp->res.jsonValue["Members@odata.count"] = 0;
+                return;
+            }
+            if (ec)
+            {
+                messages::internalError(asyncResp->res);
+                BMCWEB_LOG_ERROR << "respHandler DBus error " << ec;
+                return;
+            }
+
+            nlohmann::json& members = asyncResp->res.jsonValue["Members"];
+            members = nlohmann::json::array();
+
+            for (const std::string& path : reportPaths)
+            {
+                std::size_t pos = path.rfind("/");
+                if (pos == std::string::npos)
+                {
+                    BMCWEB_LOG_ERROR << "Failed to find '/' in " << path;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                if (path.size() <= (pos + 1))
+                {
+                    BMCWEB_LOG_ERROR << "Failed to parse path " << path;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+
+                members.push_back({{"@odata.id", uri + path.substr(pos + 1)}});
+            }
+
+            asyncResp->res.jsonValue["Members@odata.count"] = members.size();
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths",
+        "/xyz/openbmc_project/MonitoringService/Reports/TelemetryService", 1,
+        interfaces);
+}
+
+inline std::string getDbusReportPath(const std::string& id)
+{
+    std::string path =
+        "/xyz/openbmc_project/MonitoringService/Reports/TelemetryService/" + id;
+    dbus::utility::escapePathForDbus(path);
+    return path;
+}
+
+} // namespace telemetry
+} // namespace redfish
