154f2f4a7eecf3f2bb132b554f95e16459016846
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-04-27 17:24:15 +0200

    Redfish TelemetryService schema implementation
    Added TelemetryService, MetricReports, MetricReportCollection,
    MetricReportDefinition and MetricReportDefinitionCollection nodes
    with GET method support. Added TelemetryService URI to root service.
    Implemented communication with backend - Telemetry:
    https://github.com/openbmc/telemetry
    
    Added schemes attributes that are supported by Telemetry service
    design, ref.:
    https://github.com/openbmc/docs/blob/master/designs/telemetry.md
    
    Now user is able to communicate with Telemetry service using Redfish.
    To add Telemetry service user has to add 'IMAGE_INSTALL += " telemetry"'
    to image recipe. It is an optional service. If Telemetry is not present
    than Redfish TelemetryService set its state to "Absent". Other telemetry
    nodes response with internal error.
    
    Change introduces function that converts decimal value into
    duration format that is described by ISO 8601 and Redfish
    specification.
    
    Tested:
     - Added Telemetry service to Romulus image, ran image on QEMU
     - Verified DBus method calls to Telemetry service from bmcweb
     - Verified bmcweb responses from new nodes in different cases:
       - Report collection is empty
       - Report collection is filled with artificial data
       - Telemetry service is disabled
     - Verified time_utils::toDurationString() output
     - Passed RedfishServiceValidator.py
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Signed-off-by: Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: Ie6b0b49f4ef5eeaef07d1209b6c349270c04d570

---
+++ redfish-core/include/utils/telemetry_utils.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace redfish
+{
+
+namespace telemetry
+{
+
+constexpr const char* service = "xyz.openbmc_project.Telemetry";
+constexpr const char* reportInterface = "xyz.openbmc_project.Telemetry.Report";
+constexpr const char* metricReportDefinitionUri =
+    "/redfish/v1/TelemetryService/MetricReportDefinitions/";
+constexpr const char* metricReportUri =
+    "/redfish/v1/TelemetryService/MetricReports/";
+
+inline void getReportCollection(const std::shared_ptr<AsyncResp>& asyncResp,
+                                const std::string& uri)
+{
+    const std::array<const char*, 1> interfaces = {reportInterface};
+
+    crow::connections::systemBus->async_method_call(
+        [asyncResp,
+         uri](const boost::system::error_code ec,
+              const std::vector<sdbusplus::message::object_path>& reports) {
+            if (ec)
+            {
+                asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+                asyncResp->res.jsonValue["Members@odata.count"] = 0;
+                return;
+            }
+
+            nlohmann::json& members = asyncResp->res.jsonValue["Members"];
+            members = nlohmann::json::array();
+
+            for (const sdbusplus::message::object_path& report : reports)
+            {
+                std::string path = report.filename();
+                if (path.empty())
+                {
+                    BMCWEB_LOG_ERROR << "Received invalid path: " << report.str;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                members.push_back({{"@odata.id", uri + path}});
+            }
+
+            asyncResp->res.jsonValue["Members@odata.count"] = members.size();
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths",
+        "/xyz/openbmc_project/Telemetry/Reports/TelemetryService", 1,
+        interfaces);
+}
+
+inline std::string getDbusReportPath(const std::string& id)
+{
+    std::string path =
+        "/xyz/openbmc_project/Telemetry/Reports/TelemetryService/" + id;
+    dbus::utility::escapePathForDbus(path);
+    return path;
+}
+
+} // namespace telemetry
+} // namespace redfish
