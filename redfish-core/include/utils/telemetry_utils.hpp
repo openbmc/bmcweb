d8f98030d997317feb4e09e5b7b71259524b5b30
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-04-27 17:24:15 +0200

    Redfish TelemetryService schema implementation
    Added TelemetryService, MetricReports, MetricReportCollection,
    MetricReportDefinition and MetricReportDefinitionCollection schemas
    with GET method support. Added TelemetryService URI to root service.
    Implemented communication with backend - MonitoringService.
    Added schemes attributes that are supported by MonitoringService
    design. User is able to fetch basic information about reports if
    MonitoringService is present in OpenBMC.
    
    Tested:
      - Succesfully passed RedfishServiceValidator.py
      - Validated conversion to duration format using whole
        range of uint32_t type
      - Validated assigning value to JSON response using different
        closures and std::functions types
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Change-Id: Ie6b0b49f4ef5eeaef07d1209b6c349270c04d570

---
+++ redfish-core/include/utils/telemetry_utils.hpp
@@ -0,0 +1,100 @@
+/*
+// Copyright (c) 2018-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+
+namespace redfish
+{
+
+namespace telemetry
+{
+
+static constexpr const char* metricReportDefinitionUri =
+    "/redfish/v1/TelemetryService/MetricReportDefinitions/";
+static constexpr const char* metricReportUri =
+    "/redfish/v1/TelemetryService/MetricReports/";
+
+static void getReportCollection(const std::shared_ptr<AsyncResp>& asyncResp,
+                                const char* uri)
+{
+    const std::array<const char*, 1> interfaces = {
+        "xyz.openbmc_project.MonitoringService.Report"};
+
+    dbus::utility::getSubTreePaths(
+        [asyncResp, uri](const boost::system::error_code ec,
+                         const std::vector<std::string>& reports) {
+            if (ec == boost::system::errc::no_such_file_or_directory)
+            {
+                asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+                asyncResp->res.jsonValue["Members@odata.count"] = 0;
+                return;
+            }
+
+            if (ec)
+            {
+                messages::internalError(asyncResp->res);
+                BMCWEB_LOG_ERROR << "respHandler DBus error " << ec;
+                return;
+            }
+
+            json_util::dbusPathsToMembersArray(asyncResp->res, reports, uri);
+        },
+        "/xyz/openbmc_project/MonitoringService/Reports/TelemetryService", 1,
+        interfaces);
+}
+
+template <typename Callback>
+static void getReport(const std::shared_ptr<AsyncResp>& asyncResp,
+                      const std::string& id, const char* schemaType,
+                      const Callback&& callback)
+{
+    const std::array<const char*, 1> interfaces = {
+        "xyz.openbmc_project.MonitoringService.Report"};
+
+    dbus::utility::getSubTreePaths(
+        [asyncResp, id, schemaType,
+         callback](const boost::system::error_code ec,
+                   const std::vector<std::string>& reports) {
+            if (ec == boost::system::errc::no_such_file_or_directory)
+            {
+                messages::resourceNotFound(asyncResp->res, schemaType, id);
+                return;
+            }
+
+            if (ec)
+            {
+                messages::internalError(asyncResp->res);
+                BMCWEB_LOG_ERROR << "respHandler DBus error " << ec;
+                return;
+            }
+
+            const std::string target = "/xyz/openbmc_project/"
+                                       "MonitoringService/Reports/"
+                                       "TelemetryService/" +
+                                       id;
+            auto path = std::find(reports.begin(), reports.end(), target);
+            if (path == std::end(reports))
+            {
+                messages::resourceNotFound(asyncResp->res, schemaType, id);
+                return;
+            }
+            callback(asyncResp, *path, id);
+        },
+        "/xyz/openbmc_project/MonitoringService/Reports/TelemetryService", 1,
+        interfaces);
+}
+} // namespace telemetry
+} // namespace redfish
