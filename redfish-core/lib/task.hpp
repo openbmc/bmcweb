2fed47ce03dbcaeba3cf3eea3d570553fabfb84e
Zhenfei Tai <ztai@google.com>
2020-05-05 13:39:07 -0700

    bmcweb: Replace deprecated boost call
    The boost::asio::io_context::post() is deprecated and will not build
    when boost library is version 1.71.0.
    
    See CMakeLists.txt
    271 if ("${Boost_VERSION}" STREQUAL "107100")
    272     add_definitions (-DBOOST_ASIO_NO_DEPRECATED)
    273 endif ()
    
    Signed-off-by: Zhenfei Tai <ztai@google.com>
    Change-Id: Iacd657af320ba6eebb1518cd86a955f355ed5068

--- redfish-core/lib/task.hpp
+++ redfish-core/lib/task.hpp
@@ -17,6 +17,7 @@
 
 #include "node.hpp"
 
+#include <boost/asio.hpp>
 #include <boost/container/flat_map.hpp>
 #include <chrono>
 #include <variant>
@@ -182,8 +183,10 @@ struct TaskData : std::enable_shared_from_this<TaskData>
                     self->finishTask();
 
                     // reset the match after the callback was successful
-                    crow::connections::systemBus->get_io_context().post(
-                        [self] { self->match.reset(); });
+                    boost::asio::post(
+                        crow::connections::systemBus->get_io_context(),
+                        [self] { self->match.reset(); }
+                    );
                     return;
                 }
             });
