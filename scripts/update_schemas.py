bd8c5ab9b47c78afb04ba00b6b0c82ce11b76b18
Sunitha Harish <sunithaharish04@gmail.com>
2020-05-11 00:10:20 -0500

    New IBM OEM schema for Session
    This commit defines the OemSession schema for storing the clients
    unique id and IP address.
    
    Signed-off-by: Sunitha Harish <sunithaharish04@gmail.com>
    Change-Id: I759cda25dbb2dadd9b3d14cca9076e6412b41287

--- scripts/update_schemas.py
+++ scripts/update_schemas.py
@@ -133,6 +133,12 @@ with open(metadata_index_path, 'w') as metadata_index:
     metadata_index.write("        <edmx:Include Namespace=\"OemAccountService.v1_0_0\"/>\n")
     metadata_index.write("    </edmx:Reference>\n")
 
+    metadata_index.write(
+        "    <edmx:Reference Uri=\"/redfish/v1/schema/OemSession_v1.xml\">\n")
+    metadata_index.write("        <edmx:Include Namespace=\"OemSession\"/>\n")
+    metadata_index.write("        <edmx:Include Namespace=\"OemSession.v1_0_0\"/>\n")
+    metadata_index.write("    </edmx:Reference>\n")
+
     metadata_index.write("</edmx:Edmx>\n")
 
 schema_files = {}
