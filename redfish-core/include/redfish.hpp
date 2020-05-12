f54dac126bd0ed1b01e734a33c7e491ce10acdfd
George Liu <liuxiwei@inspur.com>
2020-05-12 16:06:27 +0800

    UpdateService: Support for MultipartHttpPushUri
    Add a new MultipartHttpPushUri property and use the requirements of a
    multipart/form-data to request body for HTTP push software update.
    
    Tested:
     curl -k -H "X-Auth-Token: $token" -H "Content-Type:
    multipart/form-data" -F
    "UpdateParameters={\"Targets\":[\"/redfish/v1/Managers/1\"],\"@Redfish.OperationApplyTime\":\"Immediate\"};type=appalication/json"
    -F
    "UpdateFile=@obmc-phosphor-image.static.mtd.tar;type=application/octet-stream"
    https://${bmc}/redfish/v1/UpdateService/upload
    {
      "@odata.id": "/redfish/v1/TaskService/Tasks/0",
      "@odata.type": "#Task.v1_4_3.Task",
      "Id": "0",
      "TaskState": "Running",
      "TaskStatus": "OK"
    }
    
    Passed the validator:
    {
        "@odata.id": "/redfish/v1/UpdateService",
        "@odata.type": "#UpdateService.v1_9_0.UpdateService",
        "Actions": {
            "#UpdateService.SimpleUpdate": {
                "TransferProtocol@Redfish.AllowableValues": [
                    "TFTP"
                ],
                "target": "/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate"
            }
        },
        "Description": "Service for Software Update",
        "FirmwareInventory": {
            "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory"
        },
        "HttpPushUri": "/redfish/v1/UpdateService",
        "HttpPushUriOptions": {
            "HttpPushUriApplyTime": {
                "ApplyTime": "OnReset"
            }
        },
        "Id": "UpdateService",
        "MultipartHttpPushUri": "/redfish/v1/UpdateService/upload",
        "Name": "Update Service",
        "ServiceEnabled": true
    }
    ... ...
    MultipartHttpPushUri /redfish/v1/UpdateService/upload string Yes PASS
    ... ...
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ie1ea0594337efdb073270aba6918389c4381a2b3

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -82,6 +82,7 @@ class RedfishService
         requestRoutesChassisResetAction(app);
         requestRoutesChassisResetActionInfo(app);
         requestRoutesUpdateService(app);
+        requestRoutesUpdateServiceMultipart(app);
         requestRoutesStorageCollection(app);
         requestRoutesStorage(app);
         requestRoutesDrive(app);
