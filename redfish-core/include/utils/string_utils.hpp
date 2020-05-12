f273490a84158d751774c3a7d3490c7a2428d8b9
George Liu <liuxiwei@inspur.com>
2020-05-12 16:06:27 +0800

    UpdateService: Support for MultipartHttpPushUri
    Add a new MultipartHttpPushUri property and use the requirements of a
    multipart/form-data to request body for HTTP push software update.
    
    Tested:
     curl -k -H "X-Auth-Token: $token" -H "Content-Type:
    multipart/form-data" -F
    "UpdateParameters={\"Targets\":[\"/redfish/v1/Managers/1\"],\"@Redfish.OperationApplyTime\":\"Immediate\",\"Oem\":{}};type=appalication/json"
    -F
    "UpdateFile=@obmc-phosphor-image.static.mtd.tar;type=application/octet-stream"
    https://${bmc}/redfish/v1/UpdateService/upload
    {
      "@odata.id": "/redfish/v1/TaskService/Tasks/0",
      "@odata.type": "#Task.v1_4_3.Task",
      "Id": "0",
      "TaskState": "Running",
      "TaskStatus": "OK"
    }
    
    Passed the validator:
    {
        "@odata.id": "/redfish/v1/UpdateService",
        "@odata.type": "#UpdateService.v1_8_0.UpdateService",
        "Actions": {
            "#UpdateService.SimpleUpdate": {
                "TransferProtocol@Redfish.AllowableValues": [
                    "TFTP"
                ],
                "target": "/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate"
            }
        },
        "Description": "Service for Software Update",
        "FirmwareInventory": {
            "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory"
        },
        "HttpPushUri": "/redfish/v1/UpdateService",
        "HttpPushUriOptions": {
            "HttpPushUriApplyTime": {
                "ApplyTime": "OnReset"
            }
        },
        "Id": "UpdateService",
        "MultipartHttpPushUri": "/redfish/v1/UpdateService/upload",
        "Name": "Update Service",
        "ServiceEnabled": true
    }
    ... ...
    MultipartHttpPushUri /redfish/v1/UpdateService/upload string Yes PASS
    ... ...
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ie1ea0594337efdb073270aba6918389c4381a2b3

---
+++ redfish-core/include/utils/string_utils.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iterator>
+#include <regex>
+#include <string>
+#include <vector>
+
+namespace redfish
+{
+namespace string_util
+{
+
+inline std::vector<std::string> split(const std::string& input,
+                                      const std::string& delim)
+{
+    std::vector<std::string> ret;
+    try
+    {
+        std::regex reg{delim};
+        ret = std::vector<std::string>{
+            std::sregex_token_iterator(input.begin(), input.end(), reg, -1),
+            std::sregex_token_iterator()};
+
+        for (auto& v : ret)
+        {
+            v.erase(0, v.find_first_not_of(' '));
+            v.erase(v.find_last_not_of(' ') + 1);
+        }
+    }
+    catch (const std::exception& e)
+    {}
+
+    return ret;
+}
+
+} // namespace string_util
+} // namespace redfish
