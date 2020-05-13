73bc9ab86ebf248153125f9c203915396d8225e7
Ravi Teja <raviteja28031990@gmail.com>
2020-05-13 06:35:49 -0500

    Redfish(Network): Fix PATCH of existing IPv4StaticAddresses properties
    Currently Unable to modify(PATCH) existing IP address properties.
    
    Failure case:
    add an ipv4 static address and try to do patch operation to modify
    properties of this static address entry.
    say existing entry
    "IPv4StaticAddresses": [
     {
          "Address": "223.7.7.7",
          "AddressOrigin": "Static",
          "Gateway": "223.7.7.1",
          "SubnetMask": "255.255.0.0"
     }]
    
    do patch operation, it returns success but does not update properties.
    1.PATCH -D '{"IPv4StaticAddresses": [{"Address": "10.7.7.20","SubnetMask": "255.255.0.0","Gateway":"223.7.7.1"}]}'
    2.PATCH -D '{"IPv4StaticAddresses": [{},{"Address": "10.8.8.8"}]}
    both cases expected to work.
    .
    Test By:
     Pacthing existing entry properties and creating new entries.
     PATCH -d '{"IPv4StaticAddresses": [{},{"Address": "10.7.7.20","SubnetMask": "255.255.0.0","Gateway":"10.7.7.1"}]}'
     GET https://${IP}/redfish/v1/Managers/bmc/EthernetInterfaces/eth0/
    
    
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: Ic23330592e9041ddd1e9f96d08ea0bb88c6d8ca7

--- redfish-core/lib/ethernet.hpp
+++ redfish-core/lib/ethernet.hpp
@@ -1550,17 +1550,14 @@ class EthernetInterface : public Node
 
                 if (NICIPentry != ipv4Data.cend())
                 {
-                    if (gw != nullptr || addr != nullptr)
+                    if (gw != nullptr && addr != nullptr)
                     {
-                        // Shouldn't be possible based on errorInEntry, but
-                        // it flags -wmaybe-uninitialized in the compiler,
-                        // so defend against that
-                        return;
+                        deleteAndCreateIPv4(ifaceId, NICIPentry->id,
+                                            prefixLength, *gw, *addr,
+                                            asyncResp);
+                        NICIPentry =
+                            GetNextStaticIPEntry(++NICIPentry, ipv4Data.cend());
                     }
-                    deleteAndCreateIPv4(ifaceId, NICIPentry->id, prefixLength,
-                                        *gw, *addr, asyncResp);
-                    NICIPentry =
-                        GetNextStaticIPEntry(++NICIPentry, ipv4Data.cend());
                 }
                 else
                 {
