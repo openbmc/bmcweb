9598fb5b9e8e4e6472ec69e77a6989b50e4c2061
AppaRao Puli <apparao.puli@linux.intel.com>
2020-05-15 21:09:54 +0530

    EventService: Support for Server Sent Events(SSE)
    Add support for Server Sent Events(SSE)
    Filters support is not part of this commit.
    
    Tested:
     - GET on URI /redfish/v1/EventService/Subscriptions/SSE/
       from chrome browser, can see all BMC Events on browser.
     - Redfish validator is successful.
    
    Change-Id: Icd10cdad20c4529f64c97b67d46f2e4a7e0c329c
    Signed-off-by: AppaRao Puli <apparao.puli@linux.intel.com>

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -184,6 +184,7 @@ class RedfishService
         nodes.emplace_back(std::make_unique<TaskCollection>(app));
         nodes.emplace_back(std::make_unique<Task>(app));
         nodes.emplace_back(std::make_unique<EventService>(app));
+        nodes.emplace_back(std::make_unique<EventServiceSSE>(app));
         nodes.emplace_back(std::make_unique<EventDestinationCollection>(app));
         nodes.emplace_back(std::make_unique<EventDestination>(app));
         nodes.emplace_back(std::make_unique<SubmitTestEvent>(app));
