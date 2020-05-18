76ff73a85ae431df94823fe3b0c233f864185559
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-05-18 11:56:57 +0200

    Add support for POST in MetricReportDefinitions
    Added POST action in MetricReportDefinitions node to allow user
    to add new MetricReportDefinition. Using minimal set of
    MetricReportDefinition parameters from user bmcweb converts it to
    DBus call "AddReport" to MonitoringService that serves as a backend
    for TelemetryService.
    
    Tested:
     - Succesfully passed RedfishServiceValidator.py
     - Validated good cases with different parameters for POST action
     - Validated bad cases with different parameters for POST action
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: I2fed96848594451e22fde686f8c066d7770cc65a

--- redfish-core/include/utils/time_utils.hpp
+++ redfish-core/include/utils/time_utils.hpp
@@ -16,10 +16,8 @@
 
 #pragma once
 
-#include <boost/algorithm/string/trim.hpp>
-
+#include <charconv>
 #include <chrono>
-#include <cstdint>
 #include <string>
 #include <type_traits>
 
@@ -29,9 +27,68 @@ namespace redfish
 namespace time_utils
 {
 
+namespace details
+{
+
+using Days = std::chrono::duration<long long int, std::ratio<24 * 60 * 60>>;
+
+template <typename T>
+std::chrono::milliseconds fromDurationFormatItem(std::string& fmt,
+                                                 const char postfix)
+{
+    auto pos = fmt.find(postfix);
+    if (pos == std::string::npos || fmt.size() < (pos + 1))
+    {
+        return std::chrono::milliseconds();
+    }
+
+    std::chrono::milliseconds out;
+    if constexpr (std::is_same<T, std::chrono::milliseconds>::value)
+    {
+        /* std::from_chars for floating types is not implemented in gcc */
+        const float v = std::stof(fmt.c_str(), nullptr);
+        out = T(static_cast<long>(
+            std::roundf(v * std::chrono::milliseconds::period::den)));
+    }
+    else
+    {
+        long v = 0;
+        std::from_chars(fmt.c_str(), fmt.c_str() + pos, v);
+        out = T(v);
+    }
+
+    fmt.erase(0, pos + 1);
+    return out;
+}
+
+} // namespace details
+
+std::time_t fromDurationFormat(std::string fmt)
+{
+    if (fmt.empty() || *fmt.begin() != 'P')
+    {
+        return 0;
+    }
+
+    fmt.erase(fmt.begin());
+    std::chrono::milliseconds out =
+        details::fromDurationFormatItem<details::Days>(fmt, 'D');
+    if (fmt.empty() || *fmt.begin() != 'T')
+    {
+        return static_cast<std::time_t>(out.count());
+    }
+
+    fmt.erase(fmt.begin());
+    out += details::fromDurationFormatItem<std::chrono::hours>(fmt, 'H');
+    out += details::fromDurationFormatItem<std::chrono::minutes>(fmt, 'M');
+    out += details::fromDurationFormatItem<std::chrono::milliseconds>(fmt, 'S');
+
+    return static_cast<std::time_t>(out.count());
+}
+
 /**
  * @brief Convert time value into duration format that is based on ISO 8601.
- *        Example output: "P192DT12H01M01.500000S"
+ *        Example output: "P192DT12H1M1.500000S"
  */
 std::string toDurationFormat(const std::time_t ms_time)
 {
@@ -39,8 +96,7 @@ std::string toDurationFormat(const std::time_t ms_time)
     fmt.reserve(sizeof("PxxxxxxxxxxxxDTxxHxxMxx.xxxxxxS\0"));
     std::chrono::milliseconds ms(ms_time);
 
-    using Days = std::chrono::duration<long long int, std::ratio<24 * 60 * 60>>;
-    auto days = std::chrono::duration_cast<Days>(ms);
+    auto days = std::chrono::duration_cast<details::Days>(ms);
     ms -= days;
 
     auto hours = std::chrono::duration_cast<std::chrono::hours>(ms);
