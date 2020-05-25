3ed10b3eb4fee48904357c0c2f0ea6ea0f9bbb93
AppaRao Puli <apparao.puli@linux.intel.com>
2020-05-25 16:14:39 +0530

    EventService: https client support
    Add https client support for push style
    eventing. Using this BMC can push the event
    logs/telemetry data to event listener over
    secure http channel.
    
    Tested:
     - Created subscription with https destination
       url. Using SubmitTestEvent action set the
       event and can see event on event listener.
     - Validator passed.
    
    Change-Id: I44c3918b39baa2eb5fddda9d635f99aa280a422a
    Signed-off-by: AppaRao Puli <apparao.puli@linux.intel.com>

--- redfish-core/include/event_service_manager.hpp
+++ redfish-core/include/event_service_manager.hpp
@@ -295,7 +295,7 @@ class Subscription
     {
         conn = std::make_shared<crow::HttpClient>(
             crow::connections::systemBus->get_io_context(), id, host, port,
-            path);
+            path, (uriProto == "https" ? true : false));
     }
     ~Subscription()
     {}
