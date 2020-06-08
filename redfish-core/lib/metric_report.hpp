141cee16b6886337cdc21c3714e93f7ab50bbb7b
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-06-08 17:15:54 +0200

    Add support for MetricDefinition scheme
    Added MetricDefinition node to redfish core. Now user is able to
    get all possible metrics that are present in system and are
    supported by TelemetryService.
    
    Tested:
     - Succesfully passed RedfishServiceValidator.py
     - Validated a presence of MetricDefinition members
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: I3086e1302e1ba2e5442d1367939fd5507a0cbc00

--- redfish-core/lib/metric_report.hpp
+++ redfish-core/lib/metric_report.hpp
@@ -88,7 +88,8 @@ class MetricReport : public Node
                     [asyncResp,
                      id](const boost::system::error_code ec,
                          const std::vector<std::pair<
-                             std::string, std::variant<int32_t, Readings>>>&
+                             std::string, std::variant<int32_t, Readings,
+                                                       ReadingParameters>>>&
                              ret) {
                         if (ec ==
                             boost::system::errc::no_such_file_or_directory)
@@ -112,6 +113,9 @@ class MetricReport : public Node
             });
     }
 
+    using ReadingParameters =
+        std::vector<std::tuple<std::vector<sdbusplus::message::object_path>,
+                               std::string, std::string, std::string>>;
     using Readings =
         std::vector<std::tuple<std::string, std::string, double, int32_t>>;
 
@@ -132,10 +136,61 @@ class MetricReport : public Node
         return metricValues;
     }
 
+    static void addMetricDefinition(nlohmann::json& metrics,
+                                    const ReadingParameters& params)
+    {
+        for (auto& metric : metrics)
+        {
+            auto idIt = metric.find("MetricId");
+            if (idIt == metric.end() || !idIt->is_string())
+            {
+                continue;
+            }
+
+            auto idPtr = idIt->get_ptr<std::string*>();
+            if (!idPtr)
+            {
+                continue;
+            }
+
+            auto param = std::find_if(
+                params.begin(), params.end(), [idPtr](const auto& x) {
+                    auto& [sensors, type, metricId, metadata] = x;
+                    return *idPtr == metricId;
+                });
+            if (param == params.end())
+            {
+                continue;
+            }
+
+            auto& dbusPaths =
+                std::get<std::vector<sdbusplus::message::object_path>>(*param);
+            if (dbusPaths.size() != 1)
+            {
+                continue;
+            }
+
+            auto dbusPath = dbusPaths.begin();
+            for (size_t i = 0; i < sensors::dbus::paths.size(); i++)
+            {
+                if (dbusPath->str.find(sensors::dbus::paths[i]) ==
+                    std::string::npos)
+                {
+                    continue;
+                }
+                metric["MetricDefinition"]["@odata.id"] =
+                    telemetry::metricDefinitionUri +
+                    std::string(sensors::dbus::names[i]);
+                break;
+            }
+        }
+    }
+
     static void fillReport(
         const std::shared_ptr<AsyncResp>& asyncResp, const std::string& id,
-        const std::vector<
-            std::pair<std::string, std::variant<int32_t, Readings>>>& ret)
+        const std::vector<std::pair<
+            std::string, std::variant<int32_t, Readings, ReadingParameters>>>&
+            ret)
     {
         asyncResp->res.jsonValue["@odata.type"] = schemaType;
         asyncResp->res.jsonValue["@odata.id"] = telemetry::metricReportUri + id;
@@ -146,6 +201,7 @@ class MetricReport : public Node
 
         const int32_t* timestamp = nullptr;
         const Readings* readings = nullptr;
+        const ReadingParameters* readingParams = nullptr;
         for (const auto& [key, var] : ret)
         {
             if (key == "Timestamp")
@@ -156,8 +212,12 @@ class MetricReport : public Node
             {
                 readings = std::get_if<Readings>(&var);
             }
+            else if (key == "ReadingParameters")
+            {
+                readingParams = std::get_if<ReadingParameters>(&var);
+            }
         }
-        if (!timestamp || !readings)
+        if (!timestamp || !readings || !readingParams)
         {
             BMCWEB_LOG_ERROR << "Property type mismatch or property is missing";
             messages::internalError(asyncResp->res);
@@ -167,6 +227,8 @@ class MetricReport : public Node
         asyncResp->res.jsonValue["Timestamp"] =
             crow::utility::getDateTime(*timestamp);
         asyncResp->res.jsonValue["MetricValues"] = toMetricValues(*readings);
+        addMetricDefinition(asyncResp->res.jsonValue["MetricValues"],
+                            *readingParams);
     }
 
     template <typename Callback>
