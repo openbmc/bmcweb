343c17a90b695d464f7173438fb8504409529715
James Feist <james.feist@linux.intel.com>
2020-06-11 10:39:55 -0700

    Update Service: Change error message based on error logs
    THis adds support for better error responses based on
    the logs generated by phosphor-software-manager.
    
    Tested: Got 400 error with different messages based
    on failure type
    
    {
        "error": {
            "@Message.ExtendedInfo": [
                {
                    "@odata.type": "/redfish/v1/$metadata#Message.v1_0_0.Message",
                    "Message": "Invalid file uploaded to /redfish/v1/UpdateService: invalid archive.",
                    "MessageArgs": [
                        "/redfish/v1/UpdateService",
                        "invalid archive"
                    ],
                    "MessageId": "OpenBMC.0.1.0.InvalidFile",
                    "Resolution": "None.",
                    "Severity": "Warning"
                }
            ],
            "code": "OpenBMC.0.1.0.InvalidFile",
            "message": "Invalid file uploaded to /redfish/v1/UpdateService: invalid archive."
        }
    }
    
    {
        "error": {
            "@Message.ExtendedInfo": [
                {
                    "@odata.type": "/redfish/v1/$metadata#Message.v1_0_0.Message",
                    "Message": "Invalid file uploaded to /redfish/v1/UpdateService: invalid image format.",
                    "MessageArgs": [
                        "/redfish/v1/UpdateService",
                        "invalid image format"
                    ],
                    "MessageId": "OpenBMC.0.1.0.InvalidFile",
                    "Resolution": "None.",
                    "Severity": "Warning"
                }
            ],
            "code": "OpenBMC.0.1.0.InvalidFile",
            "message": "Invalid file uploaded to /redfish/v1/UpdateService: invalid image format."
        }
    }
    
    {
        "error": {
            "@Message.ExtendedInfo": [
                {
                    "@odata.type": "#Message.v1_0_0.Message",
                    "Message": "The resource /redfish/v1/UpdateService was unable to satisfy the request due to unavailability of resources.",
                    "MessageArgs": [
                        "/redfish/v1/UpdateService"
                    ],
                    "MessageId": "Base.1.4.0.ResourceExhaustion",
                    "Resolution": "Ensure that the resources are available and resubmit the request.",
                    "Severity": "Critical"
                }
            ],
            "code": "Base.1.4.0.ResourceExhaustion",
            "message": "The resource /redfish/v1/UpdateService was unable to satisfy the request due to unavailability of resources."
        }
    }
    
    Change-Id: Ida9a23c10aedbf9a48c96f4050a04e06bddff284
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- redfish-core/include/registries/openbmc_message_registry.hpp
+++ redfish-core/include/registries/openbmc_message_registry.hpp
@@ -29,7 +29,7 @@ const Header header = {
     "0.1.0",
     "OpenBMC",
 };
-constexpr std::array<MessageEntry, 155> registry = {
+constexpr std::array<MessageEntry, 156> registry = {
     MessageEntry{
         "ADDDCCorrectable",
         {
@@ -443,6 +443,15 @@ constexpr std::array<MessageEntry, 155> registry = {
             {"string"},
             "None.",
         }},
+    MessageEntry{"InvalidFile",
+                 {
+                     "Indicates that the uploaded file was invalid.",
+                     "Invalid file uploaded to %1: %2.",
+                     "Warning",
+                     2,
+                     {"string", "string"},
+                     "None.",
+                 }},
     MessageEntry{"InvalidLoginAttempted",
                  {
                      "Indicates that a login was attempted on the specified "
