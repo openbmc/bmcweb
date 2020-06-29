44d365655cb72af3ffbb4e9ced4fe87cb5c2e061
Sunitha Harish <sunithaharish04@gmail.com>
2020-06-29 07:01:29 -0500

    EventService : Push event for the successful AcquireLock
    This implements the LockAcquired message registry and sends out this event
    when the IBM management console performs a successful acquirelock action.
    
    Tested by: (Used https://github.com/DMTF/Redfish-Event-Listener)
        1. Create a subscription through the Redfish-Event-Listener
           POST -D headers.txt https://${bmc}/redfish/v1/EventService/Subscriptions
           -d '{"Destination" : "https://<host:port> "Protocol":"Redfish"}'
        2. Perform AcquireLock.
        3. Verify the LockAcquired event is generated and posted to the subscriber.
    
    Signed-off-by: Sunitha Harish <sunithaharish04@gmail.com>
    Change-Id: Ie67e2763ac4ec6c0aa4fab46c8d192d8b6257902

---
+++ redfish-core/include/event_messages.hpp
@@ -0,0 +1,20 @@
+namespace redfish
+{
+namespace messages
+{
+
+nlohmann::json LockAcquired(const std::string& arg1, const std::string& arg2,
+                            const uint32_t arg3)
+{
+    return nlohmann::json{
+        {"EventType", "Alert"},
+        {"MessageId", "OpenBMC.0.1.0.LockAcquired"},
+        {"Message", "Management console " + arg1 +
+                        " has acquired the lock using session " + arg2 +
+                        " with transaction id " + std::to_string(arg3) + "."},
+        {"MessageArgs", {arg1, arg2, arg3}},
+        {"MessageSeverity", "OK"}};
+}
+
+} // namespace messages
+} // namespace redfish
