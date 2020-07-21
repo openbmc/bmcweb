efcbef41c4f245be133893575de0b3d9035d4552
Ed Tanous <ed@tanous.net>
2020-07-21 08:46:25 -0700

    Implement MIME parsing
    This commit adds two core features to bmcweb:
    
    1. A multipart mime parser that can read multipart form requests into
    bmcweb.  This is implemented as a generic parser that identifies the
    content-type strings and parses them into structures.
    
    2. A /login route that can be logged into with a multipart form.  This
    is to allow changing the login screen to a purely forms based
    implementation, thus removing the very large whitelist we currently have
    to maintain, and removing javascript from our threat envelope.
    
    More testing is still needed, as this is a parser that exists outside of
    the secured areas, but in this simple example, it seems to work well.
    
    Tested: curl -vvvvv --insecure -X POST -F 'username=root' -F
    'password=0penBmc' https://<bmc ip address>:18080/login
    
    Returned; { "data": "User 'root' logged in", "message": "200 OK",
    "status": "ok" }
    
    Change-Id: Icc3f4c082d584170b65b9e82f7876926cd38035d
    Signed-off-by: Ed Tanous<ed@tanous.net>
    Signed-off-by: George Liu <liuxiwei@inspur.com>

---
+++ include/ut/multipart_test.cpp
@@ -0,0 +1,62 @@
+#include <http_utility.hpp>
+#include <multipart_parser.hpp>
+
+#include <map>
+
+#include "gmock/gmock.h"
+
+TEST(MultipartTest, TestGoodMultipartParser)
+{
+    boost::beast::http::request<boost::beast::http::string_body> req{};
+
+    req.set("Content-Type",
+            "multipart/form-data; "
+            "boundary=---------------------------d74496d66958873e");
+
+    req.body() = "-----------------------------d74496d66958873e\r\n"
+                 "Content-Disposition: form-data; name=\"Test1\"\r\n\r\n"
+                 "{\"Key1\": 112233}\r\n"
+                 "-----------------------------d74496d66958873e\r\n"
+                 "Content-Disposition: form-data; name=\"Test2\"\r\n\r\n"
+                 "123456\r\n"
+                 "-----------------------------d74496d66958873e--\r\n";
+
+    std::error_code ec;
+    crow::Request reqIn(req, ec);
+    MultipartParser parser(reqIn, ec);
+    ASSERT_FALSE(ec);
+
+    ASSERT_EQ(parser.boundary,
+              "\r\n-----------------------------d74496d66958873e");
+    ASSERT_EQ(parser.mime_fields.size(), 2);
+
+    ASSERT_EQ(parser.mime_fields[0].fields.at("Content-Disposition"),
+              "form-data; name=\"Test1\"");
+    ASSERT_EQ(parser.mime_fields[0].content, "{\"Key1\": 112233}");
+
+    ASSERT_EQ(parser.mime_fields[1].fields.at("Content-Disposition"),
+              "form-data; name=\"Test2\"");
+    ASSERT_EQ(parser.mime_fields[1].content, "123456");
+}
+
+TEST(MultipartTest, TestBadMultipartParser)
+{
+    boost::beast::http::request<boost::beast::http::string_body> req{};
+
+    req.set("Content-Type",
+            "multipart/form-data; "
+            "boundary=---------------------------d74496d66958873e");
+
+    req.body() = "-----------------------------d74496d66958873e\r\n"
+                 "Content-Disposition: form-data; name=\"Test1\"\r\n"
+                 "{\"Key1\": 112233}\r\n"
+                 "-----------------------------d74496d66958873e\r\n"
+                 "Content-Disposition: form-data; name=\"Test2\"\r\n"
+                 "123456\r\n"
+                 "-----------------------------d74496d66958873e--\r\n";
+
+    std::error_code ec;
+    crow::Request reqIn(req, ec);
+    MultipartParser parser(reqIn, ec);
+    ASSERT_EQ(ec.value(), static_cast<int>(ParserError::ERROR_EMPTY_HEADER));
+}
