e7dee89a4f38f94a97424099f37a3372c4fbbc6b
Ravi Teja <raviteja28031990@gmail.com>
2020-07-22 14:23:31 -0500

    EventService : Send event for the host and bmc state change
    This commit adds support to send redfish event when
    there is a host state change on the url "/redfish/v1/Systems/system"
    and bmc state change on the url "/redfish/v1/Managers/bmc"
    
    Tested by: (Used https://github.com/DMTF/Redfish-Event-Listener)
        1. Create a subscription by passing "ResourceTypes" as ["Manager","System"]
           POST -D headers.txt https://${bmc}/redfish/v1/EventService/Subscriptions
           -d '{"Destination" : "https://<host:port>, "ResourceTypes":["Manager","System"], "Protocol":"Redfish"}'
        2. Run a command which change host and bmc state
        3. Verify if the changes are reported as events
    
    BMC state change Event payload :
    
    bodydata:  {"@odata.type":"#Event.v1_4_0.Event","Events":[{"EventId":1,"EventTimestamp":"2020-08-06T13:18:15+00:00","EventType":"ResourceChanged","MemberId":0,"Message":"One or more resource properties have changed.","MessageArgs":null,"MessageId":"ResourceEvent.1.0.3.ResourceChanged","MessageSeverity":"OK","OriginOfCondition":"/redfish/v1/Managers/bmc/PowerState","Severity":"OK"}],"Id":1,"Name":"Event Log"}
    
    Host state event Payload:
    
    bodydata:  {"@odata.type":"#Event.v1_4_0.Event","Events":[{"EventId":30,"EventTimestamp":"2020-08-06T13:16:21+00:00","EventType":"ResourceChanged","MemberId":0,"Message":"One or more resource properties have changed.","MessageArgs":null,"MessageId":"ResourceEvent.1.0.3.ResourceChanged","MessageSeverity":"OK","OriginOfCondition":"/redfish/v1/Systems/system/PowerState","Severity":"OK"}],"Id":30,"Name":"Event Log"}
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: Ia34174ad11999372a12707a5ceeeac672b1e074a

--- redfish-core/lib/event_service.hpp
+++ redfish-core/lib/event_service.hpp
@@ -27,11 +27,11 @@ static constexpr const std::array<const char*, 3> supportedRetryPolicies = {
     "TerminateAfterRetries", "SuspendRetries", "RetryForever"};
 
 #ifdef BMCWEB_ENABLE_IBM_MANAGEMENT_CONSOLE
-static constexpr const std::array<const char*, 2> supportedResourceTypes = {
-    "IBMConfigFile", "Task"};
+static constexpr const std::array<const char*, 4> supportedResourceTypes = {
+    "IBMConfigFile", "Task", "ComputerSystem", "Manager"};
 #else
-static constexpr const std::array<const char*, 1> supportedResourceTypes = {
-    "Task"};
+static constexpr const std::array<const char*, 3> supportedResourceTypes = {
+    "Task", "ComputerSystem", "Manager"};
 #endif
 
 static constexpr const uint8_t maxNoOfSubscriptions = 20;
