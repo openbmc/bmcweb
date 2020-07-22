7897752e1882cb71c2a3230f196748b8971fcb4d
Ravi Teja <raviteja28031990@gmail.com>
2020-07-22 14:23:31 -0500

    EventService : Send event for the host state change
    This commit adds support to send redfish event when
    there is a host state change on the url "/redfish/v1/Systems/system"
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: Ia34174ad11999372a12707a5ceeeac672b1e074a

--- redfish-core/lib/event_service.hpp
+++ redfish-core/lib/event_service.hpp
@@ -16,6 +16,8 @@
 #pragma once
 #include "event_service_manager.hpp"
 
+#include <event_dbus_monitor.hpp>
+
 namespace redfish
 {
 
@@ -27,11 +29,11 @@ static constexpr const std::array<const char*, 3> supportedRetryPolicies = {
     "TerminateAfterRetries", "SuspendRetries", "RetryForever"};
 
 #ifdef BMCWEB_ENABLE_IBM_MANAGEMENT_CONSOLE
-static constexpr const std::array<const char*, 2> supportedResourceTypes = {
-    "ConfigFile", "Task"};
+static constexpr const std::array<const char*, 3> supportedResourceTypes = {
+    "ConfigFile", "Task", "ComputerSystem"};
 #else
-static constexpr const std::array<const char*, 1> supportedResourceTypes = {
-    "Task"};
+static constexpr const std::array<const char*, 2> supportedResourceTypes = {
+    "Task", "ComputerSystem"};
 #endif
 
 static constexpr const uint8_t maxNoOfSubscriptions = 20;
@@ -426,6 +428,9 @@ class EventDestinationCollection : public Node
             return;
         }
 
+        // Start Host state change dbus monitor
+        crow::dbus_monitor::registerHostStateChangeSignal();
+
         messages::created(asyncResp->res);
         asyncResp->res.addHeader(
             "Location", "/redfish/v1/EventService/Subscriptions/" + id);
@@ -669,6 +674,16 @@ class EventDestination : public Node
             return;
         }
         EventServiceManager::getInstance().deleteSubscription(params[0]);
+
+        // Check subscription count for "ComputerSystem" resource.
+        // Clear D-Bus match if there are no subscriptions.
+        uint16_t count =
+            EventServiceManager::getInstance().getCountOfResourceSubscriptions(
+                "ComputerSystem");
+        if (!count)
+        {
+            crow::dbus_monitor::unregisterHostStateChangeSignal();
+        }
     }
 };
 
