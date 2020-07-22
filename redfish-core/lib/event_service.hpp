e9aee7c2b0e3e5eba2053bf1f50c8ccf240a8245
Ravi Teja <raviteja28031990@gmail.com>
2020-07-22 14:23:31 -0500

    EventService : Send event for the host state change
    This commit adds support to send redfish event when
    there is a host state change on the url "/redfish/v1/Systems/system"
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: Ia34174ad11999372a12707a5ceeeac672b1e074a

--- redfish-core/lib/event_service.hpp
+++ redfish-core/lib/event_service.hpp
@@ -16,6 +16,8 @@
 #pragma once
 #include "event_service_manager.hpp"
 
+#include <event_dbus_monitor.hpp>
+
 namespace redfish
 {
 
@@ -27,11 +29,11 @@ static constexpr const std::array<const char*, 3> supportedRetryPolicies = {
     "TerminateAfterRetries", "SuspendRetries", "RetryForever"};
 
 #ifdef BMCWEB_ENABLE_IBM_MANAGEMENT_CONSOLE
-static constexpr const std::array<const char*, 2> supportedResourceTypes = {
-    "IBMConfigFile", "Task"};
+static constexpr const std::array<const char*, 4> supportedResourceTypes = {
+    "IBMConfigFile", "Task", "ComputerSystem", "Manager"};
 #else
-static constexpr const std::array<const char*, 1> supportedResourceTypes = {
-    "Task"};
+static constexpr const std::array<const char*, 3> supportedResourceTypes = {
+    "Task", "ComputerSystem", "Manager"};
 #endif
 
 static constexpr const uint8_t maxNoOfSubscriptions = 20;
@@ -425,6 +427,9 @@ class EventDestinationCollection : public Node
             messages::internalError(asyncResp->res);
             return;
         }
+
+        // Start BMC and Host state change dbus monitor
+        crow::dbus_monitor::registerStateChangeSignal();
 
         messages::created(asyncResp->res);
         asyncResp->res.addHeader(
