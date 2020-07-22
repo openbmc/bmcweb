9a9c9eb71746050b85dddf53629513f654d08a66
Ravi Teja <raviteja28031990@gmail.com>
2020-07-22 14:23:31 -0500

    EventService : Send event for the host state change
    This commit adds support to send redfish event when
    there is a host state change on the url "/redfish/v1/Systems/system"
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: Ia34174ad11999372a12707a5ceeeac672b1e074a

---
+++ include/event_dbus_monitor.hpp
@@ -0,0 +1,120 @@
+#include <error_messages.hpp>
+#include <event_service_manager.hpp>
+#include <resource_messages.hpp>
+
+namespace crow
+{
+namespace dbus_monitor
+{
+static std::vector<std::unique_ptr<sdbusplus::bus::match::match>> matches;
+
+void registerHostStateChangeSignal();
+
+void registerBMCStateChangeSignal();
+
+inline void BMCStatePropertyChange(sdbusplus::message::message& msg)
+{
+
+            BMCWEB_LOG_DEBUG << "BMC state change match fired";
+
+            if (msg.is_method_error())
+            {
+                BMCWEB_LOG_ERROR << "BMC state property changed Signal error";
+                return;
+            }
+            std::string iface;
+            boost::container::flat_map<std::string,
+                                       std::variant<std::string, uint8_t>>
+                values;
+            std::string objName;
+            msg.read(objName, values);
+            auto find = values.find("CurrentBMCState");
+            if (find == values.end())
+            {
+                registerBMCStateChangeSignal();
+                return;
+            }
+            std::string* type = std::get_if<std::string>(&(find->second));
+            if (type != nullptr)
+            {
+                BMCWEB_LOG_DEBUG << *type;
+                // Push an event
+                std::string origin = "/redfish/v1/Managers/bmc/PowerState";
+                std::string eventId = "BMCStateChanged";
+                redfish::EventServiceManager::getInstance().sendEvent(
+                    eventId, redfish::messages::ResourceChanged(), origin);
+            }
+            registerBMCStateChangeSignal();
+}
+
+inline void HostStatePropertyChange(sdbusplus::message::message& msg)
+{
+            BMCWEB_LOG_DEBUG << "Host state change match fired";
+
+            if (msg.is_method_error())
+            {
+                BMCWEB_LOG_ERROR << "Host state property changed Signal error";
+                return;
+            }
+            std::string iface;
+            boost::container::flat_map<std::string,
+                                       std::variant<std::string, uint8_t>>
+                values;
+            std::string objName;
+            msg.read(objName, values);
+            auto find = values.find("CurrentHostState");
+            if (find == values.end())
+            {
+                registerHostStateChangeSignal();
+                return;
+            }
+            std::string* type = std::get_if<std::string>(&(find->second));
+            if (type != nullptr)
+            {
+                BMCWEB_LOG_DEBUG << *type;
+                // Push an event
+                std::string origin = "/redfish/v1/Systems/system/PowerState";
+                std::string eventId = "HostStateChanged";
+                redfish::EventServiceManager::getInstance().sendEvent(
+                    eventId, redfish::messages::ResourceChanged(), origin);
+            }
+            registerHostStateChangeSignal();
+}
+
+void registerHostStateChangeSignal()
+{
+    matches.emplace_back( std::make_unique<sdbusplus::bus::match::match>(
+        *crow::connections::systemBus,
+        "type='signal',member='PropertiesChanged',interface='org.freedesktop."
+        "DBus.Properties',path='/xyz/openbmc_project/state/host0',"
+        "arg0='xyz.openbmc_project.State.Host'",HostStatePropertyChange));
+}
+
+void registerBMCStateChangeSignal()
+{
+    matches.emplace_back( std::make_unique<sdbusplus::bus::match::match>(
+        *crow::connections::systemBus,
+        "type='signal',member='PropertiesChanged',interface='org.freedesktop."
+        "DBus.Properties',path='/xyz/openbmc_project/state/bmc0',"
+        "arg0='xyz.openbmc_project.State.BMC'",BMCStatePropertyChange));
+}
+
+
+void registerStateChangeSignal()
+{
+    if (matches.size())
+    {
+
+        BMCWEB_LOG_DEBUG << "Host/BMC state change signal already registered";
+        return;
+    }
+
+    BMCWEB_LOG_DEBUG << "Host/BMC state change signal - Register";
+
+    registerHostStateChangeSignal();
+    registerBMCStateChangeSignal();
+}
+
+
+} // namespace dbus_monitor
+} // namespace crow
