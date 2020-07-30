25d92793ba0d9d42818acbcb27c460e4014e5911
Sunitha Harish <sunharis@in.ibm.com>
2020-07-30 08:25:28 -0500

    Validate the path during ConfigFile upload
    The IBM management console usecase - ConfigFile upload was allowing
    to create or modify any file at the BMC when the path url is given
    as below.
    PUT https://${bmc}/ibm/v1/Host/ConfigFiles/../../../../../<any file under root dir> --data-binary "junk data"
    
    This commit adds validation to the "path" variable after the "ConfigFiles/"
    in the url - so that only the ConfigFiles are created or modified.
    
    The filename validation includes:
      Restrict the maximum filename length to 20 characters
      Restrict the allowed charaters to [A-Za-z0-9-]
    
    The minimum size of the file allowed is 100 bytes
    The maximum size of the file allowed is 500KB
    Maximum total size of the ConfigFile directory at BMC file system allowed is 10MB
    
    Tested by:
    
      1. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/../../../../../etc/p2 --data-binary "some data"
         Bad Request
    
      2. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/../../../etc/p2 --data-binary "some data"
         Bad Request
    
      3. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/../etc/p2 --data-binary "some data"
         Bad Request
    
      4. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/etc/p2 --data-binary "some data"
         {
           "Description": "Error while creating the file"
         }
    
      5. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/mydir/p2 --data-binary "some data"
         {
           "Description": "Error while creating the file"
         }
    
      6. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/ --data-binary "some data"
         Not Found
    
      7. PUT https://${bmc}/ibm/v1/Host/ConfigFiles --data-binary "some data"
         Method Not Allowed
    
      8. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/p2/../p2 --data-binary "some data"
         Bad Request
    
      9. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/p2/p2 --data-binary "some data"
         {
           "Description": "Error while creating the file"
         }
    
      10. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/p2/../../../p2 --data-binary "some data"
          Bad Request
    
      11. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/./../../p2 --data-binary "some data"
          Bad Request
    
      12. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/. --data-binary "some data"
          Bad Request
    
      13. PUT https://${bmc}/ibm/v1/Host/../ConfigFiles/p2 --data-binary "some data"
          Not Found
    
      14. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/p2 --data-binary "some data"
          {
            "Description": "File Created"
          }
    
      15. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/p2 --data-binary "some data"
          {
            "Description": "File Updated"
          }
    
      16. PUT https://${bmc}/ibm/v1/Host/ConfigFiles/p2.ext --data-binary "some data"
          {
            "Description": "File Created"
          }
      17. Tested sending filename greater than 20 charaters
          Bad Request
      18. Tested sending filename with special charaters
          Bad Request
      19. Tested sending filesize less than 100bytes
          Bad request
      20. Tested sending filesize greater than 500KB
          Bad request
      21. Tested uploading the file when the directory size is newarly full
          Bas request
    
    Signed-off-by: Sunitha Harish <sunharis@in.ibm.com>
    Change-Id: I838d39d5765ddc8701f7e5c533a93eebde021cbf

--- include/ibm/management_console_rest.hpp
+++ include/ibm/management_console_rest.hpp
@@ -34,6 +34,10 @@ constexpr const char* resourceNotFoundMsg = "Resource Not Found";
 constexpr const char* contentNotAcceptableMsg = "Content Not Acceptable";
 constexpr const char* internalServerError = "Internal Server Error";
 
+constexpr size_t maxSaveareaDirSize =
+    10000000; // Allow save area dir size to be max 10MB
+constexpr size_t minSaveareaFileSize =
+    100; // Allow save area file size of minimum 100B
 constexpr size_t maxSaveareaFileSize =
     500000; // Allow save area file size upto 500KB
 constexpr size_t maxBroadcastMsgSize =
@@ -101,13 +105,31 @@ inline void handleFilePut(const crow::Request& req, crow::Response& res,
         res.jsonValue["Description"] = resourceNotFoundMsg;
         return;
     }
-    // Create the file
+
     std::ofstream file;
     std::filesystem::path loc("/var/lib/obmc/bmc-console-mgmt/save-area");
-    loc /= fileID;
 
+    // Get the current size of the savearea direcotry
+    std::uintmax_t saveAreaDirSize = 0;
+    for (std::filesystem::recursive_directory_iterator it(loc);
+         it != std::filesystem::recursive_directory_iterator(); ++it)
+    {
+        if (!std::filesystem::is_directory(*it))
+            saveAreaDirSize += std::filesystem::file_size(*it);
+    }
+    BMCWEB_LOG_DEBUG << "saveAreaDirSize: " << saveAreaDirSize;
+
+    // Get the file size getting uploaded
     const std::string& data = req.body;
     BMCWEB_LOG_DEBUG << "data capaticty : " << data.capacity();
+
+    if (data.capacity() < minSaveareaFileSize)
+    {
+        res.result(boost::beast::http::status::bad_request);
+        res.jsonValue["Description"] =
+            "File size is less than minimum allowed size[100B]";
+        return;
+    }
     if (data.capacity() > maxSaveareaFileSize)
     {
         res.result(boost::beast::http::status::bad_request);
@@ -115,13 +137,43 @@ inline void handleFilePut(const crow::Request& req, crow::Response& res,
             "File size exceeds maximum allowed size[500KB]";
         return;
     }
+
+    // Form the file path
+    loc /= fileID;
     BMCWEB_LOG_DEBUG << "Writing to the file: " << loc;
 
     bool fileExists = false;
+    std::uintmax_t newSizeToWrite = 0;
     if (std::filesystem::exists(loc))
     {
         fileExists = true;
+        // Calculate the difference in the file size
+        if (data.capacity() < std::filesystem::file_size(loc))
+        {
+            newSizeToWrite = std::filesystem::file_size(loc) - data.capacity();
+        }
+        else
+        {
+            newSizeToWrite = data.capacity() - std::filesystem::file_size(loc);
+        }
     }
+    else
+    {
+        // This is a new file upload
+        newSizeToWrite = data.capacity();
+    }
+
+    // Calculate the total dir size before writing the new file
+    BMCWEB_LOG_DEBUG << "total new size: " << saveAreaDirSize + newSizeToWrite;
+
+    if ((saveAreaDirSize + newSizeToWrite) > maxSaveareaDirSize)
+    {
+        res.result(boost::beast::http::status::bad_request);
+        res.jsonValue["Description"] = "File size does not fit in the savearea "
+                                       "directory maximum allowed size[10MB]";
+        return;
+    }
+
     file.open(loc, std::ofstream::out);
     if (file.fail())
     {
@@ -604,7 +656,40 @@ inline void requestRoutes(App& app)
         .methods(boost::beast::http::verb::put, boost::beast::http::verb::get,
                  boost::beast::http::verb::delete_)(
             [](const crow::Request& req, crow::Response& res,
-               const std::string& path) { handleFileUrl(req, res, path); });
+               const std::string& path) {
+                BMCWEB_LOG_DEBUG << "ConfigFile path : " << path;
+                // Validate the path so that it will disallow
+                // modifying or creating any file in the BMC.
+                // eg: /ibm/v1/Host/ConfigFiles/../../../../../etc/resolv.conf
+                std::filesystem::path uploadPath =
+                    "/var/lib/obmc/bmc-console-mgmt/save-area/" + path;
+                if (std::filesystem::weakly_canonical(uploadPath) != uploadPath)
+                {
+                    BMCWEB_LOG_ERROR << "Bad file path : " << uploadPath;
+                    res.result(boost::beast::http::status::bad_request);
+                    res.end();
+                    return;
+                }
+                if (path.length() > 20)
+                {
+                    BMCWEB_LOG_ERROR << "Name must be maximum 20 characters. "
+                                        "Input filename length is: "
+                                     << path.length();
+                    res.result(boost::beast::http::status::bad_request);
+                    res.end();
+                    return;
+                }
+                const std::regex pattern("^[A-Za-z0-9-]+$");
+                if (!std::regex_match(path, pattern))
+                {
+                    BMCWEB_LOG_ERROR << "Unsupported character in filename: "
+                                     << path;
+                    res.result(boost::beast::http::status::bad_request);
+                    res.end();
+                    return;
+                }
+                handleFileUrl(req, res, path);
+            });
 
     BMCWEB_ROUTE(app, "/ibm/v1/HMC/LockService")
         .privileges({"ConfigureComponents", "ConfigureManager"})
