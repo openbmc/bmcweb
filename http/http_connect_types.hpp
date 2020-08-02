5ee95ba59e15375b2019d3ff7c5682d3e75d6fd7
Ed Tanous <ed@tanous.net>
2020-08-02 04:29:21 +0000

    WIP: Enable HTTP additional sockets
    This commit attempts to add the concept of an SSL detector from beast,
    and add the capability into bmcweb.  This allows directing multiple
    socket files to the bmcweb instance, and bmcweb will automatically sort
    out whether or not they're SSL, and give the correct response.  This
    allows users to plug in erroneous urls like "https://mybmc:80" and they
    will forward and work correctly.
    
    Some key design points:
    The HTTP side of bmcweb implements the exact same http headers as the
    HTTPS side, with the exception of HSTS, which is explicitly disallowed.
    This is for consistency and security.
    
    The above allows bmcweb builds to "select" the appropriate security
    posture (http, https, or both) for a given channel using the
    FileDescriptorName field within a socket file.  Items ending in:
    both: Will support both HTTPS and HTTP redirect to HTTPS
    https: Will support HTTPS only
    http: will support HTTP only
    
    Given the flexibility in bind statements, this allows administrators to
    support essentially any security posture they like.  The openbmc
    defaults are:
    HTTPS + Redirect on both ports 443 and port 80 if http-redirect is
    enabled
    
    And HTTPS only if http-redirect is disabled.
    
    Tested:
    Previous commits ran the below tests.
    Ran the server with options enabled.  Tried:
    ```
    curl -vvvv --insecure --user root:0penBmc http://192.168.7.2/redfish/v1/Managers/bmc
    *   Trying 192.168.7.2:80...
    * Connected to 192.168.7.2 (192.168.7.2) port 80 (#0)
    * Server auth using Basic with user 'root'
    > GET /redfish/v1/Managers/bmc HTTP/1.1
    > Host: 192.168.7.2
    > Authorization: Basic cm9vdDowcGVuQm1j
    > User-Agent: curl/7.72.0
    > Accept: */*
    >
    * Mark bundle as not supporting multiuse
    < HTTP/1.1 301 Moved Permanently
    < Location: https://192.168.7.2
    < X-Frame-Options: DENY
    < Pragma: no-cache
    < Cache-Control: no-Store,no-Cache
    < X-XSS-Protection: 1; mode=block
    < X-Content-Type-Options: nosniff
    < Content-Security-Policy: default-src 'none'; img-src 'self' data:; font-src 'self'; style-src 'self'; script-src 'self'; connect-src 'self' wss:
    < Date: Fri, 08 Jan 2021 01:43:49 GMT
    < Connection: close
    < Content-Length: 0
    <
    * Closing connection 0
    ```
    
    Observe above:
    webserver returned 301 redirect.
    webserver returned the appropriate security headers
    webserver immediately closed the connection.
    
    The same test above over https:// returns the values as expected
    
    Loaded the webui to test static file hosting.  Webui logs in and works
    as expected.
    
    Used the scripts/websocket_test.py to verify that websockets work.
    Sensors report as expected.
    
    Change-Id: Ib5733bbe5473fed6e0e27c56cdead0bffedf2993
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ http/http_connect_types.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace crow
+{
+enum class HttpType
+{
+    HTTPS, // Socket supports HTTPS only
+    HTTP,  // Socket supports HTTP only
+    BOTH   // Socket supports both HTTPS and HTTP, with HTTP Redirect
+};
+}
