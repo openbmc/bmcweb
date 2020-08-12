01835a589ebd8cf793ed57d3fe2bf612b6eabc3b
Jason M. Bills <jason.m.bills@linux.intel.com>
2020-08-12 14:45:32 -0700

    Add firmware activation messages to the registry
    In some situations a firmware requires activation rather than
    update.  These messages are intended to cover those situations.
    
    Tested:
    Logged these events and confirmed that they appear correctly in
    Redfish.
    
    Change-Id: I6171b9584626e049349a26c414146f37c04768c7
    Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>

--- redfish-core/include/registries/openbmc_message_registry.hpp
+++ redfish-core/include/registries/openbmc_message_registry.hpp
@@ -29,7 +29,7 @@ const Header header = {
     "0.1.0",
     "OpenBMC",
 };
-constexpr std::array<MessageEntry, 183> registry = {
+constexpr std::array<MessageEntry, 185> registry = {
     MessageEntry{
         "ADDDCCorrectable",
         {
@@ -437,6 +437,27 @@ constexpr std::array<MessageEntry, 183> registry = {
                  {
                      "Indicates that a system fan has been removed.",
                      "%1 removed.",
+                     "OK",
+                     "OK",
+                     1,
+                     {"string"},
+                     "None.",
+                 }},
+    MessageEntry{
+        "FirmwareActivationCompleted",
+        {
+            "Indicates a firmware activation has completed successfully.",
+            "%1 firmware activation completed.",
+            "OK",
+            "OK",
+            1,
+            {"string"},
+            "None.",
+        }},
+    MessageEntry{"FirmwareActivationStarted",
+                 {
+                     "Indicates a firmware activation has started.",
+                     "%1 firmware activation started",
                      "OK",
                      "OK",
                      1,
