aca715f1b642ca97fb01811dfc248845ac015d14
Jonathan Doman <jonathan.doman@intel.com>
2020-08-14 11:23:33 -0700

    Improve base64Decode bounds checking
    Index the decode array with an unsigned char rather than a signed int
    (which could accees outside the bounds of decodingData, leading to
    undefined behavior).
    Add unit tests for basic decoding functionality.
    Remove duplicate unused base64 functions.
    
    Tested: ran webtest and observed that previously failing
    Base64DecodeNonAscii now passes. Also tested basic auth:
      $ curl -vku root https://<ip>/redfish/v1/Managers/bmc
      ...
      < HTTP/1.1 200 OK
      ...
    
    Change-Id: I9f9e32650b1796f9fc0b2b25d482dffa35fac72d
    Signed-off-by: Jonathan Doman <jonathan.doman@intel.com>

---
+++ http/ut/utility_test.cpp
@@ -0,0 +1,18 @@
+#include "utility.h"
+
+#include "gmock/gmock.h"
+
+TEST(Utility, Base64DecodeAuthString)
+{
+    std::string authString("dXNlcm40bWU6cGFzc3cwcmQ=");
+    std::string result;
+    EXPECT_TRUE(crow::utility::base64Decode(authString, result));
+    EXPECT_EQ(result, "usern4me:passw0rd");
+}
+
+TEST(Utility, Base64DecodeNonAscii)
+{
+    std::string junkString("\xff\xee\xdd\xcc\x01\x11\x22\x33");
+    std::string result;
+    EXPECT_FALSE(crow::utility::base64Decode(junkString, result));
+}
