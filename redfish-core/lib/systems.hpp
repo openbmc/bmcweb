6930003194a06402eaf9739b41f803cdaf9dace2
Zhikui Ren <zhikui.ren@intel.com>
2020-09-10 11:13:14 -0700

    fix processor summary
    update processor summary to use cpu property correctly.
    
    Tested:
    1. Verified redfish validator passed
    2. Get cpu details from Redfish
    GET: https://<BMC-IP>/redfish/v1/Systems/system/Processors
    {
        "@odata.id": "/redfish/v1/Systems/system/Processors/",
        "@odata.type": "#ProcessorCollection.ProcessorCollection",
        "Members": [
            {
                "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0"
            }
        ],
        "Members@odata.count": 1,
        "Name": "Processor Collection"
    }
    
    Signed-off-by: Zhikui Ren <zhikui.ren@intel.com>
    Change-Id: I1f36118cdc80aabf05f2d678afc6ffc329d07872

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -326,7 +326,7 @@ inline void getComputerSystem(std::shared_ptr<AsyncResp> aResp,
 
                                     if (properties.size() > 0)
                                     {
-                                        const uint32_t* processorId = nullptr;
+                                        const uint64_t* processorId = nullptr;
                                         const std::string* procFamily = nullptr;
                                         nlohmann::json& procSummary =
                                             aResp->res.jsonValue["ProcessorSumm"
@@ -345,10 +345,10 @@ inline void getComputerSystem(std::shared_ptr<AsyncResp> aResp,
                                         for (const auto& property : properties)
                                         {
 
-                                            if (property.first == "ProcessorId")
+                                            if (property.first == "Id")
                                             {
                                                 processorId =
-                                                    std::get_if<uint32_t>(
+                                                    std::get_if<uint64_t>(
                                                         &property.second);
                                                 if (nullptr != procFamily)
                                                     break;
@@ -356,7 +356,7 @@ inline void getComputerSystem(std::shared_ptr<AsyncResp> aResp,
                                             }
 
                                             if (property.first ==
-                                                "ProcessorFamily")
+                                                "Family")
                                             {
                                                 procFamily =
                                                     std::get_if<std::string>(
