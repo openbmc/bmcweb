1fa593b81c1762a80f90824b9586b0b0acc95b86
Ed Tanous <ed@tanous.net>
2020-09-28 07:59:52 -0700

    Remove IBM console events from Redfish
    The /ibm/v1 console is a different tree than Redfish, and as such,
    should not be sending non-redfish resource events out.  This is very
    likely to break redfish clients on the other end.  If the management
    console wants an event-like entity, it needs to come up with its own
    EventService-like resource, considering it is a separate tree.
    
    Significant related discussion occurred here:
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/36368
    
    Signed-off-by: Ed Tanous <ed@tanous.net>
    Change-Id: Ic2a9e572099490f8810e03ab08336518f5672690

--- redfish-core/include/event_service_manager.hpp
+++ redfish-core/include/event_service_manager.hpp
@@ -1088,19 +1088,6 @@ class EventServiceManager
             }
         }
     }
-    void sendBroadcastMsg(const std::string& broadcastMsg)
-    {
-        for (const auto& it : this->subscriptionsMap)
-        {
-            std::shared_ptr<Subscription> entry = it.second;
-            nlohmann::json msgJson = {
-                {"Timestamp", crow::utility::dateTimeNow()},
-                {"OriginOfCondition", "/ibm/v1/HMC/BroadcastService"},
-                {"Name", "Broadcast Message"},
-                {"Message", broadcastMsg}};
-            entry->sendEvent(msgJson.dump());
-        }
-    }
 
 #ifndef BMCWEB_ENABLE_REDFISH_DBUS_LOG_ENTRIES
     void cacheLastEventTimestamp()
