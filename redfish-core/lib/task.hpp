5a5b2dce6380ebb9591991e6f5d6206f9b091a0b
Ed Tanous <ed@tanous.net>
2020-09-28 19:16:18 -0700

    Use std::array instead of char array
    Char arrays are outdated, and not needed in this case.
    
    Tested:
    No functional changes
    
    Signed-off-by: Ed Tanous <ed@tanous.net>
    Change-Id: Ibdcebf05880ac2697c9a30f5a86155a09ff7b3d8

--- redfish-core/lib/task.hpp
+++ redfish-core/lib/task.hpp
@@ -97,7 +97,7 @@ struct TaskData : std::enable_shared_from_this<TaskData>
 
   public:
     TaskData() = delete;
-    
+
     static std::shared_ptr<TaskData>& createTask(
         std::function<bool(boost::system::error_code,
                            sdbusplus::message::message&,
