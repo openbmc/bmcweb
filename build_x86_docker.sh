d7e91b79be1297fb5fb75417c272b0a93bcdd321
Manojkiran Eda <manojkiran.eda@gmail.com>
2020-10-08 11:58:36 +0530

    Migrate docker support to meson
    - bmcweb seems to have x86 docker support which was broken
      due to build system migration.
    
    - This commit would migrate the docker to leverage the meson
      build system.
    
    - There are a couple of changes that are needed to for meson :
    
    - Right now meson does not support the subprojects to be downloaded
      elsewhere other than subprojects directory, because of that we
      cannot delete the source directory in the bmcweb-base container as
      that affect the incremental builds.
    
    - Docker infrastructure had now does not by-default allow DNS lookups
      for the latest rpm-based distributions, because of which I need to
      add --network to the base container(so that apt update succeeds)
    
    Tested By:
    
    - Was able to successfully compile bmcweb & tests in a container and
      was able to get the build directory out of the container to the host
      machine post completion.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: Ica23bd3de22e262a27c91167181161fc459cda0d

--- build_x86_docker.sh
+++ build_x86_docker.sh
@@ -8,10 +8,8 @@ if ! command -v docker > /dev/null; then
 fi
 
 docker inspect bmcweb-base > /dev/null ||
-  docker build --no-cache --force-rm -t bmcweb-base -f Dockerfile.base .
+  docker build --network=host --no-cache --force-rm -t bmcweb-base -f Dockerfile.base .
 
 docker build -t bmcweb .
 
-mkdir -p build
-
-docker run -v "$PWD":/app -it bmcweb cp -r /build/bmcweb /build/webtest /app/build
+docker run -v "$PWD":/app -it bmcweb cp -rf /source/build/ /app/build
