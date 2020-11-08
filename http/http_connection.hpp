fb1492326bdad906fdd2c6f9e3380da7251565b0
Ravi Teja <raviteja28031990@gmail.com>
2020-11-08 11:02:38 -0600

    Add Dump offload support using http dynamic response
    This commit
    1.Enables dynamic response support
    2.Defines new bmcweb rule to support dump streaming.
    3.Implemented routes for bmc and system dump offload.
    
    Tested By:
    1. BMC dump offload
    2. System dump offload
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I3a072d255660ee8657253430b5fadbf9a7a26b8e

--- http/http_connection.hpp
+++ http/http_connection.hpp
@@ -349,6 +349,19 @@ class Connection :
                     res.completeRequestHandler = nullptr;
                     return;
                 }
+                if (boost::iequals(req->getHeaderValue(
+                                       boost::beast::http::field::content_type),
+                                   "application/octet-stream"))
+                {
+                    BMCWEB_LOG_DEBUG << "upgrade stream connection";
+                    handler->handleUpgrade(*req, res, std::move(adaptor));
+                    // delete lambda with self shared_ptr
+                    // to enable connection destruction
+                    res.completeRequestHandler = nullptr;
+
+                    return;
+                }
+
                 handler->handle(*req, res);
             }
             else
