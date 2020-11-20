6b9c0f1928364750fc1ca82f8d9d98dfa22d8be7
Gunnar Mills <gmills@us.ibm.com>
2020-11-20 14:26:04 -0600

    Look for Version Already Exists Error
    Look for Software.Version.Error.AlreadyExists and return Invalid Upload,
    Image version already exists if true.
    
    Heard from users it is hard to know why the image failed to upload
    in this case.
    
    Tested: Built with PDI and phosphor-bmc-code-mgmt changes and saw the
            error.
    
    curl -k -H "Content-Type: application/octet-stream" -X POST -T $image https://${bmc}/redfish/v1/UpdateService
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "/redfish/v1/$metadata#Message.v1_0_0.Message",
            "Message": "Invalid file uploaded to /redfish/v1/UpdateService: Image version already exists.",
            "MessageArgs": [
              "/redfish/v1/UpdateService",
              "Image version already exists"
            ],
            "MessageId": "OpenBMC.0.1.0.InvalidUpload",
    ...
    
    Change-Id: Ieab0116650dd64949da6b7ac93254193803f8f90
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -330,7 +330,7 @@ static void monitorForSoftwareAvailable(
                 return; // if this was our message, timeout will cover it
             }
             if (!boost::starts_with(*type,
-                                    "xyz.openbmc_project.Software.Image.Error"))
+                                    "xyz.openbmc_project.Software"))
             {
                 return;
             }
@@ -351,6 +351,12 @@ static void monitorForSoftwareAvailable(
             {
                 redfish::messages::invalidUpload(asyncResp->res, url,
                                                  "Invalid image format");
+            }
+            else if (*type == "xyz.openbmc_project.Software.Version.Error."
+                              "AlreadyExists")
+            {
+                redfish::messages::invalidUpload(
+                    asyncResp->res, url, "Image version already exists");
             }
             else if (*type ==
                      "xyz.openbmc_project.Software.Image.Error.BusyFailure")
