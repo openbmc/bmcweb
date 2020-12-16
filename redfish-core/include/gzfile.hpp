cfbc12ea1f60e273134ee2fa8f8a7d07f6766d53
SpencerKu <Spencer.Ku@quantatw.com>
2020-12-16 16:03:00 +0800

    Redfish: Support Host Log Entries
    This commit support to present host log entries through redfish.
    
    The host log entry contains host's console output information,
    such as boot logs or Linux kernel messages printed to the system console,
    and each entry will have an unique ID which is created by timestamp.
    
    RedfishServiceValidator results:
      /redfish/v1/Systems/system/LogServices/HostLogger
        pass: 4
        passGet: 1
        skipOptional: 9
    
      /redfish/v1/Systems/system/LogServices/HostLogger/Entries
        pass: 30
        passGet: 1
        skipOptional: 1
    
      /redfish/v1/Systems/system/LogServices/HostLogger/Entries/<str>
        pass: 8
        passGet: 1
        skipOptional: 14
    
    Sample Outputs:
      curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/HostLogger/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/HostLogger/Entries/",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of HostLogger Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/HostLogger/Entries/56",
          "@odata.type": "#LogEntry.v1_4_0.LogEntry",
          "Created": "1970-01-01T00:00:56+00:00",
          "EntryType": "Event",
          "Id": "56",
          "Message": "Host console log : >>> Log collection started at 1970-01-01 00:00:56\n ",
          "MessageArgs": [
            ">>> Log collection started at 1970-01-01 00:00:56\n"
          ],
          "MessageId": "OpenBMC.0.1.HostLogger",
          "Name": "HostLogger Entries",
          "Severity": "OK"
        },
      ...
      ],
      "Members@odata.count": 28,
      "Name": "HostLogger Entries"
    }
    
    Signed-off-by: SpencerKu <Spencer.Ku@quantatw.com>
    Change-Id: Ib9465b6fbf1ec4f8e6b4236e2eef7f675a958122

---
+++ redfish-core/include/gzfile.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <zlib.h>
+
+#include <array>
+#include <filesystem>
+
+class GzFile
+{
+  public:
+    GzFile(const std::string& filename) :
+        logStream(gzopen(filename.c_str(), "r"))
+    {
+        // If gzopen failed, set Z_NULL to logStream.
+        if (!logStream)
+        {
+            BMCWEB_LOG_ERROR << std::strerror(errno);
+            logStream = Z_NULL;
+        }
+    }
+
+    ~GzFile()
+    {
+        gzclose(logStream);
+    }
+
+    bool gzGetLine(std::string& buf)
+    {
+        constexpr int lineLimit = 8192;
+        // obmc-console define buffer size as uint8_t buf[4096] to catch one
+        // message(line) and phosphor-hostlogger will attach addtional
+        // infomation like timestamp, so we define 8192 as the buffer limit size
+        // to unzip a message.
+
+        if (!buf.empty())
+        {
+            buf.clear();
+        }
+        buf.resize(lineLimit);
+
+        // On failure, gzgets() shall return Z_NULL.
+        if (gzgets(logStream, buf.data(), static_cast<int>(buf.size())) ==
+            Z_NULL)
+        {
+            if (!gzeof(logStream))
+            {
+                int zErrnum = 0;
+                const char* errmsg = gzerror(logStream, &zErrnum);
+
+                BMCWEB_LOG_ERROR << "gzGetLine error:\n"
+                                 << "Msg: " << errmsg << '\n'
+                                 << "Errnum: " << zErrnum;
+            }
+            return false;
+        }
+        return true;
+    }
+
+  private:
+    gzFile logStream;
+
+  public:
+    GzFile(const GzFile&) = delete;
+    GzFile& operator=(const GzFile&) = delete;
+};
