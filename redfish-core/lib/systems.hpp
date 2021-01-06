7d92659b38ba5fa0a89db7001a1681e2dc2a9003
Andrew Geissler <geissonator@yahoo.com>
2021-01-06 13:48:57 -0600

    transition support for system state
    The following commit defined two new values for the host state:
    https://github.com/openbmc/phosphor-dbus-interfaces/commit/9f65dfeaa5ab22cae03db45c9916868da9864f83
    
    These new state values, TransitioningToOff and TransitioningToRunning,
    map quite well to the Redfish system PowerState values of PoweringOff
    and PoweringOn.
    
    There have been requests from external users of our Redfish interfaces
    to know this level of detail, especially in the PoweringOff path due to
    the length of time it can take (up to 2 hours is allowed for the host to
    shut itself down gracefully).
    
    Tested:
    - Put host state D-Bus property in each of new states and verified
      Redfish API returned expected results.
    
    Change-Id: I0c43dc2fa8b057beea48bc6f3dcde80d094ccfdb
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -648,6 +648,18 @@ inline void getHostState(const std::shared_ptr<AsyncResp>& aResp)
                     aResp->res.jsonValue["PowerState"] = "On";
                     aResp->res.jsonValue["Status"]["State"] = "InTest";
                 }
+                else if (*s == "xyz.openbmc_project.State.Host.HostState."
+                               "TransitioningToRunning")
+                {
+                    aResp->res.jsonValue["PowerState"] = "PoweringOn";
+                    aResp->res.jsonValue["Status"]["State"] = "Disabled";
+                }
+                else if (*s == "xyz.openbmc_project.State.Host.HostState."
+                               "TransitioningToOff")
+                {
+                    aResp->res.jsonValue["PowerState"] = "PoweringOff";
+                    aResp->res.jsonValue["Status"]["State"] = "Disabled";
+                }
                 else
                 {
                     aResp->res.jsonValue["PowerState"] = "Off";
