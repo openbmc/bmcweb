8f8308407ce4a370d0e76706b9bf868d6e586e16
Ed Tanous <edtanous@google.com>
2021-01-13 10:17:57 -0800

    Fix authorization rules
    Authorization seems to have some changes recently which are counter
    productive, and performance prohibitive.  This patchset aims to resolve
    some of the current issues with these.
    
    First off, at some point, code was checked in that changed the flow such
    that the authorization checks for websockets weren't enforced, although
    they were covered by an authentication check still.  This patchset
    resolves that, by adding the checks back.
    
    Second, it removes the async method call from the router, such that all
    requests no longer invoke an extra dbus call, which for some endpoints
    that can be completely responded to without dbus, should help the
    performance quite a bit.  It does this by re-implementing the role
    cache, which should be able to track role changes in real time, rather
    than relying on an explicit call.
    
    Third, now that the aformentioned is fixed nbd proxy no longer needs its
    own privilege mechanism, and can use the privilege mechanism in the core
    like all other endpoints do.  From the commit history, it wasn't clear
    why this was done initially, as the in-core checks should identical to
    the hardcoded checks, but because of the error on the first bullet
    point, it seems like it was done as a temporary stopgap.  The initial
    commit for this also mentions that it "caused some memory issues
    difficult to track down."  There were several memory issues in
    HttpConnection that have been resolved since then, and I suspect this
    was related, as with this change it appears to work as designed.
    
    This came up in a regular audit of endpoints for permission issues.
    
    Tested:
    Booted bmcweb and attempted to log in through the /login interface.
    Observed a 200 return code.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I57bff1ee2dbf4d542c16cd4206cb32ff01db8c57

--- src/webserver_main.cpp
+++ src/webserver_main.cpp
@@ -7,6 +7,7 @@
 #include <hostname_monitor.hpp>
 #include <ibm/management_console_rest.hpp>
 #include <image_upload.hpp>
+#include <include/sessions.hpp>
 #include <kvm_websocket.hpp>
 #include <login_routes.hpp>
 #include <obmc_console.hpp>
@@ -152,6 +153,9 @@ int main(int /*argc*/, char** /*argv*/)
     BMCWEB_LOG_INFO << "Start Hostname Monitor Service...";
     crow::hostname_monitor::registerHostnameSignal();
 #endif
+
+    // Initialize the role storage
+    persistent_data::UserRoleMap::getInstance();
 
     app.run();
     io->run();
