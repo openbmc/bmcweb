57ad2ec8567156e11c2bbdf8be82373b64b7aa55
Xiaochao Ma <maxiaochao@inspur.com>
2021-01-21 17:56:02 +0800

    Redfish: Implement new event log propety Resolved
    In the new Redfish specification 2020.4，the eventlog propety "resolved"
    is added.
    This attribute indicates whether the log is marked as "resolved".
    
    Tested:
    Befor marked the log as resolved:e marked the log as resolved:
    {
    "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/1",
    "@odata.type": "#LogEntry.v1_6_0.LogEntry",
    "Created": "1970-01-01T00:00:54+00:00",
    "EntryType": "Event",
    "Id": "1",
    "Message": "xyz.openbmc_project.Common.Device.Error.ReadFailure",
    "Modified": "1970-01-01T00:02:12+00:00",
    "Name": "System Event Log Entry",
    "Resolved": false,
    "Severity": "Critical"
    }
    after marked the log as resolved on webpage:
    {
    "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/1",
    "@odata.type": "#LogEntry.v1_6_0.LogEntry",
    "Created": "1970-01-01T00:00:54+00:00",
    "EntryType": "Event",
    "Id": "1",
    "Message": "xyz.openbmc_project.Common.Device.Error.ReadFailure",
    "Modified": "1970-01-01T00:02:12+00:00",
    "Name": "System Event Log Entry",
    "Resolved": true,
    "Severity": "Critical"
    }
    
    Signed-off-by: Xiaochao Ma <maxiaochao@inspur.com>
    Change-Id: Idd0cc0ca61ad56703303f90ba2bd1a372c321d94

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -1406,6 +1406,7 @@ class DBusEventLogEntryCollection : public Node
                         std::time_t updateTimestamp{};
                         std::string* severity = nullptr;
                         std::string* message = nullptr;
+                        bool resolved = false;
 
                         for (auto& propertyMap : interfaceMap.second)
                         {
@@ -1464,6 +1465,12 @@ class DBusEventLogEntryCollection : public Node
                                     messages::internalError(asyncResp->res);
                                 }
                             }
+                            else if (propertyMap.first == "Resolved")
+                            {
+                                bool* sscon =
+                                    std::get_if<bool>(&propertyMap.second);
+                                resolved = *sscon;
+                            }
                         }
                         thisEntry = {
                             {"@odata.type", "#LogEntry.v1_6_0.LogEntry"},
@@ -1474,6 +1481,7 @@ class DBusEventLogEntryCollection : public Node
                             {"Name", "System Event Log Entry"},
                             {"Id", std::to_string(*id)},
                             {"Message", *message},
+                            {"Resolved", resolved},
                             {"EntryType", "Event"},
                             {"Severity",
                              translateSeverityDbusToRedfish(*severity)},
@@ -1541,6 +1549,7 @@ class DBusEventLogEntry : public Node
                 std::time_t updateTimestamp{};
                 std::string* severity = nullptr;
                 std::string* message = nullptr;
+                bool resolved = false;
 
                 for (auto& propertyMap : resp)
                 {
@@ -1597,6 +1606,11 @@ class DBusEventLogEntry : public Node
                             messages::internalError(asyncResp->res);
                         }
                     }
+                    else if (propertyMap.first == "Resolved")
+                    {
+                        bool* sscon = std::get_if<bool>(&propertyMap.second);
+                        resolved = *sscon;
+                    }
                 }
                 if (id == nullptr || message == nullptr || severity == nullptr)
                 {
@@ -1611,6 +1625,7 @@ class DBusEventLogEntry : public Node
                     {"Name", "System Event Log Entry"},
                     {"Id", std::to_string(*id)},
                     {"Message", *message},
+                    {"Resolved", resolved},
                     {"EntryType", "Event"},
                     {"Severity", translateSeverityDbusToRedfish(*severity)},
                     {"Created", crow::utility::getDateTime(timestamp)},
