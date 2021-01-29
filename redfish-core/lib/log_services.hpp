118fdcfd91de30abc4c3f103042f37466b5bdfca
Adriana Kobylak <anoo@us.ibm.com>
2021-01-29 09:01:30 -0600

    log_services: Add download of log log entries
    Add a GET function for endpoint
    /redfish/v1/Systems/system/LogServices/EventLog/attachment/<str>
    which would read the File Path property of the specified entry,
    encode it as base64, and send it off. This allows the user to
    offload error logs for analysis and further parsing if needed.
    
    Tested: Ran Redfish validator and verified the contents of the
    offloaded file were the same as the file in the BMC, after
    decoding it with a base64 decoder:
    curl -k -H "X-Auth-Token: $token" https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/attachment/447 > /tmp/log
    
    Change-Id: Id9e2308ebedc70852a2ed62def107648f7e6fb7a
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -1719,6 +1719,82 @@ class DBusEventLogEntry : public Node
     }
 };
 
+class DBusEventLogEntryDownload : public Node
+{
+  public:
+    DBusEventLogEntryDownload(App& app) :
+        Node(
+            app,
+            "/redfish/v1/Systems/system/LogServices/EventLog/attachment/<str>/",
+            std::string())
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::put, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::post, {{"ConfigureManager"}}}};
+    }
+
+  private:
+    void doGet(crow::Response& res, const crow::Request&,
+               const std::vector<std::string>& params) override
+    {
+        std::shared_ptr<AsyncResp> asyncResp = std::make_shared<AsyncResp>(res);
+        if (params.size() != 1)
+        {
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        const std::string& entryID = params[0];
+
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec,
+                        const std::variant<std::string>& filePath) {
+                if (ec)
+                {
+                    BMCWEB_LOG_DEBUG << "DBUS response error " << ec;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+
+                const std::string* value = std::get_if<std::string>(&filePath);
+                if (value == nullptr)
+                {
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+
+                std::string entryPath = *value;
+                std::ifstream readFile(entryPath);
+                if (!readFile)
+                {
+                    BMCWEB_LOG_DEBUG << "Could not read: " << entryPath;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+
+                std::vector<char> data{std::istreambuf_iterator<char>(readFile),
+                                       std::istreambuf_iterator<char>()};
+                std::string strData(data.data(), data.size());
+                auto output = crow::utility::base64encode(strData);
+
+                asyncResp->res.addHeader("Content-Type", "text/plain");
+                asyncResp->res.addHeader("Content-Transfer-Encoding", "Base64");
+                std::string contentDispositionParam =
+                    "attachment; filename=\"" + entryPath + "\"";
+                asyncResp->res.addHeader("Content-Disposition",
+                                         contentDispositionParam);
+                asyncResp->res.body() = output;
+            },
+            "xyz.openbmc_project.Logging",
+            "/xyz/openbmc_project/logging/entry/" + entryID,
+            "org.freedesktop.DBus.Properties", "Get",
+            "xyz.openbmc_project.Common.FilePath", "Path");
+    }
+};
+
 class BMCLogServiceCollection : public Node
 {
   public:
