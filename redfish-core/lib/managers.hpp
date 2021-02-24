301639f9682917af1919d54f90afca0035dc6055
Ed Tanous <edtanous@google.com>
2021-02-24 14:46:46 -0800

    Make a loop not copy
    clang-tidy warned that we're taking a string by copy.  I'm not sure why
    it wasn't able to find this earlier, but alas, this is to make the build
    pass again.
    
    Tested:
    I don't have a system that implements this, so I cant functionally test,
    but this is a trivial change.  Clang-tidy now passes.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I333ae27a0d331c461c0aefc4f2c3dda60749e073

--- redfish-core/lib/managers.hpp
+++ redfish-core/lib/managers.hpp
@@ -571,14 +571,14 @@ inline void asyncPopulatePid(const std::string& connection,
                                 }
                                 auto& data = (*config)[propertyPair.first];
                                 data = nlohmann::json::array();
-                                for (std::string itemCopy : *inputs)
+                                for (const std::string& input : *inputs)
                                 {
                                     dbus::utility::escapePathForDbus(itemCopy);
                                     data.push_back(
                                         {{"@odata.id",
                                           "/redfish/v1/Managers/bmc#/Oem/"
                                           "OpenBmc/Fan/FanZones/" +
-                                              itemCopy}});
+                                              input}});
                                 }
                             }
                             // todo(james): may never happen, but this
