7c22e51bd44add96a614a82af6a524ac9d44713b
Xiaochao Ma <maxiaochao@inspur.com>
2021-03-02 15:53:13 +0800

    Add Redfish ThermalSubsystem schema in bmcweb
    The ThermalSubsystem is a new resource in Redfish version 2020.4.
    It is a root for fans and temperatures. Fans are a new schema.
    Temperature sensors will be part of the new ThermalMetrics schema.
    
    ThermalSubsystem can co-exist with the current Thermal resource.
    
    ThermalSubsystem is an improvement on the existing Thermal schema
    because
    1. It includes the latest properties like LocationIndicatorActive
    2. Fans and Temperatures were arrays in the old Thermal schema and
       this was cumbersome and could hit limits of JSON arrays
    3. Large amount of static data mixed with sensor readings, which
       hurt performance
    4. Inconsistent definitions of properties vs like Processor and
       Memory schemas
    
    In a future commits Fans and ThermalMetrics will be added soon.
    
    Reference:
    https://www.dmtf.org/sites/default/files/standards/documents/DSP0268_2020.4.pdf
    https://redfish.dmtf.org/schemas/v1/ThermalSubsystem.v1_0_0.json
    
    Test:
    1. Validator passed.
    2. doGet method:
    ~$ curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis/ThermalSubsystem
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/ThermalSubsystem",
      "@odata.type": "#ThermalSubsystem.v1_0_0.ThermalSubsystem",
      "Id": "chassis",
      "Name": "Thermal Subsystem for Chassis",
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      }
    }
    3. A bad chassis ID:
    ~$ curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassisSSBAD/ThermalSubsystem
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type Chassis named chassisSSBAD was not found.",
            "MessageArgs": [
              "Chassis",
              "chassisSSBAD"
            ],
            "MessageId": "Base.1.8.1.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.8.1.ResourceNotFound",
        "message": "The requested resource of type Chassis named chassisSSBAD was not found."
      }
    }
    
    Signed-off-by: Xiaochao Ma <maxiaochao@inspur.com>
    Change-Id: Ib19879f584304e5303f1a83d88bdd18c78a61633

---
+++ redfish-core/lib/thermal_subsystem.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <node.hpp>
+#include <utils/chassis_utils.hpp>
+#include <utils/json_utils.hpp>
+
+namespace redfish
+{
+class ThermalSubsystem : public Node
+{
+  public:
+    ThermalSubsystem(App& app) :
+        Node(app, "/redfish/v1/Chassis/<str>/ThermalSubsystem/", std::string())
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::put, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureComponents"}}},
+            {boost::beast::http::verb::post, {{"ConfigureComponents"}}}};
+    }
+
+  private:
+    void doGet(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+               const crow::Request&,
+               const std::vector<std::string>& params) override
+    {
+        if (params.size() != 1)
+        {
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        const std::string& chassisId = params[0];
+
+        auto getChassisPath =
+            [asyncResp,
+             chassisId](const std::optional<std::string>& chassisPath) {
+                if (!chassisPath)
+                {
+                    BMCWEB_LOG_ERROR << "Not a valid chassis ID" << chassisId;
+                    messages::resourceNotFound(asyncResp->res, "Chassis",
+                                               chassisId);
+                    return;
+                }
+                asyncResp->res.jsonValue = {
+                    {"@odata.type",
+                     "#ThermalSubsystem.v1_0_0.ThermalSubsystem"},
+                    {"Name", "Thermal Subsystem for Chassis"},
+                    {"Id", chassisId}};
+                asyncResp->res.jsonValue["@odata.id"] =
+                    "/redfish/v1/Chassis/" + chassisId + "/ThermalSubsystem";
+
+                asyncResp->res.jsonValue["Status"] = {{"State", "Enabled"},
+                                                      {"Health", "OK"}};
+            };
+        redfish::chassis_utils::getValidChassisID(asyncResp, chassisId,
+                                                  std::move(getChassisPath));
+    }
+};
+} // namespace redfish
