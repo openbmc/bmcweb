b849598ee357352556a0761e313e24d2d38e24ce
Xiaochao Ma <maxiaochao@inspur.com>
2021-03-02 15:53:13 +0800

    Add Redfish ThermalSubsystem schema in bmcweb
    The ThermalSubsystem is a new resource in Redfish version 2020.4.
    It is a root for fans and temperatures. Fans are a new schema.
    Temperature sensors will be part of the new ThermalMetrics schema.
    
    ThermalSubsystem can co-exist with the current Thermal resource.
    
    ThermalSubsystem is an improvement on the existing Thermal schema
    because
    1. It includes the latest properties like LocationIndicatorActive
    2. Fans and Temperatures were arrays in the old Thermal schema and
       this was cumbersome and could hit limits of JSON arrays
    3. Large amount of static data mixed with sensor readings, which
       hurt performance
    4. Inconsistent definitions of properties vs like Processor and
       Memory schemas
    
    In a future commits Fans and ThermalMetrics will be added soon.
    
    Reference:
    https://www.dmtf.org/sites/default/files/standards/documents/DSP0268_2020.4.pdf
    https://redfish.dmtf.org/schemas/v1/ThermalSubsystem.v1_0_0.json
    
    Test:
    1. Validator passed.
    2. doGet method:
    ~$ curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis/ThermalSubsystem
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/ThermalSubsystem",
      "@odata.type": "#ThermalSubsystem.v1_0_0.ThermalSubsystem",
      "Id": "chassis",
      "Name": "Thermal Subsystem for Chassis",
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      }
    }
    3. A bad chassis ID:
    ~$ curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassisSSBAD/ThermalSubsystem
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type Chassis named chassisSSBAD was not found.",
            "MessageArgs": [
              "Chassis",
              "chassisSSBAD"
            ],
            "MessageId": "Base.1.8.1.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.8.1.ResourceNotFound",
        "message": "The requested resource of type Chassis named chassisSSBAD was not found."
      }
    }
    
    Signed-off-by: Xiaochao Ma <maxiaochao@inspur.com>
    Change-Id: Ib19879f584304e5303f1a83d88bdd18c78a61633

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -42,6 +42,7 @@
 #include "../lib/task.hpp"
 #include "../lib/telemetry_service.hpp"
 #include "../lib/thermal.hpp"
+#include "../lib/thermal_subsystem.hpp"
 #include "../lib/update_service.hpp"
 #include "../lib/virtual_media.hpp"
 
@@ -70,6 +71,7 @@ class RedfishService
         requestRoutesSession(app);
         requestEthernetInterfacesRoutes(app);
         requestRoutesThermal(app);
+        requestRoutesThermalSubsystem(app);
         requestRoutesManagerCollection(app);
         requestRoutesManager(app);
         requestRoutesManagerResetAction(app);
