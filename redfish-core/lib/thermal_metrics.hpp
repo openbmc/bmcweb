80f0aae1b67aa7d31bce2a85e8e396678b0369df
zhanghch05 <zhanghch05@inspur.com>
2021-03-08 19:04:35 +0800

    Add Redfish ThermalMetrics schema in bmcweb
    The ThermalMetrics is a new resource in Redfish version 2020.4.
    It is a root for temperatures. It is a subset of ThermalSubsystem.
    The current value of each temperature sensor and the corresponding
    link are enumerated in this collection.
    
    Reference:
    https://www.dmtf.org/sites/default/files/standards/documents/DSP0268_2020.4.pdf
    https://redfish.dmtf.org/schemas/v1/ThermalMetrics.v1_0_0.json
    
    Test:
    1. Validator passed.
    2. doGet method:
    ~$  curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis/ThermalSubsystem/ThermalMetrics
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/ThermalSubsystem/ThermalMetrics",
      "@odata.type": "#ThermalMetrics.v1_0_0.ThermalMetrics",
      "Id": "ThermalMetrics",
      "Name": "Chassis Thermal Metrics",
      "TemperatureReadingsCelsius": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/psu_inlet",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/psu_inlet",
          "DeviceName": "psu_inlet",
          "Reading": 28.25
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/bmc_zone",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/bmc_zone",
          "DeviceName": "bmc_zone",
          "Reading": 30.375
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/outlet",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/outlet",
          "DeviceName": "outlet",
          "Reading": 31.75
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/ocp_zone",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/ocp_zone",
          "DeviceName": "ocp_zone",
          "Reading": 33.937
        }
      ]
    }
    
    3. A bad chassis ID:
    ~$  curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassisBAD/ThermalSubsystem/ThermalMetrics
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type ThermalMetrics named chassisBAD was not found.",
            "MessageArgs": [
              "ThermalMetrics",
              "chassisBAD"
            ],
            "MessageId": "Base.1.8.1.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.8.1.ResourceNotFound",
        "message": "The requested resource of type ThermalMetrics named chassisBAD was not found."
      }
    }
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: Ib4182e7dc6e204371636a33a391e8e2a58dad113

---
+++ redfish-core/lib/thermal_metrics.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "node.hpp"
+#include "sensors.hpp"
+
+namespace redfish
+{
+
+class ThermalMetrics : public Node
+{
+  public:
+    ThermalMetrics(App& app) :
+        Node((app),
+             "/redfish/v1/Chassis/<str>/ThermalSubsystem/ThermalMetrics/",
+             std::string())
+    {
+        entityPrivileges = {
+            {boost::beast::http::verb::get, {{"Login"}}},
+            {boost::beast::http::verb::head, {{"Login"}}},
+            {boost::beast::http::verb::patch, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::put, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::delete_, {{"ConfigureManager"}}},
+            {boost::beast::http::verb::post, {{"ConfigureManager"}}}};
+    }
+
+  private:
+    void doGet(crow::Response& res, const crow::Request&,
+               const std::vector<std::string>& params) override
+    {
+        std::shared_ptr<AsyncResp> asyncResp = std::make_shared<AsyncResp>(res);
+        if (params.size() != 1)
+        {
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        const std::string& chassisName = params[0];
+        auto sensorAsyncResp = std::make_shared<SensorsAsyncResp>(
+            asyncResp->res, chassisName,
+            sensors::dbus::paths.at(sensors::node::thermal),
+            sensors::node::thermal);
+
+        // TODO Need to get Chassis Redundancy information.
+        getThermalData(sensorAsyncResp);
+    }
+};
+
+} // namespace redfish
