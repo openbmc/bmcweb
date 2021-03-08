e9f8668ed5048b3e284899e2185ad65688e3a2bb
zhanghch05 <zhanghch05@inspur.com>
2021-03-08 19:04:35 +0800

    Implements ThermalMetrics schema
    The ThermalMetrics schema is a resource in Redfish version 2022.2[1].
    It is a child of ThermalSubsystem schema[2] and it represents the
    thermal metrics of a chassis. It contains an array of temperature
    readings.
    
    This commit implements the Get and Head methods of the Redfish
    ThermalMetrics schema and implemented the basic information of Get.
    
    [1] https://www.dmtf.org/sites/default/files/standards/documents/DSP0268_2022.2.pdf
    [2] https://redfish.dmtf.org/schemas/v1/ThermalMetrics.v1_0_1.json
    
    Test:
    1. Validator passed.
    2. doGet method:
    curl -k -H "X-Auth-Token: $token" -X GET
    https://${bmc}/redfish/v1/Chassis/chassis/ThermalSubsystem/
    ThermalMetrics
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/ThermalSubsystem/
                    ThermalMetrics",
      "@odata.type": "#ThermalMetrics.v1_0_1.ThermalMetrics",
      "Id": "ThermalMetrics",
      "Name": "Chassis Thermal Metrics",
      "TemperatureReadingsCelsius": []
    }
    
    3. A bad chassis ID:
    curl -k -H "X-Auth-Token: $token" -X GET
    https://${bmc}/redfish/v1/Chassis/chassisBAD/ThermalSubsystem/
    ThermalMetrics
    {
      "error": {
        "@Message.ExtendedInfo": [
        {
          "@odata.type": "#Message.v1_1_1.Message",
          "Message": "The requested resource of type Chassis named
                      'chassisBAD' was not found.",
          "MessageArgs": [
            "Chassis",
            "chassisBAD"
          ],
          "MessageId": "Base.1.13.0.ResourceNotFound",
          "MessageSeverity": "Critical",
          "Resolution": "Provide a valid resource identifier and resubmit
                         the request."
        }
      ],
      "code": "Base.1.13.0.ResourceNotFound",
      "message": "The requested resource of type Chassis named
                 'chassisBAD' was not found."
      }
    }
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: Ib4182e7dc6e204371636a33a391e8e2a58dad113

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -46,6 +46,7 @@
 #include "task.hpp"
 #include "telemetry_service.hpp"
 #include "thermal.hpp"
+#include "thermal_metrics.hpp"
 #include "thermal_subsystem.hpp"
 #include "trigger.hpp"
 #include "update_service.hpp"
@@ -82,6 +83,7 @@ class RedfishService
 #ifdef BMCWEB_NEW_POWERSUBSYSTEM_THERMALSUBSYSTEM
         requestRoutesEnvironmentMetrics(app);
         requestRoutesPowerSubsystem(app);
+        requestRoutesThermalMetrics(app);
         requestRoutesThermalSubsystem(app);
 #endif
         requestRoutesManagerCollection(app);
