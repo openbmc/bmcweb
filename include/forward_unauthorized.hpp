08f51de77c39034cb22a629fd915f15d9935c86d
Ed Tanous <edtanous@google.com>
2021-03-10 13:29:30 -0800

    Fix infinite redirect when webui isn't installed
    In the begining, bmcweb had its own webui checked in as source.  Largely
    conceived of clay, and built by someone that doesn't understand UI
    development (me), it was eventually superceeded by phosphor-webui.  When
    we did that, we created a bug where bmcweb was expecting a UI to always
    be installed, and when it wasn't resolved into an infinite recursive
    redirect as it tried to find the login page.
    
    This patchset fixes that, by adding a connection between the
    authorization class, and the webassets class, for bmcweb to detect at
    runtime whether or not the UI is installed, and change behavior in that
    case.
    
    Along the way, we got a circular #include, so some includes needed to be
    rearranged slightly.
    
    This patchset will change no behavior when the UI is installed.  Login
    failures will continue to redirect to /, to hit the login page.
    
    If the UI is not installed, and there is no / route, BMCWEB will return
    the plaintext UNAUTHORIZED if you attempt to open the webui from the
    browser without having a webui installed and without having credentials.
    
    Tested:
    Launched in a build without webui-vue, and observed "UNAUTHORIZED" when
    I connected through chrome.
    Also launched in a build with webui-vue installed with:
    IMAGE_INSTALL_append = "webui-vue"
    And loaded the webui in chrome, and logged in successfully.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Iac9b83ba9e80d434479685b082d547847cdfe309

---
+++ include/forward_unauthorized.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <http_request.hpp>
+#include <http_response.hpp>
+#include <http_utility.hpp>
+
+namespace forward_unauthorized
+{
+
+bool hasWebuiRoute = false;
+
+inline void sendUnauthorized(const crow::Request& req, crow::Response& res)
+{
+    // If it's a browser connecting, don't send the HTTP authenticate
+    // header, to avoid possible CSRF attacks with basic auth
+    if (http_helpers::requestPrefersHtml(req))
+    {
+        // If we have a webui installed, redirect to that login page
+        if (hasWebuiRoute)
+        {
+            res.result(boost::beast::http::status::temporary_redirect);
+            res.addHeader("Location",
+                          "/#/login?next=" + http_helpers::urlEncode(req.url));
+        }
+        else
+        {
+            // If we don't have a webui installed, just return a lame
+            // unauthorized body
+            res.result(boost::beast::http::status::unauthorized);
+            res.body() = "Unauthorized";
+        }
+    }
+    else
+    {
+        res.result(boost::beast::http::status::unauthorized);
+        // only send the WWW-authenticate header if this isn't a xhr
+        // from the browser.  most scripts,
+        if (req.getHeaderValue("User-Agent").empty())
+        {
+            res.addHeader("WWW-Authenticate", "Basic");
+        }
+    }
+}
+} // namespace forward_unauthorized
