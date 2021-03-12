d781b0cef299a6ff47df5faf54dd3e30582c545b
Sunny Srivastava <sunnsr25@in.ibm.com>
2021-03-12 10:39:16 -0600

    Implementation of FabricAdapter schema in bmcweb
    This commit implements FabricAdapter and FabricAdapter collection
    schema. This code assumes all FabricAdapters are under
    /redfish/v1/Systems/system like we do for Memory and Processors.
    
    The schema can be used to publish inventory properties for FRUs
    which can be modelled as Fabric adapters.
    
    As a current use case, this schema is required to link ports on
    fabric adapters back to the system.
    
    A FabricAdapter represents the physical fabric adapter capable of
    connecting to an interconnect fabric.
    Examples include but are not limited to Ethernet, NVMe over Fabrics,
    Gen-Z, and SAS fabric adapters.
    
    Tested: Manually tested on the system, Run Redfish validator. Found no
    error.
    
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters",
      "@odata.type": "#FabricAdapterCollection.FabricAdapterCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card3"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card4"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card8"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card10"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card11"
        }
      ],
      "Members@odata.count": 8,
      "Name": "Fabric Adapter Collection"
    }
    
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card11",
      "@odata.type": "#FabricAdapter.v1_0_0.FabricAdapter",
      "Id": "pcie_card11",
      "Name": "pcie_card11"
    }
    
    Signed-off-by: sunny srivastava <sunnsr25@in.ibm.com>
    Change-Id: I4d3bc31a6f0036c262c0e30481d0da4aaf59b5ab
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>

---
+++ redfish-core/lib/fabric_adapters.hpp
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <boost/system/error_code.hpp>
+#include <utils/collection.hpp>
+#include <utils/json_utils.hpp>
+
+#include <string>
+#include <vector>
+
+namespace redfish
+{
+
+inline void handleAdapterError(const boost::system::error_code& ec,
+                               const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                               const std::string& adapter)
+{
+    BMCWEB_LOG_ERROR << "DBus method call failed with error " << ec.value();
+
+    if (ec.value() == boost::system::errc::io_error)
+    {
+        messages::resourceNotFound(
+            aResp->res, "#FabricAdapter.v1_0_0.FabricAdapter", adapter);
+        return;
+    }
+
+    BMCWEB_LOG_ERROR << "DBus method call failed with error " << ec.value();
+    messages::internalError(aResp->res);
+}
+
+/**
+ * @brief Api to look for specific fabric adapter among
+ * all available Fabric adapters on a system.
+ *
+ * @param[in,out]   aResp       Async HTTP response.
+ * @param[in]       adapter     Fabric adapter to look for.
+ */
+inline void getAdapter(crow::App& app, const crow::Request& req,
+                       const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                       const std::string& systemName,
+                       const std::string& adapter)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    if (systemName != "system")
+    {
+        messages::resourceNotFound(aResp->res, "ComputerSystem", systemName);
+        return;
+    }
+    auto respHandler =
+        [adapter,
+         aResp](const boost::system::error_code& ec,
+                const dbus::utility::MapperGetSubTreeResponse& subtree) {
+        if (ec)
+        {
+            handleAdapterError(ec, aResp, adapter);
+            return;
+        }
+        for (const auto& [objectPath, serviceMap] : subtree)
+        {
+            std::string adapterId =
+                sdbusplus::message::object_path(objectPath).filename();
+
+            if (adapterId.empty())
+            {
+                BMCWEB_LOG_ERROR << "Failed to find / in adapter path";
+                messages::internalError(aResp->res);
+                return;
+            }
+
+            if (adapterId != adapter)
+            {
+                // this is not the adapter we are interested in
+                continue;
+            }
+            aResp->res.jsonValue["@odata.type"] =
+                "#FabricAdapter.v1_0_0.FabricAdapter";
+            aResp->res.jsonValue["@odata.id"] =
+                "/redfish/v1/Systems/system/FabricAdapters/" + adapter;
+            aResp->res.jsonValue["Id"] = adapterId;
+            aResp->res.jsonValue["Name"] = "FabricAdapter";
+
+            return;
+        }
+        BMCWEB_LOG_ERROR << "Adapter not found";
+        messages::resourceNotFound(aResp->res, "FabricAdapter", adapter);
+    };
+
+    std::vector<std::string> interfaces = {
+        "xyz.openbmc_project.Inventory.Item.FabricAdapter"};
+    dbus::utility::getSubTree("/xyz/openbmc_project/inventory", interfaces,
+                              std::move(respHandler));
+}
+
+inline void handleFabricAdapterCollectionGet(
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+    const std::string& systemName)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+    if (systemName != "system")
+    {
+        messages::resourceNotFound(aResp->res, "ComputerSystem", systemName);
+        return;
+    }
+    aResp->res.jsonValue["@odata.type"] =
+        "#FabricAdapterCollection.FabricAdapterCollection";
+    aResp->res.jsonValue["Name"] = "Fabric Adapter Collection";
+    aResp->res.jsonValue["@odata.id"] =
+        "/redfish/v1/Systems/system/FabricAdapters";
+
+    constexpr std::array<std::string_view, 1> interfaces{
+        "xyz.openbmc_project.Inventory.Item.FabricAdapter"};
+    collection_util::getCollectionMembers(
+        aResp, boost::urls::url("/redfish/v1/Systems/system/FabricAdapters"),
+        interfaces);
+}
+
+inline void requestRoutesFabricAdapterCollection(App& app)
+{
+    /**
+     * Functions triggers appropriate requests on DBus
+     */
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/")
+        .privileges(redfish::privileges::getFabricAdapterCollection)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handleFabricAdapterCollectionGet, std::ref(app)));
+}
+
+/**
+ * Systems derived class for delivering Fabric adapter Schema.
+ */
+inline void requestRoutesFabricAdapters(App& app)
+{
+    /**
+     * Functions triggers appropriate requests on DBus
+     */
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/<str>/")
+        .privileges(redfish::privileges::getFabricAdapter)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(getAdapter, std::ref(app)));
+}
+} // namespace redfish
