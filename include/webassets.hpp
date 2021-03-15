fbcc252da12a6e614588426d5e61f0b1948859b4
Gunnar Mills <gmills@us.ibm.com>
2021-03-15 12:29:30 -0500

    Remove duplicate entry in array
    json was already in the array.
    
    Tested: GUI worked in the top commit.
    Change-Id: Iac97801489e1bd03a91a613e7b33a777867bd165
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- include/webassets.hpp
+++ include/webassets.hpp
@@ -45,7 +45,6 @@ inline void requestRoutes(App& app)
              {".json", "application/json"},
              {".jpg", "image/jpeg"},
              {".jpeg", "image/jpeg"},
-             {".json", "application/json"},
              // dev tools don't care about map type, setting to json causes
              // browser to show as text
              // https://stackoverflow.com/questions/19911929/what-mime-type-should-i-use-for-javascript-source-map-files
