2d25db48cef9fdfaa22985575633631d59695470
Ed Tanous <edtanous@google.com>
2021-03-24 08:44:51 -0700

    Move requestRoutes to asyncResp
    res.end() should be avoided where possible;  This commit changes
    requestRoutes to remove the res.end() that's been present there for a
    long time.
    
    Tested:
    curl -vvvv --insecure --user root:0penBmc https://192.168.7.2/redfish/v1
    returns 200
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I798783d320cfb7bc7973a8d2c02c4a25906a3859

--- include/redfish_v1.hpp
+++ include/redfish_v1.hpp
@@ -9,8 +9,8 @@ inline void requestRoutes(App& app)
     BMCWEB_ROUTE(app, "/redfish/")
         .methods(boost::beast::http::verb::get)(
             [](const crow::Request&, crow::Response& res) {
+                auto asyncResp = std::make_shared<AsyncResp>(res);
                 res.jsonValue = {{"v1", "/redfish/v1/"}};
-                res.end();
             });
 }
 } // namespace redfish
