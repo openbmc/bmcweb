06b9efd26157d6600f436036dbe73d4d265992b3
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2021-04-13 13:00:18 +0000

    Add support for MetricDefinition scheme
    Added MetricDefinition node to Redfish code. Now user is able to list
    all available metrics in OpenBMC that are supported by Telemetry
    service. Metrics are grouped by following categories: temperature,
    power, voltage, current, fan_tach, fan_pwm, utilization.
    
    Tested:
     - MetricDefinitions response is filled with existing sensors, it works
       with and without Telemetry service
     - Validated a presence of MetricDefinition members and its attributes
     - Successfully passed RedfishServiceValidator.py using witherspoon
       image on QEMU
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: I3086e1302e1ba2e5442d1367939fd5507a0cbc00

--- redfish-core/lib/telemetry_service.hpp
+++ redfish-core/lib/telemetry_service.hpp
@@ -28,6 +28,8 @@ inline void requestRoutesTelemetryService(App& app)
                 "/redfish/v1/TelemetryService/MetricReportDefinitions";
             asyncResp->res.jsonValue["MetricReports"]["@odata.id"] =
                 "/redfish/v1/TelemetryService/MetricReports";
+            asyncResp->res.jsonValue["MetricDefinitions"]["@odata.id"] =
+                "/redfish/v1/TelemetryService/MetricDefinitions";
 
             crow::connections::systemBus->async_method_call(
                 [asyncResp](
