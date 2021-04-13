a657fa1195f6c860dc9ac7c15be770239cac108b
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2021-04-13 13:00:18 +0000

    Add support for MetricDefinition scheme
    Added MetricDefinition node to Redfish code. Now user is able to list
    all available metrics in OpenBMC that are supported by Telemetry
    service. Metrics are grouped by reading type.
    
    MetricDefinitions contains all physical sensors supported by redfish,
    algorithm iterates through all chassis and collects results for each
    node available in that chassis (Power, Thermal, Sensors).
    
    When BMCWEB_NEW_POWERSUBSYSTEM_THERMALSUBSYSTEM will be enabled by
    default (meson option redfish-new-powersubsystem-thermalsubsystem) it
    will be possible to optimize this algorithm to only get sensors from
    Sensors node. Currently Sensors node doesn't contain all available
    sensors.
    
    Tested:
     - MetricDefinitions response is filled with existing sensors, it works
       with and without Telemetry service
     - Validated a presence of MetricDefinition members and its attributes
     - Successfully passed RedfishServiceValidator.py using witherspoon
       image on QEMU
     - Tested using following GET,POST requests
    
    GET /redfish/v1/TelemetryService/MetricDefinitions
    {
      "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions",
      "@odata.type": "#MetricDefinitionCollection.MetricDefinitionCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/Fan_Pwm"
        },
        {
          "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/Fan_Tach"
        },
        {
          "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/HostCpuUtilization"
        },
        {
          "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/HostMemoryBandwidthUtilization"
        },
        {
          "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/HostPciBandwidthUtilization"
        },
        {
          "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/Inlet_BRD_Temp"
        },
        {
          "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/Left_Rear_Board_Temp"
        }
      ],
      "Members@odata.count": 7,
      "Name": "Metric Definition Collection"
    }
    
    GET /redfish/v1/TelemetryService/MetricDefinitions/Fan_Tach
    {
      "@odata.id": "/redfish/v1/TelemetryService/MetricDefinitions/Fan_Tach",
      "@odata.type": "#MetricDefinition.v1_0_3.MetricDefinition",
      "Id": "Fan_Tach",
      "IsLinear": true,
      "MaxReadingRange": 25000.0,
      "MetricDataType": "Decimal",
      "MetricProperties": [
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/0/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/1/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/2/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/3/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/4/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/5/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/6/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/7/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/8/Reading",
        "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/9/Reading"
      ],
      "MetricType": "Gauge",
      "MinReadingRange": 0.0,
      "Name": "Fan_Tach",
      "Units": "RPM"
    }
    
    POST redfish/v1/TelemetryService/MetricReportDefinitions, body:
    {
      "Id": "TestReport",
      "Metrics": [
        {
          "MetricId": "TestMetric",
          "MetricProperties": [
            "/redfish/v1/Chassis/Chassis0/Thermal#/Fans/3/Reading",
          ]
        }
      ],
      "MetricReportDefinitionType": "OnRequest",
      "ReportActions": [
        "RedfishEvent",
        "LogToMetricReportsCollection"
      ]
    }
    {
      "@Message.ExtendedInfo": [
        {
          "@odata.type": "#Message.v1_1_1.Message",
          "Message": "The resource has been created successfully",
          "MessageArgs": [],
          "MessageId": "Base.1.8.1.Created",
          "MessageSeverity": "OK",
          "Resolution": "None"
        }
      ]
    }
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: I3086e1302e1ba2e5442d1367939fd5507a0cbc00

---
+++ redfish-core/include/utils/get_chassis_names.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <include/dbus_singleton.hpp>
+
+#include <array>
+#include <string>
+#include <vector>
+
+namespace redfish
+{
+
+namespace utils
+{
+
+template <typename F>
+inline void getChassisNames(F&& cb)
+{
+    const std::array<const char*, 2> interfaces = {
+        "xyz.openbmc_project.Inventory.Item.Board",
+        "xyz.openbmc_project.Inventory.Item.Chassis"};
+
+    crow::connections::systemBus->async_method_call(
+        [callback = std::move(cb)](const boost::system::error_code ec,
+                                   const std::vector<std::string>& chassis) {
+            std::vector<std::string> chassisNames;
+
+            if (ec)
+            {
+                callback(ec, chassisNames);
+                return;
+            }
+
+            chassisNames.reserve(chassis.size());
+            for (const std::string& path : chassis)
+            {
+                sdbusplus::message::object_path dbusPath = path;
+                std::string name = dbusPath.filename();
+                if (name.empty())
+                {
+                    callback(boost::system::errc::make_error_code(
+                                 boost::system::errc::invalid_argument),
+                             chassisNames);
+                    return;
+                }
+                chassisNames.emplace_back(std::move(name));
+            }
+
+            callback(ec, chassisNames);
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths",
+        "/xyz/openbmc_project/inventory", 0, interfaces);
+}
+
+} // namespace utils
+
+} // namespace redfish
