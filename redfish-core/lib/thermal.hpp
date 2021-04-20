09af2b07a881a0bc6884a860f9fa4484d9a9ec6d
Bruce Lee <Bruce_Lee@quantatw.com>
2021-04-20 16:23:50 +0800

    Remove checkAndDoSensorsOverride funciton
    1. Move Intel special-mode function to dbus-sensors.
    2. Add an OEM action to change fan Mode to auto/manual mode.
    
    Tested:
    1. Set fan mode to manual:
       curl -k -H "X-Auth-Token: $token" -X POST -d '{"FanMode": "Manual"}' https://${IP}/redfish/v1/Chassis/${Project}/Thermal
    2. Set fan mode to auto:
       curl -k -u "X-Auth-Token: $token" -X POST -d '{"FanMode": "Auto"}' https://${IP}/redfish/v1/Chassis/${Projec}/Thermal | python -m json.tool
    
    Signed-off-by: Bruce Lee <Bruce_Lee@quantatw.com>
    Change-Id: I74356f2b65e41cc0e9d8947c160f313334b78331

--- redfish-core/lib/thermal.hpp
+++ redfish-core/lib/thermal.hpp
@@ -100,7 +100,98 @@ class Thermal : public Node
             allCollections.emplace("Fans", *std::move(fanCollections));
         }
 
-        checkAndDoSensorsOverride(sensorsAsyncResp, allCollections);
+        setSensorsOverride(sensorsAsyncResp, allCollections);
+    }
+    void doPost(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                const crow::Request& req,
+                const std::vector<std::string>& params) override
+    {
+        if (params.size() != 1)
+        {
+
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        const std::string& chassisName = params[0];
+        auto sensorsAsyncResp = std::make_shared<SensorsAsyncResp>(
+            asyncResp, chassisName,
+            sensors::dbus::paths.at(sensors::node::thermal),
+            sensors::node::thermal);
+        std::string fanMode;
+        if (!json_util::readJson(req, sensorsAsyncResp->asyncResp->res, "FanMode", fanMode))
+        {
+            messages::actionParameterUnknown(sensorsAsyncResp->asyncResp->res, "FanMode", "");
+            return;
+        }
+
+        bool munualMode;
+        if (fanMode == "Manual")
+        {
+            munualMode = true;
+        }
+        else if (fanMode == "Auto")
+        {
+            munualMode = false;
+        }
+        else
+        {
+            messages::actionParameterValueFormatError(asyncResp->res, "Manual", "Auto",
+                                                      "");
+            return;
+        }
+
+        crow::connections::systemBus->async_method_call(
+            [asyncResp{std::move(asyncResp)}, munualMode](
+                const boost::system::error_code ec,
+                const std::vector<std::pair<
+                    std::string, std::vector<std::pair<
+                                     std::string, std::vector<std::string>>>>>&
+                    subtree) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR << "fanctrl GetSubTree failed "
+                                     << " ec = ( " << ec << " )\n";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+
+                for (const auto& object : subtree)
+                {
+                    auto iter = object.first.rfind('/');
+                    if ((iter != std::string::npos) &&
+                        (iter < object.first.size()))
+                    {
+                        std::string objName = object.first.substr(iter + 1);
+                        crow::connections::systemBus->async_method_call(
+                            [asyncResp, objName,
+                             munualMode](const boost::system::error_code ec) {
+                                if (ec)
+                                {
+                                    BMCWEB_LOG_ERROR
+                                        << "Updated the Mode failed " << objName
+                                        << ": " << munualMode << " ec = ( "
+                                        << ec << " )\n";
+                                    messages::internalError(asyncResp->res);
+                                    return;
+                                }
+                            },
+                            "xyz.openbmc_project.State.FanCtrl",
+                            "/xyz/openbmc_project/settings/fanctrl/" + objName,
+                            "org.freedesktop.DBus.Properties", "Set",
+                            "xyz.openbmc_project.Control.Mode", "Manual",
+                            std::variant<bool>(munualMode));
+                        BMCWEB_LOG_DEBUG << "Updated the Mode success "
+                                         << objName << ": " << munualMode;
+                    }
+                }
+                messages::success(asyncResp->res);
+            },
+            "xyz.openbmc_project.ObjectMapper",
+            "/xyz/openbmc_project/object_mapper",
+            "xyz.openbmc_project.ObjectMapper", "GetSubTree",
+            "/xyz/openbmc_project/settings/fanctrl", 1,
+            std::array<const char*, 1>{"xyz.openbmc_project.Control.Mode"});
     }
 };
 
