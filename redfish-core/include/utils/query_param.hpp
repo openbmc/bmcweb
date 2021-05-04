f654c681f121d6478ec21f69359d4d2268345afe
zhanghch05 <zhanghch05@inspur.com>
2021-05-04 08:24:41 +0800

    Redfish: Query parameters: Only
    Add the query parameter "only" for redfish.
    The specification is based on DSP0266_1.8.0.
    
    When the query parameter carries only, it will check whether
    the requested resource has only one member.
    If it matches, The response will be replaced with the content
    of the only member.
    
    Tested:
    Validator passes
    1.Use the curl commond:
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems"
    {
    "@odata.id": "/redfish/v1/Systems",
    "@odata.type": "#ComputerSystemCollection.ComputerSystemCollection",
    "Members": [
    {
    "@odata.id": "/redfish/v1/Systems/system"
    }
    ],
    "Members@odata.count": 1,
    "Name": "Computer System Collection",
    "ProtocolFeaturesSupported": {
    "OnlyMemberQuery": true
    }
    }
    
    2.Use the curl commond:
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems?only"
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_13_0.ComputerSystem",
      "Actions": {
        "#ComputerSystem.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Systems/system/ResetActionInfo",
          "target": "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset"
        }
      },
      "Bios": {
        "@odata.id": "/redfish/v1/Systems/system/Bios"
      },
      "Boot": {
        "AutomaticRetryAttempts": 3,
        "AutomaticRetryConfig": "RetryAttempts",
        "AutomaticRetryConfig@Redfish.AllowableValues": [
          "Disabled",
          "RetryAttempts"
        ],
        "BootSourceOverrideEnabled": "Disabled",
        "BootSourceOverrideMode": "Legacy",
        "BootSourceOverrideTarget": "None",
        "BootSourceOverrideTarget@Redfish.AllowableValues": [
          "None",
          "Pxe",
          "Hdd",
          "Cd",
          "Diags",
          "BiosSetup",
          "Usb"
        ],
        "RemainingAutomaticRetryAttempts": 3
      },
      "BootProgress": {
        "LastState": "None"
      },
      "Description": "Computer System",
      "Id": "system",
      "LastResetTime": "1970-01-01T00:00:00+00:00",
      "Links": {
        "Chassis": [
          {
            "@odata.id": "/redfish/v1/Chassis/chassis"
          }
        ],
        "ManagedBy": [
          {
            "@odata.id": "/redfish/v1/Managers/bmc"
          }
        ]
      },
      "LogServices": {
        "@odata.id": "/redfish/v1/Systems/system/LogServices"
      },
      "Memory": {
        "@odata.id": "/redfish/v1/Systems/system/Memory"
      },
      "MemorySummary": {
        "Status": {
          "Health": "OK",
          "HealthRollup": "OK",
          "State": "Disabled"
        },
        "TotalSystemMemoryGiB": 0
      },
      "Name": "system",
      "PowerRestorePolicy": "AlwaysOff",
      "PowerState": "Off",
      "ProcessorSummary": {
        "Count": 0,
        "Status": {
          "Health": "OK",
          "HealthRollup": "OK",
          "State": "Disabled"
        }
      },
      "Processors": {
        "@odata.id": "/redfish/v1/Systems/system/Processors"
      },
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "Disabled"
      },
      "Storage": {
        "@odata.id": "/redfish/v1/Systems/system/Storage"
      },
      "SystemType": "Physical"
    }
    
    3.Other urls are the same as above.
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: I29f63e0b2231a9362b6d6ca65af3f820f8bc3c27

---
+++ redfish-core/include/utils/query_param.hpp
@@ -0,0 +1,100 @@
+#pragma once
+#include "error_messages.hpp"
+#include "http_request.hpp"
+
+#include <systemd/sd-journal.h>
+
+#include <string_view>
+#include <vector>
+namespace redfish
+{
+namespace query_param
+{
+template <typename Handler>
+class ProcessParam
+{
+  public:
+    ProcessParam(Handler* handlerIn) : handler(handlerIn)
+    {}
+    ~ProcessParam() = default;
+
+    bool processAllParam(crow::Request& req, crow::Response& res)
+    {
+        if (!checkParameters(req, res))
+        {
+            return false;
+        }
+        if (isOnly)
+        {
+            if (processOnly(req, res))
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+  protected:
+    bool checkParameters(crow::Request& req, crow::Response& res)
+    {
+        isOnly = false;
+        auto it = req.urlParams.find("only");
+        if (it != req.urlParams.end())
+        {
+            if (req.urlParams.size() != 1)
+            {
+                res.jsonValue.clear();
+                messages::queryCombinationInvalid(res);
+                return false;
+            }
+            if (!it->encoded_value().empty())
+            {
+                res.jsonValue.clear();
+                messages::queryParameterValueFormatError(
+                    res, it->encoded_value(), it->encoded_key());
+                return false;
+            }
+            isOnly = true;
+            return true;
+        }
+        return false;
+    }
+
+    bool processOnly(crow::Request& req, crow::Response& res)
+    {
+        auto itMembers = res.jsonValue.find("Members");
+        if (itMembers == res.jsonValue.end())
+        {
+            BMCWEB_LOG_DEBUG << "No found Members.";
+            return false;
+        }
+
+        if (itMembers->size() != 1)
+        {
+            BMCWEB_LOG_DEBUG << "Members contains " << itMembers->size()
+                             << " element, returning full collection.";
+            return false;
+        }
+        auto itMemBegin = itMembers->begin();
+        auto itUrl = itMemBegin->find("@odata.id");
+        if (itUrl == itMemBegin->end())
+        {
+            BMCWEB_LOG_DEBUG << "No found odata.id";
+            return false;
+        }
+        const std::string url = itUrl->get<const std::string>();
+        req.setUrl(url);
+        res.clearCompleted();
+        res.jsonValue.clear();
+        auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+        handler->handle(req, asyncResp);
+        return true;
+    }
+
+  private:
+    Handler* handler;
+    bool isOnly = false;
+};
+} // namespace query_param
+} // namespace redfish
