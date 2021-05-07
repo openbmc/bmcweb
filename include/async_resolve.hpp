1766915d49f050ff4feca71cadd0de2036abca6f
zhanghch05 <zhanghch05@inspur.com>
2021-05-07 17:14:59 +0800

    WIP: Redfish: Query parameters: Only
    Add the query parameter "only" for redfish.
    The specification is based on DSP0266_1.8.0.
    
    This commit is inspired by the commit that carries the same title, but
    is largely unique, namely, in that it adds the core feature to be able
    to recall handle with a new Response object, and make sure the result
    gets to the connection.  It does this by swapping the handlers and
    implementing move semantics on the Response object.  It definitely needs
    broken up into a few smaller patches, but it does pass the below tests
    without any aparent seg faults or ownership issues.
    
    It implements a number of cleanups that deserve their own patches, and
    will be split up accordingly, but for the moment, I think this is a good
    start to getting filter and expand support in the future.
    
    Tested:
    Validator passes
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems"
    
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems?only"
    
    Change-Id: I123d8ab8bcd88a0b63ff131f6b98548951989755

--- include/async_resolve.hpp
+++ include/async_resolve.hpp
@@ -13,6 +13,44 @@ namespace crow
 namespace async_resolve
 {
 
+inline bool endpointFromResolveTuple(
+    const std::tuple<int32_t, int32_t, std::vector<uint8_t>>& resolveList,
+    const std::string_view port, boost::asio::ip::tcp::endpoint& endpoint)
+{
+    std::vector<uint8_t> ipAddress = std::get<2>(resolveList);
+    if (ipAddress.size() == 4) // ipv4 address
+    {
+        BMCWEB_LOG_DEBUG << "ipv4 address";
+        boost::asio::ip::address_v4 ipv4Addr(
+            {ipAddress[0], ipAddress[1], ipAddress[2], ipAddress[3]});
+        endpoint.address(ipv4Addr);
+    }
+    else if (ipAddress.size() == 16) // ipv6 address
+    {
+        BMCWEB_LOG_DEBUG << "ipv6 address";
+        boost::asio::ip::address_v6 ipv6Addr(
+            {ipAddress[0], ipAddress[1], ipAddress[2], ipAddress[3],
+             ipAddress[4], ipAddress[5], ipAddress[6], ipAddress[7],
+             ipAddress[8], ipAddress[9], ipAddress[10], ipAddress[11],
+             ipAddress[12], ipAddress[13], ipAddress[14], ipAddress[15]});
+        endpoint.address(ipv6Addr);
+    }
+    else
+    {
+        BMCWEB_LOG_ERROR << "Resolve failed to fetch the IP address";
+        return false;
+    }
+    uint16_t portNum = 0;
+    auto it = std::from_chars(port.data(), port.data() + port.size(), portNum);
+    if (it.ec != std::errc())
+    {
+        BMCWEB_LOG_ERROR << "Failed to get the Port";
+        return false;
+    }
+    endpoint.port(portNum);
+    return true;
+}
+
 class Resolver
 {
   public:
@@ -27,62 +65,32 @@ class Resolver
         BMCWEB_LOG_DEBUG << "Trying to resolve: " << host << ":" << port;
         uint64_t flag = 0;
         crow::connections::systemBus->async_method_call(
-            [host, port, handler{std::move(handler)}](
+            [port, handler{std::move(handler)}](
                 const boost::system::error_code ec,
                 const std::vector<
                     std::tuple<int32_t, int32_t, std::vector<uint8_t>>>& resp,
                 const std::string& hostName, const uint64_t flagNum) {
-                std::vector<boost::asio::ip::tcp::endpoint> endpointList;
                 if (ec)
                 {
                     BMCWEB_LOG_ERROR << "Resolve failed: " << ec.message();
-                    handler(ec, endpointList);
+                    handler(ec, {});
                     return;
                 }
                 BMCWEB_LOG_DEBUG << "ResolveHostname returned: " << hostName
                                  << ":" << flagNum;
                 // Extract the IP address from the response
-                for (auto resolveList : resp)
+                std::vector<boost::asio::ip::tcp::endpoint> endpointList;
+
+                for (const std::tuple<int32_t, int32_t, std::vector<uint8_t>>&
+                         resolveList : resp)
                 {
-                    std::vector<uint8_t> ipAddress = std::get<2>(resolveList);
                     boost::asio::ip::tcp::endpoint endpoint;
-                    if (ipAddress.size() == 4) // ipv4 address
+                    if (!endpointFromResolveTuple(resolveList, port, endpoint))
                     {
-                        BMCWEB_LOG_DEBUG << "ipv4 address";
-                        boost::asio::ip::address_v4 ipv4Addr(
-                            {ipAddress[0], ipAddress[1], ipAddress[2],
-                             ipAddress[3]});
-                        endpoint.address(ipv4Addr);
+                        boost::system::error_code ecErr = make_error_code(
+                            boost::system::errc::address_not_available);
+                        handler(ecErr, {});
                     }
-                    else if (ipAddress.size() == 16) // ipv6 address
-                    {
-                        BMCWEB_LOG_DEBUG << "ipv6 address";
-                        boost::asio::ip::address_v6 ipv6Addr(
-                            {ipAddress[0], ipAddress[1], ipAddress[2],
-                             ipAddress[3], ipAddress[4], ipAddress[5],
-                             ipAddress[6], ipAddress[7], ipAddress[8],
-                             ipAddress[9], ipAddress[10], ipAddress[11],
-                             ipAddress[12], ipAddress[13], ipAddress[14],
-                             ipAddress[15]});
-                        endpoint.address(ipv6Addr);
-                    }
-                    else
-                    {
-                        BMCWEB_LOG_ERROR
-                            << "Resolve failed to fetch the IP address";
-                        handler(ec, endpointList);
-                        return;
-                    }
-                    uint16_t portNum;
-                    auto it = std::from_chars(
-                        port.data(), port.data() + port.size(), portNum);
-                    if (it.ec != std::errc())
-                    {
-                        BMCWEB_LOG_ERROR << "Failed to get the Port";
-                        handler(ec, endpointList);
-                        return;
-                    }
-                    endpoint.port(portNum);
                     BMCWEB_LOG_DEBUG << "resolved endpoint is : " << endpoint;
                     endpointList.push_back(endpoint);
                 }
