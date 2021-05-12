458d15216b26b515d2e8074d8d8b62061c8650a9
zhanghch05 <zhanghch05@inspur.com>
2021-05-12 15:20:36 +0800

    Redfish: Query parameters: expand
    Add the query parameter "expand" for redfish.
    The specification is based on DSP0266_1.8.0.
    
    Returns a hyperlink and its contents in-line with retrieved
    resources, as if a GET call response was included in-line with
    that hyperlink.
    
    asterisk(*):Shall expand all hyperlinks, including those in
    payload annotations, such as @Redfish.Settings,
    @Redfish.ActionInfo, and @Redfish.CollectionCapabilities.
    Example: https://resource?$expand=*
    
    period(.):Shall expand all hyperlinks not in any links
    property instances of the resource, including those in
    payload annotations, such as @Redfish.Settings,
    @Redfish.ActionInfo, and @Redfish.CollectionCapabilities.
    Example: https://resource?$expand=.
    
    tilde(~):Shall expand all hyperlinks found in all links
    property instances of the resource.
    Example: https://resource?$expand=~
    
    $levels:Number of levels the service should cascade the
    $expand operation.The default level shall be 1.
    Example: https://resource?$expand=*($levels=1)
    
    Tested:
    1.Validator passes
    2.Use the curl commond:
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems"
    {
    "@odata.id": "/redfish/v1/Systems",
    "@odata.type": "#ComputerSystemCollection.ComputerSystemCollection",
    "Members": [
    {
    "@odata.id": "/redfish/v1/Systems/system"
    }
    ],
    "Members@odata.count": 1,
    "Name": "Computer System Collection"
    }
    
    3.Add expand
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems?$expand=*(levels=1)"
    {
    "@odata.id": "/redfish/v1/Systems",
    "@odata.type": "#ComputerSystemCollection.ComputerSystemCollection",
    "Members": [
    {
    "@odata.id": "/redfish/v1/Systems/system",
    "@odata.type": "#ComputerSystem.v1_13_0.ComputerSystem",
    "Actions": {
    "#ComputerSystem.Reset": {
    "@Redfish.ActionInfo": "/redfish/v1/Systems/system/ResetActionInfo",
    "target": "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset"
    }
    },
    "Bios": {
    "@odata.id": "/redfish/v1/Systems/system/Bios"
    },
    "Boot": {
    "AutomaticRetryAttempts": 3,
    "AutomaticRetryConfig": "RetryAttempts",
    "AutomaticRetryConfig@Redfish.AllowableValues": [
    "Disabled",
    "RetryAttempts"
    ],
    "BootSourceOverrideEnabled": "Disabled",
    "BootSourceOverrideMode": "Legacy",
    "BootSourceOverrideTarget": "None",
    "BootSourceOverrideTarget@Redfish.AllowableValues": [
    "None",
    "Pxe",
    "Hdd",
    "Cd",
    "Diags",
    "BiosSetup",
    "Usb"
    ],
    "RemainingAutomaticRetryAttempts": 3
    },
    "BootProgress": {
    "LastState": "None"
    },
    "Description": "Computer System",
    "Id": "system",
    "LastResetTime": "1970-01-01T00:00:00+00:00",
    "Links": {
    "Chassis": [
    {
    "@odata.id": "/redfish/v1/Chassis/chassis"
    }
    ],
    "ManagedBy": [
    {
    "@odata.id": "/redfish/v1/Managers/bmc"
    }
    ]
    },
    "LogServices": {
    "@odata.id": "/redfish/v1/Systems/system/LogServices"
    },
    "Memory": {
    "@odata.id": "/redfish/v1/Systems/system/Memory"
    },
    "MemorySummary": {
    "Status": {
    "Health": "OK",
    "HealthRollup": "OK",
    "State": "Disabled"
    },
    "TotalSystemMemoryGiB": 0
    },
    "Name": "system",
    "PowerRestorePolicy": "AlwaysOff",
    "PowerState": "Off",
    "ProcessorSummary": {
    "Count": 0,
    "Status": {
    "Health": "OK",
    "HealthRollup": "OK",
    "State": "Disabled"
    }
    },
    "Processors": {
    "@odata.id": "/redfish/v1/Systems/system/Processors"
    },
    "Status": {
    "Health": "OK",
    "HealthRollup": "OK",
    "State": "Disabled"
    },
    "Storage": {
    "@odata.id": "/redfish/v1/Systems/system/Storage"
    },
    "SystemType": "Physical"
    }
    ],
    "Members@odata.count": 1,
    "Name": "Computer System Collection"
    }
    
    4.Other urls are the same as above.
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: I77dc16e6e416db479b0385e0b9b51ff283fb13e2

--- redfish-core/include/utils/query_param.hpp
+++ redfish-core/include/utils/query_param.hpp
@@ -42,6 +42,13 @@ class ProcessParam
             }
             return false;
         }
+        if (isExpand)
+        {
+            if (processExpand(req))
+            {
+                return true;
+            }
+        }
         isPrased = false;
         return false;
     }
@@ -69,6 +76,42 @@ class ProcessParam
             }
             isOnly = true;
             return true;
+        }
+        isExpand = false;
+        for (auto ite : req.urlParams)
+        {
+            if (ite->key() == "$expand")
+            {
+                std::string value = std::string(ite->value());
+                if (value == "*" || value == "." || value == "~")
+                {
+                    isExpand = true;
+                    expandType = std::move(value);
+                    expandLevel = 1;
+                }
+                else if (value.size() >= 12 &&
+                         value.substr(1, 9) == "($levels=" &&
+                         (value[0] == '*' || value[0] == '.' ||
+                          value[0] == '~') &&
+                         value[value.size() - 1] == ')')
+                {
+                    isExpand = true;
+                    expandType = value[0];
+                    expandLevel =
+                        strtoul(value.substr(10, value.size() - 2).c_str(),
+                                nullptr, 10);
+                    expandLevel =
+                        expandLevel < maxLevel ? expandLevel : maxLevel;
+                }
+                else
+                {
+                    res.jsonValue.clear();
+                    messages::queryParameterValueFormatError(res, ite->value(),
+                                                             ite->key());
+                    return false;
+                }
+            }
+            continue;
         }
         return true;
     }
@@ -106,12 +149,262 @@ class ProcessParam
         return true;
     }
 
+    bool processExpand(const crow::Request& req)
+    {
+        if (pendingUrlVec.size() == 0)
+        {
+            if (res.jsonValue.find("isExpand") == res.jsonValue.end())
+            {
+                res.jsonValue["isExpand"] = expandLevel + 1;
+            }
+            recursiveHyperlinks(res.jsonValue);
+            if (pendingUrlVec.size() == 0)
+            {
+                deleteExpand(res.jsonValue);
+                return false;
+            }
+            jsonValue = res.jsonValue;
+            newReq.emplace(req.req);
+            newReq->session = req.session;
+            newReq->setTarget(pendingUrlVec[0] + "?$expand=" + expandType +
+                              "($levels=" + std::to_string(expandLevel) + ")");
+            res.jsonValue.clear();
+            auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+            app.handle(*newReq, asyncResp);
+            return true;
+        }
+        if (res.jsonValue.find("isExpand") == res.jsonValue.end())
+        {
+            res.jsonValue["isExpand"] = expandLevel;
+        }
+        insertJson(jsonValue, pendingUrlVec[0], res.jsonValue);
+        pendingUrlVec.erase(pendingUrlVec.begin());
+        if (pendingUrlVec.size() != 0)
+        {
+            newReq.emplace(req.req);
+            newReq->session = req.session;
+            newReq->setTarget(pendingUrlVec[0] + "?$expand=" + expandType +
+                              "($levels=" + std::to_string(expandLevel) + ")");
+            res.jsonValue.clear();
+            auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+            app.handle(*newReq, asyncResp);
+            return true;
+        }
+        expandLevel -= 1;
+        if (expandLevel == 0)
+        {
+            res.jsonValue = jsonValue;
+            jsonValue.clear();
+            deleteExpand(res.jsonValue);
+            return false;
+        }
+        recursiveHyperlinks(jsonValue);
+        if (pendingUrlVec.size() == 0)
+        {
+            deleteExpand(res.jsonValue);
+            return false;
+        }
+        newReq.emplace(req.req);
+        newReq->session = req.session;
+        newReq->setTarget(pendingUrlVec[0] + "?$expand=" + expandType +
+                          "($levels=" + std::to_string(expandLevel) + ")");
+        res.jsonValue.clear();
+        auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+        app.handle(*newReq, asyncResp);
+        return true;
+    }
+
+    void recursiveHyperlinks(nlohmann::json& j)
+    {
+        auto itExpand = j.find("isExpand");
+        if (itExpand == j.end())
+        {
+            auto it = j.find("@odata.id");
+            if (it != j.end())
+            {
+                std::string url = it->get<std::string>();
+
+                if (url.find('#') == std::string::npos)
+                {
+                    pendingUrlVec.push_back(url);
+                }
+            }
+            else if (expandType == "*" || expandType == ".")
+            {
+                it = j.find("@Redfish.Settings");
+                if (it != j.end())
+                {
+                    std::string url = it->get<std::string>();
+                    if (url.find('#') == std::string::npos)
+                    {
+                        pendingUrlVec.push_back(url);
+                    }
+                }
+                else
+                {
+                    it = j.find("@Redfish.ActionInfo");
+                    if (it != j.end())
+                    {
+                        std::string url = it->get<std::string>();
+                        if (url.find('#') == std::string::npos)
+                        {
+                            pendingUrlVec.push_back(url);
+                        }
+                    }
+                    else
+                    {
+                        it = j.find("@Redfish.CollectionCapabilities");
+                        if (it != j.end())
+                        {
+                            std::string url = it->get<std::string>();
+                            if (url.find('#') == std::string::npos)
+                            {
+                                pendingUrlVec.push_back(url);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (auto it = j.begin(); it != j.end(); ++it)
+        {
+            if (it->is_object())
+            {
+                if (expandType == "." && it.key() == "Links")
+                {
+                    continue;
+                }
+                recursiveHyperlinks(*it);
+            }
+            else if (it->is_array())
+            {
+                for (auto& itArray : *it)
+                {
+                    if (itArray.is_object())
+                    {
+                        recursiveHyperlinks(itArray);
+                    }
+                }
+            }
+        }
+    }
+
+    bool insertJson(nlohmann::json& base, const std::string& pos,
+                    const nlohmann::json& data)
+    {
+        auto itExpand = base.find("isExpand");
+        if (itExpand != base.end())
+        {
+            if (itExpand->get<unsigned int>() <= expandLevel)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            auto it = base.find("@odata.id");
+            if (it != base.end())
+            {
+                if (it->get<std::string>() == pos)
+                {
+                    base = data;
+                    return true;
+                }
+            }
+            else if (expandType == "*" || expandType == ".")
+            {
+                it = base.find("@Redfish.Settings");
+                if (it != base.end())
+                {
+                    if (it->get<std::string>() == pos)
+                    {
+                        base = data;
+                        return true;
+                    }
+                }
+                else
+                {
+                    it = base.find("@Redfish.ActionInfo");
+                    if (it != base.end())
+                    {
+                        if (it->get<std::string>() == pos)
+                        {
+                            base = data;
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        it = base.find("@Redfish.CollectionCapabilities");
+                        if (it != base.end())
+                        {
+                            if (it->get<std::string>() == pos)
+                            {
+                                base = data;
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (auto it = base.begin(); it != base.end(); ++it)
+        {
+            if (it->is_object())
+            {
+                if (expandType == "." && it.key() == "Links")
+                {
+                    continue;
+                }
+                if (insertJson(*it, pos, data))
+                {
+                    return true;
+                }
+            }
+            else if (it->is_array())
+            {
+                for (auto& itArray : *it)
+                {
+                    if (itArray.is_object())
+                    {
+                        if (insertJson(itArray, pos, data))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    void deleteExpand(nlohmann::json& j)
+    {
+        auto it = j.find("isExpand");
+        if (it != j.end())
+        {
+            j.erase(it);
+        }
+        for (auto& it : j)
+        {
+            if (it.is_structured())
+            {
+                deleteExpand(it);
+            }
+        }
+    }
+
   private:
     App& app;
     crow::Response& res;
     std::optional<crow::Request> newReq;
     bool isPrased = false;
     bool isOnly = false;
+    bool isExpand = false;
+    uint64_t expandLevel;
+    const uint64_t maxLevel = 2;
+    std::string expandType;
+    std::vector<std::string> pendingUrlVec;
+    nlohmann::json jsonValue;
 };
 } // namespace query_param
 } // namespace redfish
