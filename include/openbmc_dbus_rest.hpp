openbmc/bmcweb

commit b623d9c1b6605978eb6158619bb43c79a9f543fd

dbus_rest: return error response with not found when bad dbus request
Symptom:
Run automation test "Create Two User Initiated Dump And Delete One"
then we got ERROR as below.
Create Two User Initiated Dump And Delete One        | FAIL |
Test Bmc Dump :: Test dump functionality of OpenBMC. | FAIL |
**ERROR** 200 != 404

Root cause:
handleGet() function of dbus_rest in bmcweb should be return setErrorResponse()
when doing get request after delete 1 bmc dump.
After checking code flow that will print "message 200 OK",
not "message 404 Not Found".

Due to /xyz/openbmc_project/dump/bmc/entry/1 object path already be deleted and not exist.
Thus when doing get request again with the deleted object path,
we will found that async_send() call return "Bad dbus request error".
But, code flow will keep to response with [200], not [404].
Response should be [404] not [200]. That's why auto test report "**ERROR** 200 != 404".

Solution:
Add setErrorResponse with not found error message return
when async_send return "Bad dbus request error".

Tested:
robot -t Create_Two_User_Initiated_Dump_And_Delete_One redfish/extended/test_bmc_dump.robot
Create Two User Initiated Dump And Delete One        | PASS |
Test Bmc Dump :: Test dump functionality of OpenBMC. | PASS |
1 critical test, 1 passed, 0 failed
1 test total, 1 passed, 0 failed

Signed-off-by: Tim Lee <timlee660101@gmail.com>
Change-Id: I81e4f22bc2a24f482a41b757835068ca81321437

include/openbmc_dbus_rest.hpp
@@ -1698,34 +1698,34 @@ inline void handleGet(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                             {
                                 BMCWEB_LOG_ERROR << "Bad dbus request error: "
                                                  << ec2;
+                                setErrorResponse(
+                                    asyncResp->res,
+                                    boost::beast::http::status::not_found,
+                                    notFoundDesc, notFoundMsg);
+                                return;
+                            }
+                            nlohmann::json properties;
+                            int r = convertDBusToJSON("a{sv}", msg, properties);
+                            if (r < 0)
+                            {
+                                BMCWEB_LOG_ERROR << "convertDBusToJSON failed";
                             }
                             else
                             {
-                                nlohmann::json properties;
-                                int r =
-                                    convertDBusToJSON("a{sv}", msg, properties);
-                                if (r < 0)
+                                for (auto& prop : properties.items())
                                 {
-                                    BMCWEB_LOG_ERROR
-                                        << "convertDBusToJSON failed";
-                                }
-                                else
-                                {
-                                    for (auto& prop : properties.items())
-                                    {
-                                        // if property name is empty, or
-                                        // matches our search query, add it
-                                        // to the response json
+                                    // if property name is empty, or
+                                    // matches our search query, add it
+                                    // to the response json
 
-                                        if (propertyName->empty())
-                                        {
-                                            (*response)[prop.key()] =
-                                                std::move(prop.value());
-                                        }
-                                        else if (prop.key() == *propertyName)
-                                        {
-                                            *response = std::move(prop.value());
-                                        }
+                                    if (propertyName->empty())
+                                    {
+                                        (*response)[prop.key()] =
+                                            std::move(prop.value());
+                                    }
+                                    else if (prop.key() == *propertyName)
+                                    {
+                                        *response = std::move(prop.value());
                                     }
                                 }
                             }

commit ab5517b8ec2c5153e2b0d962028901b7431d4ceb

do not read properties from mapper
Commit b623d9c introduced a change that will return a failure to a
caller trying to read a property over the D-Bus API if any query to a
service hosting the object of interest returns an error. It turns out
that the xyz.openbmc_project.ObjectMapper service will return an error
if you try to call the "org.freedesktop.DBus.Properties GetAll" method
against it. It's not clear why this is, probably because mapper is just
a middle-person for connecting interfaces and services. It's never meant
to actually hold the properties.

Resolves openbmc/bmcweb#206

Tested:
- Verified a code update now works with bmcweb master + this change on a
  IBM witherspoon system (HW CI machine).

Change-Id: I9cbe4238f8ff941938089d83f35b7dc63be1d5e1
Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

include/openbmc_dbus_rest.hpp
@@ -1672,6 +1672,14 @@ inline void handleGet(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
             for (const std::pair<std::string, std::vector<std::string>>&
                      connection : objectNames)
             {
+                // Ignore the mapper service since it will never host the
+                // property we are interested in and actually returns an error
+                // when we try to read properties from it
+                if (connection.first == "xyz.openbmc_project.ObjectMapper")
+                {
+                    continue;
+                }
+
                 const std::vector<std::string>& interfaceNames =
                     connection.second;
 