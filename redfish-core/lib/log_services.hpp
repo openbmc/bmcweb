91230070495c79de49f871012ca39a3a0f87ae7c
John Edward Broadbent <jebr@google.com>
2021-06-09 10:12:48 -0700

    Corrects URI by removing /<str>
    A URI was accidentally changed, and this issue is corrected in the
    change.
    
    Change-Id: Icd840dd60bba2a2fd0010ce2cf88fac801b37cf7
    Signed-off-by: John Edward Broadbent <jebr@google.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -2211,7 +2211,7 @@ inline void requestRoutesSystemDumpEntryCollection(App& app)
      * Functions triggers appropriate requests on DBus
      */
     BMCWEB_ROUTE(app,
-                 "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/<str>/")
+                 "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/")
         .privileges({"Login"})
         .methods(boost::beast::http::verb::get)(
             [](const crow::Request&,
