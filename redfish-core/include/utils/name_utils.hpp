0b19ed47f522b1890b27b49a8c8cca3c8b8ec579
Willy Tu <wltu@google.com>
2021-06-09 16:19:46 -0700

    util: Add pretty name for resources
    Created a helper function to fetch pretty name and added for some
    resources.
    
    - Chassis
    - FirmwareInventory
    - Storage
    - StorageController
    - System
    - Processor
    
    Implementation Details:
    The helper function makes a call to ObjectMapper to get the services
    that support the object path with the interfaces we want. It expects
    only one service is exporting the object path.
    
    Then it will try to get the `PrettyName` property. If it is found and is
    valid, it will set the `Name` to the value. If not, it will use the
    default value that is set before the function call.
    
    This will affect all systems and replace the Name with the PrettyName if
    it exist within the resource's object path.
    
    Tested:
    Passed Redfish Validator
    
    The output names release the existing `Name` property only if the
    PrettyName property exists. If not, it will use the default value.
    
    Output with PrettyName
    ```
    $ curl  -u root:0penBmc -X GET http://${bmc}/redfish/v1/Chassis/chassis0
    {
      "@odata.id": "/redfish/v1/Chassis/chassis0",
      "@odata.type": "#Chassis.v1_14_0.Chassis",
      "ChassisType": "RackMount",
      "Id": "chassis0",
      "Links": {
        "ComputerSystems": [
          {
            "@odata.id": "/redfish/v1/Systems/system"
          }
        ],
        "ManagedBy": [
          {
            "@odata.id": "/redfish/v1/Managers/bmc"
          }
        ]
      },
      "Name": "Test Chassis",
      ...
    }
    ```
    
    Without PrettyName
    ```
    $ curl  -u root:0penBmc -X GET http://${bmc}/redfish/v1/Chassis/chassis0
    {
      "@odata.id": "/redfish/v1/Chassis/chassis0",
      "@odata.type": "#Chassis.v1_14_0.Chassis",
      "ChassisType": "RackMount",
      "Id": "chassis0",
      "Links": {
        "ComputerSystems": [
          {
            "@odata.id": "/redfish/v1/Systems/system"
          }
        ],
        "ManagedBy": [
          {
            "@odata.id": "/redfish/v1/Managers/bmc"
          }
        ]
      },
      "Name": "chassis0",
      ...
    }
    ```
    
    Performance Diff:
     - Before:
    ```
    $ for i in {1..5}; do
        time curl -su root:0penBmc -X GET http://${bmc}/redfish/v1/Chassis/warthog_1 > /dev/null
      done
    
    real    0m0.314s
    user    0m0.006s
    sys     0m0.003s
    
    real    0m0.346s
    user    0m0.003s
    sys     0m0.009s
    
    real    0m0.369s
    user    0m0.001s
    sys     0m0.013s
    
    real    0m0.343s
    user    0m0.005s
    sys     0m0.010s
    
    real    0m0.356s
    user    0m0.006s
    sys     0m0.010s
    ```
     - After:
    ```
    $ for i in {1..5}; do
        time curl -su root:0penBmc -X GET http://${bmc}/redfish/v1/Chassis/warthog_1 > /dev/null;
      done
    
    real    0m0.322s
    user    0m0.009s
    sys     0m0.005s
    
    real    0m0.343s
    user    0m0.007s
    sys     0m0.007s
    
    real    0m0.337s
    user    0m0.000s
    sys     0m0.009s
    
    real    0m0.348s
    user    0m0.011s
    sys     0m0.005s
    
    real    0m0.379s
    user    0m0.010s
    sys     0m0.005s
    
    Change-Id: I76e65f4aae979365266ebb38525c4ac45298737b
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ redfish-core/include/utils/name_utils.hpp
@@ -0,0 +1,84 @@
+
+#pragma once
+#include <async_resp.hpp>
+
+#include <algorithm>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace redfish
+{
+namespace name_util
+{
+
+/**
+ * @brief Populate the collection "Members" from a GetSubTreePaths search of
+ *        inventory
+ *
+ * @param[i,o] asyncResp  Async response object
+ * @param[i]   path       D-bus object path to find the find pretty name
+ * @param[i]   services   List of services to exporting the D-bus object path
+ * @param[i]   namePath   Json pointer to the name field to update.
+ *
+ * @return void
+ */
+inline void getPrettyName(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& path,
+    const std::vector<std::pair<std::string, std::vector<std::string>>>&
+        services,
+    const nlohmann::json_pointer<nlohmann::json>& namePath)
+{
+    BMCWEB_LOG_DEBUG << "Get PrettyName for: " << path;
+
+    for (const std::pair<std::string, std::vector<std::string>>& service :
+         services)
+    {
+        if (service.first != "xyz.openbmc_project.Inventory.Item")
+        {
+            BMCWEB_LOG_DEBUG << "No PrettyName property available for: "
+                             << service.first;
+            continue;
+        }
+
+        crow::connections::systemBus->async_method_call(
+            [asyncResp, path,
+             namePath](const boost::system::error_code ec,
+                       const std::variant<std::string>& prettyName) {
+                if (ec)
+                {
+                    BMCWEB_LOG_DEBUG << "DBUS response error " << ec;
+                    return;
+                }
+
+                const std::string* value =
+                    std::get_if<std::string>(&prettyName);
+
+                if (!value)
+                {
+
+                    BMCWEB_LOG_ERROR << "Failed to get Pretty Name for "
+                                     << path;
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+
+                if (value->empty())
+                {
+                    return;
+                }
+
+                BMCWEB_LOG_DEBUG << "Pretty Name: " << *value;
+
+                asyncResp->res.jsonValue[namePath] = *value;
+            },
+            service.first, path, "org.freedesktop.DBus.Properties", "Get",
+            "xyz.openbmc_project.Inventory.Item", "PrettyName");
+
+        return;
+    }
+}
+
+} // namespace name_util
+} // namespace redfish
--- redfish-core/lib/chassis.hpp
+++ redfish-core/lib/chassis.hpp
@@ -22,6 +22,7 @@
 #include <boost/container/flat_map.hpp>
 #include <registries/privilege_registry.hpp>
 #include <utils/collection.hpp>
+#include <utils/name_utils.hpp>
 
 #include <variant>
 
@@ -197,7 +198,7 @@ inline void requestRoutesChassis(App& app)
                 "xyz.openbmc_project.Inventory.Item.Chassis"};
 
             crow::connections::systemBus->async_method_call(
-                [asyncResp, chassisId(std::string(chassisId))](
+                [asyncResp, chassisId(std::string(chassisId)), interfaces](
                     const boost::system::error_code ec,
                     const crow::openbmc_mapper::GetSubTreeType& subtree) {
                     if (ec)
@@ -369,7 +370,8 @@ inline void requestRoutesChassis(App& app)
                         }
 
                         crow::connections::systemBus->async_method_call(
-                            [asyncResp, chassisId(std::string(chassisId))](
+                            [asyncResp, chassisId(std::string(chassisId)), path,
+                             connectionNames](
                                 const boost::system::error_code /*ec2*/,
                                 const std::vector<
                                     std::pair<std::string, VariantType>>&
@@ -400,6 +402,11 @@ inline void requestRoutesChassis(App& app)
                                 }
                                 asyncResp->res.jsonValue["Name"] = chassisId;
                                 asyncResp->res.jsonValue["Id"] = chassisId;
+
+                                name_util::getPrettyName(asyncResp, path,
+                                                         connectionNames,
+                                                         "/Name"_json_pointer);
+
 #ifdef BMCWEB_ALLOW_DEPRECATED_POWER_THERMAL
                                 asyncResp->res.jsonValue["Thermal"] = {
                                     {"@odata.id", "/redfish/v1/Chassis/" +
