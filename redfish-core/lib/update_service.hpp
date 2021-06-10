1ec629d5379c4f92b45e9f08c18c166284db45b2
Willy Tu <wltu@google.com>
2021-06-10 13:14:56 -0700

    SoftwareInventory: Get/Set WriteProtected from new D-Bus interface
    Move FirmwareInventory to v1_3_0 and added new support for Software
    settings.
    
    Added WriteProtected and now can get Updatable from D-Bus. The
    WriteProtected property can be set vis D-bus call and a PATCH support
    is added to update WriteProtected.
    
    Tested:
    Passed Redfish Validator
    ```
    *** /redfish/v1/UpdateService/FirmwareInventory
             Type (#SoftwareInventoryCollection.SoftwareInventoryCollection), GET SUCCESS (time: 0.027364)
             PASS
    
    *** /redfish/v1/UpdateService/FirmwareInventory/f22015d9
             Type (#SoftwareInventory.v1_3_0.SoftwareInventory), GET SUCCESS (time: 0.037481)
             PASS
    ```
    
    Getter
    ```
    {
      "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/firmware_0",
      "@odata.type": "#SoftwareInventory.v1_3_0.SoftwareInventory",
      "Description": "BMC image",
      "Id": "f22015d9",
      "Name": "Software Inventory",
      "RelatedItem": [
        {
          "@odata.id": "/redfish/v1/Managers/bmc"
        }
      ],
      "RelatedItem@odata.count": 1,
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "Enabled"
      },
      "Updateable": true,
      "Version": "0.5.0.0",
      "WriteProtected": false
    }
    ```
    
    PATCH
    ```
    $ curl -k -u root:0penBmc -X PATCH \
     "http://${bmc}/redfish/v1/UpdateService/FirmwareInventory/firmware_0" \
     -d '{"WriteProtected" :  false}'
    {
      "@Message.ExtendedInfo": [
        {
          "@odata.type": "#Message.v1_1_1.Message",
          "Message": "Successfully Completed Request",
          "MessageArgs": [],
          "MessageId": "Base.1.8.1.Success",
          "MessageSeverity": "OK",
          "Resolution": "None"
        }
      ]
    
    ```
    
    Change-Id: I4c7573c9c3d2ee5a502cfdf0e2657341e1ef94b2
    Signed-off-by: Willy Tu <wltu@google.com>

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -767,6 +767,26 @@ inline static void
     }
 }
 
+inline static void
+    setWriteProtected(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                      const std::string& service, const std::string& objectPath,
+                      bool writeProtected)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, writeProtected](const boost::system::error_code ec) {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "D-Bus responses error: " << ec;
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            messages::success(asyncResp->res);
+        },
+        service, objectPath, "org.freedesktop.DBus.Properties", "Set",
+        "xyz.openbmc_project.Software.Settings", "WriteProtected",
+        std::variant<bool>{writeProtected});
+}
+
 inline void
     getSoftwareVersion(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                        std::string_view service, std::string_view path,
@@ -915,9 +935,17 @@ inline void requestRoutesSoftwareInventory(App& app)
                                     fw_util::getFwStatus(asyncResp, swId,
                                                          obj.second[0].first);
                                 }
+                                if (interface ==
+                                    "xyz.openbmc_project.Software.Settings")
+                                {
+                                    fw_util::getFirmwareSettings(
+                                        asyncResp, obj.second[0].first,
+                                        obj.first);
+                                }
                             }
                         }
                     }
+
                     if (!found)
                     {
                         BMCWEB_LOG_ERROR
@@ -929,7 +957,7 @@ inline void requestRoutesSoftwareInventory(App& app)
                         return;
                     }
                     asyncResp->res.jsonValue["@odata.type"] =
-                        "#SoftwareInventory.v1_1_0.SoftwareInventory";
+                        "#SoftwareInventory.v1_3_0.SoftwareInventory";
                     asyncResp->res.jsonValue["Name"] = "Software Inventory";
                     asyncResp->res.jsonValue["Status"]["HealthRollup"] = "OK";
 
@@ -940,10 +968,73 @@ inline void requestRoutesSoftwareInventory(App& app)
                 "/xyz/openbmc_project/object_mapper",
                 "xyz.openbmc_project.ObjectMapper", "GetSubTree", "/",
                 static_cast<int32_t>(0),
-                std::array<const char*, 2>{
+                std::array<const char*, 3>{
                     "xyz.openbmc_project.Software.Activation"
-                    "xyz.openbmc_project.Software.Version"});
+                    "xyz.openbmc_project.Software.Version",
+                    "xyz.openbmc_project.Software.Settings"});
         });
+
+    BMCWEB_ROUTE(app, "/redfish/v1/UpdateService/FirmwareInventory/<str>/")
+        .privileges(redfish::privileges::patchSoftwareInventory)
+        .methods(boost::beast::http::verb::patch)(
+            [](const crow::Request& req,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+               const std::string& swId) {
+                std::optional<bool> writeProtected;
+                if (!json_util::readJson(req, asyncResp->res, "WriteProtected",
+                                         writeProtected))
+                {
+                    return;
+                }
+
+                if (!writeProtected)
+                {
+                    return;
+                }
+
+                crow::connections::systemBus->async_method_call(
+                    [asyncResp, swId, writeProtected](
+                        const boost::system::error_code ec,
+                        const crow::openbmc_mapper::GetSubTreeType& subtree) {
+                        BMCWEB_LOG_DEBUG << "doPatch callback...";
+                        if (ec)
+                        {
+                            messages::internalError(asyncResp->res);
+                            return;
+                        }
+
+                        for (const std::pair<
+                                 std::string,
+                                 std::vector<std::pair<
+                                     std::string, std::vector<std::string>>>>&
+                                 firmware : subtree)
+                        {
+
+                            if (sdbusplus::message::object_path(firmware.first)
+                                        .filename() != swId ||
+                                firmware.second.empty())
+                            {
+                                continue;
+                            }
+
+                            setWriteProtected(asyncResp,
+                                              firmware.second[0].first,
+                                              firmware.first, *writeProtected);
+                            return;
+                        }
+
+                        messages::resourceNotFound(
+                            asyncResp->res,
+                            "#SoftwareInventory.v1_3_0.SoftwareInventory",
+                            swId);
+                    },
+                    "xyz.openbmc_project.ObjectMapper",
+                    "/xyz/openbmc_project/object_mapper",
+                    "xyz.openbmc_project.ObjectMapper", "GetSubTree", "/",
+                    static_cast<int32_t>(0),
+                    std::array<const char*, 1>{
+                        "xyz.openbmc_project.Software.Settings"});
+            });
 }
 
 } // namespace redfish
