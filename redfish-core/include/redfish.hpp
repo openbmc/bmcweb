10ca444b35a5aadb5cc9358b7ddf583249b1d6fe
Albert Zhang <zhanghaodi@inspur.com>
2021-06-13 14:39:38 +0800

    Add Redfish EnvironmentMetrics schema in bmcweb
    The EnvironmentMetrics is a new resource in Redfish version 2020.4.
    This resource shall represent the environmental metrics for a
    Redfish implementation.
    
    This resource includes Energy consumption,Fan speeds (percent),
    Power consumption (Watts), etc.And these data are mainly obtained
    from under sensors.
    
    ref:
    https://redfish.dmtf.org/schemas/v1/EnvironmentMetrics.v1_0_0.json
    
    Tested: Validator passes
    After booting
    1.doGet method to get EnvironmentMetrics
    
     curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis/EnvironmentMetrics
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/EnvironmentMetrics",
      "@odata.type": "#EnvironmentMetrics.v1_0_0.EnvironmentMetrics",
      "FanSpeedsPercent": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan0_0",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan0_0",
          "DeviceName": "Chassis Fan #fan0_0",
          "SpeedRPM": -110.0
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan0_1",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan0_1",
          "DeviceName": "Chassis Fan #fan0_1",
          "SpeedRPM": -110.0
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan1_0",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan1_0",
          "DeviceName": "Chassis Fan #fan1_0",
          "SpeedRPM": -110.0
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan1_1",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan1_1",
          "DeviceName": "Chassis Fan #fan1_1",
          "SpeedRPM": -110.0
        },
    {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan2_0",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan2_0",
          "DeviceName": "Chassis Fan #fan2_0",
          "SpeedRPM": 17022.0
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan2_1",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan2_1",
          "DeviceName": "Chassis Fan #fan2_1",
          "SpeedRPM": 14397.0
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan3_0",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan3_0",
          "DeviceName": "Chassis Fan #fan3_0",
          "SpeedRPM": 17055.0
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/fan3_1",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan3_1",
          "DeviceName": "Chassis Fan #fan3_1",
          "SpeedRPM": 14567.0
        }
      ],
      "Id": "EnvironmentMetrics",
      "Name": "Chassis Environment Metrics"
      "PowerWatts": {
        "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/total_power",
        "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/total_power",
        "Reading": 225.0
      }
    }
    2.Enter the wrong chassisId with the doGet method
    
     curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassisERROR/EnvironmentMetrics
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type Chassis named chassisERROR was not found.",
            "MessageArgs": [
              "Chassis",
              "chassisERROR"
            ],
            "MessageId": "Base.1.8.1.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.8.1.ResourceNotFound",
        "message": "The requested resource of type Chassis named chassisERROR was not found."
      }
    }
    
    Signed-off-by: Albert Zhang <zhanghaodi@inspur.com>
    Change-Id: I6a75dfbc94f39d90a1368770076b874608394691

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -19,6 +19,7 @@
 #include "../lib/bios.hpp"
 #include "../lib/certificate_service.hpp"
 #include "../lib/chassis.hpp"
+#include "../lib/environment_metrics.hpp"
 #include "../lib/ethernet.hpp"
 #include "../lib/event_service.hpp"
 #include "../lib/hypervisor_system.hpp"
@@ -202,6 +203,9 @@ class RedfishService
         requestRoutesMetricReportDefinition(app);
         requestRoutesMetricReportCollection(app);
         requestRoutesMetricReport(app);
+#ifdef BMCWEB_NEW_POWERSUBSYSTEM_THERMALSUBSYSTEM
+        requestRoutesEnvironmentMetrics(app);
+#endif
     }
 };
 
--- redfish-core/lib/chassis.hpp
+++ redfish-core/lib/chassis.hpp
@@ -256,7 +256,7 @@ inline void requestRoutesChassis(App& app)
                         }
 
                         asyncResp->res.jsonValue["@odata.type"] =
-                            "#Chassis.v1_14_0.Chassis";
+                            "#Chassis.v1_15_0.Chassis";
                         asyncResp->res.jsonValue["@odata.id"] =
                             "/redfish/v1/Chassis/" + chassisId;
                         asyncResp->res.jsonValue["Name"] = "Chassis Collection";
@@ -408,6 +408,12 @@ inline void requestRoutesChassis(App& app)
                                 asyncResp->res.jsonValue["Power"] = {
                                     {"@odata.id", "/redfish/v1/Chassis/" +
                                                       chassisId + "/Power"}};
+#endif
+#ifdef BMCWEB_NEW_POWERSUBSYSTEM_THERMALSUBSYSTEM
+                                asyncResp->res.jsonValue["EnvironmentMetrics"] =
+                                    {{"@odata.id", "/redfish/v1/Chassis/" +
+                                                       chassisId +
+                                                       "/EnvironmentMetrics"}};
 #endif
                                 // SensorCollection
                                 asyncResp->res.jsonValue["Sensors"] = {
