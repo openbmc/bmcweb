7c76b8fb83681bd6508d51f6b99fad89c0b15b4d
Abhishek Patel <Abhishek.Patel@ibm.com>
2021-06-24 10:11:33 -0500

    Retrieve port info for systems service
    In systems.hpp for Serialconsole, SSH service-enabled and its Port
    number info is hardcoded. Same for IPMI service-enabled info is also
    hardcoded.
    
    Implementation:
    
     SSH:
      check for obmc-console-ssh@2200.service service is enable or not, and
      yes then, on which port number. Retrieve service-related information
      and pass that into DMTF for "/redfish/v1/Systems/system/".
    
     IPMI:-
      check for phosphor-ipmi-net@eth0.socket service is enable or not, and
      pass that info DMTF for "/redfish/v1/Systems/system/".
    
    Tested:
     Manually tested on the Witherspoon system, there is no change in output.
     Run Redfish validator. No error found.
    
    Before:
    
    "SerialConsole": {
      "IPMI": {
        "ServiceEnabled": true
      },
      "MaxConcurrentSessions": 15,
      "SSH": {
        "HotKeySequenceDisplay": "Press ~. to exit console",
        "Port": 2200,
        "ServiceEnabled": true
      }
    }
    
    After:
    
     Note: SSH Info retrieve via Dbus ListUnit API
    
    "SerialConsole": {
      "IPMI": {
        "ServiceEnabled": true
      },
      "MaxConcurrentSessions": 15,
      "SSH": {
        "HotKeySequenceDisplay": "Press ~. to exit console",
        "Port": 2200,
        "ServiceEnabled": true
      }
    }
    
    Signed-off-by: Abhishek Patel <Abhishek.Patel@ibm.com>
    Change-Id: I70009ee785aab3ca4a61fe0d96fbc5b340831647

--- redfish-core/lib/network_protocol.hpp
+++ redfish-core/lib/network_protocol.hpp
@@ -30,8 +30,10 @@ namespace redfish
 void getNTPProtocolEnabled(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp);
 std::string getHostName();
 
-const static std::vector<std::pair<const char*, const char*>> protocolToDBus{
-    {{"SSH", "dropbear"}, {"HTTPS", "bmcweb"}, {"IPMI", "phosphor-ipmi-net"}}};
+const static std::vector<std::pair<const char*, const char*>>
+    protocolToDBusService{{{"SSH", "dropbear"},
+                           {"HTTPS", "bmcweb"},
+                           {"IPMI", "phosphor-ipmi-net"}}};
 
 inline void
     extractNTPServersAndDomainNamesData(const GetManagedObjects& dbusData,
@@ -122,7 +124,7 @@ void getNetworkData(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     asyncResp->res.jsonValue["HTTP"]["Port"] = 0;
     asyncResp->res.jsonValue["HTTP"]["ProtocolEnabled"] = false;
 
-    for (auto& protocol : protocolToDBus)
+    for (auto& protocol : protocolToDBusService)
     {
         asyncResp->res.jsonValue[protocol.first]["Port"] =
             nlohmann::detail::value_t::null;
@@ -174,7 +176,7 @@ void getNetworkData(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
              "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates"}};
     }
 
-    for (const auto& protocol : protocolToDBus)
+    for (const auto& protocol : protocolToDBusService)
     {
         const std::string& keyValue = protocol.first;
         getPortStatusAndPath(protocol.second, [asyncResp, keyValue](
