a0350e8b89cee3ef33f51ef76eea2d5c45b37864
John Edward Broadbent <jebr@google.com>
2021-06-28 11:32:33 -0700

    WIP adding new redfish testing for serviceroot
    * Break serviceroot callback into the free function(done)
    * Write the request and response objects (in progress)
    * Test the result of serviceroot free function (not done)
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I560cbb0309c25670cacd81c32bccae3445ccca7b

---
+++ redfish-core/lib/ut/service_root_test.cpp
@@ -0,0 +1,35 @@
+#include "app.hpp"
+#include "http_request.hpp"
+#include "include/async_resp.hpp"
+#include "nlohmann/json.hpp"
+#include "redfish-core/lib/service_root.hpp"
+
+#include <boost/shared_ptr.hpp>
+
+#include <string>
+
+#include "gmock/gmock.h"
+
+using namespace redfish;
+
+void assertServiceRootGet()
+{
+    // do some checks of something
+    // "/redfish/v1/"
+    // EXPECT_THAT(privileges.getActivePrivilegeNames(PrivilegeType::BASE),
+    //            ::testing::UnorderedElementsAre("Login", "ConfigureManager"));
+}
+TEST(ServiceRootTest, ServiceRootConstructor)
+{
+
+    // create arguments used in handler call
+    boost::beast::http::request<boost::beast::http::string_body> in;
+    crow::Request req(in);
+    crow::Response response(assertServiceRootGet);
+    std::shared_ptr<bmcweb::AsyncResp> asyncResp_ptr(
+        new bmcweb::AsyncResp(response));
+    const bmcweb::AsyncResp asyncResp(response);
+
+    // call handler
+    // handleServiceRootGet(req, asyncResp_ptr);
+}
