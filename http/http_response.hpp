deafbacef920f3b4f83ccd6b5e952b2d2a7304a7
John Edward Broadbent <jebr@google.com>
2021-06-28 11:32:33 -0700

    WIP adding new redfish testing for serviceroot
    * Break serviceroot callback into the free function(done)
    * Write the request and response objects (in progress)
    * Test the result of serviceroot free function (not done)
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I560cbb0309c25670cacd81c32bccae3445ccca7b

--- http/http_response.hpp
+++ http/http_response.hpp
@@ -39,6 +39,12 @@ struct Response
     Response() : stringResponse(response_type{})
     {}
 
+    // used for testing
+    Response(std::function<void()> newHandler)
+    {
+        completeRequestHandler = newHandler;
+    }
+
     Response& operator=(const Response& r) = delete;
 
     Response& operator=(Response&& r) noexcept
@@ -134,6 +140,12 @@ struct Response
     bool isAlive()
     {
         return isAliveHelper && isAliveHelper();
+    }
+
+    // used for testing
+    void setCompleteRequestHandler(std::function<void()> newCompleteRH)
+    {
+        completeRequestHandler = newCompleteRH;
     }
 
   private:
