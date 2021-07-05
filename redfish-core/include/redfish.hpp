5a9a937b0e9b2ed246bdfcd0098f6b985d0f0b90
George Liu <liuxiwei@inspur.com>
2021-07-05 12:43:56 +0800

    log_services: Add download of post code log entries
    - Add a GET method /redfish/v1/Systems/system/LogServices/PostCodes
      /Entries/<str>/attachment/, Get the attribute value through the
      getPostCodes method and encode it as base64, and send it off.
    
    - This allows the use to offload error logs for analysis and further
      parsing if needed. An http header of "Accept:
      application/octet-stream" or the default "*/*" is expected.
    
    Tested:
    - Ran Redfish validator.
    
    - Before, It broke post JSON content as HTTP
      https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/44660
      Now, I tested it passed.
    
    - pldmtool raw --data 0x80 0x3F 0xC 0x0A 0x00 0x00 0x00 0x00 0x00 0x07 0x00 0x00 0x00 0x48 0x00 0x00 0x00 0x02 0x00 0x00 0x01 0x00 0x00 0x00 0x48 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x53 0x54 0x41 0x4e 0x44 0x42 0x59 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20
    
    $curl -k https://127.0.0.1:2443/redfish/v1/Systems/system/LogServices/PostCodes/Entries/B1-1/attachment/
    output:
    AgAAAQAAAEgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFNUQU5EQlkgICAgICAgICAgICAgICAgICAgICAgICAg
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ide684146a4ae9d55dc95fb765927867b042fc27c

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -93,6 +93,7 @@ class RedfishService
 
         requestRoutesSystemLogServiceCollection(app);
         requestRoutesEventLogService(app);
+        requestRoutesPostCodesEntryAdditionalData(app);
 
         requestRoutesPostCodesLogService(app);
         requestRoutesPostCodesClear(app);
