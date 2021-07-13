b2a3289d6f86756da4bd500b6a53b1628fffcf37
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-07-13 11:56:15 -0500

    Fix System dump entries route
    The system dump entries URI was wrong (same as bmc dump entries) and
    hence causes bmcweb to crash with the following error: "handler already
    exists for /redfish/v1/Managers/bmc/LogServices/Dump/Entries/"
    
    The below commit has caused this issue:
    https://github.com/openbmc/bmcweb/commit/ed3982131dcef2b499da36e674d2d21
    b2289ef29
    The above change will impact the clients as listing of system dumps will
    never work and also this causes bmcweb to crash.
    
    This commit has the fix for the same.
    Old Incorrect URI:
    /redfish/v1/Managers/bmc/LogServices/Dump/Entries/
    is changed to the following RI:
    /redfish/v1/Systems/system/LogServices/Dump/Entries/
    
    
    Tested the same.
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: Idbf23cb2ca52575dd47d296884793a33710e07d5

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -2243,7 +2243,7 @@ inline void requestRoutesSystemDumpEntryCollection(App& app)
     /**
      * Functions triggers appropriate requests on DBus
      */
-    BMCWEB_ROUTE(app, "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/")
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/system/LogServices/Dump/Entries/")
         .privileges(redfish::privileges::getLogEntryCollection)
         .methods(boost::beast::http::verb::get)(
             [](const crow::Request&,
