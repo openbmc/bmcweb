0914d3285d6e03f98ce8b4dd5be23e18051a8019
Szymon Dompke <szymon.dompke@intel.com>
2021-07-14 21:25:31 +0200

    Add support for POST on TriggersCollection
    Added POST method on /redfish/v1/TelemetryService/Triggers uri, which
    creates new trigger in telemetry service, by using dbus call AddTrigger.
    
    By DMTF, most of the properties are not required, and as such are
    treated as optional. Some values can be deduced from others (like
    'MetricType', depending on 'DiscreteTriggers' or 'NumericThresholds').
    All properties provided in POST body by user will be verified against
    each other, and errors will be raised. Few examples of such situations:
    - 'MetricType' is set to 'Discrete' but 'NumericThresholds' was passed.
    - 'MetricType' is set to 'Numeric' but "DiscreteTriggers' or
      'DiscreteTriggerCondition' were passed
    - 'DiscreteTriggerCondition' is set to 'Specified' but
      'DiscreteTriggers' is an empty array or was not passed.
    - 'DiscreteTriggerCondition' is set to 'Changed' but 'DiscreteTriggers'
      is passed and is not an empty array.
    
    Example 1 – Trigger with discrete values:
    {
        "Id": "TestTrigger",
        "MetricType": "Discrete",
        "TriggerActions": [
            "RedfishEvent"
        ],
        "DiscreteTriggerCondition": "Specified",
        "DiscreteTriggers": [
            {
                "Value": "55.88",
                "DwellTime": "PT0.001S",
                "Severity": "Warning"
            },
            {
                "Name": "My discrete trigger",
                "Value": "55.88",
                "DwellTime": "PT0.001S",
                "Severity": "OK"
            },
            {
                "Value": "55.88",
                "DwellTime": "PT0.001S",
                "Severity": "Critical"
            }
        ],
        "MetricProperties": [
            "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/0/Reading"
        ],
        "Links": {
            "MetricReportDefinitions": []
        }
    }
    
    Example 2 – trigger with numeric threshold:
    {
        "Id": "TestTrigger2",
        "Name": "My Numeric Trigger",
        "MetricType": "Numeric",
        "TriggerActions": [
            "RedfishEvent",
            "RedfishMetricReport"
        ],
        "NumericThresholds": {
            "UpperCritical": {
                "Reading": 50,
                "Activation": "Increasing",
                "DwellTime": "PT0.001S"
            },
            "UpperWarning": {
                "Reading": 48.1,
                "Activation": "Increasing",
                "DwellTime": "PT0.004S"
            }
        },
        "MetricProperties": [
            "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/0/Reading",
            "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/17/Reading"
        ],
        "Links": {
            "MetricReportDefinitions": [
                "/redfish/v1/TelemetryService/MetricReportDefinitions/PowerMetrics",
                "/redfish/v1/TelemetryService/MetricReportDefinitions/PowerMetricStats",
                "/redfish/v1/TelemetryService/MetricReportDefinitions/PlatformPowerUsage"
            ]
        }
    }
    
    Tested:
    - Triggers were successfully created with above example message bodies.
      This can be checked by calling:
      'busctl tree xyz.openbmc_project.Telemetry'.
    - Expected errors were returned for messages with incorrect or mutually
      exclusive properties and incorrect values.
    - Redfish service validator is passing. Although it is not yet checking
      /redfish/v1/TelemetryService/MetricReports/Triggers as this commit is
      not adding this node.
    
    Signed-off-by: Szymon Dompke <szymon.dompke@intel.com>
    Change-Id: Ief8c76de8aa660ae0d2dbe4610c26a28186a290a

--- redfish-core/include/utils/telemetry_utils.hpp
+++ redfish-core/include/utils/telemetry_utils.hpp
@@ -23,5 +23,82 @@ inline std::string getDbusReportPath(const std::string& id)
     return path;
 }
 
+inline std::optional<std::string>
+    getReportNameFromReportDefinitionUri(const std::string& uri)
+{
+    constexpr size_t idx = std::string_view(metricReportDefinitionUri).length();
+    if (boost::starts_with(uri, metricReportDefinitionUri))
+    {
+        return uri.substr(idx);
+    }
+    return std::nullopt;
+}
+
+inline std::optional<std::string>
+    getTriggerIdFromDbusPath(const std::string& dbusPath)
+{
+    constexpr const char* triggerTree =
+        "/xyz/openbmc_project/Telemetry/Triggers/TelemetryService/";
+    constexpr size_t idx = std::string_view(triggerTree).length();
+    if (boost::starts_with(dbusPath, triggerTree))
+    {
+        return dbusPath.substr(idx);
+    }
+    return std::nullopt;
+}
+
+inline bool getChassisSensorNode(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::vector<std::string>& uris,
+    boost::container::flat_set<std::pair<std::string, std::string>>& matched)
+{
+    size_t uriIdx = 0;
+    for (const std::string& uri : uris)
+    {
+        std::string chassis;
+        std::string node;
+
+        if (!boost::starts_with(uri, "/redfish/v1/Chassis/") ||
+            !dbus::utility::getNthStringFromPath(uri, 3, chassis) ||
+            !dbus::utility::getNthStringFromPath(uri, 4, node))
+        {
+            BMCWEB_LOG_ERROR << "Failed to get chassis and sensor Node "
+                                "from "
+                             << uri;
+            messages::propertyValueIncorrect(asyncResp->res, uri,
+                                             "MetricProperties/" +
+                                                 std::to_string(uriIdx));
+            return false;
+        }
+
+        if (boost::ends_with(node, "#"))
+        {
+            node.pop_back();
+        }
+
+        matched.emplace(std::move(chassis), std::move(node));
+        uriIdx++;
+    }
+    return true;
+}
+
+inline std::optional<std::string>
+    redfishActionToDbusAction(const std::string& redfishAction)
+{
+    if (redfishAction == "RedfishMetricReport")
+    {
+        return "UpdateReport";
+    }
+    if (redfishAction == "RedfishEvent")
+    {
+        return "RedfishEvent";
+    }
+    if (redfishAction == "LogToLogService")
+    {
+        return "LogToLogService";
+    }
+    return std::nullopt;
+}
+
 } // namespace telemetry
 } // namespace redfish
