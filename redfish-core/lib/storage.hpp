d7bb16ae4cde6f28dc8547a2c5d94be7268c1258
Willy Tu <wltu@google.com>
2021-07-15 23:42:58 -0700

    Drive: Add MediaType Resource for Drive
    Populate the MediaType with DriveType to identify the Drive.
    
    Tested:
    
    Passed Redfish Validator with no new error.
    ```
    *** /redfish/v1/Systems/system/Storage/storage0/Drives/drive0
             Type (#Drive.v1_7_0.Drive), GET SUCCESS (time: 0.307086)
             PASS
    ```
    
    ```
    {
      "@odata.id": "/redfish/v1/Systems/system/Storage/storage0/Drives/drive0",
      "@odata.type": "#Drive.v1_7_0.Drive",
      "Id": "drive0",
      "MediaType": "SSD",
      "Name": "drive0",
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "Enabled"
      }
    }
    ```
    
    Change-Id: Iceba90b39bd2d7a423c7fae03760b81a8e010606
    Signed-off-by: Willy Tu <wltu@google.com>

--- redfish-core/lib/storage.hpp
+++ redfish-core/lib/storage.hpp
@@ -264,187 +264,249 @@ inline void requestRoutesStorage(App& app)
         });
 }
 
+inline void getDriveAsset(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                          const std::string& connectionName,
+                          const std::string& path)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp](
+            const boost::system::error_code ec,
+            const std::vector<std::pair<
+                std::string, std::variant<bool, std::string, uint64_t>>>&
+                propertiesList) {
+            if (ec)
+            {
+                // this interface isn't necessary
+                return;
+            }
+            for (const std::pair<std::string,
+                                 std::variant<bool, std::string, uint64_t>>&
+                     property : propertiesList)
+            {
+                // Store DBus properties that are also
+                // Redfish properties with same name and a
+                // string value
+                const std::string& propertyName = property.first;
+                if ((propertyName == "PartNumber") ||
+                    (propertyName == "SerialNumber") ||
+                    (propertyName == "Manufacturer") ||
+                    (propertyName == "Model"))
+                {
+                    const std::string* value =
+                        std::get_if<std::string>(&property.second);
+                    if (value == nullptr)
+                    {
+                        // illegal property
+                        messages::internalError(asyncResp->res);
+                        return;
+                    }
+                    asyncResp->res.jsonValue[propertyName] = *value;
+                }
+            }
+        },
+        connectionName, path, "org.freedesktop.DBus.Properties", "GetAll",
+        "xyz.openbmc_project.Inventory.Decorator.Asset");
+}
+
+inline void getDrivePresent(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                            const std::string& connectionName,
+                            const std::string& path)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, path](const boost::system::error_code ec,
+                          const std::variant<bool> present) {
+            // this interface isn't necessary, only check it if
+            // we get a good return
+            if (ec)
+            {
+                return;
+            }
+            const bool* enabled = std::get_if<bool>(&present);
+            if (enabled == nullptr)
+            {
+                BMCWEB_LOG_DEBUG << "Illegal property present";
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            if (!(*enabled))
+            {
+                asyncResp->res.jsonValue["Status"]["State"] = "Disabled";
+            }
+        },
+        connectionName, path, "org.freedesktop.DBus.Properties", "Get",
+        "xyz.openbmc_project.Inventory.Item", "Present");
+}
+
+inline void getDriveState(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                          const std::string& connectionName,
+                          const std::string& path)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp](const boost::system::error_code ec,
+                    const std::variant<bool> rebuilding) {
+            // this interface isn't necessary, only check it
+            // if we get a good return
+            if (ec)
+            {
+                return;
+            }
+            const bool* updating = std::get_if<bool>(&rebuilding);
+            if (updating == nullptr)
+            {
+                BMCWEB_LOG_DEBUG << "Illegal property present";
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            // updating and disabled in the backend
+            // shouldn't be able to be set at the same time,
+            // so we don't need to check for the race
+            // condition of these two calls
+            if (*updating)
+            {
+                asyncResp->res.jsonValue["Status"]["State"] = "Updating";
+            }
+        },
+        connectionName, path, "org.freedesktop.DBus.Properties", "Get",
+        "xyz.openbmc_project.State.Drive", "Rebuilding");
+}
+
+inline std::optional<std::string> convertDriveType(const std::string& driveType)
+{
+    if (driveType == "xyz.openbmc_project.Inventory.Item.Drive.DriveType.HDD")
+    {
+        return "HDD";
+    }
+    if (driveType == "xyz.openbmc_project.Inventory.Item.Drive.DriveType.SSD")
+    {
+        return "SSD";
+    }
+
+    return std::nullopt;
+}
+
+inline void getDriveType(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                         const std::string& connectionName,
+                         const std::string& path)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, path](const boost::system::error_code ec2,
+                          const std::variant<std::string> type) {
+            if (ec2)
+            {
+                BMCWEB_LOG_ERROR << "Missing property Drive Type";
+                return;
+            }
+
+            const std::string* foundType = std::get_if<std::string>(&type);
+            if (foundType == nullptr)
+            {
+                BMCWEB_LOG_ERROR << "Missing property Drive Type";
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            std::optional<std::string> driveType = convertDriveType(*foundType);
+            if (!driveType)
+            {
+                BMCWEB_LOG_DEBUG << *foundType
+                                 << " is not a supported Drive Type";
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            asyncResp->res.jsonValue["MediaType"] = *driveType;
+        },
+        connectionName, path, "org.freedesktop.DBus.Properties", "Get",
+        "xyz.openbmc_project.Inventory.Item.Drive", "Type");
+}
+
 inline void requestRoutesDrive(App& app)
 {
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/system/Storage/1/Drives/<str>/")
         .privileges(redfish::privileges::getDrive)
-        .methods(
-            boost::beast::http::verb::get)([](const crow::Request&,
-                                              const std::shared_ptr<
-                                                  bmcweb::AsyncResp>& asyncResp,
-                                              const std::string& driveId) {
-            crow::connections::systemBus->async_method_call(
-                [asyncResp,
-                 driveId](const boost::system::error_code ec,
-                          const crow::openbmc_mapper::GetSubTreeType& subtree) {
-                    if (ec)
-                    {
-                        BMCWEB_LOG_ERROR << "Drive mapper call error";
-                        messages::internalError(asyncResp->res);
-                        return;
-                    }
+        .methods(boost::beast::http::verb::get)(
+            [](const crow::Request&,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+               const std::string& driveId) {
+                crow::connections::systemBus->async_method_call(
+                    [asyncResp, driveId](
+                        const boost::system::error_code ec,
+                        const crow::openbmc_mapper::GetSubTreeType& subtree) {
+                        if (ec)
+                        {
+                            BMCWEB_LOG_ERROR << "Drive mapper call error";
+                            messages::internalError(asyncResp->res);
+                            return;
+                        }
 
-                    auto object2 = std::find_if(
-                        subtree.begin(), subtree.end(),
-                        [&driveId](auto& object) {
-                            const std::string& path = object.first;
-                            return boost::ends_with(path, "/" + driveId);
-                        });
+                        auto object2 = std::find_if(
+                            subtree.begin(), subtree.end(),
+                            [&driveId](auto& object) {
+                                const std::string& path = object.first;
+                                return boost::ends_with(path, "/" + driveId);
+                            });
 
-                    if (object2 == subtree.end())
-                    {
-                        messages::resourceNotFound(asyncResp->res, "Drive",
-                                                   driveId);
-                        return;
-                    }
+                        if (object2 == subtree.end())
+                        {
+                            messages::resourceNotFound(asyncResp->res, "Drive",
+                                                       driveId);
+                            return;
+                        }
 
-                    const std::string& path = object2->first;
-                    const std::vector<
-                        std::pair<std::string, std::vector<std::string>>>&
-                        connectionNames = object2->second;
+                        const std::string& path = object2->first;
+                        const std::vector<
+                            std::pair<std::string, std::vector<std::string>>>&
+                            interfaceMaps = object2->second;
 
-                    asyncResp->res.jsonValue["@odata.type"] =
-                        "#Drive.v1_7_0.Drive";
-                    asyncResp->res.jsonValue["@odata.id"] =
-                        "/redfish/v1/Systems/system/Storage/1/Drives/" +
-                        driveId;
-                    asyncResp->res.jsonValue["Name"] = driveId;
-                    asyncResp->res.jsonValue["Id"] = driveId;
+                        asyncResp->res.jsonValue["@odata.type"] =
+                            "#Drive.v1_7_0.Drive";
+                        asyncResp->res.jsonValue["@odata.id"] =
+                            "/redfish/v1/Systems/system/Storage/1/Drives/" +
+                            driveId;
+                        asyncResp->res.jsonValue["Name"] = driveId;
+                        asyncResp->res.jsonValue["Id"] = driveId;
 
-                    if (connectionNames.size() != 1)
-                    {
-                        BMCWEB_LOG_ERROR << "Connection size "
-                                         << connectionNames.size()
-                                         << ", greater than 1";
-                        messages::internalError(asyncResp->res);
-                        return;
-                    }
+                        if (interfaceMaps.size() != 1)
+                        {
+                            BMCWEB_LOG_ERROR << "Connection size "
+                                             << interfaceMaps.size()
+                                             << ", greater than 1";
+                            messages::internalError(asyncResp->res);
+                            return;
+                        }
 
-                    getMainChassisId(
-                        asyncResp,
-                        [](const std::string& chassisId,
-                           const std::shared_ptr<bmcweb::AsyncResp>& aRsp) {
-                            aRsp->res.jsonValue["Links"]["Chassis"] = {
-                                {"@odata.id",
-                                 "/redfish/v1/Chassis/" + chassisId}};
-                        });
+                        getMainChassisId(
+                            asyncResp,
+                            [](const std::string& chassisId,
+                               const std::shared_ptr<bmcweb::AsyncResp>& aRsp) {
+                                aRsp->res.jsonValue["Links"]["Chassis"] = {
+                                    {"@odata.id",
+                                     "/redfish/v1/Chassis/" + chassisId}};
+                            });
 
-                    const std::string& connectionName =
-                        connectionNames[0].first;
-                    crow::connections::systemBus->async_method_call(
-                        [asyncResp](
-                            const boost::system::error_code ec2,
-                            const std::vector<std::pair<
-                                std::string,
-                                std::variant<bool, std::string, uint64_t>>>&
-                                propertiesList) {
-                            if (ec2)
-                            {
-                                // this interface isn't necessary
-                                return;
-                            }
-                            for (const std::pair<
-                                     std::string,
-                                     std::variant<bool, std::string, uint64_t>>&
-                                     property : propertiesList)
-                            {
-                                // Store DBus properties that are also
-                                // Redfish properties with same name and a
-                                // string value
-                                const std::string& propertyName =
-                                    property.first;
-                                if ((propertyName == "PartNumber") ||
-                                    (propertyName == "SerialNumber") ||
-                                    (propertyName == "Manufacturer") ||
-                                    (propertyName == "Model"))
-                                {
-                                    const std::string* value =
-                                        std::get_if<std::string>(
-                                            &property.second);
-                                    if (value == nullptr)
-                                    {
-                                        // illegal property
-                                        messages::internalError(asyncResp->res);
-                                        return;
-                                    }
-                                    asyncResp->res.jsonValue[propertyName] =
-                                        *value;
-                                }
-                            }
-                        },
-                        connectionName, path, "org.freedesktop.DBus.Properties",
-                        "GetAll",
-                        "xyz.openbmc_project.Inventory.Decorator.Asset");
+                        const std::string& connectionName =
+                            interfaceMaps[0].first;
 
-                    // default it to Enabled
-                    asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
+                        // default it to Enabled
+                        asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
 
-                    auto health = std::make_shared<HealthPopulate>(asyncResp);
-                    health->inventory.emplace_back(path);
-                    health->populate();
+                        auto health =
+                            std::make_shared<HealthPopulate>(asyncResp);
+                        health->inventory.emplace_back(path);
+                        health->populate();
 
-                    crow::connections::systemBus->async_method_call(
-                        [asyncResp, path](const boost::system::error_code ec2,
-                                          const std::variant<bool> present) {
-                            // this interface isn't necessary, only check it if
-                            // we get a good return
-                            if (ec2)
-                            {
-                                return;
-                            }
-                            const bool* enabled = std::get_if<bool>(&present);
-                            if (enabled == nullptr)
-                            {
-                                BMCWEB_LOG_DEBUG << "Illegal property present";
-                                messages::internalError(asyncResp->res);
-                                return;
-                            }
-                            if (!(*enabled))
-                            {
-                                asyncResp->res.jsonValue["Status"]["State"] =
-                                    "Disabled";
-                            }
-                        },
-                        connectionName, path, "org.freedesktop.DBus.Properties",
-                        "Get", "xyz.openbmc_project.Inventory.Item", "Present");
-
-                    crow::connections::systemBus->async_method_call(
-                        [asyncResp](const boost::system::error_code ec2,
-                                    const std::variant<bool> rebuilding) {
-                            // this interface isn't necessary, only check it if
-                            // we get a good return
-                            if (ec2)
-                            {
-                                return;
-                            }
-                            const bool* updating =
-                                std::get_if<bool>(&rebuilding);
-                            if (updating == nullptr)
-                            {
-                                BMCWEB_LOG_DEBUG << "Illegal property present";
-                                messages::internalError(asyncResp->res);
-                                return;
-                            }
-
-                            // updating and disabled in the backend shouldn't be
-                            // able to be set at the same time, so we don't need
-                            // to check for the race condition of these two
-                            // calls
-                            if ((*updating))
-                            {
-                                asyncResp->res.jsonValue["Status"]["State"] =
-                                    "Updating";
-                            }
-                        },
-                        connectionName, path, "org.freedesktop.DBus.Properties",
-                        "Get", "xyz.openbmc_project.State.Drive", "Rebuilding");
-                },
-                "xyz.openbmc_project.ObjectMapper",
-                "/xyz/openbmc_project/object_mapper",
-                "xyz.openbmc_project.ObjectMapper", "GetSubTree",
-                "/xyz/openbmc_project/inventory", int32_t(0),
-                std::array<const char*, 1>{
-                    "xyz.openbmc_project.Inventory.Item.Drive"});
-        });
+                        getDriveAsset(asyncResp, connectionName, path);
+                        getDrivePresent(asyncResp, connectionName, path);
+                        getDriveState(asyncResp, connectionName, path);
+                        getDriveType(asyncResp, connectionName, path);
+                    },
+                    "xyz.openbmc_project.ObjectMapper",
+                    "/xyz/openbmc_project/object_mapper",
+                    "xyz.openbmc_project.ObjectMapper", "GetSubTree",
+                    "/xyz/openbmc_project/inventory", int32_t(0),
+                    std::array<const char*, 1>{
+                        "xyz.openbmc_project.Inventory.Item.Drive"});
+            });
 }
 } // namespace redfish
