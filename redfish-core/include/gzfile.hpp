3a51dcb81ea2edd48e2498d3284460ea43b7556f
SpencerKu <Spencer.Ku@quantatw.com>
2021-07-22 14:57:02 +0800

    Redfish: Support Host Log Entries
    This commit is designing and implementing a new logging service in
    Redfish to expose host serial console logs.The goal is that clients
    can talk to bmc via Redfish and get a real-time console. It will improve
    the debuggability of BMCs.
    
    We will add three paths to redfish tree to implement the feature:
    1. /redfish/v1/Systems/system/LogServices/HostLogger
    2. /redfish/v1/Systems/system/LogServices/HostLogger/Entries
    3. /redfish/v1/Systems/system/LogServices/HostLogger/Entries/<str>
    
    To use this feature, we expect to use phosphor-hostlogger(stream mode)
    + rsyslog + bmcweb. Phosphor-hostlooger in stream mode forwards the byte
    stream into rsyslog via the imuxsock module. The log is persisted via
    the omfile module as soon as collected. It makes Host Logger leverage
    exsisting tools (rsyslog and logrotate). Then we can expose host serial
    console logs via bmcweb.
    
    This feature can be enabled or disabled by setting the option
    "redfish-host-logger", and the default value is "enabled". If you don't
    want to expose host serial console logs, you need to turn the value to
    "disabled".
    
    RedfishServiceValidator results:
    /redfish/v1/Systems/system/LogServices/HostLogger 
    pass: 4
    passGet: 1
    skipOptional: 9
    
    /redfish/v1/Systems/system/LogServices/HostLogger/Entries
    pass: 24
    passGet: 1
    skipOptional: 1
    
    /redfish/v1/Systems/system/LogServices/HostLogger/Entries/<str>
    pass: 7
    passGet: 1
    skipOptional: 15
    
    Sample Output:
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/HostLogger/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/HostLogger/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of HostLogger Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/HostLogger/Entries/1",
          "@odata.type": "#LogEntry.v1_4_0.LogEntry",
          "EntryType": "Event",
          "Id": "1",
          "Message": "[   57.061546] gq 0000:16:00.0 eth0: link up, 100 Gbps, no PAUSE",
          "MessageArgs": [
            "[   57.061546] gq 0000:16:00.0 eth0: link up, 100 Gbps, no PAUSE"
          ],
          "MessageId": "OpenBMC.0.1.SerialLogAdded",
          "Name": "HostLogger Entries",
          "Severity": "OK"
        },
      ...
      ],
      "Members@odata.count": 22,
      "Name": "HostLogger Entries"
    }
    
    Signed-off-by: SpencerKu <Spencer.Ku@quantatw.com>
    Change-Id: I5a7873caa117400fb0a737588a50bd743e8b5063

---
+++ redfish-core/include/gzfile.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <zlib.h>
+
+#include <array>
+#include <filesystem>
+
+class GzFile
+{
+  public:
+    GzFile(const std::string& filename) :
+        logStream(gzopen(filename.c_str(), "r"))
+    {
+        // If gzopen failed, set Z_NULL to logStream.
+        if (!logStream)
+        {
+            BMCWEB_LOG_ERROR << std::strerror(errno);
+            logStream = Z_NULL;
+        }
+    }
+
+    ~GzFile()
+    {
+        gzclose(logStream);
+    }
+
+    bool gzGetLine(std::string& buf)
+    {
+        constexpr int lineLimit = 8192;
+        // obmc-console define buffer size as uint8_t buf[4096] to catch one
+        // entry, and it may contain several log entry in one line created by
+        // phosphor-hostlogger stream mode. So we define 8192 as the buffer
+        // limit size to unzip a message.
+
+        buf.clear();
+        buf.resize(lineLimit);
+
+        // On failure, gzgets() shall return Z_NULL.
+        if (gzgets(logStream, buf.data(), static_cast<int>(buf.size())) ==
+            Z_NULL)
+        {
+            if (!gzeof(logStream))
+            {
+                int zErrnum = 0;
+                const char* errmsg = gzerror(logStream, &zErrnum);
+
+                BMCWEB_LOG_ERROR << "gzGetLine error:\n"
+                                 << "Msg: " << errmsg << '\n'
+                                 << "Errnum: " << zErrnum;
+            }
+            return false;
+        }
+        buf.resize(std::strlen(buf.data()));
+        return true;
+    }
+
+  private:
+    gzFile logStream;
+
+  public:
+    GzFile(const GzFile&) = delete;
+    GzFile& operator=(const GzFile&) = delete;
+};
