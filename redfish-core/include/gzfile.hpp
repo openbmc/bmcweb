731637e209c520f27bd1322b0f2385b0a45e0eed
SpencerKu <Spencer.Ku@quantatw.com>
2021-07-22 14:57:02 +0800

    Redfish: Support Host Log Entries
    This commit is designing and implementing a new logging service in
    Redfish to expose host serial console logs.The goal is that clients
    can talk to bmc via Redfish and get a real-time console. It will improve
    the debuggability of BMCs.
    
    We will add three paths to redfish tree to implement the feature:
    1. /redfish/v1/Systems/system/LogServices/HostLogger
    2. /redfish/v1/Systems/system/LogServices/HostLogger/Entries
    3. /redfish/v1/Systems/system/LogServices/HostLogger/Entries/<str>
    
    To use this feature, we expect to use phosphor-hostlogger(stream mode)
    + rsyslog + bmcweb. Phosphor-hostlooger in stream mode forwards the byte
    stream into rsyslog via the imuxsock module. The log is persisted via
    the omfile module as soon as collected. It makes Host Logger leverage
    exsisting tools (rsyslog and logrotate). Then we can expose host serial
    console logs via bmcweb.
    
    This feature can be enabled or disabled by setting the option
    "redfish-host-logger", and the default value is "enabled". If you don't
    want to expose host serial console logs, you need to turn the value to
    "disabled".
    
    RedfishServiceValidator results:
    /redfish/v1/Systems/system/LogServices/HostLogger 
    pass: 4
    passGet: 1
    skipOptional: 9
    
    /redfish/v1/Systems/system/LogServices/HostLogger/Entries
    pass: 24
    passGet: 1
    skipOptional: 1
    
    /redfish/v1/Systems/system/LogServices/HostLogger/Entries/<str>
    pass: 7
    passGet: 1
    skipOptional: 15
    
    Sample Output:
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/HostLogger/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/HostLogger/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of HostLogger Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/HostLogger/Entries/1",
          "@odata.type": "#LogEntry.v1_4_0.LogEntry",
          "EntryType": "Event",
          "Id": "1",
          "Message": "[   57.061546] gq 0000:16:00.0 eth0: link up, 100 Gbps, no PAUSE",
          "MessageArgs": [
            "[   57.061546] gq 0000:16:00.0 eth0: link up, 100 Gbps, no PAUSE"
          ],
          "MessageId": "OpenBMC.0.1.SerialLogAdded",
          "Name": "HostLogger Entries",
          "Severity": "OK"
        },
      ...
      ],
      "Members@odata.count": 22,
      "Name": "HostLogger Entries"
    }
    
    Signed-off-by: SpencerKu <Spencer.Ku@quantatw.com>
    Change-Id: I5a7873caa117400fb0a737588a50bd743e8b5063

---
+++ redfish-core/include/gzfile.hpp
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <zlib.h>
+
+#include <array>
+#include <filesystem>
+#include <vector>
+
+class GzFileReader
+{
+  public:
+    std::string lastMessage;
+    bool messageNotComplete = false;
+
+    GzFileReader(const std::string& filename) :
+        logStream(gzopen(filename.c_str(), "r"))
+    {
+        // If gzopen failed, set Z_NULL to logStream.
+        if (!logStream)
+        {
+            BMCWEB_LOG_ERROR << "Can't open gz file: " << filename << '\n';
+            logStream = Z_NULL;
+        }
+    }
+
+    ~GzFileReader()
+    {
+        gzclose(logStream);
+    }
+
+    bool gzGetLines(std::vector<std::string>& logEntries)
+    {
+        std::string wholeFile;
+        if (!readFile(wholeFile))
+        {
+            return false;
+        }
+
+        std::vector<std::string> parseLogs;
+        parseLogs = hostLogEntryParser(wholeFile);
+        if (messageNotComplete)
+        {
+            parseLogs.front() = lastMessage + parseLogs.front();
+            messageNotComplete = false;
+        }
+        logEntries.insert(logEntries.end(), parseLogs.begin(), parseLogs.end());
+        return true;
+    }
+
+  private:
+    gzFile logStream;
+
+    void printErrorMessage()
+    {
+        int errNum = 0;
+        const char* errMsg = gzerror(logStream, &errNum);
+
+        BMCWEB_LOG_ERROR << "Error reading gz compressed data.\n"
+                         << "Error Message: " << errMsg << '\n'
+                         << "Error Number: " << errNum;
+    }
+
+    bool readFile(std::string& wholeFile)
+    {
+        constexpr int bufferLimitSize = 1024;
+        do
+        {
+            std::string bufferStr;
+            bufferStr.resize(bufferLimitSize);
+
+            int bytesRead = 0;
+            bytesRead = gzread(logStream, &bufferStr[0], bufferLimitSize);
+            // On errors, gzread() shall return a value less than 0.
+            if (bytesRead < 0)
+            {
+                printErrorMessage();
+                return false;
+            }
+            // bytesRead is supposed to be equal to bufferLimitSize in normal
+            // cases; it can be smaller if we reach the end of the file;
+            // otherwise, we must have a failure.
+            if (bytesRead < bufferLimitSize && !gzeof(logStream))
+            {
+                printErrorMessage();
+                return false;
+            }
+            bufferStr.resize(static_cast<unsigned int>(bytesRead));
+            wholeFile += bufferStr;
+        } while (!gzeof(logStream));
+
+        return true;
+    }
+
+    std::vector<std::string> hostLogEntryParser(std::string wholeFile)
+    {
+        std::string logEntry;
+        std::vector<std::string> logEntries;
+
+        // It may contain several log entry in one line, and
+        // the end of each log entry will be '\r\n' or '\r'.
+        // So we need to go through and split string by '\n' and '\r'
+        size_t pos = wholeFile.find_first_of("\n\r");
+        size_t initialPos = 0;
+
+        while (pos != std::string::npos)
+        {
+            logEntry = wholeFile.substr(initialPos, pos - initialPos);
+            if (!logEntry.empty())
+            {
+                logEntries.push_back(logEntry);
+            }
+            initialPos = pos + 1;
+            pos = wholeFile.find_first_of("\n\r", initialPos);
+        }
+
+        // Store the last message
+        if (initialPos < wholeFile.size())
+        {
+            lastMessage = wholeFile.substr(initialPos);
+            messageNotComplete = true;
+        }
+
+        return logEntries;
+    }
+
+  public:
+    GzFileReader(const GzFileReader&) = delete;
+    GzFileReader& operator=(const GzFileReader&) = delete;
+};
