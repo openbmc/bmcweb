332c3106255653c3994d42d40294f0f5c9340472
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-07-22 11:39:34 -0500

    redfish-core: LogServices: Added HardwareIsolation service
    - In an OpenPOWER based system, a user or an application (in the
      error path if the hardware is faulty) can isolate hardware and
      the respective isolated hardware part will be ignored to init
      during the next boot of the host.
    
    - The isolated hardware details are stored in BMC and a user may
      need to get that information through an external interface.
    
    - In this patch, added a new HardwareIsolation LogService to get
      or manage the isolated hardware.
    
    - This HardwareIsolation feature can be enabled by using the
      "hw-isolation" compile-time feature flag. It is disabled by default.
    
    Other Reference:
    
    - Design document for hardware isolation (aka guard)
    https://github.com/openbmc/docs/blob/master/designs/guard-on-bmc.md
    
    Tested:
    
    - Checked whether the HardwareIsolation LogService is showing in
      LogServiceCollection.
    ```
    curl -k -H "X-Auth-Token: $bmc_token" -X GET \
    https://${bmc}/redfish/v1/Systems/system/LogServices
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices",
      "@odata.type": "#LogServiceCollection.LogServiceCollection",
      "Description": "Collection of LogServices for this Computer System",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/PostCodes"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/HardwareIsolation"
        }
      ],
      "Members@odata.count": 4,
      "Name": "System Log Services Collection"
    }
    ```
    
    - Checked whether the HardwareIsolation LogService is showing with all
      required LogService members.
    ```
    curl -k -H "X-Auth-Token: $bmc_token" -X GET \
    https://${bmc}/redfish/v1/Systems/system/LogServices/HardwareIsolation
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/HardwareIsolation",
      "@odata.type": "#LogService.v1_2_0.LogService",
      "Actions": {
        "#LogService.ClearLog": {
          "target": "/redfish/v1/Systems/system/LogServices/HardwareIsolation/Actions \
                     /LogService.ClearLog"
        }
      },
      "Description": "Hardware Isolation LogService for system owned devices",
      "Entries": {
        "@odata.id": "/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries"
      },
      "Id": "HardwareIsolation",
      "Name": "Hardware Isolation LogService"
    }
    ```
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: I609ffcb7b05a0c4a5b283fbb127f222d9204fefe

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -1649,6 +1649,15 @@ inline void requestRoutesSystemLogServiceCollection(App& app)
             "/redfish/v1/Systems/system/LogServices/AuditLog";
         logServiceArray.push_back(std::move(auditLog));
 #endif // BMCWEB_ENABLE_LINUX_AUDIT_EVENTS
+#ifdef BMCWEB_ENABLE_HW_ISOLATION
+        nlohmann::json& logServiceArrayLocal =
+            asyncResp->res.jsonValue["Members"];
+        logServiceArrayLocal.push_back(
+            {{"@odata.id", "/redfish/v1/Systems/system/"
+                           "LogServices/HardwareIsolation"}});
+        asyncResp->res.jsonValue["Members@odata.count"] =
+            logServiceArrayLocal.size();
+#endif // BMCWEB_ENABLE_HW_ISOLATION
     });
 }
 
@@ -6344,5 +6353,56 @@ inline void requestRoutesFullAuditLogDownload(App& app)
 }
 
 #endif // BMCWEB_ENABLE_LINUX_AUDIT_EVENTS
+#ifdef BMCWEB_ENABLE_HW_ISOLATION
+/**
+ * @brief API Used to add the supported HardwareIsolation LogServices Members
+ *
+ * @param[in] req - The HardwareIsolation redfish request (unused now).
+ * @param[in] asyncResp - The redfish response to return.
+ *
+ * @return The redfish response in the given buffer.
+ */
+inline void getSystemHardwareIsolationLogService(
+    const crow::Request& /* req */,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    asyncResp->res.jsonValue["@odata.id"] =
+        "/redfish/v1/Systems/system/LogServices/"
+        "HardwareIsolation";
+    asyncResp->res.jsonValue["@odata.type"] = "#LogService.v1_2_0.LogService";
+    asyncResp->res.jsonValue["Name"] = "Hardware Isolation LogService";
+    asyncResp->res.jsonValue["Description"] =
+        "Hardware Isolation LogService for system owned "
+        "devices";
+    asyncResp->res.jsonValue["Id"] = "HardwareIsolation";
+
+    asyncResp->res.jsonValue["Entries"] = {
+        {"@odata.id", "/redfish/v1/Systems/system/LogServices/"
+                      "HardwareIsolation/Entries"}};
+
+    asyncResp->res.jsonValue["Actions"] = {
+        {"#LogService.ClearLog",
+         {{"target", "/redfish/v1/Systems/system/LogServices/"
+                     "HardwareIsolation/Actions/"
+                     "LogService.ClearLog"}}}};
+}
+
+/**
+ * @brief API used to route the handler for HardwareIsolation Redfish
+ *        LogServices URI
+ *
+ * @param[in] app - Crow app on which Redfish will initialize
+ *
+ * @return The appropriate redfish response for the given redfish request.
+ */
+inline void requestRoutesSystemHardwareIsolationLogService(App& app)
+{
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/Systems/system/LogServices/HardwareIsolation/")
+        .privileges(redfish::privileges::getLogService)
+        .methods(boost::beast::http::verb::get)(
+            getSystemHardwareIsolationLogService);
+}
+#endif // BMCWEB_ENABLE_HW_ISOLATION
 
 } // namespace redfish
