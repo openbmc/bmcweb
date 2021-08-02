922b04fe0fd10e377df63ea0c299e6b4b03f8600
Lukasz Kazmierczak <lukasz.kazmierczak@intel.com>
2021-08-02 13:40:27 +0200

    Add GET method for Triggers
    Added GET method for retrieving details of individual Trigger searched
    by given Trigger name, details are extracted from Telemetry service
    
    Tested:
    - Added single Trigger and requested result from bmcweb via
      /redfish/v1/TelemetryService/Triggers/<triggername>
    - Added multiple Triggers numeric and discrete with various parameters
      (empty, non-empty), and requested results from bmcweb via
      /redfish/v1/TelemetryService/Triggers/<triggername>
    - Verified uris /redfish/v1/TelemetryService/Triggers/<triggername> by
      using Redfish-Service-Validator (all passed)
    
    Signed-off-by: Lukasz Kazmierczak <lukasz.kazmierczak@intel.com>
    Change-Id: I1c966b2f792324cc6f6a8784ad18a683e5ce7bd9

--- scripts/update_schemas.py
+++ scripts/update_schemas.py
@@ -96,6 +96,7 @@ include_list = [
     'TaskService',
     'TelemetryService',
     'Thermal',
+    'Triggers',
     'UpdateService',
     'VLanNetworkInterfaceCollection',
     'VLanNetworkInterface',
