b3e6fc6d933bf38c7a3a568fb782076fb911c42f
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-08-03 01:35:23 -0500

    Add support for hardware dump
    This commit contains changes that supports listing and
    deletion of hardware dump. The hardware dumps will be listed under
    /redfish/v1/Systems/system/LogServices/Dump along with
    the system, resource and hostboot dump entries with the entry id
    prepended with "Hardware_".
    
    Tested By:
    
    GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System, Resource, Hostboot & Hardware Dump Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump/Entries/Hardware_2",
          "@odata.type": "#LogEntry.v1_7_0.LogEntry",
          "AdditionalDataSizeBytes": 0,
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/Dump/Entries/Hardware_2/attachment",
          "Created": "1970-01-01T00:00:00+00:00",
          "DiagnosticDataType": "OEM",
          "EntryType": "Event",
          "Id": "Hardware_2",
          "Name": "System Dump Entry",
          "OEMDiagnosticDataType": "Hardware"
        }
      ],
      "Members@odata.count": 1,
      "Name": "System Dump Entries"
    }
    
    GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/Hardware_1
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump/Entries/Hardware_1",
      "@odata.type": "#LogEntry.v1_7_0.LogEntry",
      "AdditionalDataSizeBytes": 0,
      "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/Dump/Entries/Hardware_1/attachment",
      "Created": "1970-01-01T00:00:00+00:00",
      "DiagnosticDataType": "OEM",
      "EntryType": "Event",
      "Id": "Hardware_1",
      "Name": "Hardware Dump Entry",
      "OEMDiagnosticDataType": "Hardware"
    }
    
    curl -k -H "X-Auth-Token:$bmc_token" -X POST https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.ClearLog
    
    curl -k -H "X-Auth-Token:$bmc_token" -X DELETE https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/Hardware_1
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: Idb76c81379236bd4209df428d7c8727278003d7f

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -412,7 +412,7 @@ inline void
         dumpPath = "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/";
     }
     else if (dumpType == "System" || dumpType == "Resource" ||
-             dumpType == "Hostboot")
+             dumpType == "Hostboot" || dumpType == "Hardware")
     {
         dumpPath = "/redfish/v1/Systems/system/LogServices/Dump/Entries/";
     }
@@ -521,7 +521,7 @@ inline void
                         entryID + "/attachment";
                 }
                 else if (dumpType == "System" || dumpType == "Resource" ||
-                         dumpType == "Hostboot")
+                         dumpType == "Hostboot" || dumpType == "Hardware")
                 {
                     std::string dumpEntryId(dumpType + "_");
                     dumpEntryId.append(entryID);
@@ -554,7 +554,7 @@ inline void
         dumpId = entryID;
     }
     else if (dumpType == "System" || dumpType == "Resource" ||
-             dumpType == "Hostboot")
+             dumpType == "Hostboot" || dumpType == "Hardware")
     {
         dumpPath = "/redfish/v1/Systems/system/LogServices/Dump/Entries/";
         std::size_t pos = entryID.find_first_of('_');
@@ -660,7 +660,7 @@ inline void
                         entryID + "/attachment";
                 }
                 else if (dumpType == "System" || dumpType == "Resource" ||
-                         dumpType == "Hostboot")
+                         dumpType == "Hostboot" || dumpType == "Hardware")
                 {
                     std::string dumpAttachment(
                         "/redfish/v1/Systems/system/LogServices/Dump/Entries/");
@@ -954,7 +954,8 @@ inline void clearDump(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                       const std::string& dumpType)
 {
     std::string dumpInterface;
-    if (dumpType == "Resource" || dumpType == "Hostboot")
+    if (dumpType == "Resource" || dumpType == "Hostboot" ||
+        dumpType == "Hardware")
     {
         dumpInterface = "com.ibm.Dump.Entry." + dumpType;
     }
@@ -2382,11 +2383,13 @@ inline void requestRoutesSystemDumpEntryCollection(App& app)
                     "/redfish/v1/Systems/system/LogServices/Dump/Entries";
                 asyncResp->res.jsonValue["Name"] = "System Dump Entries";
                 asyncResp->res.jsonValue["Description"] =
-                    "Collection of System, Resource & Hostboot Dump Entries";
+                    "Collection of System, Resource, Hostboot & Hardware Dump "
+                    "Entries";
 
                 getDumpEntryCollection(asyncResp, "System");
                 getDumpEntryCollection(asyncResp, "Resource");
                 getDumpEntryCollection(asyncResp, "Hostboot");
+                getDumpEntryCollection(asyncResp, "Hardware");
             });
 }
 
@@ -2411,6 +2414,10 @@ inline void requestRoutesSystemDumpEntry(App& app)
                 else if (boost::starts_with(param, "Hostboot"))
                 {
                     getDumpEntryById(asyncResp, param, "Hostboot");
+                }
+                else if (boost::starts_with(param, "Hardware"))
+                {
+                    getDumpEntryById(asyncResp, param, "Hardware");
                 }
                 else
                 {
@@ -2447,6 +2454,10 @@ inline void requestRoutesSystemDumpEntry(App& app)
                 {
                     deleteDumpEntry(asyncResp, dumpId, "hostboot");
                 }
+                else if (boost::starts_with(param, "Hardware"))
+                {
+                    deleteDumpEntry(asyncResp, dumpId, "hardware");
+                }
                 else
                 {
                     messages::invalidObject(asyncResp->res, "Dump Id");
@@ -2482,6 +2493,7 @@ inline void requestRoutesSystemDumpClear(App& app)
                 clearDump(asyncResp, "System");
                 clearDump(asyncResp, "Resource");
                 clearDump(asyncResp, "Hostboot");
+                clearDump(asyncResp, "Hardware");
             });
 }
 
