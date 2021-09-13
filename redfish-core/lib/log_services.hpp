fd4e75a6c7dbc881cf05e1b65ac8189c546197ba
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-09-13 03:01:29 -0500

    Add support for SBE dump
    SBE dump is basically the dump collected from
    Self Boot Engine(SBE) when it encounters an error
    and becomes unresponsive.
    
    This commit contains changes that supports listing and
    deletion of SBE dump. The SBE dumps will be listed under
    /redfish/v1/Systems/system/LogServices/Dump along with
    the system, resource hostboot and hardware dump entries
    with the entry id prepended with "SBE_".
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I41eb4759cac88dd6eb7ae1ef3b4fff22f66a3975

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -388,7 +388,8 @@ inline void
         dumpPath = "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/";
     }
     else if (dumpType == "System" || dumpType == "Resource" ||
-             dumpType == "Hostboot" || dumpType == "Hardware")
+             dumpType == "Hostboot" || dumpType == "Hardware" ||
+             dumpType == "SBE")
     {
         dumpPath = "/redfish/v1/Systems/system/LogServices/Dump/Entries/";
     }
@@ -526,7 +527,8 @@ inline void
                         entryID + "/attachment";
                 }
                 else if (dumpType == "System" || dumpType == "Resource" ||
-                         dumpType == "Hostboot" || dumpType == "Hardware")
+                         dumpType == "Hostboot" || dumpType == "Hardware" ||
+                         dumpType == "SBE")
                 {
                     std::string dumpEntryId(dumpType + "_");
                     dumpEntryId.append(entryID);
@@ -560,7 +562,8 @@ inline void
         dumpId = entryID;
     }
     else if (dumpType == "System" || dumpType == "Resource" ||
-             dumpType == "Hostboot" || dumpType == "Hardware")
+             dumpType == "Hostboot" || dumpType == "Hardware" ||
+             dumpType == "SBE")
     {
         dumpPath = "/redfish/v1/Systems/system/LogServices/Dump/Entries/";
         std::size_t pos = entryID.find_first_of('_');
@@ -697,7 +700,8 @@ inline void
                         entryID + "/attachment";
                 }
                 else if (dumpType == "System" || dumpType == "Resource" ||
-                         dumpType == "Hostboot" || dumpType == "Hardware")
+                         dumpType == "Hostboot" || dumpType == "Hardware" ||
+                         dumpType == "SBE")
                 {
                     std::string dumpAttachment(
                         "/redfish/v1/Systems/system/LogServices/Dump/Entries/");
@@ -992,7 +996,7 @@ inline void clearDump(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
 {
     std::string dumpInterface;
     if (dumpType == "Resource" || dumpType == "Hostboot" ||
-        dumpType == "Hardware")
+        dumpType == "Hardware" || dumpType == "SBE")
     {
         dumpInterface = "com.ibm.Dump.Entry." + dumpType;
     }
@@ -2395,23 +2399,25 @@ inline void requestRoutesSystemDumpEntryCollection(App& app)
      */
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/system/LogServices/Dump/Entries/")
         .privileges(redfish::privileges::getLogEntryCollection)
-        .methods(boost::beast::http::verb::get)(
-            [](const crow::Request&,
-               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
-                asyncResp->res.jsonValue["@odata.type"] =
-                    "#LogEntryCollection.LogEntryCollection";
-                asyncResp->res.jsonValue["@odata.id"] =
-                    "/redfish/v1/Systems/system/LogServices/Dump/Entries";
-                asyncResp->res.jsonValue["Name"] = "System Dump Entries";
-                asyncResp->res.jsonValue["Description"] =
-                    "Collection of System, Resource, Hostboot & Hardware Dump "
-                    "Entries";
+        .methods(
+            boost::beast::http::verb::
+                get)([](const crow::Request&,
+                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
+            asyncResp->res.jsonValue["@odata.type"] =
+                "#LogEntryCollection.LogEntryCollection";
+            asyncResp->res.jsonValue["@odata.id"] =
+                "/redfish/v1/Systems/system/LogServices/Dump/Entries";
+            asyncResp->res.jsonValue["Name"] = "System Dump Entries";
+            asyncResp->res.jsonValue["Description"] =
+                "Collection of System, Resource, Hostboot, Hardware & SBE Dump "
+                "Entries";
 
-                getDumpEntryCollection(asyncResp, "System");
-                getDumpEntryCollection(asyncResp, "Resource");
-                getDumpEntryCollection(asyncResp, "Hostboot");
-                getDumpEntryCollection(asyncResp, "Hardware");
-            });
+            getDumpEntryCollection(asyncResp, "System");
+            getDumpEntryCollection(asyncResp, "Resource");
+            getDumpEntryCollection(asyncResp, "Hostboot");
+            getDumpEntryCollection(asyncResp, "Hardware");
+            getDumpEntryCollection(asyncResp, "SBE");
+        });
 }
 
 inline void requestRoutesSystemDumpEntry(App& app)
@@ -2439,6 +2445,10 @@ inline void requestRoutesSystemDumpEntry(App& app)
                 else if (boost::starts_with(param, "Hardware"))
                 {
                     getDumpEntryById(asyncResp, param, "Hardware");
+                }
+                else if (boost::starts_with(param, "SBE"))
+                {
+                    getDumpEntryById(asyncResp, param, "SBE");
                 }
                 else
                 {
@@ -2479,6 +2489,10 @@ inline void requestRoutesSystemDumpEntry(App& app)
                 {
                     deleteDumpEntry(asyncResp, dumpId, "hardware");
                 }
+                else if (boost::starts_with(param, "SBE"))
+                {
+                    deleteDumpEntry(asyncResp, dumpId, "SBE");
+                }
                 else
                 {
                     messages::invalidObject(asyncResp->res, "Dump Id");
@@ -2515,6 +2529,7 @@ inline void requestRoutesSystemDumpClear(App& app)
                 clearDump(asyncResp, "Resource");
                 clearDump(asyncResp, "Hostboot");
                 clearDump(asyncResp, "Hardware");
+                clearDump(asyncResp, "SBE");
             });
 }
 
