a97c1eab64fc7d7966693fdbc05a7ab8908b44dd
Ed Tanous <edtanous@google.com>
2021-09-16 15:07:53 -0700

    Delete the copy constructor on the Request object
    This code was in the codebase previously, and at some point got removed
    to make copies.  Considering the previous commits to this one, making
    copies of the request object is a bad idea, and should be avoided.
    Therefore, this commit deletes the copy constructor for the Request
    object, making request copies fail to compile.
    
    It should be noted, it does leave the move constructor, which I don't
    think it's really used, but as a rule isn't an anti-pattern.
    
    Tested:
    Code compiles.  No functional change.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ib38ed79a7c60340fb00f922f29265a3c3c7beca8

--- http/http_request.hpp
+++ http/http_request.hpp
@@ -45,6 +45,9 @@ struct Request
         }
     }
 
+    Request(const Request&) = delete;
+    Request& operator=(const Request&) = delete;
+
     boost::beast::http::verb method() const
     {
         return req.method();
