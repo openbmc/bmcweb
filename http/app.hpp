cdb743922583dc4865f878b6e9039a8f2c687ff8
P Dheeraj Srujan Kumar <p.dheeraj.srujan.kumar@intel.com>
2021-09-18 01:19:04 +0530

    Add Support for privilege check in handleUpgrade
    This commit enables privilege check for user(s) in case of upgraded
    connections.
    Currently users with no privileges will also be able to access
    Websockets connections (Ex: KVM).
    
    The privilege check was already in place for normal connections (i.e.
    router->handle()). This commit lifts off the privilege check code and
    moves it into a common function (validatePrivilege()), which can be used
    both by handle() and handleUpgrade() and register required callback to
    be called.
    
    Also, the const qualifier for Request in the handleUpgrade() function's
    signature is removed to enable setting "isConfigureSelf" field of
    request. The signature of handleUpgrade() is made identical to handle()
    
    Tested:
     - websocket_test.py Passed
     - Admin and Operator users are able to access KVM on WebUI
     - Readonly User was unable to access KVM on WebUI
    
    Signed-off-by: P Dheeraj Srujan Kumar <p.dheeraj.srujan.kumar@intel.com>
    Change-Id: I6f743c27e7e6077f1c6c56e6958922027e4404e8

--- http/app.hpp
+++ http/app.hpp
@@ -58,7 +58,7 @@ class App
     App& operator=(const App&&) = delete;
 
     template <typename Adaptor>
-    void handleUpgrade(const Request& req,
+    void handleUpgrade(Request& req,
                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                        Adaptor&& adaptor)
     {
