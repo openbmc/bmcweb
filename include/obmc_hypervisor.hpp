73bf0d4d97d29bad24fbc67f71b68ee606533528
Abhishek Patel <Abhishek.Patel@ibm.com>
2021-09-22 08:53:17 -0500

    Added Hypervisor Serial Socket
    This commit adds `wss://${window.location.host}/console1` WebSocket
    into bmcweb. it retrieves 'PHYP debug shell' info and displays
    WebSocket data into WebUI using SOL (Serial over LAN) console.
    
    'console1' read data from UNIX domain socket '@obmc-console.hypervisor'
    and display it in WebUI.
    
    Privilege set to {"ConfigureComponents", "ConfigureManager"}.
    That mean only Administrator and Operator can retrieve console data.
    
    Tested:
      configure UNIX domain socket '@obmc-console.hypervisor' with
      'wss://${window.location.host}/console0' (which currently use for
      retrieve 'host serial console data on SOL'). The test is successful,
      able to receive 'PHYP debug shell' data in SOL on WebUI.
    
    Signed-off-by: Abhishek Patel <Abhishek.Patel@ibm.com>
    Change-Id: Icece6ceddd0eeab8db7ebf1e5e863c22a59ae45f

---
+++ include/obmc_hypervisor.hpp
@@ -0,0 +1,157 @@
+#pragma once
+#include <sys/socket.h>
+
+#include <app.hpp>
+#include <async_resp.hpp>
+#include <boost/asio/local/stream_protocol.hpp>
+#include <boost/container/flat_map.hpp>
+#include <boost/container/flat_set.hpp>
+#include <websocket.hpp>
+
+namespace crow
+{
+namespace obmc_hypervisor
+{
+
+static std::unique_ptr<boost::asio::local::stream_protocol::socket> hostSocket;
+
+static std::array<char, 4096> outputBuffer;
+static std::string inputBuffer;
+
+static boost::container::flat_set<crow::websocket::Connection*> sessions;
+
+static bool doingWrite = false;
+
+inline void doWrite()
+{
+    if (doingWrite)
+    {
+        BMCWEB_LOG_DEBUG << "Already writing.  Bailing out";
+        return;
+    }
+
+    if (inputBuffer.empty())
+    {
+        BMCWEB_LOG_DEBUG << "Outbuffer empty.  Bailing out";
+        return;
+    }
+
+    if (!hostSocket)
+    {
+        BMCWEB_LOG_ERROR << "doWrite(): Socket closed.";
+        return;
+    }
+
+    doingWrite = true;
+    hostSocket->async_write_some(
+        boost::asio::buffer(inputBuffer.data(), inputBuffer.size()),
+        [](boost::beast::error_code ec, std::size_t bytesWritten) {
+            doingWrite = false;
+            inputBuffer.erase(0, bytesWritten);
+
+            if (ec == boost::asio::error::eof)
+            {
+                for (crow::websocket::Connection* session : sessions)
+                {
+                    session->close("Error in reading to host port");
+                }
+                return;
+            }
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "Error in host serial write " << ec;
+                return;
+            }
+            doWrite();
+        });
+}
+
+inline void doRead()
+{
+    if (!hostSocket)
+    {
+        BMCWEB_LOG_ERROR << "doRead(): Socket closed.";
+        return;
+    }
+
+    BMCWEB_LOG_DEBUG << "Reading from socket";
+    hostSocket->async_read_some(
+        boost::asio::buffer(outputBuffer.data(), outputBuffer.size()),
+        [](const boost::system::error_code& ec, std::size_t bytesRead) {
+            BMCWEB_LOG_DEBUG << "read done.  Read " << bytesRead << " bytes";
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "Couldn't read from host serial port: "
+                                 << ec;
+                for (crow::websocket::Connection* session : sessions)
+                {
+                    session->close("Error in connecting to host port");
+                }
+                return;
+            }
+            std::string_view payload(outputBuffer.data(), bytesRead);
+            for (crow::websocket::Connection* session : sessions)
+            {
+                session->sendBinary(payload);
+            }
+            doRead();
+        });
+}
+
+inline void connectHandler(const boost::system::error_code& ec)
+{
+    if (ec)
+    {
+        BMCWEB_LOG_ERROR << "Couldn't connect to host serial port: " << ec;
+        for (crow::websocket::Connection* session : sessions)
+        {
+            session->close("Error in connecting to host port");
+        }
+        return;
+    }
+
+    doWrite();
+    doRead();
+}
+
+inline void requestRoutes(App& app)
+{
+    BMCWEB_ROUTE(app, "/console1")
+        .privileges({{"ConfigureComponents", "ConfigureManager"}})
+        .websocket()
+        .onopen([](crow::websocket::Connection& conn,
+                   const std::shared_ptr<bmcweb::AsyncResp>&) {
+            BMCWEB_LOG_DEBUG << "Connection " << &conn << " opened";
+
+            sessions.insert(&conn);
+            if (hostSocket == nullptr)
+            {
+                const std::string consoleName("\0obmc-console.hypervisor", 24);
+                boost::asio::local::stream_protocol::endpoint ep(consoleName);
+
+                hostSocket = std::make_unique<
+                    boost::asio::local::stream_protocol::socket>(
+                    conn.getIoContext());
+                hostSocket->async_connect(ep, connectHandler);
+            }
+        })
+        .onclose([](crow::websocket::Connection& conn,
+                    [[maybe_unused]] const std::string& reason) {
+            BMCWEB_LOG_INFO << "Closing websocket. Reason: " << reason;
+
+            sessions.erase(&conn);
+            if (sessions.empty())
+            {
+                hostSocket = nullptr;
+                inputBuffer.clear();
+                inputBuffer.shrink_to_fit();
+            }
+        })
+        .onmessage([]([[maybe_unused]] crow::websocket::Connection& conn,
+                      const std::string& data, [[maybe_unused]] bool isBinary) {
+            inputBuffer += data;
+            doWrite();
+        });
+}
+} // namespace obmc_hypervisor
+} // namespace crow
