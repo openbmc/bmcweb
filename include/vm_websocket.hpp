fd58be3e3ceeb3475c1e913b342e080e3c4194e3
dhineskumare <dhineskumare@ami.com>
2021-09-23 14:58:21 +0530

    Added Support For Multiple Vmedia connections
    It is a feature enhancement for Virtual Media redirection,
    which support for multiple/concurrent media connections/sessions.
    Currently it is supporting three numbers of clients, with
    each client will support one CD and one HDD. So totally six
    numbers of media instance is supporting at a time.
    
    Testing:
    tested with all three numbers of clients at a time.
    All six instances were redirected to host machine. Which
    I can able to mount in host machine.
    
    Signed-off-by: Dhines Kumar E <dhineskumare@ami.com>
    Change-Id: I371ba3a387c4a8e72c886f77607d4e79fae06645

--- include/vm_websocket.hpp
+++ include/vm_websocket.hpp
@@ -12,8 +12,6 @@ namespace crow
 namespace obmc_vm
 {
 
-static crow::websocket::Connection* session = nullptr;
-
 // The max network block device buffer size is 128kb plus 16bytes
 // for the message header:
 // https://github.com/NetworkBlockDevice/nbd/blob/master/doc/proto.md#simple-reply-message
@@ -24,11 +22,24 @@ class Handler : public std::enable_shared_from_this<Handler>
   public:
     Handler(const std::string& mediaIn, boost::asio::io_context& ios) :
         pipeOut(ios), pipeIn(ios), media(mediaIn), doingWrite(false),
+        session(nullptr),
         outputBuffer(new boost::beast::flat_static_buffer<nbdBufferSize>),
         inputBuffer(new boost::beast::flat_static_buffer<nbdBufferSize>)
     {}
 
     ~Handler() = default;
+
+    void updateMediasession(int userCount)
+    {
+        std::ofstream fileWriter;
+        fileWriter.open("/etc/nbd-proxy/mediasession",
+                        std::ios::out | std::ios::trunc);
+        if (fileWriter)
+        {
+            fileWriter << userCount;
+            fileWriter.close();
+        }
+    }
 
     void doClose()
     {
@@ -37,6 +48,8 @@ class Handler : public std::enable_shared_from_this<Handler>
         int rc = kill(proxy.id(), SIGTERM);
         if (rc)
         {
+            BMCWEB_LOG_ERROR
+                << "Couldn't found Child PID to send SIGTERM signal";
             return;
         }
         proxy.wait();
@@ -81,12 +94,15 @@ class Handler : public std::enable_shared_from_this<Handler>
             inputBuffer->data(),
             [this, self(shared_from_this())](boost::beast::error_code ec,
                                              std::size_t bytesWritten) {
-                BMCWEB_LOG_DEBUG << "Wrote " << bytesWritten << "bytes";
+                BMCWEB_LOG_DEBUG << "Wrote " << bytesWritten << "bytes,session"
+                                 << session;
                 doingWrite = false;
                 inputBuffer->consume(bytesWritten);
 
                 if (session == nullptr)
                 {
+                    BMCWEB_LOG_ERROR << "Session closed it is set to null "
+                                     << ec;
                     return;
                 }
                 if (ec == boost::asio::error::eof)
@@ -113,7 +129,7 @@ class Handler : public std::enable_shared_from_this<Handler>
             [this, self(shared_from_this())](
                 const boost::system::error_code& ec, std::size_t bytesRead) {
                 BMCWEB_LOG_DEBUG << "Read done.  Read " << bytesRead
-                                 << " bytes";
+                                 << " bytes, session-" << session;
                 if (ec)
                 {
                     BMCWEB_LOG_ERROR << "Couldn't read from VM port: " << ec;
@@ -144,6 +160,7 @@ class Handler : public std::enable_shared_from_this<Handler>
     boost::process::child proxy;
     std::string media;
     bool doingWrite;
+    crow::websocket::Connection* session;
 
     std::unique_ptr<boost::beast::flat_static_buffer<nbdBufferSize>>
         outputBuffer;
@@ -151,52 +168,173 @@ class Handler : public std::enable_shared_from_this<Handler>
         inputBuffer;
 };
 
-static std::shared_ptr<Handler> handler;
+static std::shared_ptr<Handler> gHandler[3][2] = {nullptr};
+
+inline void getmediaClient(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    std::string filePath = "/etc/nbd-proxy/mediasession";
+    std::ifstream fileReader;
+    fileReader.open(filePath);
+    if (fileReader)
+    {
+        std::string tempBuf;
+        std::getline(fileReader, tempBuf);
+
+        if (tempBuf == "0" || tempBuf == "1" || tempBuf == "2")
+        {
+            asyncResp->res.jsonValue["Media_session_id"] = tempBuf;
+            asyncResp->res.end();
+        }
+
+        else
+        {
+            BMCWEB_LOG_ERROR << "Invalid Client Id";
+            asyncResp->res.jsonValue["Media_session_id"] = "Invalid Client Id";
+        }
+        fileReader.close();
+    }
+
+    else
+    {
+        BMCWEB_LOG_ERROR << "File Not found";
+        asyncResp->res.jsonValue["Media_session_id"] = "File Not Found";
+        return;
+    }
+}
 
 inline void requestRoutes(App& app)
 {
-    BMCWEB_ROUTE(app, "/vm/0/0")
+    BMCWEB_ROUTE(app, "/vm/<str>/<str>")
         .privileges({{"ConfigureComponents", "ConfigureManager"}})
         .websocket()
         .onopen([](crow::websocket::Connection& conn,
                    const std::shared_ptr<bmcweb::AsyncResp>&) {
             BMCWEB_LOG_DEBUG << "Connection " << &conn << " opened";
+            std::string targetName;
+            targetName = conn.req.target();
 
-            if (session != nullptr)
+            int userSession, endpoint;
+            if (strstr(targetName.c_str(), "/vm/"))
             {
-                conn.close("Session already connected");
+                std::stringstream sessionName;
+                sessionName << targetName.at(4);
+                sessionName >> userSession;
+
+                std::stringstream mediaEndpoint;
+                mediaEndpoint << targetName.at(6);
+                mediaEndpoint >> endpoint;
+            }
+
+            else
+            {
+                conn.close("target did not matched...!");
                 return;
             }
 
-            if (handler != nullptr)
-            {
-                conn.close("Handler already running");
-                return;
-            }
+            std::string media = std::to_string((2 * userSession) + endpoint);
+            BMCWEB_LOG_ERROR << "media- " << media << "user_session-"
+                             << userSession << "endpoint" << endpoint;
+            std::shared_ptr<Handler> handler;
 
-            session = &conn;
-
-            // media is the last digit of the endpoint /vm/0/0. A future
-            // enhancement can include supporting different endpoint values.
-            const char* media = "0";
             handler = std::make_shared<Handler>(media, conn.getIoContext());
+            gHandler[userSession][endpoint] = handler;
+            handler->session = &conn;
             handler->connect();
+            int clientId;
+            for (clientId = 0; clientId <= 3; clientId++)
+            {
+                if (gHandler[clientId][0] == nullptr &&
+                    gHandler[clientId][1] == nullptr)
+                {
+                    handler->updateMediasession(clientId);
+                    if (clientId == 3)
+                    {
+                        BMCWEB_LOG_ERROR << "Media session reached Max number "
+                                            "of users ...!!! ";
+                    }
+                    break;
+                }
+            }
         })
         .onclose([](crow::websocket::Connection& conn,
                     const std::string& /*reason*/) {
-            if (&conn != session)
+            /*if (&conn != session)
+              {
+              return;
+              } */
+
+            std::shared_ptr<Handler> handler;
+            int clientId, endpointId;
+            for (clientId = 0; clientId < 3; clientId++)
             {
+                for (endpointId = 0; endpointId < 2; endpointId++)
+                {
+                    if (gHandler[clientId][endpointId] != nullptr &&
+                        gHandler[clientId][endpointId]->session == &conn)
+                    {
+                        handler = gHandler[clientId][endpointId];
+                        gHandler[clientId][endpointId] = nullptr;
+                        goto handler_found;
+                    }
+                }
+            }
+        handler_found:
+            if (clientId == 3)
+            {
+
+                BMCWEB_LOG_ERROR << "Handler not found for connection - "
+                                 << &conn;
                 return;
             }
 
-            session = nullptr;
+            endpointId ? sleep(4) : sleep(2);
+
             handler->doClose();
-            handler->inputBuffer->clear();
-            handler->outputBuffer->clear();
-            handler.reset();
+            handler->session->close("VM socket port closed");
+            handler->session = nullptr;
+            BMCWEB_LOG_DEBUG << "Connection " << &conn << " closed";
+            gHandler[clientId][endpointId] = nullptr;
+            for (clientId = 0; clientId <= 3; clientId++)
+            {
+                if (gHandler[clientId][0] == nullptr &&
+                    gHandler[clientId][1] == nullptr)
+                {
+                    handler->updateMediasession(clientId);
+                    if (clientId == 3)
+                    {
+                        BMCWEB_LOG_ERROR << "Media session reached Max number "
+                                            "of users ...!!! ";
+                    }
+                    break;
+                }
+            }
         })
         .onmessage([](crow::websocket::Connection& conn,
                       const std::string& data, bool) {
+            std::shared_ptr<Handler> handler;
+            int clientId, endpointId;
+            for (clientId = 0; clientId < 3; clientId++)
+            {
+                for (endpointId = 0; endpointId < 2; endpointId++)
+                {
+                    if (gHandler[clientId][endpointId] != nullptr &&
+                        gHandler[clientId][endpointId]->session == &conn)
+                    {
+                        handler = gHandler[clientId][endpointId];
+                        BMCWEB_LOG_DEBUG << "handler_onmessgae is ==>  "
+                                         << handler << " \n";
+                        goto handler_found;
+                    }
+                }
+            }
+        handler_found:
+            if (clientId == 3)
+            {
+                BMCWEB_LOG_ERROR << "No handler found with connection - "
+                                 << &conn;
+                return;
+            }
+
             if (data.length() > handler->inputBuffer->capacity())
             {
                 BMCWEB_LOG_ERROR << "Buffer overrun when writing "
@@ -210,6 +348,14 @@ inline void requestRoutes(App& app)
             handler->inputBuffer->commit(data.size());
             handler->doWrite();
         });
+
+    BMCWEB_ROUTE(app, "/vmedia/client_Id")
+        .privileges({{"ConfigureComponents", "ConfigureManager"}})
+        .methods(boost::beast::http::verb::get)(
+            [](const crow::Request&,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
+                getmediaClient(asyncResp);
+            });
 }
 
 } // namespace obmc_vm
