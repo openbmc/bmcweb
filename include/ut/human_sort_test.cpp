ece27c52a51e0adf42cdc1541f454941499168de
Ed Tanous <edtanous@google.com>
2021-09-29 14:31:13 -0700

    Implement human sort utility function
    This commit implements the ability to sort lists of strings that might
    include numbers into "human" ordered lists.  As an example of a problem
    this solves, imagine a system with 12 dimms, today std::sort would net
    you:
    
    Dimm1
    Dimm11
    Dimm12
    Dimm2
    Dimm3
    .....
    
    This method breaks apart that string and sorts them in a way humans
    would expect.
    
    This code is originally inspired by the algorithm defined here:
    http://www.davekoelle.com/alphanum.html.  The site does include c++ code
    that is MIT licensed, but is significantly more complex than what is
    present in this commit.  This commit also takes advantages in the form
    of std::string_view to deduplicate overloads, as well as other c++17
    features.
    
    Tested:
    Unit tests pass
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Iac54c2e0d9998d4d622d74049b1dd957e4b3ca75

---
+++ include/ut/human_sort_test.cpp
@@ -0,0 +1,46 @@
+#include <human_sort.hpp>
+
+#include <set>
+
+#include "gmock/gmock.h"
+
+TEST(AlphaNum, NumberTests)
+{
+    // testcases for the algorithm
+    EXPECT_EQ(alphanumComp("", ""), 0);
+    EXPECT_LT(alphanumComp("", "a"), 0);
+    EXPECT_GT(alphanumComp("a", ""), 0);
+    EXPECT_EQ(alphanumComp("a", "a"), 0);
+    EXPECT_LT(alphanumComp("", "9"), 0);
+    EXPECT_GT(alphanumComp("9", ""), 0);
+    EXPECT_EQ(alphanumComp("1", "1"), 0);
+    EXPECT_LT(alphanumComp("1", "2"), 0);
+    EXPECT_GT(alphanumComp("3", "2"), 0);
+    EXPECT_EQ(alphanumComp("a1", "a1"), 0);
+    EXPECT_LT(alphanumComp("a1", "a2"), 0);
+    EXPECT_GT(alphanumComp("a2", "a1"), 0);
+    EXPECT_LT(alphanumComp("a1a2", "a1a3"), 0);
+    EXPECT_GT(alphanumComp("a1a2", "a1a0"), 0);
+    EXPECT_GT(alphanumComp("134", "122"), 0);
+    EXPECT_EQ(alphanumComp("12a3", "12a3"), 0);
+    EXPECT_GT(alphanumComp("12a1", "12a0"), 0);
+    EXPECT_LT(alphanumComp("12a1", "12a2"), 0);
+    EXPECT_LT(alphanumComp("a", "aa"), 0);
+    EXPECT_GT(alphanumComp("aaa", "aa"), 0);
+    EXPECT_EQ(alphanumComp("Alpha 2", "Alpha 2"), 0);
+    EXPECT_LT(alphanumComp("Alpha 2", "Alpha 2A"), 0);
+    EXPECT_GT(alphanumComp("Alpha 2 B", "Alpha 2"), 0);
+
+    std::string str("Alpha 2");
+    EXPECT_EQ(alphanumComp(str, "Alpha 2"), 0);
+    EXPECT_LT(alphanumComp(str, "Alpha 2A"), 0);
+    EXPECT_GT(alphanumComp("Alpha 2 B", str), 0);
+}
+
+TEST(AlphaNum, LessTest)
+{
+    std::set<std::string, AlphanumLess<std::string>> sorted{"Alpha 10",
+                                                            "Alpha 2"};
+
+    EXPECT_THAT(sorted, ::testing::ElementsAreArray({"Alpha 2", "Alpha 10"}));
+}
