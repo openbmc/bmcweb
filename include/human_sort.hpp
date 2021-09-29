0111610e36b5a3dd705b7bb4e6fa6175108fd2c9
Ed Tanous <edtanous@google.com>
2021-09-29 14:31:13 -0700

    Implement human sort utility function
    This commit implements the ability to sort lists of strings that might
    include numbers into "human" ordered lists.  As an example of a problem
    this solves, imagine a system with 12 dimms, today std::sort would net
    you:
    
    Dimm1
    Dimm11
    Dimm12
    Dimm2
    Dimm3
    .....
    
    This method breaks apart that string and sorts them in a way humans
    would expect.
    
    Tested:
    Unit tests pass
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Iac54c2e0d9998d4d622d74049b1dd957e4b3ca75

---
+++ include/human_sort.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <functional>
+#include <sstream>
+#include <string_view>
+
+namespace details
+{
+
+// This implementation avoids the complexity of using std::isdigit, which pulls
+// in all of <locale>, and likely has other consequences.
+bool simpleIsDigit(const char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+} // namespace details
+
+int alphanumComp(const std::string_view left, const std::string_view right)
+{
+    enum class ModeType
+    {
+        STRING,
+        NUMBER
+    } mode = ModeType::STRING;
+
+    std::string_view::const_iterator l = left.begin();
+    std::string_view::const_iterator r = right.begin();
+
+    while (l != left.end() && r != right.end())
+    {
+        if (mode == ModeType::STRING)
+        {
+            while (l != left.end() && r != right.end())
+            {
+                // check if this are digit characters
+                const bool l_digit = details::simpleIsDigit(*l);
+                const bool r_digit = details::simpleIsDigit(*r);
+                // if both characters are digits, we continue in NUMBER mode
+                if (l_digit && r_digit)
+                {
+                    mode = ModeType::NUMBER;
+                    break;
+                }
+                // if only the left character is a digit, we have a result
+                if (l_digit)
+                    return -1;
+                // if only the right character is a digit, we have a result
+                if (r_digit)
+                    return +1;
+                // compute the difference of both characters
+                const int diff = *l - *r;
+                // if they differ we have a result
+                if (diff != 0)
+                    return diff;
+                // otherwise process the next characters
+                l++;
+                r++;
+            }
+        }
+        else // mode==NUMBER
+        {
+            // get the left number
+            int lInt = 0;
+            while (l != left.end() && details::simpleIsDigit(*l))
+            {
+                lInt = lInt * 10 + static_cast<int>(*l) - '0';
+                ++l;
+            }
+
+            // get the right number
+            int rInt = 0;
+            while (r != right.end() && details::simpleIsDigit(*r))
+            {
+                rInt = rInt * 10 + static_cast<int>(*r) - '0';
+                ++r;
+            }
+
+            // if the difference is not equal to zero, we have a comparison
+            // result
+            const int diff = lInt - rInt;
+            if (diff != 0)
+                return diff;
+
+            // otherwise we process the next substring in STRING mode
+            mode = ModeType::STRING;
+        }
+    }
+    if (r == right.end() && l == left.end())
+    {
+        return 0;
+    }
+    if (r == right.end())
+        return 1;
+    return -1;
+}
+
+template <class Ty>
+struct alphanumLess : public std::binary_function<Ty, Ty, bool>
+{
+    bool operator()(const Ty& left, const Ty& right) const
+    {
+        return alphanumComp(left, right) < 0;
+    }
+};
