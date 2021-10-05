459d1bc5fbc2ce2962e00b73dbcd39c2497e3377
Ed Tanous <edtanous@google.com>
2021-10-04 17:02:43 -0700

    Redfish: Query parameters: Only
    Add the query parameter "only" for redfish.
    The specification is based on DSP0266_1.8.0.
    
    This commit is inspired by the commit that carries the same title, but
    is largely unique, namely, in that it adds the core feature to be able
    to recall handle with a new Response object, and make sure the result
    gets to the connection.  It does this by swapping the handlers and
    implementing move semantics on the Response object.  It definitely needs
    broken up into a few smaller patches, but it does pass the below tests
    without any apparent seg faults or ownership issues.
    
    It implements a number of cleanups that deserve their own patches, and
    will be split up accordingly, but for the moment, I think this is a good
    start to getting filter and expand support in the future.
    
    Tested:
    Validator passes (on previous patchset)
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems"
    
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET "https://${bmc}/redfish/v1/Systems?only"
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I123d8ab8bcd88a0b63ff131f6b98548951989755

---
+++ redfish-core/include/query.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "utils/query_param.hpp"
+
+#include <bmcweb_config.h>
+
+namespace redfish
+{
+
+inline bool setUpRedfishRoute(crow::App& app, const crow::Request& req,
+                              crow::Response& res)
+{
+    BMCWEB_LOG_DEBUG << "setup redfish route";
+
+    // If query parameters aren't enabled, do nothing.
+    if constexpr (bmcwebInsecureEnableQueryParams)
+    {
+        return true;
+    }
+    std::optional<query_param::Query> query =
+        query_param::parseParameters(req.urlView.params(), res);
+    if (query == std::nullopt)
+    {
+        return false;
+    }
+
+    std::function<void(crow::Response&)> handler =
+        res.releaseCompleteRequestHandler();
+
+    BMCWEB_LOG_DEBUG << "Function was valid: " << static_cast<bool>(handler);
+
+    BMCWEB_LOG_DEBUG << "Setting completion handler for query";
+    res.setCompleteRequestHandler([&app, handler(std::move(handler)),
+                                   query](crow::Response& res) mutable {
+        BMCWEB_LOG_DEBUG << "Starting query params handling";
+        processAllParams(app, query, res, handler);
+    });
+    return true;
+}
+} // namespace redfish
