d4dcea779dba0da158df03ef239798fd2d6db7ab
AppaRao Puli <apparao.puli@linux.intel.com>
2021-10-06 21:51:16 +0000

    Input parameter validation for Event Subscription
    User input must be validated to avoid the out-of-memory issue. This
    commit adds the size check on input parameters such as Context,
    Destination and Header field  while create or update the
    EventDestination.
    
    Added a generic error message "PropertySizeExceeded" in message registry
    which is used as response when size limit is exceeded.
    
    Tested
      - Validated using POST on Event Subscription.
      - When Context, Destination and Headers were too long,
        received a error message denoting the same.
    
    Change-Id: Ibab847ce0c99f445a76e6d3aee8074428bb7d30f
    Signed-off-by: AppaRao Puli <apparao.puli@intel.com>
    Signed-off-by: Ayushi Smriti <smriti.ayushi@intel.com>
    Signed-off-by: P Dheeraj Srujan Kumar <p.dheeraj.srujan.kumar@intel.com>
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- redfish-core/lib/event_service.hpp
+++ redfish-core/lib/event_service.hpp
@@ -44,6 +44,9 @@ static constexpr const std::array<const char*, 1> supportedResourceTypes = {
 #endif
 
 static constexpr const uint8_t maxNoOfSubscriptions = 20;
+static constexpr const uint16_t maxContextSizeED = 256;
+static constexpr const uint16_t maxDestinationSize = 1024;
+static constexpr const uint16_t maxHeaderSizeED = 8096;
 
 inline void requestRoutesEventService(App& app)
 {
@@ -252,6 +255,13 @@ inline void requestRoutesEventDestinationCollection(App& app)
             return;
         }
 
+        if (destUrl.size() > maxDestinationSize)
+        {
+            messages::stringValueTooLong(asyncResp->res, "Destination",
+                                         maxDestinationSize);
+            return;
+        }
+
         if (regPrefixes && msgIds)
         {
             if (!regPrefixes->empty() && !msgIds->empty())
@@ -329,13 +339,30 @@ inline void requestRoutesEventDestinationCollection(App& app)
 
         if (context)
         {
+            if (context->size() > maxContextSizeED)
+            {
+                messages::stringValueTooLong(asyncResp->res, "Context",
+                                             maxContextSizeED);
+                return;
+            }
             subValue->customText = *context;
         }
 
         if (headers)
         {
+            size_t cumulativeLen = 0;
+
             for (const nlohmann::json& headerChunk : *headers)
             {
+                std::string hdr{headerChunk.dump(
+                    -1, ' ', true, nlohmann::json::error_handler_t::replace)};
+                cumulativeLen += hdr.length();
+                if (cumulativeLen > maxHeaderSizeED)
+                {
+                    messages::arraySizeTooLong(asyncResp->res, "HttpHeaders",
+                                               maxHeaderSizeED);
+                    return;
+                }
                 for (const auto& item : headerChunk.items())
                 {
                     const std::string* value =
