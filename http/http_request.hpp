68191a40030ea9fed5085bc1920bb6e18d74f0f0
Nan Zhou <nanzhoumails@gmail.com>
2021-10-07 18:10:17 +0000

    gRPC redfish
    gRPC wrapper for Redfish. This wrapper exposes the REST operation that
    are normally available over the HTTP interface over gRPC. This is a thin
    wrapper that does not validate or otherwise process requests, but does
    package JSON objects using struct.proto.  This feature is currently
    behind a 'insecure-grpc' option (disabled by default), since security
    features are not yet implemented.
    
    Change-Id: Ie9e4767704fdbb914d2bf68a0d09ae6e0bb075ec
    Signed-off-by: Jie Yang <jjy@google.com>
    Signed-off-by: Shounak Mitra <shounak@google.com>
    Signed-off-by: Willy Tu <wltu@google.com>
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>

--- http/http_request.hpp
+++ http/http_request.hpp
@@ -39,6 +39,31 @@ struct Request
         fields(req.base()), body(req.body())
     {}
 
+    bool read_headers()
+    {
+        // Note, despite the bmcweb coding policy on use of exceptions
+        // for error handling, this one particular use of exceptions is
+        // deemed acceptable, as it solved a significant error handling
+        // problem that resulted in seg faults, the exact thing that the
+        // exceptions rule is trying to avoid. If at some point,
+        // boost::urls makes the parser object public (or we port it
+        // into bmcweb locally) this will be replaced with
+        // parser::parse, which returns a status code
+
+        boost::urls::error_code ec;
+        urlView = boost::urls::parse_relative_ref(
+            boost::urls::string_view(req.target().data(), req.target().size()),
+            ec);
+        if (ec)
+        {
+            return false;
+        }
+        url = std::string_view(urlView.encoded_path().data(),
+                               urlView.encoded_path().size());
+        urlParams = urlView.query_params();
+        return true;
+    }
+
     boost::beast::http::verb method() const
     {
         return req.method();
