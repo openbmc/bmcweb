e2fe9d0e5b2e027157ce2a0c9b630942ee69af71
Ed Tanous <edtanous@google.com>
2021-10-09 14:49:28 -0700

    Define hex helper utils
    This commit attempts to optimize some code and reduce our dependence on
    boost libraries, as the coding standard recommends.  It does this by
    introducing a new method, intToHexString, which is a greatly simplified
    "to hex" converter that doesn't require std::locale, or stream buffers,
    and is very efficient.  This deletes our need for boost::format, as well
    as our need for boost::lexical_cast, both of which are fairly heavy
    libraries.
    
    Tested:
    Unit tests pass.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I3b0f6eeb10256f87320adcc0ae9396f6bcbc8740

---
+++ redfish-core/ut/hex_utils_test.cpp
@@ -0,0 +1,31 @@
+#include "utils/hex_utils.hpp"
+
+#include <gmock/gmock.h>
+
+TEST(ToHexString, uint64)
+{
+    EXPECT_EQ(intToHexString(0xFFFFFFFFFFFFFFFFULL), "FFFFFFFFFFFFFFFF");
+
+    EXPECT_EQ(intToHexString<uint64_t>(0), "0000000000000000");
+    EXPECT_EQ(intToHexString<uint64_t>(0, 4), "0000");
+    EXPECT_EQ(intToHexString<uint64_t>(0, 8), "00000000");
+    EXPECT_EQ(intToHexString<uint64_t>(0, 12), "000000000000");
+    EXPECT_EQ(intToHexString<uint64_t>(0, 16), "0000000000000000");
+
+    uint64_t deadbeef = 0xDEADBEEFBAD4F00DULL;
+    EXPECT_EQ(intToHexString<uint64_t>(deadbeef), "DEADBEEFBAD4F00D");
+    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 4), "F00D");
+    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 8), "BAD4F00D");
+    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 12), "BEEFBAD4F00D");
+    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 16), "DEADBEEFBAD4F00D");
+}
+
+TEST(ToHexString, uint16)
+{
+    uint16_t beef = 0xBEEF;
+    EXPECT_EQ(intToHexString(beef), "BEEF");
+    EXPECT_EQ(intToHexString(beef, 1), "F");
+    EXPECT_EQ(intToHexString(beef, 2), "EF");
+    EXPECT_EQ(intToHexString(beef, 3), "EEF");
+    EXPECT_EQ(intToHexString(beef, 4), "BEEF");
+}
