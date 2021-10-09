8fb6d93ee3c156b858d173be35d09380d7d6f3ca
Ed Tanous <edtanous@google.com>
2021-10-09 14:49:28 -0700

    Define hex helper utils
    This commit attempts to optimize some code and reduce our dependence on
    boost libraries, as the coding standard recommends.  It does this by
    introducing a new method, intToHexString, which is a greatly simplified
    "to hex" converter that doesn't require std::locale, or stream buffers,
    and is very efficient.  This deletes our need for boost::format, as well
    as our need for boost::lexical_cast, both of which are fairly heavy
    libraries.
    
    Tested:
    Unit tests pass.  TBD functional testing.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I3b0f6eeb10256f87320adcc0ae9396f6bcbc8740

---
+++ redfish-core/include/utils/hex_utils.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+template <typename IntegerType>
+inline std::string intToHexString(IntegerType value,
+                                  size_t digits = sizeof(IntegerType) << 1)
+{
+    static constexpr std::array<char, 16> digitsArray = {
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    std::string rc(digits, '0');
+    size_t bitIndex = (digits - 1) * 4;
+    for (size_t digitIndex = 0; digitIndex < digits; digitIndex++)
+    {
+        rc[digitIndex] = digitsArray[(value >> bitIndex) & 0x0f];
+        bitIndex -= 4;
+    }
+    return rc;
+}
