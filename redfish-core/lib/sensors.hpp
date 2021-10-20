cb49bdc84556055b79470a8082308c1a0fce720c
zhanghch05 <zhanghch05@inspur.com>
2021-10-20 16:04:48 +0800

    Use AsyncResp in retrieveUriToDbusMap
    We once had such a modification: Get the core using AsyncResp
    everywhere, and not have each individual handler creating its
    own object. But the code here has not been modified.
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: I6f82bdb234ddade67f689d79d004d672593fba4f

--- redfish-core/lib/sensors.hpp
+++ redfish-core/lib/sensors.hpp
@@ -2960,9 +2960,10 @@ inline void setSensorsOverride(
  * @param node  Node (group) of sensors. See sensors::node for supported values
  * @param mapComplete   Callback to be called with retrieval result
  */
-inline void retrieveUriToDbusMap(const std::string& chassis,
-                                 const std::string& node,
-                                 SensorsAsyncResp::DataCompleteCb&& mapComplete)
+inline void
+    retrieveUriToDbusMap(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                         const std::string& chassis, const std::string& node,
+                         SensorsAsyncResp::DataCompleteCb&& mapComplete)
 {
     auto pathIt = sensors::dbus::paths.find(node);
     if (pathIt == sensors::dbus::paths.end())
@@ -2972,10 +2973,8 @@ inline void retrieveUriToDbusMap(const std::string& chassis,
         return;
     }
 
-    auto res = std::make_shared<crow::Response>();
-    auto asyncResp = std::make_shared<bmcweb::AsyncResp>(*res);
     auto callback =
-        [res, asyncResp, mapCompleteCb{std::move(mapComplete)}](
+        [asyncResp, mapCompleteCb{std::move(mapComplete)}](
             const boost::beast::http::status status,
             const boost::container::flat_map<std::string, std::string>&
                 uriToDbus) { mapCompleteCb(status, uriToDbus); };
