590ea19a3d262836b2f23443edaa5d549af10432
zhanghch05 <zhanghch05@inspur.com>
2021-10-21 14:07:57 +0800

    Remove AsyncResp from openHandler
    This change, moving the openHandler back to only supporting websocket
    disconnects and not 404s.Because AsyncResp is removed from openHandler.
    
    Tested:
    Opened KVM in webui-vue and it works.
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: I90811f4ab91ad41cb298877f76252dce80932b2b

--- include/dbus_monitor.hpp
+++ include/dbus_monitor.hpp
@@ -107,8 +107,7 @@ inline void requestRoutes(App& app)
     BMCWEB_ROUTE(app, "/subscribe")
         .privileges({{"Login"}})
         .websocket()
-        .onopen([&](crow::websocket::Connection& conn,
-                    const std::shared_ptr<bmcweb::AsyncResp>&) {
+        .onopen([&](crow::websocket::Connection& conn) {
             BMCWEB_LOG_DEBUG << "Connection " << &conn << " opened";
             sessions[&conn] = DbusWebsocketSession();
         })
