611402924efec9f16a2067593176195d300dcdd5
zhanghch05 <zhanghch05@inspur.com>
2021-10-21 15:56:29 +0800

    Redfish: Query parameters: Only
    Add the query parameter "only" for redfish.
    The specification is based on DSP0266_1.8.0.
    
    When the query parameter carries only, it will check whether
    the requested resource has only one member.
    If it matches, The response will be replaced with the content
    of the only member.
    
    When there are query parameters, we will process the parameters
    in res.end(). When the only parameter exists, we will get a new
    url and call handle() again to get the resource.
    
    Tested:
    Validator passes.
    Use the curl commond:
    curl -i -k -H "X-Auth-Token: $token" -X GET
    "https://${bmc}/redfish/v1/Systems?only"
    
    The response is same as the url
    "https://${bmc}/redfish/v1/Systems/system"
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: I29f63e0b2231a9362b6d6ca65af3f820f8bc3c27

---
+++ redfish-core/include/query.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "utils/query_param.hpp"
+
+namespace redfish
+{
+inline bool setUpRedfishRoute(crow::App& app, const crow::Request& req,
+                              crow::Response& res)
+{
+    BMCWEB_LOG_DEBUG << "setup redfish route";
+    std::optional<query_param::Query> query =
+        query_param::parseParameters(req.urlParams, res);
+    std::function<void(crow::Response&)> handler =
+        res.releaseCompleteRequestHandler();
+    BMCWEB_LOG_DEBUG << "Function was valid: " << static_cast<bool>(handler);
+    BMCWEB_LOG_DEBUG << "Setting completion handler for query";
+    res.setCompleteRequestHandler([&app, handler(std::move(handler)),
+                                   query](crow::Response& res) mutable {
+        BMCWEB_LOG_DEBUG << "Starting query params handling";
+        processAllParams(app, query, res, handler);
+    });
+    return true;
+}
+} // namespace redfish
