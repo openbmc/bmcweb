43ea78d81f2c1ea1de068409a06e989258dd2113
zhanghch05 <zhanghch05@inspur.com>
2021-10-22 10:34:31 +0800

    Redfish: Query parameters: expand
    Add the query parameter "expand" for redfish.
    The specification is based on DSP0266_1.8.0.
    
    Returns a hyperlink and its contents in-line with retrieved
    resources, as if a GET call response was included in-line with
    that hyperlink.
    
    asterisk(*):Shall expand all hyperlinks, including those in
    payload annotations, such as @Redfish.Settings,
    @Redfish.ActionInfo, and @Redfish.CollectionCapabilities.
    Example: https://resource?$expand=*
    
    period(.):Shall expand all hyperlinks not in any links
    property instances of the resource, including those in
    payload annotations, such as @Redfish.Settings,
    @Redfish.ActionInfo, and @Redfish.CollectionCapabilities.
    Example: https://resource?$expand=.
    
    tilde(~):Shall expand all hyperlinks found in all links
    property instances of the resource.
    Example: https://resource?$expand=~
    
    $levels:Number of levels the service should cascade the
    $expand operation.The default level shall be 1.
    Example: https://resource?$expand=*($levels=1)
    
    Tested:
    Use the curl commond:
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET
    "https://${bmc}/redfish/v1/Systems"
    
    and commond:
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET
    "https://${bmc}/redfish/v1/Systems?$expand=*(levels=1)"
    
    and commond:
    ~$ curl -i -k -H "X-Auth-Token: $token" -X GET
    "https://${bmc}/redfish/v1/Systems?$expand=*(levels=2)"
    
    From the response, we can find that the second and third commands expand
    the first command.
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: I77dc16e6e416db479b0385e0b9b51ff283fb13e2

--- redfish-core/include/utils/query_param.hpp
+++ redfish-core/include/utils/query_param.hpp
@@ -12,10 +12,22 @@ namespace redfish
 {
 namespace query_param
 {
+const uint64_t expandMaxLevel = 2;  // todo
+const uint64_t expandMinLenth = 12; //*($levels=1)
 struct Query
 {
     bool isOnly = false;
+    bool isExpand = false;
+    uint64_t expandLevel;
+    std::string expandType;
+    std::vector<std::string> pendingUrlVec;
+    nlohmann::json jsonValue;
 };
+
+void processAllParams(crow::App& app, Query& query,
+                      crow::Response& intermediateResponse,
+                      std::function<void(crow::Response&)>& completionHandler);
+
 inline std::optional<Query>
     parseParameters(const boost::urls::query_params_view& urlParams,
                     crow::Response& res)
@@ -39,9 +51,213 @@ inline std::optional<Query>
             }
             ret.isOnly = true;
         }
+        else if (it.key() == "$expand")
+        {
+            std::string value = std::string(it.value());
+            if (value == "*" || value == "." || value == "~")
+            {
+                ret.isExpand = true;
+                ret.expandType = std::move(value);
+                ret.expandLevel = 1;
+            }
+            else if (value.size() >= expandMinLenth &&
+                     value.substr(1, 9) == "($levels=" &&
+                     (value[0] == '*' || value[0] == '.' || value[0] == '~') &&
+                     value[value.size() - 1] == ')')
+            {
+                ret.isExpand = true;
+                ret.expandType = value[0];
+                ret.expandLevel = strtoul(
+                    value.substr(10, value.size() - 2).c_str(), nullptr, 10);
+                ret.expandLevel = ret.expandLevel < expandMaxLevel
+                                      ? ret.expandLevel
+                                      : expandMaxLevel;
+            }
+            else
+            {
+                messages::queryParameterValueFormatError(res, it.value(),
+                                                         it.key());
+                return std::nullopt;
+            }
+        }
     }
     return ret;
 }
+
+inline void recursiveToGetUrls(nlohmann::json& j, Query& query)
+{
+    auto itExpand = j.find("isExpand");
+    if (itExpand == j.end())
+    {
+        auto it = j.find("@odata.id");
+        if (it != j.end())
+        {
+            std::string url = it->get<std::string>();
+
+            if (url.find('#') == std::string::npos)
+            {
+                query.pendingUrlVec.push_back(url);
+            }
+        }
+        else if (query.expandType == "*" || query.expandType == ".")
+        {
+            it = j.find("@Redfish.Settings");
+            if (it != j.end())
+            {
+                std::string url = it->get<std::string>();
+                if (url.find('#') == std::string::npos)
+                {
+                    query.pendingUrlVec.push_back(url);
+                }
+            }
+            else
+            {
+                it = j.find("@Redfish.ActionInfo");
+                if (it != j.end())
+                {
+                    std::string url = it->get<std::string>();
+                    if (url.find('#') == std::string::npos)
+                    {
+                        query.pendingUrlVec.push_back(url);
+                    }
+                }
+                else
+                {
+                    it = j.find("@Redfish.CollectionCapabilities");
+                    if (it != j.end())
+                    {
+                        std::string url = it->get<std::string>();
+                        if (url.find('#') == std::string::npos)
+                        {
+                            query.pendingUrlVec.push_back(url);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (auto it = j.begin(); it != j.end(); ++it)
+    {
+        if (it->is_object())
+        {
+            if (query.expandType == "." && it.key() == "Links")
+            {
+                continue;
+            }
+            recursiveToGetUrls(*it, query);
+        }
+        else if (it->is_array())
+        {
+            for (auto& itArray : *it)
+            {
+                recursiveToGetUrls(itArray, query);
+            }
+        }
+    }
+}
+
+inline bool insertJson(nlohmann::json& base, const std::string& pos,
+                       const nlohmann::json& data, const Query& query)
+{
+    auto itExpand = base.find("isExpand");
+    if (itExpand != base.end())
+    {
+        if (itExpand->get<unsigned int>() <= query.expandLevel)
+        {
+            return false;
+        }
+    }
+    else
+    {
+        auto it = base.find("@odata.id");
+        if (it != base.end())
+        {
+            if (it->get<std::string>() == pos)
+            {
+                base = data;
+                return true;
+            }
+        }
+        else if (query.expandType == "*" || query.expandType == ".")
+        {
+            it = base.find("@Redfish.Settings");
+            if (it != base.end())
+            {
+                if (it->get<std::string>() == pos)
+                {
+                    base = data;
+                    return true;
+                }
+            }
+            else
+            {
+                it = base.find("@Redfish.ActionInfo");
+                if (it != base.end())
+                {
+                    if (it->get<std::string>() == pos)
+                    {
+                        base = data;
+                        return true;
+                    }
+                }
+                else
+                {
+                    it = base.find("@Redfish.CollectionCapabilities");
+                    if (it != base.end())
+                    {
+                        if (it->get<std::string>() == pos)
+                        {
+                            base = data;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (auto it = base.begin(); it != base.end(); ++it)
+    {
+        if (it->is_object())
+        {
+            if (query.expandType == "." && it.key() == "Links")
+            {
+                continue;
+            }
+            if (insertJson(*it, pos, data, query))
+            {
+                return true;
+            }
+        }
+        else if (it->is_array())
+        {
+            for (auto& itArray : *it)
+            {
+                if (insertJson(itArray, pos, data, query))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+inline void deleteExpand(nlohmann::json& j)
+{
+    auto it = j.find("isExpand");
+    if (it != j.end())
+    {
+        j.erase(it);
+    }
+    for (auto& it : j)
+    {
+        if (it.is_structured())
+        {
+            deleteExpand(it);
+        }
+    }
+}
+
 inline bool processOnly(crow::App& app, crow::Response& res,
                         std::function<void(crow::Response&)>& completionHandler)
 {
@@ -94,7 +310,113 @@ inline bool processOnly(crow::App& app, crow::Response& res,
     app.handle(newReq, asyncResp);
     return true;
 }
-void processAllParams(crow::App& app, const std::optional<Query>& query,
+
+inline bool
+    processExpand(crow::App& app, crow::Response& res,
+                  std::function<void(crow::Response&)>& completionHandler,
+                  Query& query)
+{
+    if (query.pendingUrlVec.size() == 0)
+    {
+        if (res.jsonValue.find("isExpand") == res.jsonValue.end())
+        {
+            res.jsonValue["isExpand"] = query.expandLevel + 1;
+        }
+        recursiveToGetUrls(res.jsonValue, query);
+        if (query.pendingUrlVec.size() == 0)
+        {
+            deleteExpand(res.jsonValue);
+            return false;
+        }
+        query.jsonValue = res.jsonValue;
+
+        std::error_code ec;
+        crow::Request newReq(
+            {boost::beast::http::verb::get, query.pendingUrlVec[0], 11}, ec);
+        if (ec)
+        {
+            messages::internalError(res);
+            completionHandler(res);
+            return true;
+        }
+        auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+        BMCWEB_LOG_DEBUG << "setting completion handler on " << &asyncResp->res;
+        asyncResp->res.setCompleteRequestHandler(
+            [&app, handler(std::move(completionHandler)),
+             query](crow::Response& res) mutable {
+                BMCWEB_LOG_DEBUG << "Starting query params handling";
+                processAllParams(app, query, res, handler);
+            });
+        asyncResp->res.setIsAliveHelper(res.releaseIsAliveHelper());
+        app.handle(newReq, asyncResp);
+        return true;
+    }
+    if (res.jsonValue.find("isExpand") == res.jsonValue.end())
+    {
+        res.jsonValue["isExpand"] = query.expandLevel;
+    }
+    insertJson(query.jsonValue, query.pendingUrlVec[0], res.jsonValue, query);
+    query.pendingUrlVec.erase(query.pendingUrlVec.begin());
+    if (query.pendingUrlVec.size() != 0)
+    {
+        std::error_code ec;
+        crow::Request newReq(
+            {boost::beast::http::verb::get, query.pendingUrlVec[0], 11}, ec);
+        if (ec)
+        {
+            messages::internalError(res);
+            completionHandler(res);
+            return true;
+        }
+        auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+        BMCWEB_LOG_DEBUG << "setting completion handler on " << &asyncResp->res;
+        asyncResp->res.setCompleteRequestHandler(
+            [&app, handler(std::move(completionHandler)),
+             query](crow::Response& res) mutable {
+                BMCWEB_LOG_DEBUG << "Starting query params handling";
+                processAllParams(app, query, res, handler);
+            });
+        asyncResp->res.setIsAliveHelper(res.releaseIsAliveHelper());
+        app.handle(newReq, asyncResp);
+        return true;
+    }
+    query.expandLevel -= 1;
+    if (query.expandLevel == 0)
+    {
+        res.jsonValue = query.jsonValue;
+        deleteExpand(res.jsonValue);
+        return false;
+    }
+    recursiveToGetUrls(query.jsonValue, query);
+    if (query.pendingUrlVec.size() == 0)
+    {
+        deleteExpand(res.jsonValue);
+        return false;
+    }
+
+    std::error_code ec;
+    crow::Request newReq(
+        {boost::beast::http::verb::get, query.pendingUrlVec[0], 11}, ec);
+    if (ec)
+    {
+        messages::internalError(res);
+        completionHandler(res);
+        return true;
+    }
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+    BMCWEB_LOG_DEBUG << "setting completion handler on " << &asyncResp->res;
+    asyncResp->res.setCompleteRequestHandler(
+        [&app, handler(std::move(completionHandler)),
+         query](crow::Response& res) mutable {
+            BMCWEB_LOG_DEBUG << "Starting query params handling";
+            processAllParams(app, query, res, handler);
+        });
+    asyncResp->res.setIsAliveHelper(res.releaseIsAliveHelper());
+    app.handle(newReq, asyncResp);
+    return true;
+}
+
+void processAllParams(crow::App& app, Query& query,
                       crow::Response& intermediateResponse,
                       std::function<void(crow::Response&)>& completionHandler)
 {
@@ -104,13 +426,6 @@ void processAllParams(crow::App& app, const std::optional<Query>& query,
         return;
     }
     BMCWEB_LOG_DEBUG << "Processing query params";
-    if (!query)
-    {
-        BMCWEB_LOG_DEBUG << "No query params to process";
-        // Query params weren't valid, no need to continue;
-        completionHandler(intermediateResponse);
-        return;
-    }
     // If the request failed, there's no reason to even try to run query
     // params.
     if (intermediateResponse.resultInt() < 200 ||
@@ -119,10 +434,17 @@ void processAllParams(crow::App& app, const std::optional<Query>& query,
         completionHandler(intermediateResponse);
         return;
     }
-    if (query->isOnly)
+    if (query.isOnly)
     {
         processOnly(app, intermediateResponse, completionHandler);
         return;
+    }
+    if (query.isExpand)
+    {
+        if (processExpand(app, intermediateResponse, completionHandler, query))
+        {
+            return;
+        }
     }
     completionHandler(intermediateResponse);
 }
