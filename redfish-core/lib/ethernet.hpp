afadc70dd100aab59005d6670a8fda4a9af3e0d4
Jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
2021-10-27 09:56:21 +0000

    ethernet: Fix for ethernet link status for Redfish
    As per RedFish schema the ethernet link status has 3 states i.e. Linkup,
    linkdown and no link. Current implementation has only 2 states i.e
    linkup and linkdown. This check in will be adding other state i.e.
    nolink.
    
    Tested:
    1. Verified redfish validator passed with this change
    2. Verified by removing ethernet cable, RedFish shows nolink.
    3. Also verified RedFish shows linkup and linkdown status correctly.
    
    Signed-off-by: Jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
    Signed-off-by: Vinayak Risbud <vinayak.p.risbud@intel.com>
    Change-Id: Ia961f82078a816120807371b0693481dd18379e4

--- redfish-core/lib/ethernet.hpp
+++ redfish-core/lib/ethernet.hpp
@@ -101,7 +101,7 @@ struct EthernetInterfaceData
     bool NTPEnabled;
     bool HostNameEnabled;
     bool SendHostNameEnabled;
-    bool linkUp;
+    std::string linkStatus;
     bool nicEnabled;
     std::string DHCPEnabled;
     std::string operatingMode;
@@ -266,13 +266,14 @@ inline bool extractEthernetInterfaceData(const std::string& ethifaceId,
                                 ethData.speed = *speed;
                             }
                         }
-                        else if (propertyPair.first == "LinkUp")
+                        else if (propertyPair.first == "LinkStatus")
                         {
-                            const bool* linkUp =
-                                std::get_if<bool>(&propertyPair.second);
-                            if (linkUp != nullptr)
+                            const std::string* linkStatus =
+                                std::get_if<std::string>(&propertyPair.second);
+                            if (linkStatus != nullptr)
                             {
-                                ethData.linkUp = *linkUp;
+                                ethData.linkStatus = linkStatus->substr(
+                                    linkStatus->rfind(".") + 1);
                             }
                         }
                         else if (propertyPair.first == "NICEnabled")
@@ -1700,16 +1701,28 @@ inline void parseInterfaceData(
 
     if (ethData.nicEnabled)
     {
-        jsonResponse["LinkStatus"] = "LinkUp";
         jsonResponse["Status"]["State"] = "Enabled";
     }
     else
     {
-        jsonResponse["LinkStatus"] = "NoLink";
         jsonResponse["Status"]["State"] = "Disabled";
     }
-
-    jsonResponse["LinkStatus"] = ethData.linkUp ? "LinkUp" : "LinkDown";
+    if (ethData.linkStatus == "linkDown")
+    {
+        jsonResponse["LinkStatus"] = "LinkDown";
+    }
+    else if (ethData.linkStatus == "linkUp")
+    {
+        jsonResponse["LinkStatus"] = "LinkUp";
+    }
+    else if (ethData.linkStatus == "noLink")
+    {
+        jsonResponse["LinkStatus"] = "NoLink";
+    }
+    else
+    {
+        jsonResponse["LinkStatus"] = "NoLink";
+    }
     jsonResponse["SpeedMbps"] = ethData.speed;
     jsonResponse["MACAddress"] = ethData.mac_address;
     jsonResponse["DHCPv4"]["DHCPEnabled"] =
