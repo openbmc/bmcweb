cfeadd6226efd9be3ce36c54abb405af1af93288
Tim Lee <timlee660101@gmail.com>
2021-11-01 16:07:26 +0800

    systems: fix Evaluating expression failed about PowerMetrics
    Symptom:
    Run automation test "Verify Power Control Consumed Watts" got ERROR.
    Evaluating expression if not x['PowerConsumedWatts'] <=
    x['PowerMetrics']['MaxConsumedWatts']]' failed: KeyError: 'PowerMetrics'.
    
    Root cause:
    There is no "MaxConsumedWatts" related properties of "PowerMetrics"
    content in redfish chassis power. However, auto test item keyword
    "Verify Power Metric Records" will check those properties.
    Without those properties then cause this test item got failed.
    
    Solution:
    According interface xyz.openbmc_project.Power.PowerMetric to add
    "MaxConsumedWatts" related properties of PowerMetrics content in redfish.
    .AverageConsumedWatts                 property
    .IntervalInMin                        property
    .MaxConsumedWatts                     property
    .MinConsumedWatts                     property
    
    Tested:
    Run robot test item system/test_power_control_metrics.robot PASS
    
    Signed-off-by: Tim Lee <timlee660101@gmail.com>
    Change-Id: I9d71bc6092d53d7cac868066659cc3f922e24035

--- redfish-core/lib/sensors.hpp
+++ redfish-core/lib/sensors.hpp
@@ -36,6 +36,9 @@ using GetSubTreeType = std::vector<
     std::pair<std::string,
               std::vector<std::pair<std::string, std::vector<std::string>>>>>;
 
+using SensorVariant = std::variant<int64_t, double, uint16_t, uint32_t,
+                                   uint64_t, bool, std::string>;
+
 using ManagedObjectsVectorType = std::vector<std::pair<
     sdbusplus::message::object_path,
     boost::container::flat_map<
