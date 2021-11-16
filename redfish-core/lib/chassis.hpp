0b0a29649b59ccdce327fa42f4ea080bb60880ca
George Liu <liuxiwei@inspur.com>
2021-11-16 10:31:23 +0800

    Update indicator LED verification
    Extend the hasIndicatorLed array and add
    xyz.openbmc_project.Inventory.Item.Chassis interface.
    
    Tested:
    curl -k https://$bmc/redfish/v1/Chassis/chassis
    {
      "@odata.id": "/redfish/v1/Chassis/chassis",
      "@odata.type": "#Chassis.v1_14_0.Chassis",
      "Actions": {
        "#Chassis.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Chassis/chassis/ResetActionInfo",
          "target": "/redfish/v1/Chassis/chassis/Actions/Chassis.Reset"
        }
      },
      "ChassisType": "RackMount",
      "Id": "chassis",
      "Links": {
        "ComputerSystems": [
          {
            "@odata.id": "/redfish/v1/Systems/system"
          }
        ],
        "ManagedBy": [
          {
            "@odata.id": "/redfish/v1/Managers/bmc"
          }
        ]
      },
      "Location": {
        "PartLocation": {
          "ServiceLabel": "U78DA.ND0.WZS004K"
        }
      },
      "IndicatorLED": "Off",
      "LocationIndicatorActive": false,
      "Manufacturer": "",
      "Model": "23",
      "Name": "chassis",
      "PCIeDevices": {
        "@odata.id": "/redfish/v1/Systems/system/PCIeDevices"
      },
      "PartNumber": "",
      "Power": {
        "@odata.id": "/redfish/v1/Chassis/chassis/Power"
      },
      "PowerState": "Off",
      "Sensors": {
        "@odata.id": "/redfish/v1/Chassis/chassis/Sensors"
      },
      "SerialNumber": "",
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "StandbyOffline"
      },
      "Thermal": {
        "@odata.id": "/redfish/v1/Chassis/chassis/Thermal"
      }
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I02e77d56e555f9aee3f76015baeebbf1f4a292ab

--- redfish-core/lib/chassis.hpp
+++ redfish-core/lib/chassis.hpp
@@ -276,10 +276,11 @@ inline void requestRoutesChassis(App& app)
 
                         const std::vector<std::string>& interfaces2 =
                             connectionNames[0].second;
-                        const std::array<const char*, 2> hasIndicatorLed = {
+                        const std::array<const char*, 3> hasIndicatorLed = {
                             "xyz.openbmc_project.Inventory.Item.Panel",
                             "xyz.openbmc_project.Inventory.Item.Board."
-                            "Motherboard"};
+                            "Motherboard",
+                            "xyz.openbmc_project.Inventory.Item.Chassis"};
 
                         const std::string assetTagInterface =
                             "xyz.openbmc_project.Inventory.Decorator."
@@ -557,10 +558,11 @@ inline void requestRoutesChassis(App& app)
                         const std::vector<std::string>& interfaces3 =
                             connectionNames[0].second;
 
-                        const std::array<const char*, 2> hasIndicatorLed = {
+                        const std::array<const char*, 3> hasIndicatorLed = {
                             "xyz.openbmc_project.Inventory.Item.Panel",
                             "xyz.openbmc_project.Inventory.Item.Board."
-                            "Motherboard"};
+                            "Motherboard",
+                            "xyz.openbmc_project.Inventory.Item.Chassis"};
                         bool indicatorChassis = false;
                         for (const char* interface : hasIndicatorLed)
                         {
