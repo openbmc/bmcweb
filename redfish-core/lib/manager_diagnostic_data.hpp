43bf4edf620cdfec70b1b80dd21f4f5933057ee7
Sui Chen <suichen@google.com>
2021-11-15 17:09:54 -0800

    Manager: add ManagerDiagnosticData resource
    Per our discussion with DMTF, BMC health metrics in Redfish are to be
    packed into the ManagerDiagnosticData resource.
    
    The resource is still under discussion in
    https://github.com/DMTF/Redfish/pull/4877, some details may go through
    some changes but the name of the Schema should not change.
    
    This change adds a link in the Manager to an empty
    ManagerDiagnosticData resource.
    
    Tested:
    
    $ curl -k -u root:0penBmc -X GET \
      https://localhost:2443/redfish/v1/Managers/bmc | \
      grep "ManagerDiagnosticData" -A4 -B4
    },
    "LogServices": {
      "@odata.id": "/redfish/v1/Managers/bmc/LogServices"
    },
    "ManagerDiagnosticData": {
      "@odata.id": "/redfish/v1/Managers/bmc/ManagerDiagnosticData"
    },
    "ManagerType": "BMC",
    "Model": "OpenBmc",
    "Name": "OpenBmc Manager",
    
    $ curl -k -u root:0penBmc -X GET
    https://localhost:2443/redfish/v1/Managers/bmc/ManagerDiagnosticData
    {
      "@odata.id": "/redfish/v1/Managers/bmc/ManagerDiagnosticData",
      "@odata.type": "#ManagerDiagnosticData.ManagerDiagnosticData"
    }
    
    When tested with the Redfish Service Validator, there are failures, but
    the failures are for existing resources and not ManagerDiagnosticData:
    
    *** /redfish/v1/Managers/bmc
         Type (#Manager.v1_11_0.Manager), GET SUCCESS (time: 0.187228)
    complex @odata.id: Expected @odata.id to match URI link
    /redfish/v1/Managers/bmc/ManagerDiagnosticData
    Verifying property that does not belong to this version:
    Manager.v1_13_0.Manager:ManagerDiagnosticData
    Links.ManagerForServers[0]: GET of resource at URI
    /redfish/v1/Systems/system returned HTTP 500. Check URI.
    SerialConsole: The given property is deprecated by revision: This
    property has been deprecated in favor of the SerialConsole property in
    the ComputerSystem resource.
          FAIL...
    
    *** /redfish/v1/Managers/bmc/ManagerDiagnosticData
         Type (#ManagerDiagnosticData.ManagerDiagnosticData), GET SUCCESS
    (time: 0.152759)
         PASS
    
    Signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: Iba242bc3b6ebec851dbd26e149d5c92c19a7992e

---
+++ redfish-core/lib/manager_diagnostic_data.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace redfish
+{
+
+inline void requestRoutesManagerDiagnosticData(App& app)
+{
+    BMCWEB_ROUTE(app, "/redfish/v1/Managers/bmc/ManagerDiagnosticData")
+        .privileges(redfish::privileges::getManagerCollection)
+        .methods(boost::beast::http::verb::get)(
+            [](const crow::Request&,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
+                asyncResp->res.jsonValue["@odata.type"] =
+                    "#ManagerDiagnosticData.ManagerDiagnosticData";
+                asyncResp->res.jsonValue["@odata.id"] =
+                    "/redfish/v1/Managers/bmc/ManagerDiagnosticData";
+            });
+}
+
+} // namespace redfish
