63e5fc59a4fa1ad6ce9326b344d555fb96121ef6
zhanghch05 <zhanghch05@inspur.com>
2021-11-17 16:04:14 +0800

    Remove AsyncResp from openHandler
    This change, moving the openHandler back to only supporting websocket
    disconnects and not 404s.Because AsyncResp is removed from openHandler.
    
    Tested:
    Opened KVM in webui-vue and it works.
    
    Signed-off-by: zhanghch05 <zhanghch05@inspur.com>
    Change-Id: I9f7e38fde046da0bb664d50d0c6158e743ff11b5

--- http/websocket.hpp
+++ http/websocket.hpp
@@ -65,8 +65,7 @@ class ConnectionImpl : public Connection
   public:
     ConnectionImpl(
         const crow::Request& reqIn, Adaptor adaptorIn,
-        std::function<void(Connection&, std::shared_ptr<bmcweb::AsyncResp>)>
-            openHandler,
+        std::function<void(Connection&)> openHandler,
         std::function<void(Connection&, const std::string&, bool)>
             messageHandler,
         std::function<void(Connection&, const std::string&)> closeHandler,
@@ -193,14 +192,11 @@ class ConnectionImpl : public Connection
     {
         BMCWEB_LOG_DEBUG << "Websocket accepted connection";
 
-        auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
-
-        asyncResp->res.result(boost::beast::http::status::ok);
         doRead();
 
         if (openHandler)
         {
-            openHandler(*this, asyncResp);
+            openHandler(*this);
         }
     }
 
@@ -280,8 +276,7 @@ class ConnectionImpl : public Connection
     std::vector<std::string> outBuffer;
     bool doingWrite = false;
 
-    std::function<void(Connection&, std::shared_ptr<bmcweb::AsyncResp>)>
-        openHandler;
+    std::function<void(Connection&)> openHandler;
     std::function<void(Connection&, const std::string&, bool)> messageHandler;
     std::function<void(Connection&, const std::string&)> closeHandler;
     std::function<void(Connection&)> errorHandler;
