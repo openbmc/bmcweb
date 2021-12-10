41789bf0dcabba15dae1fc394f071896eda7bbc7
Sui Chen <suichen@google.com>
2021-12-10 13:17:11 -0800

    [wip] i2c stats
    Change-Id: I47e5e6ad0b5fd5a10ead6748e6ae6ab27104878c
    Signed-off-by: Sui Chen <suichen@google.com>

--- redfish-core/lib/manager_diagnostic_data.hpp
+++ redfish-core/lib/manager_diagnostic_data.hpp
@@ -15,7 +15,7 @@ using SensorVariant =
 
 // Populate one field in the ManagerDiagnosticData using the reading of one
 // utilization sensor.
-inline void populateOneManagerDiagnosticDataEntry(
+inline void populateManagerDiagnosticDataWithUtilizationSensor(
     const std::shared_ptr<bmcweb::AsyncResp>& aResp,
     const sdbusplus::message::object_path& dbusObjectPath,
     const std::string& dbusServiceName)
@@ -64,16 +64,87 @@ inline void populateOneManagerDiagnosticDataEntry(
         "xyz.openbmc_project.Sensor.Value", "Value");
 }
 
+inline int extractI2CIdFromPath(std::string_view path) {
+    int result {};
+    size_t idx = path.rfind("i2c_");
+    if (idx == std::string_view::npos) {
+        return -1;
+    }
+    auto [ptr, ec] { std::from_chars(path.data()+idx+4, path.data()+path.size(), result) };
+    if (ec == std::errc()) {
+        return result;
+    }
+    return -1;
+}
+
+// Populate one I2CBusStatistics using one I2CStats object.
+inline void populateManagerDiagnosticDataWithI2CStats(
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+    const sdbusplus::message::object_path& dbusObjectPath,
+    const std::string& dbusServiceName)
+{
+    const int i2cId = extractI2CIdFromPath(dbusObjectPath.str);
+    if (i2cId == -1) return;
+
+    crow::connections::systemBus->async_method_call(
+        [aResp, dbusServiceName, dbusObjectPath, i2cId](
+            const boost::system::error_code ec,
+            std::vector<std::pair<std::string, SensorVariant>> properties) {
+
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "Error reading from service "
+                                 << dbusServiceName << ", path "
+                                 << dbusObjectPath.str << ": " << ec;
+                return;
+            }
+
+            for (const auto& [name, var] : properties) {
+                bool is_double = std::visit([](auto&& arg) -> bool {
+                    using T = std::decay_t<decltype(arg)>;
+                    if constexpr (std::is_arithmetic_v<T>) {
+                        return true;
+                    }
+                    return false;
+                }, var);
+                if (!is_double) continue;
+
+                if (aResp->res.jsonValue.find("I2CBuses") == aResp->res.jsonValue.end())
+                {
+                    aResp->res.jsonValue["I2CBuses"] = nlohmann::json::array();
+                }
+                nlohmann::json& i2cBuses = aResp->res.jsonValue;
+                
+                double value = std::get<double>(var);
+                nlohmann::json j;
+                j["I2CBusName"] = "i2c-" + std::to_string(i2cId);
+                if (name == "BusErrorCount") {
+                    j["BusErrorCount"] = value;
+                } else if (name == "NACKCount") {
+                    j["NACKCount"] = value;
+                } else if (name == "TotalTransactionCount") {
+                    j["TotalTransactionCount"] = value;
+                }
+                i2cBuses.push_back(j);
+            }
+        },
+        dbusServiceName.c_str(), dbusObjectPath.str.c_str(),
+        "org.freedesktop.DBus.Properties", "GetAll",
+        "xyz.openbmc_project.I2CStats");
+}
+
 inline void managerDiagnosticDataGetUtilization(
     const std::shared_ptr<bmcweb::AsyncResp>& aResp)
 {
     // Must use "bmc" as a "chassis".
     const std::string chassisId = "bmc";
 
-    // Get a list of all of the sensors that implement Sensor.Value
-    // and get the path and service names associated with the sensors.
-    const std::array<const char*, 1> interfaces = {
-        "xyz.openbmc_project.Sensor.Value"};
+    // We look for two interfaces:
+    // 1. Utilization sensors that implement the Sensor.Value interface
+    // 2. I2C stats dbus objects implementing the I2C.I2CStats interface
+    const std::array<const char*, 2> interfaces = {
+        "xyz.openbmc_project.Sensor.Value",
+        "xyz.openbmc_project.I2C.I2CStats"};
 
     // All sensors on the system may be returned from this call.
     // We need just the readings from the health monitor service.
@@ -94,25 +165,39 @@ inline void managerDiagnosticDataGetUtilization(
                 sdbusplus::message::object_path objectPath(path);
                 std::string name = objectPath.filename();
 
-                // There should be exactly 1 service with an object of
-                // Sensor.Value for all the utilization sensors that are related
-                // to the ManagerDiagnosticData resource.
+                // There should be exactly 1 service for both utilization sensors
+                // and I2C stats objects
                 const size_t numServices = servicesAndInterfaces.size();
                 if (numServices != 1)
                 {
                     continue;
                 }
 
+                // The as in {sas} is a list of interfaces. If it contains
+                // "xyz.openbmc_project.I2C.I2CStats" we treat it as an I2C
+                // statistics DBus object. Otherwise we treat it as a utility sensor.
+                bool is_i2c = false;
+                for (const std::string& iface : servicesAndInterfaces[1].second) {
+                    if (iface == "xyz.openbmc_project.I2C.I2CStats") {
+                        is_i2c = true;
+                    }
+                }
+
                 // Fetch the reading and populate the response.
                 const std::string& dbusService = servicesAndInterfaces[0].first;
-                populateOneManagerDiagnosticDataEntry(aResp, objectPath,
-                                                      dbusService);
+                if (is_i2c) {
+                    populateManagerDiagnosticDataWithI2CStats(aResp,
+                        objectPath, dbusService);
+                } else {
+                    populateManagerDiagnosticDataWithUtilizationSensor(aResp,
+                        objectPath, dbusService);
+                }
             }
         },
         "xyz.openbmc_project.ObjectMapper",
         "/xyz/openbmc_project/object_mapper",
         "xyz.openbmc_project.ObjectMapper", "GetSubTree",
-        "/xyz/openbmc_project/sensors", 2, interfaces);
+        "/xyz/openbmc_project", 2, interfaces);
 }
 inline void requestRoutesManagerDiagnosticData(App& app)
 {
