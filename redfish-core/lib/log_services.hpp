8527502330e1319d2dd4f3a1811c052f6ce658e4
Willy Tu <wltu@google.com>
2021-12-14 11:03:16 -0800

    json_utils: Split readJson to readJsonPatch and readJsonAction
    Added support for multiple level element access. For example, we can now
    acess `abc/xyz` directly instead of getting `abc` and then abc[`xyz`].
    
    For extra element error, it will only be triggered if the element at the
    root level is not a parent of any of the requested elemnets.
    
    For example,
    
    {
        "abc": {
            "xyz": 12
        }
    }
    
    Getting "abc/xyz" will satify the condition so it does not throw an
    error.
    
    readJsonPatch will replace the existing readJson and be used for path
    requests. It will not allow empty json input and all requested
    keys are required.
    
    readJsonAction will be used for Action requests where it is possible for
    all of the properties to be optional and allow empty request.
    The optional properties are determined by the requested values type.
    
    Like if the required value is std::optional<int> then the json input is
    allowed to not have the requested value.
    
    All current Action readJson are placed with readJsonAction. It does not
    change the existing behavior since it needs `std::optional`. This will
    have to be updated later as we define the default behavior.
    
    Tested: testing handleEmpty for optional parameters.
    
    Snippet to test the code.
    ```
    
    template <typename ValueType>
    bool handleEmpty(ValueType&)
    {
        if (!IsOptional<ValueType>::value)
        {
            return false;
        }
        return true;
    }
    
    template <typename ValueType, typename... UnpackTypes>
    bool handleEmpty(ValueType& val, UnpackTypes&... in)
    {
        return handleEmpty(val) && handleEmpty(in...);
    }
    
    int main()
    {
        std::vector<int> x;
        fmt::print("Vector: Expected to fail: {} \n", handleEmpty(x));
    
        int y;
        std::string z;
        fmt::print("int + string: Expected to fail: {} \n",
          handleEmpty(y, z));
    
        std::optional<std::string> w;
        fmt::print("optional string + int: Expected to fail: {} \n",
                   handleEmpty(w, y));
    
        std::optional<int> a;
        fmt::print(
          "optional string + optional int: Expected to pass: {} \n",
          handleEmpty(w, a));
    }
    ```
    
    Output
    ```
    Vector: Expected to fail: false
    int + string: Expected to fail: false
    optional string + int: Expected to fail: false
    optional string + optional int: Expected to pass: true
    ```
    
    Change-Id: Ia5e1f81695c528a20f1dc985aee19c920d8adaea
    Signed-off-by: Willy Tu <wltu@google.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -795,7 +795,7 @@ inline void createDump(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     std::optional<std::string> diagnosticDataType;
     std::optional<std::string> oemDiagnosticDataType;
 
-    if (!redfish::json_util::readJson(
+    if (!redfish::json_util::readJsonAction(
             req, asyncResp->res, "DiagnosticDataType", diagnosticDataType,
             "OEMDiagnosticDataType", oemDiagnosticDataType))
     {
@@ -1652,8 +1652,8 @@ inline void requestRoutesDBusEventLogEntry(App& app)
                const std::string& entryId) {
                 std::optional<bool> resolved;
 
-                if (!json_util::readJson(req, asyncResp->res, "Resolved",
-                                         resolved))
+                if (!json_util::readJsonPatch(req, asyncResp->res, "Resolved",
+                                              resolved))
                 {
                     return;
                 }
@@ -2880,7 +2880,7 @@ inline void requestRoutesCrashdumpCollect(App& app)
                          const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
             std::string diagnosticDataType;
             std::string oemDiagnosticDataType;
-            if (!redfish::json_util::readJson(
+            if (!redfish::json_util::readJsonAction(
                     req, asyncResp->res, "DiagnosticDataType",
                     diagnosticDataType, "OEMDiagnosticDataType",
                     oemDiagnosticDataType))
