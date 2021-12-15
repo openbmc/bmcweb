5187e09b891b9d0f12c76736caaa062120fd19f7
Josh Lehan <krellan@google.com>
2021-12-14 18:29:06 -0800

    error_messages: Use int64_t in invalidIndex
    Using int64_t instead of int, to permit 64-bit indices.
    
    This is to support ExternalStorer, a new project I am working on,
    which uses a 64-bit sequence number for long-term robustness.
    
    Change-Id: I00121933067030fd722f6b02c2d2dbd1854dff1c
    Signed-off-by: Josh Lehan <krellan@google.com>

--- redfish-core/include/error_messages.hpp
+++ redfish-core/include/error_messages.hpp
@@ -785,9 +785,9 @@ void actionNotSupported(crow::Response& res, const std::string& arg1);
  * @param[in] arg1 Parameter of message that will replace %1 in its body.
  *
  * @returns Message InvalidIndex formatted to JSON */
-nlohmann::json invalidIndex(const int& arg1);
+nlohmann::json invalidIndex(int64_t arg1);
 
-void invalidIndex(crow::Response& res, const int& arg1);
+void invalidIndex(crow::Response& res, int64_t arg1);
 
 /**
  * @brief Formats EmptyJSON message into JSON
