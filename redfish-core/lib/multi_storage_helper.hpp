626da13e9ee646c0ea03005d1238f70881455217
Willy Tu <wltu@google.com>
2021-12-28 15:49:47 -0800

    storage: add support for multiple storages
    Create one storage per chassis that is created with
    xyz.openbmc_project.Inventory.Item.Board
    and xyz.openbmc_project.Inventory.Item.Chassis.
    
    The Drive/StorageController under the storage will only contain the
    object paths under board/chassis's object path.
    
    `/redfish/v1/Systems/system/Storage/1` is removed so any
    Drive/StorageController not under s valid chassis/storage is invalid.
    
    Drive will now be linked to the chassis related to the storage instead
    of the main chassis.
    
    Storage's URI will be
    ```
    /redfish/v1/Systems/system/Storage/{storageId}
    ```
    where storageId is also equal to chassisId.
    
    Tested:
    Passed Redfish Validator for related resources.
    ```
    *** /redfish/v1/Systems/system/Storage
             Type (#StorageCollection.StorageCollection), GET SUCCESS (time: 0.286526)
             PASS
    
    *** /redfish/v1/Systems/system/Storage/storage0
             Type (#Storage.v1_7_1.Storage), GET SUCCESS (time: 0.323114)
             PASS
    
    *** /redfish/v1/Systems/system/Storage/storage0#/StorageControllers/0
             Type (#Storage.v1_7_0.StorageController), GET SUCCESS (time: 0)
             PASS
    
    *** /redfish/v1/Systems/system/Storage/storage0/Drives/drive0
             Type (#Drive.v1_7_0.Drive), GET SUCCESS (time: 0.306801)
             PASS
    ```
    
    Chassis
    ```
    $ curl -u root:0penBmc -X GET http://${bmc}/redfish/v1/Chassis/chassis0
    {
        "@odata.id": "/redfish/v1/Chassis/chassis0",
        "@odata.type": "#Chassis.v1_14_0.Chassis",
        "Id": "chassis0",
        "Links": {
            "Storage": [
              {
                    "@odata.id": "/redfish/v1/Systems/system/Storage/1"
              }
            ],
            "Storage@odata.count": 1
        },
        "Name": "chassis0",
    }}
    ```
    
    Storage Collection
    ```
    $ curl -u root:0penBmc -X GET http://${bmc}/redfish/v1/Systems/system/Storage
    {
      "@odata.id": "/redfish/v1/Systems/system/Storage",
      "@odata.type": "#StorageCollection.StorageCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Storage/storage0"
        }
      ],
      "Members@odata.count": 1,
      "Name": "Storage Collection"
    }
    ```
    
    Storage
    ```
    {
      "@odata.id": "/redfish/v1/Systems/system/Storage/storage0",
      "@odata.type": "#Storage.v1_7_1.Storage",
      "Drives": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Storage/storage0/Drives/drive0"
        }
      ],
      "Drives@odata.count": 1,
      "Id": "storage0",
      "Name": "Storage",
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "Enabled"
      },
      "StorageControllers": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Storage/storage0#/StorageControllers/0",
          "@odata.type": "#Storage.v1_7_0.StorageController",
          "MemberId": "controller",
          "Name": "controller",
          "Status": {
            "Health": "OK",
            "HealthRollup": "OK",
            "State": "Enabled"
          }
        }
      ]
    }
    ```
    
    Drive
    ```
    {
      "@odata.id": "/redfish/v1/Systems/system/Storage/storage0/Drives/drive0",
      "@odata.type": "#Drive.v1_7_0.Drive",
      "Id": "drive0",
      "Links": {
        "Chassis": {
          "@odata.id": "/redfish/v1/Chassis/drive0"
        }
      },
      "Name": "Drive",
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "Enabled"
      }
    }
    ```
    
    Change-Id: Ib81b68e7f61b817d4dfa4ed2f27afd6e74e8ce58
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ redfish-core/lib/multi_storage_helper.hpp
@@ -0,0 +1,126 @@
+#pragma once
+
+// Will remove this file once it get closer to being merged.
+
+#include "error_messages.hpp"
+#include "openbmc_dbus_rest.hpp"
+
+#include <app.hpp>
+
+#include <functional>
+
+/**
+ * @brief Retrieves resources over dbus to link to the chassis
+ *
+ * @param[in] aResp       - Shared pointer for completing asynchronous calls.
+ * @param[in] associationPath  - Chassis dbus path to look for the storage.
+ * @param[in] resoruce     - Resource to link to the chassis
+ * @param[in] resourceURI  - Resource URI to add the resource
+ * @param[in] interfaces   - List of interfaces to constrain the GetSubTree
+ * search
+ *
+ * @return None.
+ */
+inline void getChassisResources(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                                const std::string& associationPath,
+                                const std::string& resource,
+                                const std::string& resourceURI)
+{
+    sdbusplus::asio::getProperty<std::vector<std::string>>(
+        *crow::connections::systemBus, "xyz.openbmc_project.ObjectMapper",
+        associationPath, "xyz.openbmc_project.Association", "endpoints",
+        [aResp, resource,
+         resourceURI](const boost::system::error_code ec,
+                      const std::vector<std::string>& resourceList) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG << "DBUS response error";
+                return;
+            }
+
+            nlohmann::json& resources = aResp->res.jsonValue["Links"][resource];
+            resources = nlohmann::json::array();
+            auto& count =
+                aResp->res.jsonValue["Links"][resource + "@odata.count"];
+            count = 0;
+            for (const std::string& resource : resourceList)
+            {
+                sdbusplus::message::object_path path(resource);
+                std::string leaf = path.filename();
+                if (leaf.empty())
+                {
+                    continue;
+                }
+
+                resources.push_back({{"@odata.id", resourceURI + leaf}});
+            }
+            count = resources.size();
+        });
+}
+
+/**
+ * @brief Get the chassis is related to the resource.
+ *
+ * The chassis related to the resource is determined by the compare function.
+ * Once it find the chassis, it will call the callback function.
+ *
+ * @param[in,out]   asyncResp    Async HTTP response
+ * @param[in]       path         Object path of the current resource
+ * @param[in]       callback     Function to call once related chassis is found
+ */
+void getChassisId(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                  const std::string& path,
+                  const std::function<void(const std::string&)>& callback)
+{
+    sdbusplus::asio::getProperty<std::vector<std::string>>(
+        *crow::connections::systemBus, "xyz.openbmc_project.ObjectMapper",
+        path + "/chassis", "xyz.openbmc_project.Association", "endpoints",
+        [asyncResp, path,
+         callback](const boost::system::error_code ec,
+                   const std::vector<std::string>& chassisList) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG << path << " has no Chassis association";
+                return;
+            }
+
+            if (chassisList.size() != 1)
+            {
+                BMCWEB_LOG_ERROR
+                    << "getChassisId require it to be only one Chassis";
+                redfish::messages::internalError(asyncResp->res);
+                return;
+            }
+            const std::string chassisId =
+                sdbusplus::message::object_path(chassisList[0]).filename();
+            crow::connections::systemBus->async_method_call(
+                [asyncResp, chassisId, callback](
+                    const boost::system::error_code ec,
+                    const dbus::utility::MapperGetSubTreeResponse& subtree) {
+                    if (ec)
+                    {
+                        BMCWEB_LOG_DEBUG << "Can't find chassis!";
+                        return;
+                    }
+
+                    for (const std::pair<std::string,
+                                         dbus::utility::MapperServiceMap>&
+                             object : subtree)
+                    {
+                        if (sdbusplus::message::object_path(object.first)
+                                .filename() == chassisId)
+                        {
+                            callback(chassisId);
+                            return;
+                        }
+                    }
+                },
+                "xyz.openbmc_project.ObjectMapper",
+                "/xyz/openbmc_project/object_mapper",
+                "xyz.openbmc_project.ObjectMapper", "GetSubTree",
+                "/xyz/openbmc_project/inventory", 0,
+                std::array<const char*, 2>{
+                    "xyz.openbmc_project.Inventory.Item.Board",
+                    "xyz.openbmc_project.Inventory.Item.Chassis"});
+        });
+}
