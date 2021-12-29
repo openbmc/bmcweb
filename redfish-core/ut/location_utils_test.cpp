af00273c178a73ff69990305dbbe433567cdbf09
Willy Tu <wltu@google.com>
2021-12-28 21:12:06 -0800

    location_utils: Add helper function for Location and LocationType
    Added helper function to support location property for different
    resources
    
    Tested: No to redfish tree yet
    Added unit tests
    
    Change-Id: I7f2a8d6172e37dd72881fbfb5a9dfeaf83b2db09
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ redfish-core/ut/location_utils_test.cpp
@@ -0,0 +1,40 @@
+#include "utils/location_utils.hpp"
+
+#include "gtest/gtest.h"
+
+using redfish::location_util::getLocationType;
+using redfish::location_util::isConnector;
+
+TEST(LocationUtility, getLocationTypeValid)
+{
+    EXPECT_EQ(getLocationType("xyz.openbmc_project.Inventory.Connector.Slot"),
+              "Slot");
+    EXPECT_EQ(
+        getLocationType("xyz.openbmc_project.Inventory.Connector.Embedded"),
+        "Embedded");
+}
+
+TEST(LocationUtility, getLocationTypeInvalid)
+{
+    EXPECT_EQ(getLocationType(""), std::nullopt);
+    EXPECT_EQ(getLocationType("xyz.openbmc_project.Inventory.Connector.RANDOM"),
+              std::nullopt);
+    EXPECT_EQ(getLocationType("xyz.openbmc_project.Inventory.Connector"),
+              std::nullopt);
+    EXPECT_EQ(getLocationType("xyz.openbmc_project.Inventory."), std::nullopt);
+    EXPECT_EQ(getLocationType("xyz.openbmc_project.Item.Connector"),
+              std::nullopt);
+}
+
+TEST(LocationUtility, isConnector)
+{
+    EXPECT_TRUE(isConnector("xyz.openbmc_project.Inventory.Connector.Slot"));
+    EXPECT_TRUE(
+        isConnector("xyz.openbmc_project.Inventory.Connector.Embedded"));
+
+    EXPECT_FALSE(isConnector(""));
+    EXPECT_FALSE(isConnector("xyz.openbmc_project.Inventory.Connector.RANDOM"));
+    EXPECT_FALSE(isConnector("xyz.openbmc_project.Inventory.Connector"));
+    EXPECT_FALSE(isConnector("xyz.openbmc_project.Inventory."));
+    EXPECT_FALSE(isConnector("xyz.openbmc_project.Item.Connector"));
+}
