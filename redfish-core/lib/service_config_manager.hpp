f0353b54a63305f37d937226e88a8e700e78a292
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-01-06 23:49:59 +0800

    Refactor functions calling service-config-manager
    This patch fixes a common error of using boost::algorithm::starts_with
    when matching service name, and replaces the ObjectMapper call with
    calling xyz.openbmc_project.Control.Service.Manager directly to get the
    objects as this service should only ever have one implementation.
    
    This patch also moves the function used to enable/disable service in
    network_protocol.hpp to a new file service_config_manager.hpp for using
    across multiple source code files. This is intended for configuring SOL
    SSH and virtual media services in future patches.
    
    Tested:
    Verified enabling/disabling SSH and network IPMI service from Redfish
    works exactly same as before.
    
    Change-Id: Ia9f969847c09cc56f16894aa9b93a3168ee20c11
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

---
+++ redfish-core/lib/service_config_manager.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "dbus_utility.hpp"
+#include "error_messages.hpp"
+#include "redfish_util.hpp"
+
+namespace redfish
+{
+
+namespace service_config
+{
+
+static bool matchService(const sdbusplus::message::object_path& objPath,
+                         const std::string& serviceName)
+{
+    // For service named as <unitName>@<instanceName>, only compare the unitName
+    // part. In DBus object path, '@' is escaped as "_40"
+    std::string fullUnitName = objPath.filename();
+    size_t pos = fullUnitName.find("_40");
+    return fullUnitName.substr(0, pos) == serviceName;
+}
+
+template <typename CallbackFunc>
+void setEnabled(const std::string& serviceName, const bool enabled,
+                CallbackFunc&& callback)
+{
+    crow::connections::systemBus->async_method_call(
+        [serviceName, enabled,
+         callback](const boost::system::error_code ec,
+                   const dbus::utility::ManagedObjectType& objects) {
+            if (ec)
+            {
+                callback(ec);
+                return;
+            }
+
+            for (const auto& object : objects)
+            {
+                if (matchService(object.first, serviceName))
+                {
+                    crow::connections::systemBus->async_method_call(
+                        [callback](const boost::system::error_code ec2) {
+                            if (ec2)
+                            {
+                                callback(ec2);
+                                return;
+                            }
+                        },
+                        "xyz.openbmc_project.Control.Service.Manager",
+                        object.first.str, "org.freedesktop.DBus.Properties",
+                        "Set", "xyz.openbmc_project.Control.Service.Attributes",
+                        "Running", dbus::utility::DbusVariantType{enabled});
+
+                    crow::connections::systemBus->async_method_call(
+                        [callback](const boost::system::error_code ec2) {
+                            if (ec2)
+                            {
+                                callback(ec2);
+                                return;
+                            }
+                        },
+                        "xyz.openbmc_project.Control.Service.Manager",
+                        object.first.str, "org.freedesktop.DBus.Properties",
+                        "Set", "xyz.openbmc_project.Control.Service.Attributes",
+                        "Enabled", dbus::utility::DbusVariantType{enabled});
+                }
+            }
+        },
+        "xyz.openbmc_project.Control.Service.Manager",
+        "/xyz/openbmc_project/control/service",
+        "org.freedesktop.DBus.ObjectManager", "GetManagedObjects");
+}
+
+} // namespace service_config
+
+} // namespace redfish
