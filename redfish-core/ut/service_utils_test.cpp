a3510305a8a5dcdd4837ead70c52d1d442aa7022
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-01-06 23:49:59 +0800

    Refactor functions calling service-config-manager
    This patch fixes a common error of using boost::algorithm::starts_with
    when matching service name. It also moves the function used to enable/
    disable service in network_protocol.hpp to a new file service_util.hpp
    for using across multiple files. This is intended for configuring SOL
    SSH and virtual media services in future patches.
    
    Tested:
    Verified enabling/disabling SSH and network IPMI service from Redfish
    works exactly same as before.
    
    Change-Id: Ia9f969847c09cc56f16894aa9b93a3168ee20c11
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

---
+++ redfish-core/ut/service_utils_test.cpp
@@ -0,0 +1,31 @@
+#include "utils/service_utils.hpp"
+
+#include <gmock/gmock.h>
+
+namespace redfish
+{
+namespace service_util
+{
+
+TEST(ServiceUtilsTest, MatchServiceGood)
+{
+    sdbusplus::message::object_path basePath("/service/base/path");
+
+    EXPECT_TRUE(matchService(basePath / "serviceUnit", "serviceUnit"));
+    EXPECT_TRUE(matchService(basePath / "service-unit", "service-unit"));
+    EXPECT_TRUE(matchService(basePath / "serviceUnit@test", "serviceUnit"));
+    EXPECT_TRUE(matchService(basePath / "service-unit@test", "service-unit"));
+}
+
+TEST(ServiceUtilsTest, MatchServiceBad)
+{
+    sdbusplus::message::object_path basePath("/service/base/path");
+
+    EXPECT_FALSE(matchService(basePath / "serviceUnit", "service-unit"));
+    EXPECT_FALSE(matchService(basePath / "serviceUnitTest", "serviceUnit"));
+    EXPECT_FALSE(matchService(basePath / "serviceUnit@test", "service-unit"));
+    EXPECT_FALSE(matchService(basePath / "service-unit-test", "service-unit"));
+}
+
+} // namespace service_util
+} // namespace redfish
