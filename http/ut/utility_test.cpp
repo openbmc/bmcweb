f3b65972ea514cf976c03d521785687a6334971b
Ed Tanous <edtanous@google.com>
2022-01-11 10:50:23 -0800

    Update unit tests for getDateTime
    The GetDateTime series needs more unit tests to ensure that we don't get
    bad behavior when we hit the extremes.
    
    Tested: Unit tests pass
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I7f45e5d84d644780832112dca14bdb9c768903ff

--- http/ut/utility_test.cpp
+++ http/ut/utility_test.cpp
@@ -2,11 +2,16 @@
 
 #include "gmock/gmock.h"
 
+namespace crow::utility
+{
+namespace
+{
+
 TEST(Utility, Base64DecodeAuthString)
 {
     std::string authString("dXNlcm40bWU6cGFzc3cwcmQ=");
     std::string result;
-    EXPECT_TRUE(crow::utility::base64Decode(authString, result));
+    EXPECT_TRUE(base64Decode(authString, result));
     EXPECT_EQ(result, "usern4me:passw0rd");
 }
 
@@ -14,7 +19,7 @@ TEST(Utility, Base64DecodeNonAscii)
 {
     std::string junkString("\xff\xee\xdd\xcc\x01\x11\x22\x33");
     std::string result;
-    EXPECT_FALSE(crow::utility::base64Decode(junkString, result));
+    EXPECT_FALSE(base64Decode(junkString, result));
 }
 
 TEST(Utility, Base64EncodeString)
@@ -22,28 +27,28 @@ TEST(Utility, Base64EncodeString)
     using namespace std::string_literals;
     std::string encoded;
 
-    encoded = crow::utility::base64encode("");
+    encoded = base64encode("");
     EXPECT_EQ(encoded, "");
 
-    encoded = crow::utility::base64encode("f");
+    encoded = base64encode("f");
     EXPECT_EQ(encoded, "Zg==");
 
-    encoded = crow::utility::base64encode("f0");
+    encoded = base64encode("f0");
     EXPECT_EQ(encoded, "ZjA=");
 
-    encoded = crow::utility::base64encode("f0\0"s);
+    encoded = base64encode("f0\0"s);
     EXPECT_EQ(encoded, "ZjAA");
 
-    encoded = crow::utility::base64encode("f0\0 "s);
+    encoded = base64encode("f0\0 "s);
     EXPECT_EQ(encoded, "ZjAAIA==");
 
-    encoded = crow::utility::base64encode("f0\0 B"s);
+    encoded = base64encode("f0\0 B"s);
     EXPECT_EQ(encoded, "ZjAAIEI=");
 
-    encoded = crow::utility::base64encode("f0\0 Ba"s);
+    encoded = base64encode("f0\0 Ba"s);
     EXPECT_EQ(encoded, "ZjAAIEJh");
 
-    encoded = crow::utility::base64encode("f0\0 Bar"s);
+    encoded = base64encode("f0\0 Bar"s);
     EXPECT_EQ(encoded, "ZjAAIEJhcg==");
 }
 
@@ -51,27 +56,54 @@ TEST(Utility, Base64EncodeDecodeString)
 {
     using namespace std::string_literals;
     std::string data("Data fr\0m 90 reading a \nFile"s);
-    std::string encoded = crow::utility::base64encode(data);
+    std::string encoded = base64encode(data);
     std::string decoded;
-    EXPECT_TRUE(crow::utility::base64Decode(encoded, decoded));
+    EXPECT_TRUE(base64Decode(encoded, decoded));
     EXPECT_EQ(data, decoded);
 }
 
-TEST(Utility, GetDateTime)
+TEST(Utility, GetDateTimeStdtime)
 {
     // some time before the epoch
-    EXPECT_EQ(crow::utility::getDateTimeStdtime(std::time_t{-1234567}),
+    EXPECT_EQ(getDateTimeStdtime(std::time_t{-1234567}),
               "1969-12-17T17:03:53+00:00");
+
     // epoch
-    EXPECT_EQ(crow::utility::getDateTimeStdtime(std::time_t{0}),
+    EXPECT_EQ(getDateTimeStdtime(std::time_t{0}), "1970-01-01T00:00:00+00:00");
+
+    // Limits
+    EXPECT_EQ(getDateTimeStdtime(std::numeric_limits<std::time_t>::max()),
+              "9999-12-31T23:59:59+00:00");
+    EXPECT_EQ(getDateTimeStdtime(std::numeric_limits<std::time_t>::min()),
               "1970-01-01T00:00:00+00:00");
-    // some time in the past after the epoch
-    EXPECT_EQ(crow::utility::getDateTimeUint(uint64_t{1638312095}),
+}
+
+TEST(Utility, GetDateTimeUint)
+{
+    EXPECT_EQ(getDateTimeUint(uint64_t{1638312095}),
               "2021-11-30T22:41:35+00:00");
     // some time in the future, beyond 2038
-    EXPECT_EQ(crow::utility::getDateTimeUint(uint64_t{41638312095}),
+    EXPECT_EQ(getDateTimeUint(uint64_t{41638312095}),
               "3289-06-18T21:48:15+00:00");
     // the maximum time we support
-    EXPECT_EQ(crow::utility::getDateTimeUint(uint64_t{253402300799}),
+    EXPECT_EQ(getDateTimeUint(uint64_t{253402300799}),
               "9999-12-31T23:59:59+00:00");
+
+    // No exception and returns the maximum Redfish date
+    EXPECT_EQ(getDateTimeUint(std::numeric_limits<uint64_t>::max()),
+              "9999-12-31T23:59:59+00:00");
+
+    EXPECT_EQ(getDateTimeUint(std::numeric_limits<uint64_t>::min()),
+              "1970-01-01T00:00:00+00:00");
 }
+
+TEST(Utility, GetDateTimeUintMs)
+{
+    // No exception and returns the maximum Redfish date
+    EXPECT_EQ(getDateTimeUintMs(std::numeric_limits<uint64_t>::max()),
+              "9999-12-31T23:59:59.999000+00:00");
+    EXPECT_EQ(getDateTimeUintMs(std::numeric_limits<uint64_t>::min()),
+              "1970-01-01T00:00:00+00:00");
+}
+} // namespace
+} // namespace crow::utility
