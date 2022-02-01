c9a712b603218cbd8ed151154a2ea50b0aacd14a
Abhishek Patel <Abhishek.Patel@ibm.com>
2022-02-01 10:07:15 -0600

    Retrieve userGroups Information ("AccountTypes")
    This commit enhances the redfish API to retrieve userGroups information
    for each user account.
    
    "Redfish" is always enabled in each user role. That's why it was
    hardcoded into JSON response data in the old redfish API, where now it
    gets retrieved using dbus interface xyz.openbmc_project.User.Attributes.
    UserGroups.
    
    UserGroups retrieve data are "redfish", "ssh", "web", and "ipmi", where
    redfish DMTF Schema has predefined enum type as described below.
    
      "AccountTypes": {
        "enum":["Redfish", "SNMP", "OEM", "HostConsole", "ManagerConsole",
                "IPMI", "KVMIP", "VirtualMedia", "WebUI"]
       }
    
    Here UserGroups ssh is mapped with two AccountTypes "HostConsole",
    "ManagerConsole".
    
      - Redfish ManagerConsole == SSH to port 22 == Phosphor User manager
        Phosphor “ssh” privilege
      - Redfish HostConsole == SSH to port 2200 (host console), Which
        OpenBMC implements using the Phosphor User manager Phosphor “ssh”
        privilege.
    
    The UserGroups redfish is also mapped to "Redfish" and "WebUI"
    
    Test:
    
    Pass Redfish Service Validator
    
    $ curl -k -X GET
    https://$bmc@r5:18080/redfish/v1/AccountService/Accounts/webuser
    {
      ...
      "AccountTypes": [
        "Redfish",
        "WebUI"
      ],
    ...
    
    Signed-off-by: Abhishek Patel <Abhishek.Patel@ibm.com>
    Change-Id: Iaa9b6c07b3d26e8994be28a50c22437e0bc9bc8f
    Signed-off-by: Shantappa Teekappanavar <shantappa.teekappanavar@ibm.com>

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -109,6 +109,51 @@ inline std::string getPrivilegeFromRoleId(std::string_view role)
     return "";
 }
 
+inline std::optional<std::vector<std::string>>
+    getAccountTypeFromUserGroup(std::string_view userGroup)
+{
+    if (userGroup == "redfish")
+    {
+        return {{"Redfish", "WebUI"}};
+    }
+    if (userGroup == "ipmi")
+    {
+        return {{"IPMI"}};
+    }
+    if (userGroup == "ssh")
+    {
+        return {{"HostConsole", "ManagerConsole"}};
+    }
+    return std::nullopt;
+}
+
+inline bool translateUserGroup(const std::vector<std::string>& userGroups,
+                               crow::Response& res)
+{
+    nlohmann::json& accountTypes = res.jsonValue["AccountTypes"];
+    accountTypes = nlohmann::json::array();
+    for (const auto& userGroup : userGroups)
+    {
+        if (auto accountType = getAccountTypeFromUserGroup(userGroup);
+            accountType)
+        {
+            for (const std::string& value : *accountType)
+            {
+                accountTypes.push_back(value);
+            }
+        }
+        else
+        {
+            if (userGroup == "web")
+            {
+                continue;
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
 inline void userErrorMessageHandler(
     const sd_bus_error* e, const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const std::string& newUser, const std::string& username)
@@ -1727,7 +1772,6 @@ inline void
         asyncResp->res.jsonValue["Name"] = "User Account";
         asyncResp->res.jsonValue["Description"] = "User Account";
         asyncResp->res.jsonValue["Password"] = nullptr;
-        asyncResp->res.jsonValue["AccountTypes"] = {"Redfish"};
 
         for (const auto& interface : userIt->second)
         {
@@ -1802,6 +1846,25 @@ inline void
                         }
                         asyncResp->res.jsonValue["PasswordChangeRequired"] =
                             *userPasswordExpired;
+                    }
+                    else if (property.first == "UserGroups")
+                    {
+                        const std::vector<std::string>* userGroups =
+                            std::get_if<std::vector<std::string>>(
+                                &property.second);
+                        if (userGroups == nullptr)
+                        {
+                            BMCWEB_LOG_ERROR
+                                << "userGroups wasn't a string vector";
+                            messages::internalError(asyncResp->res);
+                            return;
+                        }
+                        if (!translateUserGroup(*userGroups, asyncResp->res))
+                        {
+                            BMCWEB_LOG_ERROR << "userGroups mapping failed";
+                            messages::internalError(asyncResp->res);
+                            return;
+                        }
                     }
                 }
             }
