b50860e2626c6920453b81db2336d00783114e34
Gunnar Mills <gmills@us.ibm.com>
2022-02-02 21:56:44 +0000

    Remove q-factor weighting on Accept Header
    bmcweb does not do anything with the q-factor weighting (;q=) so just
    remove it from the encoding.
    
    This is needed because routes like
    "/redfish/v1/Systems/system/LogServices/EventLog/Entries/<str>/attachment"
    have a check for isOctetAccepted. Even though */* is in the Accept
    Header isOctetAccepted still fails due to the q-factor weighting.
    
    On the system I tested, on firefox, Accept looks like:
    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
    
    See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept
    Tested:
    /redfish/v1/Systems/system/LogServices/PostCodes/Entries/<str>/attachment/
    and .../EventLog/Entries/<str>/attachment now return correctly.
    
    Change-Id: I969f5f2c32c4acccd4d80615f17c44d0c8fabd0d
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- include/ut/http_utility_test.cpp
+++ include/ut/http_utility_test.cpp
@@ -8,6 +8,9 @@ TEST(HttpUtility, requestPrefersHtml)
         http_helpers::requestPrefersHtml("*/*, application/octet-stream"));
     EXPECT_TRUE(http_helpers::isOctetAccepted("*/*, application/octet-stream"));
 
+    EXPECT_TRUE(http_helpers::isOctetAccepted("text/html, */*;q=0.8"));
+    EXPECT_TRUE(http_helpers::isOctetAccepted("application/xhtml+xml,application/octet-stream,text/html"));
+
     EXPECT_TRUE(
         http_helpers::requestPrefersHtml("text/html, application/json"));
     EXPECT_FALSE(http_helpers::isOctetAccepted("text/html, application/json"));
