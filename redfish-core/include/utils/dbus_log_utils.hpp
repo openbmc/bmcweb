25fa850271b3dacc5f2fcaf152e25080b6fcb36a
Shichen Lin <shichenl@nvidia.com>
2022-02-03 15:40:37 -0800

    Redfish: Create new utility headers
    This commit creates new utility headers for registry and dbus log entry
    handling as these utility functions can be shared between LogService
    and EventService.
    
    New unit tests are also added.
    
    Tested by:
    Redfish Service Validator
    
    Signed-off-by: Shichen Lin <shichenl@nvidia.com>
    Change-Id: I7845f936d968ae13a0a1e23d1dd009f89aa90537

---
+++ redfish-core/include/utils/dbus_log_utils.hpp
@@ -0,0 +1,104 @@
+/*
+// Copyright (c) 2021 NVIDIA Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <string>
+#include <vector>
+#include <map>
+
+namespace redfish
+{
+    inline std::string translateSeverityDbusToRedfish(const std::string& s)
+    {
+        if ((s == "xyz.openbmc_project.Logging.Entry.Level.Alert") ||
+            (s == "xyz.openbmc_project.Logging.Entry.Level.Critical") ||
+            (s == "xyz.openbmc_project.Logging.Entry.Level.Emergency") ||
+            (s == "xyz.openbmc_project.Logging.Entry.Level.Error"))
+        {
+            return "Critical";
+        }
+        if ((s == "xyz.openbmc_project.Logging.Entry.Level.Debug") ||
+            (s == "xyz.openbmc_project.Logging.Entry.Level.Informational") ||
+            (s == "xyz.openbmc_project.Logging.Entry.Level.Notice"))
+        {
+            return "OK";
+        }
+        if (s == "xyz.openbmc_project.Logging.Entry.Level.Warning")
+        {
+            return "Warning";
+        }
+        return "";
+    }
+
+    class AdditionalData
+    {
+        enum SameKeyOp
+        {
+            overwrite = 0,
+            append = 1,
+        };
+
+    public:
+        // DBus Event Log additionalData format is like,
+        // "key1=val1" "key2=val2"...
+        AdditionalData(const std::vector<std::string>& additionalData,
+                       const SameKeyOp& op = overwrite)
+        {
+            convertion(additionalData, data, op);
+        }
+
+        void convertion(const std::vector<std::string>& additionalData,
+                        std::map<std::string, std::string>& data,
+                        const SameKeyOp& op)
+        {
+            for (auto& kv : additionalData)
+            {
+                std::vector<std::string> fields;
+                fields.reserve(2);
+                boost::split(fields, kv, boost::is_any_of("="));
+                if (data.count(fields[0]) <= 0)
+                {
+                    data[fields[0]] = "";
+                }
+                if (op == overwrite)
+                {
+                    data[fields[0]] = fields[1];
+                }
+                else if (op == append)
+                {
+                    // In append mode, all values for the same key will be
+                    // separated by ';', e.g., "key1=val1_1;val1_2;...;val1_n"
+                    data[fields[0]] += (!data[fields[0]].empty()) ? ";" : "";
+                    data[fields[0]] += fields[1];
+                }
+            }
+        }
+
+        std::string& operator[](const std::string& key)
+        {
+            return data[key];
+        }
+
+        std::size_t count(const std::string& key)
+        {
+            return data.count(key);
+        }
+
+    protected:
+        std::map<std::string, std::string> data;
+    };
+}
---
+++ redfish-core/ut/registry_utils_test.cpp
@@ -0,0 +1,144 @@
+/*
+// Copyright (c) 2021 NVIDIA Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+/*!
+ * @file    registry_utils_test.cpp
+ * @brief   Source code for registry utility testing.
+ */
+
+/* -------------------------------- Includes -------------------------------- */
+#include <utils/registry_utils.hpp>
+#include <gmock/gmock.h>
+
+namespace redfish
+{
+namespace message_registries
+{
+
+bool cmpMsg(const Message *msg1, const Message *msg2)
+{
+    if (strcmp(msg1->description, msg2->description) != 0)
+    {
+        return false;
+    }
+    if (strcmp(msg1->message, msg2->message) != 0)
+    {
+        return false;
+    }
+    if (strcmp(msg1->severity, msg2->severity) != 0)
+    {
+        return false;
+    }
+    if (strcmp(msg1->messageSeverity, msg2->messageSeverity) != 0)
+    {
+        return false;
+    }
+    if (msg1->numberOfArgs != msg2->numberOfArgs)
+    {
+        return false;
+    }
+    for (size_t i=0; i<msg1->numberOfArgs; i++)
+    {
+        if (strcmp(msg1->paramTypes[i], msg2->paramTypes[i]) != 0)
+        {
+            return false;
+        }
+    }
+    if (strcmp(msg1->resolution, msg2->resolution) != 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool cmpRegistry(const boost::beast::span<const MessageEntry>& registry1,
+    const boost::beast::span<const MessageEntry>& registry2)
+{
+    if (registry1.size() != registry2.size())
+    {
+        return false;
+    }
+    for (size_t i=0; i<registry1.size(); i++)
+    {
+        const MessageEntry *pRegistry1 = registry1.data();
+        const MessageEntry *pRegistry2 = registry2.data();
+        if (strcmp(pRegistry1->first, pRegistry1->first) != 0)
+        {
+            return false;
+        }
+        if (!cmpMsg(&pRegistry1->second, &pRegistry2->second))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+TEST(RegistryUtilsTest, GetRegistryFromPrefix)
+{
+    EXPECT_TRUE(cmpRegistry(
+        getRegistryFromPrefix(task_event::header.registryPrefix),
+        boost::beast::span<const MessageEntry>(task_event::registry)));
+    EXPECT_TRUE(cmpRegistry(
+        getRegistryFromPrefix("abc"),
+        boost::beast::span<const MessageEntry>()));
+}
+
+TEST(RegistryUtilsTest, GetMessageFromRegistry)
+{
+    const boost::beast::span<const MessageEntry> resourceRegistry = 
+        getRegistryFromPrefix("ResourceEvent");
+
+    EXPECT_TRUE(
+        cmpMsg(getMessageFromRegistry("LicenseAdded", resourceRegistry),
+        &resource_event::registry[0].second));
+    EXPECT_EQ(getMessageFromRegistry("abc", resourceRegistry), 
+        nullptr);
+}
+
+TEST(RegistryUtilsTest, GetPrefix)
+{
+    EXPECT_STREQ(
+        getPrefix("ResourceEvent.1.0.ResourceCreated").c_str(),
+        "ResourceEvent");
+}
+
+TEST(RegistryUtilsTest, GetMessage)
+{
+    // correct MessageId
+    EXPECT_TRUE(
+        cmpMsg(getMessage("ResourceEvent.1.0.LicenseAdded"),
+        &resource_event::registry[0].second));
+
+    // invaild MessageId
+    EXPECT_EQ(getMessage("ResourceEvent.0.LicenseAdded"),
+        nullptr);
+    EXPECT_EQ(getMessage("a.b.c.d"),
+        nullptr);
+}
+
+TEST(RegistryUtilsTest, IsMessageIdValid)
+{
+    // correct MessageId
+    EXPECT_TRUE(isMessageIdValid("ResourceEvent.1.0.LicenseAdded"));
+    
+    // invaild MessageId
+    EXPECT_FALSE(isMessageIdValid("ResourceEvent.0.LicenseAdded"));
+    EXPECT_FALSE(isMessageIdValid("a.b.c.d"));
+}
+
+} // namespace message_registries
+} // namespace redfish
