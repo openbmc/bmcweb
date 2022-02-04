e2052955a658ae1a570beb08b1b0a1d49acfd7d7
Sui Chen <suichen@google.com>
2022-02-03 22:01:27 -0800

    Update schema pack to 2021.4
    Signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: I6560ec010362df28e264d80452059b534b3f89ba

--- scripts/update_schemas.py
+++ scripts/update_schemas.py
@@ -9,7 +9,7 @@ import json
 
 import xml.etree.ElementTree as ET
 
-VERSION = "DSP8010_2021.3"
+VERSION = "DSP8010_2021.4"
 
 # To use a new schema, add to list and rerun tool
 include_list = [
@@ -47,6 +47,7 @@ include_list = [
     'ManagerAccount',
     'ManagerAccountCollection',
     'ManagerCollection',
+    'ManagerDiagnosticData',
     'ManagerNetworkProtocol',
     'Memory',
     'MemoryCollection',
