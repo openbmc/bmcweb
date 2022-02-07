c5ba4c27ddb28c7844c0ea3078df4114f531dd25
Ed Tanous <edtanous@google.com>
2022-02-07 09:59:55 -0800

    Abstract fillMessageArgs and unit test it
    EventService has a routine for taking a message registry entry and
    populate it with data.  This ideally should be part of the message
    registry namespace, not EventService, as it could be useful to later
    patchsets.  So break out the method, and write some unit tests to ensure
    that it can be relied upon in the future.
    
    Tested: Unit tests ran and passing.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I052d9492f306b63fb72cbf78286370ed0c477430

---
+++ redfish-core/ut/registries_test.cpp
@@ -0,0 +1,19 @@
+#include "registries.hpp"
+
+#include "gmock/gmock.h"
+
+TEST(RedfishRegistries, fillMessageArgs)
+{
+    using redfish::message_registries::fillMessageArgs;
+    std::string toFill("%1");
+    fillMessageArgs({{"foo"}}, toFill);
+    EXPECT_EQ(toFill, "foo");
+
+    toFill = "";
+    fillMessageArgs({}, toFill);
+    EXPECT_EQ(toFill, "");
+
+    toFill = "%1, %2";
+    fillMessageArgs({{"foo", "bar"}}, toFill);
+    EXPECT_EQ(toFill, "foo, bar");
+}
