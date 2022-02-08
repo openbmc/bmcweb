273bd6b54d709dcb37695c89f44d54e19c865b12
Ed Tanous <edtanous@google.com>
2022-02-07 23:53:03 -0800

    Change message_registries namespace to registries
    The message_registries namespace is overly wordy, and results in very
    long defines.  Doing this one minor change reduces the code by 50 lines.
    This seems worthwhile.
    
    Tested: Unit tests pass.  Namespace change only.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ib1401580b3fa47596eb56cdc86e60eeeb1c2f952

--- redfish-core/ut/registries_test.cpp
+++ redfish-core/ut/registries_test.cpp
@@ -4,7 +4,7 @@
 
 TEST(RedfishRegistries, fillMessageArgs)
 {
-    using redfish::message_registries::fillMessageArgs;
+    using redfish::registries::fillMessageArgs;
     std::string toFill("%1");
     fillMessageArgs(std::to_array<std::string_view>({"foo"}), toFill);
     EXPECT_EQ(toFill, "foo");
