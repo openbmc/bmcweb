77183856bcb5ec830f3d80adecd54416f3641084
Ed Tanous <edtanous@google.com>
2022-02-08 12:24:30 +0100

    WIP: Add option for validating content-type header
    For systems implementing to the OWASP security guidelines[1] (of which all
    should ideally) we should be checking the content-type header all times
    that we parse a request as JSON.
    
    This commit adds an option for parsing content-type, and sets a default
    of "must get content-type".
    
    Tested: WIP
    
    [1] cheatsheetseries.owasp.org/cheatsheets/REST_Security_Cheat_Sheet.html
    
    Change-Id: I0fd92520eab073b6e078da56aa070d7fe2bd40da
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- redfish-core/lib/certificate_service.hpp
+++ redfish-core/lib/certificate_service.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "http/parsing.hpp"
 #include "utils/dbus_utils.hpp"
 
 #include <app.hpp>
@@ -95,9 +96,9 @@ inline std::string getCertificateFromReqBody(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const crow::Request& req)
 {
-    nlohmann::json reqJson = nlohmann::json::parse(req.body, nullptr, false);
-
-    if (reqJson.is_discarded())
+    nlohmann::json reqJson;
+    JsonParseResult ret = parseRequestAsJson(req, reqJson);
+    if (ret != JsonParseResult::Success)
     {
         // We did not receive JSON request, proceed as it is RAW data
         return req.body;
