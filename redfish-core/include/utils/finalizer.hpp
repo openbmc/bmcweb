bd8613f4c2a617795871a98ddf1b30598fcb0c84
Szymon Dompke <szymon.dompke@intel.com>
2022-02-11 21:22:30 +0100

    Add support for POST on TriggersCollection
    Added POST method on /redfish/v1/TelemetryService/Triggers uri, which
    creates new trigger in telemetry service, by using dbus call AddTrigger.
    
    By DMTF, most of the properties are not required, and as such are
    treated as optional. Some values can be deduced from others (like
    'MetricType', depending on 'DiscreteTriggers' or 'NumericThresholds').
    All properties provided in POST body by user will be verified against
    each other, and errors will be raised. Few examples of such situations:
    - 'MetricType' is set to 'Discrete' but 'NumericThresholds' was passed.
    - 'MetricType' is set to 'Numeric' but "DiscreteTriggers' or
      'DiscreteTriggerCondition' were passed
    - 'DiscreteTriggerCondition' is set to 'Specified' but
      'DiscreteTriggers' is an empty array or was not passed.
    - 'DiscreteTriggerCondition' is set to 'Changed' but 'DiscreteTriggers'
      is passed and is not an empty array.
    
    Example 1 – Trigger with discrete values:
    {
        "Id": "TestTrigger",
        "MetricType": "Discrete",
        "TriggerActions": [
            "RedfishEvent"
        ],
        "DiscreteTriggerCondition": "Specified",
        "DiscreteTriggers": [
            {
                "Value": "55.88",
                "DwellTime": "PT0.001S",
                "Severity": "Warning"
            },
            {
                "Name": "My discrete trigger",
                "Value": "55.88",
                "DwellTime": "PT0.001S",
                "Severity": "OK"
            },
            {
                "Value": "55.88",
                "DwellTime": "PT0.001S",
                "Severity": "Critical"
            }
        ],
        "MetricProperties": [
            "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/0/Reading"
        ],
        "Links": {
            "MetricReportDefinitions": []
        }
    }
    
    Example 2 – trigger with numeric threshold:
    {
        "Id": "TestTrigger2",
        "Name": "My Numeric Trigger",
        "MetricType": "Numeric",
        "TriggerActions": [
            "RedfishEvent",
            "RedfishMetricReport"
        ],
        "NumericThresholds": {
            "UpperCritical": {
                "Reading": 50,
                "Activation": "Increasing",
                "DwellTime": "PT0.001S"
            },
            "UpperWarning": {
                "Reading": 48.1,
                "Activation": "Increasing",
                "DwellTime": "PT0.004S"
            }
        },
        "MetricProperties": [
            "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/0/Reading",
            "/redfish/v1/Chassis/AC_Baseboard/Thermal#/Fans/17/Reading"
        ],
        "Links": {
            "MetricReportDefinitions": [
                "/redfish/v1/TelemetryService/MetricReportDefinitions/PowerMetrics",
                "/redfish/v1/TelemetryService/MetricReportDefinitions/PowerMetricStats",
                "/redfish/v1/TelemetryService/MetricReportDefinitions/PlatformPowerUsage"
            ]
        }
    }
    
    Tested:
    - Triggers were successfully created with above example message bodies.
      This can be checked by calling:
      'busctl tree xyz.openbmc_project.Telemetry'.
    - Expected errors were returned for messages with incorrect or mutually
      exclusive properties and incorrect values.
    - Redfish service validator is passing.
    
    Signed-off-by: Szymon Dompke <szymon.dompke@intel.com>
    Change-Id: Ief8c76de8aa660ae0d2dbe4610c26a28186a290a

---
+++ redfish-core/include/utils/finalizer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <functional>
+
+namespace redfish
+{
+
+namespace utils
+{
+
+class Finalizer
+{
+  public:
+    Finalizer() = delete;
+    Finalizer(std::function<void()> finalizer) : finalizer(std::move(finalizer))
+    {}
+
+    Finalizer(const Finalizer&) = delete;
+    Finalizer(Finalizer&&) = delete;
+
+    Finalizer& operator=(const Finalizer&) = delete;
+    Finalizer& operator=(Finalizer&&) = delete;
+
+    ~Finalizer()
+    {
+        if (finalizer)
+        {
+            finalizer();
+        }
+    }
+
+  private:
+    std::function<void()> finalizer;
+};
+
+} // namespace utils
+
+} // namespace redfish
