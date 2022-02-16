f7dc0ddd165d181a2f93df832f344664cc34f620
Josh Lehan <krellan@google.com>
2022-02-15 17:50:42 -0800

    bmcweb: New LogServices feature ExternalStorer
    This is a new feature that lets external users dynamically update
    certain areas of the Redfish tree at runtime. The first usage is in
    LogServices.
    
    Design document:
    https://gerrit.openbmc-project.xyz/c/openbmc/docs/+/52150
    API/examples/tutorial document:
    https://gerrit.openbmc-project.xyz/c/openbmc/docs/+/52295
    
    The GET and POST commands are complete, the remaining commands will
    be implemented soon. The integration hook is complete for LogServices.
    The filesystem is used for all backing storage.
    
    Tested: The unit tests pass. With this script providing appropriate
    external input, the Redfish Service Validator passes:
    https://gist.github.com/Krellan/f511e65166ebe5435fde3f847d28fe73
    
    Signed-off-by: Josh Lehan <krellan@google.com>
    Change-Id: Iec65d9ff421cbe77afd5f47cbcb6e4330caa0ef1

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -978,8 +978,19 @@ inline void requestRoutesSystemLogServiceCollection(App& app)
                 {{"@odata.id",
                   "/redfish/v1/Systems/system/LogServices/HostLogger"}});
 #endif
-            asyncResp->res.jsonValue["Members@odata.count"] =
-                logServiceArray.size();
+
+                // This is the ExternalStorer integration point
+                for (const auto& instance :
+                     external_storer::hookLogServices->listInstances())
+                {
+                    logServiceArray.push_back(
+                        {{"@odata.id",
+                          "/redfish/v1/Systems/system/LogServices/" +
+                              instance}});
+                }
+
+                asyncResp->res.jsonValue["Members@odata.count"] =
+                    logServiceArray.size();
 
             crow::connections::systemBus->async_method_call(
                 [asyncResp](const boost::system::error_code ec,
