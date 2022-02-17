33fb96a700210d95651d5c55583bb2d17bb8292d
Ed Tanous <edtanous@google.com>
2022-02-17 11:40:25 -0800

    Detemplateify intToHexString
    The routine of intToHexString is mostly common between all the
    various implementations, the only difference is in selecting a
    reasonable default size for the type.  This commit moves to a common
    method, so the templates don't get created for each and every
    instantiation of intToHexString, and simplifies our code.
    
    This saves a trivial amount (428 bytes) of compressed binary size.
    
    Tested: Unit tests passing, and have good coverage of this.
    
    Memory resources return the values as were there previously.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I6945a81a5e03c7a64d6a2a0629b24db941271930

--- redfish-core/ut/hex_utils_test.cpp
+++ redfish-core/ut/hex_utils_test.cpp
@@ -4,28 +4,22 @@
 
 TEST(ToHexString, uint64)
 {
-    EXPECT_EQ(intToHexString(0xFFFFFFFFFFFFFFFFULL), "FFFFFFFFFFFFFFFF");
+    EXPECT_EQ(intToHexString(0xFFFFFFFFFFFFFFFFULL, 16), "FFFFFFFFFFFFFFFF");
 
-    EXPECT_EQ(intToHexString<uint64_t>(0), "0000000000000000");
-    EXPECT_EQ(intToHexString<uint64_t>(0, 4), "0000");
-    EXPECT_EQ(intToHexString<uint64_t>(0, 8), "00000000");
-    EXPECT_EQ(intToHexString<uint64_t>(0, 12), "000000000000");
-    EXPECT_EQ(intToHexString<uint64_t>(0, 16), "0000000000000000");
+    EXPECT_EQ(intToHexString(0, 4), "0000");
+    EXPECT_EQ(intToHexString(0, 8), "00000000");
+    EXPECT_EQ(intToHexString(0, 12), "000000000000");
+    EXPECT_EQ(intToHexString(0, 16), "0000000000000000");
 
-    uint64_t deadbeef = 0xDEADBEEFBAD4F00DULL;
-    EXPECT_EQ(intToHexString<uint64_t>(deadbeef), "DEADBEEFBAD4F00D");
-    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 4), "F00D");
-    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 8), "BAD4F00D");
-    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 12), "BEEFBAD4F00D");
-    EXPECT_EQ(intToHexString<uint64_t>(deadbeef, 16), "DEADBEEFBAD4F00D");
-}
+    // uint64_t sized ints
+    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 4), "F00D");
+    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 8), "BAD4F00D");
+    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 12), "BEEFBAD4F00D");
+    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 16), "DEADBEEFBAD4F00D");
 
-TEST(ToHexString, uint16)
-{
-    uint16_t beef = 0xBEEF;
-    EXPECT_EQ(intToHexString(beef), "BEEF");
-    EXPECT_EQ(intToHexString(beef, 1), "F");
-    EXPECT_EQ(intToHexString(beef, 2), "EF");
-    EXPECT_EQ(intToHexString(beef, 3), "EEF");
-    EXPECT_EQ(intToHexString(beef, 4), "BEEF");
+    // uint16_t sized ints
+    EXPECT_EQ(intToHexString(0xBEEF, 1), "F");
+    EXPECT_EQ(intToHexString(0xBEEF, 2), "EF");
+    EXPECT_EQ(intToHexString(0xBEEF, 3), "EEF");
+    EXPECT_EQ(intToHexString(0xBEEF, 4), "BEEF");
 }
