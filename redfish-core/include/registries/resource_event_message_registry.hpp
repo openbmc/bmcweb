5871292b14666a334b1df350c0d2ae71861eebf0
Ed Tanous <edtanous@google.com>
2022-02-17 09:09:25 -0800

    Make generated warning more prominent and helpful
    A number of reviews have been submitted that ignore this warning.  While
    we now have 01caf624211197a993dbbd186149293f7053f9d8 which will catch
    these violations at CI time, that is arguably already too late, and
    developers will have wasted their time.
    
    This commit changes the warning to be multiple lines, with a caps
    header, such that it's less likely to be ignored.  It also adds
    suggestions on how to proceed.
    
    As a note, this also standardizes privilege registry to use the same
    warning as the other files, which it didn't previously.
    
    Tested: Comment changes only, no functional changes.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I3224130dbf581dc962187b2fde4dc98ae26de082

--- redfish-core/include/registries/resource_event_message_registry.hpp
+++ redfish-core/include/registries/resource_event_message_registry.hpp
@@ -1,7 +1,12 @@
-
 /****************************************************************
+ *                 READ THIS WARNING FIRST
  * This is an auto-generated header which contains definitions
  * for Redfish DMTF defined messages.
+ * DO NOT modify this registry outside of running the
+ * parse_regisries.py script.  The definitions contained within
+ * this file are owned by DMTF.  Any modifications to these files
+ * should be first pushed to the relevant registry in the DMTF
+ * github organization.
  ***************************************************************/
 #pragma once
 #include <registries.hpp>
