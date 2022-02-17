27654b66d7c61145013cd3339d991d3a3e6596fc
Ed Tanous <edtanous@google.com>
2022-02-17 11:40:25 -0800

    Detemplateify intToHexString
    The routine of intToHexStringHelper is mostly common between all the
    various implementations, the only difference is in selecting a
    reasonable default size for the type.  This commit moves to a common
    method, so the templates don't get created for each and every
    instantiation of intToHexString
    
    This saves a trivial amount (428 bytes) of compressed binary size.
    
    Tested: Unit tests passing, and have good coverage of this.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I6945a81a5e03c7a64d6a2a0629b24db941271930

--- redfish-core/include/utils/hex_utils.hpp
+++ redfish-core/include/utils/hex_utils.hpp
@@ -4,9 +4,9 @@
 #include <cstddef>
 #include <string>
 
-template <typename IntegerType>
-inline std::string intToHexString(IntegerType value,
-                                  size_t digits = sizeof(IntegerType) << 1)
+namespace details
+{
+inline std::string intToHexStringHelper(uint64_t value, size_t digits)
 {
     static constexpr std::array<char, 16> digitsArray = {
         '0', '1', '2', '3', '4', '5', '6', '7',
@@ -19,4 +19,16 @@ inline std::string intToHexString(IntegerType value,
         bitIndex -= 4;
     }
     return rc;
+}
+} // namespace details
+
+template <typename IntegerType>
+inline std::string intToHexString(IntegerType value,
+                                  size_t digits = sizeof(IntegerType) << 1)
+{
+    static_assert(sizeof(IntegerType) <= sizeof(uint64_t),
+                  "Integer size too big");
+    static_assert(std::is_unsigned<IntegerType>::value,
+                  "Integer must be unsigned");
+    return details::intToHexStringHelper(value, digits);
 }
