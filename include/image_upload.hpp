a97d984a64b83a6e61efdfff4e7e0f52b9da8e39
Ed Tanous <edtanous@google.com>
2022-03-01 11:07:20 -0800

    RFC: Asio callbacks should use std::bind_front
    Now that we have c++20, we have the addition of std::bind_front[1] which
    has some lovely properties.  In the past, when we compared std::bind to
    a lambda, it was clear that the lambda had minor performance advantages,
    and had some readbility advantages.  With std::bind_front, those
    advantages seem to have disappeared, and now std::bind_front can
    generate the same code as the lambda.
    
    This is advantageous because lambdas produce very.... very long function
    definitions, and are tougher to triage in a stack trace.
    
    As a concept, we should use std::bind, and slowly replace all our lambda
    calls over time.
    
    As an example of a non-trivial handler that was ported in this way, this
    commit moves over the old image upload handler to the new pattern.  This
    has multiple captures, including a reference capture, and
    std::bind_front handles it as expected:
    
    Tested:
    
    This code generates a binary that compresses to the exact same size
    (1087048 bytes) as previously, which shows that we're generating the
    exact same code as before, just that debugability and readability is
    improved.
    
    Binary produces an identical sha1sum on my machine.
    a7e8801bbb239b6d126e0c0fbe8a353b6d7c7017
    
    1. https://en.cppreference.com/w/cpp/utility/functional/bind_front
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I69fa1e3717dc064b946db6512a778a5b918bf16c

--- include/image_upload.hpp
+++ include/image_upload.hpp
@@ -9,7 +9,9 @@
 
 #include <cstdio>
 #include <fstream>
+#include <functional>
 #include <memory>
+#include <optional>
 
 namespace crow
 {
@@ -18,12 +20,67 @@ namespace image_upload
 
 static std::unique_ptr<sdbusplus::bus::match::match> fwUpdateMatcher;
 
+inline void timeoutHandler(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                           const boost::system::error_code& ec)
+{
+    fwUpdateMatcher = nullptr;
+    if (ec == boost::asio::error::operation_aborted)
+    {
+        // expected, we were canceled before the timer completed.
+        return;
+    }
+    BMCWEB_LOG_ERROR << "Timed out waiting for Version interface";
+
+    if (ec)
+    {
+        BMCWEB_LOG_ERROR << "Async_wait failed " << ec;
+        return;
+    }
+
+    asyncResp->res.result(boost::beast::http::status::bad_request);
+    asyncResp->res.jsonValue = {
+        {"data",
+         {{"description", "Version already exists or failed to be extracted"}}},
+        {"message", "400 Bad Request"},
+        {"status", "error"}};
+}
+
+inline void
+    onSignalCallback(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                     boost::asio::steady_timer& timeout,
+                     sdbusplus::message::message& m)
+{
+    BMCWEB_LOG_DEBUG << "Match fired";
+
+    sdbusplus::message::object_path path;
+    dbus::utility::DBusInteracesMap interfaces;
+    m.read(path, interfaces);
+
+    if (std::find_if(interfaces.begin(), interfaces.end(), [](const auto& i) {
+            return i.first == "xyz.openbmc_project.Software.Version";
+        }) == interfaces.end())
+    {
+        return;
+    }
+    timeout.cancel();
+    std::string leaf = path.filename();
+    if (leaf.empty())
+    {
+        leaf = path.str;
+    }
+
+    asyncResp->res.jsonValue = {
+        {"data", leaf}, {"message", "200 OK"}, {"status", "ok"}};
+    BMCWEB_LOG_DEBUG << "ending response";
+    fwUpdateMatcher = nullptr;
+}
+
 inline void
     uploadImageHandler(const crow::Request& req,
                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
 {
     // Only allow one FW update at a time
-    if (fwUpdateMatcher != nullptr)
+    if (fwUpdateMatcher == nullptr)
     {
         asyncResp->res.addHeader("Retry-After", "30");
         asyncResp->res.result(boost::beast::http::status::service_unavailable);
@@ -35,62 +92,11 @@ inline void
 
     timeout.expires_after(std::chrono::seconds(15));
 
-    auto timeoutHandler = [asyncResp](const boost::system::error_code& ec) {
-        fwUpdateMatcher = nullptr;
-        if (ec == boost::asio::error::operation_aborted)
-        {
-            // expected, we were canceled before the timer completed.
-            return;
-        }
-        BMCWEB_LOG_ERROR << "Timed out waiting for Version interface";
-
-        if (ec)
-        {
-            BMCWEB_LOG_ERROR << "Async_wait failed " << ec;
-            return;
-        }
-
-        asyncResp->res.result(boost::beast::http::status::bad_request);
-        asyncResp->res.jsonValue = {
-            {"data",
-             {{"description",
-               "Version already exists or failed to be extracted"}}},
-            {"message", "400 Bad Request"},
-            {"status", "error"}};
-    };
-
-    std::function<void(sdbusplus::message::message&)> callback =
-        [asyncResp](sdbusplus::message::message& m) {
-            BMCWEB_LOG_DEBUG << "Match fired";
-
-            sdbusplus::message::object_path path;
-            dbus::utility::DBusInteracesMap interfaces;
-            m.read(path, interfaces);
-
-            if (std::find_if(interfaces.begin(), interfaces.end(),
-                             [](const auto& i) {
-                                 return i.first ==
-                                        "xyz.openbmc_project.Software.Version";
-                             }) != interfaces.end())
-            {
-                timeout.cancel();
-                std::string leaf = path.filename();
-                if (leaf.empty())
-                {
-                    leaf = path.str;
-                }
-
-                asyncResp->res.jsonValue = {
-                    {"data", leaf}, {"message", "200 OK"}, {"status", "ok"}};
-                BMCWEB_LOG_DEBUG << "ending response";
-                fwUpdateMatcher = nullptr;
-            }
-        };
     fwUpdateMatcher = std::make_unique<sdbusplus::bus::match::match>(
         *crow::connections::systemBus,
         "interface='org.freedesktop.DBus.ObjectManager',type='signal',"
         "member='InterfacesAdded',path='/xyz/openbmc_project/software'",
-        callback);
+        std::bind_front(onSignalCallback, asyncResp, std::ref(timeout)));
 
     std::string filepath(
         "/tmp/images/" +
@@ -100,7 +106,7 @@ inline void
                                     std::ofstream::trunc);
     out << req.body;
     out.close();
-    timeout.async_wait(timeoutHandler);
+    timeout.async_wait(std::bind_front(timeoutHandler, asyncResp));
 }
 
 inline void requestRoutes(App& app)
