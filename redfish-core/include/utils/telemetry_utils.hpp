ddebe4704752ad76bbd89b7af6916c4070fe91a0
Szymon Dompke <szymon.dompke@intel.com>
2022-03-03 14:42:52 +0100

    Add common url segments parser
    This change is adding helper template function, which can be used both
    to validate and read segments from segments_view returned by boost_url
    parser. Number of segments is also validated - in case when argument
    count differs from them, false will be returned. In case when we want to
    validate only existence of a segment, special argument can be passed in
    its place: 'anySegment'.
    
    Reasoning why url_view  was chosen instead of strings:
    - This way code generation is kept minimal.
    - There are multiple parse functions in boost_url with different rules,
      but all of them return url_view. This solution should accommodate
      every use case.
    
    Testing done:
    - Unit tests are added, passing.
    - Refactored part of telemetry to use this new approach, no regression
      spotted during simple POST/GET tests.
    
    Change-Id: I677a34e1ee570d33f2322a80dc1629f88273e0d5
    Signed-off-by: Szymon Dompke <szymon.dompke@intel.com>

--- redfish-core/include/utils/telemetry_utils.hpp
+++ redfish-core/include/utils/telemetry_utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "dbus_utility.hpp"
+#include "utility.hpp"
 
 namespace redfish
 {
@@ -26,6 +27,59 @@ inline std::string getDbusTriggerPath(const std::string& id)
     sdbusplus::message::object_path triggersPath(
         "/xyz/openbmc_project/Telemetry/Triggers/TelemetryService");
     return {triggersPath / id};
+}
+
+struct ChassisSensor
+{
+    std::string uri;
+    size_t index;
+};
+
+inline std::optional<ChassisSensor> getChassisSensorNode(
+    const std::vector<std::string>& uris,
+    boost::container::flat_set<std::pair<std::string, std::string>>& matched)
+{
+    size_t uriIdx = 0;
+    for (const std::string& uri : uris)
+    {
+        boost::urls::result<boost::urls::url_view> parsed =
+            boost::urls::parse_relative_ref(uri);
+
+        if (!parsed)
+        {
+            BMCWEB_LOG_ERROR << "Failed to get chassis and sensor Node "
+                                "from "
+                             << uri;
+            return std::make_optional<ChassisSensor>({uri, uriIdx});
+        }
+
+        std::string chassis;
+        std::string node;
+
+        if (crow::utility::readUrlSegments(*parsed, "redfish", "v1", "Chassis",
+                                           std::ref(chassis), std::ref(node)))
+        {
+            matched.emplace(std::move(chassis), std::move(node));
+            uriIdx++;
+            continue;
+        }
+
+        if (crow::utility::readUrlSegments(*parsed, "redfish", "v1", "Chassis",
+                                           std::ref(chassis), "Sensors",
+                                           crow::utility::anySegment,
+                                           crow::utility::anySegment))
+        {
+            matched.emplace(std::move(chassis), "Sensors");
+            uriIdx++;
+            continue;
+        }
+
+        BMCWEB_LOG_ERROR << "Failed to get chassis and sensor Node "
+                            "from "
+                         << uri;
+        return std::make_optional<ChassisSensor>({uri, uriIdx});
+    }
+    return std::nullopt;
 }
 
 } // namespace telemetry
