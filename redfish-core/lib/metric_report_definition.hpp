2f0c825109aa0df6efa21d458d450a92e3d7cfdc
Szymon Dompke <szymon.dompke@intel.com>
2022-03-04 14:44:31 +0100

    Add Links/Triggers to MetricReportDefinition
    This change is adding Triggers property to Links when GET is called on
    MetricReportDefinition. It contains array of @odata.id pointing to
    Trigger resource if it is also linking to given MRD.
    
    Testing done:
    - Links/Trigger property is returned by GET request on
      /redfish/v1/TelemetryService/MetricReportDefinitions/<str>/
    
    Signed-off-by: Szymon Dompke <szymon.dompke@intel.com>
    Change-Id: Ifb17eca19dc6adc5d5725765f3b5cec14c80489b

--- redfish-core/lib/metric_report_definition.hpp
+++ redfish-core/lib/metric_report_definition.hpp
@@ -48,6 +48,31 @@ std::string toDbusReportAction(std::string_view action)
     return "";
 }
 
+inline std::optional<nlohmann::json>
+    getLinkedTriggers(const std::vector<std::string>& triggerIds)
+{
+    nlohmann::json triggers = nlohmann::json::array();
+
+    for (const std::string& id : triggerIds)
+    {
+        sdbusplus::message::object_path path(id);
+        if (path.parent_path() != "TelemetryService")
+        {
+            BMCWEB_LOG_ERROR << "Property TriggerIds contains invalid value: "
+                             << id;
+            return std::nullopt;
+        }
+        triggers.push_back({
+            {"@odata.id",
+             crow::utility::urlFromPieces("redfish", "v1", "TelemetryService",
+                                          "Triggers", path.filename())
+                 .string()},
+        });
+    }
+
+    return std::make_optional(triggers);
+}
+
 inline void fillReportDefinition(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp, const std::string& id,
     const std::vector<std::pair<std::string, dbus::utility::DbusVariantType>>&
@@ -60,6 +85,7 @@ inline void fillReportDefinition(
     const std::string* name = nullptr;
     const uint64_t* appendLimit = nullptr;
     const uint64_t* interval = nullptr;
+    const std::vector<std::string>* triggerIds = nullptr;
 
     for (const auto& [key, var] : properties)
     {
@@ -91,11 +117,16 @@ inline void fillReportDefinition(
         {
             name = std::get_if<std::string>(&var);
         }
+        else if (key == "TriggerIds")
+        {
+            triggerIds = std::get_if<std::vector<std::string>>(&var);
+        }
     }
 
     if (reportActions == nullptr || readingParams == nullptr ||
         reportingType == nullptr || reportUpdates == nullptr ||
-        name == nullptr || appendLimit == nullptr || interval == nullptr)
+        name == nullptr || appendLimit == nullptr || interval == nullptr ||
+        triggerIds == nullptr)
     {
         BMCWEB_LOG_ERROR << "Property type mismatch or property is missing";
         messages::internalError(asyncResp->res);
@@ -115,6 +146,14 @@ inline void fillReportDefinition(
         }
 
         redfishReportActions.emplace_back(std::move(redfishAction));
+    }
+
+    std::optional<nlohmann::json> linkedTriggers =
+        getLinkedTriggers(*triggerIds);
+    if (!linkedTriggers)
+    {
+        messages::internalError(asyncResp->res);
+        return;
     }
 
     nlohmann::json metrics = nlohmann::json::array();
@@ -153,6 +192,7 @@ inline void fillReportDefinition(
     asyncResp->res.jsonValue["ReportActions"] = std::move(redfishReportActions);
     asyncResp->res.jsonValue["Schedule"]["RecurrenceInterval"] =
         time_utils::toDurationString(std::chrono::milliseconds(*interval));
+    asyncResp->res.jsonValue["Links"]["Triggers"] = *linkedTriggers;
 }
 
 struct AddReportArgs
