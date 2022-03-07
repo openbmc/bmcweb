e999537b77e32bc31d4bca925b88e6df98b8f055
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-03-07 15:23:05 +0800

    Use configured value for service status
    In service-config-manager, updating config of services takes about 15s
    to be applied. But currently the service status is read directly from
    systemd DBus, so Redfish won't reflect the changes immediately. The 15s
    wait is designed to accommodate multiple config changes in a single
    systemd reload to avoid race conditions and data loss according to the
    author.
    
    This patch fixes such inconsistency by reading the "configured" value
    from service-config-manager instead of the "real" value in systemd as
    the author of service-config-manager suggests.
    
    Tested:
    Changing port or enabled state will be reflected in Redfish properties
    immediately, and the new configuration will take effect in ~15s.
    
    Change-Id: I1246977063d072eb3cc82a527b6bf2c7b74802bd
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- redfish-core/include/utils/service_utils.hpp
+++ redfish-core/include/utils/service_utils.hpp
@@ -4,6 +4,8 @@
 #include "error_messages.hpp"
 #include "openbmc_dbus_rest.hpp"
 
+#include <nlohmann/json.hpp>
+
 namespace redfish
 {
 namespace service_util
@@ -17,6 +19,141 @@ static bool matchService(const sdbusplus::message::object_path& objPath,
     std::string fullUnitName = objPath.filename();
     size_t pos = fullUnitName.rfind('@');
     return fullUnitName.substr(0, pos) == serviceName;
+}
+
+template <typename T>
+static inline bool
+    getPropertyFromMap(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                       const nlohmann::json::json_pointer& valueJsonPtr,
+                       const dbus::utility::DBusPropertiesMap& properties,
+                       const std::string& propertyName)
+{
+    for (const auto& [key, val] : properties)
+    {
+        if (key != propertyName)
+        {
+            continue;
+        }
+
+        const auto* value = std::get_if<T>(&val);
+        if (value == nullptr)
+        {
+            messages::internalError(asyncResp->res);
+            return true;
+        }
+
+        // For bool (Enabled), treat false as a invalid value
+        if constexpr (std::is_same_v<bool, T>)
+        {
+            if (*value)
+            {
+                asyncResp->res.jsonValue[valueJsonPtr] = true;
+                return true;
+            }
+        }
+
+        // For uint16_t (Port), 0 is a invalid value
+        if constexpr (std::is_same_v<uint16_t, T>)
+        {
+            if (*value != 0)
+            {
+                asyncResp->res.jsonValue[valueJsonPtr] = *value;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void getEnabled(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                const std::string& serviceName,
+                const nlohmann::json::json_pointer& valueJsonPtr)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, serviceName,
+         valueJsonPtr](const boost::system::error_code ec,
+                       const dbus::utility::ManagedObjectType& objects) {
+        if (ec)
+        {
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        for (const auto& [path, interfaces] : objects)
+        {
+            if (!matchService(path, serviceName))
+            {
+                continue;
+            }
+
+            for (const auto& [interface, properties] : interfaces)
+            {
+                if (interface !=
+                    "xyz.openbmc_project.Control.Service.Attributes")
+                {
+                    continue;
+                }
+
+                // If one of the service instance is running, show it as
+                // Enabled in redfish.
+                if (getPropertyFromMap<bool>(asyncResp, valueJsonPtr,
+                                             properties, "Running"))
+                {
+                    return;
+                }
+            }
+        }
+        // Not populating the property when service is not found
+        },
+        "xyz.openbmc_project.Control.Service.Manager",
+        "/xyz/openbmc_project/control/service",
+        "org.freedesktop.DBus.ObjectManager", "GetManagedObjects");
+}
+
+void getPortNumber(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& serviceName,
+                   const nlohmann::json::json_pointer& valueJsonPtr)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, serviceName,
+         valueJsonPtr](const boost::system::error_code ec,
+                       const dbus::utility::ManagedObjectType& objects) {
+        if (ec)
+        {
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        for (const auto& [path, interfaces] : objects)
+        {
+            if (!matchService(path, serviceName))
+            {
+                continue;
+            }
+
+            for (const auto& [interface, properties] : interfaces)
+            {
+                if (interface !=
+                    "xyz.openbmc_project.Control.Service.SocketAttributes")
+                {
+                    continue;
+                }
+
+                // For service with multiple instances, return the port of first
+                // instance found as redfish only support one port value, they
+                // should be same
+                if (getPropertyFromMap<uint16_t>(asyncResp, valueJsonPtr,
+                                                 properties, "Port"))
+                {
+                    return;
+                }
+            }
+        }
+        // Not populating the property when service is not found
+        },
+        "xyz.openbmc_project.Control.Service.Manager",
+        "/xyz/openbmc_project/control/service",
+        "org.freedesktop.DBus.ObjectManager", "GetManagedObjects");
 }
 
 template <typename T>
