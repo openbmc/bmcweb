9474afd3a7844c0016ab35867e99d173ab099a24
Szymon Dompke <szymon.dompke@intel.com>
2022-03-07 14:28:06 +0100

    Add Links/Triggers to MetricReportDefinition
    This change is adding Triggers property to Links when GET is called on
    MetricReportDefinition. It contains array of @odata.id pointing to
    Trigger resource if it is also linking to given MRD.
    
    Testing done:
    - Links/Trigger property is returned by GET request on
      /redfish/v1/TelemetryService/MetricReportDefinitions/<str>/
    
    Signed-off-by: Szymon Dompke <szymon.dompke@intel.com>
    Change-Id: I5accf4b50324437b0b185003200078ad2c7020b0

--- redfish-core/lib/metric_report_definition.hpp
+++ redfish-core/lib/metric_report_definition.hpp
@@ -52,6 +52,31 @@ std::string toDbusReportAction(std::string_view action)
     return "";
 }
 
+inline std::optional<nlohmann::json>
+    getLinkedTriggers(const std::vector<std::string>& triggerIds)
+{
+    nlohmann::json triggers = nlohmann::json::array();
+
+    for (const std::string& id : triggerIds)
+    {
+        sdbusplus::message::object_path path(id);
+        if (path.parent_path() != "TelemetryService")
+        {
+            BMCWEB_LOG_ERROR << "Property TriggerIds contains invalid value: "
+                             << id;
+            return std::nullopt;
+        }
+        triggers.push_back({
+            {"@odata.id",
+             crow::utility::urlFromPieces("redfish", "v1", "TelemetryService",
+                                          "Triggers", path.filename())
+                 .string()},
+        });
+    }
+
+    return std::make_optional(triggers);
+}
+
 inline void
     fillReportDefinition(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                          const std::string& id,
@@ -65,6 +90,7 @@ inline void
     const uint64_t* appendLimit = nullptr;
     const uint64_t* interval = nullptr;
     const bool* enabled = nullptr;
+    const std::vector<std::string>* triggerIds = nullptr;
 
     for (const auto& [key, var] : properties)
     {
@@ -100,6 +126,10 @@ inline void
         {
             enabled = std::get_if<bool>(&var);
         }
+        else if (key == "TriggerIds")
+        {
+            triggerIds = std::get_if<std::vector<std::string>>(&var);
+        }
     }
 
     std::vector<std::string> redfishReportActions;
@@ -117,6 +147,17 @@ inline void
             }
 
             redfishReportActions.emplace_back(std::move(redfishAction));
+        }
+    }
+
+    std::optional<nlohmann::json> linkedTriggers;
+    if (triggerIds != nullptr)
+    {
+        linkedTriggers = getLinkedTriggers(*triggerIds);
+        if (!linkedTriggers)
+        {
+            messages::internalError(asyncResp->res);
+            return;
         }
     }
 
@@ -195,6 +236,11 @@ inline void
                       std::chrono::milliseconds(collectionDuration))},
                  {"CollectionTimeScope", collectionTimeScope}});
         }
+    }
+
+    if (triggerIds != nullptr)
+    {
+        asyncResp->res.jsonValue["Links"]["Triggers"] = *linkedTriggers;
     }
 }
 
