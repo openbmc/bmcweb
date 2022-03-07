563462a06c729325cd817341df90f55d3808b9c6
Ed Tanous <edtanous@google.com>
2022-03-07 10:22:35 -0800

    Create new overload of PropertyValueFormatError
    In a majority of cases, we use the PropertyValueFormatError overloads
    incorrectly, especially so when calling dump.  These issues can be
    separated into two classes.
    
    First, using PropertyValueFormatError to enforce things that rely on
    string values, like disallowing empty string.  Ideally, we should use
    more specific error messages in those cases, most of the time
    PropertyValueNotInList.
    
    Second, in cases where it's used properly, there are many cases where it
    calls dump with the wrong set of parameters.  Because we're generating
    an inline response, we should not be pretty printing the value, and we
    should be setting nlohmann::json::error_handler_t::replace in all cases.
    
    To solve the second problem, this commit adds a new overload of
    propertyValueFormatError that takes the nlohmann json object directly,
    and calls dump with the appropriate parameters.  In the ideal, we'd
    eventually remote the string_view version of this overload, but that
    will need to be a future set of patches, and a transition over time,
    considering there are close to 30 calls that are incorrect.
    
    Tested:
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I4d2de032ee0d68e564b9a2509a350d12914f6a65

--- redfish-core/src/error_messages.cpp
+++ redfish-core/src/error_messages.cpp
@@ -438,15 +438,17 @@ void createFailedMissingReqProperties(crow::Response& res,
  * See header file for more information
  * @endinternal
  */
-nlohmann::json propertyValueFormatError(std::string_view arg1,
+nlohmann::json propertyValueFormatError(const nlohmann::json& arg1,
                                         std::string_view arg2)
 {
+    std::string arg1Out =
+        arg1.dump(-1, ' ', true, nlohmann::json::error_handler_t::replace);
     return getLog(
         redfish::message_registries::base::Index::propertyValueFormatError,
-        std::to_array({arg1, arg2}));
+        std::to_array<std::string_view>({arg1Out, arg2}));
 }
 
-void propertyValueFormatError(crow::Response& res, std::string_view arg1,
+void propertyValueFormatError(crow::Response& res, const nlohmann::json& arg1,
                               std::string_view arg2)
 {
     res.result(boost::beast::http::status::bad_request);
