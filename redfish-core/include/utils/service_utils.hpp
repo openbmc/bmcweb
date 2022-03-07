8a9eb6a8680211ca76796c7800ad1a3d39d17a9a
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-03-07 15:23:05 +0800

    Use configured value for service status
    In service-config-manager, updating config of services takes about 15s
    to be applied. But currently the service status is read directly from
    systemd DBus, so Redfish won't reflect the changes immediately. The 15s
    wait is designed to accommodate multiple config changes in a single
    systemd reload to avoid race conditions and data loss according to the
    author.
    
    This patch fixes such inconsistency by reading the "configured" value
    from service-config-manager instead of the "real" value in systemd as
    the author of service-config-manager suggests.
    
    Tested:
    Changing port or enabled state will be reflected in Redfish properties
    immediately, and the new configuration will take effect in ~15s.
    
    Change-Id: I1246977063d072eb3cc82a527b6bf2c7b74802bd
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- redfish-core/include/utils/service_utils.hpp
+++ redfish-core/include/utils/service_utils.hpp
@@ -4,6 +4,9 @@
 #include "error_messages.hpp"
 #include "openbmc_dbus_rest.hpp"
 
+#include <boost/container/flat_map.hpp>
+#include <nlohmann/json.hpp>
+
 namespace redfish
 {
 namespace service_util
@@ -17,6 +20,128 @@ static bool matchService(const sdbusplus::message::object_path& objPath,
     std::string fullUnitName = objPath.filename();
     size_t pos = fullUnitName.find("_40");
     return fullUnitName.substr(0, pos) == serviceName;
+}
+
+void getEnabled(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                const std::string& serviceName,
+                const nlohmann::json::json_pointer& valueJsonPtr)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, serviceName,
+         valueJsonPtr](const boost::system::error_code ec,
+                       const crow::openbmc_mapper::GetSubTreeType& subtree) {
+            if (ec)
+            {
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            for (const auto& entry : subtree)
+            {
+                if (matchService(entry.first, serviceName))
+                {
+                    crow::connections::systemBus->async_method_call(
+                        [asyncResp, serviceName, valueJsonPtr](
+                            const boost::system::error_code ec2,
+                            const boost::container::flat_map<
+                                std::string, dbus::utility::DbusVariantType>&
+                                properties) {
+                            if (ec2)
+                            {
+                                messages::internalError(asyncResp->res);
+                                return;
+                            }
+
+                            bool enabled = false;
+                            bool running = false;
+                            for (const auto& [key, val] : properties)
+                            {
+                                if (key == "Enabled")
+                                {
+                                    const auto* value = std::get_if<bool>(&val);
+                                    if (value == nullptr)
+                                    {
+                                        messages::internalError(asyncResp->res);
+                                        return;
+                                    }
+                                    enabled = *value;
+                                }
+                                else if (key == "Running")
+                                {
+                                    const auto* value = std::get_if<bool>(&val);
+                                    if (value == nullptr)
+                                    {
+                                        messages::internalError(asyncResp->res);
+                                        return;
+                                    }
+                                    running = *value;
+                                }
+                            }
+                            asyncResp->res.jsonValue[valueJsonPtr] =
+                                (enabled || running);
+                        },
+                        entry.second.front().first, entry.first,
+                        "org.freedesktop.DBus.Properties", "GetAll",
+                        "xyz.openbmc_project.Control.Service.Attributes");
+                    return;
+                }
+            }
+            asyncResp->res.jsonValue[valueJsonPtr] =
+                nlohmann::json::value_t::null;
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTree",
+        "/xyz/openbmc_project/control/service", 0,
+        std::array<const char*, 1>{
+            "xyz.openbmc_project.Control.Service.Attributes"});
+}
+
+void getPortNumber(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& serviceName,
+                   const nlohmann::json::json_pointer& valueJsonPtr)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, serviceName,
+         valueJsonPtr](const boost::system::error_code ec,
+                       const crow::openbmc_mapper::GetSubTreeType& subtree) {
+            if (ec)
+            {
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            for (const auto& entry : subtree)
+            {
+                if (matchService(entry.first, serviceName))
+                {
+                    sdbusplus::asio::getProperty<uint16_t>(
+                        *crow::connections::systemBus,
+                        entry.second.front().first, entry.first,
+                        "xyz.openbmc_project.Control.Service.SocketAttributes",
+                        "Port",
+                        [asyncResp, serviceName,
+                         valueJsonPtr](const boost::system::error_code ec2,
+                                       const uint16_t portNumber) {
+                            if (ec2)
+                            {
+                                messages::internalError(asyncResp->res);
+                                return;
+                            }
+                            asyncResp->res.jsonValue[valueJsonPtr] = portNumber;
+                        });
+                    return;
+                }
+            }
+            asyncResp->res.jsonValue[valueJsonPtr] =
+                nlohmann::json::value_t::null;
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTree",
+        "/xyz/openbmc_project/control/service", 0,
+        std::array<const char*, 1>{
+            "xyz.openbmc_project.Control.Service.SocketAttributes"});
 }
 
 void setEnabled(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
