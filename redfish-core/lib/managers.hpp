6c746a7e8b222bd20ad4e0232c3aa9f8669bdbb0
Willy Tu <wltu@google.com>
2022-03-10 13:16:38 -0800

    manager: Use common getLocationCode for Manager
    Remove getLocation and use the common
    location_util::getLocationCode for all resources to reduce duplicate
    logic.
    
    Set the LocationType to `Embedded` by default. This is true for embedded
    BMC and DC-SCM. The BMC is embedded in the SC-SCM module and is
    represented under chassis. The manager/chassis relationship is
    represented with ManagerForServers and ManagedBy.
    
    ```
     wget -qO- http://localhost:80/redfish/v1/Managers/bmc
    {
      "@odata.id": "/redfish/v1/Managers/bmc",
      "@odata.type": "#Manager.v1_14_0.Manager",
      "Id": "bmc
      "LastResetTime": "9999-12-31T23:59:59+00:00",
      "Links": {
        "ActiveSoftwareImage": {
          "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/0a5eb302"
        },
        "ManagerForChassis": [
          {
            "@odata.id": "/redfish/v1/Chassis/DC_SCM"
          }
        ],
        "ManagerForChassis@odata.count": 1,
        "ManagerForServers": [
          {
            "@odata.id": "/redfish/v1/Systems/system"
          }
        ],
    ...
    ```
    
    ```
    wget -qO- http://localhost:80/redfish/v1/Chassis/DC_SCM
    {
      "@odata.id": "/redfish/v1/Chassis/DC_SCM",
      "@odata.type": "#Chassis.v1_16_0.Chassis",
      "ChassisType": "RackMount",
      "Id": "AgoraV2",
      "Links": {
        "ComputerSystems": [
          {
            "@odata.id": "/redfish/v1/Systems/system"
          }
        ],
        "ManagedBy": [
          {
            "@odata.id": "/redfish/v1/Managers/bmc"
          }
        ],
        "Storage": [],
        "Storage@odata.count": 0
      },
    
    ```
    
    Tested:
    Redfish Validator passed for Manager
    ```
    {
      "@odata.id": "/redfish/v1/Managers/bmc",
      "@odata.type": "#Manager.v1_11_0.Manager",
      "Actions": {
        "#Manager.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Managers/bmc/ResetActionInfo",
          "target": "/redfish/v1/Managers/bmc/Actions/Manager.Reset"
        },
        "#Manager.ResetToDefaults": {
          "ResetType@Redfish.AllowableValues": [
            "ResetAll"
          ],
          "target": "/redfish/v1/Managers/bmc/Actions/Manager.ResetToDefaults"
        }
      }
      ...
        "Location": {
        "PartLocation": {
          "LocationType": "Embedded",
          "ServiceLabel" : "PEX"
        }
      },
      ...
    }
    ```
    
    Change-Id: I8bde4e37009e1279f7678482035aaee4d803fdcc
    Signed-off-by: Willy Tu <wltu@google.com>

--- redfish-core/lib/managers.hpp
+++ redfish-core/lib/managers.hpp
@@ -30,6 +30,7 @@
 #include <boost/system/error_code.hpp>
 #include <sdbusplus/asio/property.hpp>
 #include <sdbusplus/unpack_properties.hpp>
+#include <utils/location_utils.hpp>
 
 #include <algorithm>
 #include <array>
@@ -1697,37 +1698,6 @@ struct SetPIDValues : std::enable_shared_from_this<SetPIDValues>
     size_t objectCount = 0;
 };
 
-/**
- * @brief Retrieves BMC manager location data over DBus
- *
- * @param[in] aResp Shared pointer for completing asynchronous calls
- * @param[in] connectionName - service name
- * @param[in] path - object path
- * @return none
- */
-inline void getLocation(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
-                        const std::string& connectionName,
-                        const std::string& path)
-{
-    BMCWEB_LOG_DEBUG << "Get BMC manager Location data.";
-
-    sdbusplus::asio::getProperty<std::string>(
-        *crow::connections::systemBus, connectionName, path,
-        "xyz.openbmc_project.Inventory.Decorator.LocationCode", "LocationCode",
-        [aResp](const boost::system::error_code& ec,
-                const std::string& property) {
-        if (ec)
-        {
-            BMCWEB_LOG_DEBUG << "DBUS response error for "
-                                "Location";
-            messages::internalError(aResp->res);
-            return;
-        }
-
-        aResp->res.jsonValue["Location"]["PartLocation"]["ServiceLabel"] =
-            property;
-        });
-}
 // avoid name collision systems.hpp
 inline void
     managerGetLastResetTime(const std::shared_ptr<bmcweb::AsyncResp>& aResp)
@@ -2041,6 +2011,8 @@ inline void requestRoutesManager(App& app)
         });
 
         static bool started = false;
+        asyncResp->res.jsonValue["Location"]["PartLocation"]["LocationType"] =
+            "Embedded";
 
         if (!started)
         {
@@ -2169,7 +2141,9 @@ inline void requestRoutesManager(App& app)
                 else if (interfaceName ==
                          "xyz.openbmc_project.Inventory.Decorator.LocationCode")
                 {
-                    getLocation(asyncResp, connectionName, path);
+                    location_util::getLocationCode(asyncResp, connectionName,
+                                                   path,
+                                                   "/Location"_json_pointer);
                 }
             }
             });
