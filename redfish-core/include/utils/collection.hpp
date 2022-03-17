c500e389129f56a7efaef2728d954f99bd386565
Naveen Moses <naveen.mosess@hcl.com>
2022-03-17 11:55:26 +0530

    Add initial multi host system support for redfish
    The redfish core library api has only support for bmc system with
    single host/computer system. The platforms with multiple hosts cannot
    be discovered and computer system specific redfish queries is not
    supported with the current implementation.
    
    This change adds initial support to discover and populate multihost
    systems as well as single host systems and support some system info
    queries. The host systems discovery is based on number of host power
     state instances.(xyz.openbmc_project.State.Host).
    
    The redfish url for single host system is represented by the name
     "system".
    
    i.e : /redfish/v1/Systems/system
    
    The multi host system uses the same name with host number at the end.
    
    /redfish/v1/Systems/system1
    /redfish/v1/Systems/system2
    .
    .
    /redfish/v1/Systems/systemN
    
    The change expects no impact on current single host implementation.
    It only extends the support for multihost bmc systems.
    
    Testing:
    This change is tested on Tiogapass and yosemite2 platforms.
    
    Signed-off-by: Naveen Moses <naveen.mosess@hcl.com>
    Change-Id: I95858ec23bac61c718d5feb9733724311ffa38a4

--- redfish-core/include/utils/collection.hpp
+++ redfish-core/include/utils/collection.hpp
@@ -3,6 +3,7 @@
 #include <human_sort.hpp>
 
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace redfish
@@ -79,6 +80,40 @@ inline void
         "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths", subtree, 0,
         interfaces);
 }
+inline std::string
+    getComputerSystemIndexString(const std::string_view& systemNameStr)
+{
+    static constexpr std::string_view systemToken = "system";
+    static constexpr std::string_view zero = "0";
+    auto location = systemNameStr.rfind(systemToken);
+    if (location == std::string::npos)
+    {
+        BMCWEB_LOG_DEBUG << "invalid host number";
+        return {};
+    }
 
+    auto index = systemNameStr.substr(location + systemToken.length());
+    if (index.empty())
+    {
+        return std::string(zero);
+    }
+    // verify if the host number string has a valid number.
+    // return empty string in case of error
+    try
+    {
+        auto hostNumber = std::stoi(std::string(index));
+        if (std::in_range<size_t>(hostNumber))
+        {
+            return std::string(index);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        BMCWEB_LOG_DEBUG << "invalid host number" << e.what();
+        return {};
+    }
+    BMCWEB_LOG_DEBUG << "invalid host number";
+    return {};
+}
 } // namespace collection_util
 } // namespace redfish
