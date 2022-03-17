c895717de02d19e698b4c507cc42f05c9d2359b3
Naveen Moses <naveen.mosess@hcl.com>
2022-03-17 11:55:26 +0530

    Add initial multi host system support for redfish
    The redfish core library api has only support for bmc system with
    single host/computer system. The platforms with multiple hosts cannot
    be discovered and computer system specific redfish queries is not
    supported with the current implementation.
    
    This change adds initial support to discover and populate multihost
    systems as well as single host systems and support some system info
    queries. The host systems discovery is based on number of host power
     state instances.(xyz.openbmc_project.State.Host).
    
    The redfish url for single host system is represented by the name
     "system".
    
    i.e : /redfish/v1/Systems/system
    
    The multi host system uses the same name with host number at the end.
    
    /redfish/v1/Systems/system1
    /redfish/v1/Systems/system2
    .
    .
    /redfish/v1/Systems/systemN
    
    The change expects no impact on current single host implementation.
    It only extends the support for multihost bmc systems.
    
    Testing:
    This change is tested on Tiogapass and yosemite2 platforms.
    service validater is executed with no new errors introduced
    
    system collection output
    
     $ curl -k -H "X-Auth-Token: $token" https://${bmc}/redfish/v1/Systems
    {
      "@odata.id": "/redfish/v1/Systems",
      "@odata.type": "#ComputerSystemCollection.ComputerSystemCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system2"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system3"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system4"
        }
      ],
      "Members@odata.count": 5,
      "Name": "Computer System Collection"
    
     valid system output
    
     $ curl -k -H "X-Auth-Token: $token" https://${bmc}/redfish/v1/Systems/system1
    {
      "@odata.id": "/redfish/v1/Systems/system1",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      "Actions": {
        "#ComputerSystem.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Systems/system1/ResetActionInfo",
          "target": "/redfish/v1/Systems/system1/Actions/ComputerSystem.Reset"
        }
      },
      "Bios": {
        "@odata.id": "/redfish/v1/Systems/system1/Bios"
      },
      "Boot": {
        "AutomaticRetryAttempts": 3,
        "AutomaticRetryConfig": "RetryAttempts",
        "AutomaticRetryConfig@Redfish.AllowableValues": [
          "Disabled",
          "RetryAttempts"
        ],
        "BootSourceOverrideEnabled": "Disabled",
        "BootSourceOverrideMode": "UEFI",
        "BootSourceOverrideMode@Redfish.AllowableValues": [
          "Legacy",
          "UEFI"
        ], . . .
    
     Invalid system output
    
    $ curl -k -H "X-Auth-Token: $token" https://${bmc}/redfish/v1/Systems/systm5
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type ComputerSystem named 'systm5' was not found.",
            "MessageArgs": [
              "ComputerSystem",
              "systm5"
            ],
            "MessageId": "Base.1.13.0.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.13.0.ResourceNotFound",
        "message": "The requested resource of type ComputerSystem named 'systm5' was not found."
      }
    
    Signed-off-by: Naveen Moses <naveen.mosess@hcl.com>
    Change-Id: I95858ec23bac61c718d5feb9733724311ffa38a4

--- redfish-core/lib/pcie.hpp
+++ redfish-core/lib/pcie.hpp
@@ -38,13 +38,14 @@ static constexpr char const* pcieDeviceInterface =
 
 static inline void
     getPCIeDeviceList(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-                      const std::string& name)
+                      const std::string& name, const std::string& systemName)
 {
     dbus::utility::getSubTreePaths(
         pciePath, 1, {},
-        [asyncResp, name](const boost::system::error_code& ec,
-                          const dbus::utility::MapperGetSubTreePathsResponse&
-                              pcieDevicePaths) {
+        [asyncResp, name,
+         systemName](const boost::system::error_code& ec,
+                     const dbus::utility::MapperGetSubTreePathsResponse&
+                         pcieDevicePaths) {
         if (ec)
         {
             BMCWEB_LOG_DEBUG << "no PCIe device paths found ec: "
@@ -68,8 +69,9 @@ static inline void
                 continue;
             }
             nlohmann::json::object_t pcieDevice;
-            pcieDevice["@odata.id"] = crow::utility::urlFromPieces(
-                "redfish", "v1", "Systems", "system", "PCIeDevices", devName);
+            boost::urls::url pcieDeviceURL = crow::utility::urlFromPieces(
+                "redfish", "v1", "Systems", systemName, "PCIeDevices", devName);
+            pcieDevice["@odata.id"] = pcieDeviceURL.buffer();
             pcieDeviceList.push_back(std::move(pcieDevice));
         }
         asyncResp->res.jsonValue[name + "@odata.count"] = pcieDeviceList.size();
@@ -117,7 +119,30 @@ inline void requestRoutesSystemPCIeDeviceCollection(App& app)
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/PCIeDevices/")
         .privileges(redfish::privileges::getPCIeDeviceCollection)
         .methods(boost::beast::http::verb::get)(
-            std::bind_front(handlePCIeDeviceCollectionGet, std::ref(app)));
+            [&app](const crow::Request& req,
+                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& systemName) {
+        if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+        {
+            return;
+        }
+        if (systemName != "system")
+        {
+            messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                       systemName);
+            return;
+        }
+
+        asyncResp->res.jsonValue["@odata.type"] =
+            "#PCIeDeviceCollection.PCIeDeviceCollection";
+        asyncResp->res.jsonValue["@odata.id"] =
+            "/redfish/v1/Systems/system/PCIeDevices";
+        asyncResp->res.jsonValue["Name"] = "PCIe Device Collection";
+        asyncResp->res.jsonValue["Description"] = "Collection of PCIe Devices";
+        asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+        asyncResp->res.jsonValue["Members@odata.count"] = 0;
+        getPCIeDeviceList(asyncResp, "Members", systemName);
+        });
 }
 
 inline std::optional<pcie_device::PCIeTypes>
