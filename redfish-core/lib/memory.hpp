1e04f3b2b55cd441bcabbb1d31b08bb2060034ea
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-03-18 17:24:32 +0800

    Memory: Fix OperatingMemoryModes and MemoryMedia properties
    OperatingMemoryModes and MemoryMedia properties of Memory should be of
    array type, and removes the extra whitespace in OperatingMemoryModes.
    
    Tested:
    Redfish validator passed.
    
    Change-Id: I1b697db93e87dc791ce90a61be41da913fe61a36
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- redfish-core/lib/memory.hpp
+++ redfish-core/lib/memory.hpp
@@ -373,7 +373,7 @@ inline void getPersistentMemoryProperties(
         {
             if (boost::ends_with(*value, v))
             {
-                aResp->res.jsonValue["OperatingMemoryModes "] = v;
+                aResp->res.jsonValue["OperatingMemoryModes"].push_back(v);
                 break;
             }
         }
@@ -394,7 +394,7 @@ inline void getPersistentMemoryProperties(
         {
             if (boost::ends_with(*value, v))
             {
-                aResp->res.jsonValue["MemoryMedia"] = v;
+                aResp->res.jsonValue["MemoryMedia"].push_back(v);
                 break;
             }
         }
