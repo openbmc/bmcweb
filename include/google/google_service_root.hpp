ade3f16bd76b5bd5fa8c3395300ee031b2dd2b16
Vidya Satyamsetti <satyamsetti@google.com>
2022-03-21 23:02:26 -0700

    Add RootOfTrustCollection and RootOfTrust under Google service root.
    For now, there will be a single configurable RootOfTrust element in the collection. Support for multiple RootsOfTrust will be added later.
    
    Tested: Manually using curl.
    
    Signed-off-by: Vidya Satyamsetti <satyamsetti@google.com>
    Change-Id: If64612468bb89e6d9251d848697608b7daf37339

--- include/google/google_service_root.hpp
+++ include/google/google_service_root.hpp
@@ -2,12 +2,36 @@
 
 #include <app.hpp>
 #include <async_resp.hpp>
+#include <boost/algorithm/hex.hpp>
+#include <error_messages.hpp>
 #include <nlohmann/json.hpp>
+#include <utils/collection.hpp>
+#include <utils/json_utils.hpp>
+
+#include <vector>
 
 namespace crow
 {
 namespace google_api
 {
+inline void fillRoTList(nlohmann::json::object_t& rots)
+{
+    rots["@odata.id"] = "/google/v1/RootOfTrustCollection";
+    rots["@odata.type"] = "#RootOfTrustCollection.RootOfTrustCollection";
+    nlohmann::json members = nlohmann::json::array();
+    // Hardcoding a single RoT right now.
+    members.push_back(
+        {{"@odata.id",
+          "/google/v1/RootOfTrustCollection/" BMCWEB_GOOGLE_SINGLETON_ROTID}});
+
+    rots["Members"] = members;
+    rots["Members@odata.count"] = members.size();
+}
+
+inline bool validateRoTId(const std::string& rotId)
+{
+    return rotId == BMCWEB_GOOGLE_SINGLETON_ROTID;
+}
 
 inline void requestRoutes(App& app)
 {
@@ -21,6 +45,104 @@ inline void requestRoutes(App& app)
                 asyncResp->res.jsonValue["Id"] = "Google Rest RootService";
                 asyncResp->res.jsonValue["Name"] = "Google Service Root";
                 asyncResp->res.jsonValue["Version"] = "1.0.0";
+                nlohmann::json::object_t rots;
+                fillRoTList(rots);
+                asyncResp->res.jsonValue["RootOfTrustCollection"] = rots;
+            });
+
+    BMCWEB_ROUTE(app, "/google/v1/RootOfTrustCollection")
+        .privileges({{"Login"}})
+        .methods(boost::beast::http::verb::get)(
+            [](const crow::Request&,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
+                nlohmann::json::object_t rots;
+                fillRoTList(rots);
+                asyncResp->res.jsonValue = rots;
+            });
+
+    BMCWEB_ROUTE(app, "/google/v1/RootOfTrustCollection/<str>")
+        .privileges({{"Login"}})
+        .methods(boost::beast::http::verb::get)(
+            [](const crow::Request&,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+               const std::string& rotId) {
+                if (!validateRoTId(rotId))
+                {
+                    BMCWEB_LOG_ERROR << "RootOfTrust ID not recognized ";
+                    redfish::messages::resourceNotFound(asyncResp->res,
+                                                        "RootOfTrust", rotId);
+
+                    return;
+                }
+
+                asyncResp->res.jsonValue["@odata.type"] =
+                    "#RootOfTrust.v1_0_0.RootOfTrust";
+                asyncResp->res.jsonValue["@odata.id"] =
+                    "/google/v1/RootOfTrustCollection/" + rotId;
+
+                nlohmann::json& status =
+                    asyncResp->res.jsonValue["Status"].emplace_back(
+                        nlohmann::json::object());
+                status["State"] = "Enabled";
+                asyncResp->res.jsonValue["Id"] = rotId;
+                asyncResp->res.jsonValue["Name"] = "RootOfTrust-" + rotId;
+                asyncResp->res
+                    .jsonValue["Actions"]["#RootOfTrust.SendCommand"] = {
+                    {"target", "/google/v1/RootOfTrustCollection/" + rotId +
+                                   "/Actions/RootOfTrust.SendCommand"}};
+            });
+
+    BMCWEB_ROUTE(
+        app,
+        "/google/v1/RootOfTrustCollection/<str>/Actions/RootOfTrust.SendCommand")
+        .privileges({{"Login"}})
+        .methods(boost::beast::http::verb::post)(
+            [](const crow::Request& req,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+               const std::string& rotId) {
+                if (!validateRoTId(rotId))
+                {
+                    BMCWEB_LOG_ERROR << "RootOfTrust ID not recognized ";
+                    redfish::messages::resourceNotFound(
+                        asyncResp->res, "RootOfTrust.SendCommand", rotId);
+
+                    return;
+                }
+
+                std::string command;
+                if (!redfish::json_util::readJsonAction(req, asyncResp->res,
+                                                        "Command", command))
+                {
+
+                    BMCWEB_LOG_DEBUG << "Missing property Command.";
+                    redfish::messages::actionParameterMissing(
+                        asyncResp->res, "SendCommand", "Command");
+                    return;
+                }
+                std::string str = boost::algorithm::unhex(command);
+                std::vector<uint8_t> bytes(str.begin(), str.end());
+
+                crow::connections::systemBus->async_method_call(
+                    [command, asyncResp](const boost::system::error_code ec,
+                                         std::vector<uint8_t>& resp_bytes) {
+                        if (ec)
+                        {
+                            BMCWEB_LOG_ERROR
+                                << "RootOfTrust.Actions.SendCommand failed: "
+                                << ec.message();
+                            redfish::messages::internalError(asyncResp->res);
+                            return;
+                        }
+                        std::string resp;
+                        boost::algorithm::hex(resp_bytes.begin(),
+                                              resp_bytes.end(),
+                                              std::back_inserter(resp));
+                        asyncResp->res.jsonValue["CommandResponse"] = resp;
+                    },
+                    "xyz.openbmc_project.Control.Hoth",
+                    "/xyz/openbmc_project/Control/Hoth",
+                    "xyz.openbmc_project.Control.Hoth", "SendHostCommand",
+                    bytes);
             });
 }
 
