c6484b389ce338fab3cd5639ec83230efd4f3782
Vidya Satyamsetti <satyamsetti@google.com>
2022-03-21 23:02:26 -0700

    Add RootOfTrustCollection and RootOfTrust under Google service root.
    TEST:
    $curl -u root:0penBmc -X GET http://[::1]:$PORT/google/v1/RootOfTrustCollection
    {
      "@odata.id": "/google/v1/RootOfTrustCollection",
      "@odata.type": "#RootOfTrustCollection.RootOfTrustCollection",
      "Members": [
        {
          "@odata.id": "/google/v1/RootOfTrustCollection/Hoth"
        }
      ],
      "Members@odata.count": 1
    }
    
    $ curl -u root:0penBmc -X GET http://[::1]:$PORT/google/v1/RootOfTrustCollection/Hoth
    {
      "@odata.id": "/google/v1/RootOfTrustCollection/Hoth",
      "@odata.type": "#RootOfTrust.v1_0_0.RootOfTrust",
      "Actions": {
        "#RootOfTrust.SendCommand": {
          "target": "/google/v1/RootOfTrustCollection/Hoth/Actions/RootOfTrust.SendCommand"
        }
      },
      "Id": "Hoth",
      "Links": {
        "ComponentsProtected": [
          {
            "@odata.id": "/google/v1"
          }
        ],
        "ComponentsProtected@odata.count": 1
      },
      "Name": "RootOfTrust-Hoth",
      "Status": {
        "State": "Enabled"
    }
    
    $ curl -u root:0penBmc -X POST -d @req.json -H "Content-Type: application/json" http://[::1]:$PORT/google/v1/RootOfTrustCollection/Hoth/Actions/RootOfTrust.SendCommand
    {
      "CommandResponse": "033B0000"
    }
    
    Signed-off-by: Vidya Satyamsetti <satyamsetti@google.com>
    Change-Id: If64612468bb89e6d9251d848697608b7daf37339

--- include/google/google_service_root.hpp
+++ include/google/google_service_root.hpp
@@ -1,27 +1,216 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
+
+#include "dbus_utils.hpp"
 
 #include <app.hpp>
 #include <async_resp.hpp>
+#include <boost/algorithm/hex.hpp>
+#include <error_messages.hpp>
 #include <nlohmann/json.hpp>
+#include <utils/collection.hpp>
+#include <utils/json_utils.hpp>
+
+#include <vector>
 
 namespace crow
 {
 namespace google_api
 {
+#define HOTH_SEARCH_PATH "/xyz/openbmc_project"
+#define HOTH_INTERFACE "xyz.openbmc_project.Control.Hoth"
+#define ROT_COLLECTION_PREFIX "/google/v1/RootOfTrustCollection"
+
+std::shared_ptr<ObjectMapperInterface> obj_mapper;
+std::shared_ptr<RedfishUtilWrapper> rf_utils;
+std::shared_ptr<HothInterface> hoth_iface;
+
+inline void getGoogleV1(const crow::Request&,
+                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    asyncResp->res.jsonValue["@odata.type"] =
+        "#GoogleServiceRoot.v1_0_0.GoogleServiceRoot";
+    asyncResp->res.jsonValue["@odata.id"] = "/google/v1";
+    asyncResp->res.jsonValue["Id"] = "Google Rest RootService";
+    asyncResp->res.jsonValue["Name"] = "Google Service Root";
+    asyncResp->res.jsonValue["Version"] = "1.0.0";
+    asyncResp->res.jsonValue["RootOfTrustCollection"] = {
+        {"@odata.id", ROT_COLLECTION_PREFIX}};
+}
+
+inline void getRootOfTrustCollection(
+    const crow::Request&, const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    asyncResp->res.jsonValue["@odata.id"] = ROT_COLLECTION_PREFIX;
+    asyncResp->res.jsonValue["@odata.type"] =
+        "#RootOfTrustCollection.RootOfTrustCollection";
+    rf_utils->populateCollectionMembers(
+        asyncResp, ROT_COLLECTION_PREFIX,
+        std::vector<const char*>{HOTH_INTERFACE}, HOTH_SEARCH_PATH);
+}
+
+using ResolvedEntityHandler = std::function<void(
+    const crow::Request&, const std::shared_ptr<bmcweb::AsyncResp>&,
+    const ResolvedEntity&)>;
+
+inline void resolveRoT(const crow::Request& req,
+                       const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                       const std::string& rotId,
+                       ResolvedEntityHandler&& entityHandler)
+{
+    auto validateFunc = [&req, asyncResp, rotId,
+                         entityHandler{std::forward<ResolvedEntityHandler>(
+                             entityHandler)}](
+                            const boost::system::error_code ec,
+                            const crow::openbmc_mapper::GetSubTreeType&
+                                subtree) {
+        if (ec)
+        {
+            redfish::messages::internalError(asyncResp->res);
+            return;
+        }
+        // Iterate over all retrieved ObjectPaths.
+        for (const std::pair<
+                 std::string,
+                 std::vector<std::pair<std::string, std::vector<std::string>>>>&
+                 object : subtree)
+        {
+            sdbusplus::message::object_path objPath(object.first);
+            if (objPath.filename() != rotId)
+            {
+                continue;
+            }
+            ResolvedEntity resolvedEntity = {.id = rotId,
+                                             .service = object.second[0].first,
+                                             .object = object.first,
+                                             .interface = HOTH_INTERFACE};
+            entityHandler(req, asyncResp, resolvedEntity);
+            return;
+        }
+
+        // Couldn't find an object with that name.  return an error
+        redfish::messages::resourceNotFound(
+            asyncResp->res, "#RootOfTrust.v1_0_0.RootOfTrust", rotId);
+    };
+    ObjectMapperGetSubTreeParams params;
+    params.depth = 0;
+    params.subtree = HOTH_SEARCH_PATH;
+    params.interfaces = {HOTH_INTERFACE};
+    static const DbusMethodAddr addr("xyz.openbmc_project.ObjectMapper",
+                                     "/xyz/openbmc_project/object_mapper",
+                                     "xyz.openbmc_project.ObjectMapper",
+                                     "GetSubTree");
+    obj_mapper->getSubTree(addr, params, validateFunc);
+}
+
+inline void populateRootOfTrustEntity(
+    const crow::Request&, const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const ResolvedEntity& resolvedEntity)
+{
+    asyncResp->res.jsonValue["@odata.type"] = "#RootOfTrust.v1_0_0.RootOfTrust";
+    asyncResp->res.jsonValue["@odata.id"] =
+        "/google/v1/RootOfTrustCollection/" + resolvedEntity.id;
+
+    asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
+    asyncResp->res.jsonValue["Id"] = resolvedEntity.id;
+    asyncResp->res.jsonValue["Name"] = "RootOfTrust-" + resolvedEntity.id;
+    asyncResp->res.jsonValue["Actions"]["#RootOfTrust.SendCommand"]["target"] =
+        "/google/v1/RootOfTrustCollection/" + resolvedEntity.id +
+        "/Actions/RootOfTrust.SendCommand";
+
+    // Need to expand this later with a call to Hoth
+    nlohmann::json& protectedComponents =
+        asyncResp->res.jsonValue["Links"]["ComponentsProtected"];
+    protectedComponents = nlohmann::json::array();
+    protectedComponents.push_back({{"@odata.id", "/google/v1"}});
+    asyncResp->res.jsonValue["Links"]["ComponentsProtected@odata.count"] = 1;
+}
+
+inline void getRootOfTrust(const crow::Request& req,
+                           const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                           const std::string& param)
+{
+    resolveRoT(req, asyncResp, param, populateRootOfTrustEntity);
+}
+
+inline void
+    invokeRoTCommand(const crow::Request& request,
+                     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                     const ResolvedEntity& resolvedEntity)
+{
+    std::string command;
+    if (!redfish::json_util::readJsonAction(request, asyncResp->res, "Command",
+                                            command))
+    {
+        BMCWEB_LOG_DEBUG << "Missing property Command.";
+        redfish::messages::actionParameterMissing(asyncResp->res, "SendCommand",
+                                                  "Command");
+        return;
+    }
+
+    auto handleFunc = [asyncResp](const boost::system::error_code ec,
+                                  std::vector<uint8_t>& resp_bytes) {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR << "RootOfTrust.Actions.SendCommand failed: "
+                             << ec.message();
+            redfish::messages::internalError(asyncResp->res);
+            return;
+        }
+
+        std::string resp;
+        boost::algorithm::hex(resp_bytes.begin(), resp_bytes.end(),
+                              std::back_inserter(resp));
+        asyncResp->res.jsonValue["CommandResponse"] = resp;
+    };
+    std::vector<uint8_t> bytes;
+    boost::algorithm::unhex(command, std::back_inserter(bytes));
+
+    hoth_iface->sendHostCommand(
+        toDbusMethodAddr(resolvedEntity, "SendHostCommand"), bytes, handleFunc);
+}
+
+inline void sendRoTCommand(const crow::Request& request,
+                           const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                           const std::string& rotId)
+{
+    resolveRoT(request, asyncResp, rotId, invokeRoTCommand);
+}
 
 inline void requestRoutes(App& app)
 {
+    crow::google_api::obj_mapper = std::make_shared<ObjectMapperInterface>();
+    crow::google_api::rf_utils = std::make_shared<RedfishUtilWrapper>();
+    crow::google_api::hoth_iface = std::make_shared<HothInterface>();
     BMCWEB_ROUTE(app, "/google/v1/")
-        .methods(boost::beast::http::verb::get)(
-            [](const crow::Request&,
-               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
-                asyncResp->res.jsonValue["@odata.type"] =
-                    "#GoogleServiceRoot.v1_0_0.GoogleServiceRoot";
-                asyncResp->res.jsonValue["@odata.id"] = "/google/v1";
-                asyncResp->res.jsonValue["Id"] = "Google Rest RootService";
-                asyncResp->res.jsonValue["Name"] = "Google Service Root";
-                asyncResp->res.jsonValue["Version"] = "1.0.0";
-            });
+        .methods(boost::beast::http::verb::get)(getGoogleV1);
+
+    BMCWEB_ROUTE(app, "/google/v1/RootOfTrustCollection")
+        .privileges({{"ConfigureManager"}})
+        .methods(boost::beast::http::verb::get)(getRootOfTrustCollection);
+
+    BMCWEB_ROUTE(app, "/google/v1/RootOfTrustCollection/<str>")
+        .privileges({{"ConfigureManager"}})
+        .methods(boost::beast::http::verb::get)(getRootOfTrust);
+
+    BMCWEB_ROUTE(
+        app,
+        "/google/v1/RootOfTrustCollection/<str>/Actions/RootOfTrust.SendCommand")
+        .privileges({{"ConfigureManager"}})
+        .methods(boost::beast::http::verb::post)(sendRoTCommand);
 }
 
 } // namespace google_api
---
+++ include/google/ut/google_service_root_test.cpp
@@ -0,0 +1,361 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/dbus_utils.hpp"
+#include "google/google_service_root.hpp"
+#include "http_request.hpp"
+#include "nlohmann/json.hpp"
+#include "openbmc_dbus_rest.hpp"
+
+#include <async_resp.hpp>
+#include <boost/algorithm/hex.hpp>
+#include <utils/collection.hpp>
+
+#include <memory>
+#include <string>
+
+#include "gmock/gmock.h"
+
+namespace crow
+{
+namespace google_api
+{
+namespace test
+{
+
+constexpr const char* resourceNotFoundMsg = "Resource Not Found";
+constexpr const char* contentNotAcceptableMsg = "Content Not Acceptable";
+constexpr const char* internalServerError = "Internal Server Error";
+
+class MockObjectMapper : public crow::google_api::ObjectMapperInterface
+{
+  public:
+    MOCK_METHOD(void, getSubTree,
+                (const DbusMethodAddr&, const ObjectMapperGetSubTreeParams&,
+                 const ObjectMapperSubTreeCallback&),
+                (override));
+};
+
+class MockHothInterface : public crow::google_api::HothInterface
+{
+  public:
+    MOCK_METHOD(void, sendHostCommand,
+                (const DbusMethodAddr&, const std::vector<uint8_t>&,
+                 const HothSendCommandCallback&),
+                (override));
+};
+
+class MockRedfishUtilWrapper : public crow::google_api::RedfishUtilWrapper
+{
+  public:
+    MOCK_METHOD(void, populateCollectionMembers,
+                (const std::shared_ptr<bmcweb::AsyncResp>&, const std::string&,
+                 const std::vector<const char*>&, const char*),
+                (override));
+};
+
+MATCHER_P(EqualsAddr, addr, "")
+{
+    return arg.objpath == addr.objpath && arg.service == addr.service &&
+           strcmp(arg.iface, addr.iface) == 0 && arg.method == addr.method;
+}
+
+MATCHER_P(EqualsSubTreeParams, other, "")
+{
+    if (arg.interfaces.size() != other.interfaces.size() ||
+        arg.depth != other.depth || strcmp(arg.subtree, other.subtree) != 0)
+        return false;
+    for (size_t i = 0; i < arg.interfaces.size(); ++i)
+    {
+        if (strcmp(arg.interfaces[i], other.interfaces[i]))
+            return false;
+    }
+
+    return true;
+}
+
+class GoogleServiceTest : public ::testing::Test
+{
+  public:
+    GoogleServiceTest() :
+        rootOfTrustIds_({std::string("Foo"), std::string("Bar")}),
+        objMapperGetSubtreeAddr_("xyz.openbmc_project.ObjectMapper",
+                                 "/xyz/openbmc_project/object_mapper",
+                                 "xyz.openbmc_project.ObjectMapper",
+                                 "GetSubTree"),
+        hothSendHostCommandAddr_("xyz.openbmc_project.Control.Hoth",
+                                 "/xyz/openbmc_project/Control/Hoth/Foo",
+                                 "xyz.openbmc_project.Control.Hoth",
+                                 "SendHostCommand")
+    {
+        rotSubTreeSearchParams_.depth = 0;
+        rotSubTreeSearchParams_.subtree = HOTH_SEARCH_PATH;
+        rotSubTreeSearchParams_.interfaces = {HOTH_INTERFACE};
+
+        for (const std::string& rot : rootOfTrustIds_)
+        {
+            rotSubTree_.push_back({"/xyz/openbmc_project/Control/Hoth/" + rot,
+                                   {{"xyz.openbmc_project.Control.Hoth",
+                                     {"xyz.openbmc_project.Control.Hoth"}}}});
+        }
+
+        auto obj_mapper = std::make_shared<MockObjectMapper>();
+        crow::google_api::obj_mapper = obj_mapper;
+        mock_obj_mapper_ = obj_mapper.get();
+
+        auto rf_utils = std::make_shared<MockRedfishUtilWrapper>();
+        crow::google_api::rf_utils = rf_utils;
+        mock_rf_utils_ = rf_utils.get();
+
+        auto hoth_iface = std::make_shared<MockHothInterface>();
+        crow::google_api::hoth_iface = hoth_iface;
+        mock_hoth_iface_ = hoth_iface.get();
+    }
+
+  protected:
+    void validateRootOfTrustCollectionGet(crow::Response& res)
+    {
+        nlohmann::json& json = res.jsonValue;
+        EXPECT_EQ(json["@odata.id"], "/google/v1/RootOfTrustCollection");
+        EXPECT_EQ(json["@odata.type"],
+                  "#RootOfTrustCollection.RootOfTrustCollection");
+        EXPECT_EQ(json["Members@odata.count"], rootOfTrustIds_.size());
+
+        nlohmann::json& members = json["Members"];
+        for (size_t i = 0; i < members.size(); ++i)
+            EXPECT_EQ(members[i]["@odata.id"],
+                      "/google/v1/RootOfTrustCollection/" + rootOfTrustIds_[i]);
+    }
+
+    void validateServiceRootGet(crow::Response& res)
+    {
+        nlohmann::json& json = res.jsonValue;
+        EXPECT_EQ(json["@odata.id"], "/google/v1");
+        EXPECT_EQ(json["@odata.type"],
+                  "#GoogleServiceRoot.v1_0_0.GoogleServiceRoot");
+        EXPECT_EQ(json["@odata.id"], "/google/v1");
+        EXPECT_EQ(json["Id"], "Google Rest RootService");
+        EXPECT_EQ(json["Name"], "Google Service Root");
+        EXPECT_EQ(json["Version"], "1.0.0");
+        EXPECT_EQ(json["RootOfTrustCollection"]["@odata.id"],
+                  "/google/v1/RootOfTrustCollection");
+    }
+
+    void validateRootOfTrustGet(crow::Response& res, std::string rotId)
+    {
+        nlohmann::json& json = res.jsonValue;
+        EXPECT_EQ(json["@odata.type"], "#RootOfTrust.v1_0_0.RootOfTrust");
+        EXPECT_EQ(json["@odata.id"],
+                  "/google/v1/RootOfTrustCollection/" + rotId);
+        EXPECT_EQ(json["Status"]["State"], "Enabled");
+        EXPECT_EQ(json["Id"], rotId);
+        EXPECT_EQ(json["Name"], "RootOfTrust-" + rotId);
+        EXPECT_EQ(json["Actions"]["#RootOfTrust.SendCommand"]["target"],
+                  "/google/v1/RootOfTrustCollection/" + rotId +
+                      "/Actions/RootOfTrust.SendCommand");
+        nlohmann::json& members = json["Links"]["ComponentsProtected"];
+        EXPECT_EQ(members[0]["@odata.id"], "/google/v1");
+        EXPECT_EQ(json["Links"]["ComponentsProtected@odata.count"], 1);
+    }
+
+    void loadRoTSubTreeInMapperWithErrorCode(boost::system::errc::errc_t errc)
+    {
+        boost::system::error_code error_code =
+            boost::system::errc::make_error_code(errc);
+        EXPECT_CALL(*mock_obj_mapper_,
+                    getSubTree(EqualsAddr(objMapperGetSubtreeAddr_),
+                               EqualsSubTreeParams(rotSubTreeSearchParams_),
+                               ::testing::_))
+            .Times(1)
+            .WillOnce(::testing::DoAll(
+                testing::InvokeArgument<2>(error_code, std::ref(rotSubTree_)),
+                ::testing::Return()));
+    }
+
+    std::vector<uint8_t> wordToBytes(const std::string& word)
+    {
+        std::vector<uint8_t> out;
+        std::copy(word.begin(), word.end(), std::back_inserter(out));
+        return out;
+    }
+
+    std::string bytesToHex(const std::vector<uint8_t>& bytes)
+    {
+        std::string out;
+        boost::algorithm::hex(bytes.begin(), bytes.end(),
+                              std::back_inserter(out));
+        return out;
+    }
+
+    void addSendCommandBody(const std::string& request, crow::Request& req)
+    {
+        std::string body = "{\"Command\": \"";
+        body += bytesToHex(wordToBytes(request));
+        body += "\"}";
+        req.body = body;
+    }
+
+    void
+        setupHostCommandResponseWithErrorCode(boost::system::errc::errc_t errc,
+                                              const std::vector<uint8_t>& input,
+                                              std::vector<uint8_t>& output)
+    {
+        boost::system::error_code success =
+            boost::system::errc::make_error_code(errc);
+        EXPECT_CALL(*mock_hoth_iface_,
+                    sendHostCommand(EqualsAddr(hothSendHostCommandAddr_),
+                                    ::testing::ElementsAreArray(input),
+                                    ::testing::_))
+            .Times(1)
+            .WillOnce(
+                ::testing::DoAll(testing::InvokeArgument<2>(success, output),
+                                 ::testing::Return()));
+    }
+
+    MockObjectMapper* mock_obj_mapper_;
+    MockRedfishUtilWrapper* mock_rf_utils_;
+    MockHothInterface* mock_hoth_iface_;
+
+    const std::vector<std::string> rootOfTrustIds_;
+    const DbusMethodAddr objMapperGetSubtreeAddr_;
+    const DbusMethodAddr hothSendHostCommandAddr_;
+    crow::openbmc_mapper::GetSubTreeType rotSubTree_;
+    ObjectMapperGetSubTreeParams rotSubTreeSearchParams_;
+};
+
+TEST_F(GoogleServiceTest, GoogleV1RootGet)
+{
+    std::error_code ec;
+    crow::Request req({}, ec);
+    auto shareAsyncResp = std::make_shared<bmcweb::AsyncResp>();
+
+    getGoogleV1(req, shareAsyncResp);
+    validateServiceRootGet(shareAsyncResp->res);
+}
+
+TEST_F(GoogleServiceTest, RootOfTrustCollectionGet)
+{
+    std::error_code ec;
+    crow::Request req({}, ec);
+    auto async_resp = std::make_shared<bmcweb::AsyncResp>();
+    EXPECT_CALL(*mock_rf_utils_,
+                populateCollectionMembers(
+                    ::testing::Eq(std::ref(async_resp)),
+                    ::testing::Eq("/google/v1/RootOfTrustCollection"),
+                    ::testing::ElementsAre(
+                        ::testing::Eq("xyz.openbmc_project.Control.Hoth")),
+                    ::testing::Eq("/xyz/openbmc_project")))
+        .Times(1)
+        .WillOnce(::testing::DoAll(::testing::InvokeWithoutArgs([&]() {
+            nlohmann::json& members = async_resp->res.jsonValue["Members"];
+            members = nlohmann::json::array();
+            for (auto& rotId : rootOfTrustIds_)
+                members.push_back(
+                    {{"@odata.id",
+                      "/google/v1/RootOfTrustCollection/" + rotId}});
+            async_resp->res.jsonValue["Members@odata.count"] =
+                rootOfTrustIds_.size();
+        })));
+
+    getRootOfTrustCollection(req, async_resp);
+    validateRootOfTrustCollectionGet(async_resp->res);
+}
+
+TEST_F(GoogleServiceTest, RootOfTrustGetSuccess)
+{
+    std::error_code ec;
+    crow::Request req({}, ec);
+    auto async_resp = std::make_shared<bmcweb::AsyncResp>();
+    loadRoTSubTreeInMapperWithErrorCode(boost::system::errc::success);
+    getRootOfTrust(req, async_resp, "Foo");
+    validateRootOfTrustGet(async_resp->res, "Foo");
+}
+
+TEST_F(GoogleServiceTest, RootOfTrustGetInternalError)
+{
+    loadRoTSubTreeInMapperWithErrorCode(boost::system::errc::broken_pipe);
+
+    std::error_code ec;
+    crow::Request req({}, ec);
+    auto async_resp = std::make_shared<bmcweb::AsyncResp>();
+    getRootOfTrust(req, async_resp, "Foo");
+    EXPECT_THAT(async_resp->res.jsonValue["error"]["code"],
+                ::testing::EndsWith("InternalError"));
+}
+
+TEST_F(GoogleServiceTest, RootOfTrustGetNotFoundError)
+{
+    loadRoTSubTreeInMapperWithErrorCode(boost::system::errc::success);
+
+    std::error_code ec;
+    crow::Request req({}, ec);
+    auto async_resp = std::make_shared<bmcweb::AsyncResp>();
+    getRootOfTrust(req, async_resp, "Baz");
+    EXPECT_THAT(async_resp->res.jsonValue["error"]["code"],
+                ::testing::EndsWith("ResourceNotFound"));
+}
+
+TEST_F(GoogleServiceTest, RootOfTrustSendCommandSuccess)
+{
+    std::string command = "Hello!";
+    std::string response = "World!";
+    std::vector<uint8_t> input = wordToBytes(command);
+    std::vector<uint8_t> output = wordToBytes(response);
+    setupHostCommandResponseWithErrorCode(boost::system::errc::success, input,
+                                          output);
+    loadRoTSubTreeInMapperWithErrorCode(boost::system::errc::success);
+
+    std::error_code ec;
+    crow::Request req({}, ec);
+    addSendCommandBody(command, req);
+    auto async_resp = std::make_shared<bmcweb::AsyncResp>();
+    sendRoTCommand(req, async_resp, "Foo");
+    EXPECT_EQ(async_resp->res.jsonValue["CommandResponse"],
+              bytesToHex(wordToBytes(response)));
+}
+
+TEST_F(GoogleServiceTest, RootOfTrustSendCommandBadInput)
+{
+    loadRoTSubTreeInMapperWithErrorCode(boost::system::errc::success);
+
+    std::error_code ec;
+    crow::Request req({}, ec);
+    req.body = "{}";
+    auto async_resp = std::make_shared<bmcweb::AsyncResp>();
+    sendRoTCommand(req, async_resp, "Foo");
+    EXPECT_THAT(async_resp->res.jsonValue["error"]["code"],
+                ::testing::EndsWith("ActionParameterMissing"));
+}
+
+TEST_F(GoogleServiceTest, RootOfTrustSendCommandInternalError)
+{
+    std::string command = "Hello!";
+    std::vector<uint8_t> input = wordToBytes(command);
+    std::vector<uint8_t> output;
+    setupHostCommandResponseWithErrorCode(boost::system::errc::broken_pipe,
+                                          input, output);
+    loadRoTSubTreeInMapperWithErrorCode(boost::system::errc::success);
+
+    std::error_code ec;
+    crow::Request req({}, ec);
+    addSendCommandBody(command, req);
+    auto async_resp = std::make_shared<bmcweb::AsyncResp>();
+    sendRoTCommand(req, async_resp, "Foo");
+    EXPECT_THAT(async_resp->res.jsonValue["error"]["code"],
+                ::testing::EndsWith("InternalError"));
+}
+
+} // namespace test
+} // namespace google_api
+} // namespace crow
