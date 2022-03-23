1862295d6ba8e07c9efdf30d3a4c62eae36ce5b1
Ed Tanous <edtanous@google.com>
2022-03-22 23:53:51 -0700

    Implement Expand
    Section 7.3 of the Redfish specification lays out a feature called
    "expand" that allows users to expand portions of the Redfish tree
    automatically on the server side.  This commit implements them to the
    specification.
    
    To accomplish this, a new class, MultiAsyncResp is created, that allows
    RAII objects to handle lifetime properly.  When an expand query is
    generated, a MultiAsyncResp object is instantiated, which allows "new"
    requests to attach themselves to the multi object, and keep the request
    alive until they all complete.  This also allows requests to be created,
    while requests are in flight, which is required for queries above
    depth=1.
    
    Negatives:
    Similar to the previous $only commit, this requires that all nodes
    redfish nodes now capture App by reference.  This is common, but does
    interfere with some of our other patterns, and attempts to improve the
    syntactic sugar for this proved unworkable.
    
    This commit only adds the above to service root and Computer systems, in
    hopes that we find a better syntax before this merges.
    
    Left to future patches in series:
    Merging the error json structures in responses.
    
    The Redfish spec isn't very clear on how errors propagate for expanded
    queries, and in a conforming we shouldn't ever hit them, but
    nonetheless, I suspect the behavior we have is sub-optimal (attaching an
    error node to every place in the tree that had an issue) and we should
    attempt to do better in the future.
    
    Tested (on previous patch):
    
    curl --insecure --user root:0penBmc https://localhost:18080/redfish/v1\?\$expand\=.\(\$levels\=255\)
    Returns the full tree
    
    Setting $levels=1 query returns only a depth of 1 tree being returned.
    
    Unit tests passing
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I874aabfaa9df5dbf832a80ec62ae65369284791d

---
+++ redfish-core/include/utils/query_param_test.cpp
@@ -0,0 +1,190 @@
+#include "query_param.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <iostream>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(QueryParams, ParseParametersOnly)
+{
+    auto ret = boost::urls::parse_relative_ref("/redfish/v1?only");
+    ASSERT_TRUE(ret);
+
+    crow::Response res;
+
+    using redfish::query_param::parseParameters;
+    using redfish::query_param::Query;
+    std::optional<Query> query = parseParameters(ret->params(), res);
+    ASSERT_TRUE(query != std::nullopt);
+    EXPECT_TRUE(query->isOnly);
+}
+
+TEST(QueryParams, ParseParametersExpand)
+{
+    auto ret = boost::urls::parse_relative_ref("/redfish/v1?$expand=*");
+    ASSERT_TRUE(ret);
+
+    crow::Response res;
+
+    using redfish::query_param::parseParameters;
+    using redfish::query_param::Query;
+    std::optional<Query> query = parseParameters(ret->params(), res);
+    ASSERT_TRUE(query != std::nullopt);
+    EXPECT_TRUE(query->expandType == redfish::query_param::ExpandType::Both);
+}
+
+TEST(QueryParams, ParseParametersUnexpectedGetsIgnored)
+{
+    auto ret = boost::urls::parse_relative_ref("/redfish/v1?unexpected_param");
+    ASSERT_TRUE(ret);
+
+    crow::Response res;
+
+    using redfish::query_param::parseParameters;
+    using redfish::query_param::Query;
+    std::optional<Query> query = parseParameters(ret->params(), res);
+    ASSERT_TRUE(query != std::nullopt);
+}
+
+TEST(QueryParams, ParseParametersUnexpectedDollarGetsError)
+{
+    auto ret = boost::urls::parse_relative_ref("/redfish/v1?$unexpected_param");
+    ASSERT_TRUE(ret);
+
+    crow::Response res;
+
+    using redfish::query_param::parseParameters;
+    using redfish::query_param::Query;
+    std::optional<Query> query = parseParameters(ret->params(), res);
+    ASSERT_TRUE(query == std::nullopt);
+    EXPECT_EQ(res.result(), boost::beast::http::status::not_implemented);
+}
+
+TEST(QueryParams, GetExpandType)
+{
+    redfish::query_param::Query query{};
+
+    EXPECT_FALSE(getExpandType("", query));
+    EXPECT_FALSE(getExpandType(".(", query));
+    EXPECT_FALSE(getExpandType(".()", query));
+    EXPECT_FALSE(getExpandType(".($levels=1", query));
+
+    EXPECT_TRUE(getExpandType("*", query));
+    EXPECT_EQ(query.expandType, redfish::query_param::ExpandType::Both);
+    EXPECT_TRUE(getExpandType(".", query));
+    EXPECT_EQ(query.expandType, redfish::query_param::ExpandType::NotLinks);
+    EXPECT_TRUE(getExpandType("~", query));
+    EXPECT_EQ(query.expandType, redfish::query_param::ExpandType::Links);
+
+    // Per redfish specification, level defaults to 1
+    EXPECT_TRUE(getExpandType(".", query));
+    EXPECT_EQ(query.expandLevel, 1);
+
+    EXPECT_TRUE(getExpandType(".($levels=42)", query));
+    EXPECT_EQ(query.expandLevel, 42);
+
+    // Overflow
+    EXPECT_FALSE(getExpandType(".($levels=256)", query));
+
+    // Negative
+    EXPECT_FALSE(getExpandType(".($levels=-1)", query));
+
+    // No number
+    EXPECT_FALSE(getExpandType(".($levels=a)", query));
+}
+
+namespace redfish::query_param
+{
+// NOLINTNEXTLINE(readability-identifier-naming)
+void PrintTo(const ExpandNode& value, ::std::ostream* os)
+{
+    *os << "ExpandNode: " << value.location << " " << value.uri;
+}
+}; // namespace redfish::query_param
+
+TEST(QueryParams, FindNavigationReferencesNonLink)
+{
+    using nlohmann::json;
+    using redfish::query_param::ExpandType;
+    using redfish::query_param::findNavigationReferences;
+    using ::testing::UnorderedElementsAre;
+    json singleTreeNode = R"({"Foo" : {"@odata.id": "/foobar"}})"_json;
+
+    // Parsing as the root should net one entry
+    EXPECT_THAT(findNavigationReferences(ExpandType::Both, singleTreeNode,
+                                         json::json_pointer("")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/Foo"), "/foobar"}));
+    // Parsing at a depth should net one entry at depth
+    EXPECT_THAT(findNavigationReferences(ExpandType::Both, singleTreeNode,
+                                         json::json_pointer("/baz")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/baz/Foo"), "/foobar"}));
+
+    // Parsing in Non-hyperlinks mode should net one entry
+    EXPECT_THAT(findNavigationReferences(ExpandType::NotLinks, singleTreeNode,
+                                         json::json_pointer("")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/Foo"), "/foobar"}));
+
+    // Parsing non-hyperlinks at depth should net one entry at depth
+    EXPECT_THAT(findNavigationReferences(ExpandType::NotLinks, singleTreeNode,
+                                         json::json_pointer("/baz")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/baz/Foo"), "/foobar"}));
+
+    // Searching for not types should return empty set
+    EXPECT_TRUE(findNavigationReferences(ExpandType::None, singleTreeNode,
+                                         json::json_pointer(""))
+                    .empty());
+
+    // Searching for hyperlinks only should return empty set
+    EXPECT_TRUE(findNavigationReferences(ExpandType::Links, singleTreeNode,
+                                         json::json_pointer(""))
+                    .empty());
+}
+
+TEST(QueryParams, FindNavigationReferencesLink)
+{
+    using nlohmann::json;
+    using redfish::query_param::ExpandType;
+    using redfish::query_param::findNavigationReferences;
+    using ::testing::UnorderedElementsAre;
+    json singleLinkNode =
+        R"({"Links" : {"Sessions": {"@odata.id": "/foobar"}}})"_json;
+
+    // Parsing as the root should net one entry
+    EXPECT_THAT(findNavigationReferences(ExpandType::Both, singleLinkNode,
+                                         json::json_pointer("")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/Links/Sessions"), "/foobar"}));
+    // Parsing at a depth should net one entry at depth
+    EXPECT_THAT(findNavigationReferences(ExpandType::Both, singleLinkNode,
+                                         json::json_pointer("/baz")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/baz/Links/Sessions"), "/foobar"}));
+
+    // Parsing in hyperlinks mode should net one entry
+    EXPECT_THAT(findNavigationReferences(ExpandType::Links, singleLinkNode,
+                                         json::json_pointer("")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/Links/Sessions"), "/foobar"}));
+
+    // Parsing hyperlinks at depth should net one entry at depth
+    EXPECT_THAT(findNavigationReferences(ExpandType::Links, singleLinkNode,
+                                         json::json_pointer("/baz")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/baz/Links/Sessions"), "/foobar"}));
+
+    // Searching for not types should return empty set
+    EXPECT_TRUE(findNavigationReferences(ExpandType::None, singleLinkNode,
+                                         json::json_pointer(""))
+                    .empty());
+
+    // Searching for non-hyperlinks only should return empty set
+    EXPECT_TRUE(findNavigationReferences(ExpandType::NotLinks, singleLinkNode,
+                                         json::json_pointer(""))
+                    .empty());
+}
