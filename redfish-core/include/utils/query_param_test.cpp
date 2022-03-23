857ce2c7a24f49bfa91776cf98a9ef9935bcfcf2
Ed Tanous <edtanous@google.com>
2022-03-22 23:53:51 -0700

    WIP: Implement Expand
    Section 7.3 of the Redfish specification lays out a feature called
    "expand" that allows users to expand portions of the Redfish tree
    automatically on the server side.  This commit implements them to the
    specification.
    
    To accomplish this, a new class, MultiAsyncResp is created, that allows
    RAII objects to handle lifetime properly.  When an expand query is
    generated, a MultiAsyncResp object is instantiated, which allows "new"
    requests to attach themselves to the multi object, and keep the request
    alive until they all complete.  This also allows requests to be created,
    while requests are in flight, which is required for queries above
    depth=1.
    
    Negatives:
    Similar to the previous $only commit, this requires that all nodes
    redfish nodes now capture App by reference.  This is common, but does
    interfere with some of our other patterns, and attempts to improve the
    syntactic sugar for this proved unworkable.
    
    This commit only adds the above to service root and Computer systems, in
    hopes that we find a better syntax before this merges.
    
    TODO: Handling ExpandType::Hyperlink and ExpandType::NotHyperlink
    Merging the error json structures in responses.
    Unit tests for findNavigationReferences.
    
    Tested:
    
    curl --insecure --user root:0penBmc https://localhost:18080/redfish/v1\?\$expand\=.\(\$levels\=255\)
    Returns the full tree
    
    Setting $levels=1 query returns only a depth of 1 tree being returned.
    
    Unit tests passing
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I874aabfaa9df5dbf832a80ec62ae65369284791d

---
+++ redfish-core/include/utils/query_param_test.cpp
@@ -0,0 +1,53 @@
+#include "query_param.hpp"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using ::testing::UnorderedElementsAre;
+
+TEST(QueryParams, GetExpandType)
+{
+    redfish::query_param::Query query{};
+
+    EXPECT_FALSE(getExpandType("", query));
+    EXPECT_FALSE(getExpandType(".(", query));
+    EXPECT_FALSE(getExpandType(".()", query));
+    EXPECT_FALSE(getExpandType(".($levels=1", query));
+
+    EXPECT_TRUE(getExpandType("*", query));
+    EXPECT_EQ(query.expandType, redfish::query_param::ExpandType::Hyperlinks);
+    EXPECT_TRUE(getExpandType(".", query));
+    EXPECT_EQ(query.expandType,
+              redfish::query_param::ExpandType::NotHyperlinks);
+    EXPECT_TRUE(getExpandType("~", query));
+    EXPECT_EQ(query.expandType, redfish::query_param::ExpandType::Both);
+
+    // Per redfish specification, level defaults to 1
+    EXPECT_TRUE(getExpandType(".", query));
+    EXPECT_EQ(query.expandLevel, 1);
+
+    EXPECT_TRUE(getExpandType(".($levels=42)", query));
+    EXPECT_EQ(query.expandLevel, 42);
+
+    // Overflow
+    EXPECT_FALSE(getExpandType(".($levels=256)", query));
+
+    // Negative
+    EXPECT_FALSE(getExpandType(".($levels=-1)", query));
+
+    // No number
+    EXPECT_FALSE(getExpandType(".($levels=a)", query));
+}
+
+TEST(QueryParams, FindNavigationReferences)
+{
+    using nlohmann::json;
+    using redfish::query_param::findNavigationReferences;
+    json j = "{\"Foo\" : {\"@odata.id\": \"/foobar\"}}"_json;
+    std::vector<redfish::query_param::ExpandNode> expected{
+        redfish::query_param::ExpandNode{json::json_pointer("/Foo"),
+                                         "/foobar"}};
+    ASSERT_THAT(findNavigationReferences(j, json::json_pointer("")),
+                UnorderedElementsAre(redfish::query_param::ExpandNode{
+                    json::json_pointer("/Foo"), "/foobar"}));
+}
