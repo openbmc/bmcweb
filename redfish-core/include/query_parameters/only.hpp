2f8396035f983a3530b1e9a7b30f318f6e79531d
Nan Zhou <nanzhoumails@gmail.com>
2022-03-23 09:14:03 -0700

    query parameters: only: add class skeleton and implement parsing
    Added a QueryParameter base class. Future individual query parameter
    will implement the interface it defines.
    Also added a handler class to explain how these interfaces will be used
    in future PRs.
    
    Added implementation of parsing the "only" parameter. Added
    corresponding unit tests.
    
    The next change will implement the "process" part.
    
    Tested:
    unit tests.
    The next change will include integration tests.
    
    This commit is split from these changes:
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/38952
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/47474
    Credits to maxiaochao@inspur.com, ed@tanous.net, and
    zhanghch05@inspur.com.
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: Id361626f7c891d877d406362b41e945246f6c7d1

---
+++ redfish-core/include/query_parameters/only.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "error_messages.hpp"
+#include "query.hpp"
+
+#include <boost/url/impl/params_view.hpp>
+#include <boost/url/params_view.hpp>
+#include <boost/url/string.hpp>
+
+namespace redfish::query_parameters
+{
+
+class OnlyMemberQuery : public Parameter
+{
+  public:
+    ~OnlyMemberQuery() override = default;
+    OnlyMemberQuery(const OnlyMemberQuery& other) = default;
+    OnlyMemberQuery& operator=(const OnlyMemberQuery& other) = default;
+    OnlyMemberQuery(OnlyMemberQuery&& other) = default;
+    OnlyMemberQuery& operator=(OnlyMemberQuery&& other) = default;
+
+    static std::unique_ptr<Parameter>
+        parse(const boost::urls::params_view& urlParams,
+              crow::Response& intermediateResponse)
+    {
+        auto it = urlParams.find(onlyParamString);
+        if (it == urlParams.end())
+        {
+            return nullptr;
+        }
+        boost::urls::params_view::value_type param = *it;
+        if (!param.value.empty())
+        {
+            messages::queryParameterValueFormatError(
+                intermediateResponse, {param.value.begin(), param.value.end()},
+                {param.key.begin(), param.key.end()});
+            return nullptr;
+        }
+        return std::unique_ptr<OnlyMemberQuery>(new OnlyMemberQuery());
+    }
+
+    ParameterType type() override
+    {
+        return ParameterType::only;
+    }
+
+    void process(const boost::urls::params_view& /*urlParams*/,
+                 crow::Response& /*intermediateResponse*/) override
+    {
+        // TODO: add the actual implementation in the children PRs
+    }
+
+  private:
+    OnlyMemberQuery() = default;
+
+    static constexpr std::string_view onlyParamString = "only";
+};
+
+} // namespace redfish::query_parameters
---
+++ redfish-core/include/query_parameters/query.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "async_resp.hpp"
+#include "only.hpp"
+
+#include <boost/url/impl/params_view.hpp>
+#include <boost/url/params_view.hpp>
+
+#include <memory>
+
+namespace redfish::query_parameters
+{
+
+enum class ParameterType
+{
+    only,
+};
+
+// The interface for all query parameters.
+// Derived class shall implement the |parse| factory function.
+class Parameter
+{
+  public:
+    virtual ~Parameter() = default;
+    Parameter() = default;
+    Parameter(const Parameter& other) = default;
+    Parameter& operator=(const Parameter& other) = default;
+    Parameter(Parameter&& other) = default;
+    Parameter& operator=(Parameter&& other) = default;
+
+    // Parses the |urlParams|, sets |intermediateResponse| to errros on failure
+    // otherwise returns a parsed parameter.
+    template <typename Derived>
+    static std::unique_ptr<Parameter>
+        parse(const boost::urls::params_view& urlParams,
+              crow::Response& intermediateResponse)
+    {
+        return Derived::parse(urlParams, intermediateResponse);
+    }
+
+    // Hijacks the |intermediateResponse| and processes the query parameter.
+    virtual void process(const boost::urls::params_view& urlParams,
+                         crow::Response& intermediateResponse) = 0;
+    // Returns the type of the implemented query parameter.
+    virtual ParameterType type() = 0;
+};
+
+class ParametersHandler
+{
+  public:
+    // TODO: this will parse all the query parameters and store them into a
+    // container. It calls individual parameter's |parse| function.
+    void parseParameters()
+    {}
+    // TODO: this will reorder all parameters according to the spec. It raises
+    // errors if the conbination of parameters are not supported.
+    // It calls individual parameter's |type| function.
+    void reorderParameters()
+    {}
+    // TODO: this will process the reordered parameters by calling individual
+    // parameter's |process| function.
+    void processParameters()
+    {}
+};
+
+} // namespace redfish::query_parameters
