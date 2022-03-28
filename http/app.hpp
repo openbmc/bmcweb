1c8252799b1c97fad5b05c5746ec512292a1f66c
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-03-28 15:17:30 +0800

    Drop unused App.sslFile() method
    App.sslFile() method is never called, the sslContext is generated in
    ensuressl::getSslContext() function now. So remove these unused code.
    
    Tested:
    Build pass.
    
    Change-Id: I2737462a3a2ec2e0dc792e5070e9e5a7244bc889
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- http/app.hpp
+++ http/app.hpp
@@ -151,35 +151,6 @@ class App
     }
 
 #ifdef BMCWEB_ENABLE_SSL
-    App& sslFile(const std::string& crtFilename, const std::string& keyFilename)
-    {
-        sslContext = std::make_shared<ssl_context_t>(
-            boost::asio::ssl::context::tls_server);
-        sslContext->set_verify_mode(boost::asio::ssl::verify_peer);
-        sslContext->use_certificate_file(crtFilename, ssl_context_t::pem);
-        sslContext->use_private_key_file(keyFilename, ssl_context_t::pem);
-        sslContext->set_options(boost::asio::ssl::context::default_workarounds |
-                                boost::asio::ssl::context::no_sslv2 |
-                                boost::asio::ssl::context::no_sslv3 |
-                                boost::asio::ssl::context::no_tlsv1 |
-                                boost::asio::ssl::context::no_tlsv1_1);
-        return *this;
-    }
-
-    App& sslFile(const std::string& pemFilename)
-    {
-        sslContext = std::make_shared<ssl_context_t>(
-            boost::asio::ssl::context::tls_server);
-        sslContext->set_verify_mode(boost::asio::ssl::verify_peer);
-        sslContext->load_verify_file(pemFilename);
-        sslContext->set_options(boost::asio::ssl::context::default_workarounds |
-                                boost::asio::ssl::context::no_sslv2 |
-                                boost::asio::ssl::context::no_sslv3 |
-                                boost::asio::ssl::context::no_tlsv1 |
-                                boost::asio::ssl::context::no_tlsv1_1);
-        return *this;
-    }
-
     App& ssl(std::shared_ptr<boost::asio::ssl::context>&& ctx)
     {
         sslContext = std::move(ctx);
@@ -191,18 +162,6 @@ class App
     std::shared_ptr<ssl_context_t> sslContext = nullptr;
 
 #else
-    template <typename T, typename... Remain>
-    App& ssl_file(T&&, Remain&&...)
-    {
-        // We can't call .ssl() member function unless BMCWEB_ENABLE_SSL is
-        // defined.
-        static_assert(
-            // make static_assert dependent to T; always false
-            std::is_base_of<T, void>::value,
-            "Define BMCWEB_ENABLE_SSL to enable ssl support.");
-        return *this;
-    }
-
     template <typename T>
     App& ssl(T&&)
     {
