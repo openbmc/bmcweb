c56d0c839e415751a854804f810e37912bbbfcae
Shounak Mitra <shounak@google.com>
2022-03-31 13:51:08 -0700

    Inventory properties via Assembly schema on bmcweb
    This commit implements Redfish Assembly schema on bmcweb.
    This schema will be used to publish inventory data for FRUs which are
    attached to a given Chassis and does not map to any specific schema
    definition.
    
    The properties which are published in this commit are LocationCode,
    SparePartNumber, Model, SerialNumber and PartNumber.
    
    One of the major use case to publish these properties via redfish
    is for service engineers to identify the FRU and its location in the
    system, which in turn will help them in repair/replacement related to
    that FRU.
    
    The validator has been executed on the change and no error has been
    found.
    As this has been tested on a development image some fields are empty
    in the below pasted output for which warning was thrown by validator but
    no errors.
    
    Sample Output:
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish/v1/Chassis/chassis/Assembly/
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/Assembly",
      "@odata.type": "#Assembly.v1_3_0.Assembly",
      "Assemblies": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/0",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "0",
          "Model": "",
          "Name": "base_op_panel_blyth",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B85010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/1",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "1",
          "Model": "",
          "Name": "lcd_op_panel_hill",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B86010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/2",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "2",
          "Model": "",
          "Name": "tpm_wilson",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B59010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/3",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "3",
          "Model": "",
          "Name": "vdd_vrm0",
          "PartNumber": "PN12345",
          "SerialNumber": "YL2E32010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/4",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "4",
          "Model": "",
          "Name": "vdd_vrm1",
          "PartNumber": "PN12345",
          "SerialNumber": "YL2E32010000",
          "SparePartNumber": ""
        }
      ],
      "Assemblies@odata.count": 5,
      "Id": "Assembly",
      "Name": "Assembly Collection"
    }
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I2d462340fe1a0b0eb387697f0ff70fcafde3f8d9
    Signed-off-by: Shounak Mitra <shounak@google.com>

---
+++ redfish-core/lib/assembly.hpp
@@ -0,0 +1,254 @@
+#pragma once
+
+#include <dbus_utility.hpp>
+#include <human_sort.hpp>
+#include <sdbusplus/asio/property.hpp>
+#include <utility.hpp>
+#include <utils/json_utils.hpp>
+#include <utils/location_utils.hpp>
+
+namespace redfish
+{
+inline void addAssemblyAssetProperties(
+    const std::string& serviceName, const std::string& path,
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp, std::size_t index)
+{
+    sdbusplus::asio::getAllProperties(
+        *crow::connections::systemBus, serviceName, path,
+        "xyz.openbmc_project.Inventory.Decorator.Asset",
+        [aResp, index](const boost::system::error_code ec,
+                       const dbus::utility::DBusPropertiesMap& propertiesList) {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR << "Failed to get asset properties: " << ec;
+            messages::internalError(aResp->res);
+            return;
+        }
+
+        nlohmann::json& assembliesJson = aResp->res.jsonValue["Assemblies"];
+        if (!assembliesJson.is_array() || assembliesJson.size() <= index)
+        {
+            BMCWEB_LOG_ERROR << "Assemblies list too small";
+            messages::internalError(aResp->res);
+            return;
+        }
+
+        for (const auto& [propertyName, value] : propertiesList)
+        {
+            // This should really be a string_view, but nlohmann::json
+            // doesn't support string_view keys.
+            std::string jsonKey;
+            if ((propertyName == "PartNumber") ||
+                (propertyName == "SerialNumber") || (propertyName == "Model") ||
+                (propertyName == "SparePartNumber"))
+            {
+                jsonKey = propertyName;
+            }
+            else if (propertyName == "Manufacturer")
+            {
+                jsonKey = "Producer";
+            }
+            else if (propertyName == "BuildDate")
+            {
+                jsonKey = "ProductionDate";
+            }
+            else
+            {
+                // Not supported.
+                BMCWEB_LOG_WARNING << "Unsupported assembly asset "
+                                   << propertyName;
+                continue;
+            }
+
+            const auto* valueStr = std::get_if<std::string>(&value);
+            if (valueStr == nullptr)
+            {
+                BMCWEB_LOG_ERROR << "Null value returned for " << propertyName;
+                messages::internalError(aResp->res);
+                return;
+            }
+            // SparePartNumber default is empty
+            if (propertyName == "SparePartNumber" && valueStr->empty())
+            {
+                continue;
+            }
+
+            assembliesJson[index][jsonKey] = *valueStr;
+        }
+        });
+}
+
+/**
+ * @brief Get properties for an assembly
+ * @param[in] aResp - Shared pointer for asynchronous calls.
+ * @param[in] assemblyId - Assembly resource ID.
+ * @param[in] assemblies - list of all the dbus paths associated as an assembly
+ * with the parent.
+ * @return None.
+ */
+inline void
+    getAssemblyProperties(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                          const boost::urls::url& assemblyId,
+                          const std::vector<std::string>& assemblies)
+{
+    aResp->res.jsonValue["Assemblies@odata.count"] = assemblies.size();
+    aResp->res.jsonValue["Assemblies"] = nlohmann::json::array();
+
+    for (const std::string& assembly : assemblies)
+    {
+        std::string name = sdbusplus::message::object_path(assembly).filename();
+        if (name.empty())
+        {
+            BMCWEB_LOG_DEBUG << "Empty name in assembly";
+            messages::internalError(aResp->res);
+            return;
+        }
+
+        crow::connections::systemBus->async_method_call(
+            [aResp, assembly, assemblyId, name](
+                const boost::system::error_code ec,
+                const std::vector<
+                    std::pair<std::string, std::vector<std::string>>>& object) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG << "DBUS response error";
+                messages::internalError(aResp->res);
+                return;
+            }
+
+            nlohmann::json& assembliesJson = aResp->res.jsonValue["Assemblies"];
+            std::size_t assemblyIndex = assembliesJson.size();
+            boost::urls::url dataID = assemblyId;
+            dataID.set_fragment(
+                crow::utility::urlFromPieces("Assemblies", name).string());
+
+            assembliesJson.push_back(
+                {{"@odata.type", "#Assembly.v1_3_0.AssemblyData"},
+                 {"@odata.id", dataID.string()},
+                 {"MemberId", std::to_string(assemblyIndex)},
+                 {"Name", name}});
+
+            for (const auto& [serviceName, interfaceList] : object)
+            {
+                for (const auto& interface : interfaceList)
+                {
+                    if (interface ==
+                        "xyz.openbmc_project.Inventory.Decorator.Asset")
+                    {
+                        addAssemblyAssetProperties(serviceName, assembly, aResp,
+                                                   assemblyIndex);
+                    }
+                    else if (interface == "xyz.openbmc_project.Inventory."
+                                          "Decorator.LocationCode")
+                    {
+                        location_util::getLocationCode(
+                            aResp, serviceName, assembly,
+                            "/Assemblies"_json_pointer / assemblyIndex /
+                                "Location");
+                    }
+                    else
+                    {
+                        std::optional<std::string> type =
+                            location_util::getLocationType(interface);
+                        if (type)
+                        {
+                            assembliesJson[assemblyIndex]["Location"]
+                                          ["PartLocation"]["LocationType"] =
+                                              *type;
+                        }
+                    }
+                }
+            }
+            },
+            "xyz.openbmc_project.ObjectMapper",
+            "/xyz/openbmc_project/object_mapper",
+            "xyz.openbmc_project.ObjectMapper", "GetObject", assembly,
+            std::to_array<std::string>(
+                {"xyz.openbmc_project.Inventory.Decorator.Asset",
+                 "xyz.openbmc_project.Inventory.Decorator.LocationCode",
+                 "xyz.openbmc_project.Inventory.Connector.Embedded",
+                 "xyz.openbmc_project.Inventory.Connector.Slot"}));
+    }
+}
+
+/**
+ * @brief Api to get assembly endpoints from mapper.
+ * @param[in] aResp - Shared pointer for asynchronous calls.
+ * @param[in] assemblyId - Assembly resource ID.
+ * @param[in] assemblyPath - DBus path of assembly association.
+ *
+ * @return None.
+ */
+inline void
+    getAssemblyEndpoints(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                         const boost::urls::url& assemblyId,
+                         const std::string& assemblyPath)
+{
+    // if there is assembly association, look
+    // for endpoints
+    sdbusplus::asio::getProperty<std::vector<std::string>>(
+        *crow::connections::systemBus, "xyz.openbmc_project.ObjectMapper",
+        assemblyPath, "xyz.openbmc_project.Association", "endpoints",
+        [aResp, assemblyId](const boost::system::error_code ec,
+                            const std::vector<std::string>& endpoints) {
+        if (ec)
+        {
+            BMCWEB_LOG_DEBUG << "DBUS response error";
+            messages::internalError(aResp->res);
+            return;
+        }
+
+        std::vector<std::string> sortedAssemblyList = endpoints;
+        std::sort(sortedAssemblyList.begin(), sortedAssemblyList.end(),
+                  AlphanumLess<std::string>());
+
+        getAssemblyProperties(aResp, assemblyId, sortedAssemblyList);
+        });
+}
+
+/**
+ * @brief Gets assemblies from assembly associations to an object.
+ * @param[in] aResp - Shared pointer for asynchronous calls.
+ * @param[in] assemblyId - Assembly resource ID.
+ * @param[in] parentPath - DBus path of parent of assembly.
+ *
+ * @return None.
+ */
+inline void getAssembly(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                        const boost::urls::url& assemblyId,
+                        const std::string& parentPath)
+{
+    aResp->res.jsonValue["@odata.type"] = "#Assembly.v1_3_0.Assembly";
+    aResp->res.jsonValue["@odata.id"] = assemblyId.string();
+    aResp->res.jsonValue["Name"] = "Assembly Collection";
+    aResp->res.jsonValue["Id"] = "Assembly";
+
+    aResp->res.jsonValue["Assemblies"] = nlohmann::json::array();
+    aResp->res.jsonValue["Assemblies@odata.count"] = 0;
+
+    // check if this chassis hosts any association
+    crow::connections::systemBus->async_method_call(
+        [aResp, assemblyId](const boost::system::error_code ec,
+                            const std::vector<std::string>& paths) {
+        if (ec)
+        {
+            BMCWEB_LOG_DEBUG << "DBUS response error";
+            messages::internalError(aResp->res);
+            return;
+        }
+        for (const std::string& path : paths)
+        {
+            if (sdbusplus::message::object_path(path).filename() == "assembly")
+            {
+                getAssemblyEndpoints(aResp, assemblyId, path);
+                return;
+            }
+        }
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths", parentPath, 0,
+        std::array<const char*, 1>{"xyz.openbmc_project.Association"});
+}
+
+} // namespace redfish
