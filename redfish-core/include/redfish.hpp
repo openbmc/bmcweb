e080a5016644a9a0b2c7a57f180e74945f2b25e2
Shounak Mitra <shounak@google.com>
2022-03-31 13:51:08 -0700

    Inventory properties via Assembly schema on bmcweb
    This commit implements Redfish Assembly schema on bmcweb.
    This schema will be used to publish inventory data for FRUs which are
    attached to a given Chassis and does not map to any specific schema
    definition.
    
    The properties which are published in this commit are LocationCode,
    SparePartNumber, Model, SerialNumber and PartNumber.
    
    One of the major use case to publish these properties via redfish
    is for service engineers to identify the FRU and its location in the
    system, which in turn will help them in repair/replacement related to
    that FRU.
    
    The validator has been executed on the change and no error has been
    found.
    As this has been tested on a development image some fields are empty
    in the below pasted output for which warning was thrown by validator but
    no errors.
    
    Sample Output:
    curl -k -H "X-Auth-Token: $bmc_token"
     -X GET https://${bmc}/redfish/v1/Chassis/chassis/Assembly/
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/Assembly",
      "@odata.type": "#Assembly.v1_3_0.Assembly",
      "Assemblies": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/0",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "0",
          "Model": "",
          "Name": "base_op_panel_blyth",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B85010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/1",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "1",
          "Model": "",
          "Name": "lcd_op_panel_hill",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B86010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/2",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "2",
          "Model": "",
          "Name": "tpm_wilson",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B59010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/3",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "3",
          "Model": "",
          "Name": "vdd_vrm0",
          "PartNumber": "PN12345",
          "SerialNumber": "YL2E32010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/4",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "4",
          "Model": "",
          "Name": "vdd_vrm1",
          "PartNumber": "PN12345",
          "SerialNumber": "YL2E32010000",
          "SparePartNumber": ""
        }
      ],
      "Assemblies@odata.count": 5,
      "Id": "Assembly",
      "Name": "Assembly Collection"
    }
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I2d462340fe1a0b0eb387697f0ff70fcafde3f8d9
    Signed-off-by: Shounak Mitra <shounak@google.com>
    Signed-off-by: Gobinath Krishnamoorthy<gobinathk@google.com>

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -16,6 +16,7 @@
 #pragma once
 
 #include "account_service.hpp"
+#include "assembly.hpp"
 #include "bios.hpp"
 #include "cable.hpp"
 #include "certificate_service.hpp"
@@ -94,6 +95,7 @@ class RedfishService
         requestRoutesChassis(app);
         requestRoutesChassisResetAction(app);
         requestRoutesChassisResetActionInfo(app);
+        requestRoutesChassisAssembly(app);
         requestRoutesChassisDrive(app);
         requestRoutesChassisDriveName(app);
         requestRoutesUpdateService(app);
