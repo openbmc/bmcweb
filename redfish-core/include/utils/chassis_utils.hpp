287557c43ef98be315e59365af4e59659584a0b9
Shounak Mitra <shounak@google.com>
2022-03-31 14:01:06 -0700

    chassis_utils: Add chassis utils
    This is based on https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/39970/121/redfish-core/include/utils/chassis_utils.hpp
    Splitting the file out so it can be used for other changes without
    needing to depend on the entire PCIeSlots patch.
    
    Change-Id: I049fa74e647e6cca35c78e9710fbd6f9381b39f7
    Signed-off-by: Shounak Mitra <shounak@google.com>

---
+++ redfish-core/include/utils/chassis_utils.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <dbus_utility.hpp>
+
+namespace redfish
+{
+
+namespace chassis_utils
+{
+
+constexpr std::array<const char*, 2> dBusChassisInterfaces = {
+    "xyz.openbmc_project.Inventory.Item.Board",
+    "xyz.openbmc_project.Inventory.Item.Chassis",
+};
+
+/**
+ * @brief Retrieves valid chassis ID
+ * @param chassisId Chassis Id to search for
+ * @param callback  Callback for next step to get valid chassis ID
+ */
+template <typename Callback>
+void getValidChassisPath(const std::string& chassisID, Callback&& callback)
+{
+    auto respHandler =
+        [callback{std::forward<Callback>(callback)}, chassisID](
+            const boost::system::error_code ec,
+            const dbus::utility::MapperGetSubTreePathsResponse& chassisPaths) {
+        if (ec)
+        {
+            callback(ec, std::nullopt);
+            return;
+        }
+
+        for (const std::string& chassis : chassisPaths)
+        {
+            sdbusplus::message::object_path path(chassis);
+            std::string chassisName = path.filename();
+            if (chassisName.empty())
+            {
+                BMCWEB_LOG_ERROR << "Failed to find chassisName in " << chassis;
+                continue;
+            }
+            if (chassisName == chassisID)
+            {
+                callback(ec, path);
+                return;
+            }
+        }
+        callback(ec, std::nullopt);
+    };
+
+    // Get the Chassis Collection
+    crow::connections::systemBus->async_method_call(
+        respHandler, "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths",
+        "/xyz/openbmc_project/inventory", 0, dBusChassisInterfaces);
+}
+
+} // namespace chassis_utils
+} // namespace redfish
