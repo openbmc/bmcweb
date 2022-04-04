8f31b00d23a4316f3e4459abc61260936ee93da7
Sui Chen <suichen@google.com>
2022-04-04 11:21:54 -0700

    Add OpenBMC.1.0.ServiceStarted to registry and add a registry test
    This change adds OpenBMC.1.0.ServiceStarted to the registry, and adds a
    test case for getMessage and getMessageFromRegistry. The
    getMessageFromRegistry function is moved to a .cpp file so it can be
    called from the test.
    
    Unit test passes.
    
    When tested in QEMU: With
    https://gerrit.openbmc-project.xyz/c/openbmc/openbmc/+/52591 applied,
    restarting a service generates a corresponding log entry.
    
    bmc# systemctl restart rsyslogd
    
    host$ wget -q -O - \
    http://localhost:10080/redfish/v1/Systems/system/LogServices/EventLog/Entries/
    
    ...
    {
      "@odata.id": /redfish/v1/Systems/system/LogServices/EventLog/Entries/47441_1",
      "@odata.type": "#LogEntry.v1_8_0.LogEntry",
      "Created": "1970-01-01T13:10:41+00:00",
      "EntryType": "Event",
      "Id": "47441_1",
      "Message": "Service rsyslog.service has started successfully.",
      "MessageArgs": [
        "rsyslog.service"
      ],
      "MessageId": "OpenBMC.1.0.ServiceStarted",
      "Name": "System Event Log Entry",
      "Severity": "OK"
    },
    
    Signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: Iea6fa6e3ea8591853169043c1c04f5a7cf00b2f6

--- redfish-core/src/error_messages.cpp
+++ redfish-core/src/error_messages.cpp
@@ -15,7 +15,10 @@
 */
 #include "http_response.hpp"
 #include "registries/base_message_registry.hpp"
+#include "registries/openbmc_message_registry.hpp"
 
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include <boost/beast/http/status.hpp>
 #include <boost/url/url.hpp>
 #include <error_messages.hpp>
@@ -26,6 +29,59 @@
 
 namespace redfish
 {
+
+namespace registries
+{
+
+// Taken from log_services.hpp
+const Message*
+    getMessage(const std::string_view& messageID)
+{
+    // Redfish MessageIds are in the form
+    // RegistryName.MajorVersion.MinorVersion.MessageKey, so parse it to find
+    // the right Message
+    std::vector<std::string> fields;
+    fields.reserve(4);
+    boost::split(fields, messageID, boost::is_any_of("."));
+    std::string& registryName = fields[0];
+    std::string& messageKey = fields[3];
+
+    // Find the right registry and check it for the MessageKey
+    if (std::string(base::header.registryPrefix) ==
+        registryName)
+    {
+        return getMessageFromRegistry(
+            messageKey, std::span<const MessageEntry>(
+                            base::registry));
+    }
+    if (std::string(openbmc::header.registryPrefix) ==
+        registryName)
+    {
+        return getMessageFromRegistry(
+            messageKey, std::span<const MessageEntry>(
+                            openbmc::registry));
+    }
+    return nullptr;
+}
+
+const Message*
+    getMessageFromRegistry(const std::string& messageKey,
+                           const std::span<const MessageEntry> registry)
+{
+    std::span<const MessageEntry>::iterator messageIt = std::find_if(
+        registry.begin(), registry.end(),
+        [&messageKey](const MessageEntry& messageEntry) {
+            return std::strcmp(messageEntry.first, messageKey.c_str()) == 0;
+        });
+    if (messageIt != registry.end())
+    {
+        return &messageIt->second;
+    }
+
+    return nullptr;
+}
+
+} // namespace registries
 
 namespace messages
 {
--- redfish-core/ut/registries_test.cpp
+++ redfish-core/ut/registries_test.cpp
@@ -1,5 +1,11 @@
 #include "registries.hpp"
+#include "registries/base_message_registry.hpp"
 #include "registries/openbmc_message_registry.hpp"
+
+#include <string.h>
+
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 
 #include "gmock/gmock.h"
 
@@ -17,4 +23,20 @@ TEST(RedfishRegistries, fillMessageArgs)
     toFill = "%1, %2";
     fillMessageArgs({{"foo", "bar"}}, toFill);
     EXPECT_EQ(toFill, "foo, bar");
+}
+
+// Change 52351; the message shall pass the Validator
+TEST(RedfishRegistries, serviceStartedMessage)
+{
+    const redfish::registries::Message* msg = getMessageFromRegistry(
+        "Non-Existent", redfish::registries::openbmc::registry);
+    ASSERT_EQ(msg, nullptr);
+
+    const redfish::registries::Message* msg1 = getMessageFromRegistry(
+        "ServiceStarted", redfish::registries::openbmc::registry);
+    ASSERT_NE(msg1, nullptr);
+
+    const redfish::registries::Message* msg2 =
+        getMessage("OpenBMC.1.0.Non_Existent_Message");
+    ASSERT_EQ(msg2, nullptr);
 }
