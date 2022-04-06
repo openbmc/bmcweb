0918bffb4f27777dc38b56774d0cc14a6857f707
Heinz Boehmer Fiehn <heinzboehmer@google.com>
2022-04-06 16:37:32 -0700

    bmcweb: Add preliminary unit tests for log_services.hpp.
    This change adds an sdbus connection mock and tests for getDumpEntryCollection(), getDumpEntryById() and createDump(). These are all just preliminary additions and will get expanded upon in the future. The idea behind this change is to have a starting point for unit testing once the features described in the HW Fault Management design doc (https://gerrit.openbmc-project.xyz/c/openbmc/docs/+/46567) start to get checked in.
    
    Tested: Built and ran unit tests.
    Signed-off-by: Heinz Boehmer Fiehn <heinzboehmer@google.com>
    Change-Id: I7c1551027211cb8b97adf2d6b70ba936725df031

---
+++ redfish-core/lib/ut/sdbus_connection_mock.hpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <variant>
+
+#include "gmock/gmock.h"
+
+using handlerEcDumpId_t = std::function<void(const boost::system::error_code ec,
+                                             const uint32_t& dumpId)>&&;
+using handlerEcResp_t =
+    std::function<void(const boost::system::error_code ec,
+                       dbus::utility::ManagedObjectType& resp)>&&;
+
+class MockSdbusConnection
+{
+  public:
+    MOCK_METHOD(void, async_method_call,
+                (handlerEcResp_t handler, const std::string& service,
+                 const std::string& objpath, const std::string& interf,
+                 const std::string& method),
+                ());
+
+    MOCK_METHOD(void, async_method_call,
+                (handlerEcDumpId_t handler, const std::string& service,
+                 const std::string& objpath, const std::string& interf,
+                 const std::string& method),
+                ());
+};
