f88b21706f70ba70951c2876a7b41eb0ccaed361
Ed Tanous <edtanous@google.com>
2022-04-15 13:55:05 -0700

    Filter Expression parser
    This commit implements a parser for $filter expressions, per the redfish
    specification and odata specification.  This is intended to be used to
    support $filter query for collections.
    
    For parsing libraries, this commit chooses boost spirit x3.  It's chosen
    because it doesn't require a new external dependency, and is done
    entirely in the compiler, using C++ syntax.  While the syntax is still
    somewhat difficult to read, there's a slew of unit tests included to
    make sure that at least the common things we expect to work will parse
    correctly.
    
    Tested: Unit tests pass (good coverage).  Code not yet used.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I1b0ab615bc49064acab4dad47f0a8aa499557bfc

---
+++ redfish-core/src/filter_expr_printer.cpp
@@ -0,0 +1,148 @@
+#include "filter_expr_printer.hpp"
+
+#include "filter_expr_parser_ast.hpp"
+#include "filter_expr_parser_grammar.hpp"
+#include "logging.hpp"
+
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <optional>
+#include <string>
+
+namespace redfish
+{
+
+///////////////////////////////////////////////////////////////////////////
+//  The AST Printer
+//  Prints a $filter AST as a string to be compared, including explicit braces
+//  around all operations to help debugging AST issues.
+///////////////////////////////////////////////////////////////////////////
+
+using result_type = std::string;
+std::string FilterExpressionPrinter::operator()(double x) const
+{
+    return std::format("double({})", x);
+}
+std::string FilterExpressionPrinter::operator()(int64_t x) const
+{
+    return std::format("int({})", x);
+}
+std::string
+    FilterExpressionPrinter::operator()(const filter_ast::QuotedString& x) const
+{
+    return std::format("quoted_string(\"{}\")", static_cast<std::string>(x));
+}
+std::string FilterExpressionPrinter::operator()(
+    const filter_ast::UnquotedString& x) const
+{
+    return std::format("unquoted_string(\"{}\")", static_cast<std::string>(x));
+}
+
+std::string
+    FilterExpressionPrinter::operator()(const filter_ast::LogicalNot& x) const
+{
+    std::string prefix;
+    std::string postfix;
+    if (x.isLogicalNot)
+    {
+        prefix = "not(";
+        postfix = ")";
+    }
+    return std::format("{}{}{}", prefix, (*this)(x.operand), postfix);
+}
+std::string
+    FilterExpressionPrinter::operator()(const filter_ast::LogicalOr& x) const
+{
+    std::string prefix;
+    std::string postfix;
+    if (!x.rest.empty())
+    {
+        prefix = "(";
+        postfix = ")";
+    }
+    std::string out = std::format("{}{}{}", prefix, (*this)(x.first), postfix);
+
+    for (const filter_ast::LogicalNot& oper : x.rest)
+    {
+        out += std::format(" or ({})", (*this)(oper));
+    }
+    return out;
+}
+
+std::string
+    FilterExpressionPrinter::operator()(const filter_ast::LogicalAnd& x) const
+{
+    std::string prefix;
+    std::string postfix;
+    if (!x.rest.empty())
+    {
+        prefix = "(";
+        postfix = ")";
+    }
+    std::string out = std::format("{}{}{}", prefix, (*this)(x.first), postfix);
+
+    for (const filter_ast::LogicalOr& oper : x.rest)
+    {
+        out += std::format(" and ({})", (*this)(oper));
+    }
+    return out;
+}
+
+static std::string toString(filter_ast::ComparisonOpEnum rel)
+{
+    switch (rel)
+    {
+        case filter_ast::ComparisonOpEnum::GreaterThan:
+            return "Greater Than";
+        case filter_ast::ComparisonOpEnum::GreaterThanOrEqual:
+            return "Greater Than Or Equal";
+        case filter_ast::ComparisonOpEnum::LessThan:
+            return "Less Than";
+        case filter_ast::ComparisonOpEnum::LessThanOrEqual:
+            return "Less Than Or Equal";
+        case filter_ast::ComparisonOpEnum::Equals:
+            return "Equals";
+        case filter_ast::ComparisonOpEnum::NotEquals:
+            return "Not Equal";
+        default:
+            return "Invalid";
+    }
+}
+
+std::string
+    FilterExpressionPrinter::operator()(const filter_ast::Comparison& x) const
+{
+    std::string left = boost::apply_visitor(*this, x.left);
+    std::string right = boost::apply_visitor(*this, x.right);
+
+    return std::format("{} {} {}", left, toString(x.token), right);
+}
+
+std::string FilterExpressionPrinter::operator()(
+    const filter_ast::BooleanOp& operation) const
+{
+    return boost::apply_visitor(*this, operation);
+}
+
+std::optional<filter_grammar::program> parseFilter(std::string_view expr)
+{
+    const auto& grammar = filter_grammar::grammar;
+    filter_grammar::program program;
+
+    std::string_view::iterator iter = expr.begin();
+    const std::string_view::iterator end = expr.end();
+    BMCWEB_LOG_DEBUG("Parsing input string \"{}\"", expr);
+    bool r = boost::spirit::x3::parse(iter, end, grammar, program);
+
+    if (!r)
+    {
+        std::string rest(iter, end);
+
+        BMCWEB_LOG_ERROR("Parsing failed stopped at \"{}\"", rest);
+        return std::nullopt;
+    }
+    BMCWEB_LOG_DEBUG("Parsed AST: \"{}\"", FilterExpressionPrinter()(program));
+    return {std::move(program)};
+}
+} // namespace redfish
---
+++ test/redfish-core/include/filter_expr_parser_test.cpp
@@ -0,0 +1,122 @@
+#include "filter_expr_parser_ast.hpp"
+#include "filter_expr_printer.hpp"
+
+#include <optional>
+#include <string_view>
+
+#include "gmock/gmock.h"
+
+namespace redfish
+{
+
+static void parse(std::string_view filterExpression, std::string_view outputAst)
+{
+    std::optional<filter_ast::LogicalAnd> ast = parseFilter(filterExpression);
+    FilterExpressionPrinter pr;
+    EXPECT_TRUE(ast);
+    if (ast)
+    {
+        EXPECT_EQ(pr(*ast), outputAst);
+    }
+}
+
+TEST(FilterParser, SpecificationExamples)
+{
+    parse("ProcessorSummary/Count eq 2",
+          "unquoted_string(\"ProcessorSummary/Count\") Equals int(2)");
+    parse(
+        "ProcessorSummary/Count ge 2",
+        "unquoted_string(\"ProcessorSummary/Count\") Greater Than Or Equal int(2)");
+    parse("ProcessorSummary/Count gt 2",
+          "unquoted_string(\"ProcessorSummary/Count\") Greater Than int(2)");
+    parse(
+        "MemorySummary/TotalSystemMemoryGiB le 64",
+        "unquoted_string(\"MemorySummary/TotalSystemMemoryGiB\") Less Than Or Equal int(64)");
+    parse(
+        "MemorySummary/TotalSystemMemoryGiB lt 64",
+        "unquoted_string(\"MemorySummary/TotalSystemMemoryGiB\") Less Than int(64)");
+    parse(
+        "SystemType ne 'Physical'",
+        R"(unquoted_string("SystemType") Not Equal quoted_string("Physical"))");
+    parse(
+        "ProcessorSummary/Count eq 2 or ProcessorSummary/Count eq 4",
+        R"((unquoted_string("ProcessorSummary/Count") Equals int(2)) or (unquoted_string("ProcessorSummary/Count") Equals int(4)))");
+    parse("not ProcessorSummary/Count eq 2",
+          "not(unquoted_string(\"ProcessorSummary/Count\") Equals int(2))");
+    parse("not ProcessorSummary/Count eq -2",
+          "not(unquoted_string(\"ProcessorSummary/Count\") Equals int(-2))");
+    parse("Status/State eq 'Enabled')",
+          R"(unquoted_string("Status/State") Equals quoted_string("Enabled"))");
+    parse(
+        "ProcessorSummary/Count eq 2 and MemorySummary/TotalSystemMemoryGiB eq 64",
+        R"((unquoted_string("ProcessorSummary/Count") Equals int(2)) and (unquoted_string("MemorySummary/TotalSystemMemoryGiB") Equals int(64)))");
+    parse(
+        "Status/State eq 'Enabled' and Status/Health eq 'OK' or SystemType eq 'Physical'",
+        R"((unquoted_string("Status/State") Equals quoted_string("Enabled")) and ((unquoted_string("Status/Health") Equals quoted_string("OK")) or (unquoted_string("SystemType") Equals quoted_string("Physical"))))");
+    parse(
+        "(Status/State eq 'Enabled' and Status/Health eq 'OK') or SystemType eq 'Physical'",
+        R"(((unquoted_string("Status/State") Equals quoted_string("Enabled")) and (unquoted_string("Status/Health") Equals quoted_string("OK"))) or (unquoted_string("SystemType") Equals quoted_string("Physical")))");
+}
+
+TEST(FilterParser, BasicOperations)
+{
+    // Negation
+    EXPECT_TRUE(parseFilter("not(ProcessorSummary/Count eq 2)"));
+
+    // Negative numbers
+    EXPECT_TRUE(parseFilter("not(ProcessorSummary/Count eq -2)"));
+
+    // Empty strings
+    EXPECT_TRUE(parseFilter("(foo eq '')"));
+
+    // Identity
+    EXPECT_TRUE(parseFilter("1 eq 1"));
+    EXPECT_TRUE(parseFilter("'foo' eq 'foo'"));
+
+    // Inverted params
+    EXPECT_TRUE(parseFilter("2 eq ProcessorSummary/Count"));
+    EXPECT_TRUE(parseFilter("'OK' eq Status/Health"));
+
+    // Floating point
+    EXPECT_TRUE(parseFilter("Reading eq 4.0"));
+    EXPECT_TRUE(parseFilter("Reading eq 1e20"));
+    EXPECT_TRUE(parseFilter("Reading eq 1.575E1"));
+    EXPECT_TRUE(parseFilter("Reading eq -2.5E-3"));
+    EXPECT_TRUE(parseFilter("Reading eq 25E-4"));
+
+    // numeric operators
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count eq 2"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count ne 2"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count gt 2"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count ge 2"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count lt 2"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count le 2"));
+
+    // String comparison
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count eq 'foo'"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count ne 'foo'"));
+
+    // Future, datetime values are strings and need to be compared
+    // Make sure they parse
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count lt 'Physical'"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count le 'Physical'"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count gt 'Physical'"));
+    EXPECT_TRUE(parseFilter("ProcessorSummary/Count ge 'Physical'"));
+    EXPECT_TRUE(parseFilter("'Physical' lt ProcessorSummary/Count"));
+    EXPECT_TRUE(parseFilter("'Physical' le ProcessorSummary/Count"));
+    EXPECT_TRUE(parseFilter("'Physical' gt ProcessorSummary/Count"));
+    EXPECT_TRUE(parseFilter("'Physical' ge ProcessorSummary/Count"));
+}
+TEST(FilterParser, Failures)
+{
+    // Invalid expressions
+    EXPECT_FALSE(parseFilter("("));
+    EXPECT_FALSE(parseFilter(")"));
+    EXPECT_FALSE(parseFilter("()"));
+    EXPECT_FALSE(parseFilter(""));
+    EXPECT_FALSE(parseFilter(" "));
+    EXPECT_FALSE(parseFilter("ProcessorSummary/Count eq"));
+    EXPECT_FALSE(parseFilter("eq ProcessorSummary/Count"));
+    EXPECT_FALSE(parseFilter("not(ProcessorSummary/Count)"));
+}
+} // namespace redfish
