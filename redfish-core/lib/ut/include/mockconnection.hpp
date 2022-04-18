74e2abf6609d21a89a795cfdaf814e7f597926fe
John Edward Broadbent <jebr@google.com>
2022-04-18 16:37:48 -0700

    Add unit testing for cable
    Trying a different form of mocking
    
    Change-Id: I0d888f08df9555528256431ca20915be4345cb54
    Signed-off-by: John Edward Broadbent <jebr@google.com>

---
+++ redfish-core/lib/ut/include/mockconnection.hpp
@@ -0,0 +1,31 @@
+#include <boost/asio/io_context.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/message.hpp>
+
+#include <iostream>
+#include <string>
+
+#define MOCK_CONNECTION
+class MockConnection : public sdbusplus::asio::connection
+{
+  public:
+    MockConnection(boost::asio::io_context& io) :
+        sdbusplus::asio::connection(io)
+    {}
+
+    template <typename MessageHandler, typename... InputArgs>
+    void async_method_call(MessageHandler&& handler,
+                           const std::string& /*unused*/, // service,
+                           const std::string& /*unused*/, // objpath,
+                           const std::string& /*unused*/, // interf,
+                           const std::string& /*unused*/, // method,
+                           const InputArgs&... /*unused*/)
+    {
+
+        // std::tuple responseData;
+
+        std::apply(handler);
+
+        std::cerr << "in mock connnection FINDTEXT" << std::endl;
+    }
+};
