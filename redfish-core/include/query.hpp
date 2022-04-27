7f927f409ec4ff106d1b333cfd02ff3bdd82bcd8
Carson Labrado <clabrado@google.com>
2022-04-27 21:11:37 +0000

    WIP: bmcweb: Router to satellite resources
    This CL is a WIP and broken in its current state.  Do not trust the
    rest of this message!
    
    Adds ability to route requests to either native resources or
    resources that belong to a satellite BMC as part of Redfish
    Aggregation.  A prefix in the URI denotes if the resource is actually
    from a satellite BMC.  Prefixes are only used to denote satellite
    resources.  The URI of resources on the local/aggregating BMC will
    remain unchanged.
    
    Prefixes are separated from the resource ID by an underscore.  This
    means that underscores cannot be used in the prefix name itself.
    The prefixes used by satellite BMCs are revealed via D-Bus as well as
    the config information needed to connect to that BMC.
    
    Requests for satellite resources will be handled locally and also
    forwarded to the satellite.  The prefix should result in the local
    BMC wanting to return a 404 since that resource should not exist.
    That will get overridden by the satellite BMC's response to the
    forwarded message.
    
    Care should be taken to not name any local resources in a way that
    could cause a collision (e.g. having a Chassis object named
    "aggregated0_1U" on the aggregating BMC).  A future patch is planned
    which will prevent collisions by adding the prefix "main_" to local
    resource IDs.
    
    Note that URIs returned in the responses will not have the proper
    prefix included.  Fixing these URIs will be addressed in future
    patches.
    
    Example URIs w/o Redfish Aggregation:
    /redfish/v1/Chassis/1U/
    /redfish/v1/Systems/system/
    /redfish/v1/Managers/bmc/
    
    Example URIs after enabling Redfish Aggregation if the associated
    resources are located on the local/aggregating BMC:
    /redfish/v1/Chassis/1U/
    /redfish/v1/Systems/system/
    /redfish/v1/Managers/bmc/
    
    Example URIs if resources are instead located on a satellite BMC
    named "aggregated0":
    /redfish/v1/Chassis/aggregated0_1U/
    /redfish/v1/Systems/aggregated0_system/
    /redfish/v1/Managers/aggregated0_bmc/
    
    Tested:
    I was able to query supported resources located on the local BMC
    as well as on a satellite BMC.  Requests with unknown prefixes return
    a 404.
    
    Signed-off-by: Carson Labrado <clabrado@google.com>
    Change-Id: I87a3deb730bda95e72ecd3144ea40b0e5ee7d491

--- redfish-core/include/query.hpp
+++ redfish-core/include/query.hpp
@@ -4,6 +4,8 @@
 
 #include <bmcweb_config.h>
 
+#include <redfish_aggregator.hpp>
+
 namespace redfish
 {
 
@@ -12,10 +14,28 @@ namespace redfish
 // handled by redfish-core/lib codes, then default query parameter handler won't
 // process these parameters.
 [[nodiscard]] inline bool setUpRedfishRouteWithDelegation(
-    crow::App& app, const crow::Request& req, crow::Response& res,
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     query_param::Query& delegated,
     const query_param::QueryCapabilities& queryCapabilities)
 {
+#ifdef BMCWEB_ENABLE_REDFISH_AGGREGATION
+    // Asynchronously determine if this request needs to be routed to a
+    // satellite BMC.  We will locally process the request at the same time.
+    // If the request should be forwarded to a satellite, then local handling
+    // won't recognize the path due to the prefix and set res to return a 404.
+    // That error will get overwritten by the actual response from the
+    // satellite BMC.
+
+    // TODO: There is a chance of a race condition where the local BMC does
+    // not finish until after the response from the satellite BMC has already
+    // been processed.  In that instance, the valid response from the
+    // satellite would get clobbered by us locally writing a 404.  Future
+    // changes should be made to remove the possibility of this occurring.
+
+    RedfishAggregator::getInstance().beginAggregation(req, asyncResp);
+#endif
+
     BMCWEB_LOG_DEBUG << "setup redfish route";
 
     // Section 7.4 of the redfish spec "Redfish Services shall process the
@@ -25,14 +45,14 @@ namespace redfish
     std::string_view odataHeader = req.getHeaderValue("OData-Version");
     if (!odataHeader.empty() && odataHeader != "4.0")
     {
-        messages::preconditionFailed(res);
+        messages::preconditionFailed(asyncResp->res);
         return false;
     }
 
-    res.addHeader("OData-Version", "4.0");
+    asyncResp->res.addHeader("OData-Version", "4.0");
 
     std::optional<query_param::Query> queryOpt =
-        query_param::parseParameters(req.urlView.params(), res);
+        query_param::parseParameters(req.urlView.params(), asyncResp->res);
     if (queryOpt == std::nullopt)
     {
         return false;
@@ -46,8 +66,8 @@ namespace redfish
 
     delegated = query_param::delegate(queryCapabilities, *queryOpt);
     std::function<void(crow::Response&)> handler =
-        res.releaseCompleteRequestHandler();
-    res.setCompleteRequestHandler(
+        asyncResp->res.releaseCompleteRequestHandler();
+    asyncResp->res.setCompleteRequestHandler(
         [&app, handler(std::move(handler)),
          query{*queryOpt}](crow::Response& res) mutable {
             processAllParams(app, query, handler, res);
@@ -56,13 +76,13 @@ namespace redfish
 }
 
 // Sets up the Redfish Route. All parameters are handled by the default handler.
-[[nodiscard]] inline bool setUpRedfishRoute(crow::App& app,
-                                            const crow::Request& req,
-                                            crow::Response& res)
+[[nodiscard]] inline bool
+    setUpRedfishRoute(crow::App& app, const crow::Request& req,
+                      const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
 {
     // This route |delegated| is never used
     query_param::Query delegated;
-    return setUpRedfishRouteWithDelegation(app, req, res, delegated,
+    return setUpRedfishRouteWithDelegation(app, req, asyncResp, delegated,
                                            query_param::QueryCapabilities{});
 }
 } // namespace redfish
