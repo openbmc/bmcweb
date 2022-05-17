46f7a90148e04c9b0814c641dd5c8dc6dd5ce487
Sui Chen <suichen@google.com>
2022-05-17 14:49:28 -0700

    memory: Precisely match DIMM ID
    Currently, getDimmData uses std::string::find to determine if a DBus
    path contains a dimmId. The result is if we have the following paths:
    
    /xyz/openbmc_project/inventory/system/chassis/motherboard/dimm1
    /xyz/openbmc_project/inventory/system/chassis/motherboard/dimm10
    /xyz/openbmc_project/inventory/system/chassis/motherboard/dimm11
    ...
    /xyz/openbmc_project/inventory/system/chassis/motherboard/dimm19
    
    They will all be considered to match "dimm1".
    
    This fix makes sure numbers are not partially matched to result in false
    positives.
    
    Signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: I122774be1f81da6e5c3a833b83d2bd81b437c298

--- redfish-core/lib/memory.hpp
+++ redfish-core/lib/memory.hpp
@@ -801,6 +801,25 @@ inline void getDimmPartitionData(std::shared_ptr<bmcweb::AsyncResp> aResp,
         "xyz.openbmc_project.Inventory.Item.PersistentMemory.Partition");
 }
 
+// Fixes potential errors resulting from imprecise matching.
+// dimmId are in the format of "dimm0", "dimm1", etc.
+// Using a string::find would result in "dimm1" to be matched with "dimm10",
+// "dimm11", etc. This function prevents this problem from happening.
+inline bool containsDimmId(const std::string& dimmId, const std::string& path)
+{
+    size_t idx = path.find(dimmId);
+    if (idx == std::string::npos)
+        return false;
+    size_t idxOnePast = idx + dimmId.size(); // look 1 character past dimmId
+    if (idxOnePast < path.size())
+    {
+        const char c = path[idxOnePast];
+        if (c >= '0' && c <= '9')
+            return false; // the dimmId is imprecisely matched
+    }
+    return true;
+}
+
 inline void getDimmData(std::shared_ptr<bmcweb::AsyncResp> aResp,
                         const std::string& dimmId)
 {
@@ -819,7 +838,7 @@ inline void getDimmData(std::shared_ptr<bmcweb::AsyncResp> aResp,
             bool found = false;
             for (const auto& [path, object] : subtree)
             {
-                if (path.find(dimmId) != std::string::npos)
+                if (containsDimmId(dimmId, path))
                 {
                     for (const auto& [service, interfaces] : object)
                     {
