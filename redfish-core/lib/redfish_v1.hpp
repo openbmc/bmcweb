928166adff1835db69a0931dae5403fc12b0e1c8
Ed Tanous <edtanous@google.com>
2022-05-25 12:00:51 -0700

    Move the JsonSchemas to be generated at runtime
    Generating all of these index files at compile time is kind of wasteful,
    considering that it's really not that much code to generate them.  This
    commit modifies the update_schemas script to generate a C++ version of
    the schemas that can then be used to generate a route.  This allows
    deleting a LOT of fixed files, for a very small incremental binary size
    increase.
    
    This change will cause two impacts that a user could possibly notice.
    
    Expand will now work properly on JsonSchemas tree.
    Keys on the JsonSchema schemas will now be sorted alphabetically, as we
    do elsewhere.
    
    All other things should remain the same.
    
    Tested:
    Redfish service validator passes.
    
    curl --insecure --user root:0penBmc https://192.168.7.2/redfish/v1/JsonSchemas/Triggers
    Returns the same object as before with changes above.
    
    This adds 2512 bytes to the bmcweb binary size, and interestingly, adds
    3764 bytes to the overall rootfs size, despite the level of file
         deletion seen in the patch.  While this is debatably "worse" than
         what we had before in this regard, making JsonSchemas work similar
         to how the rest of the Redfish tree operates, is worth the minor
         increase in code size.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ic0d568112a3784821aa6867f7bcf50705dc586db

--- redfish-core/lib/redfish_v1.hpp
+++ redfish-core/lib/redfish_v1.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "error_messages.hpp"
+#include "utility.hpp"
+
 #include <app.hpp>
 #include <http_request.hpp>
 #include <http_response.hpp>
+#include <schemas.hpp>
 
 #include <string>
 
@@ -42,6 +46,82 @@ inline void redfish404(App& app, const crow::Request& req,
     messages::resourceNotFound(asyncResp->res, "", nameStr);
 }
 
+inline void
+    jsonSchemaIndexGet(App& app, const crow::Request& req,
+                       const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    nlohmann::json& json = asyncResp->res.jsonValue;
+    json["@odata.id"] = "/redfish/v1/JsonSchemas";
+    json["@odata.context"] =
+        "/redfish/v1/$metadata#JsonSchemaFileCollection.JsonSchemaFileCollection";
+    json["@odata.type"] = "#JsonSchemaFileCollection.JsonSchemaFileCollection";
+    json["Name"] = "JsonSchemaFile Collection";
+    json["Description"] = "Collection of JsonSchemaFiles";
+    nlohmann::json::array_t members;
+    for (const std::string_view schema : schemas)
+    {
+        nlohmann::json::object_t member;
+        member["@odata.id"] = crow::utility::urlFromPieces(
+            "redfish", "v1", "JsonSchemas", schema);
+        members.push_back(std::move(member));
+    }
+    json["Members"] = std::move(members);
+    json["Members@odata.count"] = schemas.size();
+}
+
+inline void jsonSchemaGet(App& app, const crow::Request& req,
+                          const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                          const std::string& schema)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    if (std::find(schemas.begin(), schemas.end(), schema) == schemas.end())
+    {
+        messages::resourceNotFound(asyncResp->res,
+                                   "JsonSchemaFile.JsonSchemaFile", schema);
+        return;
+    }
+
+    nlohmann::json& json = asyncResp->res.jsonValue;
+    json["@odata.context"] =
+        "/redfish/v1/$metadata#JsonSchemaFile.JsonSchemaFile";
+    json["@odata.id"] =
+        crow::utility::urlFromPieces("redfish", "v1", "JsonSchemas", schema);
+    json["@odata.type"] = "#JsonSchemaFile.v1_0_2.JsonSchemaFile";
+    json["Name"] = schema + " Schema File";
+    json["Description"] = schema + " Schema File Location";
+    json["Id"] = schema;
+    std::string schemaName = "#";
+    schemaName += schema;
+    schemaName += ".";
+    schemaName += schema;
+    json["Schema"] = std::move(schemaName);
+    constexpr std::array<std::string_view, 1> languages{"en"};
+    json["Languages"] = languages;
+    json["Languages@odata.count"] = languages.size();
+
+    nlohmann::json::array_t locationArray;
+    nlohmann::json::object_t locationEntry;
+    locationEntry["Language"] = "en";
+    locationEntry["PublicationUri"] =
+        "http://redfish.dmtf.org/schemas/v1/" + schema + ".json";
+    locationEntry["Uri"] = crow::utility::urlFromPieces(
+        "redfish", "v1", "JsonSchemas", schema, std::string(schema) + ".json");
+
+    locationArray.emplace_back(locationEntry);
+
+    json["Location"] = std::move(locationArray);
+    json["Location@odata.count"] = 1;
+>>>>>>> 12ea0a5c (Move the JsonSchemas to be generated at runtime)
+}
+
 inline void requestRoutesRedfish(App& app)
 {
     BMCWEB_ROUTE(app, "/redfish/")
@@ -50,6 +130,14 @@ inline void requestRoutesRedfish(App& app)
 
     BMCWEB_ROUTE(app, "/redfish/<path>")
     (std::bind_front(redfish404, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/JsonSchemas/<str>/")
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(jsonSchemaGet, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/JsonSchemas/")
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(jsonSchemaIndexGet, std::ref(app)));
 }
 
 } // namespace redfish
