c733d9cd70ef419c032f81fae4038c582c677bad
Ed Tanous <edtanous@google.com>
2022-05-25 17:30:29 -0700

    Generate schema versions
    One problem that comes up quite often is folks forgetting to update the
    schema versions in the odata.type tags for every resource.  This can
    cause service validator failures of varying degrees.
    
    This commit attempts to automate the generation of schemas by making the
    update_schemas.py script generate a constexpr struct of the latest
    versions of schemas and the latest version namespace.  This allows this
    object to be used in lieu of hardcoding the value, hopefully reducing
    the chance of getting it incorrect.
    
    As a consequence of this, all schema files are moved to the latest
    version of the schema.  This is done in a few examples in this commit,
    and more completely in the following commit, to split up the work some,
    but given they're both related to $metadata, they will need to merge
    together.
    
    Impacts to users:
    1. The /redfish/v1/$metadata endpoint now only returns the latest
       version of a given schema namespace.  As a side benefit, this greatly
       reduces the test setup time when running redfish-service-validator.
    2. All schemas now return the latest version, regardless of which
       parameters they use.
    
    Tested (with next commit added):
    Redfish-Service-Validator passes
    GET /redfish/v1/$metadata returns a valid XML document, with the latest
    versions.  When piped to xmlint, it agrees the document is valid.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I2d660de0acdbca24b553923886a71c784caf8acb

---
+++ redfish-core/include/schema_common.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+struct SchemaVersion
+{
+    const std::string_view name;
+    uint8_t versionMajor;
+    uint8_t versionMinor;
+    uint8_t versionPatch;
+
+    std::string namespaceString() const
+    {
+        std::string ret;
+        ret += name;
+        // All zero versions mean the schema is unversioned
+        if (versionMajor != 0 || versionMinor != 0 || versionPatch != 0)
+        {
+            ret += '.';
+            ret += 'v';
+            ret += std::to_string(versionMajor);
+            ret += '_';
+            ret += std::to_string(versionMinor);
+            ret += '_';
+            ret += std::to_string(versionPatch);
+        }
+        return ret;
+    }
+
+    std::string toString() const
+    {
+        std::string ret;
+        ret += '#';
+        ret += namespaceString();
+        ret += '.';
+        ret += name;
+        return ret;
+    }
+};
+
+namespace nlohmann
+{
+template <>
+struct adl_serializer<SchemaVersion>
+{
+    // nlohmann requires a specific casing to look these up in adl
+    // NOLINTNEXTLINE(readability-identifier-naming)
+    static void to_json(json& j, const SchemaVersion& schema)
+    {
+        j = schema.toString();
+    }
+};
+} // namespace nlohmann
