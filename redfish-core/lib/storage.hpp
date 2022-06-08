e4ba93a21993de8fb03c84418b9de233b9eddff3
John Edward Broadbent <jebr@google.com>
2022-06-08 14:35:21 -0700

    redfish: Add EncryptionStatus to Drive
    This change takes the locked, and EncryptionStatus properties from
    Drives interfaces, and creates the redrish property EncryptionStatus.
    
    Tested:
    $ wget -qO- http://localhost:80/redfish/v1/Chassis/AgoraV2/Drives/mmcblk0
    {
      "@odata.context": "/redfish/v1/$metadata#Drive.Drive",
      "@odata.id": "/redfish/v1/Chassis/Drives/mmcblk0",
      "@odata.type": "#Drive.v1_7_0.Drive",
      "CapacityBytes": 15634268160,
      "EncryptionStatus": "Unencrypted",
      "Id": "mmcblk0",
      "Links": {
        "Chassis": "Enabled"
      },
      "Name": "mmcblk0",
      "PredictedMediaLifeLeftPercent": 100,
      "Status": {
        "State": "Enabled"
      }
    }
    
    Change-Id: Ic7b58614466535b3fd6b8c097050d3e9c8de8203
    Signed-off-by: John Edward Broadbent <jebr@google.com>

--- redfish-core/lib/storage.hpp
+++ redfish-core/lib/storage.hpp
@@ -401,6 +401,8 @@ inline void
             // this interface isn't required
             return;
         }
+        const std::string* encryptionStatus = nullptr;
+        const bool* isLocked = nullptr;
         for (const std::pair<std::string, dbus::utility::DbusVariantType>&
                  property : propertiesList)
         {
@@ -467,6 +469,71 @@ inline void
                 }
                 asyncResp->res.jsonValue["Protocol"] = *proto;
             }
+            else if (propertyName == "PredictedMediaLifeLeftPercent")
+            {
+                const uint8_t* lifeLeft =
+                    std::get_if<uint8_t>(&property.second);
+                if (lifeLeft == nullptr)
+                {
+                    BMCWEB_LOG_ERROR
+                        << "Illegal property: PredictedMediaLifeLeftPercent";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                // 255 means reading the value is not supported
+                if (*lifeLeft != 255)
+                {
+                    asyncResp->res.jsonValue["PredictedMediaLifeLeftPercent"] =
+                        *lifeLeft;
+                }
+            }
+            else if (propertyName == "EncryptionStatus")
+            {
+                encryptionStatus = std::get_if<std::string>(&property.second);
+                if (encryptionStatus == nullptr)
+                {
+                    BMCWEB_LOG_ERROR << "Illegal property: EncryptionStatus";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+            }
+            else if (propertyName == "Locked")
+            {
+                isLocked = std::get_if<bool>(&property.second);
+                if (isLocked == nullptr)
+                {
+                    BMCWEB_LOG_ERROR << "Illegal property: Locked";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+            }
+        }
+
+        if (encryptionStatus == nullptr || isLocked == nullptr)
+        {
+            asyncResp->res.jsonValue["EncryptionStatus"] = "Unknown";
+            return;
+        }
+        if (*encryptionStatus !=
+            "xyz.openbmc_project.Drive.DriveEncryptionState.Encrypted")
+        {
+            //"The drive is not currently encrypted."
+            asyncResp->res.jsonValue["EncryptionStatus"] = "Unencrypted";
+            return;
+        }
+        if (*isLocked)
+        {
+            //"The drive is currently encrypted and the data is not
+            // accessible to the user."
+            asyncResp->res.jsonValue["EncryptionStatus"] = "Locked";
+            return;
+        }
+        if (!*isLocked)
+        {
+            // "The drive is currently encrypted but the data is accessible
+            // to the user in unencrypted form."
+            asyncResp->res.jsonValue["EncryptionStatus"] = "Unlocked";
+            return;
         }
         });
 }
