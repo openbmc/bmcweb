16bd58a96c9f60778066eef97839216333d1d3a2
John Edward Broadbent <jebr@google.com>
2022-06-08 14:35:21 -0700

    redfish: Add EncryptionStatus to Drive
    This change takes the locked, and EncryptionStatus properties from
    Drives interfaces, and creates the redrish property EncryptionStatus.
    
    Tested:
    Redfish Validator did not show any errors from this change.
    
    $ wget -qO- http://localhost:80/redfish/v1/Chassis/AgoraV2/Drives/mmcblk0
    {
      "@odata.context": "/redfish/v1/$metadata#Drive.Drive",
      "@odata.id": "/redfish/v1/Chassis/Drives/mmcblk0",
      "@odata.type": "#Drive.v1_7_0.Drive",
      "CapacityBytes": 15634268160,
      "EncryptionStatus": "Unencrypted",
      "Id": "mmcblk0",
      "Links": {
        "Chassis": "Enabled"
      },
      "Name": "mmcblk0",
      "PredictedMediaLifeLeftPercent": 100,
      "Status": {
        "State": "Enabled"
      }
    }
    
    Change-Id: Ic7b58614466535b3fd6b8c097050d3e9c8de8203
    Signed-off-by: John Edward Broadbent <jebr@google.com>

--- redfish-core/lib/storage.hpp
+++ redfish-core/lib/storage.hpp
@@ -401,6 +401,8 @@ inline void
             // this interface isn't required
             return;
         }
+        const std::string* encryptionStatus = nullptr;
+        const bool* isLocked = nullptr;
         for (const std::pair<std::string, dbus::utility::DbusVariantType>&
                  property : propertiesList)
         {
@@ -466,6 +468,77 @@ inline void
                     return;
                 }
                 asyncResp->res.jsonValue["Protocol"] = *proto;
+            }
+            else if (propertyName == "PredictedMediaLifeLeftPercent")
+            {
+                const uint8_t* lifeLeft =
+                    std::get_if<uint8_t>(&property.second);
+                if (lifeLeft == nullptr)
+                {
+                    BMCWEB_LOG_ERROR
+                        << "Illegal property: PredictedMediaLifeLeftPercent";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                // 255 means reading the value is not supported
+                if (*lifeLeft != 255)
+                {
+                    asyncResp->res.jsonValue["PredictedMediaLifeLeftPercent"] =
+                        *lifeLeft;
+                }
+            }
+            else if (propertyName == "EncryptionStatus")
+            {
+                encryptionStatus = std::get_if<std::string>(&property.second);
+                if (encryptionStatus == nullptr)
+                {
+                    BMCWEB_LOG_ERROR << "Illegal property: EncryptionStatus";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+            }
+            else if (propertyName == "Locked")
+            {
+                isLocked = std::get_if<bool>(&property.second);
+                if (isLocked == nullptr)
+                {
+                    BMCWEB_LOG_ERROR << "Illegal property: Locked";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+            }
+        }
+        if (encryptionStatus == nullptr)
+        {
+            asyncResp->res.jsonValue["EncryptionStatus"] =
+                "xyz.openbmc_project.Drive.DriveEncryptionState.Unknown";
+        }
+        else
+        {
+            if (*encryptionStatus ==
+                "xyz.openbmc_project.Drive.DriveEncryptionState.Encrypted")
+            {
+                if (isLocked == nullptr)
+                {
+                    asyncResp->res.jsonValue["EncryptionStatus"] =
+                        "Unencrypted";
+                }
+                else
+                {
+                    if (*isLocked)
+                    {
+                        asyncResp->res.jsonValue["EncryptionStatus"] = "Locked";
+                    }
+                    else
+                    {
+                        asyncResp->res.jsonValue["EncryptionStatus"] =
+                            "Unlocked";
+                    }
+                }
+            }
+            else
+            {
+                asyncResp->res.jsonValue["EncryptionStatus"] = "Unencrypted";
             }
         }
         });
