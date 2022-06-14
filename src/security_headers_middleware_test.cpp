4c97f2d8532b7d9d79e2c47dc130e22fc9c60f06
Nan Zhou <nanzhoumails@gmail.com>
2022-06-14 17:04:17 +0000

    security_headers_test: revive test
    The test today exists but it isn't enabled. This commit revives the
    test, and fixed obsolete interfaces (App, boost, etc).
    
    To make test pass, this commit also updates the expectation of these
    security headers and fixed includes.
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I6217346b2aeb90b572d72b8293c4f754d6ceed4b

--- src/security_headers_middleware_test.cpp
+++ src/security_headers_middleware_test.cpp
@@ -1,5 +1,10 @@
-#include <app.hpp>
-#include <security_headers_middleware.hpp>
+#include <http/ut/app_in_test.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/address.hpp>
+#include <boost/beast/http/status.hpp>
+#include <logging.hpp>
+
+#include <future>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -10,45 +15,56 @@ using namespace std;
 // Tests that the security headers are added correctly
 TEST(SecurityHeaders, TestHeadersExist)
 {
-    App<SecurityHeadersMiddleware> app;
+    auto appIOContext = std::make_shared<boost::asio::io_context>();
+    App app(appIOContext);
     app.bindaddr("127.0.0.1").port(45451);
     BMCWEB_ROUTE(app, "/")([]() { return boost::beast::http::status::ok; });
-    auto _ = async(launch::async, [&] { app.run(); });
+    auto _ = async(std::launch::async, [&app, appIOContext] {
+        app.run();
+        appIOContext->run();
+    });
 
-    asio::io_context is;
+    boost::asio::io_context is;
     std::array<char, 2048> buf;
-    std::string sendmsg;
+    std::string sendMsg;
 
     {
         // Retry a couple of times waiting for the server to come up
         // TODO(ed)  This is really unfortunate, and should use some form of
         // mock
-        asio::ip::tcp::socket c(is);
+        boost::asio::ip::tcp::socket c(is);
         for (int i = 0; i < 200; i++)
         {
             try
             {
-                c.connect(asio::ip::tcp::endpoint(
-                    asio::ip::address::from_string("127.0.0.1"), 45451));
+                c.connect(boost::asio::ip::tcp::endpoint(
+                    boost::asio::ip::make_address("127.0.0.1"), 45451));
                 c.close();
+                BMCWEB_LOG_DEBUG << "Server is up; iterations=" << i;
                 break;
             }
             catch (const std::exception& e)
             {
                 // do nothing.  We expect this to fail while the server is
                 // starting up
+                BMCWEB_LOG_DEBUG << "Server isn't up yet; iterations=" << i;
+                sleep(1);
             }
         }
     }
 
     // Test correct login credentials
-    sendmsg = "GET /\r\n\r\n";
+    sendMsg = ("GET / HTTP/1.1\r\n"
+               "Host: localhost:45451\r\n"
+               "\r\n");
 
-    asio::ip::tcp::socket c(is);
-    c.connect(asio::ip::tcp::endpoint(
-        asio::ip::address::from_string("127.0.0.1"), 45451));
-    c.send(asio::buffer(sendmsg));
-    c.receive(asio::buffer(buf));
+    boost::asio::ip::tcp::socket c(is);
+    c.connect(boost::asio::ip::tcp::endpoint(
+        boost::asio::ip::make_address("127.0.0.1"), 45451));
+    c.send(boost::asio::buffer(sendMsg));
+    BMCWEB_LOG_DEBUG << "Sent the request";
+    c.receive(boost::asio::buffer(buf));
+    BMCWEB_LOG_DEBUG << "Received the request";
     c.close();
     auto return_code = std::string(&buf[9], &buf[12]);
     EXPECT_EQ("200", return_code);
@@ -72,14 +88,24 @@ TEST(SecurityHeaders, TestHeadersExist)
     headers.push_back(response.substr(prev));
 
     EXPECT_EQ(headers[0], "HTTP/1.1 200 OK");
-    EXPECT_THAT(headers, ::testing::Contains("Strict-Transport-Security: "
-                                             "max-age=31536000; "
-                                             "includeSubdomains; preload"));
-    EXPECT_THAT(headers, ::testing::Contains("X-UA-Compatible: IE=11"));
-    EXPECT_THAT(headers, ::testing::Contains("X-Frame-Options: DENY"));
-    EXPECT_THAT(headers,
-                ::testing::Contains("X-XSS-Protection: 1; mode=block"));
-    EXPECT_THAT(headers, ::testing::Contains(
-                             "X-Content-Security-Policy: default-src 'self'"));
+    EXPECT_THAT(headers, testing::Contains("Strict-Transport-Security: "
+                                           "max-age=31536000; "
+                                           "includeSubdomains; preload"));
+    EXPECT_THAT(headers, testing::Contains("X-Frame-Options: DENY"));
+    EXPECT_THAT(headers, testing::Contains("Pragma: no-cache"));
+    EXPECT_THAT(headers, testing::Contains("Cache-Control: no-Store,no-Cache"));
+    EXPECT_THAT(headers, testing::Contains("X-XSS-Protection: 1; mode=block"));
+    EXPECT_THAT(headers, testing::Contains("X-Content-Type-Options: nosniff"));
+    EXPECT_THAT(headers, testing::Contains(
+                             "Content-Security-Policy: default-src 'none'; "
+                             "img-src 'self' data:; "
+                             "font-src 'self'; "
+                             "style-src 'self'; "
+                             "script-src 'self'; "
+                             "connect-src 'self' wss:; "
+                             "form-action 'none'; "
+                             "frame-ancestors 'none'; "
+                             "object-src 'none'; "
+                             "base-uri 'none'"));
     app.stop();
 }
