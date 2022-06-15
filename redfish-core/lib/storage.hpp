148d4431bcf4e9924c34a80d432ed526b1de0895
Nan Zhou <nanzhoumails@gmail.com>
2022-06-15 05:12:24 +0000

    storage: simplify dbus call types
    |connectionNames| can use MapperServiceMap, which is defined in
    dbus_utility.hpp for GetSubTree response.
    
    This commit also uses Structured binding declaration, which simplifies a
    lot of for loops.
    
    Tested: code compiles. Strong type guarantees the correctness.
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I50601e7551b0de49c2891b81789e97550bead6e3

--- redfish-core/lib/storage.hpp
+++ redfish-core/lib/storage.hpp
@@ -537,8 +537,8 @@ inline void requestRoutesDrive(App& app)
             }
 
             const std::string& path = drive->first;
-            const std::vector<std::pair<std::string, std::vector<std::string>>>&
-                connectionNames = drive->second;
+            const dbus::utility::MapperServiceMap& connectionNames =
+                drive->second;
 
             asyncResp->res.jsonValue["@odata.type"] = "#Drive.v1_7_0.Drive";
             asyncResp->res.jsonValue["@odata.id"] =
@@ -612,15 +612,8 @@ void chassisDriveCollectionGet(
         }
 
         // Iterate over all retrieved ObjectPaths.
-        for (const std::pair<
-                 std::string,
-                 std::vector<std::pair<std::string, std::vector<std::string>>>>&
-                 object : subtree)
+        for (const auto& [path, connectionNames] : subtree)
         {
-            const std::string& path = object.first;
-            const dbus::utility::MapperGetObject& connectionNames =
-                object.second;
-
             sdbusplus::message::object_path objPath(path);
             if (objPath.filename() != chassisId)
             {
@@ -710,15 +703,8 @@ void buildDrive(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     }
 
     // Iterate over all retrieved ObjectPaths.
-    for (const std::pair<
-             std::string,
-             std::vector<std::pair<std::string, std::vector<std::string>>>>&
-             object : subtree)
+    for (const auto& [path, connectionNames] : subtree)
     {
-        const std::string& path = object.first;
-        const std::vector<std::pair<std::string, std::vector<std::string>>>&
-            connectionNames = object.second;
-
         sdbusplus::message::object_path objPath(path);
         if (objPath.filename() != driveName)
         {
@@ -806,15 +792,8 @@ void handleChassisDriveGet(crow::App& app, const crow::Request& req,
         }
 
         // Iterate over all retrieved ObjectPaths.
-        for (const std::pair<
-                 std::string,
-                 std::vector<std::pair<std::string, std::vector<std::string>>>>&
-                 object : subtree)
+        for (const auto& [path, connectionNames] : subtree)
         {
-            const std::string& path = object.first;
-            const std::vector<std::pair<std::string, std::vector<std::string>>>&
-                connectionNames = object.second;
-
             sdbusplus::message::object_path objPath(path);
             if (objPath.filename() != chassisId)
             {
