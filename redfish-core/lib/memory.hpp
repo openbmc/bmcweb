d37c07683aa6eeb0b1ae5dfad24ce3605724743e
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-06-20 12:32:22 +0800

    memory: Use dbus::utility::DBusPropertiesMap::value_type
    Both dimmPropToHex() and getPersistentMemoryProperties() uses a custom
    type for DBusPropertiesMap entry, use the value_type instead.
    
    Tested:
    Build pass.
    
    Change-Id: Iffce0bec160d1bae684164db13994bf34ef8594f
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- redfish-core/lib/memory.hpp
+++ redfish-core/lib/memory.hpp
@@ -139,10 +139,11 @@ inline std::string translateMemoryTypeToRedfish(const std::string& memoryType)
     return "";
 }
 
-inline void dimmPropToHex(
-    const std::shared_ptr<bmcweb::AsyncResp>& aResp, const char* key,
-    const std::pair<std::string, dbus::utility::DbusVariantType>& property,
-    const nlohmann::json::json_pointer& jsonPtr)
+inline void
+    dimmPropToHex(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                  const char* key,
+                  const dbus::utility::DBusPropertiesMap::value_type& property,
+                  const nlohmann::json::json_pointer& jsonPtr)
 {
     const uint16_t* value = std::get_if<uint16_t>(&property.second);
     if (value == nullptr)
@@ -156,7 +157,7 @@ inline void dimmPropToHex(
 
 inline void getPersistentMemoryProperties(
     const std::shared_ptr<bmcweb::AsyncResp>& aResp,
-    const std::pair<std::string, dbus::utility::DbusVariantType>& property,
+    const dbus::utility::DBusPropertiesMap::value_type& property,
     const nlohmann::json::json_pointer& jsonPtr)
 {
     if (property.first == "ModuleManufacturerID")
