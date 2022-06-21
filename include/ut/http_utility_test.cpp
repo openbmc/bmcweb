2acea66587d11327ad19916ef7f76858900f35b0
Nan Zhou <nanzhoumails@gmail.com>
2022-06-21 17:46:56 +0000

    http_utility_test: fix test case names
    This commit applies the GTest test case naming convention: Camel case,
    use decriptive Test names.
    
    It also groups test cases according to the name.
    
    Reference:
    https://www.google.com/search?q=Writing+Descriptive+Test+Names+Google+test&oq=Writing+Descriptive+Test+Names+Google+test&aqs=chrome..69i57j33i160l2.7607j0j7&sourceid=chrome&ie=UTF-8
    
    Tested: unit test passes.
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: Ie008ceac9e027b518e660745722ac5cc0af502da

--- include/ut/http_utility_test.cpp
+++ include/ut/http_utility_test.cpp
@@ -7,15 +7,25 @@ namespace http_helpers
 namespace
 {
 
-TEST(HttpUtility, requestPrefersHtml)
+TEST(RequestPrefersHtml, ContainsHtmlReturnsTrue)
+{
+    EXPECT_TRUE(requestPrefersHtml("text/html, application/json"));
+}
+
+TEST(RequestPrefersHtml, NoHtmlReturnsFalse)
 {
     EXPECT_FALSE(requestPrefersHtml("*/*, application/octet-stream"));
-    EXPECT_TRUE(isOctetAccepted("*/*, application/octet-stream"));
-
-    EXPECT_TRUE(requestPrefersHtml("text/html, application/json"));
-    EXPECT_FALSE(isOctetAccepted("text/html, application/json"));
-
     EXPECT_FALSE(requestPrefersHtml("application/json"));
+}
+
+TEST(IsOctetAccepted, ContainsOctetReturnsTrue)
+{
+    EXPECT_TRUE(isOctetAccepted("*/*, application/octet-stream"));
+}
+
+TEST(IsOctetAccepted, NoOctetReturnsFalse)
+{
+    EXPECT_FALSE(isOctetAccepted("text/html, application/json"));
     EXPECT_FALSE(isOctetAccepted("application/json"));
 }
 
