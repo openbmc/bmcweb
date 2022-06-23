f7ea8ca3d621e65292a35ffa0335a9cb2d39c2de
Nan Zhou <nanzhoumails@gmail.com>
2022-06-23 22:28:25 +0000

    dbus_singleton: use static local variable
    Currently, the |systemBus| connection is a static variable declared in
    headers. This has a problem that every translation unit will keep its
    own copy. It's not a problem today because there's only one translation
    unit "webserver_main.cpp.o". This issue was brounght up in
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/54758
    
    This commits implements a "textbook" singleton based on static local
    variables. It is based on the fact that static local variables
    in function will only be initialized once at the first time the code
    is executed. It's even thread-safe (though we only have one thread in
    this project).
    
    Reference:
    1. thread safety
    https://stackoverflow.com/questions/449436/singleton-instance-declared-as-static-variable-of-getinstance-method-is-it-thre/449823#449823
    2. singleton
    https://stackoverflow.com/questions/1008019/c-singleton-design-pattern
    3. Meyer’s Singleton
    https://laristra.github.io/flecsi/src/developer-guide/patterns/meyers_singleton.html
    4. Extern
    https://stackoverflow.com/questions/1433204/how-do-i-use-extern-to-share-variables-between-source-files
    
    Tested:
    1. Romulus QEMU robot Redfish test passed;
    2. Start and restart service on real hardware, no issues;
    3. No new validator failures
    4. Code compies
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I03b387bd5f218a86c9d1765415a46e3c2ad83ff9

--- include/dbus_singleton.hpp
+++ include/dbus_singleton.hpp
@@ -1,11 +1,29 @@
 #pragma once
+#include <boost/asio/io_context.hpp>
 #include <sdbusplus/asio/connection.hpp>
 
 namespace crow
 {
 namespace connections
 {
-static std::shared_ptr<sdbusplus::asio::connection> systemBus;
+
+// Kept for backward compatibility. Initialze before using!
+// Please see webserver_main for the example how this variable is initialzed,
+extern sdbusplus::asio::connection* systemBus;
+
+// A wrapper around sdbusplus::asio::connection.
+// Typical usage:
+//  DBusSingleton::initialize(io);
+//  DBusSingleton::systemBus()->async_method_call(...);
+class DBusSingleton
+{
+  public:
+    static void initialize(boost::asio::io_context& io);
+    static sdbusplus::asio::connection& systemBus();
+
+  private:
+    static sdbusplus::asio::connection& systemBus(boost::asio::io_context* io);
+};
 
 } // namespace connections
 } // namespace crow
