f783773b78762594d018239b30ec5ee27b1e2ade
Nan Zhou <nanzhoumails@gmail.com>
2022-06-23 22:28:25 +0000

    dbus_singleton: use stack variable and extern
    Currently, the |systemBus| connection is a static variable declared in
    headers. This has a problem that every translation unit will keep its
    own copy. It's not a problem today because there's only one translation
    unit "webserver_main.cpp.o". This issue was brounght up in
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/54758
    
    Actually, the |systemBus| doesn't need to be a singleton. It can just be
    a stack variable, which is normally more efficient than heap variables.
    
    To keep minimum changes treeside, this commits keeps the existing
    |systemBus| variable as an external variable. It is defined in its own
    translation unit. It is initialized in the main translation unit.
    
    Reference:
    1. Extern
    https://stackoverflow.com/questions/1433204/how-do-i-use-extern-to-share-variables-between-source-files
    
    Tested:
    1. Romulus QEMU robot Redfish test passed;
    2. Start and restart service on real hardware, no issues;
    3. No new validator failures
    4. Code compies
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I03b387bd5f218a86c9d1765415a46e3c2ad83ff9

--- include/dbus_singleton.hpp
+++ include/dbus_singleton.hpp
@@ -1,11 +1,30 @@
 #pragma once
+#include <boost/asio/io_context.hpp>
 #include <sdbusplus/asio/connection.hpp>
+
+#include <memory>
 
 namespace crow
 {
 namespace connections
 {
-static std::shared_ptr<sdbusplus::asio::connection> systemBus;
+
+// Initialze before using!
+// Please see webserver_main for the example how this variable is initialzed,
+extern sdbusplus::asio::connection* systemBus;
+
+class DBusConnection
+{
+  public:
+    explicit DBusConnection(
+        const std::shared_ptr<boost::asio::io_context>& ioIn);
+    sdbusplus::asio::connection* getSystemBus();
+
+  private:
+    // Note, |io| shall be destructed after |systemBus|
+    std::shared_ptr<boost::asio::io_context> io;
+    sdbusplus::asio::connection systemBus;
+};
 
 } // namespace connections
 } // namespace crow
