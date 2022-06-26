74301778db123df4d74b1b8742374439aed5b3fa
Nan Zhou <nanzhoumails@gmail.com>
2022-06-26 23:55:13 +0000

    http: router_test: fix namespace
    Add using directives and namespaces.
    
    Teseted: unit test passed.
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I4c04f07484a2c1ce29ae26b523d62071ea03996c

--- http/ut/router_test.cpp
+++ http/ut/router_test.cpp
@@ -12,34 +12,39 @@
 
 #include <gtest/gtest.h>
 
+namespace crow
+{
+namespace
+{
+
+using ::crow::black_magic::getParameterTag;
+
 TEST(Router, AllowHeader)
 {
     // Callback handler that does nothing
-    auto nullCallback = [](const crow::Request&,
+    auto nullCallback = [](const Request&,
                            const std::shared_ptr<bmcweb::AsyncResp>&) {};
 
-    crow::Router router;
+    Router router;
     std::error_code ec;
 
     constexpr const std::string_view url = "/foo";
 
-    crow::Request req{{boost::beast::http::verb::get, url, 11}, ec};
+    Request req{{boost::beast::http::verb::get, url, 11}, ec};
 
     // No route should return no methods.
     router.validate();
     EXPECT_EQ(router.buildAllowHeader(req), "");
 
-    router
-        .newRuleTagged<crow::black_magic::getParameterTag(url)>(
-            std::string(url))
+    router.newRuleTagged<getParameterTag(url)>(std::string(url))
         .methods(boost::beast::http::verb::get)(nullCallback);
     router.validate();
     EXPECT_EQ(router.buildAllowHeader(req), "GET");
 
-    router
-        .newRuleTagged<crow::black_magic::getParameterTag(url)>(
-            std::string(url))
+    router.newRuleTagged<getParameterTag(url)>(std::string(url))
         .methods(boost::beast::http::verb::patch)(nullCallback);
     router.validate();
     EXPECT_EQ(router.buildAllowHeader(req), "GET, PATCH");
 }
+} // namespace
+} // namespace crow
