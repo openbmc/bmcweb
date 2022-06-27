cc7810e0d0a54b44b33ca462d9b1eb67f9cc66e5
Ed Tanous <edtanous@google.com>
2022-06-27 11:11:53 -0700

    Move duplicate reduction
    getEthernetIfaceData callback should ideally be returning exactly the
    values the system has, minus duplicates.  In every case this function is
    used, we don't want duplicates, so move where we check for duplicates.
    
    Tested: In conjunction with
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/54829 test cases pass.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ib202db99f70e1a6fdddf18969e15b2382e287848

--- redfish-core/lib/network_protocol.hpp
+++ redfish-core/lib/network_protocol.hpp
@@ -78,6 +78,7 @@ inline void extractNTPServersAndDomainNamesData(
             }
         }
     }
+    stl_utils::removeDuplicate(ntpData);
 }
 
 template <typename CallbackFunc>
@@ -141,7 +142,6 @@ inline void getNetworkData(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                                        "NetworkProtocol");
             return;
         }
-        stl_utils::removeDuplicate(ntpServers);
         asyncResp->res.jsonValue["NTP"]["NTPServers"] = ntpServers;
         if (!hostName.empty())
         {
