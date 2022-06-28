c7aa1317a5073b8f2c6eca7dcae59346d1a23ef9
Ed Tanous <edtanous@google.com>
2022-06-28 12:17:18 -0700

    WIP: Allow building bmcweb without dbus
    DO NOT MERGE
    
    There's some interest in making bmcweb a more generalized server
    solution, and some assertions were made that bmcweb required dbus.
    While there is some truth to this, the architecture should be largely
    usable without DBus.
    
    This patchset for sure needs broken down into smaller pieces, but a lot
    of it can merge as is.  The changes largely involve:
    
    1. Fixing header includes to only include what they use.
    2. Create the redfish-dbus meson option, which when disabled, allows
       building bmcweb without dbus present
    3. Fixes to AsyncResolver, which currently relies on systemd on dbus to
       do async name resolution.  This is changed to go back to relying on
       boost::asio::tcp::resolver if dbus is no longer present.
    4. The inclusion of appropriate ifdefs to ensure that dbus/sdbusplus isn't
       included unless there are options that enable it.
    <More>
    
    There's work that needs to be done to abstract the authorization role
    mapping into something that systems can implement (authentication
    already runs over pam, which is pluggable already).
    
    This is a POC at best, although a number of the fixes in it can be
    broken out and merged today.
    
    Tested:
    built bmcweb locally with
    '''
    meson buildima --buildtype=debug -Dredfish='enabled' -Dredfish-dbus='disabled' -Dhost-serial-socket='disabled' -Dkvm='disabled' -Drest='disabled' -Dgoogle-api='disabled' -Dredfish-dbus-log='disabled'
    '''
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I778d29201f5c197a07f6852614d953b9056d230d

--- include/dbus_singleton.hpp
+++ include/dbus_singleton.hpp
@@ -5,7 +5,8 @@ namespace crow
 {
 namespace connections
 {
+#ifdef BMCWEB_ENABLE_REDFISH_DBUS
 static std::shared_ptr<sdbusplus::asio::connection> systemBus;
-
+#endif
 } // namespace connections
 } // namespace crow
