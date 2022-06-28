0a60bb63c9797f700bcb5162bf547ad49c887690
Ed Tanous <edtanous@google.com>
2022-06-28 12:12:03 -0700

    WIP: Allow async resolver to be optional
    This commit adds a meson option to allow selecting which dns resolver
    bmcweb uses.  There are use cases, like OCP IMA, that would require not
    using dbus, which would require us to fall back to the asio resolver.
    This commit makes the existing asio resolver constructor, and
    async_resolve methods match the equivalents in asio (which we intended
    to do anyway), then adds a macro and configure option for being able to
    select which resolver backend to rely on.
    
    Tested: WIP
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I3220214367179f131a60082bdfaf7e725d35c125

--- http/http_client.hpp
+++ http/http_client.hpp
@@ -34,6 +34,10 @@
 #include <include/async_resolve.hpp>
 #include <logging.hpp>
 
+#ifdef BMCWEB_DBUS_DNS_RESOLVER
+#include <include/async_resolve.hpp>
+#endif
+
 #include <cstdlib>
 #include <functional>
 #include <iostream>
@@ -136,7 +140,14 @@ class ConnectionInfo : public std::enable_shared_from_this<ConnectionInfo>
 
     // Ascync callables
     std::function<void(bool, uint32_t, Response&)> callback;
-    crow::async_resolve::Resolver resolver;
+
+#ifdef BMCWEB_DBUS_DNS_RESOLVER
+    using Resolver = crow::async_resolve::Resolver;
+#else
+    using Resolver = boost::asio::ip::tcp::resolver;
+#endif
+    Resolver resolver;
+
     boost::beast::tcp_stream conn;
     boost::asio::steady_timer timer;
 
@@ -149,11 +160,9 @@ class ConnectionInfo : public std::enable_shared_from_this<ConnectionInfo>
                          << std::to_string(port)
                          << ", id: " << std::to_string(connId);
 
-        auto respHandler =
-            [self(shared_from_this())](
-                const boost::beast::error_code ec,
-                const std::vector<boost::asio::ip::tcp::endpoint>&
-                    endpointList) {
+        auto respHandler = [self(shared_from_this())](
+                               const boost::system::error_code& ec,
+                               const Resolver::results_type& endpointList) {
             if (ec || (endpointList.empty()))
             {
                 BMCWEB_LOG_ERROR << "Resolve failed: " << ec.message();
@@ -167,11 +176,11 @@ class ConnectionInfo : public std::enable_shared_from_this<ConnectionInfo>
             self->doConnect(endpointList);
         };
 
-        resolver.asyncResolve(host, port, std::move(respHandler));
+        resolver.async_resolve(host, std::to_string(port),
+                               std::move(respHandler));
     }
 
-    void doConnect(
-        const std::vector<boost::asio::ip::tcp::endpoint>& endpointList)
+    void doConnect(const Resolver::results_type& endpointList)
     {
         state = ConnState::connectInProgress;
 
@@ -409,7 +418,8 @@ class ConnectionInfo : public std::enable_shared_from_this<ConnectionInfo>
                             const uint16_t destPort,
                             const unsigned int connIdIn) :
         subId(idIn),
-        host(destIP), port(destPort), connId(connIdIn), conn(ioc), timer(ioc)
+        host(destIP), port(destPort), connId(connIdIn), resolver(ioc),
+        conn(ioc), timer(ioc)
     {}
 };
 
@@ -630,8 +640,7 @@ class HttpClient
   private:
     std::unordered_map<std::string, std::shared_ptr<ConnectionPool>>
         connectionPools;
-    boost::asio::io_context& ioc =
-        crow::connections::systemBus->get_io_context();
+    boost::asio::io_context& ioc;
     std::unordered_map<std::string, RetryPolicyData> retryInfo;
     HttpClient() = default;
 
@@ -650,10 +659,20 @@ class HttpClient
     HttpClient& operator=(HttpClient&&) = delete;
     ~HttpClient() = default;
 
+    HttpClient(boost::asio::io_context& iocIn) : ioc(iocIn)
+    {}
+
+    static std::optional<HttpClient> httpClientSingleton;
+
+    static void initialize(boost::asio::io_context& ioc)
+    {
+        httpClientSingleton.emplace(ioc);
+    }
+
     static HttpClient& getInstance()
     {
-        static HttpClient handler;
-        return handler;
+        // TODO(ed) check for validity?
+        return *httpClientSingleton;
     }
 
     // Send a request to destIP:destPort where additional processing of the
