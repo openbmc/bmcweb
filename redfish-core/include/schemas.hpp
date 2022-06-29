13ae7bd1716fc3c963070ef10ddeb1441fcc7ade
Xiaochao Ma <maxiaochao@inspur.com>
2022-06-29 09:52:24 +0800

    Add TermalSubsystem schema
    as topic
    
    Signed-off-by: Xiaochao Ma <maxiaochao@inspur.com>
    Change-Id: I4540fcaa57455ec95d4c8fff71429309c2eebce3

--- redfish-core/include/schemas.hpp
+++ redfish-core/include/schemas.hpp
@@ -72,6 +72,7 @@ namespace redfish
         "TaskService",
         "TelemetryService",
         "Thermal",
+        "ThermalSubsystem",
         "Triggers",
         "UpdateService",
         "VirtualMedia",
