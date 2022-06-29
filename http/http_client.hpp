ca723762cdc83a2ad0bc75807d287abce219636d
Ed Tanous <edtanous@google.com>
2022-06-28 19:40:39 -0700

    Fix unused branches in http_client
    cppcheck correctly finds that these branches will always be hit, so the
    branch is unneeded.  Let's start by getting the code cleaned up.
    
    Tested: CI only, cpp check.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I7b89337a81e676915243d5bc9d26c24a89c74aef

--- http/http_client.hpp
+++ http/http_client.hpp
@@ -371,10 +371,8 @@ class ConnectionInfo : public std::enable_shared_from_this<ConnectionInfo>
         BMCWEB_LOG_DEBUG << host << ":" << std::to_string(port)
                          << ", id: " << std::to_string(connId)
                          << " closed gracefully";
-        if ((state != ConnState::suspended) && (state != ConnState::terminated))
-        {
-            state = ConnState::closed;
-        }
+
+        state = ConnState::closed;
     }
 
     void doCloseAndRetry()
@@ -395,12 +393,10 @@ class ConnectionInfo : public std::enable_shared_from_this<ConnectionInfo>
         BMCWEB_LOG_DEBUG << host << ":" << std::to_string(port)
                          << ", id: " << std::to_string(connId)
                          << " closed gracefully";
-        if ((state != ConnState::suspended) && (state != ConnState::terminated))
-        {
-            // Now let's try to resend the data
-            state = ConnState::retry;
-            this->doResolve();
-        }
+
+        // Now let's try to resend the data
+        state = ConnState::retry;
+        doResolve();
     }
 
   public:
