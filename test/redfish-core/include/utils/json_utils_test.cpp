49ad6103396d28d06686f98e9ecf931dedf0ed22
Nan Zhou <nanzhoumails@gmail.com>
2022-06-30 00:39:48 +0000

    json utility: add sort
    This commit adds a utility function |sortJsonArrayByKey|. It can sort an
    json array by value of a given key of each element.
    
    Use cases includes:
    1. sort the MemberCollection by @odata.id
    
    Tested:
    1. unit test passed;
    2. used in the child patch, which worked on hardware;
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: Idc175fab3af5c6102a5a3439b712b659ecb76468

--- test/redfish-core/include/utils/json_utils_test.cpp
+++ test/redfish-core/include/utils/json_utils_test.cpp
@@ -28,6 +28,8 @@ using ::testing::ElementsAre;
 using ::testing::IsEmpty;
 using ::testing::Not;
 
+using JsonArray = nlohmann::json::array_t;
+
 TEST(ReadJson, ValidElementsReturnsTrueResponseOkValuesUnpackedCorrectly)
 {
     crow::Response res;
@@ -352,6 +354,22 @@ TEST(ReadJsonAction, EmptyObjectReturnsTrueResponseOk)
     ASSERT_TRUE(readJsonAction(req, res, "integer", integer));
     EXPECT_EQ(res.result(), boost::beast::http::status::ok);
     EXPECT_THAT(res.jsonValue, IsEmpty());
+}
+
+TEST(SortJsonArrayByKey, ElementMissingKeyReturnsFalseArrayIsPartlySorted)
+{
+    JsonArray array = R"([{"foo" : "100"}, {"bar": "1"}, {"foo" : "20"}])"_json;
+    ASSERT_FALSE(sortJsonArrayByKey("foo", array));
+    // Objects with other keys are always larger than those with the specified
+    // key.
+    EXPECT_EQ(array, R"([{"bar": "1"}, {"foo" : "20"}, {"foo" : "100"}])"_json);
+}
+
+TEST(SortJsonArrayByKey, SortedByStringValueOnSuccessArrayIsSorted)
+{
+    JsonArray array = R"([{"foo": "20"}, {"foo" : "3"}, {"foo" : "100"}])"_json;
+    ASSERT_TRUE(sortJsonArrayByKey("foo", array));
+    EXPECT_EQ(array, R"([{"foo": "3"}, {"foo" : "20"}, {"foo" : "100"}])"_json);
 }
 
 } // namespace
