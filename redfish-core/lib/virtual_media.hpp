e590b674230acc8c5fea42896f99437ff5b8919d
Ed Tanous <edtanous@google.com>
2022-06-30 22:30:50 -0700

    Avoid copy
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I1e98ca63ee346ad683844e5637a173f64286102a

--- redfish-core/lib/virtual_media.hpp
+++ redfish-core/lib/virtual_media.hpp
@@ -573,7 +573,7 @@ class CredentialsProvider
         return credentials.password();
     }
 
-    SecureBuffer pack(FormatterFunc formatter)
+    SecureBuffer pack(FormatterFunc* formatter)
     {
         SecureBuffer packed{new Buffer{}};
         if (formatter != nullptr)
