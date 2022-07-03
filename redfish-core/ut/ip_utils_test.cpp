7e669ab3cba4eb2acb7f5ec75fed2dbd95389232
Nan Zhou <nanzhoumails@gmail.com>
2022-07-03 23:55:10 +0000

    test treewide: apply test names best practices
    1. test suite names are usually related to the function under tests, not
    the whole source or header file
    2. test case names should be verbose and describe the high level
    behavior
    
    This commits also splits test cases when they are for different
    functions, and merges test cases when they are very similar.
    
    Reference:
    1. https://github.com/google/googletest/tree/main/googletest/samples
    2. https://testing.googleblog.com/2007/02/tott-naming-unit-tests-responsibly.html
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I498cb2076bbaa9f0829f50bca45a2e38a6f4ab7d

--- redfish-core/ut/ip_utils_test.cpp
+++ redfish-core/ut/ip_utils_test.cpp
@@ -13,17 +13,25 @@ namespace
 
 using ::boost::asio::ip::make_address;
 
-TEST(IpToString, v4mapped)
+TEST(IpToString, ReturnsCorrectIpStringForIpv4Addresses)
 {
     EXPECT_EQ(toString(make_address("127.0.0.1")), "127.0.0.1");
     EXPECT_EQ(toString(make_address("192.168.1.1")), "192.168.1.1");
     EXPECT_EQ(toString(make_address("::1")), "::1");
+}
+
+TEST(IpToString, ReturnsCorrectIpStringForIpv6Addresses)
+{
     EXPECT_EQ(toString(make_address("fd03:f9ab:25de:89ec::0001")),
               "fd03:f9ab:25de:89ec::1");
     EXPECT_EQ(toString(make_address("fd03:f9ab:25de:89ec::1234:abcd")),
               "fd03:f9ab:25de:89ec::1234:abcd");
     EXPECT_EQ(toString(make_address("fd03:f9ab:25de:89ec:1234:5678:90ab:cdef")),
               "fd03:f9ab:25de:89ec:1234:5678:90ab:cdef");
+}
+
+TEST(IpToString, ReturnsCorrectIpStringForIpv4MappedIpv6Addresses)
+{
     EXPECT_EQ(toString(make_address("::ffff:127.0.0.1")), "127.0.0.1");
 }
 } // namespace
