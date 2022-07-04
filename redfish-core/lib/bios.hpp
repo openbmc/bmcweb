c95cfa309b765410fea3007c5f1771ce2efa0e4e
Arun Lal K M <arun.lal@intel.com>
2022-07-04 10:48:22 +0000

    Add support for GET to BiosAttributeRegistry.
    BiosAttributeRegistry contains all the editable knobs in BIOS menu.
    
    Reference:
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/29670
    
    Tested:
    1) By giving GET to 'redfish/v1/Registries/BiosAttributeRegistry/
    BiosAttributeRegistry'.
    
    Response:
    {
        "@odata.id": "/redfish/v1/Registries/BiosAttributeRegistry/
        BiosAttributeRegistry",
        "@odata.type": "#AttributeRegistry.v1_3_2.AttributeRegistry",
        "Id": "BiosAttributeRegistry",
        "Language": "en",
        "Name": "Bios Attribute Registry",
        "OwningEntity": "OpenBMC",
        "RegistryEntries": {
            "Attributes": [
                {
                    "AttributeName": "xxxx",
                    "CurrentValue": "0x00",
                    "DefaultValue": "0x00",
                    "DisplayName": "xxxx",
                    "HelpText": "Enable/Disable xxxx",
                    "MenuPath": "./",
                    "ReadOnly": false,
                    "Type": "String",
                    "Value": [
                        {
                            "OneOf": "0x0"
                        },
                        {
                            "OneOf": "0x1"
                        }
                    ]
                },
                ...
                ...
                ...
                {
                    "AttributeName": "yyyy",
                    "CurrentValue": "0x00",
                    "DefaultValue": "0x00",
                    "DisplayName": "yyyy",
                    "HelpText": "Enable/Disable yyyy",
                    "MenuPath": "./",
                    "ReadOnly": false,
                    "Type": "String",
                    "Value": [
                        {
                            "OneOf": "0x0"
                        },
                        {
                            "OneOf": "0x1"
                        }
                    ]
                }
            ]
        },
        "RegistryVersion": "1.0.0"
    }
    
    2) By running Redfish-Service-Validator.
    
    Result:
    Elapsed time: 0:09:36
    invalidPropertyValue: 108
    metadataNamespaces: 2185
    missingNamespaces: 1
    optionalAction: 9
    pass: 13772
    passAction: 22
    passGet: 541
    reflink: 1
    repeat: 47
    serviceNamespaces: 75
    skipOptional: 9276
    unverifiedComplexAdditional: 1
    warnDeprecated: 230
    warningPresent: 54
    Validation has succeeded.
    
    Signed-off-by: Arun Lal K M <arun.lal@intel.com>
    Change-Id: I31971c2ad505b976a9478074e18305f8542db2fa

--- redfish-core/lib/bios.hpp
+++ redfish-core/lib/bios.hpp
@@ -7,6 +7,120 @@
 
 namespace redfish
 {
+using BiosBaseTableType = boost::container::flat_map<
+    std::string,
+    std::tuple<
+        std::string, bool, std::string, std::string, std::string,
+        std::variant<int64_t, std::string>, std::variant<int64_t, std::string>,
+        std::vector<
+            std::tuple<std::string, std::variant<int64_t, std::string>>>>>;
+
+using BiosBaseTableItemType = std::pair<
+    std::string,
+    std::tuple<
+        std::string, bool, std::string, std::string, std::string,
+        std::variant<int64_t, std::string>, std::variant<int64_t, std::string>,
+        std::vector<
+            std::tuple<std::string, std::variant<int64_t, std::string>>>>>;
+using OptionsItemType =
+    std::tuple<std::string, std::variant<int64_t, std::string>>;
+
+using PendingAttributesType = boost::container::flat_map<
+    std::string, std::tuple<std::string, std::variant<int64_t, std::string>>>;
+
+using PendingAttributesItemType =
+    std::pair<std::string,
+              std::tuple<std::string, std::variant<int64_t, std::string>>>;
+
+enum BiosBaseTableIndex
+{
+    biosBaseAttrType = 0,
+    biosBaseReadonlyStatus,
+    biosBaseDisplayName,
+    biosBaseDescription,
+    biosBaseMenuPath,
+    biosBaseCurrValue,
+    biosBaseDefaultValue,
+    biosBaseOptions
+};
+enum OptionsItemIndex
+{
+    optItemType = 0,
+    optItemValue
+};
+
+enum PendingAttributesIndex
+{
+    pendingAttrType = 0,
+    pendingAttrValue
+};
+static std::string mapAttrTypeToRedfish(const std::string_view typeDbus)
+{
+    std::string ret;
+    if (typeDbus == "xyz.openbmc_project.BIOSConfig."
+                    "Manager.AttributeType.String")
+    {
+        ret = "String";
+    }
+    else if (typeDbus == "xyz.openbmc_project.BIOSConfig."
+                         "Manager.AttributeType.Integer")
+    {
+        ret = "Integer";
+    }
+    else if (typeDbus == "xyz.openbmc_project.BIOSConfig."
+                         "Manager.AttributeType.Enumeration")
+    {
+        ret = "Enumeration";
+    }
+    else
+    {
+        ret = "UNKNOWN";
+    }
+
+    return ret;
+}
+
+static std::string mapBoundTypeToRedfish(const std::string_view typeDbus)
+{
+    std::string ret;
+    if (typeDbus ==
+        "xyz.openbmc_project.BIOSConfig.Manager.BoundType.ScalarIncrement")
+    {
+        ret = "ScalarIncrement";
+    }
+    else if (typeDbus ==
+             "xyz.openbmc_project.BIOSConfig.Manager.BoundType.LowerBound")
+    {
+        ret = "LowerBound";
+    }
+    else if (typeDbus ==
+             "xyz.openbmc_project.BIOSConfig.Manager.BoundType.UpperBound")
+    {
+        ret = "UpperBound";
+    }
+    else if (typeDbus ==
+             "xyz.openbmc_project.BIOSConfig.Manager.BoundType.MinStringLength")
+    {
+        ret = "MinStringLength";
+    }
+    else if (typeDbus ==
+             "xyz.openbmc_project.BIOSConfig.Manager.BoundType.MaxStringLength")
+    {
+        ret = "MaxStringLength";
+    }
+    else if (typeDbus ==
+             "xyz.openbmc_project.BIOSConfig.Manager.BoundType.OneOf")
+    {
+        ret = "OneOf";
+    }
+    else
+    {
+        ret = "UNKNOWN";
+    }
+
+    return ret;
+}
+
 /**
  * BiosService class supports handle get method for bios.
  */
@@ -73,6 +187,245 @@ inline void requestRoutesBiosReset(App& app)
         .privileges(redfish::privileges::postBios)
         .methods(boost::beast::http::verb::post)(
             std::bind_front(handleBiosResetPost, std::ref(app)));
+}
+
+inline void handleBiosAttributeRegistryGet(
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    asyncResp->res.jsonValue["@odata.id"] =
+        "/redfish/v1/Registries/BiosAttributeRegistry/"
+        "BiosAttributeRegistry";
+    asyncResp->res.jsonValue["@odata.type"] =
+        "#AttributeRegistry.v1_3_2.AttributeRegistry";
+    asyncResp->res.jsonValue["Name"] = "Bios Attribute Registry";
+    asyncResp->res.jsonValue["Id"] = "BiosAttributeRegistry";
+    asyncResp->res.jsonValue["RegistryVersion"] = "1.0.0";
+    asyncResp->res.jsonValue["Language"] = "en";
+    asyncResp->res.jsonValue["OwningEntity"] = "OpenBMC";
+    asyncResp->res.jsonValue["RegistryEntries"]["Attributes"] =
+        nlohmann::json::array();
+
+    crow::connections::systemBus->async_method_call(
+        [asyncResp](const boost::system::error_code ec,
+                    const dbus::utility::MapperGetObject& getObjectType) {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR << "ObjectMapper::GetObject call failed: " << ec;
+            messages::internalError(asyncResp->res);
+
+            return;
+        }
+
+        if (getObjectType.empty())
+        {
+            BMCWEB_LOG_ERROR << "getObjectType is empty.";
+            messages::internalError(asyncResp->res);
+
+            return;
+        }
+
+        std::string service = getObjectType.begin()->first;
+
+        crow::connections::systemBus->async_method_call(
+            [asyncResp](const boost::system::error_code ec,
+                        const std::variant<BiosBaseTableType>& retBiosTable) {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "getBiosAttributeRegistry DBUS error: "
+                                 << ec;
+                messages::resourceNotFound(asyncResp->res, "Registries/Bios",
+                                           "Bios");
+                return;
+            }
+            const BiosBaseTableType* baseBiosTable =
+                std::get_if<BiosBaseTableType>(&retBiosTable);
+            nlohmann::json& attributeArray =
+                asyncResp->res.jsonValue["RegistryEntries"]["Attributes"];
+            if (baseBiosTable == nullptr)
+            {
+                BMCWEB_LOG_ERROR << "baseBiosTable == nullptr ";
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            for (const BiosBaseTableItemType& item : *baseBiosTable)
+            {
+                const std::string& itemType =
+                    std::get<biosBaseAttrType>(item.second);
+                std::string attrType = mapAttrTypeToRedfish(itemType);
+                if (attrType == "UNKNOWN")
+                {
+                    BMCWEB_LOG_ERROR << "UNKNOWN attrType == " << itemType;
+                    continue;
+                }
+
+                nlohmann::json attributeItem;
+                attributeItem["AttributeName"] = item.first;
+                attributeItem["Type"] = attrType;
+                attributeItem["ReadOnly"] =
+                    std::get<biosBaseReadonlyStatus>(item.second);
+                attributeItem["DisplayName"] =
+                    std::get<biosBaseDisplayName>(item.second);
+                attributeItem["HelpText"] =
+                    std::get<biosBaseDescription>(item.second);
+                attributeItem["MenuPath"] =
+                    std::get<biosBaseMenuPath>(item.second);
+
+                if (attrType == "String" || attrType == "Enumeration")
+                {
+                    const std::string* currValue = std::get_if<std::string>(
+                        &std::get<biosBaseCurrValue>(item.second));
+
+                    if (!currValue)
+                    {
+                        BMCWEB_LOG_ERROR << "Unable to get currValue, no "
+                                            "std::string data in BIOS "
+                                            "attributes item data";
+                        continue;
+                    }
+
+                    const std::string* defValue = std::get_if<std::string>(
+                        &std::get<biosBaseDefaultValue>(item.second));
+
+                    if (!defValue)
+                    {
+                        BMCWEB_LOG_ERROR << "Unable to get defValue, no "
+                                            "std::string data in BIOS "
+                                            "attributes item data";
+                        continue;
+                    }
+
+                    attributeItem["CurrentValue"] =
+                        currValue != nullptr ? *currValue : "";
+                    attributeItem["DefaultValue"] =
+                        defValue != nullptr ? *defValue : "";
+                }
+                else if (attrType == "Integer")
+                {
+                    const int64_t* currValue = std::get_if<int64_t>(
+                        &std::get<biosBaseCurrValue>(item.second));
+
+                    if (!currValue)
+                    {
+                        BMCWEB_LOG_ERROR << "Unable to get currValue, no "
+                                            "int64_t data in BIOS "
+                                            "attributes item data";
+                        continue;
+                    }
+
+                    const int64_t* defValue = std::get_if<int64_t>(
+                        &std::get<biosBaseDefaultValue>(item.second));
+
+                    if (!defValue)
+                    {
+                        BMCWEB_LOG_ERROR << "Unable to get defValue, no "
+                                            "int64_t data in BIOS "
+                                            "attributes item data";
+                        continue;
+                    }
+
+                    attributeItem["CurrentValue"] =
+                        currValue != nullptr ? *currValue : 0;
+                    attributeItem["DefaultValue"] =
+                        defValue != nullptr ? *defValue : 0;
+                }
+                else
+                {
+                    BMCWEB_LOG_ERROR << "UNKNOWN attrType == " << itemType;
+                    continue;
+                }
+
+                nlohmann::json optionsArray = nlohmann::json::array();
+                const std::vector<OptionsItemType>& optionsVector =
+                    std::get<biosBaseOptions>(item.second);
+                for (const OptionsItemType& optItem : optionsVector)
+                {
+                    nlohmann::json optItemJson;
+                    const std::string& strOptItemType =
+                        std::get<optItemType>(optItem);
+                    std::string optItemTypeRedfish =
+                        mapBoundTypeToRedfish(strOptItemType);
+                    if (optItemTypeRedfish == "UNKNOWN")
+                    {
+                        BMCWEB_LOG_ERROR << "UNKNOWN optItemTypeRedfish == "
+                                         << strOptItemType;
+                        continue;
+                    }
+                    if (optItemTypeRedfish == "OneOf")
+                    {
+                        const std::string* currValue = std::get_if<std::string>(
+                            &std::get<optItemValue>(optItem));
+
+                        if (!currValue)
+                        {
+                            BMCWEB_LOG_ERROR << "Unable to get currValue, "
+                                                "no "
+                                                "std::string data in option "
+                                                "item value";
+                            continue;
+                        }
+
+                        optItemJson["ValueDisplayName"] =
+                            currValue != nullptr ? *currValue : "";
+                        optItemJson["ValueName"] =
+                            currValue != nullptr ? *currValue : "";
+                    }
+                    else
+                    {
+                        const int64_t* currValue = std::get_if<int64_t>(
+                            &std::get<optItemValue>(optItem));
+
+                        if (!currValue)
+                        {
+                            BMCWEB_LOG_ERROR << "Unable to get currValue, "
+                                                "no "
+                                                "int64_t data in option "
+                                                "item value";
+                            continue;
+                        }
+
+                        optItemJson["ValueDisplayName"] =
+                            currValue != nullptr ? *currValue : 0;
+                        optItemJson["ValueName"] =
+                            currValue != nullptr ? *currValue : 0;
+                    }
+
+                    optionsArray.push_back(optItemJson);
+                }
+
+                if (optionsArray.empty())
+                {
+                    BMCWEB_LOG_ERROR << "optionsArray is empty";
+                    continue;
+                }
+
+                attributeItem["Value"] = optionsArray;
+                attributeArray.push_back(attributeItem);
+            }
+            },
+            service, "/xyz/openbmc_project/bios_config/manager",
+            "org.freedesktop.DBus.Properties", "Get",
+            "xyz.openbmc_project.BIOSConfig.Manager", "BaseBIOSTable");
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetObject",
+        "/xyz/openbmc_project/bios_config/manager",
+        std::array<const char*, 0>());
+}
+
+/**
+ * BiosAttributeRegistry class supports handle get method for BIOS attribute
+ * registry.
+ */
+inline void requestRoutesBiosAttributeRegistry(App& app)
+{
+    BMCWEB_ROUTE(
+        app,
+        "/redfish/v1/Registries/BiosAttributeRegistry/BiosAttributeRegistry/")
+        .privileges(redfish::privileges::getBios)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handleBiosAttributeRegistryGet, std::ref(app)));
 }
 
 } // namespace redfish
