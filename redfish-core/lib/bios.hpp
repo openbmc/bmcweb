8808ae8b9380079ec1f9746d301e095a9f798c3a
Arun Lal K M <arun.lal@intel.com>
2022-07-04 11:17:08 +0000

    Add support to ResetBios action
    Reference:
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/39183
    
    Tested:
    1) Bios reset flag can be modified throw redfish
    POST https://IP_ADDR/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios
    
    2) By running Redfish-Service-Validator.
    Result:
    Elapsed time: 0:09:36
    invalidPropertyValue: 108
    metadataNamespaces: 2185
    missingNamespaces: 1
    optionalAction: 9
    pass: 13772
    passAction: 22
    passGet: 541
    reflink: 1
    repeat: 47
    serviceNamespaces: 75
    skipOptional: 9276
    unverifiedComplexAdditional: 1
    warnDeprecated: 230
    warningPresent: 54
    Validation has succeeded.
    
    Signed-off-by: Arun Lal K M <arun.lal@intel.com>
    Change-Id: I382c4b1034da988ea2f7bfe195ee68c46726f2ac

--- redfish-core/lib/bios.hpp
+++ redfish-core/lib/bios.hpp
@@ -54,17 +54,24 @@ inline void
     {
         return;
     }
+
+    std::string resetFlag =
+        "xyz.openbmc_project.BIOSConfig.Manager.ResetFlag.FactoryDefaults";
+
     crow::connections::systemBus->async_method_call(
         [asyncResp](const boost::system::error_code ec) {
         if (ec)
         {
-            BMCWEB_LOG_ERROR << "Failed to reset bios: " << ec;
+            BMCWEB_LOG_ERROR << "doPost bios reset got error " << ec;
             messages::internalError(asyncResp->res);
             return;
         }
         },
-        "org.open_power.Software.Host.Updater", "/xyz/openbmc_project/software",
-        "xyz.openbmc_project.Common.FactoryReset", "Reset");
+        "xyz.openbmc_project.BIOSConfigManager",
+        "/xyz/openbmc_project/bios_config/manager",
+        "org.freedesktop.DBus.Properties", "Set",
+        "xyz.openbmc_project.BIOSConfig.Manager", "ResetBIOSSettings",
+        std::variant<std::string>(resetFlag));
 }
 
 inline void requestRoutesBiosReset(App& app)
