19925e487f3ba3d74c97cdc6655594b5ede00819
Nan Zhou <nanzhoumails@gmail.com>
2022-07-04 01:21:06 +0000

    lock test: remove unused function
    Tested: unit test passed.
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I6f9c6b4648d98f4f035830d8a5da094ba76e7458

--- redfish-core/ut/lock_test.cpp
+++ redfish-core/ut/lock_test.cpp
@@ -103,11 +103,6 @@ class MockLock : public crow::ibm_mc_lock::Lock
         auto status = Lock::isItMyLock(tids, ids);
         return status;
     }
-    RcGetLockList getList(const std::vector<std::string>& listSessionid)
-    {
-        auto status = Lock::getLockList(listSessionid);
-        return status;
-    }
     friend class LockTest;
 };
 
