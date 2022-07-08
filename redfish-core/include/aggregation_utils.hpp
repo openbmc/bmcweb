0fa850314d472bee49e0ba2175c9814d0f2ea4dd
Carson Labrado <clabrado@google.com>
2022-07-08 20:56:52 +0000

    Aggregation: Generate collections from schema
    In aggregation we need to account for when a satellite BMC supports a
    resource collection that the aggregating BMC does not.  We need to
    add links in responses from upstream resources to indicate that these
    satellite only resources exist.  These top level collections do not
    always have the location format of /redfish/v1/<collection_id>.  We
    determine all of those from the schema rather than hardcoding a few
    choice exceptions that do not fit that format such as
    /redfish/v1/UpdateService/FirmwareInventory.
    
    This patch is the first step in accomplishing this.  We parse all
    xml schema files in order to determine: 1) what are the top level
    collection URIs and 2) for URIs that are upstream from top level
    collections, what URIs could appear in the response which are on the
    same path to a top level collection.
    
    The xml files for resources that aren't currently supported by bmcweb
    are ignored when creating the schema index and later deleted once the
    above parsing completes.
    
    Later patches will use this information to make sure that we are
    properly handling all schema defined top level collections.
    
    Tested:
    Compiles with aggregation enabled
    
    Signed-off-by: Carson Labrado <clabrado@google.com>
    Change-Id: Icff825f4e4e9d84c96254561350f82839abdc074

---
+++ redfish-core/include/aggregation_utils.hpp
@@ -0,0 +1,66 @@
+#pragma once
+/****************************************************************
+ *                 READ THIS WARNING FIRST
+ * This is an auto-generated header which contains definitions
+ * for Redfish DMTF defined schemas.
+ * DO NOT modify this registry outside of running the
+ * update_schemas.py script.  The definitions contained within
+ * this file are owned by DMTF.  Any modifications to these files
+ * should be first pushed to the relevant registry in the DMTF
+ * github organization.
+ ***************************************************************/
+// clang-format off
+#include <array>
+
+namespace redfish
+{
+constexpr std::array topCollections{
+    "/redfish/v1/AggregationService/Aggregates",
+    "/redfish/v1/AggregationService/AggregationSources",
+    "/redfish/v1/AggregationService/ConnectionMethods",
+    "/redfish/v1/Cables",
+    "/redfish/v1/CertificateService/CertificateCollection",
+    "/redfish/v1/CertificateService/CertificateLocations/Certificates",
+    "/redfish/v1/Chassis",
+    "/redfish/v1/ComponentIntegrity",
+    "/redfish/v1/CompositionService/ActivePool",
+    "/redfish/v1/CompositionService/CompositionReservations",
+    "/redfish/v1/CompositionService/FreePool",
+    "/redfish/v1/CompositionService/ResourceBlocks",
+    "/redfish/v1/CompositionService/ResourceZones",
+    "/redfish/v1/EventService/Subscriptions",
+    "/redfish/v1/Fabrics",
+    "/redfish/v1/Facilities",
+    "/redfish/v1/JobService/Jobs",
+    "/redfish/v1/JobService/LogService/Entries",
+    "/redfish/v1/KeyService/NVMeoFKeyPolicies",
+    "/redfish/v1/KeyService/NVMeoFSecrets",
+    "/redfish/v1/LicenseService/Licenses",
+    "/redfish/v1/Managers",
+    "/redfish/v1/NVMeDomains",
+    "/redfish/v1/PowerEquipment/ElectricalBuses",
+    "/redfish/v1/PowerEquipment/FloorPDUs",
+    "/redfish/v1/PowerEquipment/ManagedBy",
+    "/redfish/v1/PowerEquipment/PowerShelves",
+    "/redfish/v1/PowerEquipment/RackPDUs",
+    "/redfish/v1/PowerEquipment/Switchgear",
+    "/redfish/v1/PowerEquipment/TransferSwitches",
+    "/redfish/v1/RegisteredClients",
+    "/redfish/v1/Registries",
+    "/redfish/v1/ResourceBlocks",
+    "/redfish/v1/Storage",
+    "/redfish/v1/StorageServices",
+    "/redfish/v1/StorageSystems",
+    "/redfish/v1/Systems",
+    "/redfish/v1/TaskService/Tasks",
+    "/redfish/v1/TelemetryService/LogService/Entries",
+    "/redfish/v1/TelemetryService/MetricDefinitions",
+    "/redfish/v1/TelemetryService/MetricReportDefinitions",
+    "/redfish/v1/TelemetryService/MetricReports",
+    "/redfish/v1/TelemetryService/Triggers",
+    "/redfish/v1/UpdateService/ClientCertificates",
+    "/redfish/v1/UpdateService/FirmwareInventory",
+    "/redfish/v1/UpdateService/RemoteServerCertificates",
+    "/redfish/v1/UpdateService/SoftwareInventory",
+};
+} // namespace redfish
