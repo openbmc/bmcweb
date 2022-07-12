d6c79cf12a83f279e558cdf0c44f084d281e4a76
Claire Weinan <cweinan@google.com>
2022-07-12 15:50:10 -0700

    utility: Add support for microsecond timestamps
    The first intended use case (to be implemented in a follow-on commit)
    will be to change the FaultLog LogService
    (/redfish/v1/Managers/bmc/LogServices/FaultLog) to use
    microsecond-precision 'Created' timestamps. The motivation for
    increased precision is to increase the chance of having unique
    timestamps in case faults happen in quick succession. Unique timestamps
    are helpful for data center tools to keep log entries in chronological
    order and to track which entries have been seen before.
    
    Also fixed an issue where getDateTimeUintMs() could output the wrong
    number of fraction digits (e.g. 0 or 6 digits instead of the expected
    3).
    
    Changes to the Redfish tree:
    Endpoints that call getDateTimeUintMs() to get a timestamp string will
    now output 3 fraction digits in the timestamp string.
    
    Tested:
    - Added unit tests in utility_test.cpp.
    - Called getDateTimeUintUs() from getDumpEntryById() and
      getDumpEntryCollection() in log_services.hpp.
    
      Example command:
      curl -k -H "X-Auth-Token: $token" -X GET http://${bmc}/redfish/v1/Managers/bmc/LogServices/FaultLog/Entries
    
      Example timestamp output:
      "Created": "2022-07-12T15:56:33.017346+00:00",
    
    - Confirmed that getDateTimeUintMs() now outputs 3 fraction digits.
      First enabled redfish-dbus-log in bmcweb.
    
      Forced creation of a log entry from BMC console:
      busctl call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Logging.Create Create ssa{ss} “mymessage” xyz.openbmc_project.Logging.Entry.Level.Emergency 0
    
      Example command to retrieve log entry:
      curl -k -H "X-Auth-Token: $token" -X GET http://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries/
    
      Example timestamp output:
      "Created": "2022-07-12T17:36:05.458+00:00",
    
    - Confirmed that getDateTimeUint() still works.
    
      Example command:
      curl -k -H "X-Auth-Token: $token" -X GET http://${bmc}/redfish/v1/Managers/bmc/LogServices/Dump/Entries
    
      Example timestamp output:
      "Created": "2022-07-12T16:45:13+00:00",
    
    Signed-off-by: Claire Weinan <cweinan@google.com>
    Change-Id: Ie931382ee5cf2e47675e32a8d3583b8d7b597b04

--- http/ut/utility_test.cpp
+++ http/ut/utility_test.cpp
@@ -78,15 +78,42 @@ TEST(Utility, Base64EncodeDecodeString)
     EXPECT_EQ(data, decoded);
 }
 
-TEST(Utility, GetDateTimeStdtime)
+TEST(ToIsoExtendedStringWithNumFractionalDigits, SpecialValuesReturnEmptyString)
+{
+    EXPECT_EQ(details::toIsoExtendedStringWithNumFractionalDigits(
+                  boost::posix_time::ptime(
+                      boost::date_time::special_values::not_a_date_time),
+                  0),
+              "");
+
+    EXPECT_EQ(details::toIsoExtendedStringWithNumFractionalDigits(
+                  boost::posix_time::ptime(
+                      boost::date_time::special_values::neg_infin),
+                  0),
+              "");
+
+    EXPECT_EQ(details::toIsoExtendedStringWithNumFractionalDigits(
+                  boost::posix_time::ptime(
+                      boost::date_time::special_values::pos_infin),
+                  0),
+              "");
+}
+
+TEST(GetDateTimeStdtime, PreEpoch)
 {
     // some time before the epoch
     EXPECT_EQ(getDateTimeStdtime(std::time_t{-1234567}),
               "1969-12-17T17:03:53+00:00");
+}
 
+TEST(GetDateTimeStdtime, Epoch)
+{
     // epoch
     EXPECT_EQ(getDateTimeStdtime(std::time_t{0}), "1970-01-01T00:00:00+00:00");
+}
 
+TEST(GetDateTimeStdtime, MinAndMaxDateTime)
+{
     // Limits
     EXPECT_EQ(getDateTimeStdtime(std::numeric_limits<std::time_t>::max()),
               "9999-12-31T23:59:59+00:00");
@@ -94,8 +121,106 @@ TEST(Utility, GetDateTimeStdtime)
               "1970-01-01T00:00:00+00:00");
 }
 
-TEST(Utility, GetDateTimeUint)
+TEST(GetDateTime, NonZeroFractionValueVaryNumFractionalDigits)
 {
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 0),
+        "2021-11-30T22:41:35+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 1),
+        "2021-11-30T22:41:35.1+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 2),
+        "2021-11-30T22:41:35.12+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 3),
+        "2021-11-30T22:41:35.123+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 4),
+        "2021-11-30T22:41:35.1234+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 5),
+        "2021-11-30T22:41:35.12345+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 6),
+        "2021-11-30T22:41:35.123456+00:00");
+    // Max 6 fractional digits -- requesting more is considered an error
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095123456}), 7),
+        "");
+}
+
+TEST(GetDateTime, ZeroFractionValueVaryNumFractionalDigits)
+{
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 0),
+        "2021-11-30T22:41:35+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 1),
+        "2021-11-30T22:41:35.0+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 2),
+        "2021-11-30T22:41:35.00+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 3),
+        "2021-11-30T22:41:35.000+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 4),
+        "2021-11-30T22:41:35.0000+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 5),
+        "2021-11-30T22:41:35.00000+00:00");
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 6),
+        "2021-11-30T22:41:35.000000+00:00");
+    // Max 6 fraction digits -- requesting more is considered an error
+    EXPECT_EQ(
+        details::getDateTime(
+            boost::posix_time::microseconds(uint64_t{1638312095000000}), 7),
+        "");
+}
+
+TEST(GetDateTime, MinAndMaxDateTime)
+{
+    // returns the maximum Redfish date
+    EXPECT_EQ(details::getDateTime(
+                  boost::posix_time::microseconds(details::maxMicroSeconds), 0),
+              "9999-12-31T23:59:59+00:00");
+    EXPECT_EQ(details::getDateTime(
+                  boost::posix_time::microseconds(details::maxMicroSeconds), 3),
+              "9999-12-31T23:59:59.999+00:00");
+    EXPECT_EQ(details::getDateTime(
+                  boost::posix_time::microseconds(details::maxMicroSeconds), 6),
+              "9999-12-31T23:59:59.999999+00:00");
+    // returns the minimum Redfish date
+    EXPECT_EQ(details::getDateTime(boost::posix_time::microseconds(
+                                       std::numeric_limits<uint64_t>::min()),
+                                   0),
+              "1970-01-01T00:00:00+00:00");
+    EXPECT_EQ(details::getDateTime(boost::posix_time::microseconds(
+                                       std::numeric_limits<uint64_t>::min()),
+                                   6),
+              "1970-01-01T00:00:00.000000+00:00");
+}
+
+TEST(GetDateTimeUint, MiscellaneousDateTime)
+{
+    // some time in the past
     EXPECT_EQ(getDateTimeUint(uint64_t{1638312095}),
               "2021-11-30T22:41:35+00:00");
     // some time in the future, beyond 2038
@@ -104,22 +229,60 @@ TEST(Utility, GetDateTimeUint)
     // the maximum time we support
     EXPECT_EQ(getDateTimeUint(uint64_t{253402300799}),
               "9999-12-31T23:59:59+00:00");
+}
 
+TEST(GetDateTimeUint, MinAndMaxDateTime)
+{
     // returns the maximum Redfish date
     EXPECT_EQ(getDateTimeUint(std::numeric_limits<uint64_t>::max()),
               "9999-12-31T23:59:59+00:00");
-
+    // returns the minimum Redfish date
     EXPECT_EQ(getDateTimeUint(std::numeric_limits<uint64_t>::min()),
               "1970-01-01T00:00:00+00:00");
 }
 
-TEST(Utility, GetDateTimeUintMs)
+TEST(GetDateTimeUintMs, NonZeroFractionValue)
+{
+    EXPECT_EQ(getDateTimeUintMs(uint64_t{1638312095120}),
+              "2021-11-30T22:41:35.120+00:00");
+}
+
+TEST(GetDateTimeUintMs, ZeroFractionValue)
+{
+    EXPECT_EQ(getDateTimeUintMs(uint64_t{1638312095000}),
+              "2021-11-30T22:41:35.000+00:00");
+}
+
+TEST(GetDateTimeUintMs, MinAndMaxDateTime)
 {
     // returns the maximum Redfish date
     EXPECT_EQ(getDateTimeUintMs(std::numeric_limits<uint64_t>::max()),
-              "9999-12-31T23:59:59.999000+00:00");
+              "9999-12-31T23:59:59.999+00:00");
+    // returns the minimum Redfish date
     EXPECT_EQ(getDateTimeUintMs(std::numeric_limits<uint64_t>::min()),
-              "1970-01-01T00:00:00+00:00");
+              "1970-01-01T00:00:00.000+00:00");
+}
+
+TEST(GetDateTimeUintUs, NonZeroFractionValue)
+{
+    EXPECT_EQ(getDateTimeUintUs(uint64_t{1638312095123456}),
+              "2021-11-30T22:41:35.123456+00:00");
+}
+
+TEST(GetDateTimeUintUs, ZeroFractionValue)
+{
+    EXPECT_EQ(getDateTimeUintUs(uint64_t{1638312095000000}),
+              "2021-11-30T22:41:35.000000+00:00");
+}
+
+TEST(GetDateTimeUintUs, MinAndMaxDateTime)
+{
+    // returns the maximum Redfish date
+    EXPECT_EQ(getDateTimeUintUs(std::numeric_limits<uint64_t>::max()),
+              "9999-12-31T23:59:59.999999+00:00");
+    // returns the minimum Redfish date
+    EXPECT_EQ(getDateTimeUintUs(std::numeric_limits<uint64_t>::min()),
+              "1970-01-01T00:00:00.000000+00:00");
 }
 
 TEST(Utility, UrlFromPieces)
