3acced2ce93ab735f147ba8204afcc18bdc0c3ea
Nan Zhou <nanzhoumails@gmail.com>
2022-07-12 23:21:02 +0000

    authn: correct meson behaviors
    Today `basic-auth` (and other options) can be enabled even if
    `insecure-disable-auth` is enabled, which doesn't make sense.
    With this block this commit added in meson, If we disable authx with
    `insecure-disable-auth`, then all these auth options will be ignored.
    
    Tested:
    1. code compiles with and without 'insecure-disable-auth'.
    2. No new service validator errors when 'insecure-disable-auth' is
    turned on.
    3. No new service validator errors when 'insecure-disable-auth' is
    turned off.
    
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I2c634851f7aa7b9e57158770c5d40c12954c93a7

--- include/authentication.hpp
+++ include/authentication.hpp
@@ -260,7 +260,8 @@ static std::shared_ptr<persistent_data::UserSession>
 [[maybe_unused]] static std::shared_ptr<persistent_data::UserSession>
     authenticate(
         const boost::asio::ip::address& ipAddress [[maybe_unused]],
-        Response& res [[maybe_unused]], boost::beast::http::verb method,
+        Response& res [[maybe_unused]],
+        boost::beast::http::verb method [[maybe_unused]],
         const boost::beast::http::header<true>& reqHeader,
         [[maybe_unused]] const std::shared_ptr<persistent_data::UserSession>&
             session)
