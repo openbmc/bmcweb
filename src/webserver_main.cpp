ccb2df157159f5c5d65b95ed4dd6fdae0487554b
Josh Lehan <krellan@google.com>
2022-07-14 14:03:29 -0700

    bmcweb: Show exception what() before exiting
    Although exceptions should not happen in bmcweb, the underlying Crow
    code sometimes throws, and so do bugs caused by accidental usage of
    functions that throw.
    
    Adding e.what() output when std::exception is thrown.
    
    Tested: Accidentally tested more often than I would care to admit....
    
    Signed-off-by: Josh Lehan <krellan@google.com>
    Change-Id: Ifcd30dc53369708b21bf958c627755651422f18a

--- src/webserver_main.cpp
+++ src/webserver_main.cpp
@@ -158,6 +158,11 @@ int main(int /*argc*/, char** /*argv*/)
     {
         return run();
     }
+    catch (const std::exception& e)
+    {
+        BMCWEB_LOG_CRITICAL << "Threw exception to main: " << e.what();
+        return -1;
+    }
     catch (...)
     {
         BMCWEB_LOG_CRITICAL << "Threw exception to main";
