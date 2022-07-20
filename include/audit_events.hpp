85bf3cdc93b41e4281578b92714e94a2899c21de
Manojkiran Eda <manojkiran.eda@gmail.com>
2022-07-20 23:13:14 +0530

    Add audit events support in bmcweb
    This is a very minimal prototype on how userspace application
    can leverage the linux-audit[1] farmework for logging audit events.
    
    The linux audit subsystem helps system admistrators create an audit
    trail, a log for every action on the server. we can track
    security-relevant events, record the events in a log file, and detect
    misuse or unauthorized activities by inspecting the audit log files.
    
    Through auditd - we can track:
    1. Access to objects in the file system
    2. System calls
    3. User logins
    4. Actions with accounts and preivileges
    
    Audid is found in all well known distros like Ubuntu[2], Fedora[3],
    Arch[4] e.t.c
    So its has the following advantages:
    1. Its well maintained.
    2. Proven solution for capturing/streaming audit logs.
    3. Ready available package and recipe in yocto
    4. It has ton of felxibility and options to configure - so definitely alot
       better than any home cooked solution.
    
    The way that user space daemons can generate audit events is by using API's
    that are exposed by libaudit[5] shared library. This idea is not new, various
    distro login daemons like lightdm[6], gdm[7],openssh[8].
    
    Following the similar opensource work in these user-space daemons to log
    audit events - i tried to cook up a small prototype through which we can
    log USER_LOGIN type audit messages from bmcweb when the redfish user is
    authenticated.
    
    [1] https://man.archlinux.org/man/auditd.8.en
    [2] https://packages.ubuntu.com/jammy/auditd
    [3] https://packages.fedoraproject.org/pkgs/audit/audit/
    [4] https://wiki.archlinux.org/title/Audit_framework
    [5] https://packages.ubuntu.com/jammy/libaudit-dev
    [6] https://github.com/canonical/lightdm/blob/224b090ed78abbf43ca75f825dba78966c97c082/configure.ac#L130
    [7] https://github.com/GNOME/gdm/blob/c8d17ad2566fabecee0d598dcb5bf53939ee9bbe/meson_options.txt#L14
    [8] https://github.com/openssh/openssh-portable/blob/715c892f0a5295b391ae92c26ef4d6a86ea96e8e/loginrec.c#L476
    
    Testing :
    =========
    1. Authentication successfull - below is the event generated in
     /var/log/audit/audit.log file
    type=USER_LOGIN msg=audit(1658338272.749:43): pid=2136 uid=0 auid=4294967295
    ses=4294967295 msg='op=login id=0 exe="/usr/bin/bmcweb" hostname=rain135bmc
    addr=::ffff:9.43.24.189 terminal=? res=success'UID="root" AUID="unset" ID="root"
    
    2. Authentication failed
    
    type=USER_LOGIN msg=audit(1658338293.749:44): pid=2136 uid=0 auid=4294967295
    ses=4294967295 msg='op=login id=0 exe="/usr/bin/bmcweb" hostname=rain135bmc
    addr=::ffff:9.43.24.189 terminal=? res=failed'UID="root" AUID="unset" ID="root"
    
    3. Using aureport to generate a login report
    root@rain135bmc:~# aureport -l
    
    Login Report
    ============================================
    ============================================
    1. 07/20/22 17:31:12 -1 rain135bmc ? /usr/bin/bmcweb yes 43
    2. 07/20/22 17:31:33 -1 rain135bmc ? /usr/bin/bmcweb no 44
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: I20838adcc613757bec0640473d63d27e2deef2c3

---
+++ include/audit_events.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <libaudit.h>
+
+namespace audit
+{
+
+void audit_acct_event(int type, const char* username, uid_t uid,
+                      const char* remote_host_name,
+                      const char* remote_ip_address, const char* tty,
+                      bool success)
+{
+#ifdef BMCWEB_ENABLE_LINUX_AUDIT_EVENTS
+    int auditfd, result;
+    const char* op = NULL;
+
+    auditfd = audit_open();
+    if (auditfd < 0)
+    {
+        BMCWEB_LOG_ERROR << "Error opening audit socket : " << strerror(errno);
+        return;
+    }
+    if (type == AUDIT_USER_LOGIN)
+        op = "login";
+    else if (type == AUDIT_USER_LOGOUT)
+        op = "logout";
+    result = success == true ? 1 : 0;
+
+    if (audit_log_acct_message(auditfd, type, NULL, op, username, uid,
+                               remote_host_name, remote_ip_address, tty,
+                               result) <= 0)
+        BMCWEB_LOG_ERROR << "Error writing audit message: " << strerror(errno);
+
+    close(auditfd);
+#endif
+}
+
+} // namespace audit
