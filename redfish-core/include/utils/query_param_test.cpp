3648c8be1c03c8ebe099185bc651613862fc0027
Ed Tanous <edtanous@google.com>
2022-07-25 13:39:59 -0700

    Query param: fix regression in top parameters
    With the inclusion of ce8ea743055f1b82c60790db40aa3295e03bdf9c it looks
    like we're now returning 400 error codes, with a response error of
    QueryNotSupportedOnResource for resources which don't support top and
    skip (like RegistryFile).  This would imply that the Query object NEEDS
    a way to represent "user didn't provide us a skip/top parameter" which
    arguably means this needs to go back to a std::optional<size_t>.
    
    The error gets added from:
    https://github.com/openbmc/bmcweb/blob/d5c80ad9c07b94465d8ea62d2b6f87c30cac765e/redfish-core/include/utils/query_param.hpp#L304
    and appears to be a basic logic error in that now all queries assume
    that the user provided top and skip, which fails for non-collections.
    
    This commit moves that direction, changing the Query object back to
    std::optional<size_t>.  This has the unintended consequence of now
    putting the idea of "defaults" back into the per-delegated handlers. This
    seems reasonable, as arguably the defaults for each individual collection
    are going to be different, and at some point we might want to take advant
    age of that.
    
    Tested:
    1. Tested on Romulus QEMU. All passed.
    2. Tested on s7106, Validator passed.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Signed-off-by: Nan Zhou <nanzhoumails@gmail.com>
    Change-Id: I9f912957d130694b281c6e391602de158eaddcb3

--- redfish-core/include/utils/query_param_test.cpp
+++ redfish-core/include/utils/query_param_test.cpp
@@ -81,7 +81,7 @@ TEST(Delegate, TopNegative)
         .top = 42,
     };
     Query delegated = delegate(QueryCapabilities{}, query);
-    EXPECT_EQ(delegated.top, maxEntriesPerPage);
+    EXPECT_EQ(delegated.top, std::nullopt);
     EXPECT_EQ(query.top, 42);
 }
 
@@ -95,7 +95,7 @@ TEST(Delegate, TopPositive)
     };
     Query delegated = delegate(capabilities, query);
     EXPECT_EQ(delegated.top, 42);
-    EXPECT_EQ(query.top, maxEntriesPerPage);
+    EXPECT_EQ(query.top, std::nullopt);
 }
 
 TEST(Delegate, SkipNegative)
@@ -104,7 +104,7 @@ TEST(Delegate, SkipNegative)
         .skip = 42,
     };
     Query delegated = delegate(QueryCapabilities{}, query);
-    EXPECT_EQ(delegated.skip, 0);
+    EXPECT_EQ(delegated.skip, std::nullopt);
     EXPECT_EQ(query.skip, 42);
 }
 
