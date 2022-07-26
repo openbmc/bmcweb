e82d2f938dc75a7aa1db977c83ea1355e20fcb6f
Willy Tu <wltu@google.com>
2022-07-26 00:41:55 +0000

    storage: add support for multiple storages
    Updated Storage resource to `#Storage.v1_9_1.Storage` to support the
    change.
    
    Follow the Swordfish spec to setup the Storage relationship[1].
    
    There will now be two Storage Collection `/redfish/v1/Stroage` and
    `/redfish/v1/Systems/system/Storage`. The storage in `/Storage` will be
    treated as a subsystem and only link to the `/Systems/system/Storage`
    under `Links/StorageServices` resource.
    
    The `/Storage` won't contain Drives or StorageControllers.
    
    Tested:
    Passed Redfish Validator for related resources.
    ```
    *** /redfish/v1/Storage/storage_1
    INFO - 	 Type (Storage.v1_7_1.Storage), GET SUCCESS (time: 0)
    WARNING - StorageControllers: The given property is deprecated: This property has been deprecated in favor of Controllers to allow for storage controllers to be represented as their own resources.
    INFO - Attempt 1 of /redfish/v1/Chassis/chassis0/Drives/drive0
    INFO - Response Time for GET to /redfish/v1/Chassis/chassis_0/Drives/drive_0: 0.07591272401623428 seconds.
    INFO - 	 PASS
    INFO -
    ```
    
    Chassis
    ```
    wget -qO- http://localhost:80/redfish/v1/Chassis/chassis0
    {
        "@odata.id": "/redfish/v1/Chassis/chassis0",
        "@odata.type": "#Chassis.v1_14_0.Chassis",
        "Id": "chassis0",
        "Links": {
            "Storage": [
              {
                    "@odata.id": "/redfish/v1/Systems/system/Storage/storage0"
              }
            ],
            "Storage@odata.count": 1
        },
        "Name": "chassis0",
    }}
    ```
    
    Storage Collection
    ```
    wget -qO- http://localhost:80/redfish/v1/Storage
    {
      "@odata.id": "/redfish/v1/Storage",
      "@odata.type": "#StorageCollection.StorageCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Storage/storage0"
        }
      ],
      "Members@odata.count": 1,
      "Name": "Storage Collection"
    }
    
    wget -qO- http://localhost:80/redfish/v1/Systems/system/Storage
    {
      "@odata.id": "/redfish/v1/Systems/system/Storage",
      "@odata.type": "#StorageCollection.StorageCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Storage/storage0"
        }
      ],
      "Members@odata.count": 1,
      "Name": "Storage Collection"
    }
    ```
    
    Storage
    ```
    wget -qO- http://localhost:80/redfish/v1/Storage/storage0
    {
      "@odata.id": "/redfish/v1/Storage/storage0",
      "@odata.type": "#Storage.v1_9_1.Storage",
      "Id": "storage0",
      "Links": {
        "StorageServices": [
          {
            "@odata.id": "/redfish/v1/Systems/system/Storage/storage0"
          }
        ],
        "StorageServices@odata.count": 1
      },
      "Name": "Storage",
      "Status": {
        "State": "Enabled"
      }
    }
    
    wget -qO- http://localhost:80/redfish/v1/Systems/system/Storage/storage0
    {
      "@odata.id": "/redfish/v1/Systems/system/Storage/storage0",
      "@odata.type": "#Storage.v1_9_1.Storage",
      "Drives": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis0/Drives/drive0"
        }
      ],
      "Drives@odata.count": 1,
      "Id": "storage0",
      "Name": "Storage",[1]
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "Enabled"
      },
      "StorageControllers": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Storage/storage0#/StorageControllers/0",
          "@odata.type": "#Storage.v1_7_0.StorageController",
          "MemberId": "controller",
          "Name": "controller",
          "Status": {
            "Health": "OK",
            "HealthRollup": "OK",
            "State": "Enabled"
          }
        }
      ]
    }
    ```
    
    [1] https://www.snia.org/sites/default/files/technical-work/swordfish/draft/v1.2.2/pdf/Swordfish_v1.2.2_NVMeMappingGuide.pdf#page=17
    
    Patch Tracking Bug: b/232038811
    Upstream info / review: http://go/openbmccl/43744
    Upstream-Status: Submitted
    Justification: The patch is needed to support multiple Storage/Drive.
    The plan is to upstream this by 2023 Q1 if possible.
    Change-Id: Ib81b68e7f61b817d4dfa4ed2f27afd6e74e8ce58
    Signed-off-by: Willy Tu <wltu@google.com>
    Signed-off-by: Tom Tung <shes050117@gmail.com>
    Signed-off-by: Willy Tu <wltu@google.com>

--- redfish-core/lib/storage.hpp
+++ redfish-core/lib/storage.hpp
@@ -58,12 +58,36 @@ inline void requestRoutesStorageCollection(App& app)
         asyncResp->res.jsonValue["@odata.id"] =
             "/redfish/v1/Systems/system/Storage";
         asyncResp->res.jsonValue["Name"] = "Storage Collection";
-        nlohmann::json::array_t members;
-        nlohmann::json::object_t member;
-        member["@odata.id"] = "/redfish/v1/Systems/system/Storage/1";
-        members.emplace_back(member);
-        asyncResp->res.jsonValue["Members"] = std::move(members);
-        asyncResp->res.jsonValue["Members@odata.count"] = 1;
+
+        constexpr std::array<std::string_view, 1> interface {
+            "xyz.openbmc_project.Inventory.Item.Storage"
+        };
+        collection_util::getCollectionMembers(
+            asyncResp,
+            crow::utility::urlFromPieces("redfish", "v1", "Systems", "system",
+                                         "Storage"),
+            interface);
+        });
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Storage/")
+        .privileges(redfish::privileges::getStorageCollection)
+        .methods(boost::beast::http::verb::get)(
+            [&app](const crow::Request& req,
+                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
+        if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+        {
+            return;
+        }
+        asyncResp->res.jsonValue["@odata.type"] =
+            "#StorageCollection.StorageCollection";
+        asyncResp->res.jsonValue["@odata.id"] = "/redfish/v1/Storage";
+        asyncResp->res.jsonValue["Name"] = "Storage Collection";
+        constexpr std::array<std::string_view, 1> interface {
+            "xyz.openbmc_project.Inventory.Item.Storage"
+        };
+        collection_util::getCollectionMembers(
+            asyncResp, crow::utility::urlFromPieces("redfish", "v1", "Storage"),
+            interface);
         });
 }
 
@@ -114,28 +138,134 @@ inline void getDrives(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
 
 inline void requestRoutesStorage(App& app)
 {
-    BMCWEB_ROUTE(app, "/redfish/v1/Systems/system/Storage/1/")
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/Storage/<str>/")
         .privileges(redfish::privileges::getStorage)
         .methods(boost::beast::http::verb::get)(
             [&app](const crow::Request& req,
-                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
+                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& systemName,
+                   const std::string& storageId) {
         if (!redfish::setUpRedfishRoute(app, req, asyncResp))
         {
             return;
         }
-        asyncResp->res.jsonValue["@odata.type"] = "#Storage.v1_13_0.Storage";
-        asyncResp->res.jsonValue["@odata.id"] =
-            "/redfish/v1/Systems/system/Storage/1";
-        asyncResp->res.jsonValue["Name"] = "Storage";
-        asyncResp->res.jsonValue["Id"] = "1";
-        asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
+        if (systemName != "system")
+        {
+            messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                       systemName);
+            return;
+        }
 
-        auto health = std::make_shared<HealthPopulate>(asyncResp);
-        health->populate();
+        constexpr std::array<std::string_view, 1> interfaces = {
+            "xyz.openbmc_project.Inventory.Item.Storage"};
+        dbus::utility::getSubTree(
+            "/xyz/openbmc_project/inventory", 0, interfaces,
+            [asyncResp, storageId](
+                const boost::system::error_code ec,
+                const dbus::utility::MapperGetSubTreeResponse& subtree) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG << "requestRoutesStorage DBUS response error";
+                messages::resourceNotFound(
+                    asyncResp->res, "#Storage.v1_13_0.Storage", storageId);
+                return;
+            }
+            auto storage = std::find_if(
+                subtree.begin(), subtree.end(),
+                [&storageId](
+                    const std::pair<std::string,
+                                    dbus::utility::MapperServiceMap>& object) {
+                return sdbusplus::message::object_path(object.first)
+                           .filename() == storageId;
+                });
+            if (storage == subtree.end())
+            {
+                messages::resourceNotFound(
+                    asyncResp->res, "#Storage.v1_13_0.Storage", storageId);
+                return;
+            }
 
-        getDrives(asyncResp, health);
-        asyncResp->res.jsonValue["Controllers"]["@odata.id"] =
-            "/redfish/v1/Systems/system/Storage/1/Controllers";
+            asyncResp->res.jsonValue["@odata.type"] =
+                "#Storage.v1_13_0.Storage";
+            asyncResp->res.jsonValue["@odata.id"] =
+                crow::utility::urlFromPieces("redfish", "v1", "Systems",
+                                             "system", "Storage", storageId);
+            asyncResp->res.jsonValue["Name"] = "Storage";
+            asyncResp->res.jsonValue["Id"] = storageId;
+            asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
+
+            auto health = std::make_shared<HealthPopulate>(asyncResp);
+            health->populate();
+
+            getDrives(asyncResp, health);
+            asyncResp->res.jsonValue["Controllers"]["@odata.id"] =
+                crow::utility::urlFromPieces("redfish", "v1", "Systems",
+                                             "system", "Storage", storageId,
+                                             "Controllers");
+            });
+        });
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Storage/<str>/")
+        .privileges(redfish::privileges::getStorage)
+        .methods(boost::beast::http::verb::get)(
+            [&app](const crow::Request& req,
+                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& storageId) {
+        if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+        {
+            BMCWEB_LOG_DEBUG << "requestRoutesStorage setUpRedfishRoute failed";
+            return;
+        }
+
+        constexpr std::array<std::string_view, 1> interfaces = {
+            "xyz.openbmc_project.Inventory.Item.Storage"};
+        dbus::utility::getSubTree(
+            "/xyz/openbmc_project/inventory", 0, interfaces,
+            [asyncResp, storageId](
+                const boost::system::error_code ec,
+                const dbus::utility::MapperGetSubTreeResponse& subtree) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG << "requestRoutesStorage DBUS response error";
+                messages::resourceNotFound(
+                    asyncResp->res, "#Storage.v1_13_0.Storage", storageId);
+                return;
+            }
+            auto storage = std::find_if(
+                subtree.begin(), subtree.end(),
+                [&storageId](
+                    const std::pair<std::string,
+                                    dbus::utility::MapperServiceMap>& object) {
+                return sdbusplus::message::object_path(object.first)
+                           .filename() == storageId;
+                });
+            if (storage == subtree.end())
+            {
+                messages::resourceNotFound(
+                    asyncResp->res, "#Storage.v1_13_0.Storage", storageId);
+                return;
+            }
+
+            asyncResp->res.jsonValue["@odata.type"] =
+                "#Storage.v1_13_0.Storage";
+            asyncResp->res.jsonValue["@odata.id"] =
+                crow::utility::urlFromPieces("redfish", "v1", "Storage",
+                                             storageId);
+            asyncResp->res.jsonValue["Name"] = "Storage";
+            asyncResp->res.jsonValue["Id"] = storageId;
+            asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
+
+            // Storage subsystem to Stroage link.
+            nlohmann::json::array_t storageServices;
+            nlohmann::json::object_t storageService;
+            storageService["@odata.id"] = crow::utility::urlFromPieces(
+                "redfish", "v1", "Systems", "system", "Storage", storageId);
+            storageServices.emplace_back(storageService);
+            asyncResp->res.jsonValue["Links"]["StorageServices"] =
+                std::move(storageServices);
+            asyncResp->res.jsonValue["Links"]["StorageServices@odata.count"] =
+                1;
+            });
         });
 }
 
@@ -610,7 +740,6 @@ inline void buildDrive(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                        const boost::system::error_code& ec,
                        const dbus::utility::MapperGetSubTreeResponse& subtree)
 {
-
     if (ec)
     {
         BMCWEB_LOG_DEBUG << "DBUS response error " << ec;
@@ -658,7 +787,6 @@ inline void
                       const std::string& driveName,
                       const std::vector<std::string>& resp)
 {
-
     for (const std::string& drivePath : resp)
     {
         sdbusplus::message::object_path path(drivePath);
