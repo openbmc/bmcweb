e6094b657e803139caf285041ce4542502f27203
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2022-07-28 15:44:22 +0200

    used sdbusplus::unpackPropertiesNoThrow part 1
    used sdbusplus::unpackPropertiesNoThrow in processor.hpp, also replaced
    all usages of "GetAll" with sdbusplus::asio::getAllProperties
    
        bmcweb size: 2917424 -> 2913336 (-4088)
    compressed size: 1193677 -> 1191485 (-2192)
    
    Tested:
      Performed get on:
      - redfish/v1/Systems/system/Processors
      - redfish/v1/Systems/system/Processors/cpu0
    
      Get result before and after the change was the same
    
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: Ib28d842e348ebd8f160ec23b629ee4c4b280329b

---
+++ redfish-core/include/utils/dbus_utils.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "logging.hpp"
+
+#include <sdbusplus/unpack_properties.hpp>
+
+namespace redfish
+{
+namespace dbus_utils
+{
+
+struct UnpackErrorPrinter
+{
+    void operator()(const sdbusplus::UnpackErrorReason reason,
+                    const std::string& property) const noexcept
+    {
+        BMCWEB_LOG_DEBUG
+            << "DBUS property error in property: " << property << ", reason: "
+            << static_cast<
+                   std::underlying_type_t<sdbusplus::UnpackErrorReason>>(
+                   reason);
+    }
+};
+
+} // namespace dbus_utils
+} // namespace redfish
