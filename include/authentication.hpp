4077d65daa9d4123ab20ced9c19990fbcd592b2e
Ed Tanous <edtanous@google.com>
2022-08-02 17:07:54 -0700

    WIP: Move ClientID parameter out of OEM
    Redfish might add support for the Context parameter on the session
    interface.  This parameter has the same function that the
    OemSession.ClientId field served.  This commit moves all the existing
    ClientId code to produce Context as well.
    
    Functionally, this has one important difference, in that Context in
    Redfish is optionally provided by the user, which means we need to omit
    it if not given by the user.  The old implementation left it set to
    empty string ("").
    
    Because of this, a few minor interfaces need to change to use
    std::optional.  Existing uses of clientId are moved to using
    value_or("") to keep the same behavior as before.
    
    Tested: WIP.  Will test once we have a decision on the "do we want to
    support the old" as well as once the latest Redfish spec is released.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I4df358623f93f3e6cb659e99970ad909cefebc62

--- include/authentication.hpp
+++ include/authentication.hpp
@@ -84,9 +84,8 @@ static std::shared_ptr<persistent_data::UserSession>
     // needed.
     // This whole flow needs to be revisited anyway, as we can't be
     // calling directly into pam for every request
-    std::string unsupportedClientId;
     return persistent_data::SessionStore::getInstance().generateUserSession(
-        user, clientIp, unsupportedClientId,
+        user, clientIp, std::nullopt,
         persistent_data::PersistenceType::SINGLE_REQUEST, isConfigureSelfOnly);
 }
 #endif
