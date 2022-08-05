13ea269c91c8fbbde012621063e03a5a54cd79be
Ed Tanous <edtanous@google.com>
2022-08-05 14:10:34 -0700

    WIP: Create resource abstraction
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I5dff3d87ea3e3dde3721c1c97780188797ab4bd3

---
+++ redfish-core/include/resource.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "http_response.hpp"
+
+#include <string>
+
+namespace redfish
+{
+inline void declareResource(crow::Response& res,
+                            std::string_view schemaNamespace,
+                            std::string_view version,
+                            std::string_view schemaName)
+{
+    std::string type;
+    type.reserve(schemaNamespace.size() + version.size() + schemaName.size() +
+                 4);
+    type += '#';
+    type += schemaNamespace;
+    type += '.';
+    type += 'v';
+    type += version;
+    type += '.';
+    type += schemaName;
+    res.jsonValue["@odata.type"] = std::move(type);
+
+    std::string link("</redfish/v1/JsonSchemas/");
+    link += schemaNamespace;
+    link += '/';
+    link += schemaNamespace;
+    link += ".json>; rel=describedby";
+}
+} // namespace redfish
