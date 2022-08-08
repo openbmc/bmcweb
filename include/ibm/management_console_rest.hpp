33db830ffd70422079bfd85c40507b45c6df4ffd
Sunitha Harish <sunithaharish04@gmail.com>
2022-08-08 02:08:39 -0500

    IBM Mgmt console lock algorithm improvement
    This commit optimizes the release lock code and adds some traces to give
    more data for lock conflict scenarios
    
    Tested by:
     1. With dual client connected, verified the conflicts are returned
     2. Tested releaseLock usecase
    
    Signed-off-by: Sunitha Harish <sunithaharish04@gmail.com>
    Change-Id: I3cf99aaa5cc7c2967ae8dbc9c76c9f7378ecebdd

--- include/ibm/management_console_rest.hpp
+++ include/ibm/management_console_rest.hpp
@@ -489,7 +489,7 @@ inline void
         }
         if (validityStatus.first && (validityStatus.second == 1))
         {
-            BMCWEB_LOG_DEBUG << "There is a conflict within itself";
+            BMCWEB_LOG_ERROR << "There is a conflict within itself";
             asyncResp->res.result(boost::beast::http::status::conflict);
             return;
         }
@@ -530,7 +530,7 @@ inline void
         }
 
         returnJson["SegmentFlags"] = myarray;
-
+        BMCWEB_LOG_ERROR << "Conflicting lock record: " << returnJson;
         asyncResp->res.jsonValue["Record"] = returnJson;
         return;
     }
@@ -564,6 +564,7 @@ inline void
     if (!varReleaselock.first)
     {
         // validation Failed
+        BMCWEB_LOG_ERROR << "handleReleaseLockAPI: validation failed";
         asyncResp->res.result(boost::beast::http::status::bad_request);
         return;
     }
@@ -598,6 +599,7 @@ inline void
     }
 
     returnJson["SegmentFlags"] = myArray;
+    BMCWEB_LOG_DEBUG << "handleReleaseLockAPI: lockrecord: " << returnJson;
     asyncResp->res.jsonValue["Record"] = returnJson;
 }
 
