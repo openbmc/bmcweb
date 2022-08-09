d8f13997aa13642f0be0a12b7a38c4b0a4cc5d0b
Ravi Teja <raviteja28031990@gmail.com>
2022-08-09 03:18:38 -0500

    AccountService: Handle InvalidArgument D-bus errors
    Currently LDAP configuration D-bus errors are mapped to Redfish
    InternalError so it returns internalError irrespective of D-bus
    error.
    
    This commit handles InvalidArgument D-bus error for LDAP config
    and maps to PropertyValueIncorrect redfish error.
    
    Tested By:
    Configure LDAP with various invalid arguments for ldap properties
    "LdapServerURI", "BaseDistinguishedNames" and "RemoteRoleMapping"
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I6adaedd936fb3d9d906750649792a4d414b54b73

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -265,10 +265,27 @@ inline void handleRoleMapPatch(
                 {
                     crow::connections::systemBus->async_method_call(
                         [asyncResp, roleMapObjData, serverType, index,
-                         remoteGroup](const boost::system::error_code ec) {
+                         remoteGroup](const boost::system::error_code ec,
+                                      const sdbusplus::message::message& msg) {
                         if (ec)
                         {
                             BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+                            const sd_bus_error* dbusError = msg.get_error();
+                            if (dbusError == nullptr)
+                            {
+                                messages::internalError(asyncResp->res);
+                                return;
+                            }
+                            if ((strcmp(
+                                     dbusError->name,
+                                     "xyz.openbmc_project.Common.Error.InvalidArgument") ==
+                                 0))
+                            {
+                                messages::propertyValueFormatError(
+                                    asyncResp->res, "RemoteGroup",
+                                    *remoteGroup);
+                                return;
+                            }
                             messages::internalError(asyncResp->res);
                             return;
                         }
@@ -289,10 +306,27 @@ inline void handleRoleMapPatch(
                 {
                     crow::connections::systemBus->async_method_call(
                         [asyncResp, roleMapObjData, serverType, index,
-                         localRole](const boost::system::error_code ec) {
+                         localRole](const boost::system::error_code ec,
+                                    const sdbusplus::message::message& msg) {
                         if (ec)
                         {
                             BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+                            const sd_bus_error* dbusError = msg.get_error();
+                            if (dbusError == nullptr)
+                            {
+                                messages::internalError(asyncResp->res);
+                                return;
+                            }
+
+                            if ((strcmp(
+                                     dbusError->name,
+                                     "xyz.openbmc_project.Common.Error.InvalidArgument") ==
+                                 0))
+                            {
+                                messages::propertyValueFormatError(
+                                    asyncResp->res, "LocalRole", *localRole);
+                                return;
+                            }
                             messages::internalError(asyncResp->res);
                             return;
                         }
@@ -618,11 +652,27 @@ inline void handleServiceAddressPatch(
 {
     crow::connections::systemBus->async_method_call(
         [asyncResp, ldapServerElementName,
-         serviceAddressList](const boost::system::error_code ec) {
+         serviceAddressList](const boost::system::error_code ec,
+                             sdbusplus::message::message& msg) {
         if (ec)
         {
             BMCWEB_LOG_DEBUG
                 << "Error Occurred in updating the service address";
+            const sd_bus_error* dbusError = msg.get_error();
+            if (dbusError == nullptr)
+            {
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            if ((strcmp(dbusError->name,
+                        "xyz.openbmc_project.Common.Error.InvalidArgument") ==
+                 0))
+            {
+                messages::propertyValueFormatError(asyncResp->res,
+                                                   "ServiceAddresses",
+                                                   serviceAddressList.front());
+                return;
+            }
             messages::internalError(asyncResp->res);
             return;
         }
@@ -725,10 +775,26 @@ inline void
 {
     crow::connections::systemBus->async_method_call(
         [asyncResp, baseDNList,
-         ldapServerElementName](const boost::system::error_code ec) {
+         ldapServerElementName](const boost::system::error_code ec,
+                                const sdbusplus::message::message& msg) {
         if (ec)
         {
             BMCWEB_LOG_DEBUG << "Error Occurred in Updating the base DN";
+            const sd_bus_error* dbusError = msg.get_error();
+            if (dbusError == nullptr)
+            {
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            if ((strcmp(dbusError->name,
+                        "xyz.openbmc_project.Common.Error.InvalidArgument") ==
+                 0))
+            {
+                messages::propertyValueFormatError(asyncResp->res,
+                                                   "BaseDistinguishedNames",
+                                                   baseDNList.front());
+                return;
+            }
             messages::internalError(asyncResp->res);
             return;
         }
