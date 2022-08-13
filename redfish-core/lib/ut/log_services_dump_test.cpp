cfee7ed9401b6e6c78b32f0e6557b959498487de
Claire Weinan <cweinan@google.com>
2022-08-12 18:20:17 -0700

    LogService: System dump calls common dump handlers
    This patchset transitions System dump route handlers to the new style
    of calling common dump handlers via std::bind_front() instead of
    defining a lambda. BMC dump and FaultLog dump were previously
    transitioned to the new style (see "LogService: Add support for Fault
    Log" at https://gerrit.openbmc.org/c/openbmc/bmcweb/+/53175).
    
    Note that System dump wasn't using a common dump handler for outputting
    its LogService info (/redfish/v1/Systems/system/LogServices/Dump/)
    before, so calling the common handler here is new.
    
    No expected client impact.
    
    Tested:
    
    Get System dump LogService info:
    curl -k -H "X-Auth-Token: $token" -X GET http://${bmc}/redfish/v1/Systems/system/LogServices/Dump/
    
    (Diffed the ouput before and after applying this patchset and confirmed
    they were equivalent except for the "DateTime", as expected. Also added
    unit tests for getting dump LogService info.)
    
    Create System dump entry. (Existing code for creating an entry via the
    Redfish LogService action CollectDiagnosticData isn't currently working
    for System dump, so instead directly call the corresponding D-Bus
    method by running the following from the BMC console):
    busctl call xyz.openbmc_project.Dump.Manager /xyz/openbmc_project/dump/system xyz.openbmc_project.Dump.Create CreateDump a{sv} 0
    
    Get Entry:
    curl -k -H "X-Auth-Token: $token" -X GET http://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/1
    
    Get Entry Collection:
    curl -k -H "X-Auth-Token: $token" -X GET http://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries
    
    Delete Entry:
    curl -k -H "X-Auth-Token: $token" -X DELETE http://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/1
    
    Clear Log:
    curl -k -H "X-Auth-Token: $token" -X POST http://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.ClearLog
    
    Redfish Service Validator succeeded on the following URI tree:
    /redfish/v1/Systems/system/LogServices/Dump
    
    Signed-off-by: Claire Weinan <cweinan@google.com>
    Change-Id: I5d66da17794c29672be8713481018bf3ce397ddf

---
+++ redfish-core/lib/ut/log_services_dump_test.cpp
@@ -0,0 +1,108 @@
+#include "app.hpp"
+#include "event_service_manager.hpp"
+#include "include/async_resp.hpp"
+#include "redfish-core/lib/health.hpp"
+#include "redfish-core/lib/log_services.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+void assertLogServicesDumpServiceGet(crow::Response& res)
+{
+    nlohmann::json& json = res.jsonValue;
+    EXPECT_EQ(json["@odata.type"], "#LogService.v1_2_0.LogService");
+    EXPECT_EQ(json["Name"], "Dump LogService");
+}
+
+void assertLogServicesBMCDumpServiceGet(crow::Response& res)
+{
+    assertLogServicesDumpServiceGet(res);
+
+    nlohmann::json& json = res.jsonValue;
+    EXPECT_EQ(json["@odata.id"], "/redfish/v1/Managers/bmc/LogServices/Dump");
+    EXPECT_EQ(
+        json["Actions"]["#LogService.ClearLog"]["target"],
+        "/redfish/v1/Managers/bmc/LogServices/Dump/Actions/LogService.ClearLog");
+    EXPECT_EQ(
+        json["Actions"]["#LogService.CollectDiagnosticData"]["target"],
+        "/redfish/v1/Managers/bmc/LogServices/Dump/Actions/LogService.CollectDiagnosticData");
+    EXPECT_EQ(json["Description"], "BMC Dump LogService");
+    EXPECT_EQ(json["Entries"]["@odata.id"],
+              "/redfish/v1/Managers/bmc/LogServices/Dump/Entries");
+    EXPECT_EQ(json["Id"], "Dump");
+    EXPECT_EQ(json["OverWritePolicy"], "WrapsWhenFull");
+}
+
+void assertLogServicesFaultLogDumpServiceGet(crow::Response& res)
+{
+    assertLogServicesDumpServiceGet(res);
+
+    nlohmann::json& json = res.jsonValue;
+    EXPECT_EQ(json["@odata.id"],
+              "/redfish/v1/Managers/bmc/LogServices/FaultLog");
+    EXPECT_EQ(
+        json["Actions"]["#LogService.ClearLog"]["target"],
+        "/redfish/v1/Managers/bmc/LogServices/FaultLog/Actions/LogService.ClearLog");
+    EXPECT_EQ(json["Actions"]["#LogService.CollectDiagnosticData"]["target"],
+              nlohmann::detail::value_t::null);
+    EXPECT_EQ(json["Description"], "FaultLog Dump LogService");
+    EXPECT_EQ(json["Entries"]["@odata.id"],
+              "/redfish/v1/Managers/bmc/LogServices/FaultLog/Entries");
+    EXPECT_EQ(json["Id"], "FaultLog");
+    EXPECT_EQ(json["OverWritePolicy"], "Unknown");
+}
+
+void assertLogServicesSystemDumpServiceGet(crow::Response& res)
+{
+    assertLogServicesDumpServiceGet(res);
+
+    nlohmann::json& json = res.jsonValue;
+    EXPECT_EQ(json["@odata.id"], "/redfish/v1/Systems/system/LogServices/Dump");
+    EXPECT_EQ(
+        json["Actions"]["#LogService.ClearLog"]["target"],
+        "/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.ClearLog");
+    EXPECT_EQ(
+        json["Actions"]["#LogService.CollectDiagnosticData"]["target"],
+        "/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.CollectDiagnosticData");
+    EXPECT_EQ(json["Description"], "System Dump LogService");
+    EXPECT_EQ(json["Entries"]["@odata.id"],
+              "/redfish/v1/Systems/system/LogServices/Dump/Entries");
+    EXPECT_EQ(json["Id"], "Dump");
+    EXPECT_EQ(json["OverWritePolicy"], "WrapsWhenFull");
+}
+
+TEST(LogServicesDumpServiceTest,
+     LogServicesBMCDumpServiceStaticAttributesAreExpected)
+{
+    auto shareAsyncResp = std::make_shared<bmcweb::AsyncResp>();
+    shareAsyncResp->res.setCompleteRequestHandler(
+        assertLogServicesBMCDumpServiceGet);
+    redfish::getDumpServiceInfo(shareAsyncResp, "BMC");
+}
+
+TEST(LogServicesDumpServiceTest,
+     LogServicesFaultLogDumpServiceStaticAttributesAreExpected)
+{
+    auto shareAsyncResp = std::make_shared<bmcweb::AsyncResp>();
+    shareAsyncResp->res.setCompleteRequestHandler(
+        assertLogServicesFaultLogDumpServiceGet);
+    redfish::getDumpServiceInfo(shareAsyncResp, "FaultLog");
+}
+
+TEST(LogServicesDumpServiceTest,
+     LogServicesSystemDumpServiceStaticAttributesAreExpected)
+{
+    auto shareAsyncResp = std::make_shared<bmcweb::AsyncResp>();
+    shareAsyncResp->res.setCompleteRequestHandler(
+        assertLogServicesSystemDumpServiceGet);
+    redfish::getDumpServiceInfo(shareAsyncResp, "System");
+}
+
+} // namespace
+} // namespace redfish
