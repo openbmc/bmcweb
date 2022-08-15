db1f955946d6e8e9323bcd5d31c83dabd9d06f02
Ed Tanous <edtanous@google.com>
2022-08-15 10:09:23 -0700

    Add unit tests for ipv4VerifyIpAndGetBitcount
    Per the title, add unit tests for this function.
    
    Tested: Unit tests pass
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ifdd9c314d2fa62ae3fa3b8f8150fcdd224a7eb03

--- redfish-core/ut/ip_utils_test.cpp
+++ redfish-core/ut/ip_utils_test.cpp
@@ -34,5 +34,37 @@ TEST(IpToString, ReturnsCorrectIpStringForIpv4MappedIpv6Addresses)
 {
     EXPECT_EQ(toString(make_address("::ffff:127.0.0.1")), "127.0.0.1");
 }
+
+TEST(ipv4VerifyIpAndGetBitcount, PositiveTests)
+{
+    EXPECT_TRUE(ipv4VerifyIpAndGetBitcount("192.168.1.1", nullptr));
+
+    uint8_t bits = 0;
+    EXPECT_TRUE(ipv4VerifyIpAndGetBitcount("255.255.255.0", &bits));
+    EXPECT_EQ(bits, 24);
+    EXPECT_TRUE(ipv4VerifyIpAndGetBitcount("255.255.255.128", &bits));
+    EXPECT_EQ(bits, 25);
+}
+
+TEST(ipv4VerifyIpAndGetBitcount, NegativeTests)
+{
+    uint8_t bits = 0;
+    // > 256 in any field
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("256.0.0.0", &bits));
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("1.256.0.0", &bits));
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("1.1.256.0", &bits));
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("1.1.1.256", &bits));
+
+    // Non contiguous mask
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("255.0.255.0", &bits));
+
+    // Too many fields
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("1.1.1.1.1", &bits));
+    // Not enough fields
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("1.1.1", &bits));
+
+    // Empty string
+    EXPECT_FALSE(ipv4VerifyIpAndGetBitcount("", &bits));
+}
 } // namespace
 } // namespace redfish::ip_util
