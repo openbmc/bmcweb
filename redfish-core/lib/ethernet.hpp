22951fef54ce5c7341bec090d170bbda143ab1e0
Ed Tanous <edtanous@google.com>
2022-08-15 09:47:37 -0700

    Move ipv4VerifyIpAndGetBitcount into ip_utils
    This is a minor reorganization of helper code from ethernet (which is
    redfish specific) into utils.  This function is generic, and should be
    in ip utils.
    
    Tested: Code compiles.  Code move only.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I4aac5a98379c9844aea6c21d2294d1ed7ae2ea9b

--- redfish-core/lib/ethernet.hpp
+++ redfish-core/lib/ethernet.hpp
@@ -15,6 +15,8 @@
 */
 #pragma once
 
+#include "utils/ip_utils.hpp"
+
 #include <app.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
@@ -578,96 +580,6 @@ inline void
             }
         }
     }
-}
-
-/**
- * @brief Helper function that verifies IP address to check if it is in
- *        proper format. If bits pointer is provided, also calculates active
- *        bit count for Subnet Mask.
- *
- * @param[in]  ip     IP that will be verified
- * @param[out] bits   Calculated mask in bits notation
- *
- * @return true in case of success, false otherwise
- */
-inline bool ipv4VerifyIpAndGetBitcount(const std::string& ip,
-                                       uint8_t* bits = nullptr)
-{
-    std::vector<std::string> bytesInMask;
-
-    boost::split(bytesInMask, ip, boost::is_any_of("."));
-
-    static const constexpr int ipV4AddressSectionsCount = 4;
-    if (bytesInMask.size() != ipV4AddressSectionsCount)
-    {
-        return false;
-    }
-
-    if (bits != nullptr)
-    {
-        *bits = 0;
-    }
-
-    char* endPtr = nullptr;
-    long previousValue = 255;
-    bool firstZeroInByteHit = false;
-    for (const std::string& byte : bytesInMask)
-    {
-        if (byte.empty())
-        {
-            return false;
-        }
-
-        // Use strtol instead of stroi to avoid exceptions
-        long value = std::strtol(byte.c_str(), &endPtr, 10);
-
-        // endPtr should point to the end of the string, otherwise given string
-        // is not 100% number
-        if (*endPtr != '\0')
-        {
-            return false;
-        }
-
-        // Value should be contained in byte
-        if (value < 0 || value > 255)
-        {
-            return false;
-        }
-
-        if (bits != nullptr)
-        {
-            // Mask has to be continuous between bytes
-            if (previousValue != 255 && value != 0)
-            {
-                return false;
-            }
-
-            // Mask has to be continuous inside bytes
-            firstZeroInByteHit = false;
-
-            // Count bits
-            for (long bitIdx = 7; bitIdx >= 0; bitIdx--)
-            {
-                if ((value & (1L << bitIdx)) != 0)
-                {
-                    if (firstZeroInByteHit)
-                    {
-                        // Continuity not preserved
-                        return false;
-                    }
-                    (*bits)++;
-                }
-                else
-                {
-                    firstZeroInByteHit = true;
-                }
-            }
-        }
-
-        previousValue = value;
-    }
-
-    return true;
 }
 
 /**
@@ -1387,7 +1299,7 @@ inline void handleIPv4StaticPatch(
             bool errorInEntry = false;
             if (address)
             {
-                if (ipv4VerifyIpAndGetBitcount(*address))
+                if (ip_util::ipv4VerifyIpAndGetBitcount(*address))
                 {
                     addr = &(*address);
                 }
@@ -1411,7 +1323,8 @@ inline void handleIPv4StaticPatch(
 
             if (subnetMask)
             {
-                if (!ipv4VerifyIpAndGetBitcount(*subnetMask, &prefixLength))
+                if (!ip_util::ipv4VerifyIpAndGetBitcount(*subnetMask,
+                                                         &prefixLength))
                 {
                     messages::propertyValueFormatError(
                         asyncResp->res, *subnetMask,
@@ -1421,8 +1334,8 @@ inline void handleIPv4StaticPatch(
             }
             else if (nicIpEntry != ipv4Data.cend())
             {
-                if (!ipv4VerifyIpAndGetBitcount(nicIpEntry->netmask,
-                                                &prefixLength))
+                if (!ip_util::ipv4VerifyIpAndGetBitcount(nicIpEntry->netmask,
+                                                         &prefixLength))
                 {
                     messages::propertyValueFormatError(
                         asyncResp->res, nicIpEntry->netmask,
@@ -1439,7 +1352,7 @@ inline void handleIPv4StaticPatch(
 
             if (gateway)
             {
-                if (ipv4VerifyIpAndGetBitcount(*gateway))
+                if (ip_util::ipv4VerifyIpAndGetBitcount(*gateway))
                 {
                     gw = &(*gateway);
                 }
