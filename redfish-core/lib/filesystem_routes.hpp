da7f24e2131fbf1983c1495cc5d56b98a1bfeff6
Jason M. Bills <jason.m.bills@intel.com>
2022-08-18 15:49:32 -0700

    Add POST support for simple Actions
    This adds the ability to find Redfish Actions for a filesystem resource
    in an "Actions" folder. It enables POST to the Actions URI with no data
    to trigger simple actions.
    
    Tested:
    Confirmed that a POST to the Action target causes a write to the file in
    the "Actions" folder.
    
    Change-Id: I282ea7672ed80cc5036bb40534ceea5fcc48c47c
    Signed-off-by: Jason M. Bills <jason.m.bills@intel.com>

--- redfish-core/lib/filesystem_routes.hpp
+++ redfish-core/lib/filesystem_routes.hpp
@@ -44,6 +44,7 @@ inline void
 
     std::string dir("/var" + std::string(req.url));
 
+    // Find properties in the filesystem
     for (const std::filesystem::directory_entry& dirEntry :
          std::filesystem::directory_iterator(dir))
     {
@@ -62,6 +63,25 @@ inline void
             std::string value;
             std::getline(ifs, value);
             asyncResp->res.jsonValue[key] = value;
+        }
+    }
+
+    // Find Actions in the filesystem
+    if (std::filesystem::exists(dir + "/Actions") &&
+        std::filesystem::is_directory(dir + "/Actions") &&
+        !std::filesystem::is_symlink(dir + "/Actions"))
+    {
+        for (const std::filesystem::directory_entry& dirEntry :
+             std::filesystem::directory_iterator(dir + "/Actions"))
+        {
+            if (dirEntry.is_directory() || dirEntry.is_symlink())
+            {
+                continue;
+            }
+            std::string name = dirEntry.path().filename();
+            std::ifstream ifs(dirEntry.path(), std::ios::in);
+            asyncResp->res.jsonValue["Actions"]["#" + name]["target"] =
+                std::string(req.url) + "/Actions/" + name;
         }
     }
 }
@@ -98,6 +118,29 @@ inline void
     }
 }
 
+inline void
+    handleFileSystemActions(App& app, const crow::Request& req,
+                            const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                            const std::string& action)
+{
+    std::cerr << "handleFileSystemActions: " << req.url << ", " << action
+              << "\n";
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    std::filesystem::path path("/var" + std::string(req.url));
+    if (!std::filesystem::exists(path))
+    {
+        messages::actionNotSupported(asyncResp->res, action);
+        return;
+    }
+
+    std::ofstream ofs(path, std::ios::out);
+    ofs << std::string("0");
+}
+
 inline void requestRoutesFromFilesystem(App& app)
 {
     const std::filesystem::path redfishDir = "/var/redfish/v1";
@@ -121,14 +164,25 @@ inline void requestRoutesFromFilesystem(App& app)
             "/" + std::filesystem::relative(dirEntry.path(), "/var").string() +
             "/");
         std::cerr << url << '\n';
-        app.template route<0>(std::move(url))
+        std::string url1(url);
+        app.template route<0>(std::move(url1))
             .privileges(redfish::privileges::privilegeSetConfigureComponents)
             .methods(boost::beast::http::verb::get)(
                 std::bind_front(handleFileSystemGet, std::ref(app)));
-        app.template route<0>(std::move(url))
+        std::string url2(url);
+        app.template route<0>(std::move(url2))
             .privileges(redfish::privileges::privilegeSetConfigureComponents)
             .methods(boost::beast::http::verb::patch)(
                 std::bind_front(handleFileSystemPatch, std::ref(app)));
+        if (url.ends_with("Actions/"))
+        {
+            std::string url3(url + "<str>/");
+            app.template route<4>(std::move(url3))
+                .privileges(
+                    redfish::privileges::privilegeSetConfigureComponents)
+                .methods(boost::beast::http::verb::post)(
+                    std::bind_front(handleFileSystemActions, std::ref(app)));
+        }
     }
 }
 
