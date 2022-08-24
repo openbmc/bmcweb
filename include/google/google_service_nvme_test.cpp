b39939acb42d1fa66a855d000f550fd42cc972a6
Willy Tu <wltu@google.com>
2022-08-24 22:09:37 +0000

    Support Google NVMe to get Log and Identity
    Create handler for NVMe devices and subsystem controllers to request log
    package + identity binary dumps. The Action will request a file
    descriptor from nvmed and read it to return the binary dump as the
    response.
    
    Schema Example,
    ```
    wget -qO- http://localhost:80/google/v1/NVMe
    {
      "@odata.id": "/google/v1/NVMe",
      "@odata.type": "#GoogleNvmeCollection.GoogleNvmeCollection",
      "Members": [
        {
          "@odata.id": "/google/v1/NVMe/nvme_0"
        },
        {
          "@odata.id": "/google/v1/NVMe/nvme_1"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Google NVMe Collection"
    }
    
    wget -qO- http://localhost:80/google/v1/NVMe/nvme_0
    {
      "@odata.id": "/google/v1/NVMe/nvme_0",
      "Controllers": [
        {
          "@odata.id": "/google/v1/NVMe/nvme_0/controllers/controller_3"
        },
        {
          "@odata.id": "/google/v1/NVMe/nvme_0/controllers/controller_2"
        },
        {
          "@odata.id": "/google/v1/NVMe/nvme_0/controllers/controller_1"
        },
        {
          "@odata.id": "/google/v1/NVMe/nvme_0/controllers/controller_0"
        }
      ],
      "Controllers@odata.count": 4,
      "Name": "Google NVMe",
      "Storage": {
        "@odata.id": "/redfish/v1/Systems/system/Storage/test_storage_0"
      }
    }
    
    wget -qO- http://localhost:80/google/v1/NVMe/nvme_0/controllers/controller_0
    {
      "@odata.id": "/google/v1/NVMe/nvme_0/controllers/controller_0",
      "Actions": {
        "#Nvme.Identify": {
          "target": "/google/v1/NVMe/nvme_0/controllers/controller_0/Actions/NVMe.GetIdentify"
        },
        "#Nvme.Log": {
          "target": "/google/v1/NVMe/nvme_0/controllers/controller_0/Actions/NVMe.GetLogPage"
        }
      },
      "NVMe": {
        "@odata.id": "/google/v1/NVMe/nvme_0"
      },
      "Name": "Google NVMe Controller"
      "StorageController": {
        "@odata.id": "/redfish/v1/Systems/system/Storage/test_storage_0#/StorageControllers/0"
      }
    }
    ```
    
    Tested:
    ```
    $ curl  -u root:0penBmc -X POST http://localhost:18080/google/v1/NVMe/nvme_0/controllers/controller_0/Actions/NVMe.GetLogPage -d '{"LogSpecificId": "hello", "LogId": "world"}' -H "Content-Type: application/json"  > /tmp/test.log
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100 1024k  100 1024k  100    44  1116k     47 --:--:-- --:--:-- --:--:-- 1116k
    
    $ hexdump -C /tmp/test.log
    00000000  68 69 0a 00 00 00 00 04  04 04 04 04 04 04 04 04  |hi..............|
    00000010  04 04 04 04 04 04 04 04  04 04 04 04 04 04 04 04  |................|
    *
    00100000  04 04 04 04 04 04 04                              |.......|
    00100007
    
    $ curl  -u root:0penBmc -X POST http://localhost:18080/google/v1/NVMe/nvme_0/controllers/controller_0/Actions/NVMe.GetIdentify -d '{"ControllerNamespace": "hello", "ControllerId": "world"}' -H "Content-Type: application/json"  > /tmp/test.log
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100 2048k  100 2047k  100    57  2144k     59 --:--:-- --:--:-- --:--:-- 2144k
    $ hexdump -C /tmp/test.log
    00000000  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
    *
    000ffff0  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 06  |................|
    00100000  06 06 06 06 06 06 06 06  06 06 06 06 06 06 06 06  |................|
    *
    001ffff0  06 06 06 06 06 06 06 06  06 06 06 06 06 06 06     |...............|
    001fffff
    ```
    
    Change-Id: Ib122295688ff11b2ff71f9804568af48173eed64
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ include/google/google_service_nvme_test.cpp
@@ -0,0 +1,39 @@
+#include "async_resp.hpp"
+#include "google_service_nvme.hpp"
+#include "http_request.hpp"
+#include "nlohmann/json.hpp"
+
+#include <gtest/gtest.h>
+
+namespace crow::google_api
+{
+namespace
+{
+
+// void validateGoogleNVMeCollectionGet(crow::Response& res)
+// {
+//     nlohmann::json& json = res.jsonValue;
+//     EXPECT_EQ(json["@odata.id"], "/google/v1");
+//     EXPECT_EQ(json["@odata.type"],
+//               "#GoogleServiceRoot.v1_0_0.GoogleServiceRoot");
+//     EXPECT_EQ(json["@odata.id"], "/google/v1");
+//     EXPECT_EQ(json["Id"], "Google Rest RootService");
+//     EXPECT_EQ(json["Name"], "Google Service Root");
+//     EXPECT_EQ(json["Version"], "1.0.0");
+//     EXPECT_EQ(json["RootOfTrustCollection"]["@odata.id"],
+//               "/google/v1/RootOfTrustCollection");
+// }
+
+// TEST(HandleGoogleV1Get, OnSuccess)
+// {
+//     std::error_code ec;
+//     auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+
+//     asyncResp->res.setCompleteRequestHandler(validateServiceRootGet);
+
+//     crow::Request dummyRequest{{boost::beast::http::verb::get, "", 11}, ec};
+//     handleGoogleV1Get(dummyRequest, asyncResp);
+// }
+
+} // namespace
+} // namespace crow::google_api
