5742060e351618617ad867149a1bae6c865b079d
George Liu <liuxiwei@inspur.com>
2022-10-05 15:44:11 +0800

    Implements PowerSupplies schema
    This commit ipmlements the Redfish PowerSubsystem/PowerSupplies
    schema for collecting all powerSupplyNames.
    
    Tested: Validator passes
    1. curl -k  -H "X-Auth-Token: $token" -X GET
    https://${bmc}/redfish/v1/Chassis/chassis/PowerSubsystem/
    PowerSupplies
    {
    "@odata.id": "/redfish/v1/Chassis/chassis/PowerSubsystem/
                  PowerSupplies"
    "@odata.type": "#PowerSupplyCollection.PowerSupplyCollection",
    "Description": "The collection of PowerSupply resource instances
                    chassis",
    "Members": [
    {
    "@odata.id":
    "/redfish/v1/Chassis/chassis/PowerSubsystem/PowerSupplies#/
     powersupply0"
    },
    {
    "@odata.id":
    "/redfish/v1/Chassis/chassis/PowerSubsystem/PowerSupplies#/
     powersupply1"
    }
    ],
    "Members@odata.count": 2,
    "Name": "Power Supply Collection"
    }
    
    2. Bad chassisId
    curl -k  -H "X-Auth-Token: $token" -X GET
    https://${bmc}/redfish/v1/Chassis/chassisError/PowerSubsystem/
    PowerSupplies
    {
    "error": {
    "@Message.ExtendedInfo": [
    {
    "@odata.type": "#Message.v1_1_1.Message",
    "Message": "The requested resource of type Chassis named
                'chassisError' was not found.",
    "MessageArgs": [
    "Chassis",
    "chassisError"
    ],
    "MessageId": "Base.1.13.0.ResourceNotFound",
    "MessageSeverity": "Critical",
    "Resolution": "Provide a valid resource identifier and resubmit the
                   request."
    }
    ],
    "code": "Base.1.13.0.ResourceNotFound",
    "message": "The requested resource of type Chassis named
                'chassisError' was not found."
    }
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I48e087d6fb52013e3a96b44391cc4d86049ac176

---
+++ redfish-core/lib/power_supply.hpp
@@ -0,0 +1,155 @@
+#pragma once
+
+#include <app.hpp>
+#include <query.hpp>
+#include <registries/privilege_registry.hpp>
+#include <utils/chassis_utils.hpp>
+
+namespace redfish
+{
+using MapperGetAssociationResponse =
+    std::vector<std::tuple<std::string, std::string, std::string>>;
+
+template <typename Callback>
+inline void
+    getChassisAssoction(const std::string& service, const std::string& objPath,
+                        const std::string& chassisPath, Callback&& callback)
+{
+    const static std::string assIntf =
+        "xyz.openbmc_project.Association.Definitions";
+    sdbusplus::asio::getProperty<MapperGetAssociationResponse>(
+        *crow::connections::systemBus, service, objPath, assIntf,
+        "Associations",
+        [callback,
+         chassisPath](const boost::system::error_code ec,
+                      const MapperGetAssociationResponse& associations) {
+        if (ec)
+        {
+            return;
+        }
+
+        for (const auto& assoc : associations)
+        {
+            const auto& [rType, tType, endpoint] = assoc;
+            if (rType == "chassis" && endpoint == chassisPath)
+            {
+                callback();
+                return;
+            }
+        }
+        });
+}
+
+inline void updatePowerSupplyList(
+    const std::shared_ptr<bmcweb::AsyncResp>& /* asyncResp */,
+    const std::string& /* chassisId */, const std::string& powerSupplyPath)
+{
+    std::string powerSupplyName =
+        sdbusplus::message::object_path(powerSupplyPath).filename();
+    if (powerSupplyName.empty())
+    {
+        return;
+    }
+
+    // TODO In order for the validator to pass, the Members property will be
+    // implemented on the next commit
+}
+
+inline void powerSupplyCollection(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& chassisId, const std::string& validChassisPath,
+    const dbus::utility::MapperGetSubTreeResponse& subtree)
+{
+    asyncResp->res.jsonValue["@odata.type"] =
+        "#PowerSupplyCollection.PowerSupplyCollection";
+    asyncResp->res.jsonValue["Name"] = "Power Supply Collection";
+    asyncResp->res.jsonValue["@odata.id"] =
+        crow::utility::urlFromPieces("redfish", "v1", "Chassis", chassisId,
+                                     "PowerSubsystem", "PowerSupplies");
+    asyncResp->res.jsonValue["Description"] =
+        "The collection of PowerSupply resource instances.";
+    asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+    asyncResp->res.jsonValue["Members@odata.count"] = 0;
+
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/PowerSupplyCollection/PowerSupplyCollection.json>; rel=describedby");
+
+    for (const auto& [powerSupplyPath, serviceMap] : subtree)
+    {
+        for (const auto& [service, interfaces] : serviceMap)
+        {
+            auto respHandler =
+                [asyncResp, powerSupplyPath{powerSupplyPath}, chassisId]() {
+                updatePowerSupplyList(asyncResp, chassisId, powerSupplyPath);
+            };
+            getChassisAssoction(service, powerSupplyPath, validChassisPath,
+                                std::move(respHandler));
+        }
+    }
+}
+
+inline void
+    doPowerSupplyCollection(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                            const std::string& chassisId,
+                            const std::optional<std::string>& validChassisPath)
+{
+    if (!validChassisPath)
+    {
+        BMCWEB_LOG_ERROR << "Not a valid chassis ID" << chassisId;
+        messages::resourceNotFound(asyncResp->res, "Chassis", chassisId);
+        return;
+    }
+
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, chassisId, validChassisPath{*validChassisPath}](
+            const boost::system::error_code ec,
+            const dbus::utility::MapperGetSubTreeResponse& subtree) {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR << "D-Bus response error on GetSubTree " << ec;
+            if (ec.value() ==
+                boost::system::linux_error::bad_request_descriptor)
+            {
+                messages::resourceNotFound(asyncResp->res, "Chassis",
+                                           chassisId);
+                return;
+            }
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        powerSupplyCollection(asyncResp, chassisId, validChassisPath, subtree);
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTree",
+        "/xyz/openbmc_project", 0,
+        std::array<const char*, 1>{
+            "xyz.openbmc_project.Inventory.Item.PowerSupply"});
+}
+
+inline void handlePowerSupplyCollectionGet(
+    App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& chassisId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    redfish::chassis_utils::getValidChassisPath(
+        asyncResp, chassisId,
+        std::bind_front(doPowerSupplyCollection, asyncResp, chassisId));
+}
+
+inline void requestRoutesPowerSupplyCollection(App& app)
+{
+    BMCWEB_ROUTE(app, "/redfish/v1/Chassis/<str>/PowerSubsystem/PowerSupplies/")
+        .privileges(redfish::privileges::getPowerSupplyCollection)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handlePowerSupplyCollectionGet, std::ref(app)));
+}
+
+} // namespace redfish
