b3310470e2ee7fc6d33c3b64635f2a0f992b263e
George Liu <liuxiwei@inspur.com>
2022-10-05 17:00:00 +0800

    Add asset information for Fan
    This commit is to add asset information according to the Redfish
    PowerSupply schema.
    If the `xyz.openbmc_project.Inventory.Decorator.Asset` interface does
    not exist, the asset information property is not displayed.
    
    ref: http://redfish.dmtf.org/schemas/v1/PowerSupply.v1_5_0.json
    
    Tested: Validator passes
    curl -k -H "X-Auth-Token: $token" -X GET
    https://${bmc}/redfish/v1/Chassis/chassis/PowerSubsystem/
    PowerSupplies/powersupply0
    {
    "@odata.id": "/redfish/v1/Chassis/chassis/PowerSubsystem/
                  PowerSupplies/powersupply0",
    "@odata.type": "#PowerSupply.v1_5_0.PowerSupply",
    "Id": "powersupply0",
    "Manufacturer": "",
    "Model": "51E9",
    "Name": "powersupply0",
    "PartNumber": "03KP466",
    "SerialNumber": "YL10KY26E073",
    "SparePartNumber": "03FP378",
    "Status": {
    "Health": "OK"
    }
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I83f1a9375f83e3470089cb2b5207db9665cc69df

--- redfish-core/lib/power_supply.hpp
+++ redfish-core/lib/power_supply.hpp
@@ -195,6 +195,66 @@ inline void
 }
 
 inline void
+    getPowerSupplyAsset(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                        const std::string& service, const std::string& path,
+                        const std::string& intf)
+{
+
+    sdbusplus::asio::getAllProperties(
+        *crow::connections::systemBus, service, path, intf,
+        [asyncResp](const boost::system::error_code ec,
+                    const dbus::utility::DBusPropertiesMap& propertiesList) {
+        if (ec)
+        {
+            return;
+        }
+
+        const std::string* partNumber = nullptr;
+        const std::string* serialNumber = nullptr;
+        const std::string* manufacturer = nullptr;
+        const std::string* model = nullptr;
+        const std::string* sparePartNumber = nullptr;
+
+        const bool success = sdbusplus::unpackPropertiesNoThrow(
+            dbus_utils::UnpackErrorPrinter(), propertiesList, "PartNumber",
+            partNumber, "SerialNumber", serialNumber, "Manufacturer",
+            manufacturer, "Model", model, "SparePartNumber", sparePartNumber);
+
+        if (!success)
+        {
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        if (partNumber != nullptr)
+        {
+            asyncResp->res.jsonValue["PartNumber"] = *partNumber;
+        }
+
+        if (serialNumber != nullptr)
+        {
+            asyncResp->res.jsonValue["SerialNumber"] = *serialNumber;
+        }
+
+        if (manufacturer != nullptr)
+        {
+            asyncResp->res.jsonValue["Manufacturer"] = *manufacturer;
+        }
+
+        if (model != nullptr)
+        {
+            asyncResp->res.jsonValue["Model"] = *model;
+        }
+
+        // SparePartNumber is optional on D-Bus so skip if it is empty
+        if (sparePartNumber != nullptr && !sparePartNumber->empty())
+        {
+            asyncResp->res.jsonValue["SparePartNumber"] = *sparePartNumber;
+        }
+        });
+}
+
+inline void
     doPowerSupplyGet(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                      const std::string& chassisId,
                      const std::string& powerSupplyId,
@@ -228,6 +288,10 @@ inline void
             if (intf == "xyz.openbmc_project.State.Decorator.OperationalStatus")
             {
                 getPowerSupplyHealth(asyncResp, service, powerSupplyPath, intf);
+            }
+            if (intf == "xyz.openbmc_project.Inventory.Decorator.Asset")
+            {
+                getPowerSupplyAsset(asyncResp, service, powerSupplyPath, intf);
             }
         }
     };
