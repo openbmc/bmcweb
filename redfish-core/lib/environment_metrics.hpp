01ef6c16e1ac23aac404dbd551a127e402cba9bc
George Liu <liuxiwei@inspur.com>
2022-10-06 12:53:36 +0800

    Add FanSpeedsPercent for EnvironmentMetrics
    This commit is to add FanSpeedsPercent information according to the
    Redfish EnvironmentMetrics schema.
    
    ref:http://redfish.dmtf.org/schemas/v1/EnvironmentMetrics.v1_3_0.json
    
    Tested: Validator passes
    curl -k -H "X-Auth-Token: $token" -X GET
    https://${bmc}/redfish/v1/Chassis/chassis/EnvironmentMetrics
    {
    "@odata.id": "/redfish/v1/Chassis/chassis/EnvironmentMetrics",
    "@odata.type": "#EnvironmentMetrics.v1_3_0.EnvironmentMetrics",
    "FanSpeedsPercent": [
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan0_0",
    "DeviceName": "Chassis Fan #fan0_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan0_1",
    "DeviceName": "Chassis Fan #fan0_1",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan1_0",
    "DeviceName": "Chassis Fan #fan1_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan1_1",
    "DeviceName": "Chassis Fan #fan1_1",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan2_0",
    "DeviceName": "Chassis Fan #fan2_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan2_1",
    "DeviceName": "Chassis Fan #fan2_1",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan3_0",
    "DeviceName": "Chassis Fan #fan3_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan3_1",
    "DeviceName": "Chassis Fan #fan3_1",
    "SpeedRPM": -110.0
    }
    ],
    "FanSpeedsPercent@odata.count": 8,
    "Id": "EnvironmentMetrics",
    "Name": "Chassis Environment Metrics"
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I4cfc0aa28d68e7e0fa947251363deb6f06e36225

--- redfish-core/lib/environment_metrics.hpp
+++ redfish-core/lib/environment_metrics.hpp
@@ -1,14 +1,115 @@
 #pragma once
 
 #include "app.hpp"
+#include "dbus_utility.hpp"
+#include "error_messages.hpp"
 #include "utils/chassis_utils.hpp"
 
 #include <memory>
 #include <optional>
 #include <string>
+#include <vector>
 
 namespace redfish
 {
+inline void
+    updateFanSensorList(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                        const std::string& chassisId,
+                        const std::string& fanSensorPath, double value)
+{
+    std::string fanSensorName =
+        sdbusplus::message::object_path(fanSensorPath).filename();
+    if (fanSensorName.empty())
+    {
+        return;
+    }
+
+    nlohmann::json item;
+    item["DataSourceUri"] = crow::utility::urlFromPieces(
+        "redfish", "v1", "Chassis", chassisId, "Sensors", fanSensorName);
+    item["DeviceName"] = "Chassis Fan #" + fanSensorName;
+    item["SpeedRPM"] = value;
+
+    nlohmann::json& fanSensorList =
+        asyncResp->res.jsonValue["FanSpeedsPercent"];
+    fanSensorList.emplace_back(std::move(item));
+    asyncResp->res.jsonValue["FanSpeedsPercent@odata.count"] =
+        fanSensorList.size();
+}
+
+inline void getFanSensors(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                          const std::string& chassisId,
+                          const std::string& service, const std::string& path)
+{
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, service, path,
+        "xyz.openbmc_project.Sensor.Value", "Value",
+        [asyncResp, chassisId, path](const boost::system::error_code ec,
+                                     const double value) {
+        if (ec)
+        {
+            BMCWEB_LOG_DEBUG << "DBUS response error " << ec.message();
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        updateFanSensorList(asyncResp, chassisId, path, value);
+        });
+}
+
+inline void
+    getFanSensorPaths(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                      const std::string& fanSensorPath,
+                      const std::string& chassisId)
+{
+    auto respHandler =
+        [asyncResp, chassisId, fanSensorPath](const std::string& service) {
+        getFanSensors(asyncResp, chassisId, service, fanSensorPath);
+    };
+    std::vector<std::string> intefaces{"xyz.openbmc_project.Sensor.Value"};
+    dbus::utility::getDbusObject(asyncResp, fanSensorPath, intefaces,
+                                 std::move(respHandler));
+}
+
+inline void
+    getFanSpeedsPercent(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                        const std::string& chassisPath,
+                        const std::string& chassisId)
+{
+    std::string fanPath = chassisPath + "/cooled_by";
+    auto respInventoryHandler =
+        [asyncResp,
+         chassisId](const std::optional<dbus::utility::MapperEndPoints>&
+                        inventoryEndpointsPtr) {
+        if (!inventoryEndpointsPtr || inventoryEndpointsPtr->empty())
+        {
+            return;
+        }
+
+        for (const auto& ivPoint : *inventoryEndpointsPtr)
+        {
+            std::string sensorPath = ivPoint + "/sensors";
+            auto respSensorHandler =
+                [asyncResp,
+                 chassisId](const std::optional<dbus::utility::MapperEndPoints>&
+                                sensorEndpointsPtr) {
+                if (!sensorEndpointsPtr || sensorEndpointsPtr->empty())
+                {
+                    return;
+                }
+
+                for (const auto& sensorPint : *sensorEndpointsPtr)
+                {
+                    getFanSensorPaths(asyncResp, sensorPint, chassisId);
+                }
+            };
+            dbus::utility::getAssociationEndPoints(
+                asyncResp, sensorPath, std::move(respSensorHandler));
+        }
+    };
+    dbus::utility::getAssociationEndPoints(asyncResp, fanPath,
+                                           std::move(respInventoryHandler));
+}
 
 inline void handleEnvironmentMetricsHead(
     App& app, const crow::Request& req,
@@ -64,6 +165,8 @@ inline void handleEnvironmentMetricsGet(
         asyncResp->res.jsonValue["Id"] = "EnvironmentMetrics";
         asyncResp->res.jsonValue["@odata.id"] = crow::utility::urlFromPieces(
             "redfish", "v1", "Chassis", chassisId, "EnvironmentMetrics");
+
+        getFanSpeedsPercent(asyncResp, *validChassisPath, chassisId);
     };
 
     redfish::chassis_utils::getValidChassisPath(asyncResp, chassisId,
