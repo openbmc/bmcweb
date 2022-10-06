557dae75d2525b5e64e26b4089d3b03e530ce69a
George Liu <liuxiwei@inspur.com>
2022-10-06 12:53:36 +0800

    Add FanSpeedsPercent for EnvironmentMetrics
    This commit is to add FanSpeedsPercent information according to the
    Redfish EnvironmentMetrics schema.
    Relevant associated documents are here:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/58300
    
    ref:http://redfish.dmtf.org/schemas/v1/EnvironmentMetrics.v1_3_0.json
    
    Tested: Validator passes
    curl -k -H "X-Auth-Token: $token" -X GET
    https://${bmc}/redfish/v1/Chassis/chassis/EnvironmentMetrics
    {
    "@odata.id": "/redfish/v1/Chassis/chassis/EnvironmentMetrics",
    "@odata.type": "#EnvironmentMetrics.v1_3_0.EnvironmentMetrics",
    "FanSpeedsPercent": [
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan0_0",
    "DeviceName": "Chassis Fan #fan0_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan0_1",
    "DeviceName": "Chassis Fan #fan0_1",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan1_0",
    "DeviceName": "Chassis Fan #fan1_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan1_1",
    "DeviceName": "Chassis Fan #fan1_1",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan2_0",
    "DeviceName": "Chassis Fan #fan2_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan2_1",
    "DeviceName": "Chassis Fan #fan2_1",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan3_0",
    "DeviceName": "Chassis Fan #fan3_0",
    "SpeedRPM": -110.0
    },
    {
    "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/fan3_1",
    "DeviceName": "Chassis Fan #fan3_1",
    "SpeedRPM": -110.0
    }
    ],
    "FanSpeedsPercent@odata.count": 8,
    "Id": "EnvironmentMetrics",
    "Name": "Chassis Environment Metrics"
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I4cfc0aa28d68e7e0fa947251363deb6f06e36225

--- include/dbus_utility.hpp
+++ include/dbus_utility.hpp
@@ -221,5 +221,20 @@ inline void
         "GetManagedObjects");
 }
 
+inline void
+    getDbusObject(const std::string& path, std::span<const char*> interfaces,
+                  std::function<void(const boost::system::error_code&,
+                                     const MapperGetObject&)>&& callback)
+{
+    crow::connections::systemBus->async_method_call(
+        [callback{std::move(callback)}](const boost::system::error_code& ec,
+                                        const MapperGetObject& object) {
+        callback(ec, object);
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetObject", path, interfaces);
+}
+
 } // namespace utility
 } // namespace dbus
