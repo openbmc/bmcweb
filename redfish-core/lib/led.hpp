8c31b3ed5eb52de686244eb84b68818c7dad71e1
George Liu <liuxiwei@inspur.com>
2022-10-08 09:27:47 +0800

    Refactor Location Indicator Active property
    The Redfish Location Indicator Active property handling has been updated.
    Currently, the code has been hard-coded the LED group path and LED group
    service for System/Chassis. However, with the introduction of the
    "identifying" association[1], we planned to dynamically retrieve this
    information. This change breaks the systems that were previously working.
    
    To maintain compatibility and prevent disruption, keep the existing code
    with the LED group path and service hard-coded for System/Chassis. Modify
    the function names and use the generic names for Powersupplies, fans etc.
    
    Implement the dynamic retrieval of this information in the new commit[2].
    
    Reference:
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/58299
    [2] https://gerrit.openbmc.org/c/openbmc/bmcweb/+/42221
    
    Tested: built bmcweb successfully and Validator passes
    
    '''
    1. Get LocationIndicatorActive
    
    curl -k https://$bmc/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      "Actions": {
        "#ComputerSystem.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Systems/system/ResetActionInfo",
          "target": "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset"
        }
      },
    .....
      "Description": "Computer System",
      "FabricAdapters": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters"
      },
      "Id": "system",
      "IdlePowerSaver": {
        "Enabled": false,
        "EnterDwellTimeSeconds": 240,
        "EnterUtilizationPercent": 8,
        "ExitDwellTimeSeconds": 10,
        "ExitUtilizationPercent": 12
      },
      "IndicatorLED": "Off",
      "LastResetTime": "2023-10-26T07:09:52+00:00",
      "Links": {
        "Chassis": [
          {
            "@odata.id": "/redfish/v1/Chassis/chassis"
          }
        ],
        "ManagedBy": [
          {
            "@odata.id": "/redfish/v1/Managers/bmc"
          }
        ]
      },
      "LocationIndicatorActive": false,
    .....
    }
    
    5. Set LocationIndicatorActive
    
    curl -k -X PATCH https://${bmc}/redfish/v1/Systems/system -d '{"LocationIndicatorActive":true}'
    
    curl -k https://$bmc/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      "Actions": {
        "#ComputerSystem.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Systems/system/ResetActionInfo",
          "target": "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset"
        }
      },
    ....
        "ManagedBy": [
          {
            "@odata.id": "/redfish/v1/Managers/bmc"
          }
        ]
      },
      "LocationIndicatorActive": true,
    '''
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I0c5d538c1dd9f0d8dd1e6ae4d8ba4f606b804373
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>

--- redfish-core/lib/led.hpp
+++ redfish-core/lib/led.hpp
@@ -156,13 +156,13 @@ inline void
 }
 
 /**
- * @brief Retrieves identify led group properties over dbus
+ * @brief Retrieves identify system led group properties over dbus
  *
  * @param[in] asyncResp     Shared pointer for generating response message.
  *
  * @return None.
  */
-inline void getLocationIndicatorActive(
+inline void getSystemLocationIndicatorActive(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
 {
     BMCWEB_LOG_DEBUG("Get LocationIndicatorActive");
@@ -214,14 +214,14 @@ inline void getLocationIndicatorActive(
 }
 
 /**
- * @brief Sets identify led group properties
+ * @brief Sets identify system led group properties
  *
  * @param[in] asyncResp     Shared pointer for generating response message.
  * @param[in] ledState  LED state passed from request
  *
  * @return None.
  */
-inline void setLocationIndicatorActive(
+inline void setSystemLocationIndicatorActive(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp, const bool ledState)
 {
     BMCWEB_LOG_DEBUG("Set LocationIndicatorActive");
