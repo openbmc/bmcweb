de69d83afa052f3a08da45409a0b5d1440581b9a
George Liu <liuxiwei@inspur.com>
2022-10-08 09:27:47 +0800

    Update Location Indicator Active property
    When Redfish get/set Location Indicator Active property, it should
    first find the corresponding ledGroup through the identify_led_group
    association, and then get/set Asserted property, instead of
    hard-coding ledGroup.
    
    Tested: built bmcweb successfully and Validator passes
    1. get LocationIndicatorActive
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/
    Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      ...
      "LocationIndicatorActive": true,
      ...
    }
    
    2. set LocationIndicatorActive
    curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json"
    -X PATCH -d '{"LocationIndicatorActive": false}'
    https://${bmc}/redfish/v1/Systems/system
    
    get LocationIndicatorActive
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/
    Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      ...
      "LocationIndicatorActive": false,
      ...
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I0c5d538c1dd9f0d8dd1e6ae4d8ba4f606b804373

--- redfish-core/lib/chassis.hpp
+++ redfish-core/lib/chassis.hpp
@@ -333,7 +333,7 @@ inline void
                               interface) != interfaces2.end())
                 {
                     getIndicatorLedState(asyncResp);
-                    getLocationIndicatorActive(asyncResp);
+                    getLocationIndicatorActive(asyncResp, path);
                     break;
                 }
             }
@@ -549,7 +549,7 @@ inline void
             {
                 if (indicatorChassis)
                 {
-                    setLocationIndicatorActive(asyncResp,
+                    setLocationIndicatorActive(asyncResp, path,
                                                *locationIndicatorActive);
                 }
                 else
