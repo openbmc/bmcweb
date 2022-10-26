6926505c619961392f4941c2ab5cd032c00c5e2d
George Liu <liuxiwei@inspur.com>
2022-10-26 13:52:47 +0800

    Add Link header handling to PowerSubsystem
    This commit is to update PowerSubsystem to respond to HEAD requests
    in the way the redfish spec requires.
    
    Tested:
    HEAD /redfish/v1/Chassis/<str>/PowerSubsystem returns a correct
    looking Link header.
    Following that Link header resolves the schema file.
    
    Get /redfish/v1/Chassis/BadChassis/PowerSubsystem returns 404.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I2f2a0e80b74471ddc037e899cfaf3ba307741475

--- redfish-core/lib/power_subsystem.hpp
+++ redfish-core/lib/power_subsystem.hpp
@@ -12,30 +12,30 @@
 namespace redfish
 {
 
-inline void doPowerSubsystemCollection(
+inline void handlePowerSubsystemCollectionHead(
+    App& app, const crow::Request& req,
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-    const std::string& chassisId,
-    const std::optional<std::string>& validChassisPath)
+    const std::string& chassisId)
 {
-    if (!validChassisPath)
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
     {
-        BMCWEB_LOG_ERROR << "Not a valid chassis ID" << chassisId;
-        messages::resourceNotFound(asyncResp->res, "Chassis", chassisId);
         return;
     }
 
-    asyncResp->res.jsonValue["@odata.type"] =
-        "#PowerSubsystem.v1_1_0.PowerSubsystem";
-    asyncResp->res.jsonValue["Name"] = "Power Subsystem";
-    asyncResp->res.jsonValue["Id"] = "PowerSubsystem";
-    asyncResp->res.jsonValue["@odata.id"] = crow::utility::urlFromPieces(
-        "redfish", "v1", "Chassis", chassisId, "PowerSubsystem");
-    asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
-    asyncResp->res.jsonValue["Status"]["Health"] = "OK";
-
-    asyncResp->res.addHeader(
-        boost::beast::http::field::link,
-        "</redfish/v1/JsonSchemas/PowerSubsystem/PowerSubsystem.json>; rel=describedby");
+    auto respHandler = [asyncResp, chassisId](
+                           const std::optional<std::string>& validChassisPath) {
+        if (!validChassisPath)
+        {
+            BMCWEB_LOG_ERROR << "Not a valid chassis ID" << chassisId;
+            messages::resourceNotFound(asyncResp->res, "Chassis", chassisId);
+            return;
+        }
+        asyncResp->res.addHeader(
+            boost::beast::http::field::link,
+            "</redfish/v1/JsonSchemas/PowerSubsystem/PowerSubsystem.json>; rel=describedby");
+    };
+    redfish::chassis_utils::getValidChassisPath(asyncResp, chassisId,
+                                                std::move(respHandler));
 }
 
 inline void handlePowerSubsystemCollectionGet(
@@ -48,13 +48,39 @@ inline void handlePowerSubsystemCollectionGet(
         return;
     }
 
-    redfish::chassis_utils::getValidChassisPath(
-        asyncResp, chassisId,
-        std::bind_front(doPowerSubsystemCollection, asyncResp, chassisId));
+    auto respHandler = [asyncResp, chassisId](
+                           const std::optional<std::string>& validChassisPath) {
+        if (!validChassisPath)
+        {
+            BMCWEB_LOG_ERROR << "Not a valid chassis ID" << chassisId;
+            messages::resourceNotFound(asyncResp->res, "Chassis", chassisId);
+            return;
+        }
+
+        asyncResp->res.addHeader(
+            boost::beast::http::field::link,
+            "</redfish/v1/JsonSchemas/PowerSubsystem/PowerSubsystem.json>; rel=describedby");
+        asyncResp->res.jsonValue["@odata.type"] =
+            "#PowerSubsystem.v1_1_0.PowerSubsystem";
+        asyncResp->res.jsonValue["Name"] = "Power Subsystem";
+        asyncResp->res.jsonValue["Id"] = "PowerSubsystem";
+        asyncResp->res.jsonValue["@odata.id"] = crow::utility::urlFromPieces(
+            "redfish", "v1", "Chassis", chassisId, "PowerSubsystem");
+        asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
+        asyncResp->res.jsonValue["Status"]["Health"] = "OK";
+    };
+
+    redfish::chassis_utils::getValidChassisPath(asyncResp, chassisId,
+                                                std::move(respHandler));
 }
 
 inline void requestRoutesPowerSubsystem(App& app)
 {
+    BMCWEB_ROUTE(app, "/redfish/v1/Chassis/<str>/PowerSubsystem/")
+        .privileges(redfish::privileges::headPowerSubsystem)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handlePowerSubsystemCollectionHead, std::ref(app)));
+
     BMCWEB_ROUTE(app, "/redfish/v1/Chassis/<str>/PowerSubsystem/")
         .privileges(redfish::privileges::getPowerSubsystem)
         .methods(boost::beast::http::verb::get)(
