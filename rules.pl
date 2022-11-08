ea1283f3b77dccc08ef5229f4aa9ff0d41f4299a
Brad Bishop <bradleyb@fuzziesquirrel.com>
2022-11-08 13:47:44 -0500

    Revert "add test submit rules"
    This reverts commit 7f4124ed541b97d896bf44d35e228f9bdb08e623.
    
    Done testing.
    
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- rules.pl
+++
@@ -1,6 +0,0 @@
-submit_rule(S) :-
-  gerrit:default_submit(D),
-  D =.. [submit | Ds],
-  findall(U, gerrit:commit_label(label('Code-Review', 2), U), Approvers),
-  gerrit_owners:add_owner_approval(Approvers, Ds, A),
-  S =.. [submit | A].
