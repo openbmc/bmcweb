76d7d49aa666085e29c1c7cba23e8a6fbe9ba703
Nikhil Namjoshi <nikhilnamjoshi@google.com>
2022-11-10 20:53:04 +0000

    Collection: Add API to get sorted list of associated members
    As of today, we only have an API (getCollectionMembers) to get the
    sorted collection of inventory items which are related to
    an inventory item by path hierarchy
    
    But we also need an API, that gives a sorted collection of
    inventory items which are related to an inventory item by
    associations.
    
    Add a new API getAssociatedCollectionMembers() to get the
    sorted collection of inventory items using association
    endpoint property.
    
    Tested:
    This change has been tested with #38570 and #54900
    
    ```
    wget -O- http://localhost/redfish/v1/Systems/system/Processors/cpu0/SubProcessors
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors",
      "@odata.type": "#ProcessorCollection.ProcessorCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core2"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core3"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core4"
        }
    
       ],
      "Members@odata.count": 5,
      "Name": "SubProcessor Collection"
    }
    
    wget -O- http://localhost/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core0/SubProcessors
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core0/SubProcessors",
      "@odata.type": "#ProcessorCollection.ProcessorCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core0/SubProcessors/cpu0_core0_thread0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core0/SubProcessors/cpu0_core0_thread1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/cpu0_core0/SubProcessors/cpu0_core0_thread2"
        }
      ],
      "Members@odata.count": 3,
      "Name": "SubProcessor Collection"
    }
    
    ```
    
    Ref:
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/38570
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/54900
    
    Change-Id: I65e248746576978b76092c6ab444a876e6736e64
    Signed-off-by: Nikhil Namjoshi <nikhilnamjoshi@google.com>

--- redfish-core/include/utils/collection.hpp
+++ redfish-core/include/utils/collection.hpp
@@ -19,6 +19,50 @@ namespace redfish
 namespace collection_util
 {
 
+inline void handleCollectionMembers(
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreePathsResponse& objects)
+{
+    if (ec == boost::system::errc::io_error)
+    {
+        asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+        asyncResp->res.jsonValue["Members@odata.count"] = 0;
+        return;
+    }
+
+    if (ec)
+    {
+        BMCWEB_LOG_DEBUG << "DBUS response error " << ec.value();
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    std::vector<std::string> pathNames;
+    for (const auto& object : objects)
+    {
+        sdbusplus::message::object_path path(object);
+        std::string leaf = path.filename();
+        if (leaf.empty())
+        {
+            continue;
+        }
+        pathNames.push_back(leaf);
+    }
+    std::sort(pathNames.begin(), pathNames.end(), AlphanumLess<std::string>());
+
+    nlohmann::json& members = asyncResp->res.jsonValue["Members"];
+    members = nlohmann::json::array();
+    for (const std::string& leaf : pathNames)
+    {
+        boost::urls::url url = collectionPath;
+        crow::utility::appendUrlPieces(url, leaf);
+        nlohmann::json::object_t member;
+        member["@odata.id"] = std::move(url);
+        members.emplace_back(std::move(member));
+    }
+    asyncResp->res.jsonValue["Members@odata.count"] = members.size();
+}
+
 /**
  * @brief Populate the collection "Members" from a GetSubTreePaths search of
  *        inventory
@@ -44,45 +88,41 @@ inline void
         [collectionPath, asyncResp{std::move(asyncResp)}](
             const boost::system::error_code& ec,
             const dbus::utility::MapperGetSubTreePathsResponse& objects) {
-        if (ec == boost::system::errc::io_error)
-        {
-            asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
-            asyncResp->res.jsonValue["Members@odata.count"] = 0;
-            return;
-        }
+        handleCollectionMembers(ec, objects);
+        });
+}
 
-        if (ec)
-        {
-            BMCWEB_LOG_DEBUG << "DBUS response error " << ec.value();
-            messages::internalError(asyncResp->res);
-            return;
-        }
+/**
+ * @brief Populate the collection "Members" from the Associations endpoint
+ *        path
+ *
+ * @param[in, out] aResp  Async response object
+ * @param[in]      collectionPath  Redfish collection path which is used for the
+ *                 Members Redfish Path
+ * @param[in]      interfaces  List of interfaces to constrain the associated
+ *                 members
+ * @param[in]      associationEndPointPath  Path that points to the association
+ *                 end point
+ * @param[in]      path  Base path to search for the subtree
+ *
+ * @return void
+ */
+inline void getAssociatedCollectionMembers(
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+    const boost::urls::url& collectionPath,
+    std::vector<std::string_view>& interfaces,
+    const std::string& associationEndPointPath,
+    const sdbusplus::message::object_path& path)
+{
+    BMCWEB_LOG_DEBUG << "Get associated collection members for: "
+                     << collectionPath;
 
-        std::vector<std::string> pathNames;
-        for (const auto& object : objects)
-        {
-            sdbusplus::message::object_path path(object);
-            std::string leaf = path.filename();
-            if (leaf.empty())
-            {
-                continue;
-            }
-            pathNames.push_back(leaf);
-        }
-        std::sort(pathNames.begin(), pathNames.end(),
-                  AlphanumLess<std::string>());
-
-        nlohmann::json& members = asyncResp->res.jsonValue["Members"];
-        members = nlohmann::json::array();
-        for (const std::string& leaf : pathNames)
-        {
-            boost::urls::url url = collectionPath;
-            crow::utility::appendUrlPieces(url, leaf);
-            nlohmann::json::object_t member;
-            member["@odata.id"] = std::move(url);
-            members.emplace_back(std::move(member));
-        }
-        asyncResp->res.jsonValue["Members@odata.count"] = members.size();
+    dbus::utility::getAssociatedSubTreePaths(
+        associationEndPointPath, path, 0, interfaces,
+        [collectionPath, asyncResp{std::move(asyncResp)}](
+            const boost::system::error_code& ec,
+            const dbus::utility::MapperGetSubTreePathsResponse& objects) {
+        handleCollectionMembers(ec, objects);
         });
 }
 
