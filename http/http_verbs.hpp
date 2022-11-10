b2a7f1c3d63b96b55810860f0e7a03aebca159e6
Edward Lee <edwarddl@google.com>
2022-11-10 18:46:18 +0000

    Move code to prevent circular dependency
    While implementing https://gerrit.openbmc.org/c/openbmc/bmcweb/+/57932,
     there has been an issue where there is a circular dependency between
    routing.hpp and privileges.hpp. This code move predates this change
    to resolve it before implementing the heart of redfish authz.
    
    Tested: bitbake bmcweb
    Code compiles
    
    Signed-off-by: Edward Lee <edwarddl@google.com>
    Change-Id: I5e42f04af443dc03b0cdcde62ed9dc9b6d97db1f

---
+++ http/http_verbs.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <boost/beast/http/verb.hpp>
+
+namespace crow
+{
+
+// Note, this is an imperfect abstraction.  There are a lot of verbs that we
+// use memory for, but are basically unused by most implementations.
+// Ideally we would have a list of verbs that we do use, and only index in
+// to a smaller array of those, but that would require a translation from
+// boost::beast::http::verb, to the bmcweb index.
+inline constexpr size_t maxVerbIndex =
+    static_cast<size_t>(boost::beast::http::verb::patch);
+
+// MaxVerb + 1 is designated as the "not found" verb.  It is done this way
+// to keep the BaseRule as a single bitfield (thus keeping the struct small)
+// while still having a way to declare a route a "not found" route.
+inline constexpr size_t notFoundIndex = maxVerbIndex + 1;
+inline constexpr size_t methodNotAllowedIndex = notFoundIndex + 1;
+inline constexpr size_t maxNumMethods = methodNotAllowedIndex + 1;
+
+} // namespace http
