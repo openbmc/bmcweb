bf242dc9a56752a67bdfcf7225257bfdd15fd021
Carson Labrado <clabrado@google.com>
2022-11-23 20:55:18 +0000

    Aggregation: Improve prefix fixup matching
    Utilize the new array of top level collection URIs to determine if a
    given URI in the response needs to have the aggregation prefix added.
    This removes the need to check for specific collections like
    /redfish/v1/UpdateService/FirmwareInventory which do not fit the
    generic format of /redfish/v1/<collection>.
    
    Future patches will use this same approach to improve the logic for
    initially determining if and how a request should be aggregated.
    
    This patch also adds a series of unit tests for the function
    responsible for adding a prefix to a given URI.  Cases covered include
    valid URIs that involve a selection of aggregated resources, top level
    collection URIs, other invalid URIs, and URIs with a trailing "/".
    
    Tested:
    Unit tests pass.
    
    Signed-off-by: Carson Labrado <clabrado@google.com>
    Change-Id: I676983d3c77ae3126c04e9f57ad8698c51df2675

--- test/redfish-core/include/redfish_aggregator_test.cpp
+++ test/redfish-core/include/redfish_aggregator_test.cpp
@@ -38,4 +38,102 @@ TEST(IsPropertyUri, UnsupportedPropertyReturnsFalse)
     EXPECT_FALSE(isPropertyUri("Id"));
 }
 
+TEST(addPrefixToItem, ValidURIs)
+{
+    nlohmann::json jsonRequest;
+    constexpr std::array validRoots{"Cables",
+                                    "Chassis",
+                                    "Fabrics",
+                                    "PowerEquipment/FloorPDUs",
+                                    "Systems",
+                                    "TaskService/Tasks",
+                                    "TelemetryService/LogService/Entries",
+                                    "UpdateService/SoftwareInventory"};
+
+    // We're only testing prefix fixing so it's alright that some of the
+    // resulting URIs will not actually be possible as defined by the schema
+    constexpr std::array validIDs{"1",
+                                  "1/",
+                                  "Test",
+                                  "Test/",
+                                  "Extra_Test",
+                                  "Extra_Test/",
+                                  "Extra_Test/Sensors",
+                                  "Extra_Test/Sensors/",
+                                  "Extra_Test/Sensors/power_sensor",
+                                  "Extra_Test/Sensors/power_sensor/"};
+
+    // Construct URIs which should have prefix fixing applied
+    for (const auto& root : validRoots)
+    {
+        for (const auto& id : validIDs)
+        {
+            std::string initial("/redfish/v1/" + std::string(root) + "/");
+            std::string correct(initial + "asdfjkl_" + std::string(id));
+            initial += id;
+            jsonRequest["@odata.id"] = initial;
+            addPrefixToItem(jsonRequest["@odata.id"], "asdfjkl");
+            EXPECT_EQ(jsonRequest["@odata.id"], correct);
+        }
+    }
+}
+
+TEST(addPrefixToItem, UnsupportedURIs)
+{
+    nlohmann::json jsonRequest;
+    constexpr std::array invalidRoots{
+        "FakeCollection",           "JsonSchemas",
+        "PowerEquipment",           "TaskService",
+        "TelemetryService/Entries", "UpdateService"};
+
+    constexpr std::array validIDs{"1",
+                                  "1/",
+                                  "Test",
+                                  "Test/",
+                                  "Extra_Test",
+                                  "Extra_Test/",
+                                  "Extra_Test/Sensors",
+                                  "Extra_Test/Sensors/",
+                                  "Extra_Test/Sensors/power_sensor",
+                                  "Extra_Test/Sensors/power_sensor/"};
+
+    // Construct URIs which should NOT have prefix fixing applied
+    for (const auto& root : invalidRoots)
+    {
+        for (const auto& id : validIDs)
+        {
+            std::string initial("/redfish/v1/" + std::string(root) + "/");
+            std::string correct(initial + "asdfjkl_" + std::string(id));
+            initial += id;
+            jsonRequest["@odata.id"] = initial;
+            addPrefixToItem(jsonRequest["@odata.id"], "asdfjkl");
+            EXPECT_EQ(jsonRequest["@odata.id"], initial);
+        }
+    }
+}
+
+TEST(addPrefixToItem, TopLevelCollections)
+{
+    nlohmann::json jsonRequest;
+    constexpr std::array validRoots{"Cables",
+                                    "Chassis/",
+                                    "Fabrics",
+                                    "JsonSchemas",
+                                    "PowerEquipment/FloorPDUs",
+                                    "Systems",
+                                    "TaskService/Tasks",
+                                    "TelemetryService/LogService/Entries",
+                                    "TelemetryService/LogService/Entries/",
+                                    "UpdateService/SoftwareInventory/"};
+
+    // Construct URIs for top level collections.  Prefixes should NOT be
+    // applied to any of the URIs
+    for (const auto& root : validRoots)
+    {
+        std::string initial("/redfish/v1/" + std::string(root));
+        jsonRequest["@odata.id"] = initial;
+        addPrefixToItem(jsonRequest["@odata.id"], "perfix");
+        EXPECT_EQ(jsonRequest["@odata.id"], initial);
+    }
+}
 } // namespace redfish
