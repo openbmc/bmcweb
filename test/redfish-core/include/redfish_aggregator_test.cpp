bf336a76514550cb556a523281118f293bdef656
Carson Labrado <clabrado@google.com>
2022-11-23 23:58:01 +0000

    Aggregation: Detect and fix all URI properties
    There are a number of properties of Type "string (uri)" for which we
    do not currently support adding prefixes.  This patch adds support
    for all existing URI properties which are missed by the existing
    implementation.
    
    This change will be needed by future patches which will expand
    aggregation support to all top level collections defined by the
    schema.  Those collections that are not currently supported include
    properties whose URIs should be fixed, but would be missed by the
    existing implementation.
    
    Tested:
    URI properties are still handled correctly.
    
    ```shell
    curl localhost/redfish/v1/Chassis/5B247A_<chassisID>
    {
      "@odata.id": "/redfish/v1/Chassis/5B247A_<chassisID>",
      "@odata.type": "#Chassis.v1_16_0.Chassis",
      "Actions": {
        "#Chassis.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Chassis/5B247A_<chassisID>/ResetActionInfo",
          "target": "/redfish/v1/Chassis/5B247A_<chassisID>/Actions/Chassis.Reset"
        }
      },
      ...
    }
    ```
    
    Signed-off-by: Carson Labrado <clabrado@google.com>
    Change-Id: I3b3e06ee3191564d266598f7bc9f1641e6fcb333

---
+++ test/redfish-core/include/redfish_aggregator_test.cpp
@@ -0,0 +1,43 @@
+#include "redfish_aggregator.hpp"
+
+#include <string>
+
+#include <gest/gtest.h> // IWYU pragma: keep
+
+namespace redfish
+{
+
+TEST(IsPropertyUri, SupportedPropertyReturnsTrue)
+{
+    EXPECT_TRUE(isPropertyUri("@Redfish.ActionInfo"));
+    EXPECT_TRUE(isPropertyUri("@odata.id"));
+    EXPECT_TRUE(isPropertyUri("Image"));
+    EXPECT_TRUE(isPropertyUri("MetricProperty"));
+    EXPECT_TRUE(isPropertyUri("OriginOfCondition"));
+    EXPECT_TRUE(isPropertyUri("TaskMonitor"));
+    EXPECT_TRUE(isPropertyUri("target"));
+}
+
+TEST(IsPropertyUri, CaseInsensitiveURIReturnsTrue)
+{
+    EXPECT_TRUE(isPropertyUri("AdditionalDataURI"));
+    EXPECT_TRUE(isPropertyUri("DataSourceUri"));
+    EXPECT_TRUE(isPropertyUri("uri"));
+    EXPECT_TRUE(isPropertyUri("URI"));
+}
+
+TEST(IsPropertyUri, SpeificallyIgnoredPropertyReturnsFalse)
+{
+    EXPECT_FALSE(isPropertyUri("@odata.context"));
+    EXPECT_FALSE(isPropertyUri("Destination"));
+    EXPECT_FALSE(isPropertyUri("HostName"));
+}
+
+TEST(IsPropertyUri, UnsupportedPropertyReturnsFalse)
+{
+    EXPECT_FALSE(isPropertyUri("Name"));
+    EXPECT_FALSE(isPropertyUri("Health"));
+    EXPECT_FALSE(isPropertyUri("Id"));
+}
+
+} //namespace redfish
