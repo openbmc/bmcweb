3bfafdba6b7f9ca481774e1664df5d0392ef2477
Carson Labrado <clabrado@google.com>
2022-11-28 22:43:38 +0000

    Aggregation: Aggregate top level collections
    Adds aggregation support for all top level collections which do not
    follow the usual form of /redfish/v1/<collection>.
    
    As part of this change we no longer forward all requests that fit the
    above format such as /redfish/v1/UpdateService.  We now skip the
    forwarding rather than sending a request.  Previously we forwarded all
    potential collection requests and then relied on the lack of a
    "Members" array in the response to denote that the request was not
    actually for a valid collection.
    
    Tested:
    We aggregate or try to aggregate these URIs as collections:
    /redfish/v1/Chassis
    /redfish/v1/Chassis/
    /redfish/v1/Fabrics
    /redfish/v1/Fabrics/
    /redfish/v1/TelemetryService/MetricReports
    /redfish/v1/TelemetryService/MetricReports/
    
    We aggregate or try to aggregate these URIs as satellite resources
    /redfish/v1/Chassis/5B247A_<chassisID>
    /redfish/v1/TelemetryService/MetricReports/5B247A_reportID
    /redfish/v1/UpdateService/FirmwareInventory/5B247A_firmwareID
    
    We do not attempt to aggregate these URIs at all:
    /redfish/v1/Fake
    /redfish/v1/Fake/
    /redfish/v1/Fake/5B247A_fakeID
    /redfish/v1/TelemetryService
    /redfish/v1/TelemetryService/
    /redfish/v1/TelemetryService/5B247A_fakeID
    
    Signed-off-by: Carson Labrado <clabrado@google.com>
    Change-Id: I133503ab3e4df7777df094768786e511880ca70f

--- test/redfish-core/include/redfish_aggregator_test.cpp
+++ test/redfish-core/include/redfish_aggregator_test.cpp
@@ -81,8 +81,9 @@ TEST(addPrefixToItem, ValidURIs)
 TEST(addPrefixToItem, UnsupportedURIs)
 {
     nlohmann::json jsonRequest;
-    constexpr std::array invalidRoots{"FakeCollection", "PowerEquipment",
-                                      "TaskService", "TelemetryService/Entries",
+    constexpr std::array invalidRoots{"FakeCollection", "JsonSchemas",
+                                      "PowerEquipment", "TaskService",
+                                      "TelemetryService/Entries",
                                       "UpdateService"};
 
     constexpr std::array validIDs{"1",
@@ -117,6 +118,7 @@ TEST(addPrefixToItem, TopLevelCollections)
     constexpr std::array validRoots{"Cables",
                                     "Chassis/",
                                     "Fabrics",
+                                    "JsonSchemas",
                                     "PowerEquipment/FloorPDUs",
                                     "Systems",
                                     "TaskService/Tasks",
