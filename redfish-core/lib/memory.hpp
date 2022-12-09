4b1792ca345e3f6c15d30dc7d52ac0c60c9c90ea
George Liu <liuxiwei@inspur.com>
2022-12-09 11:06:16 +0800

    Refactor GetSubTree method
    Since the GetSubTree method has been implemented in dbus_utility and
    this commit is to integrate all the places where the GetSubTree
    method is called, and use the method in dbus_utility uniformly.
    
    Tested: Redfish Validator Passed
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: If3852b487d74e7cd8f123e0efffbd4affe92743c

--- redfish-core/lib/memory.hpp
+++ redfish-core/lib/memory.hpp
@@ -15,10 +15,10 @@
 */
 #pragma once
 
+#include "dbus_utility.hpp"
 #include "health.hpp"
 
 #include <app.hpp>
-#include <dbus_utility.hpp>
 #include <nlohmann/json.hpp>
 #include <query.hpp>
 #include <registries/privilege_registry.hpp>
@@ -705,9 +705,13 @@ inline void getDimmData(std::shared_ptr<bmcweb::AsyncResp> aResp,
                         const std::string& dimmId)
 {
     BMCWEB_LOG_DEBUG << "Get available system dimm resources.";
-    crow::connections::systemBus->async_method_call(
+    constexpr std::array<std::string_view, 2> dimmInterfaces = {
+        "xyz.openbmc_project.Inventory.Item.Dimm",
+        "xyz.openbmc_project.Inventory.Item.PersistentMemory.Partition"};
+    dbus::utility::getSubTree(
+        "/xyz/openbmc_project/inventory", 0, dimmInterfaces,
         [dimmId, aResp{std::move(aResp)}](
-            const boost::system::error_code ec,
+            const boost::system::error_code& ec,
             const dbus::utility::MapperGetSubTreeResponse& subtree) {
         if (ec)
         {
@@ -757,14 +761,7 @@ inline void getDimmData(std::shared_ptr<bmcweb::AsyncResp> aResp,
         aResp->res.jsonValue["@odata.id"] =
             "/redfish/v1/Systems/system/Memory/" + dimmId;
         return;
-        },
-        "xyz.openbmc_project.ObjectMapper",
-        "/xyz/openbmc_project/object_mapper",
-        "xyz.openbmc_project.ObjectMapper", "GetSubTree",
-        "/xyz/openbmc_project/inventory", 0,
-        std::array<const char*, 2>{
-            "xyz.openbmc_project.Inventory.Item.Dimm",
-            "xyz.openbmc_project.Inventory.Item.PersistentMemory.Partition"});
+        });
 }
 
 inline void requestRoutesMemoryCollection(App& app)
