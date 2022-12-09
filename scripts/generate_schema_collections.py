f2260b96db9aef6b488e1085616d5aa73b2506bf
Carson Labrado <clabrado@google.com>
2022-12-09 22:14:46 +0000

    Scripts: Parse entire tree via schema files
    Expands the generate schema collections script so that it now parses
    the entire tree rather than stopping when it hits a top level
    collection.  In the future we'll need to parse the entire tree.
    
    Tested:
    TODO
    
    Signed-off-by: Carson Labrado <clabrado@google.com>
    Change-Id: Id6d6fc5a08fd0fad13b37b815a7a0195328cb509

--- scripts/generate_schema_collections.py
+++ scripts/generate_schema_collections.py
@@ -36,8 +36,14 @@ EDMX = "{http://docs.oasis-open.org/odata/ns/edmx}"
 EDM = "{http://docs.oasis-open.org/odata/ns/edm}"
 
 
-def parse_node(path, top_collections, xml_file):
+def parse_node(path, top_collections, found_top, xml_file, depth):
+    print("\nIn xml file: ", xml_file)
     filepath = os.path.join(REDFISH_SCHEMA_DIR, xml_file)
+
+    # Bail if we are attempting to open a schema we haven't pulled in
+    if (not os.path.exists(filepath)):
+        return
+
     tree = ET.parse(filepath)
     root = tree.getroot()
 
@@ -59,43 +65,102 @@ def parse_node(path, top_collections, xml_file):
         for entity_type in schema.findall(EDM + "EntityType"):
             for nav_prop in entity_type.findall(EDM + "NavigationProperty"):
                 parse_navigation_property(nav_prop, path, top_collections,
-                                          xml_map)
-        for complex_type in schema.findall(EDM + "ComplexType"):
-            for nav_prop in complex_type.findall(EDM + "NavigationProperty"):
-                parse_navigation_property(nav_prop, path, top_collections,
-                                          xml_map)
+                                          found_top, xml_map, depth)
+#        for complex_type in schema.findall(EDM + "ComplexType"):
+#            for nav_prop in complex_type.findall(EDM + "NavigationProperty"):
+#                parse_navigation_property(nav_prop, path, top_collections,
+#                                          found_top, xml_map, depth)
 
 
 # Helper function which expects a NavigationProperty to be passed in.  We need
 # this because NavigationProperty appears under both EntityType and ComplexType
-def parse_navigation_property(element, path, top_collections, xml_map):
+def parse_navigation_property(element, path, top_collections, found_top,
+                              xml_map, depth):
     if (element.tag != (EDM + "NavigationProperty")):
         return
 
     # We don't want to aggregate JsonSchemas as well as anything under
     # AccountService or SessionService
     nav_name = element.get("Name")
+    print("Found ", nav_name, " at depth: ", depth)
+    print("Path is: ", path)
     if ((nav_name == "JsonSchemas") or
             (nav_name == "AccountService") or
             (nav_name == "SessionService")):
-        return
+        #return
+        found_top = True
 
     # ServiceRoot links to Sessions which we aren't going to aggregate
     if (nav_name == "Sessions"):
-        return
+        #return
+        found_top = True
 
     nav_type_split = element.get("Type").split(".")
+    print("nav_type_split is: ", nav_type_split)
     if (nav_type_split[0].endswith("Collection") or
             nav_type_split[0].startswith("Collection")):
-        new_path = path + "/" + nav_name
-        top_collections.add(new_path)
-        print("Added top level collection: " + new_path)
+#        new_path = path + "/" + nav_name
+
+        print("Found a collection")
+
+        # We contain a collection array instead of link to a collection
+        if (nav_type_split[0].startswith("Collection")):
+
+            # nav_type should be of the form "Collection(<resource>.<resource>)"
+            file_key = nav_type_split[0].split("(")[1]
+            if (file_key != nav_type_split[1].split(")")[0]):
+                print("Don't match!!!!!")
+                return
+
+            prev_count = path.count(file_key + "Id")
+            if (prev_count):
+                new_path = (path + "/{" + file_key + "Id" + str(prev_count+1)
+                            + "}")
+            else:
+                new_path = path + "/{" + file_key + "Id}"
+        else:
+            # Escape if we've found a circular dependency like SubProcessors
+            if (path.count(nav_name) >= 2):
+                print("Stopping here!!!!!!")
+                return
+
+            file_key = nav_type_split[0]
+            new_path = path + "/" + nav_name
+
+        # Did we find the top level collection in the current path or did we
+        # previously find it?
+        if (not found_top):
+            top_collections.add(new_path)
+            found_top = True
+            print("Added top level collection: " + new_path)
     else:
         # Traverse the children until we hit top level collection or
         # run out of nodes in the path
+
+        # Bail if we've found a circular dependency like MetricReport
+        if (path.count(nav_type_split[0])):
+            print("We're out of here, resource is: ", nav_type_split[0])
+            return
+
         new_path = path + "/" + nav_type_split[0]
-        parse_node(new_path, top_collections,
-                   xml_map[nav_type_split[0]])
+        file_key = nav_type_split[0]
+
+    # Stop walking the tree if we've hit a circular dependency
+    # if ((file_key == "Resource") or new_path.endswith("Resource")):
+    if (file_key == "Resource"):
+        print("Don't visit the Resource!!!!!!")
+        return
+    if ((file_key == "Resource") and path.endswith("Resource")):
+        print("Don't go in circles!!!!!!!!")
+        return
+
+#    if (
+
+    # Keep walking the Redfish tree
+    print("new_path is: ", new_path)
+    print("file_key is: ", file_key)
+    parse_node(new_path, top_collections, found_top, xml_map[file_key],
+               depth+1)
 
 
 def main():
@@ -118,7 +183,7 @@ def main():
 
     # Begin parsing from the Service Root
     curr_path = "/redfish/v1"
-    parse_node(curr_path, top_collections, "ServiceRoot_v1.xml")
+    parse_node(curr_path, top_collections, False, "ServiceRoot_v1.xml", 2)
 
     print("Finished traversal!")
 
