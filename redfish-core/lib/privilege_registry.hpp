e7910d575e34851df847a12843563a2a4d61656e
Edward Lee <edwarddl@google.com>
2022-12-20 21:14:17 +0000

    Implement GET on PrivilegeRegistry
    This commit is part of Dynamic Redfish Authorization
    
    This commit does the following:
    1. Makes privileges dynamic and adds some helpers to get OEM privileges
    2. Adds a GET route to PrivilegeRegistry resource
    3. Adds a reference to PrivilegeMap in AccountService
    
    Testing:
    No new Redfish Validator Errors
    Startup time minimal (3 trials saw ~1 ms change)
    Binary Size increased by ~63KB
    
    Signed-off-by: Edward Lee <edwarddl@google.com>
    Change-Id: Ida622ecd4de41c1ce0fff0280894c5d47700604e

---
+++ redfish-core/lib/privilege_registry.hpp
@@ -0,0 +1,83 @@
+#include "privileges.hpp"
+
+#include <app.hpp>
+#include <registries/privilege_registry.hpp>
+#include <verb.hpp>
+
+#include <array>
+#include <string>
+#include <vector>
+
+namespace redfish
+{
+
+using EntityTag = redfish::privileges::EntityTag;
+
+constexpr std::array<HttpVerb, 6> registryMethods = {
+    HttpVerb::Get, HttpVerb::Head,   HttpVerb::Patch,
+    HttpVerb::Put, HttpVerb::Delete, HttpVerb::Post,
+};
+
+inline void handlePrivilegeRegistryGet(
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    asyncResp->res.jsonValue["@odata.id"] =
+        "/redfish/v1/AccountService/PrivilegeMap";
+    asyncResp->res.jsonValue["@odata.type"] =
+        "#PrivilegeRegistry.v1_1_4.PrivilegeRegistry";
+    asyncResp->res.jsonValue["Id"] = "Redfish_1.3.0_PrivilegeRegistry";
+    asyncResp->res.jsonValue["Name"] = "Privilege Mapping array collection";
+
+    // Get all base privileges
+    asyncResp->res.jsonValue["PrivilegesUsed"] = basePrivileges;
+
+    asyncResp->res.jsonValue["OEMPrivilegesUsed"] = getAllOemPrivileges();
+
+    asyncResp->res.jsonValue["Mappings"] = nlohmann::json::array();
+
+    for (uint entityTag = 0; entityTag < redfish::privileges::entities.size();
+         ++entityTag)
+    {
+        nlohmann::json mapping;
+        mapping["Entity"] = redfish::privileges::entities[entityTag];
+
+        nlohmann::json operationMap;
+        for (HttpVerb verb : registryMethods)
+        {
+            EntityTag curTag = static_cast<EntityTag>(entityTag);
+
+            nlohmann::json privilegeSetJson = nlohmann::json::array();
+            const std::span<const Privileges> privilegeSet =
+                redfish::privileges::getPrivilegeFromEntityAndMethod(curTag,
+                                                                     verb);
+
+            for (Privileges privilege : privilegeSet)
+            {
+                nlohmann::json privilegeJson;
+                privilegeJson["Privilege"] =
+                    privilege.getAllActivePrivilegeNames();
+                privilegeSetJson.push_back(privilegeJson);
+            }
+
+            operationMap[httpVerbToString(verb)] = privilegeSetJson;
+        }
+        mapping["OperationMap"] = operationMap;
+        asyncResp->res.jsonValue["Mappings"].push_back(mapping);
+    }
+}
+
+inline void requestPrivilegeRegistryRoutes(App& app)
+{
+    BMCWEB_ROUTE(app, "/redfish/v1/AccountService/PrivilegeMap")
+        .privileges(redfish::privileges::getAccountService)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handlePrivilegeRegistryGet, std::ref(app)));
+}
+
+} // namespace redfish
