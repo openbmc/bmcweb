262f115299479c8f1142954622a358d53318e9fd
Gunnar Mills <gmills@us.ibm.com>
2022-12-20 15:18:47 -0600

    Return bad request if can't construct
    If given a bad URI, e.g. "https://$bmc/?a=id;" return http bad request
    (400) like we do other places, e.g. a few lines below.
    
    Certain scanners expect to see bad request when crawling and probing for
    vulnerabilities and using not valid URIs. Just dropping the connection
    causes errors with these scanners. They think connection problem or
    worse the server was taken down.
    
    Tested: Tested downstream https://$bmc/?a=id; returns bad request.
    
    Change-Id: I99a52d4c5e7f366046c5de1cf22c4db95ab39e13
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- http/http_connection.hpp
+++ http/http_connection.hpp
@@ -323,6 +323,8 @@ class Connection :
         if (reqEc)
         {
             BMCWEB_LOG_DEBUG << "Request failed to construct" << reqEc;
+            res.result(boost::beast::http::status::bad_request);
+            completeRequest(res);
             return;
         }
         thisReq.session = userSession;
