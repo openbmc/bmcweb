2187cfa9c4a996f49538ca229540d8730be138d9
Edward Lee <edwarddl@google.com>
2022-12-20 21:14:17 +0000

    Implement GET on PrivilegeRegistry
    This commit is part of Dynamic Redfish Authorization
    
    This commit does the following:
    1. Makes privileges dynamic and adds some helpers to get OEM privileges
    2. Adds a GET route to PrivilegeRegistry resource
    3. Adds a reference to PrivilegeMap in AccountService
    4. Adds a test against BasePrivilegeRegistry.
    
    PrivilegeRegistry is a NavigationProperty under AccountService.
    This is shown in [1]. Since it does not have its own type in
    the privilege registry, it inherits privileges from AccountService.
    
    Testing:
    Unit Test passes
    No new Redfish Validator Errors
    Startup time minimal (3 trials saw ~1 ms change)
    Binary Size increased by ~65KB (This commit is around 65KB).
    
    `wget -qO- http://localhost:80/redfish/v1/AccountService/PrivilegeMap`
    
    [1] https://github.com/openbmc/bmcweb/blob/master/static/redfish/v1/schema/AccountService_v1.xml#L230
    
    Signed-off-by: Edward Lee <edwarddl@google.com>
    Change-Id: Ida622ecd4de41c1ce0fff0280894c5d47700604e

---
+++ test/redfish-core/lib/privilege_registry_test.cpp
@@ -0,0 +1,80 @@
+#include "async_resp.hpp"
+#include "privilege_registry.hpp"
+
+#include <nlohmann/json.hpp>
+#include <registries/privilege_registry.hpp>
+
+#include <array>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <span>
+#include <string>
+
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+constexpr const char* baseRegistryRelativePath =
+    "../static/redfish/v1/registries/Redfish_1.3.0_PrivilegeRegistry.json";
+
+const static std::array<std::string, 3> overrideStrings{
+    {"PropertyOverrides", "ResourceURIOverrides", "SubordinateOverrides"}};
+
+nlohmann::json getBasePrivilegeRegistry()
+{
+    auto baseRegistryPath = std::filesystem::canonical(
+        std::filesystem::path(baseRegistryRelativePath));
+    std::ifstream ifs(baseRegistryPath);
+    return nlohmann::json::parse(ifs);
+}
+
+bool containsOverrides(nlohmann::json mapping)
+{
+    for (const auto& override : overrideStrings)
+    {
+        if (mapping.contains(override))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void assertPrivilegeRegistryGet(crow::Response& res)
+{
+    nlohmann::json::object_t baseRegistryJson = getBasePrivilegeRegistry();
+    nlohmann::json& respJson = res.jsonValue;
+    EXPECT_EQ(baseRegistryJson["Name"], respJson["Name"]);
+    EXPECT_EQ(baseRegistryJson["PrivilegesUsed"], respJson["PrivilegesUsed"]);
+    EXPECT_EQ(baseRegistryJson["OEMPrivilegesUsed"],
+              respJson["OEMPrivilegesUsed"]);
+
+    for (size_t i = 0; i < baseRegistryJson["Mappings"].size(); ++i)
+    {
+        EXPECT_EQ(baseRegistryJson["Mappings"][i]["Entity"],
+                  respJson["Mappings"][i]["Entity"]);
+        EXPECT_EQ(baseRegistryJson["Mappings"][i]["OperationMap"],
+                  respJson["Mappings"][i]["OperationMap"]);
+
+        if (containsOverrides(baseRegistryJson["Mappings"][i]))
+        {
+            std::cout << baseRegistryJson["Mappings"][i]["Entity"]
+                      << " contains override" << std::endl;
+        }
+    }
+}
+
+TEST(PrivilegeRegistryTest, PrivilegeRegistryIsSameAsBaseWithoutPatches)
+{
+    auto shareAsyncResp = std::make_shared<bmcweb::AsyncResp>();
+    shareAsyncResp->res.setCompleteRequestHandler(assertPrivilegeRegistryGet);
+    fillInPrivilegeRegistry(shareAsyncResp);
+}
+
+} // namespace
+} // namespace redfish
