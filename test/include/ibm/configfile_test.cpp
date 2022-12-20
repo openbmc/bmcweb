012449c21e0e82d0c3e232181b9492e25ff36f93
Isha Sahni <isha.sahni1@ibm.com>
2022-12-20 01:55:04 -0600

    added testcases for handleFileUpload
    Signed-off-by: Isha Sahni <isha.sahni1@ibm.com>
    Change-Id: Id19ce54ddc7e984ab48b6435356839e85058f512

--- test/include/ibm/configfile_test.cpp
+++ test/include/ibm/configfile_test.cpp
@@ -1,61 +1,173 @@
+#include "include/ibm/management_console_rest.hpp"
+#include "nlohmann/json.hpp"
+#include "http_request.hpp"
 #include "http_response.hpp"
-#include "ibm/management_console_rest.hpp"
-
+#include <boost/beast/http/fields.hpp>
+#include <boost/beast/http/message.hpp>
+#include <boost/beast/http/string_body.hpp>
 #include <string>
-
-#include <gtest/gtest.h> // IWYU pragma: keep
-
-// IWYU pragma: no_include <gtest/gtest-message.h>
-// IWYU pragma: no_include <gtest/gtest-test-part.h>
-// IWYU pragma: no_include "gtest/gtest_pred_impl.h"
+#include <gtest/gtest.h>
 
 namespace crow
 {
 namespace ibm_mc
 {
-
 TEST(IsValidConfigFileName, FileNameValidCharReturnsTrue)
 {
-    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
-
+    nlohmann::json jsonValue ;
     EXPECT_TRUE(
-        isValidConfigFileName("GoodConfigFile", asyncResp->res.jsonValue));
+        isValidConfigFileName("GoodConfigFile",jsonValue));
 }
 TEST(IsValidConfigFileName, FileNameInvalidCharReturnsFalse)
 {
-    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
-
-    EXPECT_FALSE(isValidConfigFileName("Bad@file", asyncResp->res.jsonValue));
+    nlohmann::json jsonValue ;
+    EXPECT_FALSE(isValidConfigFileName("Bad@file", jsonValue));
 }
 TEST(IsValidConfigFileName, FileNameInvalidPathReturnsFalse)
 {
-    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+    nlohmann::json jsonValue ;
+    EXPECT_FALSE(isValidConfigFileName("/../../../../../etc/badpath",jsonValue));
+    EXPECT_FALSE(
+        isValidConfigFileName("/../../etc/badpath", jsonValue));
 
-    EXPECT_FALSE(isValidConfigFileName("/../../../../../etc/badpath",
-                                       asyncResp->res.jsonValue));
     EXPECT_FALSE(
-        isValidConfigFileName("/../../etc/badpath", asyncResp->res.jsonValue));
-    EXPECT_FALSE(
-        isValidConfigFileName("/mydir/configFile", asyncResp->res.jsonValue));
+        isValidConfigFileName("/mydir/configFile", jsonValue));
 }
-
 TEST(IsValidConfigFileName, EmptyFileNameReturnsFalse)
 {
-    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
-    EXPECT_FALSE(isValidConfigFileName("", asyncResp->res.jsonValue));
+    nlohmann::json jsonValue ;
+    EXPECT_FALSE(isValidConfigFileName("", jsonValue));
 }
-
 TEST(IsValidConfigFileName, SlashFileNameReturnsFalse)
 {
-    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
-    EXPECT_FALSE(isValidConfigFileName("/", asyncResp->res.jsonValue));
+    nlohmann::json jsonValue ;
+    EXPECT_FALSE(isValidConfigFileName("/",jsonValue));
 }
 TEST(IsValidConfigFileName, FileNameMoreThan20CharReturnsFalse)
 {
-    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
-    EXPECT_FALSE(isValidConfigFileName("BadfileBadfileBadfile",
-                                       asyncResp->res.jsonValue));
+    nlohmann::json jsonValue ;
+    EXPECT_FALSE(isValidConfigFileName("BadfileBadfileBadfile",jsonValue));
 }
+ 
+TEST(handleFileUpload,  contentNotAcceptable1)
+{
+    std::error_code ec;
+    crow::Response res;
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+    boost::beast::http::request<boost::beast::http::string_body> req{};
+    req.set("Content-Type",
+            "multipart/form-data; "
+            "boundary=---------------------------d74496d66958873e");
+
+    req.body() = "-----------------------------d74495d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test1\"\r\n\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test2\"\r\n\r\n"
+            "{\r\n-----------------------------d74496d66958873e123456\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+             "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test3\"\r\n\r\n"
+            "{\r\n--------d74496d6695887}\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+             "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+             "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+             "1111111111111111111111111122222222222222222222222222222223333333333333333333333333333333333444444444444444444444444444\r\n"
+            "-----------------------------d74496d66958873e--\r\n";
+    req.method(boost::beast::http::verb::put) ;
+    crow::Request reqIn(req, ec);
+    const std::string& fileID = "Test1" ;
+    handleFileUpload(reqIn, asyncResp ,fileID) ;
+    EXPECT_EQ(asyncResp->res.jsonValue["Description"],std::string(contentNotAcceptableMsg));
+}
+
+TEST(handleFileUpload,  contentNotAcceptable2){
+    std::error_code ec;
+    crow::Response res;
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+    boost::beast::http::request<boost::beast::http::string_body> req{};
+    req.set("Content-Type",
+            "application/octet-stream; "
+            "boundary=---------------------------d74496d66958873e");
+
+    req.body() = "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test1\"\r\n\r\n"
+            "111111111111111111111111112222222222222222222222222222222\r\n"
+            "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test2\"\r\n\r\n"
+            "{\r\n-----------------------------d74496d66958873e123456\r\n"
+            "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test3\"\r\n\r\n"
+            "{\r\n--------d74496d6695887}\r\n"
+            "-----------------------------d74496d66958873e--\r\n";
+    req.method(boost::beast::http::verb::post) ;
+    crow::Request reqIn(req, ec);
+    const std::string& fileID = "Test1" ;
+
+    handleFileUpload(reqIn, asyncResp ,fileID) ;
+    EXPECT_EQ(asyncResp->res.jsonValue["Description"],std::string(contentNotAcceptableMsg));
+} 
+TEST(handleFileUpload,  contentAcceptable1){
+    std::error_code ec;
+    boost::beast::http::request<boost::beast::http::string_body> req{};
+    crow::Response res;
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+    req.set("Content-Type",
+            "multipart/form-data; "
+            "boundary=---------------------------d74496d66958873e");
+
+    req.body() = "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test1\"\r\n\r\n"
+            "111111111111111111111111112222222222222222222222222222222\r\n"
+            "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test2\"\r\n\r\n"
+            "{\r\n-----------------------------d74496d66958873e123456\r\n"
+            "-----------------------------d74496d66958873e\r\n"
+            "Content-Disposition: form-data; name=\"Test3\"\r\n\r\n"
+            "{\r\n--------d74496d6695887}\r\n"
+            "-----------------------------d74496d66958873e--\r\n";
+    req.method(boost::beast::http::verb::post) ;
+    crow::Request reqIn(req, ec);
+    const std::string& fileID = "Test1" ;
+
+    handleFileUpload(reqIn, asyncResp ,fileID) ;
+    EXPECT_NE(asyncResp->res.jsonValue["Description"],std::string(contentNotAcceptableMsg));
+    EXPECT_NE(asyncResp->res.jsonValue["Description"],std::string(resourceNotFoundMsg));
+    EXPECT_NE(asyncResp->res.jsonValue["Description"],std::string(badRequestMsg));
+
+} 
+TEST(handleFileUpload,  contentAcceptable2){
+    std::error_code ec;
+    crow::Response res;
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>(res);
+    boost::beast::http::request<boost::beast::http::string_body> req{};
+    req.set("Content-Type",
+            "application/octet-stream");
+
+    req.body() = "-----------------------------d74496d66958873e";
+    req.method(boost::beast::http::verb::put) ;
+    crow::Request reqIn(req, ec);
+    const std::string& fileID = "Test1" ;
+
+    handleFileUpload(reqIn, asyncResp ,fileID) ;
+    EXPECT_NE(asyncResp->res.jsonValue["Description"],std::string(contentNotAcceptableMsg));
+    EXPECT_NE(asyncResp->res.jsonValue["Description"],std::string(resourceNotFoundMsg));
+    EXPECT_NE(asyncResp->res.jsonValue["Description"],std::string(badRequestMsg));
+
+} 
+
+
 
 } // namespace ibm_mc
 } // namespace crow
