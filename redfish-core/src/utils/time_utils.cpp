01cb103ef9bb0530006aff474deda42ac6d43b17
Ed Tanous <edtanous@google.com>
2023-01-08 12:35:35 -0800

    Simplify datetime parsing
    This code as it stands pulls in the full datetime library from boost,
    including io, and a bunch of timezone code.  The bmc doesn't make use of
    any of this, so we can rely on a much simplified version.
    
    Unfortunately for us, gcc still doesn't implement the c++20
    std::chrono::parse[1].  There is a reference library available from [2]
    that backports the parse function to compilers that don't yet support
    it, and is the basis for the libc++ version.  This commit opts to copy
    in the header as-written, under the assumption that we will never need
    to pull in new versions of this library, and will move to the std
    ersion as soon as it's available in the next gcc version.
    
    This commit simplifies things down to improve compile times and binary
    size.  It saves ~22KB of compressed binary size, or about 3%.
    
    Tested: Unit tests pass.  Pretty good coverage.
    
    [1] https://en.cppreference.com/w/cpp/chrono/parse
    [2] https://github.com/HowardHinnant/date/blob/master/include/date/date.h
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I706b91cc3d9df3f32068125bc47ff0c374eb8d87

---
+++ redfish-core/src/utils/time_utils.cpp
@@ -0,0 +1,43 @@
+#include "utils/time_utils.hpp"
+
+#include "utils/extern/date.h"
+
+#include <array>
+#include <chrono>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <version>
+
+namespace redfish::time_utils
+{
+using usSinceEpoch = std::chrono::duration<uint64_t, std::micro>;
+std::optional<usSinceEpoch> dateStringToEpoch(std::string_view datetime)
+{
+    for (const char* format : std::to_array({"%FT%T%Ez", "%FT%TZ", "%FT%T"}))
+    {
+        // Parse using signed so we can detect negative dates
+        std::chrono::sys_time<std::chrono::duration<int64_t, std::micro>> date;
+        std::istringstream iss(std::string{datetime});
+#if __cpp_lib_chrono >= 201907L
+        namespace chrono_from_stream = std::chrono;
+#else
+        namespace chrono_from_stream = date;
+#endif
+        if (chrono_from_stream::from_stream(iss, format, date))
+        {
+            if (date.time_since_epoch().count() < 0)
+            {
+                return std::nullopt;
+            }
+            if (iss.rdbuf()->in_avail() != 0)
+            {
+                // More information left at end of string.
+                continue;
+            }
+            return usSinceEpoch{date.time_since_epoch().count()};
+        }
+    }
+    return std::nullopt;
+}
+} // namespace redfish::time_utils
