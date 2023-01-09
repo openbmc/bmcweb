d912fd8503a6523a1e8b0cc3aa735f6332e4a955
Ed Tanous <edtanous@google.com>
2023-01-09 10:45:53 -0800

    Fix keepalive false
    When we changed [1] to using a std::move of the Request object instead
    of an unsafe reference, we missed one spot where we were using the
    request object, post handle.  This commit moves the keepalive function
    to be set on the response object before calling handle.
    
    Tested:
    curl request with -H "Connection: close", -H "Connection: keep-alive"
    and no header all return the correct values.
    
    [1] https://github.com/openbmc/bmcweb/blob/master/http/routing.hpp#L1375
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I91fe32162407f1e1bdfcc06f1751e02d11f8a697

--- http/http_connection.hpp
+++ http/http_connection.hpp
@@ -328,6 +328,7 @@ class Connection :
             return;
         }
         thisReq.session = userSession;
+        res.keepAlive(thisReq.keepAlive());
 
         // Fetch the client IP address
         readClientIp();
@@ -440,8 +441,9 @@ class Connection :
             return;
         }
         res = std::move(thisRes);
+
         BMCWEB_LOG_INFO << "Response: " << this << ' ' << req->url << ' '
-                        << res.resultInt() << " keepalive=" << req->keepAlive();
+                        << res.resultInt() << " keepalive=" << res.keepAlive();
 
         addSecurityHeaders(*req, res);
 
@@ -508,8 +510,6 @@ class Connection :
         }
 
         res.addHeader(boost::beast::http::field::date, getCachedDateStr());
-
-        res.keepAlive(req->keepAlive());
 
         doWrite(res);
 
@@ -673,7 +673,7 @@ class Connection :
                 BMCWEB_LOG_DEBUG << this << " from write(2)";
                 return;
             }
-            if (!res.keepAlive())
+            if (!serializer->get().keep_alive())
             {
                 close();
                 BMCWEB_LOG_DEBUG << this << " from write(1)";
