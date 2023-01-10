a35f8ef19c3ec9e58961f66deb66e4dd2ade7c74
Myung Bae <myungbae@us.ibm.com>
2023-01-10 14:20:28 -0600

    Add a new meson option for setting a logging level
    In order to be able to more easily debug bmcweb related issue,
    a new meson option is added to set a specific logging level
    Which generates the targeted logging traces rather than all of debug
    traces.
    
    This option allows to enable the specific level traces (e.g. error or
    Critical instead of all traces) to be written to the journal.
    
    This new option will be effective if the existing bmcweb-logging
    feature is enabled.
    
        -Dbmcweb-logging-level=<log-level>
    
    where <log-level> can be 'debug', 'info', 'warning', 'error',
        or 'critical'.
    
     - 'debug' :  enables all of bmcweb log traces.
               If bmcweb-logging-level option is not specified,
               'debug' level will be used.
     - 'info': enables the log traces with 'info' level or higher-level -
            info, warning, error, or critical
     - 'warning': enables the log traces with 'warning' or higher level
     - 'error':  enables the log traces with 'error' or higher level
     - 'critical':  enables the log traces with 'critical' level.
    
    As an example, to enable only 'error', 'critical' log entries,
    bmcweb can be built with
    
    	-Dbmcweb-logging=enabled
    	-Dbmcweb-logging-level=error
    
    Testing:
    - Verified that only the specific logs (e.g. error and critical logs)
      were displayed by compiling bmcweb with the specific bmcweb-logging
      and bmc-logging-level options.
    
    Change-Id: I522ca26700ea420fee1a5cf688d3e8c6661f2f55
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- src/webserver_main.cpp
+++ src/webserver_main.cpp
@@ -63,7 +63,6 @@ inline void setupSocket(crow::App& app)
 
 static int run()
 {
-    crow::Logger::setLogLevel(crow::LogLevel::Debug);
 
     auto io = std::make_shared<boost::asio::io_context>();
     App app(io);
