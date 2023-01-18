1768087ad4686e1398fc8c5843126e5d10a4ee57
Ninad Palsule <ninadpalsule@us.ibm.com>
2023-01-18 13:04:47 -0600

    Added meson option for logged in connect timeout
    The default timeout value may not be sufficient for image upload in the
    slow network environment. Hence this fix provide a way for different
    recipes to tune this timeout.
    
    Tested:
      Loaded the code on system and made sure that newly tunned timeout
      value is used during code update.
    
    Change-Id: Iefccf35332b61ab685a94595ade9f8e5591ce6ec
    Signed-off-by: Ninad Palsule <ninadpalsule@us.ibm.com>

--- http/http_connection.hpp
+++ http/http_connection.hpp
@@ -576,7 +576,8 @@ class Connection :
         bool loggedIn = userSession != nullptr;
         if (loggedIn)
         {
-            timeout = std::chrono::seconds(60);
+            // Use timeout value configured based on the network speed.
+            timeout = std::chrono::seconds(bmcwebLoggedInConnectionTimeout);
         }
 
         std::weak_ptr<Connection<Adaptor, Handler>> weakSelf = weak_from_this();
