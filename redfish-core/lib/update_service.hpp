414aebca1f9a8478acbf0d98cf75bac067400ec0
moni kumar <monix.kumar@intel.com>
2023-01-25 06:23:52 +0000

    made the extendtimer configurable
    Change-Id: I96956c431288206e99bd07ccd5d29f8effef8850
    Signed-off-by: moni kumar <monix.kumar@intel.com>

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -236,7 +236,8 @@ static void
 
                             // if we're getting status updates it's
                             // still alive, update timer
-                            taskData->extendTimer(std::chrono::minutes(5));
+                            taskData->extendTimer(
+                                std::chrono::minutes(bmcwebExtendTimer));
                         }
 
                         // as firmware update often results in a
