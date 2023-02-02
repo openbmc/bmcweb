c1af9b93968eca5b0e8d0231a15662e4899a4009
Matt Spinler <spinler@us.ibm.com>
2023-02-02 08:27:26 -0600

    Check PowerSupply availability prop for State/Hlth
    This commit makes use of the Available property on the power supply
    D-Bus object when determining the power supply state and health in the
    PowerSupply response.  The power supply monitor code from phosphor-power
    will set the property to false if it can determine that the power supply
    isn't able to provide power due to certain faults.
    
    If the PS isn't available, then the Health property will be set to
    Critical, and the State will be set to UnavailableOffline (assuming it
    shouldn't be Absent instead).
    
    This is necessary because on IBM systems the Functional property
    determines the fault LED state as well as the health value, and in
    certain cases the fault LED needs to stay off even though the PS health
    is not OK.  A specific example of this is when the PS cord is unplugged:
    no fault LED is desired but it is desired for the Redfish output to show
    that there is an issue with the PS.
    
    If the Available property isn't present on D-Bus, it acts as if it had a
    value of true and behaves the same as it does today.
    
    Tested:
    Available = false:
        "Health": "Critical",
        "State": "UnavailableOffline"
    
    Available = true, Functional = false:
        "Health": "Critical",
        "State": "Enabled"
    
    PS missing:
        "Health": "Critical",
        "State": "Absent"
    
    Everything OK:
        "Health": "OK",
        "State": "Enabled"
    
    No Available property on D-Bus:
        "Health": "OK",
        "State": "Enabled"
    
    Change-Id: I1a3194bf3a6ca3936954b31439070dcc2f343411
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I8758e1e6f0ce7e7ab5ff83fa540abe1b20c6c0e2

--- redfish-core/lib/power_supply.hpp
+++ redfish-core/lib/power_supply.hpp
@@ -205,12 +205,14 @@ inline void getValidPowerSupplyPath(
 
 inline void
     getPowerSupplyState(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-                        const std::string& service, const std::string& path)
+                        const std::string& service, const std::string& path,
+                        bool available)
 {
     sdbusplus::asio::getProperty<bool>(
         *crow::connections::systemBus, service, path,
         "xyz.openbmc_project.Inventory.Item", "Present",
-        [asyncResp](const boost::system::error_code& ec, const bool value) {
+        [asyncResp, available](const boost::system::error_code& ec,
+                               const bool value) {
         if (ec)
         {
             if (ec.value() != EBADR)
@@ -226,17 +228,23 @@ inline void
         {
             asyncResp->res.jsonValue["Status"]["State"] = "Absent";
         }
+        else if (!available)
+        {
+            asyncResp->res.jsonValue["Status"]["State"] = "UnavailableOffline";
+        }
     });
 }
 
 inline void
     getPowerSupplyHealth(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-                         const std::string& service, const std::string& path)
+                         const std::string& service, const std::string& path,
+                         bool available)
 {
     sdbusplus::asio::getProperty<bool>(
         *crow::connections::systemBus, service, path,
         "xyz.openbmc_project.State.Decorator.OperationalStatus", "Functional",
-        [asyncResp](const boost::system::error_code& ec, const bool value) {
+        [asyncResp, available](const boost::system::error_code& ec,
+                               const bool value) {
         if (ec)
         {
             if (ec.value() != EBADR)
@@ -248,10 +256,36 @@ inline void
             return;
         }
 
-        if (!value)
+        if (!value || !available)
         {
             asyncResp->res.jsonValue["Status"]["Health"] = "Critical";
         }
+    });
+}
+
+inline void getPowerSupplyStateAndHealth(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& service, const std::string& path)
+{
+    sdbusplus::asio::getProperty<bool>(
+        *crow::connections::systemBus, service, path,
+        "xyz.openbmc_project.State.Decorator.Availability", "Available",
+        [asyncResp, service, path](const boost::system::error_code& ec,
+                                   const bool available) {
+        if (ec)
+        {
+            if (ec.value() != EBADR)
+            {
+                BMCWEB_LOG_ERROR("DBUS response error for Available {}",
+                                 ec.value());
+                messages::internalError(asyncResp->res);
+            }
+            return;
+        }
+
+        getPowerSupplyState(asyncResp, service, path, available);
+
+        getPowerSupplyHealth(asyncResp, service, path, available);
     });
 }
 
@@ -491,10 +525,8 @@ inline void
                 return;
             }
 
-            getPowerSupplyState(asyncResp, object.begin()->first,
-                                powerSupplyPath);
-            getPowerSupplyHealth(asyncResp, object.begin()->first,
-                                 powerSupplyPath);
+            getPowerSupplyStateAndHealth(asyncResp, object.begin()->first,
+                                         powerSupplyPath);
             getPowerSupplyAsset(asyncResp, object.begin()->first,
                                 powerSupplyPath);
             getPowerSupplyFirmwareVersion(asyncResp, object.begin()->first,
