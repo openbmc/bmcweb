0ca1aca9ecfdf476c00f6ca6fc7fb5007c89ecdf
Ed Tanous <ed@tanous.net>
2023-02-03 19:18:33 -0800

    WIP: Remove usages of nlohmann::json::begin()
    nlohmann::json::begin() throws an uncaught exception.
    
    Signed-off-by: Ed Tanous <ed@tanous.net>
    Change-Id: I08244b0787cd4d6e592b0731196490a5160aba62

--- redfish-core/include/redfish_aggregator.hpp
+++ redfish-core/include/redfish_aggregator.hpp
@@ -1134,7 +1134,13 @@ class RedfishAggregator
 
                 auto& members = asyncResp->res.jsonValue["Members"];
                 auto& satMembers = jsonVal["Members"];
-                for (auto& satMem : satMembers)
+                nlohmann::json::array_t* satMembersArr =
+                    satMembers.get_ptr<nlohmann::json::array_t*>();
+                if (satMembersArr == nullptr)
+                {
+                    return;
+                }
+                for (auto& satMem : *satMembersArr)
                 {
                     members.emplace_back(std::move(satMem));
                 }
