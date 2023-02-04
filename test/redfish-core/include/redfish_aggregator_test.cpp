f11ccc8f671f551f8fb9de5183b24c713e92d25f
Ed Tanous <edtanous@google.com>
2023-02-03 19:18:33 -0800

    WIP: Remove usages of begin()
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I08244b0787cd4d6e592b0731196490a5160aba62

--- test/redfish-core/include/redfish_aggregator_test.cpp
+++ test/redfish-core/include/redfish_aggregator_test.cpp
@@ -369,7 +369,10 @@ TEST(processCollectionResponse, localOnly)
     EXPECT_EQ(asyncResp->res.getHeaderValue("Content-Type"),
               "application/json");
     EXPECT_EQ(asyncResp->res.jsonValue["Members@odata.count"], 1);
-    for (auto& member : asyncResp->res.jsonValue["Members"])
+    const nlohmann::json::array_t* arr =
+        asyncResp->res.jsonValue["Members"]
+            .get_ptr<const nlohmann::json::array_t*>();
+    for (const auto& member : *arr)
     {
         // There should only be one member
         EXPECT_EQ(member["@odata.id"], "/redfish/v1/Systems/system");
@@ -390,7 +393,10 @@ TEST(processCollectionResponse, satelliteOnly)
     EXPECT_EQ(asyncResp->res.getHeaderValue("Content-Type"),
               "application/json");
     EXPECT_EQ(asyncResp->res.jsonValue["Members@odata.count"], 1);
-    for (auto& member : asyncResp->res.jsonValue["Members"])
+    const nlohmann::json::array_t* arr =
+        asyncResp->res.jsonValue["Members"]
+            .get_ptr<const nlohmann::json::array_t*>();
+    for (const auto& member : *arr)
     {
         // There should only be one member
         EXPECT_EQ(member["@odata.id"], "/redfish/v1/Systems/prefix_system");
@@ -414,7 +420,10 @@ TEST(processCollectionResponse, bothExist)
 
     bool foundLocal = false;
     bool foundSat = false;
-    for (const auto& member : asyncResp->res.jsonValue["Members"])
+    const nlohmann::json::array_t* arr =
+        asyncResp->res.jsonValue["Members"]
+            .get_ptr<const nlohmann::json::array_t*>();
+    for (const auto& member : *arr)
     {
         if (member["@odata.id"] == "/redfish/v1/Systems/system")
         {
@@ -446,7 +455,10 @@ TEST(processCollectionResponse, satelliteWrongContentHeader)
     EXPECT_EQ(asyncResp->res.getHeaderValue("Content-Type"),
               "application/json");
     EXPECT_EQ(asyncResp->res.jsonValue["Members@odata.count"], 1);
-    for (auto& member : asyncResp->res.jsonValue["Members"])
+    const nlohmann::json::array_t* arr =
+        asyncResp->res.jsonValue["Members"]
+            .get_ptr<const nlohmann::json::array_t*>();
+    for (const auto& member : *arr)
     {
         EXPECT_EQ(member["@odata.id"], "/redfish/v1/Systems/system");
     }
