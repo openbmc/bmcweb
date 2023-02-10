4a6b06b47f5086bc300b690b48454e02564ae7fe
Ed Tanous <edtanous@google.com>
2023-02-09 18:08:23 -0800

    WIP: Remove number support from the router
    The router historically came from crow.  Crow supported wildcards of
    <int>, <float>, and <double>.  bmcweb doesn't use them, nor should it in
    basically any case, as we now have explicit 404 handling.
    
    This commit removes them.  This amounts to about -450 lines of code, but
    it's some of the scarier code we have, some of it existing in the
    namespace "black_magic".  Reducing the brain debt for people working in
    this subsystem seems worthwhile.  There is no case in the future where
    we would use integer based url parameters.
    
    Tested: WIP.  Will test service validator and protocol validator.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I34add8df7d3486952474ca7ec3dc6be990c50ed0

--- http/common.hpp
+++ http/common.hpp
@@ -14,73 +14,10 @@ namespace crow
 
 enum class ParamType
 {
-    INT,
-    UINT,
-    DOUBLE,
     STRING,
     PATH,
 
     MAX
 };
-
-struct RoutingParams
-{
-    std::vector<int64_t> intParams;
-    std::vector<uint64_t> uintParams;
-    std::vector<double> doubleParams;
-    std::vector<std::string> stringParams;
-
-    void debugPrint() const
-    {
-        std::cerr << "RoutingParams" << std::endl;
-        for (auto i : intParams)
-        {
-            std::cerr << i << ", ";
-        }
-        std::cerr << std::endl;
-        for (auto i : uintParams)
-        {
-            std::cerr << i << ", ";
-        }
-        std::cerr << std::endl;
-        for (auto i : doubleParams)
-        {
-            std::cerr << i << ", ";
-        }
-        std::cerr << std::endl;
-        for (const std::string& i : stringParams)
-        {
-            std::cerr << i << ", ";
-        }
-        std::cerr << std::endl;
-    }
-
-    template <typename T>
-    T get(unsigned) const;
-};
-
-template <>
-inline int64_t RoutingParams::get<int64_t>(unsigned index) const
-{
-    return intParams[index];
-}
-
-template <>
-inline uint64_t RoutingParams::get<uint64_t>(unsigned index) const
-{
-    return uintParams[index];
-}
-
-template <>
-inline double RoutingParams::get<double>(unsigned index) const
-{
-    return doubleParams[index];
-}
-
-template <>
-inline std::string RoutingParams::get<std::string>(unsigned index) const
-{
-    return stringParams[index];
-}
 
 } // namespace crow
