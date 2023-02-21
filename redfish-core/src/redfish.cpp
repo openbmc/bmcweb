36de642a6929826dd13350a3062a3202f14f9e5d
George Liu <liuxiwei@inspur.com>
2023-02-21 18:02:26 +0800

    WIP: Implement Fabric PortCollection and Port schemas
    This commit implements FabricAdapter PortCollection and Port schemas.
    PortCollection is a list of associated Ports for a given FabricAdapter.
    
    It is also built using `GetAssociatedSubTreePathsById` &
    `GetAssociatedSubTreeById`.
    - https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/69999
    
    The association is defined via
    - https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/62881.
    
    The backend port examples are also committed via
    - https://gerrit.openbmc.org/c/openbmc/openpower-vpd-parser/+/66540
    - https://gerrit.openbmc.org/c/openbmc/openpower-vpd-parser/+/70888
    - https://gerrit.openbmc.org/c/openbmc/openbmc/+/66541
    
    The current submission only implements the basic properties of Port
    (e.g. Id, Name etc) as a foundation of the future additional
    properties.
    
    Tested:
    - Redfish Validator passes
    - perform GET methods like these:
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0",
      "@odata.type": "#FabricAdapter.v1_4_0.FabricAdapter",
      "Id": "disk_backplane0",
      ...
      "Ports": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports"
      },
      ...
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports",
      "@odata.type": "#PortCollection.PortCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector4"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector5"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Port Collection"
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}:18080/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector4
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector4",
      "@odata.type": "#Port.v1_7_0.Port",
      "Id": "dp0_connector4",
      "Name": "dp0_connector4"
    }%
    ```
    
    Also try the invalid port like
    
    ```
    curl -k -X GET https://${bmc}:18080/redfish/v1/Systems/system/FabricAdapters/io_module1/Ports/INVALID
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type Port named 'INVALID' was not found.",
            "MessageArgs": [
              "Port",
              "INVALID"
            ],
            "MessageId": "Base.1.16.0.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.16.0.ResourceNotFound",
        "message": "The requested resource of type Port named 'INVALID' was not found."
      }
    }%
    ```
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I8c64c16764e85c0716e264263708b18f897a2c0c
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/src/redfish.cpp
+++ redfish-core/src/redfish.cpp
@@ -14,6 +14,7 @@
 #include "event_service.hpp"
 #include "eventservice_sse.hpp"
 #include "fabric_adapters.hpp"
+#include "fabric_ports.hpp"
 #include "fan.hpp"
 #include "hypervisor_system.hpp"
 #include "log_services.hpp"
@@ -217,6 +218,7 @@ RedfishService::RedfishService(App& app)
     requestRoutesEventDestination(app);
     requestRoutesFabricAdapters(app);
     requestRoutesFabricAdapterCollection(app);
+    requestRoutesFabricPort(app);
     requestRoutesSubmitTestEvent(app);
 
     requestRoutesHypervisorSystems(app);
