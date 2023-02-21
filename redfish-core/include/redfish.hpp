13d26609b2ff8c05a9611f44f9057a04c09a1014
George Liu <liuxiwei@inspur.com>
2023-02-21 18:02:26 +0800

    Implement PortCollection and Port schemas
    This commit implements the PortCollection and Port schemas.
    PortCollection is a list of associated Ports for a given FabricAdapter.
    
    The association is defined via
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/62881.
    
    The current submission only implements the basic properties of Port
    (e.g. Id, Name etc) as a foundation of the future additional
    propertiess.
    
    Tested:
    - Redfish Validator passes
    - perform GET methods like these:
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0",
      "@odata.type": "#FabricAdapter.v1_4_0.FabricAdapter",
      "Id": "disk_backplane0",
      ...
      "Ports": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports"
      },
      ...
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports",
      "@odata.type": "#PortCollection.PortCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port2"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Port Collection"
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}:18080/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1",
      "@odata.type": "#Port.v1_7_0.Port",
      "Id": "dp0_port1",
      "Name": "dp0_port1"
    }%
    ```
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I8c64c16764e85c0716e264263708b18f897a2c0c
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -37,6 +37,7 @@
 #include "network_protocol.hpp"
 #include "pcie.hpp"
 #include "pcie_slots.hpp"
+#include "port.hpp"
 #include "power.hpp"
 #include "power_subsystem.hpp"
 #include "power_supply.hpp"
@@ -228,6 +229,8 @@ class RedfishService
         requestRoutesEventDestination(app);
         requestRoutesFabricAdapters(app);
         requestRoutesFabricAdapterCollection(app);
+        requestRoutesPort(app);
+        requestRoutesPortCollection(app);
         requestRoutesSubmitTestEvent(app);
 
         requestRoutesHypervisorSystems(app);
