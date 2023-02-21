5a677910583f7da9ed90d31b41b5681417f05fe4
George Liu <liuxiwei@inspur.com>
2023-02-21 18:02:26 +0800

    Implementation of PortCollection schema in bmcweb
    This commit implements the PortCollection collection schema.
    PortCollection is a list of associated Ports for a given FabricAdapter.
    
    The association is defined via
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/62881.
    
    The current submission only implements the basic properties of
    PortCollection(Id, Name, etc.), The Port implementation and
    implementation of Port properties such as serial number, et., will
    follow in later commits.
    
    Tested:
    - Validator passes except Port schemas which will be submitted
      separately.
      For example, they are:
    
    ```
    2 err.Port.Port errors in /redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports
    2 failMandatoryProp errors in /redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports
    1 failGet errors in /redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port2
    1 failGet errors in /redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1
    
    ```
    
    - perform GET methods like these:
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0",
      "@odata.type": "#FabricAdapter.v1_4_0.FabricAdapter",
      "Id": "disk_backplane0",
      ...
      "Ports": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports"
      },
      ...
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports",
      "@odata.type": "#PortCollection.PortCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port2"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Port Collection"
    }
    ```
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I8c64c16764e85c0716e264263708b18f897a2c0c
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

---
+++ redfish-core/lib/port.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include "app.hpp"
+#include "fabric_adapters.hpp"
+#include "human_sort.hpp"
+#include "query.hpp"
+#include "registries/privilege_registry.hpp"
+
+#include <boost/system/error_code.hpp>
+#include <boost/url/format.hpp>
+
+#include <array>
+#include <memory>
+#include <string>
+#include <string_view>
+
+namespace redfish
+{
+
+inline void getPortCollection(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                              const std::string& systemName,
+                              const std::string& fabricAdapterPath,
+                              const std::string& adapterId)
+{
+    BMCWEB_LOG_DEBUG << "Get Port collection members for: " << adapterId;
+
+    dbus::utility::getAssociationEndPoints(
+        fabricAdapterPath + "/connecting",
+        [aResp, systemName,
+         adapterId](const boost::system::error_code& ec,
+                    const dbus::utility::MapperEndPoints& endpoints) {
+        if (ec)
+        {
+            if (ec.value() == EBADR)
+            {
+                BMCWEB_LOG_DEBUG << "Port association not found";
+                return;
+            }
+            BMCWEB_LOG_ERROR << "DBUS response error " << ec.message();
+            messages::internalError(aResp->res);
+            return;
+        }
+
+        // Use the only leaf part of the portId names
+        std::vector<std::string> portIdNames;
+        for (const auto& path : endpoints)
+        {
+            std::string portId =
+                sdbusplus::message::object_path(path).filename();
+            portIdNames.emplace_back(std::move(portId));
+        }
+
+        std::sort(portIdNames.begin(), portIdNames.end(),
+                  AlphanumLess<std::string>());
+
+        nlohmann::json& members = aResp->res.jsonValue["Members"];
+        members = nlohmann::json::array();
+        for (const std::string& portId : portIdNames)
+        {
+            nlohmann::json item;
+            item["@odata.id"] = boost::urls::format(
+                "/redfish/v1/Systems/{}/FabricAdapters/{}/Ports/{}", systemName,
+                adapterId, portId);
+            members.emplace_back(std::move(item));
+        }
+        aResp->res.jsonValue["Members@odata.count"] = members.size();
+        });
+}
+
+inline void
+    handlePortCollectionHead(crow::App& app, const crow::Request& req,
+                             const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                             const std::string& systemName,
+                             const std::string& adapterId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    getValidFabricAdapterPath(adapterId, systemName, aResp,
+                              [aResp](const std::string&, const std::string&) {
+        aResp->res.addHeader(
+            boost::beast::http::field::link,
+            "</redfish/v1/JsonSchemas/PortCollection/PortCollection.json>; rel=describedby");
+    });
+}
+
+inline void doPortCollectionGet(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                                const std::string& systemName,
+                                const std::string& fabricAdapterPath,
+                                const std::string& adapterId)
+{
+    aResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/PortCollection/PortCollection.json>; rel=describedby");
+    aResp->res.jsonValue["@odata.type"] = "#PortCollection.PortCollection";
+    aResp->res.jsonValue["Name"] = "Port Collection";
+    aResp->res.jsonValue["@odata.id"] =
+        boost::urls::format("/redfish/v1/Systems/{}/FabricAdapters/{}/Ports",
+                            systemName, adapterId);
+    aResp->res.jsonValue["Members"] = nlohmann::json::array();
+    aResp->res.jsonValue["Members@odata.count"] = 0;
+
+    getPortCollection(aResp, systemName, fabricAdapterPath, adapterId);
+}
+
+inline void
+    handlePortCollectionGet(App& app, const crow::Request& req,
+                            const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                            const std::string& systemName,
+                            const std::string& adapterId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    getValidFabricAdapterPath(
+        adapterId, systemName, aResp,
+        [aResp, systemName, adapterId](const std::string& fabricAdapterPath,
+                                       const std::string&) {
+        doPortCollectionGet(aResp, systemName, fabricAdapterPath, adapterId);
+        });
+}
+
+inline void requestRoutesPortCollection(App& app)
+{
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/")
+        .privileges(redfish::privileges::headPortCollection)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handlePortCollectionHead, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/")
+        .privileges(redfish::privileges::getPortCollection)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handlePortCollectionGet, std::ref(app)));
+}
+
+} // namespace redfish
