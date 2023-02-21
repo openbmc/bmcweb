d42fc5b427cabba551ff7b098407dfdb69fd05f5
George Liu <liuxiwei@inspur.com>
2023-02-21 18:02:26 +0800

    WIP: Implement Fabric PortCollection and Port schemas
    This commit implements FabricAdapter PortCollection and Port schemas.
    PortCollection is a list of associated Ports for a given FabricAdapter.
    
    The association is defined via
    - https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/62881.
    
    The backend port examples are also committed via
    - https://gerrit.openbmc.org/c/openbmc/openpower-vpd-parser/+/66540
    - https://gerrit.openbmc.org/c/openbmc/openpower-vpd-parser/+/70888
    - https://gerrit.openbmc.org/c/openbmc/openbmc/+/66541
    
    The current submission only implements the basic properties of Port
    (e.g. Id, Name etc) as a foundation of the future additional
    properties.
    
    Tested:
    - Redfish Validator passes
    - perform GET methods like these:
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0",
      "@odata.type": "#FabricAdapter.v1_4_0.FabricAdapter",
      "Id": "disk_backplane0",
      ...
      "Ports": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports"
      },
      ...
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports",
      "@odata.type": "#PortCollection.PortCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector4"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector5"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Port Collection"
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}:18080/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector4
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector4",
      "@odata.type": "#Port.v1_7_0.Port",
      "Id": "dp0_connector4",
      "Name": "dp0_connector4"
    }%
    ```
    
    Also try the invalid port like
    
    ```
    curl -k -X GET https://${bmc}:18080/redfish/v1/Systems/system/FabricAdapters/io_module1/Ports/INVALID
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type Port named 'INVALID' was not found.",
            "MessageArgs": [
              "Port",
              "INVALID"
            ],
            "MessageId": "Base.1.16.0.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.16.0.ResourceNotFound",
        "message": "The requested resource of type Port named 'INVALID' was not found."
      }
    }%
    ```
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I8c64c16764e85c0716e264263708b18f897a2c0c
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

---
+++ redfish-core/lib/fabric_ports.hpp
@@ -0,0 +1,392 @@
+#pragma once
+
+#include "bmcweb_config.h"
+
+#include "app.hpp"
+#include "dbus_utility.hpp"
+#include "fabric_adapters.hpp"
+#include "human_sort.hpp"
+#include "query.hpp"
+#include "registries/privilege_registry.hpp"
+
+#include <boost/system/error_code.hpp>
+#include <boost/url/format.hpp>
+
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <memory>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <system_error>
+
+namespace redfish
+{
+
+inline void
+    getFabricPortProperties(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                            const std::string& systemName,
+                            const std::string& adapterId,
+                            const std::string& portId)
+{
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/Port/Port.json>; rel=describedby");
+
+    asyncResp->res.jsonValue["@odata.type"] = "#Port.v1_11_0.Port";
+    asyncResp->res.jsonValue["@odata.id"] =
+        boost::urls::format("/redfish/v1/Systems/{}/FabricAdapters/{}/Ports/{}",
+                            systemName, adapterId, portId);
+    asyncResp->res.jsonValue["Id"] = portId;
+    asyncResp->res.jsonValue["Name"] = "Fabric Port";
+}
+
+inline void afterGetAssociatedFabricPortSubTree(
+    const std::string& fabricAdapterPath, const std::string& fabricServiceName,
+    const std::function<void(const boost::system::error_code&,
+                             const dbus::utility::MapperGetSubTreeResponse&)>&
+        callback)
+{
+    if (fabricAdapterPath.empty() || fabricServiceName.empty())
+    {
+        // Adapter Not found
+        boost::system::error_code ec1{EBADR, boost::system::system_category()};
+        callback(ec1, dbus::utility::MapperGetSubTreeResponse{});
+        return;
+    }
+
+    constexpr std::array<std::string_view, 1> portInterfaces{
+        "xyz.openbmc_project.Inventory.Connector.Port"};
+    dbus::utility::getAssociatedSubTree(
+        fabricAdapterPath + "/connecting",
+        sdbusplus::message::object_path("/xyz/openbmc_project/inventory"), 0,
+        portInterfaces,
+        [callback](const boost::system::error_code& ec,
+                   const dbus::utility::MapperGetSubTreeResponse& portSubTree) {
+        callback(ec, portSubTree);
+    });
+}
+
+inline void getAssociatedFabricPortSubTree(
+    const std::string& adapterId,
+    std::function<void(const boost::system::error_code&,
+                       const dbus::utility::MapperGetSubTreeResponse&)>&&
+        callback)
+{
+    getValidFabricAdapterPath(
+        adapterId,
+        [callback{std::move(callback)}](const boost::system::error_code& ec,
+                                        const std::string& fabricAdapterPath,
+                                        const std::string& fabricServiceName) {
+        if (ec)
+        {
+            callback(ec, dbus::utility::MapperGetSubTreeResponse{});
+            return;
+        }
+        afterGetAssociatedFabricPortSubTree(fabricAdapterPath,
+                                            fabricServiceName, callback);
+    });
+}
+
+inline void afterGetValidFabricPortPath(
+    const std::string& portId,
+    std::function<void(const boost::system::error_code&,
+                       const std::string& portPath,
+                       const std::string& portServiceName)>& callback,
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreeResponse& portSubTree)
+{
+    if (ec)
+    {
+        callback(ec, std::string{}, std::string{});
+        return;
+    }
+    for (const auto& [portPath, serviceMap] : portSubTree)
+    {
+        if (portId == sdbusplus::message::object_path(portPath).filename())
+        {
+            callback(ec, portPath, serviceMap.begin()->first);
+            return;
+        }
+    }
+    boost::system::error_code ec2{EBADR, boost::system::system_category()};
+    callback(ec2, std::string{}, std::string{});
+    return;
+}
+
+inline void getValidFabricPortPath(
+    const std::string& adapterId, const std::string& portId,
+    std::function<void(const boost::system::error_code& ec,
+                       const std::string& portPath,
+                       const std::string& portServiceName)>&& callback)
+{
+    getAssociatedFabricPortSubTree(
+        adapterId, std::bind_front(afterGetValidFabricPortPath, portId,
+                                   std::move(callback)));
+}
+
+inline void afterHandleFabricPortHead(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& portId, const boost::system::error_code& ec)
+{
+    if (ec)
+    {
+        if (ec.value() != EBADR)
+        {
+            BMCWEB_LOG_ERROR("DBUS response error {}", ec.value());
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        BMCWEB_LOG_WARNING("Port not found");
+        messages::resourceNotFound(asyncResp->res, "Port", portId);
+        return;
+    }
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/Port/Port.json>; rel=describedby");
+}
+
+inline void
+    handleFabricPortHead(crow::App& app, const crow::Request& req,
+                         const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                         const std::string& systemName,
+                         const std::string& adapterId,
+                         const std::string& portId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    if constexpr (bmcwebEnableMultiHost)
+    {
+        // Option currently returns no systems.  TBD
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+    if (systemName != "system")
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+
+    getValidFabricPortPath(
+        adapterId, portId,
+        [asyncResp, portId](const boost::system::error_code& ec,
+                            const std::string& /*unused*/,
+                            const std::string& /*unused*/) {
+        afterHandleFabricPortHead(asyncResp, portId, ec);
+    });
+}
+
+inline void afterHandleFabricPortGet(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemName, const std::string& adapterId,
+    const std::string& portId, const boost::system::error_code& ec)
+{
+    if (ec)
+    {
+        if (ec.value() != EBADR)
+        {
+            BMCWEB_LOG_ERROR("DBUS response error {}", ec.value());
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        BMCWEB_LOG_WARNING("Port not found");
+        messages::resourceNotFound(asyncResp->res, "Port", portId);
+        return;
+    }
+    getFabricPortProperties(asyncResp, systemName, adapterId, portId);
+}
+
+inline void
+    handleFabricPortGet(App& app, const crow::Request& req,
+                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                        const std::string& systemName,
+                        const std::string& adapterId, const std::string& portId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    if constexpr (bmcwebEnableMultiHost)
+    {
+        // Option currently returns no systems.  TBD
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+    if (systemName != "system")
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+    getValidFabricPortPath(adapterId, portId,
+                           [asyncResp, systemName, adapterId,
+                            portId](const boost::system::error_code& ec,
+                                    const std::string& /*unused*/,
+                                    const std::string& /*unused*/) {
+        afterHandleFabricPortGet(asyncResp, systemName, adapterId, portId, ec);
+    });
+}
+
+inline void afterHandleFarbicPortCollectionHead(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& adapterId, const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreeResponse& /*unused*/)
+{
+    if (ec)
+    {
+        if (ec.value() != EBADR)
+        {
+            BMCWEB_LOG_ERROR("DBUS response error {}", ec.value());
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        BMCWEB_LOG_WARNING("Adapter not found");
+        messages::resourceNotFound(asyncResp->res, "Adapter", adapterId);
+        return;
+    }
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/PortCollection/PortCollection.json>; rel=describedby");
+}
+
+inline void handleFarbicPortCollectionHead(
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemName, const std::string& adapterId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    if constexpr (bmcwebEnableMultiHost)
+    {
+        // Option currently returns no systems.  TBD
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+    if (systemName != "system")
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+
+    getAssociatedFabricPortSubTree(
+        adapterId, std::bind_front(afterHandleFarbicPortCollectionHead,
+                                   asyncResp, adapterId));
+}
+
+inline void doHandleFabricPortCollectionGet(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemName, const std::string& adapterId,
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreeResponse& portSubTree)
+{
+    if (ec)
+    {
+        if (ec.value() != EBADR)
+        {
+            BMCWEB_LOG_ERROR("DBUS response error {}", ec.value());
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        BMCWEB_LOG_WARNING("Adapter not found");
+        messages::resourceNotFound(asyncResp->res, "Adapter", adapterId);
+        return;
+    }
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/PortCollection/PortCollection.json>; rel=describedby");
+
+    asyncResp->res.jsonValue["@odata.type"] = "#PortCollection.PortCollection";
+    asyncResp->res.jsonValue["Name"] = "Port Collection";
+    asyncResp->res.jsonValue["@odata.id"] =
+        boost::urls::format("/redfish/v1/Systems/{}/FabricAdapters/{}/Ports",
+                            systemName, adapterId);
+    asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+
+    std::vector<std::string> portIdNames;
+    for (const auto& [portPath, serviceMap] : portSubTree)
+    {
+        std::string portId =
+            sdbusplus::message::object_path(portPath).filename();
+        if (!portId.empty())
+        {
+            portIdNames.emplace_back(std::move(portId));
+        }
+    }
+
+    std::ranges::sort(portIdNames, AlphanumLess<std::string>());
+
+    nlohmann::json& members = asyncResp->res.jsonValue["Members"];
+    for (const std::string& portId : portIdNames)
+    {
+        nlohmann::json item;
+        item["@odata.id"] = boost::urls::format(
+            "/redfish/v1/Systems/{}/FabricAdapters/{}/Ports/{}", systemName,
+            adapterId, portId);
+        members.emplace_back(std::move(item));
+    }
+    asyncResp->res.jsonValue["Members@odata.count"] = members.size();
+}
+
+inline void handleFabricPortCollectionGet(
+    App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemName, const std::string& adapterId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    if constexpr (bmcwebEnableMultiHost)
+    {
+        // Option currently returns no systems.  TBD
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+    if (systemName != "system")
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+
+    getAssociatedFabricPortSubTree(
+        adapterId, std::bind_front(doHandleFabricPortCollectionGet, asyncResp,
+                                   systemName, adapterId));
+}
+inline void requestRoutesFabricPort(App& app)
+{
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/<str>/")
+        .privileges(redfish::privileges::headPort)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handleFabricPortHead, std::ref(app)));
+
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/<str>/")
+        .privileges(redfish::privileges::getPort)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handleFabricPortGet, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/")
+        .privileges(redfish::privileges::headPortCollection)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handleFarbicPortCollectionHead, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/")
+        .privileges(redfish::privileges::getPortCollection)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handleFabricPortCollectionGet, std::ref(app)));
+}
+
+} // namespace redfish
