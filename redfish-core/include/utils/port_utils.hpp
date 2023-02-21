84d02352b2c7c9d6dfe65230709518db273b1ab4
George Liu <liuxiwei@inspur.com>
2023-02-21 18:02:26 +0800

    Implement PortCollection and Port schemas
    This commit implements the PortCollection and Port schemas.
    PortCollection is a list of associated Ports for a given FabricAdapter.
    
    The association is defined via
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/62881.
    
    The current submission only implements the basic properties of Port
    (e.g. Id, Name etc) as a foundation of the future additional
    propertiess.
    
    Tested:
    - Redfish Validator passes
    - perform GET methods like these:
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0",
      "@odata.type": "#FabricAdapter.v1_4_0.FabricAdapter",
      "Id": "disk_backplane0",
      ...
      "Ports": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports"
      },
      ...
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports",
      "@odata.type": "#PortCollection.PortCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port2"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Port Collection"
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}:18080/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_port1",
      "@odata.type": "#Port.v1_7_0.Port",
      "Id": "dp0_port1",
      "Name": "dp0_port1"
    }%
    ```
    
    Also try the invalid port like
    
    ```
    curl -k -X GET https://${bmc}:18080/redfish/v1/Systems/system/FabricAdapters/io_module1/Ports/INVALID
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type Port named 'INVALID' was not found.",
            "MessageArgs": [
              "Port",
              "INVALID"
            ],
            "MessageId": "Base.1.16.0.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.16.0.ResourceNotFound",
        "message": "The requested resource of type Port named 'INVALID' was not found."
      }
    }%
    ```
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I8c64c16764e85c0716e264263708b18f897a2c0c
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

---
+++ redfish-core/include/utils/port_utils.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "async_resp.hpp"
+#include "dbus_utility.hpp"
+#include "error_messages.hpp"
+
+#include <array>
+#include <functional>
+#include <string>
+#include <string_view>
+
+namespace redfish
+{
+
+namespace port_utils
+{
+
+inline void afterGetPortList(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::function<void(const dbus::utility::MapperEndPoints&)>& callback,
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperEndPoints& portPaths)
+{
+    if (ec)
+    {
+        if (ec.value() == EBADR)
+        {
+            BMCWEB_LOG_DEBUG("Port association not found");
+            return;
+        }
+        BMCWEB_LOG_ERROR("DBUS response error {}", ec.value());
+        messages::internalError(asyncResp->res);
+        return;
+    }
+    // caller will handle the case of empty Ports
+    callback(portPaths);
+}
+
+/**
+ * @brief Retrieves Ports
+ * @param asyncResp   Pointer to object holding response data
+ * @param fabricAdapterPath Adapter dbus path
+ * @param callback  Callback for next step to handle port list
+ */
+
+inline void getPortList(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& fabricAdapterPath,
+    std::function<void(const dbus::utility::MapperEndPoints&)>&& callback)
+{
+    constexpr std::array<std::string_view, 1> portInterface{
+        "xyz.openbmc_project.Inventory.Connector.Port"};
+
+    dbus::utility::getAssociatedSubTreePaths(
+        fabricAdapterPath + "/connecting",
+        sdbusplus::message::object_path("/xyz/openbmc_project/inventory"), 0,
+        portInterface, std::bind_front(afterGetPortList, asyncResp, callback));
+}
+
+} // namespace port_utils
+} // namespace redfish
