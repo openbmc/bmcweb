9380b413a5d2bb541288a696db30af108338d1eb
Ed Tanous <edtanous@google.com>
2023-02-21 10:27:14 -0800

    Disable token compress in str
    There are certain cases where we use this split function, and we expect
    tokens to be read out.  For example:
    
    /xyz/openbmc_project/sensors/unit/name
    
    Should split into a "" in the first position.  This use case is not
    common, and a quick grep shows only two places in the code expect this
    behavior.
    
    Tested: New unit tests passing.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Iec3dcbf2b495b2b3b4ed419172c4133b16f7c65d

--- test/include/str_utility_test.cpp
+++ test/include/str_utility_test.cpp
@@ -23,9 +23,31 @@ TEST(Split, PositiveTests)
     std::vector<std::string> vec;
     split(vec, "xx-abc-xx-abb", '-');
     EXPECT_THAT(vec, ElementsAre("xx", "abc", "xx", "abb"));
+
     vec.clear();
     split(vec, "", '-');
-    EXPECT_THAT(vec, IsEmpty());
+    EXPECT_THAT(vec, ElementsAre(""));
+
+    vec.clear();
+    split(vec, "foo/", '/');
+    EXPECT_THAT(vec, ElementsAre("foo", ""));
+
+    vec.clear();
+    split(vec, "/bar", '/');
+    EXPECT_THAT(vec, ElementsAre("", "bar"));
+
+    vec.clear();
+    split(vec, "/", '/');
+    EXPECT_THAT(vec, ElementsAre("", ""));
+}
+
+TEST(Split, Sensor)
+{
+    using bmcweb::split;
+    std::vector<std::string> vec;
+    split(vec, "/xyz/openbmc_project/sensors/unit/name", '/');
+    EXPECT_THAT(vec, ElementsAre("", "xyz", "openbmc_project", "sensors",
+                                 "unit", "name"));
 }
 
 } // namespace
