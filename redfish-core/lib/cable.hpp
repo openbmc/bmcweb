8a4dcac93b07dbd2ec62b7ba99b4489faa41f59f
Myung Bae <myungbae@us.ibm.com>
2023-02-22 06:13:01 -0500

    WIP: Add PCIe Topology Cable Links
    NOTE: On-hold until the following PRs are merged.
      - https://gerrit.openbmc.org/c/openbmc/bmcweb/+/60872
      - https://gerrit.openbmc.org/c/openbmc/bmcweb/+/39574
    
    This commit adds a downstream/upstream port, downstream/upstream resource, and
    downstream/upstream chassis link into the cable JSON response, which represents
    an association between resources associated via cables for williwakas
    and cable cards.
    
    Tested & verified Redfish-Service-Validator
    
    ```
    $ curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Cables/dp0_cable0
    {
      "@odata.id": "/redfish/v1/Cables/dp0_cable0",
      "@odata.type": "#Cable.v1_2_0.Cable",
      "CableStatus": "Normal",
      "CableType": "",
      "Id": "dp0_cable0",
      "Links": {
        "DownstreamPorts": [
          {
            "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/disk_backplane0/Ports/dp0_connector1"
          }
        ],
        "DownstreamResources": [
          {
            "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/24"
          }
        ],
        "UpstreamPorts": [
          {
            "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_card10/Ports/c10_connector0"
          }
        ],
        "UpstreamResources": [
          {
            "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card8"
          },
          {
            "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card10"
          }
        ]
      },
      "Name": "Cable",
      ...
    }
    ```
    
    Change-Id: I87a3daf5c4f9ed4af6b9fdf967ca6852c3babeba
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/lib/cable.hpp
+++ redfish-core/lib/cable.hpp
@@ -10,12 +10,59 @@
 #include <boost/system/error_code.hpp>
 #include <sdbusplus/asio/property.hpp>
 #include <sdbusplus/unpack_properties.hpp>
+#include <utils/chassis_utils.hpp>
+#include <utils/dbus_utils.hpp>
+#include <utils/json_utils.hpp>
 
 #include <array>
+#include <memory>
+#include <string>
 #include <string_view>
+#include <variant>
+#include <vector>
 
 namespace redfish
 {
+
+/**
+ * @brief Api to get Cable Association.
+ * @param[in,out]   asyncResp       Async HTTP response.
+ * @param[in]       cableObjectPath Object path of the Cable with association.
+ * @param[in]       callback        callback method
+ */
+template <typename Callback>
+inline void
+    linkAssociatedCable(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                        const std::string& cableObjectPath, Callback&& callback)
+{
+
+    dbus::utility::getAssociationEndPoints(
+        cableObjectPath,
+        [asyncResp, callback](const boost::system::error_code& ec,
+                              const dbus::utility::MapperEndPoints& endpoints) {
+        if (ec)
+        {
+            if (ec.value() == EBADR)
+            {
+                // This cable have no association.
+                BMCWEB_LOG_DEBUG << "No association found";
+                return;
+            }
+            BMCWEB_LOG_ERROR << "DBUS response error" << ec.message();
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        if (endpoints.empty())
+        {
+            BMCWEB_LOG_DEBUG << "No association found for cable";
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        callback(endpoints);
+        });
+}
+
 /**
  * @brief Fill cable specific properties.
  * @param[in,out]   resp        HTTP response.
@@ -36,10 +83,11 @@ inline void
 
     const std::string* cableTypeDescription = nullptr;
     const double* length = nullptr;
+    const std::string* cableStatus = nullptr;
 
     const bool success = sdbusplus::unpackPropertiesNoThrow(
         dbus_utils::UnpackErrorPrinter(), properties, "CableTypeDescription",
-        cableTypeDescription, "Length", length);
+        cableTypeDescription, "Length", length, "CableStatus", cableStatus);
 
     if (!success)
     {
@@ -68,6 +116,31 @@ inline void
             resp.jsonValue["LengthMeters"] = *length;
         }
     }
+
+    if (cableStatus != nullptr && !cableStatus->empty())
+    {
+        if (*cableStatus ==
+            "xyz.openbmc_project.Inventory.Item.Cable.Status.Inactive")
+        {
+            resp.jsonValue["CableStatus"] = "Normal";
+            resp.jsonValue["Status"]["State"] = "StandbyOffline";
+            resp.jsonValue["Status"]["Health"] = "OK";
+        }
+        else if (*cableStatus == "xyz.openbmc_project.Inventory.Item."
+                                 "Cable.Status.Running")
+        {
+            resp.jsonValue["CableStatus"] = "Normal";
+            resp.jsonValue["Status"]["State"] = "Enabled";
+            resp.jsonValue["Status"]["Health"] = "OK";
+        }
+        else if (*cableStatus == "xyz.openbmc_project.Inventory.Item."
+                                 "Cable.Status.PoweredOff")
+        {
+            resp.jsonValue["CableStatus"] = "Disabled";
+            resp.jsonValue["Status"]["State"] = "StandbyOffline";
+            resp.jsonValue["Status"]["Health"] = "OK";
+        }
+    }
 }
 
 /**
@@ -83,6 +156,170 @@ inline void
                        const dbus::utility::MapperServiceMap& serviceMap)
 {
     BMCWEB_LOG_DEBUG << "Get Properties for cable " << cableObjectPath;
+
+    // retrieve Upstream Resources
+    std::string upstreamResource = cableObjectPath + "/upstream_resource";
+    linkAssociatedCable(asyncResp, upstreamResource,
+                        [asyncResp](const std::vector<std::string>& value) {
+        nlohmann::json& linkArray =
+            asyncResp->res.jsonValue["Links"]["UpstreamResources"];
+        linkArray = nlohmann::json::array();
+
+        for (const auto& fullPath : value)
+        {
+            sdbusplus::message::object_path path(fullPath);
+            std::string leaf = path.filename();
+            if (leaf.empty())
+            {
+                continue;
+            }
+            linkArray.push_back(
+                {{"@odata.id",
+                  "/redfish/v1/Systems/system/PCIeDevices/" + leaf}});
+        }
+    });
+
+    // retrieve Downstream Resources
+    auto chassisAssociation =
+        [asyncResp,
+         cableObjectPath](std::vector<std::string>& updatedAssemblyList) {
+        std::string downstreamResource =
+            cableObjectPath + "/downstream_resource";
+        linkAssociatedCable(asyncResp, downstreamResource,
+                            [asyncResp, updatedAssemblyList](
+                                const std::vector<std::string>& value) {
+            nlohmann::json& linkArray =
+                asyncResp->res.jsonValue["Links"]["DownstreamResources"];
+            linkArray = nlohmann::json::array();
+
+            for (const auto& fullPath : value)
+            {
+                auto it = find(updatedAssemblyList.begin(),
+                               updatedAssemblyList.end(), fullPath);
+
+                // If element was found
+                if (it != updatedAssemblyList.end())
+                {
+
+                    uint index =
+                        static_cast<uint>(it - updatedAssemblyList.begin());
+                    linkArray.push_back(
+                        {{"@odata.id", "/redfish/v1/Chassis/chassis/"
+                                       "Assembly#/Assemblies/" +
+                                           std::to_string(index)}});
+                }
+                else
+                {
+                    BMCWEB_LOG_ERROR << "in Downstream Resources " << fullPath
+                                     << " isn't found in chassis assembly list";
+                }
+            }
+        });
+    };
+
+    redfish::chassis_utils::getChassisAssembly(asyncResp, "chassis",
+                                               std::move(chassisAssociation));
+
+    // retrieve Upstream Ports
+    std::string upstreamConnector = cableObjectPath + "/upstream_connector";
+    linkAssociatedCable(asyncResp, upstreamConnector,
+                        [asyncResp](const std::vector<std::string>& value) {
+        nlohmann::json& linkArray =
+            asyncResp->res.jsonValue["Links"]["UpstreamPorts"];
+        linkArray = nlohmann::json::array();
+
+        for (const auto& fullPath : value)
+        {
+            sdbusplus::message::object_path path(fullPath);
+            std::string leaf = path.filename();
+            if (leaf.empty())
+            {
+                continue;
+            }
+            std::string endpointLeaf = path.parent_path().filename();
+            if (endpointLeaf.empty())
+            {
+                continue;
+            }
+
+            //  insert/create link using endpointLeaf
+            endpointLeaf += "/Ports/";
+            endpointLeaf += leaf;
+            linkArray.push_back(
+                {{"@odata.id", "/redfish/v1/Systems/system/FabricAdapters/" +
+                                   endpointLeaf}});
+        }
+    });
+
+    // retrieve Downstream Ports
+    std::string downstreamConnector = cableObjectPath + "/downstream_connector";
+    linkAssociatedCable(asyncResp, downstreamConnector,
+                        [asyncResp](const std::vector<std::string>& value) {
+        nlohmann::json& linkArray =
+            asyncResp->res.jsonValue["Links"]["DownstreamPorts"];
+        linkArray = nlohmann::json::array();
+
+        for (const auto& fullPath : value)
+        {
+            sdbusplus::message::object_path path(fullPath);
+            std::string leaf = path.filename();
+            if (leaf.empty())
+            {
+                continue;
+            }
+            std::string endpointLeaf = path.parent_path().filename();
+            if (endpointLeaf.empty())
+            {
+                continue;
+            }
+
+            //  insert/create link suing endpointLeaf
+            endpointLeaf += "/Ports/";
+            endpointLeaf += leaf;
+            linkArray.push_back(
+                {{"@odata.id", "/redfish/v1/Systems/system/FabricAdapters/" +
+                                   endpointLeaf}});
+        }
+    });
+
+    // retrieve Upstream Chassis
+    std::string upstreamChassis = cableObjectPath + "/upstream_chassis";
+    linkAssociatedCable(asyncResp, upstreamChassis,
+                        [asyncResp](const std::vector<std::string>& value) {
+        nlohmann::json& linkArray =
+            asyncResp->res.jsonValue["Links"]["UpstreamChassis"];
+        linkArray = nlohmann::json::array();
+
+        for (const auto& fullPath : value)
+        {
+            sdbusplus::message::object_path path(fullPath);
+            std::string leaf = path.filename();
+            if (leaf.empty())
+            {
+                continue;
+            }
+            linkArray.push_back({{"@odata.id", "/redfish/v1/Chassis/" + leaf}});
+        }
+    });
+
+    // retrieve Downstream Chassis
+    std::string downstreamChassis = cableObjectPath + "/downstream_chassis";
+    linkAssociatedCable(asyncResp, downstreamChassis,
+                        [asyncResp](const std::vector<std::string>& value) {
+        nlohmann::json& linkArray =
+            asyncResp->res.jsonValue["Links"]["DownstreamChassis"];
+        linkArray = nlohmann::json::array();
+        for (const auto& fullPath : value)
+        {
+            sdbusplus::message::object_path path(fullPath);
+            std::string leaf = path.filename();
+            if (leaf.empty())
+            {
+                continue;
+            }
+            linkArray.push_back({{"@odata.id", "/redfish/v1/Chassis/" + leaf}});
+        }
+    });
 
     for (const auto& [service, interfaces] : serviceMap)
     {
@@ -100,6 +337,26 @@ inline void
                     const boost::system::error_code ec,
                     const dbus::utility::DBusPropertiesMap& properties) {
                 fillCableProperties(asyncResp->res, ec, properties);
+                });
+
+            sdbusplus::asio::getProperty<std::string>(
+                *crow::connections::systemBus, service, cableObjectPath,
+                "xyz.openbmc_project.Inventory.Decorator.Asset", "PartNumber",
+                [asyncResp](const boost::system::error_code ec,
+                            const std::string& property) {
+                if (ec.value() == EBADR)
+                {
+                    // PartNumber is optional, ignore the failure if it
+                    // doesn't exist.
+                    return;
+                }
+                if (ec)
+                {
+                    BMCWEB_LOG_DEBUG << "DBus response error for PartNumber";
+                    messages::internalError(asyncResp->res);
+                    return;
+                }
+                asyncResp->res.jsonValue["PartNumber"] = property;
                 });
         }
     }
@@ -149,7 +406,7 @@ inline void requestRoutesCable(App& app)
                     continue;
                 }
 
-                asyncResp->res.jsonValue["@odata.type"] = "#Cable.v1_0_0.Cable";
+                asyncResp->res.jsonValue["@odata.type"] = "#Cable.v1_2_0.Cable";
                 asyncResp->res.jsonValue["@odata.id"] =
                     "/redfish/v1/Cables/" + cableId;
                 asyncResp->res.jsonValue["Id"] = cableId;
