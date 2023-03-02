51196ff0e03aa33c4ac53f3ef14059fef6d53213
Ed Tanous <edtanous@google.com>
2023-03-01 16:34:26 -0800

    RFC: Track response time
    This commit adds connection tracking telemetry to bmcweb, with
    statistics reported externally to the system.  These statistics are
    intended to be collected through automation, and to provide a per-boot
    view of the health of a system at any given point in time.
    
    At this time, these are implemented as OpenBMC OEM schemas.  This is
    because this kind of data is system-specific.  For example, the bmcweb
    webserver implements a flow of read -> process -> write, all as
    independent stages.  Many other webservers treat these as a
    "streaming" operation, where read/process/write are all in one section.
    
    Each of the metrics are implemented as counters, such that averages can
    be determined for any arbitrary section of time, without requiring the
    BMC itself to pick a given time period.
    
    For example, to get the average processing latency for any given request
    between two points T0 and T1, one would do the math:
    
    (ProcessingLatencyMs[T1] - ProcessingLatencyMs[T0]) / (ResponseCount[T1]
    - ResponseCount[T0])
    
    This would return the average processing latency for any given time
    period.  If one wanted to measure "all time" one could simply replace
    the T0 values with 0, as all counters start at zero.
    
    Services looking to determine when the counters were reset can query the
    ServiceRootUptimeSeconds parameter, recently added to
    ManagerDiagnosticData, can be used to determine the "time since reset"
    and to generate any sort of "per second" metrics.  For example:
    
    ResponseCount / ServiceRootUptimeSeconds
    
    Would give the average number of requests per second the service was
    handling.
    
    In addition to the above, it also provides a property "ResponseCodes",
    that tracks counters of each response code, and how many replies have
    carried that code.  The array is initially populated with the list of
    OpenBMC codes that bmcweb uses, but potentially could get new entries if
    aggregated client controllers respond with additional error codes.
    
    Times are acquired using a new class, fast_monotonic_clock, which is a
    std::chrono compliant implementation that backs with
    CLOCK_MONOTONIC_COARSE.  This is added to reduce the impact of the
    measurements on the total system performance compared to CLOCK_MONOTONIC
    or CLOCK_REALTIME.
    
    Tested:
    OEM schema passes the Redfish csdl validator
    Redfish service validator run
    
    GET /redfish/v1/Managers/bmc/ManagerDiagnosticData
    returns a link to HttpStatistics
    
    GET /redfish/v1/Managers/bmc/ManagerDiagnosticData/OpenBMCHttpStatistics
    returns the required parameters.  These parameters can be seen to be
    incrementing on each request.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: If2a06d3061795596c030986986bf52e3f9bb97f2

---
+++ http/fast_monotonic_clock.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <chrono>
+
+class fast_monotonic_clock
+{
+  public:
+    using duration = std::chrono::nanoseconds;
+    using rep = duration::rep;
+    using period = duration::period;
+    using time_point = std::chrono::time_point<fast_monotonic_clock>;
+
+    static constexpr bool is_steady = true;
+
+    static time_point now() noexcept
+    {
+        struct timespec t
+        {};
+        // Return code intentionally ignored here.  chrono clock API doesn't
+        // allow for failures.
+        clock_gettime(CLOCK_MONOTONIC_COARSE, &t);
+        return time_point{convert(t)};
+    }
+
+    static duration get_resolution() noexcept
+    {
+        struct timespec t
+        {};
+        clock_getres(CLOCK_MONOTONIC_COARSE, &t);
+        return convert(t);
+    }
+
+  private:
+    static duration convert(const timespec& t)
+    {
+        return std::chrono::seconds(t.tv_sec) +
+               std::chrono::nanoseconds(t.tv_nsec);
+    }
+};
---
+++ http/statistics.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <boost/beast/http/status.hpp>
+#include <boost/container/flat_map.hpp>
+
+#include <chrono>
+
+namespace bmcweb
+{
+
+// Duration type that uses int64_t on 32 bit systems
+using int64_duration = std::chrono::duration<int64_t, std::milli>;
+
+struct ConnectionStatistics
+{
+    int64_t opened = 0;
+    int64_t closed = 0;
+    int64_t bytesSent = 0;
+    int64_t bytesReceived = 0;
+    int64_t requestCount = 0;
+    int64_t responseCount = 0;
+
+    using status = boost::beast::http::status;
+    // Prepopulate with existing codes to make it easier on users.
+    boost::container::flat_map<status, unsigned> responseStatus{
+        {status::accepted, 0},
+        {status::bad_gateway, 0},
+        {status::bad_request, 0},
+        {status::conflict, 0},
+        {status::created, 0},
+        {status::forbidden, 0},
+        {status::insufficient_storage, 0},
+        {status::internal_server_error, 0},
+        {status::method_not_allowed, 0},
+        {status::no_content, 0},
+        {status::not_acceptable, 0},
+        {status::not_found, 0},
+        {status::not_implemented, 0},
+        {status::not_modified, 0},
+        {status::ok, 0},
+        {status::precondition_failed, 0},
+        {status::service_unavailable, 0},
+        {status::temporary_redirect, 0},
+        {status::too_many_requests, 0},
+        {status::unauthorized, 0},
+        {status::unsupported_media_type, 0}};
+
+    // Connections go through a lifecycle
+    // TLS ─► Read ─► Process ─► Write
+    //         ▲                   │
+    //         └───── Idle ◄───────┘
+    //
+    // The following counters reprent the total time that any connection spent
+    // in the various states.
+    int64_duration tlsHandshakeTotalMs{};
+    int64_duration requestLatencyTotalMs{};
+    int64_duration processingLatencyTotalMs{};
+    int64_duration responseWriteLatencyTotalMs{};
+    int64_duration idleLatencyTotalMs{};
+
+    void appendResponseCode(status code)
+    {
+        responseStatus[code]++;
+    }
+};
+
+inline ConnectionStatistics& stats()
+{
+    static ConnectionStatistics stats;
+    return stats;
+}
+} // namespace bmcweb
