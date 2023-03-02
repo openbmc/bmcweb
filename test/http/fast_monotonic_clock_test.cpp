8184c0ff8eb15c8fde04c51d90ed7f6614e41231
Ed Tanous <edtanous@google.com>
2023-03-01 16:34:26 -0800

    RFC: Track response time
    This commit adds connection tracking telemetry to bmcweb, with
    statistics reported externally to the system.  These statistics are
    intended to be collected through automation, and to provide a per-boot
    view of the health of a system at any given point in time.
    
    At this time, these are implemented as OpenBMC OEM schemas.  This is
    because this kind of data is system-specific.  For example, the bmcweb
    webserver implements a flow of read -> process -> write, all as
    independent stages.  Many other webservers treat these as a
    "streaming" operation, where read/process/write are all in one section.
    
    Each of the metrics are implemented as counters, such that averages can
    be determined for any arbitrary section of time, without requiring the
    BMC itself to pick a given time period.
    
    For example, to get the average processing latency for any given request
    between two points T0 and T1, one would do the math:
    
    (ProcessingLatencyMs[T1] - ProcessingLatencyMs[T0]) / (ResponseCount[T1]
    - ResponseCount[T0])
    
    This would return the average processing latency for any given time
    period.  If one wanted to measure "all time" one could simply replace
    the T0 values with 0, as all counters start at zero.
    
    Services looking to determine when the counters were reset can query the
    ServiceRootUptimeSeconds parameter, recently added to
    ManagerDiagnosticData, can be used to determine the "time since reset"
    and to generate any sort of "per second" metrics.  For example:
    
    ResponseCount / ServiceRootUptimeSeconds
    
    Would give the average number of requests per second the service was
    handling.
    
    In addition to the above, it also provides a property "ResponseCodes",
    that tracks counters of each response code, and how many replies have
    carried that code.  The array is initially populated with the list of
    OpenBMC codes that bmcweb uses, but potentially could get new entries if
    aggregated client controllers respond with additional error codes.
    
    Times are acquired using a new class, fast_monotonic_clock, which is a
    std::chrono compliant implementation that backs with
    CLOCK_MONOTONIC_COARSE.  This is added to reduce the impact of the
    measurements on the total system performance compared to CLOCK_MONOTONIC
    or CLOCK_REALTIME.
    
    Tested:
    OEM schema passes the Redfish csdl validator
    Redfish service validator run
    
    GET /redfish/v1/Managers/bmc/ManagerDiagnosticData
    returns a link to HttpStatistics
    
    GET /redfish/v1/Managers/bmc/ManagerDiagnosticData/OpenBMCHttpStatistics
    returns the required parameters.  These parameters can be seen to be
    incrementing on each request.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: If2a06d3061795596c030986986bf52e3f9bb97f2

---
+++ test/http/fast_monotonic_clock_test.cpp
@@ -0,0 +1,13 @@
+#include "fast_monotonic_clock.hpp"
+
+#include <gtest/gtest.h> // IWYU pragma: keep
+
+TEST(FastMonotonicClock, BasicUsage)
+{
+    static_assert(fast_monotonic_clock::is_steady);
+
+    fast_monotonic_clock clock;
+
+    EXPECT_GT(clock.now().time_since_epoch().count(), 0);
+    EXPECT_EQ(clock.get_resolution(), std::chrono::milliseconds(4));
+}
