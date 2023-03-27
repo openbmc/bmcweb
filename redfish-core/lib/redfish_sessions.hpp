604ffa8c26ff3cf5a36d51967d25d01386557c73
Ninad Palsule <ninadpalsule@us.ibm.com>
2023-03-27 17:19:55 -0500

    Added new pre-defined usergroup called hostconsole
    The new pre-defined usergroup named "hostconsole" is added to
    differentiate access between host console and manager console.
    The only users allowed to interact with host console are part of the
    "hostconsole" group.
    
    Note: The changes are spread across multiple repositories listed under
    "Related commits:"
    
    The bmcweb changes to incorporate new group are as follows:
    - The ssh usergroup is only translated to ManagerConsole redfish group
      and hostconsole usergroup is translated to HostConsole redfish group.
    - The following changes are made to check the privileges for host console
      access
        - The new OEM privilege "OpenBMCHostConsole" added for host console
          access. This privilege is not shared externally hence it is not
          documented.
        - Updated obmc_console BMCWEB_ROUTE to use the new privilege.
        - Router functions now save user role and user groups in the session
        - getUserPrivileges() function now takes session reference instead
          of user role. This function now also checks for the user group
          "hostconsole" and add the new privilege if user is member of this
          group.
        - Updated all callers of the getUserPrivileges to pass session
          reference.
        - Added test to validate that new privilege is set correctly.
    
    Tested:
      Loaded code on the system and validated that;
      - New user gets added in hostconsole group. NOTE: Currently all groups
        are assigned to new user.
      - Access from the web gui is only available for users in hostconsole
        group. Used simulator to test this.
      - Checked the role collection and there is no change.
      $ curl -k -H "X-Auth-Token: $TOKEN" -X GET \
             https://${bmc}/redfish/v1/AccountService/Roles
      $ curl -k -H "X-Auth-Token: $TOKEN" -X GET \
             https://${bmc}/redfish/v1/AccountService/Roles/Administrator
      $ curl -k -H "X-Auth-Token: $TOKEN" -X GET \
             https://${bmc}/redfish/v1/AccountService/Roles/ReadOnly
      $ curl -k -H "X-Auth-Token: $TOKEN" -X GET \
             https://${bmc}/redfish/v1/AccountService/Roles/Operator
    
      - HostConsole is in AccountType when hostconsole group is present in
        UserGroups D-Bus property
    
      $ id user99
      uid=1006(user99) gid=100(users) groups=1000(priv-admin),1005(web),\
          1006(redfish),1013(hostconsole),100(users)
    
      $ curl -k https://${bmc}/redfish/v1/AccountService/Accounts/user99
      {
        "@odata.id": "/redfish/v1/AccountService/Accounts/user99",
        "@odata.type": "#ManagerAccount.v1_4_0.ManagerAccount",
        "AccountTypes": [
          "HostConsole",
          "Redfish",
          "WebUI",
          "ManagerConsole"
        ],
        "Description": "User Account",
        "Enabled": true,
        "Id": "user99",
        "Links": {
          "Role": {
            "@odata.id": "/redfish/v1/AccountService/Roles/Administrator"
          }
        },
        "Locked": false,
        "Locked@Redfish.AllowableValues": [
          "false"
        ],
        "Name": "User Account",
        "Password": null,
        "PasswordChangeRequired": false,
        "RoleId": "Administrator",
        "UserName": "user99"
    
    Related commits:
      docs: https://gerrit.openbmc.org/c/openbmc/docs/+/60968
      phosphor-user-manager: https://gerrit.openbmc.org/c/openbmc/phosphor-user-manager/+/61583
      openbmc: https://gerrit.openbmc.org/c/openbmc/openbmc/+/61582
      obmc-console: https://gerrit.openbmc.org/c/openbmc/obmc-console/+/61581
      bmcweb: https://gerrit.openbmc.org/c/openbmc/bmcweb/+/61580
    
    Change-Id: Ia5a33dafc9a76444e6a8e74e752f0f90cb0a31c8
    Signed-off-by: Ninad Palsule <ninadpalsule@us.ibm.com>

--- redfish-core/lib/redfish_sessions.hpp
+++ redfish-core/lib/redfish_sessions.hpp
@@ -105,7 +105,7 @@ inline void
         session->username != req.session->username)
     {
         Privileges effectiveUserPrivileges =
-            redfish::getUserPrivileges(req.userRole);
+            redfish::getUserPrivileges(*req.session);
 
         if (!effectiveUserPrivileges.isSupersetOf({"ConfigureUsers"}))
         {
