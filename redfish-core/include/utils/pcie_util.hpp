0ff5d922af521666446cd7a92bd4303e7ed569d5
Lakshmi Yadlapati <lakshmiy@us.ibm.com>
2023-03-30 14:09:57 -0500

    Add empty PCIe Slots to PCIeDevice Collection
    This commit adds empty PCIe Slots to PCIeDevice Collection
    
    Redfish Commit:
    ```
    https://github.com/DMTF/Redfish/commit/85c01556b648696ab81d10ecfb7416be402ef534
    ```
    
    Tested:
    ```
    curl -k https://$bmc/redfish/v1/Systems/system/PCIeDevices
    {
      "@odata.id": "/redfish/v1/Systems/system/PCIeDevices",
      "@odata.type": "#PCIeDeviceCollection.PCIeDeviceCollection",
      "Description": "Collection of PCIe Devices",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/dp0_drive0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/dp0_drive1"
        },
    ....
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/nvme0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/nvme0"
        },
    .....
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcieslot11"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcieslot12"
        }
      ],
      "Members@odata.count": 56,
      "Name": "PCIe Device Collection"
    }
    
    curl -k https://$bmc/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      "Actions": {
        "#ComputerSystem.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Systems/system/ResetActionInfo",
          "target": "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset"
        }
      },
    ......
      "PCIeDevices": [
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/dp0_drive0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/dp0_drive1"
        },
    ....
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/nvme0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/nvme0"
        },
    .....
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcieslot11"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcieslot12"
        }
      ],
      "Members@odata.count": 56,
    .....
    .....
    }
    
    ```
    
    Change-Id: I00f3ad858d3886fe085c6de4b7b9e8499bcc392f
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>

--- redfish-core/include/utils/pcie_util.hpp
+++ redfish-core/include/utils/pcie_util.hpp
@@ -13,6 +13,122 @@ namespace redfish
 namespace pcie_util
 {
 
+static constexpr char const* inventoryPath = "/xyz/openbmc_project/inventory";
+
+inline bool checkPCIeSlotDevicePath(
+    const std::string& pcieDevicePath,
+    const dbus::utility::MapperGetSubTreePathsResponse& pcieDevicePaths)
+{
+    for (const std::string& devicePath : pcieDevicePaths)
+    {
+        if (pcieDevicePath == devicePath)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+inline void checkPCIeSlotEndpoints(
+    const dbus::utility::MapperEndPoints& endpoints,
+    const dbus::utility::MapperGetSubTreePathsResponse& pcieDevicePaths,
+    const std::string& pcieSlotName, nlohmann::json& pcieDeviceList)
+{
+    bool found = false;
+    /* There may be multiple different objects with the same
+     * association path. Walk through each object and check if the
+     * PCIeSlot contains a PCIe Device.*/
+    for (const auto& endpoint : endpoints)
+    {
+        if (checkPCIeSlotDevicePath(endpoint, pcieDevicePaths))
+        {
+            found = true;
+            break;
+        }
+    }
+    /* If the PCIeSlot doesn't contain any PCIeDevice then add the
+     * PCIeSlot to pcieDevice list */
+    if (!found)
+    {
+        nlohmann::json::object_t pcieDevice;
+        pcieDevice["@odata.id"] = crow::utility::urlFromPieces(
+            "redfish", "v1", "Systems", "system", "PCIeDevices", pcieSlotName);
+        pcieDeviceList.push_back(std::move(pcieDevice));
+    }
+}
+
+/**
+ * @brief Populate the PCIe Slot list from a GetSubTreePaths search of
+ *        inventory
+ *
+ * @param[i,o] aResp  Async response object
+ * @param[i]   Name   Key to store the list of PCIe Slots in aResp
+ *
+ * @return void
+ */
+
+inline void handleGetEmptyPCIeSlots(
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+    const dbus::utility::MapperGetSubTreePathsResponse& pcieDevicePaths,
+    const dbus::utility::MapperGetSubTreePathsResponse& pcieSlotPaths,
+    const std::string& name)
+{
+    nlohmann::json& pcieDeviceList = aResp->res.jsonValue[name];
+    for (const std::string& pcieSlotPath : pcieSlotPaths)
+    {
+        std::string slotName =
+            sdbusplus::message::object_path(pcieSlotPath).filename();
+        if (slotName.empty())
+        {
+            continue;
+        }
+
+        std::string associationPath = pcieSlotPath + "/containing";
+
+        dbus::utility::getAssociationEndPoints(
+            associationPath,
+            [aResp, pcieDevicePaths, slotName,
+             &pcieDeviceList](const boost::system::error_code& ec1,
+                              const dbus::utility::MapperEndPoints& endpoints) {
+            if (ec1)
+            {
+                if (ec1.value() != EBADR)
+                {
+                    BMCWEB_LOG_ERROR << "DBUS response error: "
+                                     << ec1.message();
+                    messages::internalError(aResp->res);
+                    return;
+                }
+            }
+
+            checkPCIeSlotEndpoints(endpoints, pcieDevicePaths, slotName,
+                                   pcieDeviceList);
+            });
+    }
+    aResp->res.jsonValue[name + "@odata.count"] = pcieDeviceList.size();
+}
+
+inline void getEmptyPCIeSlots(
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+    const dbus::utility::MapperGetSubTreePathsResponse& pcieDevicePaths,
+    const std::string& name)
+{
+    constexpr std::array<std::string_view, 1> pcieSlotInterface = {
+        "xyz.openbmc_project.Inventory.Item.PCIeSlot"};
+    dbus::utility::getSubTreePaths(
+        inventoryPath, 0, pcieSlotInterface,
+        [aResp, pcieDevicePaths, name](
+            const boost::system::error_code& ec,
+            const dbus::utility::MapperGetSubTreePathsResponse& pcieSlotPaths) {
+        if (ec)
+        {
+            BMCWEB_LOG_DEBUG << "no PCIe Slot paths found ec: " << ec.message();
+            return;
+        }
+        handleGetEmptyPCIeSlots(aResp, pcieDevicePaths, pcieSlotPaths, name);
+        });
+}
+
 /**
  * @brief Populate the PCIe Device list from a GetSubTreePaths search of
  *        inventory
@@ -27,8 +143,6 @@ inline void
     getPCIeDeviceList(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                       const std::string& name)
 {
-    static constexpr char const* inventoryPath =
-        "/xyz/openbmc_project/inventory";
     constexpr std::array<std::string_view, 1> pcieDeviceInterface = {
         "xyz.openbmc_project.Inventory.Item.PCIeDevice"};
 
@@ -65,6 +179,7 @@ inline void
             pcieDeviceList.push_back(std::move(pcieDevice));
         }
         asyncResp->res.jsonValue[name + "@odata.count"] = pcieDeviceList.size();
+        getEmptyPCIeSlots(asyncResp, pcieDevicePaths, name);
         });
 }
 
