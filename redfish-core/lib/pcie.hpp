154356c930fc6c38d3552770c961338965bd8d2f
Lakshmi Yadlapati <lakshmiy@us.ibm.com>
2023-04-12 17:01:34 -0500

    Add Health information for PCIeDevice
    This commit is to add Health information according to the Redfish
    PCIeDevice schema.
    ref: https://redfish.dmtf.org/schemas/v1/PCIeDevice.v1_9_0.json
    
    Tested: Validator passed
    
    '''
     curl -k https://$bmc/redfish/v1/Systems/system/PCIeDevices/pcie_card8
    {
      "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card8",
      "@odata.type": "#PCIeDevice.v1_9_0.PCIeDevice",
      "Id": "pcie_card8",
      "Manufacturer": "",
      "Model": "6B87",
      "Name": "PCIe Device",
      "PCIeFunctions": {
        "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card8/PCIeFunctions"
      },
      "PCIeInterface": {
        "LanesInUse": -1
      },
      "PartNumber": "03FL194",
      "SerialNumber": "Y131UF09S00J",
      "Slot": {
        "Location": {
          "PartLocation": {
            "ServiceLabel": "U78DB.ND0.WZS0018-P0-C8"
          }
        }
      },
      "SparePartNumber": "03FL195",
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      }
    }
    '''
    
    Change-Id: I53026792d0c223c10065c58aef9f3b9dc04a24ed
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>

--- redfish-core/lib/pcie.hpp
+++ redfish-core/lib/pcie.hpp
@@ -177,6 +177,30 @@ inline std::optional<pcie_device::PCIeTypes>
     return std::nullopt;
 }
 
+inline void getPCIeDeviceHealth(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                                const std::string& pcieDevicePath,
+                                const std::string& service)
+{
+    sdbusplus::asio::getProperty<bool>(
+        *crow::connections::systemBus, service, pcieDevicePath,
+        "xyz.openbmc_project.State.Decorator.OperationalStatus", "Functional",
+        [aResp](const boost::system::error_code ec, const bool value) {
+        if (ec)
+        {
+            if (ec.value() != EBADR)
+            {
+                messages::internalError(aResp->res);
+            }
+            return;
+        }
+
+        if (!value)
+        {
+            aResp->res.jsonValue["Status"]["Health"] = "Critical";
+        }
+        });
+}
+
 inline void getPCIeDeviceState(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
                                const std::string& pcieDevicePath,
                                const std::string& service)
@@ -382,6 +406,7 @@ inline void addPCIeDeviceCommonProperties(
     aResp->res.jsonValue["Name"] = "PCIe Device";
     aResp->res.jsonValue["Id"] = pcieDeviceId;
     aResp->res.jsonValue["Status"]["State"] = "Enabled";
+    aResp->res.jsonValue["Status"]["Health"] = "OK";
 }
 
 inline void handlePCIeDeviceGet(App& app, const crow::Request& req,
@@ -407,6 +432,7 @@ inline void handlePCIeDeviceGet(App& app, const crow::Request& req,
         getPCIeDeviceAsset(aResp, pcieDevicePath, service);
         getPCIeDeviceLocation(aResp, pcieDevicePath, service);
         getPCIeDeviceState(aResp, pcieDevicePath, service);
+        getPCIeDeviceHealth(aResp, pcieDevicePath, service);
         getPCIeDeviceProperties(
             aResp, pcieDevicePath, service,
             [aResp, pcieDeviceId](
