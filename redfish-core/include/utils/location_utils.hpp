1b376d201c6f3a870de6f4076e18df4668c2968c
Rahul Kapoor <rahulkpr@google.com>
2023-05-08 07:36:41 +0000

    location_utils: Implement PartLocationContext
    Implements https://github.com/DMTF/Redfish/pull/5481
    PartLocationContext along with ServiceLabel uniquely identifies part
    in an enclosure where just the ServiceLabel cannot.
    
    PartLocationContext is derived from LocationCode following Chassis
    associations - Chassis to Chassis using '/conatainedby' association and
    Chassis to other objects using '/chassis'.
    
    Example:
    Upstream chassis assocations for a Processor resource:
    cpu0
        <-{/chassis} ChassisC
            <-{/conatainedby} ChassisB
                <-{/conatainedby} ChassisA
    
    Location object based on the associations above:
        "Location": {
        "PartLocationContext": <ChassisB LocationCode> / <ChassisC LocationCode>
        "PartLocation": {
            "LocationType": "Slot"
            "ServiceLabel": "<cpu0 LocationCode>"
        },
    
    Change-Id: I3ad685bbe291710845e933534a3278355dd75b9b
    Signed-off-by: Rahul Kapoor <rahulkpr@google.com>
    Signed-off-by: Willy Tu <wltu@google.com>

--- redfish-core/include/utils/location_utils.hpp
+++ redfish-core/include/utils/location_utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "dbus_singleton.hpp"
+#include "dbus_utility.hpp"
 #include "error_messages.hpp"
 
 #include <async_resp.hpp>
@@ -65,6 +66,170 @@ inline void getLocationCode(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
         asyncResp->res.jsonValue[location]["PartLocation"]["ServiceLabel"] =
             property;
         });
+}
+
+/**
+ * @brief Fill out PartLocationContext of a redfish resource.
+ *
+ * @param[in,out]   asyncResp  Async HTTP response
+ * @param[in]       jsonPtr    Json path of where to put the location context
+ * @param[in]       label      Service label to update the location context with
+ */
+inline void
+    updateLocationContext(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                          const nlohmann::json::json_pointer& jsonPtr,
+                          const std::string& label)
+{
+    BMCWEB_LOG_DEBUG << "Add service label " << label
+                     << " to PartLocationContext";
+    nlohmann::json& propertyJson =
+        asyncResp->res.jsonValue[jsonPtr]["PartLocationContext"];
+    const std::string* val = propertyJson.get_ptr<const std::string*>();
+    if (val && !val->empty())
+    {
+        propertyJson = label + "/" + *val;
+    }
+    else
+    {
+        propertyJson = label;
+    }
+}
+
+template <typename Callback>
+inline void getAssociatedChassisSubtree(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& associationPath, Callback&& callback)
+{
+    constexpr std::array<std::string_view, 2> chassisInterfaces = {
+        "xyz.openbmc_project.Inventory.Item.Board",
+        "xyz.openbmc_project.Inventory.Item.Chassis"};
+
+    sdbusplus::message::object_path association(associationPath);
+    sdbusplus::message::object_path root("/xyz/openbmc_project/inventory");
+    dbus::utility::getAssociatedSubTree(
+        association, root, 0, chassisInterfaces,
+        [asyncResp, associationPath,
+         callback{std::forward<Callback>(callback)}](
+            const boost::system::error_code& ec,
+            const dbus::utility::MapperGetSubTreeResponse& subtree) {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR << "Failed to get chassis associations for "
+                             << associationPath << " ec: " << ec.message();
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        callback(asyncResp, subtree);
+        });
+}
+
+/**
+ * @brief Parses given chassis subtree for object path and connection to retrive
+ * chassis location code used to fill PartLocationContext.
+ *
+ * @param[in]       upstreamChassisPaths   Set of chassis object paths queried.
+ * @param[in]       jsonPtr     Json location to fill PartLocationContext
+ * @param[in,out]   asyncResp   Async HTTP response
+ * @param[in]       subtree     Subtree associated with chassis endpoint
+ */
+inline void getAssociatedChassisSubtreeCallback(
+    std::set<std::string> upstreamChassisPaths,
+    const nlohmann::json::json_pointer& jsonPtr,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const dbus::utility::MapperGetSubTreeResponse& subtree)
+{
+    // Base case as we recurse to get upstream chassis association.
+    if (subtree.empty())
+    {
+        BMCWEB_LOG_DEBUG << "Chassis association not found."
+                            "Must be root chassis or link is broken";
+        return;
+    }
+
+    if (subtree.size() > 1)
+    {
+        BMCWEB_LOG_ERROR << "Found multiple upstream chassis.";
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    auto chassisPathToConnMap = subtree.begin();
+
+    // Upstream chassis.
+    const std::string& chassisPath = chassisPathToConnMap->first;
+    if (!upstreamChassisPaths.insert(chassisPath).second)
+    {
+        BMCWEB_LOG_ERROR << "Loop detected in upstream chassis associations.";
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    const dbus::utility::MapperServiceMap& serviceMap =
+        chassisPathToConnMap->second;
+    if (serviceMap.empty())
+    {
+        BMCWEB_LOG_ERROR << "Associated chassis obj path " << chassisPath
+                         << " has no service mapping.";
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    if (serviceMap.size() > 1)
+    {
+        BMCWEB_LOG_ERROR << "Associated chassis path " << chassisPath
+                         << " found in multiple connections";
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    // Get service name to retrieve upstream chassis location code property.
+    const std::string& service = serviceMap.begin()->first;
+
+    sdbusplus::asio::getProperty<std::string>(
+        *crow::connections::systemBus, service, chassisPath,
+        "xyz.openbmc_project.Inventory.Decorator.LocationCode", "LocationCode",
+        [asyncResp, jsonPtr, chassisPath,
+         upstreamChassisPaths{std::move(upstreamChassisPaths)}](
+            const boost::system::error_code ec2,
+            const std::string& locationCode) {
+        if (ec2)
+        {
+            BMCWEB_LOG_DEBUG << "DBUS response error for Location, ec: "
+                             << ec2.message();
+            return;
+        }
+
+        updateLocationContext(asyncResp, jsonPtr, locationCode);
+
+        getAssociatedChassisSubtree(
+            asyncResp, chassisPath + "/containedby",
+            std::bind_front(getAssociatedChassisSubtreeCallback,
+                            std::move(upstreamChassisPaths), jsonPtr));
+        });
+}
+
+/**
+ * @brief Recursively gets LocationCode for upstream chassis to populate
+ * PartLocationContext.
+ *
+ * @param[in,out]   asyncResp       Async HTTP response
+ * @param[in]       jsonPtr         Json location to fill PartLocationContext
+ * @param[in]       associationPath Path used to associate with upstream chassis
+ */
+inline void
+    getPartLocationContext(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                           const nlohmann::json::json_pointer& jsonPtr,
+                           const std::string& associationPath)
+{
+    BMCWEB_LOG_DEBUG << "Get chassis endpoints associated with "
+                     << associationPath;
+    // Set of chassis object paths used to detect cycle as we resolve usptream
+    // chassis associations to get to root chassis to create PartLocationContext
+    std::set<std::string> upstreamChassisPaths;
+    getAssociatedChassisSubtree(
+        asyncResp, associationPath,
+        std::bind_front(getAssociatedChassisSubtreeCallback,
+                        std::move(upstreamChassisPaths), jsonPtr));
 }
 
 } // namespace location_util
