03cd9033b7b5d92107224e385d4cba12b8ee066e
Ed Tanous <edtanous@google.com>
2023-05-15 11:44:27 -0700

    Clean up preparePayload
    boost::beast::http::message::prepare_payload [1] can throw, which isn't
    really the behavior we want (as it throws to the io_context).  Luckily,
    every part of that function is using public methods, and we can simplify
    it.
    
    In past commits, we've worked around this issue:
    6295becabb9edba2edb53a3c0dddc13d2ffac8dd
    
    This is an attempt to fix it properly.
    
    [1] https://github.com/boostorg/beast/blob/ae01f0201dbf940cbc32d96d7a78dc584a02ab26/include/boost/beast/http/impl/message.hpp#L398
    
    Redfish service validator passes
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ie88ddeecfd226bba75a7659cfb7ddddd38eb27cb

--- http/http_response.hpp
+++ http/http_response.hpp
@@ -143,7 +143,25 @@ struct Response
 
     void preparePayload()
     {
-        stringResponse->prepare_payload();
+        // This code is a throw-free equivalent to
+        // beast::http::message::prepare_payload
+        boost::optional<uint64_t> n = stringResponse->payload_size();
+        using boost::beast::http::status;
+        using boost::beast::http::status_class;
+        using boost::beast::http::to_status_class;
+        if ((!n || *n > 0) &&
+            ((to_status_class(stringResponse->result()) ==
+                  status_class::informational ||
+              stringResponse->result() == status::no_content ||
+              stringResponse->result() == status::not_modified)))
+        {
+            BMCWEB_LOG_CRITICAL
+                << this
+                << " Response content provided but code was no-content or not_modified, which aren't allowed to have a body";
+            n = 0;
+            body().clear();
+        }
+        stringResponse->content_length(*n);
     }
 
     void clear()
