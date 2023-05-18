cf17f2bd995e470a4cd23d874b81ff96d7385eae
Ed Tanous <edtanous@google.com>
2023-05-18 14:05:17 -0700

    Write test capable of catching regressions
    This is a pretty simple test, but should be able to catch the regression
    injected in the previous commit.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I67de097059a6e0dd8d2c02c1aa6c69954a6d7be3

--- test/include/json_html_serializer.cpp
+++ test/include/json_html_serializer.cpp
@@ -37,5 +37,26 @@ TEST(JsonHtmlSerializer, dumpHtmlLink)
         "</body>\n"
         "</html>\n");
 }
+
+TEST(JsonHtmlSerializer, dumpint)
+{
+    std::string out;
+    nlohmann::json j = 42;
+    dumpHtml(out, j);
+    EXPECT_EQ(
+        out,
+        "<html>\n"
+        "<head>\n"
+        "<title>Redfish API</title>\n"
+        "<link href=\"/redfish.css\" rel=\"stylesheet\">\n"
+        "</head>\n"
+        "<body>\n"
+        "<div class=\"container\">\n"
+        "<img src=\"/DMTF_Redfish_logo_2017.svg\" alt=\"redfish\" height=\"406px\" width=\"576px\">\n"
+        "<div class=\"content\">\n42</div>\n"
+        "</div>\n"
+        "</body>\n"
+        "</html>\n");
+}
 } // namespace
 } // namespace json_html_util
