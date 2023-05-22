1e71a0dbd8fa79bae0467c0dc2067f2ebcba8561
Ed Tanous <edtanous@google.com>
2023-05-22 12:04:37 -0700

    Use boost::asio::dispatch instead of post
    Because dispatch is allowed to reuse the stack if it wants to, this
    potentially can increase the performance when operating on a single
    request.  The difference between the two is documented further[1].
    
    All of the bmcweb calls are reentrant, so this should just be a "free"
    performance improvement, although likely unmeasurable, it's always good
    to do the "right" thing.
    
    [1] https://stackoverflow.com/questions/2326588/boost-asio-io-service-dispatch-vs-post
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I3914ad88e7f26273b4af33925fe5b5a627c741d7

--- redfish-core/lib/managers.hpp
+++ redfish-core/lib/managers.hpp
@@ -27,6 +27,7 @@
 #include "utils/systemd_utils.hpp"
 #include "utils/time_utils.hpp"
 
+#include <boost/asio/dispatch.hpp>
 #include <boost/system/error_code.hpp>
 #include <boost/url/format.hpp>
 #include <sdbusplus/asio/property.hpp>
@@ -1302,9 +1303,9 @@ struct GetPIDValues : std::enable_shared_from_this<GetPIDValues>
 
     ~GetPIDValues()
     {
-        boost::asio::post(crow::connections::systemBus->get_io_context(),
-                          std::bind_front(&processingComplete, asyncResp,
-                                          std::move(complete)));
+        boost::asio::dispatch(crow::connections::systemBus->get_io_context(),
+                              std::bind_front(&processingComplete, asyncResp,
+                                              std::move(complete)));
     }
 
     GetPIDValues(const GetPIDValues&) = delete;
