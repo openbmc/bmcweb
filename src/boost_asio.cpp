20abcdaea255e87aeeb4e8e9381d61274dc08b7f
Ed Tanous <edtanous@google.com>
2023-05-30 13:00:57 -0700

    Provide bmcweb-specific bmcweb exception handler
    Providing our own boost exception handler, this has the benefits of:
    1. Not generating the unwind stacks in the binary, which saves about 1%
       on the total compressed binary size.
    2. Allows us to start burning down all the places where we incorrectly
       call boost methods that throw exceptions.  Throwing exceptions to
       master has caused exceptions in the past.
    
    Tested:
    No good way to test exceptional cases.  Code inspection and compilation
    only.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I0037ff72d09fd538543882dff771c3193a293e9f

--- src/boost_asio.cpp
+++ src/boost_asio.cpp
@@ -1,1 +1,22 @@
+#include "logging.hpp"
+
 #include <boost/asio/impl/src.hpp>
+#include <boost/assert/source_location.hpp>
+
+#include <exception>
+
+namespace boost
+{
+void throw_exception(const std::exception& e)
+{
+    BMCWEB_LOG_CRITICAL << "Boost exception thrown " << e.what();
+    std::terminate();
+}
+
+void throw_exception(const std::exception& e, const source_location& loc)
+{
+    BMCWEB_LOG_CRITICAL << "Boost exception thrown " << e.what() << " from "
+                        << loc;
+    std::terminate();
+}
+} // namespace boost
