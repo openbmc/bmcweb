b198af81d6af730a38a7fbf8c5e23c3b7df1cd0c
Ed Tanous <edtanous@google.com>
2023-06-01 16:42:21 -0700

    Move registry code out of event service manager
    This code was added into the registries namespace, from event service
    manager.  It is operating on registries, it belongs in the registry
    header.
    
    Tested: Redfish service validator passes.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I89e830dde185178b1d2e52b18a1a0a2baa4e0fb3

---
+++ redfish-core/include/registries_selector.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "registries/base_message_registry.hpp"
+#include "registries/openbmc_message_registry.hpp"
+#include "registries/task_event_message_registry.hpp"
+
+#include <span>
+#include <string_view>
+
+namespace redfish::registries
+{
+inline std::span<const MessageEntry>
+    getRegistryFromPrefix(std::string_view registryName)
+{
+    if (task_event::header.registryPrefix == registryName)
+    {
+        return {task_event::registry};
+    }
+    if (openbmc::header.registryPrefix == registryName)
+    {
+        return {openbmc::registry};
+    }
+    if (base::header.registryPrefix == registryName)
+    {
+        return {base::registry};
+    }
+    return {openbmc::registry};
+}
+} // namespace redfish::registries
