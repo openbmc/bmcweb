ac106bf6d10841a25088ed14105a73636ba71519
Ed Tanous <edtanous@google.com>
2023-06-07 09:24:59 -0700

    Consistently name AsyncResp variables
    In about half of our code, AsyncResp objects take the name asyncResp,
    and in the other half they take the name aResp.  While the difference
    between them is negligeble and arbitrary, having two naming conventions
    makes it more difficult to do automated changes over time via grep.
    
    This commit was generated automtatically with the command:
    git grep -l 'aResp' | xargs sed -i 's|aResp|asyncResp|g'
    
    Tested: Code compiles.
    
    Change-Id: Id363437b6a78f51e91cbf60aa0a0c2286f36a037
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- redfish-core/lib/chassis.hpp
+++ redfish-core/lib/chassis.hpp
@@ -42,19 +42,19 @@ namespace redfish
 /**
  * @brief Retrieves chassis state properties over dbus
  *
- * @param[in] aResp - Shared pointer for completing asynchronous calls.
+ * @param[in] asyncResp - Shared pointer for completing asynchronous calls.
  *
  * @return None.
  */
-inline void getChassisState(std::shared_ptr<bmcweb::AsyncResp> aResp)
+inline void getChassisState(std::shared_ptr<bmcweb::AsyncResp> asyncResp)
 {
     // crow::connections::systemBus->async_method_call(
     sdbusplus::asio::getProperty<std::string>(
         *crow::connections::systemBus, "xyz.openbmc_project.State.Chassis",
         "/xyz/openbmc_project/state/chassis0",
         "xyz.openbmc_project.State.Chassis", "CurrentPowerState",
-        [aResp{std::move(aResp)}](const boost::system::error_code& ec,
-                                  const std::string& chassisState) {
+        [asyncResp{std::move(asyncResp)}](const boost::system::error_code& ec,
+                                          const std::string& chassisState) {
         if (ec)
         {
             if (ec == boost::system::errc::host_unreachable)
@@ -64,8 +64,8 @@ inline void getChassisState(std::shared_ptr<bmcweb::AsyncResp> aResp)
                 BMCWEB_LOG_DEBUG << "Service not available " << ec;
                 return;
             }
-            BMCWEB_LOG_ERROR << "DBUS response error " << ec;
-            messages::internalError(aResp->res);
+            BMCWEB_LOG_DEBUG << "DBUS response error " << ec;
+            messages::internalError(asyncResp->res);
             return;
         }
 
@@ -73,19 +73,19 @@ inline void getChassisState(std::shared_ptr<bmcweb::AsyncResp> aResp)
         // Verify Chassis State
         if (chassisState == "xyz.openbmc_project.State.Chassis.PowerState.On")
         {
-            aResp->res.jsonValue["PowerState"] = "On";
-            aResp->res.jsonValue["Status"]["State"] = "Enabled";
+            asyncResp->res.jsonValue["PowerState"] = "On";
+            asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
         }
         else if (chassisState ==
                  "xyz.openbmc_project.State.Chassis.PowerState.Off")
         {
-            aResp->res.jsonValue["PowerState"] = "Off";
-            aResp->res.jsonValue["Status"]["State"] = "StandbyOffline";
+            asyncResp->res.jsonValue["PowerState"] = "Off";
+            asyncResp->res.jsonValue["Status"]["State"] = "StandbyOffline";
         }
         });
 }
 
-inline void getIntrusionByService(std::shared_ptr<bmcweb::AsyncResp> aResp,
+inline void getIntrusionByService(std::shared_ptr<bmcweb::AsyncResp> asyncResp,
                                   const std::string& service,
                                   const std::string& objPath)
 {
@@ -94,8 +94,8 @@ inline void getIntrusionByService(std::shared_ptr<bmcweb::AsyncResp> aResp,
     sdbusplus::asio::getProperty<std::string>(
         *crow::connections::systemBus, service, objPath,
         "xyz.openbmc_project.Chassis.Intrusion", "Status",
-        [aResp{std::move(aResp)}](const boost::system::error_code& ec,
-                                  const std::string& value) {
+        [asyncResp{std::move(asyncResp)}](const boost::system::error_code& ec,
+                                          const std::string& value) {
         if (ec)
         {
             // do not add err msg in redfish response, because this is not
@@ -104,21 +104,23 @@ inline void getIntrusionByService(std::shared_ptr<bmcweb::AsyncResp> aResp,
             return;
         }
 
-        aResp->res.jsonValue["PhysicalSecurity"]["IntrusionSensorNumber"] = 1;
-        aResp->res.jsonValue["PhysicalSecurity"]["IntrusionSensor"] = value;
+        asyncResp->res.jsonValue["PhysicalSecurity"]["IntrusionSensorNumber"] =
+            1;
+        asyncResp->res.jsonValue["PhysicalSecurity"]["IntrusionSensor"] = value;
         });
 }
 
 /**
  * Retrieves physical security properties over dbus
  */
-inline void getPhysicalSecurityData(std::shared_ptr<bmcweb::AsyncResp> aResp)
+inline void
+    getPhysicalSecurityData(std::shared_ptr<bmcweb::AsyncResp> asyncResp)
 {
     constexpr std::array<std::string_view, 1> interfaces = {
         "xyz.openbmc_project.Chassis.Intrusion"};
     dbus::utility::getSubTree(
         "/xyz/openbmc_project/Intrusion", 1, interfaces,
-        [aResp{std::move(aResp)}](
+        [asyncResp{std::move(asyncResp)}](
             const boost::system::error_code& ec,
             const dbus::utility::MapperGetSubTreeResponse& subtree) {
         if (ec)
@@ -134,7 +136,7 @@ inline void getPhysicalSecurityData(std::shared_ptr<bmcweb::AsyncResp> aResp)
             if (!object.second.empty())
             {
                 const auto service = object.second.front();
-                getIntrusionByService(aResp, service.first, object.first);
+                getIntrusionByService(asyncResp, service.first, object.first);
                 return;
             }
         }
