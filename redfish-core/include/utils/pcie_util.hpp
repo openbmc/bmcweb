8d95d3e2a2f8f62405119164149106f86d3bd26f
Lakshmi Yadlapati <lakshmiy@us.ibm.com>
2023-06-08 04:37:18 -0500

    Refactor PCIeDeviceList to use getCollectionMembers
    This commit refactors the code in the PCIeDeviceList function to use the
    getCollectionMembers function for retrieving collection members.
    Additionally, a new function getCollectionToKey() is added to
    handle the retrieval of collection members with custom key name.
    
    Tested: Validator passed
    
    '''
    Test1: Redfish query of PCI devices on a system that does not have
    any PCIe devices
    
    curl -k https://$bmc/redfish/v1/Systems/system/PCIeDevices
    {
      "@odata.id": "/redfish/v1/Systems/system/PCIeDevices",
      "@odata.type": "#PCIeDeviceCollection.PCIeDeviceCollection",
      "Description": "Collection of PCIe Devices",
      "Members": [],
      "Members@odata.count": 0,
      "Name": "PCIe Device Collection"
    }
    
    Test2: Redfish query of PCIe devices on a system that has PCIe devices
    
    curl -k https://$bmc/redfish/v1/Systems/system/PCIeDevices
    {
      "@odata.id": "/redfish/v1/Systems/system/PCIeDevices",
      "@odata.type": "#PCIeDeviceCollection.PCIeDeviceCollection",
      "Description": "Collection of PCIe Devices",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/drive0"
        },
    .......
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card2"
        },
    .......
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card12"
        }
      ],
      "Members@odata.count": 22,
      "Name": "PCIe Device Collection"
    }
    
    Test3: Redfish query of system with PCIe devices
    curl -k https://$bmc/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      "Actions": {
        "#ComputerSystem.Reset": {
          "@Redfish.ActionInfo": "/redfish/v1/Systems/system/ResetActionInfo",
          "target": "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset"
        }
      },
    ......
      "PCIeDevices": [
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/drive0"
        },
    .......
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card1"
        },
    ....
      ],
      "PCIeDevices@odata.count": 22,
      "PartNumber": "",
    ....
      "SubModel": "S0",
      "SystemType": "Physical"
    }
    '''
    
    Change-Id: Icb38945a2c7bc5219ff3917fbbc8a9986c9c6155
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>

--- redfish-core/include/utils/pcie_util.hpp
+++ redfish-core/include/utils/pcie_util.hpp
@@ -6,6 +6,7 @@
 #include "generated/enums/pcie_device.hpp"
 #include "generated/enums/pcie_slots.hpp"
 #include "http/utility.hpp"
+#include "utils/collection.hpp"
 
 #include <boost/system/error_code.hpp>
 #include <boost/url/format.hpp>
@@ -34,44 +35,16 @@ namespace pcie_util
 
 inline void
     getPCIeDeviceList(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-                      const std::string& name)
+                      const nlohmann::json::json_pointer& jsonKeyName)
 {
     static constexpr std::array<std::string_view, 1> pcieDeviceInterface = {
         "xyz.openbmc_project.Inventory.Item.PCIeDevice"};
+    static const boost::urls::url pcieDeviceUrl =
+        boost::urls::url("/redfish/v1/Systems/system/PCIeDevices");
 
-    dbus::utility::getSubTreePaths(
-        "/xyz/openbmc_project/inventory", 0, pcieDeviceInterface,
-        [asyncResp, name](const boost::system::error_code& ec,
-                          const dbus::utility::MapperGetSubTreePathsResponse&
-                              pcieDevicePaths) {
-        if (ec)
-        {
-            BMCWEB_LOG_DEBUG("no PCIe device paths found ec: {}", ec.message());
-            // Not an error, system just doesn't have PCIe info
-            return;
-        }
-        nlohmann::json& pcieDeviceList = asyncResp->res.jsonValue[name];
-        pcieDeviceList = nlohmann::json::array();
-        for (const std::string& pcieDevicePath : pcieDevicePaths)
-        {
-            size_t devStart = pcieDevicePath.rfind('/');
-            if (devStart == std::string::npos)
-            {
-                continue;
-            }
-
-            std::string devName = pcieDevicePath.substr(devStart + 1);
-            if (devName.empty())
-            {
-                continue;
-            }
-            nlohmann::json::object_t pcieDevice;
-            pcieDevice["@odata.id"] = boost::urls::format(
-                "/redfish/v1/Systems/system/PCIeDevices/{}", devName);
-            pcieDeviceList.emplace_back(std::move(pcieDevice));
-        }
-        asyncResp->res.jsonValue[name + "@odata.count"] = pcieDeviceList.size();
-    });
+    collection_util::getCollectionToKey(
+        asyncResp, pcieDeviceUrl, pcieDeviceInterface,
+        "/xyz/openbmc_project/inventory", jsonKeyName);
 }
 
 inline std::optional<pcie_slots::SlotTypes>
