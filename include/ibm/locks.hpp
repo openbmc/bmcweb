32cee5ccbc09e23144b36bfbf28ab63f391b2b3f
Ravi Teja <raviteja28031990@gmail.com>
2023-06-13 11:55:02 +0000

    Fix clang-tidy error for ibm/locks
    ibm/locks that has had clang-tidy warnings disabled for a while due to
    multiple safety and endianness issues.
    https://github.com/openbmc/bmcweb/commit/26b3630b181d1093483e4d5ebe6aeeb91a6a2976
    
    this commit uncomments locks code which causes warnings and fixes these warnings
    
    Tested by: create & list locks with redfish curl and management console
    
    Change-Id: I569e0dc5c11f259b7cca4b22722c9b4d87c68c80
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- include/ibm/locks.hpp
+++ include/ibm/locks.hpp
@@ -38,6 +38,7 @@ using RcAcquireLock = std::pair<bool, std::variant<Rc, std::pair<bool, int>>>;
 using RcReleaseLockApi = std::pair<bool, std::variant<bool, RcRelaseLock>>;
 using SessionFlags = std::pair<SType, SType>;
 using ListOfSessionIds = std::vector<std::string>;
+static constexpr uint8_t lockResourceIdSizeInBytes = 8;
 
 class Lock
 {
@@ -187,7 +188,7 @@ class Lock
 
 inline RcGetLockList Lock::getLockList(const ListOfSessionIds& listSessionId)
 {
-    std::vector<std::pair<uint32_t, LockRequests>> lockList;
+    std::vector<std::pair<uint32_t, LockRequests>> lockList{};
 
     if (!lockTable.empty())
     {
@@ -513,30 +514,27 @@ inline bool Lock::isConflictRequest(const LockRequests& refLockRequestStructure)
 // If all the elements in the lock requests which are subjected for comparison
 // are same, then the last comparison would be to check for the respective
 // bytes in the resourceid based on the segment length.
-
-inline bool Lock::checkByte(uint64_t /*resourceId1*/, uint64_t /*resourceId2*/,
-                            uint32_t /*position*/)
+inline bool Lock::checkByte(uint64_t resourceId1, uint64_t resourceId2,
+                            uint32_t position)
 {
-    BMCWEB_LOG_ERROR
-        << "This code is disabled due to clang-tidy issues that prevent CI "
-           "from passing.";
-    std::terminate();
 
-#if 0
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-    uint8_t* p = reinterpret_cast<uint8_t*>(&resourceId1);
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-    uint8_t* q = reinterpret_cast<uint8_t*>(&resourceId2);
+    BMCWEB_LOG_ERROR << "checkByte";
+    std::array<uint8_t, 8> res1;
+    std::array<uint8_t, 8> res2;
 
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    uint8_t pPosition = p[position];
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    uint8_t qPosition = q[position];
+    std::memcpy(res1.data(), &resourceId1, sizeof(resourceId1));
+    std::memcpy(res2.data(), &resourceId2, sizeof(resourceId2));
+    BMCWEB_LOG_ERROR << "comparing resource ids";
 
-    BMCWEB_LOG_DEBUG << "Comparing bytes " << std::to_string(pPosition) << ","
-                     << std::to_string(qPosition);
+    uint8_t pPosition = 0;
+    uint8_t qPosition = 0;
+    if (position < lockResourceIdSizeInBytes)
+    {
+        pPosition = res1[position];
+        qPosition = res2[position];
+    }
+
     return pPosition == qPosition;
-#endif
 }
 
 inline bool Lock::isConflictRecord(const LockRequest& refLockRecord1,
