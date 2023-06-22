e684b6013fa7bb55f808d69e11717bcb7d63ef96
Ed Tanous <edtanous@google.com>
2023-06-22 14:06:22 -0700

    Break out request completion from connection
    There's a large part of the http::Connection class that has nothing to
    do with the connection at all, and is all about parsing, and finalizing
    the response.  Break that portion out into its own method that can (in
    the future) be unit tested.
    
    Tested: WIP
    
    Change-Id: Ic608d432e69e25c0e0a1555ecc24ed62adba2664
    Signed-off-by: Ed Tanous <edtanous@google.com>

---
+++ http/complete_response_fields.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "http_request.hpp"
+#include "http_response.hpp"
+#include "json_html_serializer.hpp"
+#include "logging.hpp"
+#include "security_headers.hpp"
+#include "utils/hex_utils.hpp"
+
+#include <boost/beast/http/message.hpp>
+#include <boost/beast/http/string_body.hpp>
+#include <nlohmann/json.hpp>
+
+namespace crow
+{
+
+inline void completeResponseFields(const Request& req, Response& res)
+{
+    BMCWEB_LOG_INFO << "Response: " << ' ' << req.url().encoded_path() << ' '
+                    << res.resultInt();
+    addSecurityHeaders(req, res);
+
+    authentication::cleanupTempSession(req);
+
+    res.setHashAndHandleNotModified();
+
+    if (res.body().empty() && res.jsonValue.is_structured())
+    {
+        using http_helpers::ContentType;
+        std::array<ContentType, 3> allowed{ContentType::CBOR, ContentType::JSON,
+                                           ContentType::HTML};
+        ContentType prefered =
+            getPreferedContentType(req.getHeaderValue("Accept"), allowed);
+
+        if (prefered == ContentType::HTML)
+        {
+            json_html_util::prettyPrintJson(res);
+        }
+        else if (prefered == ContentType::CBOR)
+        {
+            res.addHeader(boost::beast::http::field::content_type,
+                          "application/cbor");
+            nlohmann::json::to_cbor(res.jsonValue, res.body());
+        }
+        else
+        {
+            // Technically prefered could also be NoMatch here, but we'd
+            // like to default to something rather than return 400 for
+            // backward compatibility.
+            res.addHeader(boost::beast::http::field::content_type,
+                          "application/json");
+            res.body() = res.jsonValue.dump(
+                2, ' ', true, nlohmann::json::error_handler_t::replace);
+        }
+    }
+}
+} // namespace crow
