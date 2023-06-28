d098565fb0ac734f8d3cdca3c7d217c629649cd5
Ed Tanous <edtanous@google.com>
2023-06-28 11:28:38 -0700

    Use openssl random number generator
    We already have a generator class.  We should use it.  Wrap this into a
    function that can be unit tested, and add unit tests.
    
    Note, some files also needed to change name, because random.hpp
    conflicts with the built in random, and causes circular build problems.
    This commit changes it to ossl_random.
    
    Tested: Unit tests pass.  Now has coverage.
    
    Change-Id: I5f8eee1af5f4843a352c6fd0e26d67fd3320ef53
    Signed-off-by: Ed Tanous <edtanous@google.com>

---
+++ src/ossl_random.cpp
@@ -0,0 +1,13 @@
+#include "ossl_random.hpp"
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+std::string bmcweb::getRandomUUID()
+{
+    using bmcweb::OpenSSLGenerator;
+    OpenSSLGenerator ossl;
+    return boost::uuids::to_string(
+        boost::uuids::basic_random_generator<OpenSSLGenerator>(ossl)());
+}
