43439d9ff5a047bb1ec0d45861c8fcb1e7be8516
Abhilash Raju <abhilash.kollam@gmail.com>
2023-07-17 08:53:11 -0500

    Reponse: Add file body response for file uploads
    Bmcweb has limited support for handling large size response data
    The support for large size data is often required for usecases
    such as dump offloading ,where data wil be in hundreds of megebytes
    The usage of stringbody for such use cases are not possible becuase
    we should load all data in to memory.As beast comes with builtin
    file_body abstraction to handle such large data transfer, we can
    enhance the  current response structure to accomodate file body
    response type.
    
    Change-Id: Icbf8585b5b04c3ac5120d7b334c13d89ed3eb4aa
    Signed-off-by: Abhilash Raju <abhilash.kollam@gmail.com>

---
+++ test/http/http_connection_test.cpp
@@ -0,0 +1,110 @@
+#include "app.hpp"
+#include "boost/beast/core/flat_buffer.hpp"
+#include "boost/beast/http/serializer.hpp"
+#include "http/http_connection.hpp"
+#include "http/http_response.hpp"
+#include "simple_client.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <thread>
+
+#include "gtest/gtest.h"
+using namespace crow;
+using namespace boost::beast::http;
+using namespace boost::asio;
+using namespace boost::beast;
+namespace
+{
+struct Router
+{
+    std::string root = "/tmp/";
+    void validate() {}
+    void handleUpgrade(
+        const Request& req, const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+        boost::beast::ssl_stream<boost::asio::ip::tcp::socket>&& adaptor)
+    {}
+    void handle(Request& req,
+                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+    {
+        asyncResp->res.addHeader("myheader", "myvalue");
+        if (req.target() == "/hello")
+        {
+            asyncResp->res.body() = "Hello World";
+            return;
+        }
+        auto paths = crow::utility::split(req.target(), '/');
+
+        boost::beast::http::file_body::value_type body;
+        boost::beast::error_code ec{};
+        auto filetofetch = std::filesystem::path(root).c_str() + paths.back();
+        asyncResp->res.openFile(filetofetch);
+    }
+};
+struct Server
+{
+    using App = crow::App<Router>;
+    std::thread serverThread;
+    std::shared_ptr<boost::asio::io_context> io{
+        std::make_shared<boost::asio::io_context>()};
+    App app{io};
+
+    Server()
+    {
+        serverThread = std::thread([this]() {
+            app.port(8082);
+            app.run();
+            io->run();
+        });
+    }
+    void exit()
+    {
+        io->stop();
+        serverThread.join();
+    }
+};
+class ConnectionTest : public ::testing::Test
+{
+    Server server;
+    std::string_view path = "/tmp/temp.txt";
+
+  protected:
+    virtual void SetUp()
+    {
+        std::string_view s = "sample text from file";
+        std::ofstream file;
+        file.open(path.data());
+        file << s;
+        file.close();
+    }
+
+    virtual void TearDown()
+    {
+        std::filesystem::remove(path);
+        server.exit();
+    }
+};
+
+TEST_F(ConnectionTest, GetRootStringBody)
+{
+    SimpleClient client("127.0.0.1", "8082");
+    auto res = client.get("/hello");
+    pritnFileds(res);
+    EXPECT_EQ(res.body(), "Hello World");
+    EXPECT_EQ(res["myheader"], "myvalue");
+
+    EXPECT_EQ(atoi(res["Content-Length"].data()),
+              std::string_view("Hello World").length());
+}
+
+TEST_F(ConnectionTest, GetRootFileBody)
+{
+    SimpleClient client("127.0.0.1", "8082");
+    auto res = client.get("/temp.txt");
+    EXPECT_EQ(res.body(), "sample text from file");
+    EXPECT_EQ(res["myheader"], "myvalue");
+    EXPECT_EQ(atoi(res["Content-Length"].data()),
+              std::string_view("sample text from file").length());
+}
+
+} // Namespace
