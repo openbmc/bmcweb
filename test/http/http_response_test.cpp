e412d86c84716545fa3239c5a11bdfb557b19e2d
Abhilash Raju <abhilash.kollam@gmail.com>
2023-07-17 08:53:11 -0500

    Reponse: Add file body response for file uploads
    Bmcweb has limited support for handling large size response data
    The support for large size data is often required for usecases
    such as dump offloading ,where data wil be in hundreds of megebytes
    The usage of stringbody for such use cases are not possible becuase
    we should load all data in to memory.As beast comes with builtin
    file_body abstraction to handle such large data transfer, we can
    enhance the  current response structure to accomodate file body
    response type.
    
    Change-Id: Icbf8585b5b04c3ac5120d7b334c13d89ed3eb4aa
    Signed-off-by: Abhilash Raju <abhilash.kollam@gmail.com>

---
+++ test/http/http_response_test.cpp
@@ -0,0 +1,143 @@
+#include "boost/beast/core/flat_buffer.hpp"
+#include "boost/beast/http/serializer.hpp"
+#include "http/http_connection.hpp"
+#include "http/http_response.hpp"
+
+#include <filesystem>
+#include <fstream>
+
+#include "gtest/gtest.h"
+using namespace crow;
+using namespace boost::beast::http;
+using namespace boost::asio;
+using namespace boost::beast;
+static void addHeaders(Response& res)
+{
+    res.addHeader("myheader", "myvalue");
+    res.keepAlive(true);
+    res.result(boost::beast::http::status::ok);
+}
+static void varifyHeaders(Response& res)
+{
+    EXPECT_EQ(res.getHeaderValue("myheader"), "myvalue");
+    EXPECT_EQ(res.keepAlive(), true);
+    EXPECT_EQ(res.result(), boost::beast::http::status::ok);
+}
+
+template <class Serializer>
+struct lambda
+{
+    Serializer& sr;
+    mutable boost::beast::flat_buffer buffer;
+    lambda(Serializer& sr_) : sr(sr_) {}
+
+    template <class ConstBufferSequence>
+    void operator()(error_code& ec, const ConstBufferSequence& buffers) const
+    {
+        ec = {};
+        buffer.commit(
+            net::buffer_copy(buffer.prepare(buffer_bytes(buffers)), buffers));
+        sr.consume(buffer_bytes(buffers));
+    }
+};
+template <class Serializer>
+lambda(Serializer&) -> lambda<Serializer>;
+
+template <bool isRequest, class Body, class Fields>
+auto writeMessage(message<isRequest, Body, Fields>& m, error_code& ec)
+{
+    serializer<isRequest, Body, Fields> sr{m};
+    sr.split(true);
+    auto body = lambda(sr);
+    auto header = lambda(sr);
+    do
+    {
+        sr.next(ec, header);
+    } while (!sr.is_header_done());
+    if (!ec && !sr.is_done())
+    {
+        do
+        {
+            sr.next(ec, body);
+        } while (!ec && !sr.is_done());
+    }
+    return body;
+}
+
+TEST(http_response, Defaults)
+{
+    Response res;
+    EXPECT_EQ(std::holds_alternative<Response::string_body_response_type>(
+                  res.genericResponse.value()),
+              true);
+}
+TEST(http_response, headers)
+{
+    Response res;
+    addHeaders(res);
+    varifyHeaders(res);
+}
+TEST(http_response, stringbody)
+{
+    Response res;
+    addHeaders(res);
+    auto& body = res.body();
+    std::string_view bodyvalue = "this is my new body";
+    body += bodyvalue;
+    EXPECT_EQ(res.body(), bodyvalue);
+    varifyHeaders(res);
+}
+TEST(http_response, filebody)
+{
+    Response res;
+    addHeaders(res);
+    std::ofstream file;
+    std::string_view s = "sample text";
+    std::string_view path = "/tmp/temp.txt";
+    file.open(path.data());
+    file << s;
+    file.close();
+
+    res.openFile(path);
+
+    error_code ec{};
+    Response::file_body_response_type& body_resp =
+        std::get<Response::file_body_response_type>(*res.genericResponse);
+
+    lambda visit = writeMessage(body_resp, ec);
+
+    // EXPECT_EQ(ec, 0);
+    const auto b = buffers_front(visit.buffer.data());
+    std::string_view s1{reinterpret_cast<const char*>(b.data()), b.size()};
+    EXPECT_EQ(s1, s);
+    std::filesystem::remove(path);
+
+    varifyHeaders(res);
+}
+TEST(http_response, body_transitions)
+{
+    Response res;
+    addHeaders(res);
+    std::ofstream file;
+    std::string_view s = "sample text";
+    std::string_view path = "/tmp/temp.txt";
+    file.open(path.data());
+    file << s;
+    file.close();
+
+    res.openFile(path);
+
+    EXPECT_EQ(std::holds_alternative<Response::file_body_response_type>(
+                  res.genericResponse.value()),
+              true);
+
+    varifyHeaders(res);
+    res.body() += "body text";
+
+    EXPECT_EQ(std::holds_alternative<Response::string_body_response_type>(
+                  res.genericResponse.value()),
+              true);
+
+    varifyHeaders(res);
+    std::filesystem::remove(path);
+}
