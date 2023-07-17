5c65b80ee623d0bc2b7819bbc3e4193bf3c8e089
Abhilash Raju <abhilash.kollam@gmail.com>
2023-07-17 08:53:11 -0500

    Reponse: Fix incomplete implementation in write
    Write function in http_response.hpp missing implementation for first
    write after changing from filebody.
    Usecase:
    Current resonse type is filebody. Developer tries to change the body
    type to stringbody by calling write function.
    Observed:
    The write fails to update the body type.
    Expected:
    Write should succeed and body should change to string body.
    
    Tested:
    unit test has been added for crow::Response. Manual sanity test
    done for file offloads using curl.
    
    Change-Id: Icbf8585b5b04c3ac5120d7b334c13d89ed3eb4aa
    Signed-off-by: Abhilash Raju <abhilash.kollam@gmail.com>

---
+++ test/http/http_response_test.cpp
@@ -0,0 +1,95 @@
+#include "boost/beast/core/flat_buffer.hpp"
+#include "boost/beast/http/serializer.hpp"
+#include "http/http_response.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <thread>
+
+#include "gtest/gtest.h"
+namespace
+{
+static void addHeaders(crow::Response& res)
+{
+    res.addHeader("myheader", "myvalue");
+    res.keepAlive(true);
+    res.result(boost::beast::http::status::ok);
+}
+static void verifyHeaders(crow::Response& res)
+{
+    EXPECT_EQ(res.getHeaderValue("myheader"), "myvalue");
+    EXPECT_EQ(res.keepAlive(), true);
+    EXPECT_EQ(res.result(), boost::beast::http::status::ok);
+}
+static std::string makePath(std::thread::id threadID)
+{
+    std::stringstream stream;
+    stream << "/tmp/" << threadID << ".txt";
+    return stream.str();
+}
+static void makeFile(const std::string& path)
+{
+    std::ofstream file;
+    std::string_view s = "sample text";
+    file.open(path);
+    file << s;
+    file.close();
+}
+TEST(HttpResponse, Defaults)
+{
+    crow::Response res;
+    EXPECT_EQ(
+        boost::variant2::holds_alternative<crow::Response::string_response>(
+            res.response),
+        true);
+}
+TEST(HttpResponse, Headers)
+{
+    crow::Response res;
+    addHeaders(res);
+    verifyHeaders(res);
+}
+TEST(HttpResponse, StringBody)
+{
+    crow::Response res;
+    addHeaders(res);
+    std::string_view bodyvalue = "this is my new body";
+    res.write({bodyvalue.data(), bodyvalue.length()});
+    EXPECT_EQ(*res.body(), bodyvalue);
+    verifyHeaders(res);
+}
+TEST(HttpResponse, FileBody)
+{
+    crow::Response res;
+    addHeaders(res);
+    std::string path = makePath(std::this_thread::get_id());
+    makeFile(path);
+    res.openFile(path);
+
+    verifyHeaders(res);
+    std::filesystem::remove(path);
+}
+TEST(HttpResponse, BodyTransitions)
+{
+    crow::Response res;
+    addHeaders(res);
+    std::string path = makePath(std::this_thread::get_id());
+    makeFile(path);
+    res.openFile(path);
+
+    EXPECT_EQ(boost::variant2::holds_alternative<crow::Response::file_response>(
+                  res.response),
+              true);
+
+    verifyHeaders(res);
+    res.write("body text");
+
+    EXPECT_EQ(
+        boost::variant2::holds_alternative<crow::Response::string_response>(
+            res.response),
+        true);
+
+    verifyHeaders(res);
+    std::filesystem::remove(path);
+}
+} // namespace
