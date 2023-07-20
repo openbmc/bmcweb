625222aca8b622a42c8aa1039aa8ec77ee540f81
George Liu <liuxiwei@inspur.com>
2023-07-20 09:52:40 +0800

    Implement SubProcessors for processor collection
    The SubProcessors is a collection under the processor collection
    schema. The association objects, (containing, contained_by), are
    used to link the processor.
    
    The association between processor and core have been documented in
    phosphor-dbus-interfaces [1]
    
    [1] https://github.com/openbmc/phosphor-dbus-interfaces/commit/8c79b1dc0270d01c0b713a345c8ec39533c542e4
    
    Tested:
    
    - GET cpu and cpu subprocessors
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/Processors/cpu0
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0",
      "@odata.type": "#Processor.v1_18_0.Processor",
      "Id": "cpu0",
     ...
      "SubProcessors": {
        "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors"
      },
      ...
    }
    ```
    
    ```
    curl -k -X GET https://${bmc}/redfish/v1/Systems/system/Processors/cpu0/SubProcessors
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors",
      "@odata.type": "#ProcessorCollection.ProcessorCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/core0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/core1"
        },
        ...
      ],
      "Members@odata.count": 4,
      "Name": "SubProcessor Collection"
    }
    ```
    
    - Verified that below return a link header
    - GET /redfish/v1/Systems/system/Processors/cpu0/SubProcessors
    - HEAD /redfish/v1/Systems/system/Processors/cpu0/SubProcessors
    - Redfish Validator Passed
    
    Change-Id: If155b97b0c782d82541c00ecf5ee70cb0180f71f
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>
    Signed-off-by: Nikhil Namjoshi <nikhilnamjoshi@google.com>
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/include/utils/processor_utils.hpp
+++ redfish-core/include/utils/processor_utils.hpp
@@ -13,4 +13,8 @@ constexpr std::array<std::string_view, 2> processorInterfaces = {
     "xyz.openbmc_project.Inventory.Item.Cpu",
     "xyz.openbmc_project.Inventory.Item.Accelerator"};
 
+// Interfaces which imply a D-Bus object represents a Processor Core
+constexpr std::array<std::string_view, 1> processorCoreInterfaces = {
+    "xyz.openbmc_project.Inventory.Item.CpuCore"};
+
 } // namespace redfish
