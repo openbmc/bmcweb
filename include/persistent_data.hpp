70ffb9b5bb399ea07f1f39f6e359bc2d33509da4
Ed Tanous <edtanous@google.com>
2023-08-02 12:56:52 -0700

    WIP: Move UserSubscription to composition
    This allows for two very important simplifying changes.  First, we can
    use the default copy operators on the UserSubscription class, which is
    far less error prone than writing it manually, which we have two copies
    of in code already.
    
    Second, it allows the Subscription class to move to using values rather
    than shared_ptr everywhere, which cleans up a significant amount of
    code.
    
    Change-Id: I6d258cfe3594edddf3960ae2d4559d70acca1bf8
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- include/persistent_data.hpp
+++ include/persistent_data.hpp
@@ -141,10 +141,10 @@ class ConfigFile
                     {
                         for (const auto& elem : item.value())
                         {
-                            std::shared_ptr<UserSubscription> newSubscription =
+                            std::optional<UserSubscription> newSubscription =
                                 UserSubscription::fromJson(elem);
 
-                            if (newSubscription == nullptr)
+                            if (!newSubscription)
                             {
                                 BMCWEB_LOG_ERROR("Problem reading subscription "
                                                  "from persistent store");
@@ -156,7 +156,7 @@ class ConfigFile
                                              newSubscription->customText);
                             EventServiceStore::getInstance()
                                 .subscriptionsConfigMap.emplace(
-                                    newSubscription->id, newSubscription);
+                                    newSubscription->id, *newSubscription);
                         }
                     }
                     else
@@ -247,15 +247,15 @@ class ConfigFile
         for (const auto& it :
              EventServiceStore::getInstance().subscriptionsConfigMap)
         {
-            std::shared_ptr<UserSubscription> subValue = it.second;
-            if (subValue->subscriptionType == "SSE")
+            const UserSubscription& subValue = it.second;
+            if (subValue.subscriptionType == "SSE")
             {
                 BMCWEB_LOG_DEBUG("The subscription type is SSE, so skipping.");
                 continue;
             }
             nlohmann::json::object_t headers;
             for (const boost::beast::http::fields::value_type& header :
-                 subValue->httpHeaders)
+                 subValue.httpHeaders)
             {
                 // Note, these are technically copies because nlohmann doesn't
                 // support key lookup by std::string_view.  At least the
@@ -267,19 +267,19 @@ class ConfigFile
 
             nlohmann::json::object_t subscription;
 
-            subscription["Id"] = subValue->id;
-            subscription["Context"] = subValue->customText;
-            subscription["DeliveryRetryPolicy"] = subValue->retryPolicy;
-            subscription["Destination"] = subValue->destinationUrl;
-            subscription["EventFormatType"] = subValue->eventFormatType;
+            subscription["Id"] = subValue.id;
+            subscription["Context"] = subValue.customText;
+            subscription["DeliveryRetryPolicy"] = subValue.retryPolicy;
+            subscription["Destination"] = subValue.destinationUrl;
+            subscription["EventFormatType"] = subValue.eventFormatType;
             subscription["HttpHeaders"] = std::move(headers);
-            subscription["MessageIds"] = subValue->registryMsgIds;
-            subscription["Protocol"] = subValue->protocol;
-            subscription["RegistryPrefixes"] = subValue->registryPrefixes;
-            subscription["ResourceTypes"] = subValue->resourceTypes;
-            subscription["SubscriptionType"] = subValue->subscriptionType;
+            subscription["MessageIds"] = subValue.registryMsgIds;
+            subscription["Protocol"] = subValue.protocol;
+            subscription["RegistryPrefixes"] = subValue.registryPrefixes;
+            subscription["ResourceTypes"] = subValue.resourceTypes;
+            subscription["SubscriptionType"] = subValue.subscriptionType;
             subscription["MetricReportDefinitions"] =
-                subValue->metricReportDefinitions;
+                subValue.metricReportDefinitions;
 
             subscriptions.emplace_back(std::move(subscription));
         }
