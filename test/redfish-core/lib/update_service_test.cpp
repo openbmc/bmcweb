f86bcc875a496b3c321a4ed102579a4031617800
Ed Tanous <edtanous@google.com>
2023-08-25 09:34:07 -0700

    Clean up tftp update to use URL
    Similar to transforms we've done elsewhere, we shouldn't be parsing
    urls using std::string::find, regex, or anything else, as they don't
    handle URL % encoding properly.
    
    Change-Id: I48bb30c0c737c4df2ae73f40fc49c63bac5b658f
    Signed-off-by: Ed Tanous <edtanous@google.com>

---
+++ test/redfish-core/lib/update_service_test.cpp
@@ -0,0 +1,55 @@
+
+#include "update_service.hpp"
+
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+TEST(UpdateService, ParseTFTPPostitive)
+{
+    crow::Response res;
+    {
+        // No protocol, schema on url
+        std::optional<TftpUrl> ret = parseTftpUrl("tftp://1.1.1.1/path",
+                                                  std::nullopt, res);
+        ASSERT_NE(ret, std::nullopt);
+        EXPECT_EQ(ret->tftpServer, "1.1.1.1");
+        EXPECT_EQ(ret->fwFile, "path");
+    }
+    {
+        // Protocol, no schema on url
+        std::optional<TftpUrl> ret = parseTftpUrl("1.1.1.1/path", "TFTP", res);
+        ASSERT_NE(ret, std::nullopt);
+        EXPECT_EQ(ret->tftpServer, "1.1.1.1");
+        EXPECT_EQ(ret->fwFile, "path");
+    }
+    {
+        // Both protocl and schema on url
+        std::optional<TftpUrl> ret = parseTftpUrl("tftp://1.1.1.1/path", "TFTP",
+                                                  res);
+        ASSERT_NE(ret, std::nullopt);
+        EXPECT_EQ(ret->tftpServer, "1.1.1.1");
+        EXPECT_EQ(ret->fwFile, "path");
+    }
+}
+
+TEST(UpdateService, ParseTFTPNegative)
+{
+    crow::Response res;
+    // No protocol, no schema
+    ASSERT_EQ(parseTftpUrl("1.1.1.1/path", std::nullopt, res), std::nullopt);
+    // No host
+    ASSERT_EQ(parseTftpUrl("/path", "TFTP", res), std::nullopt);
+
+    // No host
+    ASSERT_EQ(parseTftpUrl("path", "TFTP", res), std::nullopt);
+
+    // No path
+    ASSERT_EQ(parseTftpUrl("tftp://1.1.1.1", "TFTP", res), std::nullopt);
+    ASSERT_EQ(parseTftpUrl("tftp://1.1.1.1/", "TFTP", res), std::nullopt);
+}
+} // namespace
+} // namespace redfish
