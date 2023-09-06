f95de9b19ebdd1624b83734e91c351250574c221
Ed Tanous <edtanous@google.com>
2023-09-06 13:57:30 -0700

    Fix missing date
    At some point, the date got removed from http1 requests.  HTTP2 does not
    show this issue, but this showed up in unit tests (which is why the
    prior commit is adding unit tests).
    
    The Date Header is useful for synchronizing things like
    Cache-Control-Policy, with the actual server time, instead of the local
    system time.
    
    Tested: Unit tests pass.
    
    Change-Id: I8f105f0cbb6c816c5ec6b14cbeae587d728a20d2
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- test/http/http_connection_test.cpp
+++ test/http/http_connection_test.cpp
@@ -2,6 +2,7 @@
 #include "http/http_request.hpp"
 #include "http/http_response.hpp"
 
+#include <boost/asio/steady_timer.hpp>
 #include <boost/beast/_experimental/test/stream.hpp>
 #include <boost/asio/steady_timer.hpp>
 
@@ -32,6 +33,12 @@ struct FakeHandler
     {
         EXPECT_EQ(req.method(), boost::beast::http::verb::get);
         EXPECT_EQ(req.target(), "/");
+        EXPECT_EQ(req.getHeaderValue(boost::beast::http::field::host),
+                  "openbmc_project.xyz");
+        EXPECT_FALSE(req.keepAlive());
+        EXPECT_EQ(req.version(), 11);
+        EXPECT_EQ(req.body(), "");
+
         called = true;
         io.stop();
     }
@@ -39,14 +46,20 @@ struct FakeHandler
     bool called = false;
 };
 
-static std::string getDateStr()
+struct ClockFake
 {
-    return "TestTime";
-}
+    bool wascalled = false;
+    std::string getDateStr()
+    {
+        wascalled = true;
+        return "TestTime";
+    }
+};
 
 TEST(http_connection, RequestPropogates)
 {
     boost::asio::io_context io;
+    ClockFake clock;
     boost::beast::test::stream stream(io);
     boost::beast::test::stream out(io);
     stream.connect(out);
@@ -54,34 +67,36 @@ TEST(http_connection, RequestPropogates)
         "GET / HTTP/1.1\r\nHost: openbmc_project.xyz\r\nConnection: close\r\n\r\n"));
     FakeHandler handler(io);
     boost::asio::steady_timer timer(io);
-    std::function<std::string()> date(&getDateStr);
+    std::function<std::string()> date(
+        std::bind_front(&ClockFake::getDateStr, &clock));
     std::shared_ptr<crow::Connection<boost::beast::test::stream, FakeHandler>>
         conn = std::make_shared<
             crow::Connection<boost::beast::test::stream, FakeHandler>>(
             &handler, std::move(timer), date, std::move(stream));
     conn->start();
     io.run();
-    BMCWEB_LOG_DEBUG("out.str() is {}", out.str());
     EXPECT_TRUE(handler.called);
 
     EXPECT_EQ(
         out.str(),
-        std::string_view(
-            "HTTP/1.1 200 OK\r\n"
-            "Connection: close\r\n"
-            "Strict-Transport-Security: max-age=31536000; includeSubdomains\r\n"
-            "X-Frame-Options: DENY\r\n"
-            "Pragma: no-cache\r\n"
-            "Cache-Control: no-store, max-age=0\r\n"
-            "X-Content-Type-Options: nosniff\r\n"
-            "Referrer-Policy: no-referrer\r\n"
-            "Permissions-Policy: accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(self),legacy-image-formats=(self),magnetometer=(),microphone=(),midi=(),oversized-images=(self),payment=(),picture-in-picture=(),publickey-credentials-get=(),speaker-selection=(),sync-xhr=(self),unoptimized-images=(self),unsized-media=(self),usb=(),screen-wak-lock=(),web-share=(),xr-spatial-tracking=()\r\n"
-            "X-Permitted-Cross-Domain-Policies: none\r\n"
-            "Cross-Origin-Embedder-Policy: require-corp\r\n"
-            "Cross-Origin-Opener-Policy: same-origin\r\n"
-            "Cross-Origin-Resource-Policy: same-origin\r\n"
-            "Content-Security-Policy: default-src 'none'; img-src 'self' data:; font-src 'self'; style-src 'self'; script-src 'self'; connect-src 'self' wss:; form-action 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'none'\r\n"
-            "Content-Length: 0\r\n\r\n"));
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Strict-Transport-Security: max-age=31536000; includeSubdomains\r\n"
+        "X-Frame-Options: DENY\r\n"
+        "Pragma: no-cache\r\n"
+        "Cache-Control: no-store, max-age=0\r\n"
+        "X-Content-Type-Options: nosniff\r\n"
+        "Referrer-Policy: no-referrer\r\n"
+        "Permissions-Policy: accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(self),legacy-image-formats=(self),magnetometer=(),microphone=(),midi=(),oversized-images=(self),payment=(),picture-in-picture=(),publickey-credentials-get=(),speaker-selection=(),sync-xhr=(self),unoptimized-images=(self),unsized-media=(self),usb=(),screen-wak-lock=(),web-share=(),xr-spatial-tracking=()\r\n"
+        "X-Permitted-Cross-Domain-Policies: none\r\n"
+        "Cross-Origin-Embedder-Policy: require-corp\r\n"
+        "Cross-Origin-Opener-Policy: same-origin\r\n"
+        "Cross-Origin-Resource-Policy: same-origin\r\n"
+        "Content-Security-Policy: default-src 'none'; img-src 'self' data:; font-src 'self'; style-src 'self'; script-src 'self'; connect-src 'self' wss:; form-action 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'none'\r\n"
+        "Date: TestTime\r\n"
+        "Content-Length: 0\r\n\r\n");
+
+    EXPECT_TRUE(clock.wascalled);
 }
 
 } // namespace crow
