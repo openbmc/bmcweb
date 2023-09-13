9ed09bdbdf8809a36f698ccc0e90da5ef80b82e1
Rohit PAI <ropai@nvidia.com>
2023-09-13 10:32:18 +0530

    MR to review the proposal for URI based threading model to handle aggregate URIs
    Signed-off-by: Rohit PAI <rohitpai77@gmail.com>
    Change-Id: If3c142e63f053b60eea4efddbb3cc7240cd397b6

--- src/dbus_singleton.cpp
+++ src/dbus_singleton.cpp
@@ -7,5 +7,12 @@ namespace connections
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 sdbusplus::asio::connection* systemBus = nullptr;
 
+boost::asio::io_context& getNextIoContext()
+{
+    int threadCount = 4;
+    static boost::asio::io_context io(threadCount);
+    return io;
+}
+
 } // namespace connections
 } // namespace crow
