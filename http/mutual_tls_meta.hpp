ee16618b842e2c52469d04bf3292ea1e786adc05
Marco Kawajiri <kawajiri@meta.com>
2023-10-31 13:36:58 -0700

    mutual-tls: Optional support for Meta internal certificates
    Meta Inc's client certificates use an internal Subject CN format
    which AFAIK is specific to Meta and don't adhere to a known standard:
    
      Subject: CN = <type>:<entity>/<hostname>
    
    Commit adds the `meta-mutual-tls-common-name-parsing` option to, on
    Meta builds, parse the Subject CN field and map either the <entity>
    or the <hostname> to a local user.
    
    The <type> field determines what kind of identity the cert represents:
      "user": A user cert, <entity> is the unixname
      "svc": A service cert, <entity> is the service name
      "host": A host cert, <entity> is empty, <hostname> is the hostname
    
    Some example Subject -> user mappings:
      "user:kawmarco/dev123.facebook.com" -> "user_kawmarco"
      "svc:openbmc_configure" -> "svc_openbmc_configure"
      "host:/dev123.facebook.com" -> "host_dev123"
    
    Also added logic to ignore the keyAgreement field on
    -Dmeta-mutual-tls-common-name-parsing as Meta does not use this field
    for internal client certificates.
    
    Change-Id: I35ee9b92d163ce56815a5bd9cce5296ba1a44eef
    Signed-off-by: Marco Kawajiri <kawajiri@meta.com>

---
+++ http/mutual_tls_meta.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "logging.hpp"
+
+#include <regex>
+
+// Regex matching the Subject CommonName format used by internal Meta client
+// certs
+const std::string sslUserRegexStr =
+    "^(user|svc|host):"  // userType
+    "([A-Za-z0-9._-]*)"  // userName
+    "(/([A-Za-z0-9.-]+)" // userHost (optional if userType != "host")
+    "[.](facebook[.]com|tfbnw[.]net|thefacebook[.]com)" // host suffix
+    ")?$";
+
+inline int mtlsMetaParseSslUser(std::string& sslUser)
+{
+    // Parses a Meta internal TLS client certificate user name in
+    // '<type>:<username or service name>[/<hostname>]' format and writes the
+    // resulting POSIX-compatible user name in sslUser (followed by zero-byte so
+    // it's similar to X509_get_subject_name() behaviour). Returns 0 on success,
+    // -1 otherwise.
+    //
+    // Examples:
+    // "user:a_username/hostname" -> "user_a_username"
+    // "svc:an_internal_service_name" -> "svc_an_internal_service_name"
+    // "host:/hostname.facebook.com" -> "host_hostname" (note the stripped
+    // hostname suffix)
+    const static std::regex sslUserRegex(sslUserRegexStr);
+
+    std::smatch sm;
+    if (!std::regex_match(sslUser, sm, sslUserRegex))
+    {
+        BMCWEB_LOG_DEBUG(
+            "Invalid Meta TLS user name: '{}', expected it to match: /{}/",
+            sslUser, sslUserRegexStr);
+        return -1;
+    }
+
+    std::string userType = sm[1]; // e.g. "user" or "svc"
+    std::string userName = sm[2]; // e.g. "a_username"
+    std::string userHost = sm[4]; // e.g. "hostname"
+
+    // Use the hostname as user name if userType == "host"
+    // e.g. "host:/hostname.facebook.com" -> "host_hostname"
+    if (userType == "host")
+    {
+        userName = userHost;
+    }
+
+    if (userType.empty() || userName.empty())
+    {
+        BMCWEB_LOG_DEBUG("Invalid Meta TLS user name: '{}'", sslUser);
+        return -1;
+    }
+
+    sslUser = userType + "_" + userName;
+
+    return 0;
+}
