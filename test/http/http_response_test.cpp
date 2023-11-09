02d6aa686ee5c27579dc516aa94ca65ca6b95105
Abhilash Raju <abhilash.kollam@gmail.com>
2023-11-08 22:32:44 -0600

    Dump-offload: Make use of filebody for dump offload
    Logservice has been rewritten to use file_body to offload dump files
    from BMC.
    
    There are two kind of dump files, BMC dump and System dump.While BMC
    dump just requires default support from beast::file_body, System dump
    requires base64 encoding support from beast. But beast::file_body do not
    have ready-made support for base64 encoding. So a custom file_body has
    been written for the base64 encoding.
    
    The openFile apis in crow::Response do not have support for unix file
    descriptor. Since dump files are accesses via descriptors, added new
    openFile api that accepts descriptors.
    
    Tested:
    Functionality test have been executed to verify the bmc dump offload.
    Did sanity test by invoking bmcweb pages via browser.
    
    Change-Id: I24192657c03d8b2f0394d31e7424c6796ba3227a
    Signed-off-by: Abhilash Raju <abhilash.kollam@gmail.com>

--- test/http/http_response_test.cpp
+++ test/http/http_response_test.cpp
@@ -22,16 +22,68 @@ void verifyHeaders(crow::Response& res)
     EXPECT_EQ(res.result(), boost::beast::http::status::ok);
 }
 
-std::string makeFile()
+std::string makeFile(std::function<std::string()> sampleData)
 {
     std::filesystem::path path = std::filesystem::temp_directory_path();
     path /= "bmcweb_http_response_test_XXXXXXXXXXX";
     std::string stringPath = path.string();
     int fd = mkstemp(stringPath.data());
     EXPECT_GT(fd, 0);
-    std::string_view sample = "sample text";
+    std::string sample = sampleData();
     EXPECT_EQ(write(fd, sample.data(), sample.size()), sample.size());
     return stringPath;
+}
+std::string makeFile()
+{
+    return makeFile([]() { return "sample text"; });
+}
+template <typename Body>
+void readHeader(boost::beast::http::serializer<false, Body>& sr,
+                boost::beast::error_code& ec)
+{
+    while (!sr.is_header_done())
+    {
+        sr.next(ec, [&sr](boost::beast::error_code&, const auto& buffer) {
+            sr.consume(boost::beast::buffer_bytes(buffer));
+        });
+    }
+}
+template <typename Body>
+std::string readBody(boost::beast::http::serializer<false, Body>& sr,
+                     boost::beast::error_code& ec)
+{
+    std::string ret;
+    while (!sr.is_done())
+    {
+        sr.next(ec, [&sr, &ret](boost::beast::error_code&, const auto& buffer) {
+            std::accumulate(boost::asio::buffer_sequence_begin(buffer),
+                            boost::asio::buffer_sequence_end(buffer),
+                            std::ref(ret),
+                            [&](auto sofar, const auto& innerBuf) {
+                auto view = std::string_view(
+                    static_cast<char const*>(innerBuf.data()), innerBuf.size());
+                sofar.get() += view;
+                sr.consume(innerBuf.size());
+                return sofar;
+            });
+        });
+    }
+
+    return ret;
+}
+template <class Body, typename Fields>
+std::string getData(boost::beast::http::message<false, Body, Fields>& m,
+                    boost::beast::error_code& ec)
+{
+    boost::beast::http::serializer<false, Body> sr{m};
+    std::stringstream ret;
+    sr.split(true);
+    readHeader(sr, ec);
+    if (ec)
+    {
+        return {};
+    }
+    return readBody(sr, ec);
 }
 TEST(HttpResponse, Defaults)
 {
@@ -66,6 +118,42 @@ TEST(HttpResponse, FileBody)
     verifyHeaders(res);
     std::filesystem::remove(path);
 }
+TEST(HttpResponse, FileBodyWithFd)
+{
+    crow::Response res;
+    addHeaders(res);
+    std::string path = makeFile();
+    int fd = open(path.c_str(), O_RDONLY);
+    if (fd != -1)
+    {
+        res.openFile(fd);
+        verifyHeaders(res);
+    }
+    std::filesystem::remove(path);
+}
+TEST(HttpResponse, Base64FileBody)
+{
+    crow::Response res;
+    addHeaders(res);
+    std::string path = makeFile();
+    res.openBase64File(path);
+
+    verifyHeaders(res);
+    std::filesystem::remove(path);
+}
+TEST(HttpResponse, Base64FileBodyWithFd)
+{
+    crow::Response res;
+    addHeaders(res);
+    std::string path = makeFile();
+    int fd = open(path.c_str(), O_RDONLY);
+    if (fd != -1)
+    {
+        res.openBase64File(path);
+        verifyHeaders(res);
+    }
+    std::filesystem::remove(path);
+}
 TEST(HttpResponse, BodyTransitions)
 {
     crow::Response res;
@@ -88,4 +176,71 @@ TEST(HttpResponse, BodyTransitions)
     verifyHeaders(res);
     std::filesystem::remove(path);
 }
+void testFileData(crow::Response& res, const std::string& data)
+{
+    boost::variant2::visit(
+        [&res, &data](auto&& arg) {
+        boost::beast::error_code ec;
+        auto fileData = getData(arg, ec);
+        EXPECT_EQ(ec.value(), 0);
+        EXPECT_EQ(fileData, data);
+    },
+        res.response);
+}
+TEST(HttpResponse, Base64FileBodyWriter)
+{
+    crow::Response res;
+    std::string data = "sample text";
+    std::string path = makeFile([&data]() { return data; });
+    int fd = open(path.c_str(), O_RDONLY);
+    res.openBase64File(dup(fd));
+    testFileData(res, crow::utility::base64encode(data));
+    close(fd);
+    std::filesystem::remove(path);
+}
+TEST(HttpResponse, Base64FileBodyWriterLarge)
+{
+    crow::Response res;
+    auto dataGen = []() {
+        std::string result;
+        size_t i = 0;
+        while (i < 10000)
+        {
+            result += "sample text";
+            i += std::string("sample text").length();
+        }
+        return result;
+    };
+
+    std::string path = makeFile(dataGen);
+    int fd = open(path.c_str(), O_RDONLY);
+    res.openBase64File(dup(fd));
+    auto data = dataGen();
+    testFileData(res, crow::utility::base64encode(data));
+    close(fd);
+    std::filesystem::remove(path);
+}
+
+TEST(HttpResponse, FileBodyWriterLarge)
+{
+    crow::Response res;
+    auto dataGen = []() {
+        std::string result;
+        size_t i = 0;
+        while (i < 10000)
+        {
+            result += "sample text";
+            i += std::string("sample text").length();
+        }
+        return result;
+    };
+
+    std::string path = makeFile(dataGen);
+    int fd = open(path.c_str(), O_RDONLY);
+    res.openFile(dup(fd));
+    testFileData(res, dataGen());
+    close(fd);
+    std::filesystem::remove(path);
+}
+
 } // namespace
