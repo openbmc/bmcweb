9644995a0c6a1d88e79d01b58a0db4d100c8a62c
Jagpal Singh Gill <paligill@gmail.com>
2023-12-20 15:35:41 -0800

    manager_diagnostic_data: add metric get
    Add support to fetch MemoryStatistics, FreeStorageSpaceKiB and
    ProcessorStatistics for Manager Diagnostic Data.
    https://redfish.dmtf.org/schemas/v1/ManagerDiagnosticData.v1_2_1.json
    This change is in relation to following design and D-Bus interface -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    Test:
    Redfish query output -
    {
      "@odata.type": "#ManagerDiagnosticData.v1_2_0.ManagerDiagnosticData",
      "FreeStorageSpaceKiB": 3,
      "Id": "ManagerDiagnosticData",
      "MemoryStatistics": {
        "AvailableBytes": 370296000,
        "BuffersAndCacheBytes": 65424000,
        "FreeBytes": 321980000,
        "SharedBytes": 11800000,
        "TotalBytes": 425516000
      },
      "Name": "Manager Diagnostic Data",
      "ProcessorStatistics": {
        "KernelPercent": 61.4316,
        "UserPercent": 37.7594
      },
      "ServiceRootUptimeSeconds": 69.101
    }
    
    Redfish service validator passing -
    Validation Passed:
    Elapsed time: 0:04:04
    metadataNamespaces: 3726
    pass: 5139
    passAction: 9
    passGet: 206
    passRedfishUri: 198
    skipNoSchema: 3
    skipOptional: 3499
    warnDeprecated: 4
    warningPresent: 7
    Validation has succeeded.
    
    Change-Id: I43758a993eb7f342cb9ac5f5574498b37261c2cc
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- redfish-core/lib/manager_diagnostic_data.hpp
+++ redfish-core/lib/manager_diagnostic_data.hpp
@@ -8,6 +8,7 @@
 #include "registries/privilege_registry.hpp"
 #include "routing.hpp"
 
+#include <boost/system/linux_error.hpp>
 #include <nlohmann/json.hpp>
 #include <sdbusplus/asio/property.hpp>
 
@@ -16,13 +17,254 @@
 namespace redfish
 {
 
+static constexpr auto healthMonitorServiceName =
+    "xyz.openbmc_project.HealthMon";
+static constexpr auto valueInterface = "xyz.openbmc_project.Metric.Value";
+static constexpr auto valueProperty = "Value";
+static constexpr double roundFactor = 10000; // 4 decimal places
+
+void checkErrors(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                 const boost::system::error_code& ec, std::string message)
+{
+    if (ec.value() == boost::asio::error::basic_errors::host_unreachable)
+    {
+        BMCWEB_LOG_WARNING("Failed to find server, Dbus error {}", ec);
+    }
+    else if (ec.value() == boost::system::linux_error::bad_request_descriptor)
+    {
+        BMCWEB_LOG_WARNING("Invalid Path, Dbus error {}", ec);
+    }
+    else
+    {
+        BMCWEB_LOG_ERROR("{} failed, Dbus error {}", message, ec);
+        messages::internalError(asyncResp->res);
+    }
+}
+
+inline void afterGetFreeStorageStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double freeStorage)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetFreeStorageStatistics");
+        return;
+    }
+    if (!std::isfinite(freeStorage))
+    {
+        asyncResp->res.jsonValue["FreeStorageSpaceKiB"] = nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["FreeStorageSpaceKiB"] =
+        static_cast<int64_t>(freeStorage / 1024.0);
+}
+
+inline void managerGetStorageStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    constexpr auto freeStorageObjPath =
+        "/xyz/openbmc_project/metric/bmc/storage/rw";
+
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName,
+        freeStorageObjPath, valueInterface, valueProperty,
+        std::bind_front(afterGetFreeStorageStatistics, asyncResp));
+}
+
+inline void afterGetProcessorKernelStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double kernelCPU)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetProcessorKernelStatistics");
+        return;
+    }
+    if (!std::isfinite(kernelCPU))
+    {
+        asyncResp->res.jsonValue["ProcessorStatistics"]["KernelPercent"] =
+            nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["ProcessorStatistics"]["KernelPercent"] =
+        std::round(kernelCPU * roundFactor) / roundFactor;
+}
+
+inline void afterGetProcessorUserStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double userCPU)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetProcessorUserStatistics");
+        return;
+    }
+    if (!std::isfinite(userCPU))
+    {
+        asyncResp->res.jsonValue["ProcessorStatistics"]["UserPercent"] =
+            nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["ProcessorStatistics"]["UserPercent"] =
+        std::round(userCPU * roundFactor) / roundFactor;
+}
+
+inline void managerGetProcessorStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    constexpr auto kernelCPUObjPath =
+        "/xyz/openbmc_project/metric/bmc/cpu/kernel";
+    constexpr auto userCPUObjPath = "/xyz/openbmc_project/metric/bmc/cpu/user";
+
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName,
+        kernelCPUObjPath, valueInterface, valueProperty,
+        std::bind_front(afterGetProcessorKernelStatistics, asyncResp));
+
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName, userCPUObjPath,
+        valueInterface, valueProperty,
+        std::bind_front(afterGetProcessorUserStatistics, asyncResp));
+}
+
+inline void afterGetAvailableMemoryStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double availableMemory)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetAvailableMemoryStatistics");
+        return;
+    }
+    if (!std::isfinite(availableMemory))
+    {
+        asyncResp->res.jsonValue["MemoryStatistics"]["AvailableBytes"] =
+            nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["MemoryStatistics"]["AvailableBytes"] =
+        static_cast<int64_t>(availableMemory);
+}
+
+inline void afterGetBufferedAndCachedMemoryStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double bufferedAndCachedMemory)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetBufferedAndCachedMemoryStatistics");
+        return;
+    }
+    if (!std::isfinite(bufferedAndCachedMemory))
+    {
+        asyncResp->res.jsonValue["MemoryStatistics"]["BuffersAndCacheBytes"] =
+            nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["MemoryStatistics"]["BuffersAndCacheBytes"] =
+        static_cast<int64_t>(bufferedAndCachedMemory);
+}
+
+inline void afterGetFreeMemoryStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double freeMemory)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetFreeMemoryStatistics");
+        return;
+    }
+    if (!std::isfinite(freeMemory))
+    {
+        asyncResp->res.jsonValue["MemoryStatistics"]["FreeBytes"] = nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["MemoryStatistics"]["FreeBytes"] =
+        static_cast<int64_t>(freeMemory);
+}
+
+inline void afterGetSharedMemoryStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double sharedMemory)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetSharedMemoryStatistics");
+        return;
+    }
+    if (!std::isfinite(sharedMemory))
+    {
+        asyncResp->res.jsonValue["MemoryStatistics"]["SharedBytes"] = nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["MemoryStatistics"]["SharedBytes"] =
+        static_cast<int64_t>(sharedMemory);
+}
+
+inline void afterGetTotalMemoryStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, double totalMemory)
+{
+    if (ec)
+    {
+        checkErrors(asyncResp, ec, "afterGetTotalMemoryStatistics");
+        return;
+    }
+    if (!std::isfinite(totalMemory))
+    {
+        asyncResp->res.jsonValue["MemoryStatistics"]["TotalBytes"] = nullptr;
+        return;
+    }
+    asyncResp->res.jsonValue["MemoryStatistics"]["TotalBytes"] =
+        static_cast<int64_t>(totalMemory);
+}
+
+inline void managerGetMemoryStatistics(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    constexpr auto availableMemoryObjPath =
+        "/xyz/openbmc_project/metric/bmc/memory/available";
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName,
+        availableMemoryObjPath, valueInterface, valueProperty,
+        std::bind_front(afterGetAvailableMemoryStatistics, asyncResp));
+
+    constexpr auto bufferedAndCachedMemoryObjPath =
+        "/xyz/openbmc_project/metric/bmc/memory/buffered_and_cached";
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName,
+        bufferedAndCachedMemoryObjPath, valueInterface, valueProperty,
+        std::bind_front(afterGetBufferedAndCachedMemoryStatistics, asyncResp));
+
+    constexpr auto freeMemoryObjPath =
+        "/xyz/openbmc_project/metric/bmc/memory/free";
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName,
+        freeMemoryObjPath, valueInterface, valueProperty,
+        std::bind_front(afterGetFreeMemoryStatistics, asyncResp));
+
+    constexpr auto sharedMemoryObjPath =
+        "/xyz/openbmc_project/metric/bmc/memory/shared";
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName,
+        sharedMemoryObjPath, valueInterface, valueProperty,
+        std::bind_front(afterGetSharedMemoryStatistics, asyncResp));
+
+    constexpr auto totalMemoryObjPath =
+        "/xyz/openbmc_project/metric/bmc/memory/total";
+    sdbusplus::asio::getProperty<double>(
+        *crow::connections::systemBus, healthMonitorServiceName,
+        totalMemoryObjPath, valueInterface, valueProperty,
+        std::bind_front(afterGetTotalMemoryStatistics, asyncResp));
+}
+
 inline void afterGetManagerStartTime(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const boost::system::error_code& ec, uint64_t bmcwebResetTime)
 {
     if (ec)
     {
-        // Not all servers will be running in systemd, so ignore the error.
+        checkErrors(asyncResp, ec, "afterGetManagerStartTime");
         return;
     }
     using std::chrono::steady_clock;
@@ -82,6 +324,9 @@ inline void handleManagerDiagnosticDataGet(
     asyncResp->res.jsonValue["Name"] = "Manager Diagnostic Data";
 
     managerGetServiceRootUptime(asyncResp);
+    managerGetProcessorStatistics(asyncResp);
+    managerGetMemoryStatistics(asyncResp);
+    managerGetStorageStatistics(asyncResp);
 }
 
 inline void requestRoutesManagerDiagnosticData(App& app)
