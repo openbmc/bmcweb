3cba246f8bb7691d9fb38e419d57c319dc340b2e
Jagpal Singh Gill <paligill@gmail.com>
2023-12-20 15:35:41 -0800

    manager_diagnostic_data: add metric get
    Add support to fetch MemoryStatistics, FreeStorageSpaceKiB and
    ProcessorStatistics for Manager Diagnostic Data.
    https://redfish.dmtf.org/schemas/v1/ManagerDiagnosticData.v1_2_1.json
    This change is in relation to following design and D-Bus interface -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    Test:
    Redfish query output -
    {
      "@odata.id": "/redfish/v1/Managers/bmc/ManagerDiagnosticData",
      "@odata.type": "#ManagerDiagnosticData.v1_2_0.ManagerDiagnosticData",
      "FreeStorageSpaceKiB": 3772,
      "Id": "ManagerDiagnosticData",
      "MemoryStatistics": {
        "AvailableBytes": 354224066,
        "BuffersAndCacheBytes": 78984633,
        "SharedBytes": 11876066,
        "TotalBytes": 425516000
      },
      "Name": "Manager Diagnostic Data",
      "ProcessorStatistics": {
        "KernelPercent": 13.0234,
        "UserPercent": 5.7374
      },
      "ServiceRootUptimeSeconds": 2255.117
    }
    
    Redfish service validator passing -
    Elapsed time: 0:03:12
    metadataNamespaces: 3726
    pass: 5133
    passAction: 9
    passGet: 205
    passRedfishUri: 197
    skipNoSchema: 3
    skipOptional: 3492
    warnDeprecated: 4
    warningPresent: 7
    Validation has succeeded.
    
    Change-Id: I43758a993eb7f342cb9ac5f5574498b37261c2cc
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ test/redfish-core/lib/manager_diagnostic_data_test.cpp
@@ -0,0 +1,75 @@
+#include "async_resp.hpp"
+#include "manager_diagnostic_data.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <memory>
+
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+using json_pointer = nlohmann::json::json_pointer;
+
+void testDataGetNoError(boost::system::error_code ec)
+{
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+
+    setBytesProperty(asyncResp,
+                     json_pointer("/MemoryStatistics/FreeStorageSpace"), ec, 0);
+    EXPECT_TRUE(asyncResp->res.jsonValue.is_null());
+    EXPECT_EQ(asyncResp->res.result(), boost::beast::http::status::ok);
+}
+
+TEST(ManagerDiagnosticDataTest, ManagerDataGetServerUnreachable)
+{
+    testDataGetNoError(boost::asio::error::basic_errors::host_unreachable);
+}
+
+TEST(ManagerDiagnosticDataTest, ManagerDataGetPathInvalid)
+{
+    testDataGetNoError(boost::system::linux_error::bad_request_descriptor);
+}
+
+void verifyError(crow::Response& res)
+{
+    EXPECT_EQ(res.result(), boost::beast::http::status::internal_server_error);
+    res.clear();
+}
+
+TEST(ManagerDiagnosticDataTest, ManagerDataGetFailure)
+{
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec = boost::asio::error::operation_aborted;
+
+    setBytesProperty(asyncResp,
+                     json_pointer("/MemoryStatistics/FreeStorageSpace"), ec, 0);
+    verifyError(asyncResp->res);
+}
+
+TEST(ManagerDiagnosticDataTest, ManagerDataGetNullPtr)
+{
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+
+    setPercentProperty(
+        asyncResp,
+        nlohmann::json::json_pointer("/MemoryStatistics/FreeStorageSpace"), {},
+        std::numeric_limits<double>::quiet_NaN());
+    EXPECT_EQ(asyncResp->res.jsonValue["FreeStorageSpaceKiB"], nullptr);
+}
+
+TEST(ManagerDiagnosticDataTest, ManagerDataGetSuccess)
+{
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+
+    setBytesProperty(asyncResp, json_pointer("/FreeStorageSpaceKiB"), {},
+                     204800.0);
+    EXPECT_EQ(asyncResp->res.jsonValue["FreeStorageSpaceKiB"].get<int64_t>(),
+              200);
+}
+
+} // namespace
+} // namespace redfish
