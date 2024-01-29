4a319b7f8d4dcb8f362512163978cdae9731f6e6
Ed Tanous <ed@tanous.net>
2024-01-29 08:34:59 -0800

    Write unit tests for http2 connection
    This unit test currently only tests a simple connect and settings frame
    transfer, but should form the basis for more complex testing in the
    future.
    
    Tested: Unit tests pass
    
    Change-Id: Ieb803dbe490129ec5fe99fb3d4505a06202e282e
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ test/http/http2_connection_test.cpp
@@ -0,0 +1,92 @@
+#include "http/http2_connection.hpp"
+#include "http/http_request.hpp"
+#include "http/http_response.hpp"
+
+#include <boost/asio/steady_timer.hpp>
+#include <boost/beast/_experimental/test/stream.hpp>
+
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <string>
+#include <string_view>
+
+#include "gtest/gtest.h"
+namespace crow
+{
+
+namespace
+{
+struct FakeHandler
+{
+    static void
+        handleUpgrade(Request& /*req*/,
+                      const std::shared_ptr<bmcweb::AsyncResp>& /*asyncResp*/,
+                      boost::beast::test::stream&& /*adaptor*/)
+    {
+        // Handle Upgrade should never be called
+        EXPECT_FALSE(true);
+    }
+
+    void handle(Request& /*req*/,
+                const std::shared_ptr<bmcweb::AsyncResp>& /*asyncResp*/)
+    {}
+};
+
+std::string getDateStr()
+{
+    return "TestTime";
+}
+
+TEST(http_connection, RequestPropogates)
+{
+    boost::asio::io_context io;
+    boost::beast::test::stream stream(io);
+    boost::beast::test::stream out(io);
+    stream.connect(out);
+
+    out.write_some(boost::asio::buffer(
+        // Hello
+        "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
+        // Empty SETTINGS frame to stream 0
+        "\0\0\0\x4\0\0\0\0\0"
+        // ACK the servers SETTINGS frame on stream 0
+        "\0\0\0\x4\x1\0\0\0\0"
+
+        // Headers frame on stream 1
+        //"\0\0\<len>\x1\0\0\0\0\x1"
+        //"\0\0\0\0"
+        ));
+
+    FakeHandler handler;
+    boost::asio::steady_timer timer(io);
+    std::function<std::string()> date(getDateStr);
+    auto conn = std::make_shared<
+        HTTP2Connection<boost::beast::test::stream, FakeHandler>>(
+        std::move(stream), &handler, date);
+    conn->start();
+
+    using namespace std::literals;
+    std::string_view expected =
+        // Settings frame size 15
+        "\0\0\f\x4\0\0\0\0\0"
+        // 4 max concurrent streams
+        "\0\x3\0\0\0\x4"
+        // Enable push = false
+        "\0\x2\0\0\0\0"
+        // Settings ACK from server to client
+        "\0\0\0\x4\x1\0\0\0\0"sv;
+
+    std::string outStr;
+    while (outStr.size() != expected.size())
+    {
+        io.run_one();
+        outStr = out.str();
+    }
+
+    EXPECT_EQ(outStr, expected);
+}
+
+} // namespace
+} // namespace crow
