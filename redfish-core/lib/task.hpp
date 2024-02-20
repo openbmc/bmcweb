b524c7406d85f5cf47308b64a79f3e9df5632fae
Myung Bae <myungbae@us.ibm.com>
2024-02-20 09:55:40 -0500

    Add OemMessage for taskAbort Error
    This commit adds the Message OEM that will display error log
    information to taskAbort. This addition will allow users to see the
    exact error that caused the code update failure.
    
    Tested:
    - Ensured that upon code update failure, the task message includes the
    populated OEM with the relevant information.
    
    ```
    {
      "@odata.id": "/redfish/v1/TaskService/Tasks/0",
      "@odata.type": "#Task.v1_4_3.Task",
      "EndTime": "2022-05-11T17:01:15+00:00",
      "Id": "0",
      "Messages": [
        {
          "@odata.type": "#Message.v1_0_0.Message",
          "Message": "The task with id 0 has started.",
          "MessageArgs": [
            "0"
          ],
          "MessageId": "TaskEvent.1.0.1.TaskStarted",
          "Resolution": "None.",
          "Severity": "OK"
        },
        {
          "@odata.type": "#Message.v1_0_0.Message",
          "Message": "The task with id 0 has been aborted.",
          "MessageArgs": [
    ...
          ],
          "MessageId": "TaskEvent.1.0.1.TaskAborted",
          "Oem": {
            "OpenBMC": {
              "@odata.type": "#OemMessage.v1_0_0.Message",
              "AbortReason": "xyz.openbmc_project.Software.Version.Error.ExpiredAccessKey",
              "AdditionalData": "BUILD_ID=20220722 EXP_DATE=20220515 _PID=598 ",
              "EventId": "BD8D3608 00080055 2E330010 00000000 00000000 00000000 00000000 00000000 00000000"
            }
          },
          "Resolution": "None.",
          "Severity": "Critical"
        }
      ],
      "Name": "Task 0",
    ...
      "TaskMonitor": "/redfish/v1/TaskService/Tasks/0/Monitor",
      "TaskState": "Exception",
      "TaskStatus": "Warning"
    }
    ```
    
    Excerpt from validation pass:
    ```
    Schema file OemMessage_v1.xml not found in ./SchemaFiles/metadata
    Attempt 1 of /redfish/v1/schema/OemMessage_v1.xml
    Response Time for GET to /redfish/v1/schema/OemMessage_v1.xml: 0.010929436422884464 seconds.
    Writing online XML to file: OemMessage_v1.xml
    ```
    
    Co-authored-by: zamiseck <33493107+zamiseck@users.noreply.github.com>
    Change-Id: Id0cdeb0887a7dfed54b0b2c41b0312fca6593110
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/lib/task.hpp
+++ redfish-core/lib/task.hpp
@@ -191,8 +191,8 @@ struct TaskData : std::enable_shared_from_this<TaskData>
             self->finishTask();
             self->state = "Cancelled";
             self->status = "Warning";
-            self->messages.emplace_back(
-                messages::taskAborted(std::to_string(self->index)));
+            self->messages.emplace_back(messages::taskAborted(
+                std::to_string(self->index), "None", "None", "None"));
             // Send event :TaskAborted
             self->sendTaskEvent(self->state, self->index);
             self->callback(ec, msg, self);
@@ -237,8 +237,9 @@ struct TaskData : std::enable_shared_from_this<TaskData>
         else if (state == "Stopping")
         {
             redfish::EventServiceManager::getInstance().sendEvent(
-                redfish::messages::taskAborted(std::to_string(index)), origin,
-                resType);
+                redfish::messages::taskAborted(std::to_string(index), "None",
+                                               "None", "None"),
+                origin, resType);
         }
         else if (state == "Completed")
         {
