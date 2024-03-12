ce380bf5b4b827f12dd48dae6b876e22d02f7b99
Ed Tanous <ed@tanous.net>
2024-03-12 16:58:35 -0700

    WIP: Implement zstd decompression
    Given the size of Redfish schemas these days, it would be nice to be
    able to store them on disk in a zstd format.  Unfortunately, not all
    clients support zstd at this time.
    
    This commit implements reading of zstd files from disk, as well as
    decompressing zstd in the case where the client does not support zstd as
    a return type.
    
    Change-Id: I8b631bb943de99002fdd6745340aec010ee591ff
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ include/zstd_decompressor.hpp
@@ -0,0 +1,20 @@
+#include <zstd.h>
+
+#include <boost/asio/buffer.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
+
+#include <array>
+#include <optional>
+
+class ZstdDecompressor
+{
+    boost::beast::flat_buffer compressionBuf;
+
+    ZSTD_DCtx* dctx;
+
+  public:
+    ZstdDecompressor();
+    std::optional<boost::asio::const_buffer>
+        decompress(boost::asio::const_buffer buffIn);
+    ~ZstdDecompressor();
+};
