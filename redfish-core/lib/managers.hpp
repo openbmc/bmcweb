openbmc/bmcweb

commit ea2b670d306d85d6975b6174024985491af04a89

Fix regression in pid setting
It looks like one of the patchset merge conflicts got rebased poorly (ie
commented out).  In the meantime, a number of these structures have gone
from map->vector, so modify the algorithm to account for that.

Tested:
1. Redfish validator - passed with this change.
2. Verified from Redfish by patching "StepwiseControllers" sensors.

Signed-off-by: Ed Tanous <edtanous@google.com>
Signed-off-by: Jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
Change-Id: I89d36f1b0b3699b6bf07a17af029e5a2504c85de

redfish-core/lib/managers.hpp
@@ -1530,38 +1530,37 @@ struct SetPIDValues : std::enable_shared_from_this<SetPIDValues>
                 BMCWEB_LOG_DEBUG << "Found = " << !createNewObject;
 
                 std::string iface;
-                /*
-                if (type == "PidControllers" || type == "FanControllers")
+                if (!createNewObject)
                 {
-                    iface = pidConfigurationIface;
-                    if (!createNewObject &&
-                        pathItr->second.find(pidConfigurationIface) ==
-                            pathItr->second.end())
+                    for (const auto& interface : pathItr->second)
                     {
-                        createNewObject = true;
+                        if (interface.first == pidConfigurationIface)
+                        {
+                            if (type == "PidControllers" ||
+                                type == "FanControllers")
+                            {
+                                iface = pidConfigurationIface;
+                                createNewObject = true;
+                            }
+                        }
+                        else if (interface.first == pidZoneConfigurationIface)
+                        {
+                            if (type == "FanZones")
+                            {
+                                iface = pidConfigurationIface;
+                                createNewObject = true;
+                            }
+                        }
+                        else if (interface.first == stepwiseConfigurationIface)
+                        {
+                            if (type == "StepwiseControllers")
+                            {
+                                iface = stepwiseConfigurationIface;
+                                createNewObject = true;
+                            }
+                        }
                     }
                 }
-                else if (type == "FanZones")
-                {
-                    iface = pidZoneConfigurationIface;
-                    if (!createNewObject &&
-                        pathItr->second.find(pidZoneConfigurationIface) ==
-                            pathItr->second.end())
-                    {
-
-                        createNewObject = true;
-                    }
-                }
-                else if (type == "StepwiseControllers")
-                {
-                    iface = stepwiseConfigurationIface;
-                    if (!createNewObject &&
-                        pathItr->second.find(stepwiseConfigurationIface) ==
-                            pathItr->second.end())
-                    {
-                        createNewObject = true;
-                    }
-                }*/
 
                 if (createNewObject && it.value() == nullptr)
                 {

commit d1def0fd59f181908d6f39edb0a69be22d25d316

Fix PID Zone interface name
The interface name is being loaded incorrectly which causes the DBus
call to fail resulting in Internal server error.
This commit changes the interface to the correct name.

This is a regression caused by ea2b670d306d85d6975b6174024985491af04a89

TESTED:
 - PATCH /redfish/v1/Managers/bmc with below body succeeded
   {
     "Oem": {
         "OpenBmc": {
             "Fan": {
                 "FanZones": {
                     "Left": {
                         "MinThermalOutput": 30.0
                     }
                 }
             }
         }
      }
   }

Change-Id: I0bcf858cd541dc11e802431ad983954a57d8c790
Signed-off-by: PavanKumarIntel <pavanx.kumar.martha@intel.com>

redfish-core/lib/managers.hpp
@@ -1534,7 +1534,7 @@ struct SetPIDValues : std::enable_shared_from_this<SetPIDValues>
                         {
                             if (type == "FanZones")
                             {
-                                iface = pidConfigurationIface;
+                                iface = pidZoneConfigurationIface;
                                 findInterface = true;
                                 break;
                             }