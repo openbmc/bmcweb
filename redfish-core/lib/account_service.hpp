58d5cb6c84168f0182206cab4e62416e98d4fbde
Ed Tanous <ed@tanous.net>
2024-03-27 16:31:46 -0700

    Fix moves/forward
    Clang has new checks for std::move/std::forward correctness, which
    catches quite a few "wrong" things where we were making copies of
    callback handlers.
    
    Unfortunately, the lambda syntax of
    
    callback{std::forward<Callback>(callback)}
    
    in a capture confuses it, so change usages to
    callback = std::forward<Callback>(callback)
    
    to be consistent.
    
    Tested: Redfish service validator passes.
    
    Change-Id: I7a111ec00cf78ecb7d5f5b102c786c1c14d74384
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -513,7 +513,7 @@ inline void getLDAPConfigData(const std::string& ldapType,
 
     dbus::utility::getDbusObject(
         ldapConfigObjectName, interfaces,
-        [callback,
+        [callback = std::forward<CallbackFunc>(callback),
          ldapType](const boost::system::error_code& ec,
                    const dbus::utility::MapperGetObject& resp) mutable {
         if (ec || resp.empty())
