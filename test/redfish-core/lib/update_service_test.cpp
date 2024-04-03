bc648342c48e9effcf8b4caa76c791c136eedfe7
Ed Tanous <ed@tanous.net>
2024-04-03 14:32:38 -0700

    Refactor tftp parser
    This function in the next patch will be used for more than just TFTP, so
    rename it to match intent, and refactor to use non-TFTP specific types.
    
    Tested: Rename only.  Need help on TFTP setups if we need it.
    
    Change-Id: Ifc7485aa60ec53407c38b3d1bec530bdacf50075
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- test/redfish-core/lib/update_service_test.cpp
+++ test/redfish-core/lib/update_service_test.cpp
@@ -16,38 +16,42 @@ TEST(UpdateService, ParseTFTPPostitive)
     crow::Response res;
     {
         // No protocol, schema on url
-        std::optional<TftpUrl> ret = parseTftpUrl("tftp://1.1.1.1/path",
-                                                  std::nullopt, res);
+        std::optional<boost::urls::url> ret =
+            parseSimpleUpdateUrl("tftp://1.1.1.1/path", std::nullopt, res);
         ASSERT_TRUE(ret);
         if (!ret)
         {
             return;
         }
-        EXPECT_EQ(ret->tftpServer, "1.1.1.1");
-        EXPECT_EQ(ret->fwFile, "path");
+        EXPECT_EQ(ret->encoded_host_and_port(), "1.1.1.1");
+        EXPECT_EQ(ret->encoded_path(), "/path");
+        EXPECT_EQ(ret->scheme(), "tftp");
     }
     {
         // Protocol, no schema on url
-        std::optional<TftpUrl> ret = parseTftpUrl("1.1.1.1/path", "TFTP", res);
+        std::optional<boost::urls::url> ret =
+            parseSimpleUpdateUrl("1.1.1.1/path", "TFTP", res);
         ASSERT_TRUE(ret);
         if (!ret)
         {
             return;
         }
-        EXPECT_EQ(ret->tftpServer, "1.1.1.1");
-        EXPECT_EQ(ret->fwFile, "path");
+        EXPECT_EQ(ret->encoded_host_and_port(), "1.1.1.1");
+        EXPECT_EQ(ret->encoded_path(), "/path");
+        EXPECT_EQ(ret->scheme(), "tftp");
     }
     {
         // Both protocl and schema on url
-        std::optional<TftpUrl> ret = parseTftpUrl("tftp://1.1.1.1/path", "TFTP",
-                                                  res);
+        std::optional<boost::urls::url> ret =
+            parseSimpleUpdateUrl("tftp://1.1.1.1/path", "TFTP", res);
         ASSERT_TRUE(ret);
         if (!ret)
         {
             return;
         }
-        EXPECT_EQ(ret->tftpServer, "1.1.1.1");
-        EXPECT_EQ(ret->fwFile, "path");
+        EXPECT_EQ(ret->encoded_host_and_port(), "1.1.1.1");
+        EXPECT_EQ(ret->encoded_path(), "/path");
+        EXPECT_EQ(ret->scheme(), "tftp");
     }
 }
 
@@ -55,16 +59,19 @@ TEST(UpdateService, ParseTFTPNegative)
 {
     crow::Response res;
     // No protocol, no schema
-    ASSERT_EQ(parseTftpUrl("1.1.1.1/path", std::nullopt, res), std::nullopt);
+    ASSERT_EQ(parseSimpleUpdateUrl("1.1.1.1/path", std::nullopt, res),
+              std::nullopt);
     // No host
-    ASSERT_EQ(parseTftpUrl("/path", "TFTP", res), std::nullopt);
+    ASSERT_EQ(parseSimpleUpdateUrl("/path", "TFTP", res), std::nullopt);
 
     // No host
-    ASSERT_EQ(parseTftpUrl("path", "TFTP", res), std::nullopt);
+    ASSERT_EQ(parseSimpleUpdateUrl("path", "TFTP", res), std::nullopt);
 
     // No path
-    ASSERT_EQ(parseTftpUrl("tftp://1.1.1.1", "TFTP", res), std::nullopt);
-    ASSERT_EQ(parseTftpUrl("tftp://1.1.1.1/", "TFTP", res), std::nullopt);
+    ASSERT_EQ(parseSimpleUpdateUrl("tftp://1.1.1.1", "TFTP", res),
+              std::nullopt);
+    ASSERT_EQ(parseSimpleUpdateUrl("tftp://1.1.1.1/", "TFTP", res),
+              std::nullopt);
 }
 } // namespace
 } // namespace redfish
