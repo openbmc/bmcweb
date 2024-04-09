2932a9c4e54874c3286b9cf28043bafca9c69ca9
Ed Tanous <ed@tanous.net>
2024-04-08 17:18:12 -0700

    Figure out which line throws an exception
    Change-Id: I1dd4739e06eb506d68989a066d122109b71b92cd
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- http/logging.hpp
+++ http/logging.hpp
@@ -169,9 +169,10 @@ inline void vlog(std::format_string<Args...> format, Args... args,
     constexpr std::string_view levelString = mapLogLevelFromName[stringIndex];
     std::string_view filename = loc.file_name();
     filename = filename.substr(filename.rfind('/') + 1);
-    std::cout << std::format("[{} {}:{}] ", levelString, filename, loc.line())
-              << std::format(format, std::forward<Args>(args)...) << '\n'
-              << std::flush;
+    std::cout << std::format("[{} {}:{}] ", levelString, filename, loc.line());
+    std::cout << std::format(format, std::forward<Args>(args)...);
+    std::cout << '\n';
+    std::cout << std::flush;
 }
 } // namespace crow
 
@@ -183,7 +184,8 @@ struct BMCWEB_LOG_CRITICAL
                         const std::source_location& loc =
                             std::source_location::current()) noexcept
     {
-        crow::vlog<crow::LogLevel::Critical, Args...>(format, args..., loc);
+        crow::vlog<crow::LogLevel::Critical, Args...>(
+            format, std::forward<Args>(args)..., loc);
     }
 };
 
@@ -195,7 +197,8 @@ struct BMCWEB_LOG_ERROR
                      const std::source_location& loc =
                          std::source_location::current()) noexcept
     {
-        crow::vlog<crow::LogLevel::Error, Args...>(format, args..., loc);
+        crow::vlog<crow::LogLevel::Error, Args...>(
+            format, std::forward<Args>(args)..., loc);
     }
 };
 
@@ -207,7 +210,8 @@ struct BMCWEB_LOG_WARNING
                        const std::source_location& loc =
                            std::source_location::current()) noexcept
     {
-        crow::vlog<crow::LogLevel::Warning, Args...>(format, args..., loc);
+        crow::vlog<crow::LogLevel::Warning, Args...>(
+            format, std::forward<Args>(args)..., loc);
     }
 };
 
@@ -219,7 +223,8 @@ struct BMCWEB_LOG_INFO
                     const std::source_location& loc =
                         std::source_location::current()) noexcept
     {
-        crow::vlog<crow::LogLevel::Info, Args...>(format, args..., loc);
+        crow::vlog<crow::LogLevel::Info, Args...>(
+            format, std::forward<Args>(args)..., loc);
     }
 };
 
@@ -231,7 +236,8 @@ struct BMCWEB_LOG_DEBUG
                      const std::source_location& loc =
                          std::source_location::current()) noexcept
     {
-        crow::vlog<crow::LogLevel::Debug, Args...>(format, args..., loc);
+        crow::vlog<crow::LogLevel::Debug, Args...>(
+            format, std::forward<Args>(args)..., loc);
     }
 };
 
