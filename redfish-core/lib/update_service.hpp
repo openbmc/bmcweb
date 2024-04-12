51d00335d2bb19ca693865d3c09f3a4a2450162f
Ed Tanous <ed@tanous.net>
2024-04-11 22:21:38 -0700

    Fix nullptr failures for image upload
    Several places that call *req.ioService were missing nullptr checks.
    Add them, and fix the one case where it might not be filled in.
    
    Tested:  With HTTP2 enabled, the following command succeeds.
    ```
    curl -k https://192.168.7.2/redfish/v1/UpdateService/update -F 'UpdateParameters={"Targets":["/redfish/v1/Managers/bmc"]} ;type=application/json' --user "root:0penBmc" -F UpdateFile=@/home/ed/bmcweb/16mb.txt -v -H "Expect:"
    ```
    
    Change-Id: I81e7944c22f5922d461bf5d231086c7468a16e62
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -405,6 +405,12 @@ static void monitorForSoftwareAvailable(
         return;
     }
 
+    if (req.ioService == nullptr)
+    {
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
     fwAvailableTimer =
         std::make_unique<boost::asio::steady_timer>(*req.ioService);
 
