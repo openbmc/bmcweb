b4d62b340ae588580c031cbc7d224807bf223049
Ed Tanous <ed@tanous.net>
2024-04-16 08:45:54 -0700

    Store Request Fields that are needed later
    Because of recent changes to how dbus authentication is done, Requests
    might be moved out before they can be used.  This commit is an attempt
    to mitigate the problem without needing to revert that patch.
    
    This commit does two relatively distinct things.
    
    First, it moves basic auth types to a model where they're timed out
    instead of removed on destruction.  This removes the need for a Request
    object to track that state, and arguably gives better behavior, as
    basic auth sessions will survive through the timeout.
    To prevent lots of basic auth sessions getting created, a basic auth
    session is reused if it was:
    1. Created by basic auth previously.
    2. Created by the same user.
    3. Created from the same source IP address.
    
    Second, both connection classes now store the accept, and origin headers
    from the request in the connection class itself, removing the need for
    them.
    
    Tested: HTML page now loads when pointing at a redfish URL with a
    browser.
    
    Change-Id: I623b43cbcbb43d9e65b408853660be09a5edb2b3
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- include/login_routes.hpp
+++ include/login_routes.hpp
@@ -160,11 +160,11 @@ inline void handleLogin(const crow::Request& req,
         }
         else
         {
-            auto session = persistent_data::SessionStore::getInstance()
-                               .generateUserSession(
-                                   username, req.ipAddress, std::nullopt,
-                                   persistent_data::PersistenceType::TIMEOUT,
-                                   isConfigureSelfOnly);
+            auto session =
+                persistent_data::SessionStore::getInstance()
+                    .generateUserSession(username, req.ipAddress, std::nullopt,
+                                         persistent_data::SessionType::Session,
+                                         isConfigureSelfOnly);
 
             asyncResp->res.addHeader(boost::beast::http::field::set_cookie,
                                      "XSRF-TOKEN=" + session->csrfToken +
