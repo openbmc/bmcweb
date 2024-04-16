83d9b7b97171f4762a4c6946e78f19e92cd7fae3
Ed Tanous <ed@tanous.net>
2024-04-16 11:10:17 -0700

    Add missing headers
    Most of these were found by breaking every redfish class handler into
    its own compile unit:
    https://github.com/openbmc/bmcweb/commit/4fdee9e39e9f03122ee16a6fb251a380681f56ac
    
    When that's done, these missing headers become compile errors.
    
    We should just fix them.
    
    Tested: Code compiles.
    
    Change-Id: Ifa27ac4a512362b7ded7cc3068648dc4aea6ad7b
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -19,13 +19,16 @@
 
 #include "app.hpp"
 #include "dbus_utility.hpp"
+#include "error_messages.hpp"
 #include "multipart_parser.hpp"
 #include "ossl_random.hpp"
 #include "query.hpp"
 #include "registries/privilege_registry.hpp"
 #include "task.hpp"
+#include "task_messages.hpp"
 #include "utils/collection.hpp"
 #include "utils/dbus_utils.hpp"
+#include "utils/json_utils.hpp"
 #include "utils/sw_utils.hpp"
 
 #include <boost/system/error_code.hpp>
