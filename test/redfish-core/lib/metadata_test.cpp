c9f212d2913d3dd4f579ee8ba299b10772222b20
Ed Tanous <ed@tanous.net>
2024-05-18 16:07:23 -0700

    Generate metadata at runtime
    In the initial implementation of metadata indexing the bmc knew at
    compile time what schemas it could potentially publish.  bmcweb took the
    approach of adding all schemas of all versions to the $metadata
    resource.  Since that was made, two major changes have happened.
    First, Redfish has added significantly more versions of each schema, as
    well as significantly more schemas to the point where the metadata index
    is now 213KB.  While this file compresses fairly well, the size is
    obvious from the large amount of time that redfish service validator
    takes to parse the schemas, compared to actually acquiring BMC redfish
    resources.
    Second, aggregation was added, where an aggregated Redfish service might
    implement any number of schemas, including OEM ones.
    
    In an effort to fix this, this patch takes the compile-time algorithm in
    update_schemas.py, and moves it into bmcweb itself, parsing the files on
    disk as needed on demand.  This has some immediate benefits;  First, is
    that now schemas can be potentially installed from anywhere, not only
    from within the bmcweb build, and they will be resolved at runtime.
    
    Second, patches that want to add support for a given schema need to only
    symlink the schema into the correct folder, without needing to rerun
    update_schemas.py.  This saves time in review.
    
    Finally, this opens to door to reducing the schema versions present in
    the metadata to the unique set of only what this bmcweb instance, and
    its aggregated BMCs expose.
    
    Tested: Redfish service validator passes.  Need A/B checking to verify
    the file is byte for byte the same.
    GET /redfish/v1/$metadata returns what looks like sane results, with a
    correct content-type.
    
    Change-Id: I326159099c6b6c4056023b2e173c5f074ed88ce1
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ test/redfish-core/lib/metadata_test.cpp
@@ -0,0 +1,52 @@
+#include "file_test_utilities.hpp"
+#include "metadata.hpp"
+
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+/*
+Example from Redfish and OData whitepaper.
+
+https://www.dmtf.org/sites/default/files/standards/documents/DSP2052_1.0.0.pdf
+*/
+constexpr std::string_view content =
+    "<edmx:Edmx xmlns:edms=\"http://docs.oasis-open.org/odata/ns/edmx\" Version=\"4.0\">\n"
+    "  <edmx:Reference Uri=\"http://contoso.org/schemas/ExternalSchema.xml\">\n"
+    "    <edmx:Include Namespace=\"ExternalNamespace\"/>\n"
+    "    <edmx:Include Namespace=\"Other.Namespace\"/>\n"
+    "  </edmx:Reference>\n"
+    "  <edmx:DataServices>\n"
+    "    <Schema xmlns=\"http://docs.oasis-open.org/odata/ns/edm\" Namespace=\"MyNewNamespace\">\n"
+    "      <ComplexType Name=\"MyDataType\">\n"
+    "        <Property Name=\"MyProperty\" Type=\"ExternalNamespace.ReferencedDataType\"/>\n"
+    "        <Property Name=\"MyProperty2\" Type=\"Other.Namespace.OtherDataType\"/>\n"
+    "        <Property Name=\"MyProperty3\" Type=\"Edm.Int64\"/>\n"
+    "      </ComplexType>\n"
+    "    </Schema>\n"
+    "  </edmx:DataServices>\n"
+    "</edmx:Edmx>\n";
+
+TEST(MetadataGet, GetOneFile)
+{
+    TemporaryFileHandle file(content);
+
+    std::filesystem::path path{file.stringPath};
+    EXPECT_EQ(
+        getMetadataPieceForFile(path),
+        std::format("    <edmx:Reference Uri=\"/redfish/v1/schema/{}\">\n"
+                    "        <edmx:Include Namespace=\"MyNewNamespace\"/>\n"
+                    "    </edmx:Reference>\n",
+                    path.filename().string()));
+
+    EXPECT_EQ(getMetadataPieceForFile("DoesNotExist_v1.xml"), "");
+}
+
+} // namespace
+} // namespace redfish
