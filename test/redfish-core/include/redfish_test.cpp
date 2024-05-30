86bea95aa6ed685097b26a7c412bf9ae4b7b96a8
Ed Tanous <ed@tanous.net>
2024-05-30 09:13:43 -0700

    Write test to ensure that redfish validates
    Dependent on configuration options, we might have overlapping paths, as
    what had to be fixed in:
    36a59eafed32c0063d444fc0722f899c0024092d
    and
    5ffd11f248f155614bf30b498cb01a4e2065094d
    
    We should be able to catch these in a unit test, so this commit adds a
    test that initializes the Redfish tree, and ensures that the router
    validates.  In theory this will catch any misconfigurations we might
    have.
    
    Tested: Unit tests pass.
    
    Change-Id: I8721380c96301b57e40819f5ae2e725057ea1bb7
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ test/redfish-core/include/redfish_test.cpp
@@ -0,0 +1,27 @@
+#include "app.hpp"
+#include "redfish.hpp"
+
+#include <boost/asio/io_context.hpp>
+
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+TEST(Redfish, PathsShouldValidate)
+{
+    auto io = std::make_shared<boost::asio::io_context>();
+    crow::App app(io);
+
+    RedfishService redfish(app);
+
+    app.validate();
+}
+
+} // namespace
+} // namespace redfish
