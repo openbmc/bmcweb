60a79efcf07d0bcf67b31729d700d54f3c11afc2
Ed Tanous <ed@tanous.net>
2024-06-05 08:47:30 -0700

    WIP: Allow POST on aggregation sources
    It would be useful for both testing and production if one could create
    aggregation sources dynamically, rather than using detected hardware.
    Redfish sources might exist outside of the physical chassis in a way
    that's detectable, so giving DC software a way to set up a non-trivial
    aggregation topology make this more extensible.
    
    Tested:
    
    Example commands succeed, and return the expected results.
    ```
    curl -vvvv -k --http1.1 --user "root:0penBmc" --request DELETE https://192.168.7.2/redfish/v1/AggregationService/AggregationSources/
    curl -vvvv -k --http1.1 --user "root:0penBmc" https://192.168.7.2/redfish/v1/AggregationService/AggregationSources -H "Content-Type: application/json" --request POST --data '{"HostName":"https://localhost:8000"}'
    ```
    
    Redfish service validator passes.
    
    Change-Id: I15711c3075645291b5f2555eefb80bb80418e7e5
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/lib/aggregation_service.hpp
+++ redfish-core/lib/aggregation_service.hpp
@@ -4,6 +4,7 @@
 #include "error_messages.hpp"
 #include "http_request.hpp"
 #include "http_response.hpp"
+#include "persistent_data.hpp"
 #include "query.hpp"
 #include "redfish_aggregator.hpp"
 #include "registries/privilege_registry.hpp"
@@ -105,7 +106,7 @@ inline void handleAggregationSourceCollectionGet(
     json["Name"] = "Aggregation Source Collection";
 
     // Query D-Bus for satellite configs and add them to the Members array
-    RedfishAggregator::getSatelliteConfigs(
+    RedfishAggregator::getInstance().getSatelliteConfigs(
         std::bind_front(populateAggregationSourceCollection, asyncResp));
 }
 
@@ -192,7 +193,7 @@ inline void handleAggregationSourceGet(
 
     // Query D-Bus for satellite config corresponding to the specified
     // AggregationSource
-    RedfishAggregator::getSatelliteConfigs(std::bind_front(
+    RedfishAggregator::getInstance().getSatelliteConfigs(std::bind_front(
         populateAggregationSource, aggregationSourceId, asyncResp));
 }
 
@@ -214,6 +215,74 @@ inline void handleAggregationSourceHead(
                      aggregationSourceId);
 }
 
+inline void handleAggregationSourceCollectionPost(
+    App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    std::string hostname;
+    // ConfigureSelf accounts can only modify their password
+    if (!json_util::readJsonPatch(req, asyncResp->res, "HostName", hostname))
+    {
+        return;
+    }
+
+    boost::system::result<boost::urls::url> url =
+        boost::urls::parse_absolute_uri(hostname);
+    if (!url)
+    {
+        messages::propertyValueOutOfRange(asyncResp->res, hostname, "HostName");
+        return;
+    }
+    url->normalize();
+    if (url->scheme() != "http" && url->scheme() != "https")
+    {
+        messages::propertyValueOutOfRange(asyncResp->res, hostname, "HostName");
+        return;
+    }
+    crow::utility::setPortDefaults(*url);
+
+    std::string prefix = bmcweb::getRandomIdOfLength(8);
+    RedfishAggregator::getInstance().currentAggregationSources.emplace(prefix,
+                                                                       *url);
+
+    asyncResp->res.addHeader(
+        boost::beast::http::field::location,
+        boost::urls::format("/redfish/v1/AggregationSources/{}", prefix)
+            .buffer());
+
+    messages::created(asyncResp->res);
+}
+
+inline void handleAggregationSourceDelete(
+    App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& aggregationSourceId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/AggregationService/AggregationSource.json>; rel=describedby");
+
+    size_t deleted =
+        RedfishAggregator::getInstance().currentAggregationSources.erase(
+            aggregationSourceId);
+    if (deleted == 0)
+    {
+        messages::resourceNotFound(asyncResp->res, "AggregationSource",
+                                   aggregationSourceId);
+        return;
+    }
+
+    messages::success(asyncResp->res);
+}
+
 inline void requestRoutesAggregationSource(App& app)
 {
     BMCWEB_ROUTE(app,
@@ -221,6 +290,23 @@ inline void requestRoutesAggregationSource(App& app)
         .privileges(redfish::privileges::getAggregationSource)
         .methods(boost::beast::http::verb::get)(
             std::bind_front(handleAggregationSourceGet, std::ref(app)));
+
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/AggregationService/AggregationSources/<str>/")
+        .privileges(redfish::privileges::deleteAggregationSource)
+        .methods(boost::beast::http::verb::delete_)(
+            std::bind_front(handleAggregationSourceDelete, std::ref(app)));
+
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/AggregationService/AggregationSources/<str>/")
+        .privileges(redfish::privileges::headAggregationSource)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handleAggregationSourceHead, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/AggregationService/AggregationSources/")
+        .privileges(redfish::privileges::postAggregationSourceCollection)
+        .methods(boost::beast::http::verb::post)(std::bind_front(
+            handleAggregationSourceCollectionPost, std::ref(app)));
 }
 
 } // namespace redfish
