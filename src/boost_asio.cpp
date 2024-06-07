36c255f410a5e8d6d650b265bf7c14049f22dff0
Ed Tanous <ed@tanous.net>
2024-06-07 15:47:32 -0700

    WIP: Fix cancel logic
    Change-Id: Icd9a8a6cbf000b3a3739973bc304d26532a8d2f3
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- src/boost_asio.cpp
+++ src/boost_asio.cpp
@@ -1,4 +1,5 @@
 #include "logging.hpp"
+#include "stacktrace.hpp"
 
 #include <boost/asio/impl/src.hpp>
 #include <boost/assert/source_location.hpp>
@@ -10,13 +11,15 @@ namespace boost
 void throw_exception(const std::exception& e)
 {
     BMCWEB_LOG_CRITICAL("Boost exception thrown {}", e.what());
-    std::terminate();
+    print_stacktrace();
+    std::abort();
 }
 
 void throw_exception(const std::exception& e, const source_location& loc)
 {
     BMCWEB_LOG_CRITICAL("Boost exception thrown {} from {}:{}", e.what(),
                         loc.file_name(), loc.line());
-    std::terminate();
+    print_stacktrace();
+    std::abort();
 }
 } // namespace boost
