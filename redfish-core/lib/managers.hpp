91f75caf02dbebc549ce95c9a282302169c06ad7
Ed Tanous <ed@tanous.net>
2024-06-10 13:56:43 -0700

    Fix regression in Manager chassis finding
    18f8f60 introduced two regressions where the config was checking
    parent_path() instead of filename, where previously they used ends_with,
    not starts_with.  This was found via inspection.
    
    Testing: Unclear how to exactly test this, but we should at least get
    the code to appear correct in inspection.
    
    [1] https://github.com/openbmc/bmcweb/commit/18f8f608b966c802b3e2a389e3c1ec5a1fd9407b#diff-c49b52746d73fb038cb7892f9204d663efd1b9e4fa6b4dac38c5c9620b2c2944R1495
    
    Change-Id: I45441a6e1549f09e7a570c74d3ee4438ecc07cbe
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/lib/managers.hpp
+++ redfish-core/lib/managers.hpp
@@ -1512,7 +1512,7 @@ struct SetPIDValues : std::enable_shared_from_this<SetPIDValues>
 
                 auto pathItr = std::ranges::find_if(
                     managedObj, [&dbusObjName](const auto& obj) {
-                    return obj.first.parent_path() == dbusObjName;
+                    return obj.first.filename() == dbusObjName;
                 });
                 dbus::utility::DBusPropertiesMap output;
 
@@ -1640,7 +1640,7 @@ struct SetPIDValues : std::enable_shared_from_this<SetPIDValues>
                     bool foundChassis = false;
                     for (const auto& obj : managedObj)
                     {
-                        if (obj.first.parent_path() == chassis)
+                        if (obj.first.filename() == chassis)
                         {
                             chassis = obj.first.str;
                             foundChassis = true;
