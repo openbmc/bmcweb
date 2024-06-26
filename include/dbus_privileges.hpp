openbmc/bmcweb

commit ce22f6099e7e28ae26591348bf484ebedbc1ed42

Expose current Role in Session object
As discussed at https://gerrit.openbmc.org/c/openbmc/webui-vue/+/70758
having that information available to front end is desired for UX
purposes (hiding pages and actions that are not permitted to the current
user anyway).

This field was added in #Session.v1_7_0.Session released on 2023.2.

Tested: current role correctly reported (e.g. "Administrator") as the
only array element, Redfish-Service-Validator passes.

Fixes: #275
Change-Id: Ic9d8f02ca5859cb4b9996fd85b0896ce76ea22aa
Signed-off-by: Paul Fertser <fercerpav@gmail.com>

redfish-core/lib/redfish_sessions.hpp
@@ -15,6 +15,7 @@
 */
 #pragma once
 
+#include "account_service.hpp"
 #include "app.hpp"
 #include "error_messages.hpp"
 #include "http/utility.hpp"
@@ -33,9 +34,12 @@ inline void fillSessionObject(crow::Response& res,
 {
     res.jsonValue["Id"] = session.uniqueId;
     res.jsonValue["UserName"] = session.username;
+    nlohmann::json::array_t roles;
+    roles.emplace_back(redfish::getRoleIdFromPrivilege(session.userRole));
+    res.jsonValue["Roles"] = std::move(roles);
     res.jsonValue["@odata.id"] = boost::urls::format(
         "/redfish/v1/SessionService/Sessions/{}", session.uniqueId);
-    res.jsonValue["@odata.type"] = "#Session.v1_5_0.Session";
+    res.jsonValue["@odata.type"] = "#Session.v1_7_0.Session";
     res.jsonValue["Name"] = "User Session";
     res.jsonValue["Description"] = "Manager User Session";
     res.jsonValue["ClientOriginIPAddress"] = session.clientIp;

commit 7c1e192608425f66e72e0ae14204e278c0df16e4

Fix returning Roles for Sessions POST
When the session is just getting created the normal privileges validation
workflow isn't executed and so the current role remains unknown. Fix this by
refactoring dbus_privileges.hpp to allow obtaining the information from
phosphor-user-manager late in the request processing.

Tested: Redfish Service Validator passes.

Creating a session for local user:
```
$ curl -k -H "Content-Type: application/json" -X POST https://172.41.1.250:18080/redfish/v1/SessionService/Sessions -d '{"UserName":"root", "Password":"0penBmc"}'
{
  "@odata.id": "/redfish/v1/SessionService/Sessions/lfFsCNjshV",
  "@odata.type": "#Session.v1_7_0.Session",
  "ClientOriginIPAddress": "172.40.1.4",
  "Description": "Manager User Session",
  "Id": "lfFsCNjshV",
  "Name": "User Session",
  "Roles": [
    "Administrator"
  ],
  "UserName": "root"
}
```
Creating a session for remote user mapped to Operator:
```
$ curl -k -H "Content-Type: application/json" -X POST https://172.41.1.250:18080/redfish/v1/SessionService/Sessions -d '{"UserName":ldap_sync", "Password":"ldap_password"}'
{
  "@odata.id": "/redfish/v1/SessionService/Sessions/qVffc4ePJK",
  "@odata.type": "#Session.v1_7_0.Session",
  "ClientOriginIPAddress": "172.40.1.4",
  "Description": "Manager User Session",
  "Id": "qVffc4ePJK",
  "Name": "User Session",
  "Roles": [
    "Operator"
  ],
  "UserName": "ldap_sync"
}
```

Fixes: https://github.com/openbmc/bmcweb/issues/280
Fixes: ce22f6099e7e28ae26591348bf484ebedbc1ed42
Change-Id: If76c43563244e3819ee3fbc60d9df7f6a21c1fa3
Signed-off-by: Paul Fertser <fercerpav@gmail.com>

include/dbus_privileges.hpp
@@ -18,14 +18,9 @@ namespace crow
 {
 // Populate session with user information.
 inline bool
-    populateUserInfo(Request& req,
+    populateUserInfo(persistent_data::UserSession& session,
                      const dbus::utility::DBusPropertiesMap& userInfoMap)
 {
-    if (req.session == nullptr)
-    {
-        return false;
-    }
-
     std::string userRole;
     bool remoteUser = false;
     std::optional<bool> passwordExpired;
@@ -49,19 +44,18 @@ inline bool
         return false;
     }
 
-    req.session->userRole = userRole;
-    BMCWEB_LOG_DEBUG("userName = {} userRole = {}", req.session->username,
-                     userRole);
+    session.userRole = userRole;
+    BMCWEB_LOG_DEBUG("userName = {} userRole = {}", session.username, userRole);
 
     // Set isConfigureSelfOnly based on D-Bus results.  This
     // ignores the results from both pamAuthenticateUser and the
     // value from any previous use of this session.
-    req.session->isConfigureSelfOnly = passwordExpired.value_or(false);
+    session.isConfigureSelfOnly = passwordExpired.value_or(false);
 
-    req.session->userGroups.clear();
+    session.userGroups.clear();
     if (userGroups)
     {
-        req.session->userGroups.swap(*userGroups);
+        session.userGroups.swap(*userGroups);
     }
 
     return true;
@@ -106,21 +100,11 @@ inline bool
     return true;
 }
 
-inline bool
-    afterGetUserInfo(Request& req,
-                     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-                     BaseRule& rule, const boost::system::error_code& ec,
-                     const dbus::utility::DBusPropertiesMap& userInfoMap)
+inline bool afterGetUserInfoValidate(
+    Request& req, const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    BaseRule& rule, const dbus::utility::DBusPropertiesMap& userInfoMap)
 {
-    if (ec)
-    {
-        BMCWEB_LOG_ERROR("GetUserInfo failed...");
-        asyncResp->res.result(
-            boost::beast::http::status::internal_server_error);
-        return false;
-    }
-
-    if (!populateUserInfo(req, userInfoMap))
+    if (req.session == nullptr || !populateUserInfo(*req.session, userInfoMap))
     {
         BMCWEB_LOG_ERROR("Failed to populate user information");
         asyncResp->res.result(
@@ -140,6 +124,28 @@ inline bool
 }
 
 template <typename CallbackFn>
+void requestUserInfo(const std::string& username,
+                     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                     CallbackFn&& callback)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, callback = std::forward<CallbackFn>(callback)](
+            const boost::system::error_code& ec,
+            const dbus::utility::DBusPropertiesMap& userInfoMap) mutable {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR("GetUserInfo failed...");
+            asyncResp->res.result(
+                boost::beast::http::status::internal_server_error);
+            return;
+        }
+        callback(userInfoMap);
+        },
+        "xyz.openbmc_project.User.Manager", "/xyz/openbmc_project/user",
+        "xyz.openbmc_project.User.Manager", "GetUserInfo", username);
+}
+
+template <typename CallbackFn>
 void validatePrivilege(const std::shared_ptr<Request>& req,
                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                        BaseRule& rule, CallbackFn&& callback)
@@ -148,18 +154,36 @@ void validatePrivilege(const std::shared_ptr<Request>& req,
     {
         return;
     }
-    std::string username = req->session->username;
-    crow::connections::systemBus->async_method_call(
+
+    requestUserInfo(
+        req->session->username, asyncResp,
         [req, asyncResp, &rule, callback = std::forward<CallbackFn>(callback)](
-            const boost::system::error_code& ec,
             const dbus::utility::DBusPropertiesMap& userInfoMap) mutable {
-        if (afterGetUserInfo(*req, asyncResp, rule, ec, userInfoMap))
+        if (afterGetUserInfoValidate(*req, asyncResp, rule, userInfoMap))
         {
             callback();
         }
-    },
-        "xyz.openbmc_project.User.Manager", "/xyz/openbmc_project/user",
-        "xyz.openbmc_project.User.Manager", "GetUserInfo", username);
+        });
+}
+
+template <typename CallbackFn>
+void getUserInfo(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                 const std::string& username,
+                 persistent_data::UserSession& session, CallbackFn&& callback)
+{
+    requestUserInfo(
+        username, asyncResp,
+        [asyncResp, &session, callback = std::forward<CallbackFn>(callback)](
+            const dbus::utility::DBusPropertiesMap& userInfoMap) {
+        if (!populateUserInfo(session, userInfoMap))
+        {
+            BMCWEB_LOG_ERROR("Failed to populate user information");
+            asyncResp->res.result(
+                boost::beast::http::status::internal_server_error);
+            return;
+        }
+        callback();
+        });
 }
 
 } // namespace crow

redfish-core/lib/redfish_sessions.hpp
@@ -257,7 +257,9 @@ inline void handleSessionCollectionPost(
                                 session->username));
     }
 
-    fillSessionObject(asyncResp->res, *session);
+    crow::getUserInfo(asyncResp, username, *session, [asyncResp, session]() {
+        fillSessionObject(asyncResp->res, *session);
+    });
 }
 inline void handleSessionServiceHead(
     crow::App& app, const crow::Request& req,