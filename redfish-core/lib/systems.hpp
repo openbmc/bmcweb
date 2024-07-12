0d977e5102997cdbef6ba2963cd40d520d8c5f64
Asmitha Karunanithi <asmitk01@in.ibm.com>
2024-07-12 12:52:51 -0500

    Show hypervisor path on GET of Systems collection
    Redfish GET on `/redfish/v1/Systems` is missing to show the hypervisor
    resource in its members list, even though hypervisor is available.
    This commit fixes missing hypervisor resource in a GET response on
    Systems collection. The `Members@odata.count` is retrieved as `uint64_t`
    pointer. But the Json library might store this value as "int" or
    "size_t". Therefore, directly casting it to uint64_t* might not work
    as expected.
    
    Tested By:
    Verified "GET https://${bmc}/redfish/v1/Systems" returns the hypervisor
    resource
    
    Change-Id: If8713fc70663cf72cc555f695b8f4ec6547215a2
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -2817,18 +2817,19 @@ inline void handleComputerSystemCollectionGet(
             BMCWEB_LOG_CRITICAL("Count wasn't found??");
             return;
         }
-        uint64_t* count = val->get_ptr<uint64_t*>();
+        int64_t* count = val->get_ptr<int64_t*>();
         if (count == nullptr)
         {
             BMCWEB_LOG_CRITICAL("Count wasn't found??");
             return;
         }
-        *count = *count + 1;
+        *count++;
         BMCWEB_LOG_DEBUG("Hypervisor is available");
         nlohmann::json& ifaceArray2 = asyncResp->res.jsonValue["Members"];
         nlohmann::json::object_t hypervisor;
         hypervisor["@odata.id"] = "/redfish/v1/Systems/hypervisor";
         ifaceArray2.emplace_back(std::move(hypervisor));
+        asyncResp->res.jsonValue["Members@odata.count"] = count;
     });
 }
 
