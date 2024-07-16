33c76026078c4b4b2928d8c5de0de0672bbfa9c8
Ed Tanous <ed@tanous.net>
2024-07-15 19:09:01 -0700

    WIP: Use std::format
    Change-Id: I71000506573314d6c9326c4677f5fbca1ca02b46
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- http/http_response.hpp
+++ http/http_response.hpp
@@ -222,7 +222,7 @@ struct Response
             return "";
         }
         size_t hashval = std::hash<nlohmann::json>{}(jsonValue);
-        return "\"" + intToHexString(hashval, 8) + "\"";
+        return std::format("\"{:08X}\"", hashval);
     }
 
     void write(std::string&& bodyPart)
@@ -274,7 +274,7 @@ struct Response
             return;
         }
         size_t hashval = std::hash<nlohmann::json>{}(jsonValue);
-        std::string hexVal = "\"" + intToHexString(hashval, 8) + "\"";
+        std::string hexVal = std::format("\"{:08X}\"", hashval);
         addHeader(http::field::etag, hexVal);
         if (expectedHash && hexVal == *expectedHash)
         {
