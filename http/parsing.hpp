b829e070b38374f0d8fcc13503bce2f4c5687db4
Ed Tanous <ed@tanous.net>
2024-07-16 12:28:44 -0700

    WIP: Add range support
    Change-Id: If9dbc5dc0c3093d8f991401cae99db5f9839c1b7
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- http/parsing.hpp
+++ http/parsing.hpp
@@ -4,6 +4,8 @@
 #include "logging.hpp"
 #include "str_utility.hpp"
 
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/home/x3.hpp>
 #include <nlohmann/json.hpp>
 
 #include <algorithm>
@@ -45,4 +47,34 @@ inline JsonParseResult parseRequestAsJson(const crow::Request& req,
     }
 
     return JsonParseResult::Success;
+}
+
+struct ByteRange
+{
+    size_t start = 0;
+    std::optional<size_t> end;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(ByteRange, start, end);
+
+inline std::optional<ByteRange> parseRangeHeader(const crow::Request& req)
+{
+    std::string_view range =
+        req.getHeaderValue(boost::beast::http::field::range);
+    if (range.empty())
+    {
+        return std::nullopt;
+    }
+
+    ByteRange parsedRange;
+    using boost::spirit::x3::lit;
+    using boost::spirit::x3::parse;
+    using boost::spirit::x3::uint64;
+
+    auto parser = lit("bytes ") >> uint64 >> *lit('-') >> uint64;
+    if (!parse(range.begin(), range.end(), parser, parsedRange))
+    {
+        return std::nullopt;
+    }
+    return {parsedRange};
 }
