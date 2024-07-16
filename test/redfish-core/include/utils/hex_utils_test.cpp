45ed9a0cbd0a397a7ac0322e12a0aa53b331d05f
Ed Tanous <etanous@nvidia.com>
2024-07-15 19:09:01 -0700

    Use std::format for hex conversions
    Deprecate intoToHex handler now that we can do everything using
    std::format.
    
    
    Tested: WIP
    
    Change-Id: I71000506573314d6c9326c4677f5fbca1ca02b46
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- test/redfish-core/include/utils/hex_utils_test.cpp
+++ test/redfish-core/include/utils/hex_utils_test.cpp
@@ -15,31 +15,9 @@ namespace
 
 using ::testing::IsEmpty;
 
-TEST(IntToHexString, ReturnsCorrectHexForUint64)
-{
-    EXPECT_EQ(intToHexString(0xFFFFFFFFFFFFFFFFULL, 16), "FFFFFFFFFFFFFFFF");
-
-    EXPECT_EQ(intToHexString(0, 4), "0000");
-    EXPECT_EQ(intToHexString(0, 8), "00000000");
-    EXPECT_EQ(intToHexString(0, 12), "000000000000");
-    EXPECT_EQ(intToHexString(0, 16), "0000000000000000");
-
-    // uint64_t sized ints
-    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 4), "F00D");
-    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 8), "BAD4F00D");
-    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 12), "BEEFBAD4F00D");
-    EXPECT_EQ(intToHexString(0xDEADBEEFBAD4F00DULL, 16), "DEADBEEFBAD4F00D");
-
-    // uint16_t sized ints
-    EXPECT_EQ(intToHexString(0xBEEF, 1), "F");
-    EXPECT_EQ(intToHexString(0xBEEF, 2), "EF");
-    EXPECT_EQ(intToHexString(0xBEEF, 3), "EEF");
-    EXPECT_EQ(intToHexString(0xBEEF, 4), "BEEF");
-}
-
 TEST(BytesToHexString, OnSuccess)
 {
-    EXPECT_EQ(bytesToHexString({0x1a, 0x2b}), "1A2B");
+    EXPECT_EQ(bytesToHexString({{0x1a, 0x2b}}), "1A2B");
 }
 
 TEST(HexCharToNibble, ReturnsCorrectNibbleForEveryHexChar)
