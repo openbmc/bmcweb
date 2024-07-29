3aadf9db75cefb4fca575baa2bc3fb4dd522d89f
Ed Tanous <etanous@nvidia.com>
2024-07-29 14:20:50 -0700

    Fix redfish response for unknown method
    Redfish protocol validator recently added a new test to check for
    invalid methods[1] and expect a valid redfish response.
    
    bmcweb will return the correct response if the HTTP method is known, but
    returns 404 for unknown methods.
    
    This commit implements support for returning the proper code for
    completely unknown methods.
    
    Tested: Redfish protocol validator passes.
    
    ```
    curl -k -X METHODNOTEXIST "https://192.168.7.2/redfish/v1"
    curl --http1.1 -k -X METHODNOTEXIST "https://192.168.7.2/redfish/v1"
    ```
    
    works for both HTTP1 and HTTP2.
    
    [1] https://github.com/DMTF/Redfish-Protocol-Validator/commit/2476e126b818f040c7231de3e9dc4df93ee636a1
    
    Change-Id: Id7436345042bd387d7a7b706acd06afda744ddc4
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- http/http2_connection.hpp
+++ http/http2_connection.hpp
@@ -441,8 +441,7 @@ class HTTP2Connection :
             if (verb == boost::beast::http::verb::unknown)
             {
                 BMCWEB_LOG_ERROR("Unknown http verb {}", valueSv);
-                close();
-                return -1;
+                verb = boost::beast::http::verb::trace;
             }
             thisReq.method(verb);
         }
