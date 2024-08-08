ab34422df9614c520dc12688a6df84c311341294
Ed Tanous <etanous@nvidia.com>
2024-08-07 18:01:23 -0700

    Fix tabbing on readJsonPatch
    Tabbing is wrong.  Fix it.
    
    Tested: Whitespace only.
    
    Change-Id: I4ac63a8bdef4e65ff3a4056bc0fe16e55281222d
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -3196,30 +3196,30 @@ inline void handleComputerSystemPatch(
     std::optional<uint64_t> ipsExitTime;
 
     // clang-format off
-                if (!json_util::readJsonPatch(
-                        req, asyncResp->res,
-                        "IndicatorLED", indicatorLed,
-                        "LocationIndicatorActive", locationIndicatorActive,
-                        "AssetTag", assetTag,
-                        "PowerRestorePolicy", powerRestorePolicy,
-                        "PowerMode", powerMode,
-                        "HostWatchdogTimer/FunctionEnabled", wdtEnable,
-                        "HostWatchdogTimer/TimeoutAction", wdtTimeOutAction,
-                        "Boot/BootSourceOverrideTarget", bootSource,
-                        "Boot/BootSourceOverrideMode", bootType,
-                        "Boot/BootSourceOverrideEnabled", bootEnable,
-                        "Boot/AutomaticRetryConfig", bootAutomaticRetry,
-                        "Boot/AutomaticRetryAttempts", bootAutomaticRetryAttempts,
-                        "Boot/TrustedModuleRequiredToBoot", bootTrustedModuleRequired,
-                        "Boot/StopBootOnFault", stopBootOnFault,
-                        "IdlePowerSaver/Enabled", ipsEnable,
-                        "IdlePowerSaver/EnterUtilizationPercent", ipsEnterUtil,
-                        "IdlePowerSaver/EnterDwellTimeSeconds", ipsEnterTime,
-                        "IdlePowerSaver/ExitUtilizationPercent", ipsExitUtil,
-                        "IdlePowerSaver/ExitDwellTimeSeconds", ipsExitTime))
-                {
-                    return;
-                }
+    if (!json_util::readJsonPatch(
+            req, asyncResp->res,
+            "IndicatorLED", indicatorLed,
+            "LocationIndicatorActive", locationIndicatorActive,
+            "AssetTag", assetTag,
+            "PowerRestorePolicy", powerRestorePolicy,
+            "PowerMode", powerMode,
+            "HostWatchdogTimer/FunctionEnabled", wdtEnable,
+            "HostWatchdogTimer/TimeoutAction", wdtTimeOutAction,
+            "Boot/BootSourceOverrideTarget", bootSource,
+            "Boot/BootSourceOverrideMode", bootType,
+            "Boot/BootSourceOverrideEnabled", bootEnable,
+            "Boot/AutomaticRetryConfig", bootAutomaticRetry,
+            "Boot/AutomaticRetryAttempts", bootAutomaticRetryAttempts,
+            "Boot/TrustedModuleRequiredToBoot", bootTrustedModuleRequired,
+            "Boot/StopBootOnFault", stopBootOnFault,
+            "IdlePowerSaver/Enabled", ipsEnable,
+            "IdlePowerSaver/EnterUtilizationPercent", ipsEnterUtil,
+            "IdlePowerSaver/EnterDwellTimeSeconds", ipsEnterTime,
+            "IdlePowerSaver/ExitUtilizationPercent", ipsExitUtil,
+            "IdlePowerSaver/ExitDwellTimeSeconds", ipsExitTime))
+    {
+        return;
+    }
     // clang-format on
 
     asyncResp->res.result(boost::beast::http::status::no_content);
