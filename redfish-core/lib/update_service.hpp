441f5831ba2d7ba4a2cd1b265f5c60c19cd9d8d5
Jagpal Singh Gill <paligill@gmail.com>
2024-08-12 16:17:58 -0700

    update service: update the updateable BMC
    Update the updateable BMC rather than functional BMC because for dual
    image systems functional BMC is not updated in place.
    
    Tested:
    ```
    > curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/octet-stream" -X POST -T obmc-phosphor-image-romulus-20240529184214.static.mtd.tar https://${bmc}/redfish/v1/UpdateService/update
    {
      "@odata.id": "/redfish/v1/TaskService/Tasks/0",
      "@odata.type": "#Task.v1_4_3.Task",
      "Id": "0",
      "TaskState": "Running",
      "TaskStatus": "OK"
    }
    ```
    
    Change-Id: Ia11bd276ba5ac1d849942b33a7b6c50e74335024
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -945,7 +945,7 @@ inline void processUpdateRequest(
     if (!targets.empty() && targets[0] == BMCWEB_REDFISH_MANAGER_URI_NAME)
     {
         dbus::utility::getAssociationEndPoints(
-            "/xyz/openbmc_project/software/bmc/functional",
+            "/xyz/openbmc_project/software/bmc/updateable",
             [asyncResp, payload = std::move(payload), memfd = std::move(memfd),
              applyTime](
                 const boost::system::error_code& ec,
