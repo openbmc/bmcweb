54318f4efbb89cc522d5f5b5503a411283ea29d2
Oliver Brewka <mox669.dev@gmail.com>
2024-08-12 15:04:41 +0200

    Introducing systems_utils.hpp and getSystemCollectionMembers
    This change introduces a new a function similar to getCollectionMembers
    but specific to computerSystem discovery. Since more
    functionality will be added soon to support multi host redfish
    and in general request handling with the long term goal not using
    hardcoded URIs anymore, a new utility header for systems.hpp is being
    introduced in this patch that will hold the rest of the incoming
    definitions.
    
    getSystemCollectionMembers supports multi host platforms like
    yosemite4 or bletchley. Currently we have to distinguish between single
    and multi host platforms and unfortunately handle both separate
    from each other, thus cannot use the generic getCollectionMembers from
    collection.hpp. However, this should change sometime in the future.
    It should be mentioned, that functionalitywise the newly introduced
    function shares almost all of the code used in getCollectionMembers from
    collection.hpp. Only simplification has been done, since this function
    only serves a single usecase.
    
    Testing: To test the functionality, requests have been manually made
    to check the response for both, single and multi host emulated in qemu.
    For the multi host platform a yosemite4 image with 8 emulated eeproms
    has been used, single host was performed on the same image with only 1
    emulated eeprom that EM would probe against to identify an available
    host.
    
    "single host"
    {
      "@odata.id": "/redfish/v1/Systems",
      "@odata.type": "#ComputerSystemCollection.ComputerSystemCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system"
        }
      ],
      "Members@odata.count": 1,
      "Name": "Computer System Collection"
    }
    
    multi host
    {
      "@odata.id": "/redfish/v1/Systems",
      "@odata.type": "#ComputerSystemCollection.ComputerSystemCollection",
      "Members": [
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_1"
    },
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_2"
    },
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_3"
    },
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_4"
    },
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_5"
    },
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_6"
    },
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_7"
    },
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_T1_Slot_8"
    }
      ],
      "Members@odata.count": 8,
      "Name": "Computer System Collection"
    }%
    
    Change-Id: I82d59487b7c17b22cd638acd8f687f31c96ca156
    Signed-off-by: Oliver Brewka <mox669.dev@gmail.com>

---
+++ redfish-core/include/utils/systems_utils.hpp
@@ -0,0 +1,129 @@
+#pragma once
+
+#include "async_resp.hpp"
+#include "dbus_utility.hpp"
+#include "error_messages.hpp"
+#include "human_sort.hpp"
+
+#include <boost/url/format.hpp>
+
+#include <string_view>
+
+namespace redfish
+{
+
+inline void handleSystemCollectionMembers(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreePathsResponse& objects)
+{
+    if (ec == boost::system::errc::io_error)
+    {
+        asyncResp->res.jsonValue["Members"] = nlohmann::json::array();
+        asyncResp->res.jsonValue["Members@odata.count"] = 0;
+        return;
+    }
+
+    if (ec)
+    {
+        BMCWEB_LOG_DEBUG("DBUS response error {}", ec.value());
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    nlohmann::json& ifaceArray = asyncResp->res.jsonValue["Members"];
+    ifaceArray = nlohmann::json::array();
+
+    if (objects.size() == 1)
+    {
+        asyncResp->res.jsonValue["Members@odata.count"] = 1;
+        nlohmann::json::object_t system;
+        system["@odata.id"] = boost::urls::format(
+            "/redfish/v1/Systems/{}", BMCWEB_REDFISH_SYSTEM_URI_NAME);
+        ifaceArray.emplace_back(std::move(system));
+
+        sdbusplus::asio::getProperty<std::string>(
+            *crow::connections::systemBus, "xyz.openbmc_project.Settings",
+            "/xyz/openbmc_project/network/hypervisor",
+            "xyz.openbmc_project.Network.SystemConfiguration", "HostName",
+            [asyncResp](const boost::system::error_code& ec2,
+                        const std::string& /*hostName*/) {
+                if (ec2)
+                {
+                    return;
+                }
+                auto val = asyncResp->res.jsonValue.find("Members@odata.count");
+                if (val == asyncResp->res.jsonValue.end())
+                {
+                    BMCWEB_LOG_CRITICAL("Count wasn't found??");
+                    return;
+                }
+                int64_t* count = val->get_ptr<int64_t*>();
+                if (count == nullptr)
+                {
+                    BMCWEB_LOG_CRITICAL("Count wasn't found??");
+                    return;
+                }
+                *count = *count + 1;
+                BMCWEB_LOG_DEBUG("Hypervisor is available");
+                nlohmann::json& ifaceArray2 =
+                    asyncResp->res.jsonValue["Members"];
+                nlohmann::json::object_t hypervisor;
+                hypervisor["@odata.id"] = "/redfish/v1/Systems/hypervisor";
+                ifaceArray2.emplace_back(std::move(hypervisor));
+            });
+
+        return;
+    }
+
+    std::vector<std::string> pathNames;
+    for (const auto& object : objects)
+    {
+        sdbusplus::message::object_path path(object);
+        std::string leaf = path.filename();
+        if (leaf.empty())
+        {
+            continue;
+        }
+        pathNames.push_back(leaf);
+    }
+    std::ranges::sort(pathNames, AlphanumLess<std::string>());
+
+    for (const std::string& l : pathNames)
+    {
+        boost::urls::url url("/redfish/v1/Systems");
+        crow::utility::appendUrlPieces(url, l);
+        nlohmann::json::object_t member;
+        member["@odata.id"] = std::move(url);
+        ifaceArray.emplace_back(std::move(member));
+    }
+    asyncResp->res.jsonValue["Members@odata.count"] = ifaceArray.size();
+}
+
+/**
+ * @brief Populate the system collection members from a GetSubTreePaths search
+ * of inventory
+ *
+ * @param[i,o] asyncResp  Async response object
+ * @param[i]   collectionPath  Redfish collection path which is used for the
+ *             Members Redfish Path
+ * @param[i]   interfaces  List of interfaces to constrain the GetSubTree search
+ * @param[in]  subtree     D-Bus base path to constrain search to.
+ * @param[in]  jsonKeyName Key name in which the collection members will be
+ *             stored.
+ *
+ * @return void
+ */
+inline void getSystemCollectionMembers(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    constexpr std::array<std::string_view, 1> interfaces{
+        "xyz.openbmc_project.Inventory.Decorator.ManagedHost"};
+
+    BMCWEB_LOG_DEBUG("Get system collection members for /redfish/v1/Systems");
+
+    dbus::utility::getSubTreePaths(
+        "/xyz/openbmc_project/inventory", 0, interfaces,
+        std::bind_front(handleSystemCollectionMembers, asyncResp));
+}
+} // namespace redfish
