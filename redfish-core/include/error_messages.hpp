25c648c32f7077b5db3ed3b5086305681b7b0c2c
Ed Tanous <etanous@nvidia.com>
2024-08-23 14:53:42 -0700

    Add property error
    Change-Id: Ib5f0e2025e07c9add41e5e8df8288d7e54104ee7
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- redfish-core/include/error_messages.hpp
+++ redfish-core/include/error_messages.hpp
@@ -580,6 +580,19 @@ void propertyValueTypeError(crow::Response& res, const nlohmann::json& arg1,
                             std::string_view arg2);
 
 /**
+ * @brief Formats PropertyValueError message into JSON
+ * Message body: "The value <arg1> for the property <arg2> is of a different
+ * type than the property can accept."
+ *
+ * @param[in] arg1 Parameter of message that will replace %1 in its body.
+ * @param[in] arg2 Parameter of message that will replace %2 in its body.
+ *
+ * @returns Message PropertyValueTypeError formatted to JSON */
+nlohmann::json propertyValueError(std::string_view arg1);
+
+void propertyValueError(crow::Response& res, std::string_view arg1);
+
+/**
  * @brief Formats ResourceNotFound message into JSON
  * Message body: "The requested resource of type <arg1> named <arg2> was not
  * found."
