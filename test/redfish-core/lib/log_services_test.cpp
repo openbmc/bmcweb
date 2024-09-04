98bf7e8b85f7677c017b9411b90f1944fb1a0520
Ed Tanous <etanous@nvidia.com>
2024-09-03 17:03:29 -0700

    WIP: Break out post codes
    Similr to other patches, break out the post codes log services into
    their own file.  log_services.hpp is far too large.
    
    Change-Id: I3cb644d52a9d3b5f9a15a2f90c1b69c87491e5c8
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- test/redfish-core/lib/log_services_test.cpp
+++ test/redfish-core/lib/log_services_test.cpp
@@ -68,34 +68,5 @@ TEST(LogServicesBMCJouralTest, LogServicesBMCJouralGetReturnsError)
     EXPECT_EQ(indexOut, 1);
 }
 
-TEST(LogServicesPostCodeParse, PostCodeParse)
-{
-    uint64_t currentValue = 0;
-    uint16_t index = 0;
-    EXPECT_TRUE(parsePostCode("B1-2", currentValue, index));
-    EXPECT_EQ(currentValue, 2);
-    EXPECT_EQ(index, 1);
-    EXPECT_TRUE(parsePostCode("B200-300", currentValue, index));
-    EXPECT_EQ(currentValue, 300);
-    EXPECT_EQ(index, 200);
-
-    EXPECT_FALSE(parsePostCode("", currentValue, index));
-    EXPECT_FALSE(parsePostCode("B", currentValue, index));
-    EXPECT_FALSE(parsePostCode("B1", currentValue, index));
-    EXPECT_FALSE(parsePostCode("B1-", currentValue, index));
-    EXPECT_FALSE(parsePostCode("B1A-2", currentValue, index));
-    EXPECT_FALSE(parsePostCode("B1A-2", currentValue, index));
-    EXPECT_FALSE(parsePostCode("B1A-2z", currentValue, index));
-    // Uint16_t max + 1
-    EXPECT_FALSE(parsePostCode("B65536-1", currentValue, index));
-
-    // Uint64_t max + 1
-    EXPECT_FALSE(parsePostCode("B1-18446744073709551616", currentValue, index));
-
-    // Negative numbers
-    EXPECT_FALSE(parsePostCode("B-1-2", currentValue, index));
-    EXPECT_FALSE(parsePostCode("B-1--2", currentValue, index));
-}
-
 } // namespace
 } // namespace redfish
