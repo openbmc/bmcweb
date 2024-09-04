9b7b8e022c9f23f840de6949a844c1ca736f636d
Ed Tanous <etanous@nvidia.com>
2024-09-04 10:46:47 -0700

    Add extra subscription params support
    OriginResource allows filtering messages on a per-device basis.  This
    was already listed as supported in our docs.
    
    RegistryPrefixes is also added.
    
    Tested: Unit tests pass.
    
    Change-Id: Idfde8416f2f466ce11957177e052b540fc669888
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- include/persistent_data.hpp
+++ include/persistent_data.hpp
@@ -297,6 +297,7 @@ class ConfigFile
             subscription["MessageIds"] = subValue.registryMsgIds;
             subscription["Protocol"] = subValue.protocol;
             subscription["RegistryPrefixes"] = subValue.registryPrefixes;
+            subscription["OriginResources"] = subValue.originResources;
             subscription["ResourceTypes"] = subValue.resourceTypes;
             subscription["SubscriptionType"] = subValue.subscriptionType;
             subscription["MetricReportDefinitions"] =
