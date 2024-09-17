76833f1994e7fdaf42f86d16181144d33d70459f
Abhilash Raju <abhilash.kollam@gmail.com>
2024-09-17 03:05:49 -0500

    MFA Flow: Utility for MFA flow for first log-in
    In an MFA enabled BMC system we should enforce MFA secret key
    configuration for every user unless the user is bypassed for the MFA.
    This commit adds the GoogleAuthenticator class that do the needed MFA
    configuration checks.Based on the global MFA configuration, user level
    bypass configuration,and status of user's secret key configuration the
    authenticator class deduce if secret key generation is required or not.
    The class calls the continuation function with secrete_key_required
    boolean status
    
    Tested By:
    
    Used Romulus simulator environment to test the first time log-in flow
    This commit depends on the commit
    https://gerrit.openbmc.org/c/openbmc/phosphor-user-manager/+/73053
    
    Change-Id: Id23aef0a77e33bc762cfd4782c9acce33044cb63
    Signed-off-by: Abhilash Raju <abhilash.kollam@gmail.com>

---
+++ include/google_authenticator.hpp
@@ -0,0 +1,128 @@
+#pragma once
+
+#include "registries/privilege_registry.hpp"
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <utility
+#include <boost/system/error_code.hpp>
+
+#include <format>
+namespace bmcweb
+{
+
+struct GoogleAuthenticator :
+    public std::enable_shared_from_this<GoogleAuthenticator>
+{
+    using Handler =
+        std::function<void(const boost::system::error_code& ec, bool)>;
+
+    Handler continuation;
+
+    std::string userName;
+
+    void tryMfa(const std::string& user, Handler handler)
+    {
+        userName = user;
+        continuation = std::move(handler);
+        checkMfa();
+    }
+    void checkBypassCallback(const boost::system::error_code& ec,
+                             const std::string& bypass)
+    {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR("Failed to get BypassMFA {}", ec.message());
+            continuation(ec, false);
+            return;
+        }
+        if (bypass ==
+            "xyz.openbmc_project.User.MultiFactorAuthConfiguration.Type.GoogleAuthenticator")
+        {
+            BMCWEB_LOG_DEBUG("BypassMFA is enabled");
+            continuation(ec, false);
+            return;
+        }
+
+        checkAuthentication();
+    }
+    void checkBypass()
+    {
+        std::string objectPathStr =
+            std::format("/xyz/openbmc_project/user/{}", userName);
+        sdbusplus::asio::getProperty<std::string>(
+            *crow::connections::systemBus, "xyz.openbmc_project.User.Manager",
+            objectPathStr.data(), "xyz.openbmc_project.User.TOTPAuthenticator",
+            "BypassedProtocol",
+            std::bind_front(&GoogleAuthenticator::checkBypassCallback,
+                            this->shared_from_this()));
+    }
+    void checkAuthenticationCallback(const boost::system::error_code& ec,
+                                     bool present)
+    {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR(
+                "Failed to get Google Authenticator SecretKeyIsValidProperty {}",
+                ec.message());
+            continuation(ec, false);
+            return;
+        }
+
+        if (!present)
+        {
+            BMCWEB_LOG_DEBUG("Google Authenticator secret key is not setup");
+            continuation(ec, true);
+            return;
+        }
+        continuation(ec, false);
+    }
+    void checkAuthentication()
+    {
+        std::string objectPathStr =
+            std::format("/xyz/openbmc_project/user/{}", userName);
+        sdbusplus::asio::getProperty<bool>(
+            *crow::connections::systemBus, "xyz.openbmc_project.User.Manager",
+            objectPathStr.data(), "xyz.openbmc_project.User.TOTPAuthenticator",
+            "SecretKeyIsValid",
+            std::bind_front(&GoogleAuthenticator::checkAuthenticationCallback,
+                            this->shared_from_this()));
+    }
+    void checkMfaCallback(const boost::system::error_code& ec,
+                          const std::string& authTypes)
+    {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR("Failed to get Google Authenticator status {}",
+                             ec.message());
+            continuation(ec, false);
+            return;
+        }
+        bool enabled =
+            authTypes.find(
+                "xyz.openbmc_project.User.MultiFactorAuthConfiguration.Type.GoogleAuthenticator") !=
+            std::string::npos;
+
+        if (!enabled)
+        {
+            BMCWEB_LOG_DEBUG(
+                "Google Authenticator is not enabled proceed with session creation");
+            continuation(ec, false);
+            return;
+        }
+        BMCWEB_LOG_DEBUG(
+            "Google Authenticator is enabled check for secret key setup");
+        checkBypass();
+    }
+    void checkMfa()
+    {
+        sdbusplus::asio::getProperty<std::string>(
+            *crow::connections::systemBus, "xyz.openbmc_project.User.Manager",
+            "/xyz/openbmc_project/user",
+            "xyz.openbmc_project.User.MultiFactorAuthConfiguration", "Enabled",
+            std::bind_front(&GoogleAuthenticator::checkMfaCallback,
+                            this->shared_from_this()));
+    }
+};
+} // namespace bmcweb
