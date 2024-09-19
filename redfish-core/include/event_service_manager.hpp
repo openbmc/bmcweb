8d577a390bb10c57b1466776d5bef7c7247fadda
Myung Bae <myungbae@us.ibm.com>
2024-09-19 08:46:45 -0400

    Remove subscription for TerminateAfterRetries policy
    Redfish Data Model [1] section 6.42.5.2 and EventDestination schema [2]
    specify that the subscription is terminated under the following policy
    and the conditions.
    
    DeliveryRetryPolicy:
    - `TerminateAfterRetries`:
    ```
      This value shall indicate the subscription is terminated after the
      maximum number of retries is reached, specified by the
      DeliveryRetryAttempts property in the event service.
    ```
    
    This implements this policy to delete  subscription of the stale client
    connections after trying `DeliveryRetryAttempts` when
    `DeliveryRetryPolicy == TerminateAfterRetries`.
    
    Tested:
    1) Subscription with blocked communication between bmcweb and listener
    
    - Run Redfish Event Listener [3] to subscribe events with
     `DeliveryRetryPolicy == TerminateAfterRetries`
    
    - Check the subscription creation
    ```
     curl -k -X GET https://${bmc}/redfish/v1/EventService/Subscriptions/
    ```
    
    - Generate an event and check the delivery to the listener.
    For example,
    ```
     curl -k  --user "root:0penBmc" -X POST \
      "https://${bmc}/redfish/v1/Managers/bmc/LogServices/Dump/Actions/LogService.CollectDiagnosticData" \
      -H "Content-Type: application/json" -d '{"DiagnosticDataType": "Manager"}'
    ```
    
    - Block the communication between bmcweb and listener
    (or modify the listener not to delete the subscription at its exit, and
    kill the listener so that its subscription is still alive)
    
    - If the above task is already finished, generate more events and wait
    for the sufficient time till `DeliveryRetryAttempts`.
    
    - bmcweb journal log may contain the entries like
    ```
    Sep 20 10:47:55 p10bmc bmcwebd[286]: [ERROR http_client.hpp:444] Maximum number of retries reached. https://9.3.62.209:8080/Redfish-Event-Listener
    Sep 20 10:47:55 p10bmc bmcwebd[286]: [ERROR event_service_manager.hpp:1459]  Subscription 590587653 is deleted after MaxRetryAttempts
    ```
    
    - Check the subscription again if the subscription is deleted.
    
    2) Redfish Validator passes
    
    [1] https://www.dmtf.org/sites/default/files/standards/documents/DSP0268_2024.3.html
    [2] https://github.com/openbmc/bmcweb/blob/878edd599b1706ec8ffe6c3d81ba7cb3534f6393/redfish-core/schema/dmtf/csdl/EventDestination_v1.xml#L857
    [3] https://github.com/DMTF/Redfish-Event-Listener
    
    Change-Id: I6e41288995cbb6e37e17a7ef1be093abb7ce54b9
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/include/event_service_manager.hpp
+++ redfish-core/include/event_service_manager.hpp
@@ -50,6 +50,7 @@ limitations under the License.
 #include <ranges>
 #include <span>
 #include <string>
+#include <string_view>
 
 namespace redfish
 {
@@ -251,7 +252,7 @@ inline int formatEventLogEntry(
 
 } // namespace event_log
 
-class Subscription
+class Subscription : std::enable_shared_from_this<Subscription>
 {
   public:
     Subscription(const Subscription&) = delete;
@@ -275,6 +276,35 @@ class Subscription
 
     ~Subscription() = default;
 
+    // callback for subscription sendData
+    inline void resHandler(const std::shared_ptr<Subscription>& /*unused*/,
+                           const crow::Response& res)
+    {
+        BMCWEB_LOG_DEBUG("Response handled with return code: {}",
+                         res.resultInt());
+
+        if (!client)
+        {
+            BMCWEB_LOG_ERROR(
+                "Http client wasn't filled but http client callback was called.");
+            return;
+        }
+
+        if (userSub.retryPolicy != "TerminateAfterRetries")
+        {
+            return;
+        }
+        if (client->isTerminated())
+        {
+            BMCWEB_LOG_INFO("Subscription {} is deleted after MaxRetryAttempts",
+                            subId);
+            if (deleter)
+            {
+                deleter();
+            }
+        }
+    }
+
     bool sendEventToSubscriber(std::string&& msg)
     {
         persistent_data::EventServiceConfig eventServiceConfig =
@@ -287,11 +317,13 @@ class Subscription
 
         if (client)
         {
-            client->sendData(std::move(msg), userSub.destinationUrl,
-                             static_cast<ensuressl::VerifyCertificate>(
-                                 userSub.verifyCertificate),
-                             userSub.httpHeaders,
-                             boost::beast::http::verb::post);
+            client->sendDataWithCallback(
+                std::move(msg), userSub.destinationUrl,
+                static_cast<ensuressl::VerifyCertificate>(
+                    userSub.verifyCertificate),
+                userSub.httpHeaders, boost::beast::http::verb::post,
+                std::bind_front(&Subscription::resHandler, this,
+                                shared_from_this()));
             return true;
         }
 
@@ -474,6 +506,7 @@ class Subscription
     }
 
     persistent_data::UserSubscription userSub;
+    std::function<void()> deleter;
 
   private:
     std::string subId;
@@ -563,9 +596,13 @@ class EventServiceManager
             }
             std::shared_ptr<Subscription> subValue =
                 std::make_shared<Subscription>(*url, ioc);
+            std::string id = subValue->userSub.id;
+            subValue->deleter = [id]() {
+                EventServiceManager::getInstance().deleteSubscription(id);
+            };
             subValue->userSub = newSub;
 
-            subscriptionsMap.insert(std::pair(subValue->userSub.id, subValue));
+            subscriptionsMap.emplace(id, subValue);
 
             updateNoOfSubscribersCount();
 
