262dcc1ca87af630218644204fc6948f7f592b9f
Alexander Hansen <alexander.hansen@9elements.com>
2024-09-19 12:04:03 +0200

    event logs: introduce struct DbusEventLogEntry
    This creates a partial representation of dbus interface
    xyz.openbmc_project.Logging.Entry as a struct.
    
    There is also a function to extract it from dbus.
    
    Which helps to refactor function fillEventLogLogEntryFromPropertyMap
    to be smaller.
    
    Tested: not tested, simple refactoring. All the data types remain the
    same as before.
    
    Change-Id: Ib32cac967bde487b137ceaf845dfb682e605a175
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ redfish-core/include/utils/dbus_event_log_entry.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "dbus_utility.hpp"
+#include "utils/dbus_utils.hpp"
+
+#include <optional>
+#include <string>
+
+namespace redfish
+{
+struct DbusEventLogEntry
+{
+    // represents a subset of an instance of dbus interface
+    // xyz.openbmc_project.Logging.Entry
+
+    uint32_t Id = 0;
+    std::string Message;
+    const std::string* Path = nullptr;
+    const std::string* Resolution = nullptr;
+    bool Resolved = false;
+    std::string ServiceProviderNotify;
+    std::string Severity;
+    uint64_t Timestamp = 0;
+    uint64_t UpdateTimestamp = 0;
+};
+
+inline std::optional<DbusEventLogEntry> fillDbusEventLogEntryFromPropertyMap(
+    const dbus::utility::DBusPropertiesMap& resp)
+{
+    DbusEventLogEntry entry;
+
+    // clang-format off
+    bool success = sdbusplus::unpackPropertiesNoThrow(
+        dbus_utils::UnpackErrorPrinter(), resp,
+        "Id", entry.Id,
+        "Message", entry.Message,
+        "Path", entry.Path,
+        "Resolution", entry.Resolution,
+        "Resolved", entry.Resolved,
+        "ServiceProviderNotify", entry.ServiceProviderNotify,
+        "Severity", entry.Severity,
+        "Timestamp", entry.Timestamp,
+        "UpdateTimestamp", entry.UpdateTimestamp
+    );
+    // clang-format on
+    if (!success)
+    {
+        return std::nullopt;
+    }
+    return entry;
+}
+} // namespace redfish
