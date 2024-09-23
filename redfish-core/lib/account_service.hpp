fd3f7643bc698d4aaa2f622c6f92ef260cdc9d9a
Asmitha Karunanithi <asmitk01@in.ibm.com>
2024-09-23 04:08:27 -0500

    MFA: Add verifyTOTP redfish action for manager acc
    This commit adds support for the VerifyTOTP Redfish action, enabling
    users to validate their TOTP during the initial MFA setup. Successful
    verification confirms that MFA has been successfully configured for the
    user.
    
    Tested By:
    
    [1] POST https://${bmc}/redfish/v1/AccountService/Accounts/testUser/Actions/ManagerAccount.VerifyTimeBasedOneTimePassword -d '{"TimeBasedOneTimePassword":"<>"}'
    
    Change-Id: I95dbecc107258836f3ec8f8f06b80cd6f91b6f79
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -2111,7 +2111,7 @@ inline void handleAccountHead(
         "</redfish/v1/JsonSchemas/ManagerAccount/ManagerAccount.json>; rel=describedby");
 }
 
-inline void checkAndAddGenerateSecretKeyActionCallback(
+inline void checkAndAddSecretKeyActionsCallback(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const std::string& accountName)
 {
@@ -2119,9 +2119,14 @@ inline void checkAndAddGenerateSecretKeyActionCallback(
     actions["#ManagerAccount.GenerateSecretKey"]["target"] = boost::urls::format(
         "/redfish/v1/AccountService/Accounts/{}/Actions/ManagerAccount.GenerateSecretKey",
         accountName);
+
+    actions["#ManagerAccount.VerifyTimeBasedOneTimePassword"]["target"] =
+        boost::urls::format(
+            "/redfish/v1/AccountService/Accounts/{}/Actions/ManagerAccount.VerifyTimeBasedOneTimePassword",
+            accountName);
 }
 
-inline void checkAndAddGenerateSecretKeyAction(
+inline void checkAndAddSecretKeyActions(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const std::string& accountName)
 {
@@ -2135,7 +2140,7 @@ inline void checkAndAddGenerateSecretKeyAction(
                     accountName);
                 return;
             }
-            checkAndAddGenerateSecretKeyActionCallback(asyncResp, accountName);
+            checkAndAddSecretKeyActionsCallback(asyncResp, accountName);
         },
         "xyz.openbmc_project.ObjectMapper",
         "/xyz/openbmc_project/object_mapper",
@@ -2351,7 +2356,7 @@ inline void handleAccountGet(
             asyncResp->res.jsonValue["Id"] = accountName;
             asyncResp->res.jsonValue["UserName"] = accountName;
 
-            checkAndAddGenerateSecretKeyAction(asyncResp, accountName);
+            checkAndAddSecretKeyActions(asyncResp, accountName);
         });
 }
 
