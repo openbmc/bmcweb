2c64c23c3ae993d289324acf5178edf366491b87
Chandra Harkude <Chandramohan.harkude@gmail.com>
2024-09-23 22:02:35 +0530

    Fix error codes
    Changes added : Added error code for invalid certificate
    
    Problem : When Invalid certificate was passed to replace certificate
    It was throwing error Internal error
    
    Below is the redfishtool log:
    
    redfishtool: Transport: Response Error: status_code: 500 --
    Internal Error
    redfishtool: raw: Error sending POST to resource, aborting
    
    Solution : Return proper error code that is 400 -- Bad Request
    
    Tested : I tested with redfish tool and below is the log with correct error code
    
    redfishtool: Transport: Response Error: status_code: 400 -- Bad Request
       redfishtool: raw: Error sending POST to resource, aborting
    
    Change-Id: I71e7a72a4c156dc80321641f279c20aff4bd6df1
    Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

--- redfish-core/lib/certificate_service.hpp
+++ redfish-core/lib/certificate_service.hpp
@@ -465,6 +465,26 @@ inline void handleCertificateLocationsGet(
                        "/Links/Certificates@odata.count"_json_pointer);
 }
 
+inline void handleError(const std::string_view dbusErrorName,
+                        const std::string& id, const std::string& certificate,
+                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    if (dbusErrorName == "org.freedesktop.DBus.Error.UnknownObject")
+    {
+        messages::resourceNotFound(asyncResp->res, "Certificate", id);
+    }
+    else if (dbusErrorName ==
+             "xyz.openbmc_project.Certs.Error.InvalidCertificate")
+    {
+        messages::propertyValueIncorrect(asyncResp->res, "Certificate",
+                                         certificate);
+    }
+    else
+    {
+        messages::internalError(asyncResp->res);
+    }
+}
+
 inline void handleReplaceCertificateAction(
     App& app, const crow::Request& req,
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
@@ -549,25 +569,27 @@ inline void handleReplaceCertificateAction(
     std::shared_ptr<CertificateFile> certFile =
         std::make_shared<CertificateFile>(certificate);
     crow::connections::systemBus->async_method_call(
-        [asyncResp, certFile, objectPath, service, url{*parsedUrl}, id,
-         name](const boost::system::error_code& ec) {
-            if (ec)
+        [asyncResp, certFile, objectPath, service, url{*parsedUrl}, id, name,
+         certificate](const boost::system::error_code& ec,
+                      sdbusplus::message::message& m) {
+        if (ec)
+        {
+            BMCWEB_LOG_ERROR("DBUS response error: {}", ec);
+            const sd_bus_error* dbusError = m.get_error();
+            if (dbusError && dbusError->name)
             {
-                BMCWEB_LOG_ERROR("DBUS response error: {}", ec);
-                if (ec.value() ==
-                    boost::system::linux_error::bad_request_descriptor)
-                {
-                    messages::resourceNotFound(asyncResp->res, "Certificate",
-                                               id);
-                    return;
-                }
-                messages::internalError(asyncResp->res);
-                return;
+                handleError(dbusError->name, id, certificate, asyncResp);
             }
-            getCertificateProperties(asyncResp, objectPath, service, id, url,
-                                     name);
-            BMCWEB_LOG_DEBUG("HTTPS certificate install file={}",
-                             certFile->getCertFilePath());
+            else
+            {
+                messages::internalError(asyncResp->res);
+            }
+            return;
+        }
+        getCertificateProperties(asyncResp, objectPath, service, id, url,
+                                       name);
+        BMCWEB_LOG_DEBUG("HTTPS certificate install file={}",
+                               certFile->getCertFilePath());
         },
         service, objectPath, certs::certReplaceIntf, "Replace",
         certFile->getCertFilePath());
