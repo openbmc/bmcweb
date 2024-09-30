d651b8f548a8ff7143148b127b4695580578993b
Jishnu CM <jishnunambiarcm@duck.com>
2024-09-30 13:43:13 -0500

    Add "GenerateSecretKeyRequired" registry in bmcweb
    This commit adds the GenerateSecretKeyRequired to the bmcweb registry
    which will indicate if the MFA is enabled and if the user has to
    authenticate with TOTP configuring the secret key generated in a
    device the user posses.
    
    Tested by:
    
    Enable MFA and try to create a session,
    
    Response:
    
    {
      "@Message.ExtendedInfo": [
        {
          "@odata.type": "#Message.v1_1_1.Message",
          "Message": "The Time-based One-Time Password (TOTP) secret key
    for this account must be generated before access is granted. Perform
    the GenerateSecretKey action at URI
    '/redfish/v1/AccountService/Accounts/service' and retain the secret
    key from the response.",
          "MessageArgs": [
            "/redfish/v1/AccountService/Accounts/service"
          ],
          "MessageId": "Base.1.19.0.GenerateSecretKeyRequired",
          "MessageSeverity": "Critical",
          "Resolution": "Generate secret key for this account by performing
    the `GenerateSecretKey` action on the referenced URI and retaining the
    secret key from the action response to produce a Time-based One-Time
    Password (TOTP) for the `Token` property in future session creation
    requests."
        }
      ],
      "@odata.id": "/redfish/v1/SessionService/Sessions/neSLpiOFJz",
      "@odata.type": "#Session.v1_5_0.Session",
      "ClientOriginIPAddress": <ip>,
      "Description": "Manager User Session",
      "Id": "neSLpiOFJz",
      "Name": "User Session",
      "UserName": <user>
    
    Change-Id: Icfeb7486ae76d436a73afde51b0158b369a62ad0
    Signed-off-by: Jishnu CM <jishnunambiarcm@duck.com>

--- include/sessions.hpp
+++ include/sessions.hpp
@@ -44,6 +44,7 @@ struct UserSession
     SessionType sessionType{SessionType::None};
     bool cookieAuth = false;
     bool isConfigureSelfOnly = false;
+    bool isGenerateSecretKeyRequired = false;
     std::string userRole;
     std::vector<std::string> userGroups;
 
@@ -263,7 +264,7 @@ class SessionStore
     std::shared_ptr<UserSession> generateUserSession(
         std::string_view username, const boost::asio::ip::address& clientIp,
         const std::optional<std::string>& clientId, SessionType sessionType,
-        bool isConfigureSelfOnly = false)
+        bool isConfigureSelfOnly = false, bool isGenerateSecretKeyRequired = false)
     {
         // Only need csrf tokens for cookie based auth, token doesn't matter
         std::string sessionToken =
@@ -289,6 +290,7 @@ class SessionStore
             sessionType,
             false,
             isConfigureSelfOnly,
+            isGenerateSecretKeyRequired,
             "",
             {}});
         auto it = authTokens.emplace(sessionToken, session);
