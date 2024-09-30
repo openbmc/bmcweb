9444df26f18daf04ead76664f5f263d1bdcb1a8c
Jishnu CM <jishnunambiarcm@duck.com>
2024-09-30 13:43:13 -0500

    Add "GenerateSecretKeyRequired" registry in bmcweb
    This commit adds the GenerateSecretKeyRequired to the bmcweb registry
    which will indicate if the MFA is enabled and if the user has to
    authenticate with TOTP configuring the secret key generated in a
    device the user posses.
    
    Tested by:
    
    Enable MFA and try to create a session,
    
    Response:
    
    {
      "@Message.ExtendedInfo": [
        {
          "@odata.type": "#Message.v1_1_1.Message",
          "Message": "The Time-based One-Time Password (TOTP) secret key
    for this account must be generated before access is granted. Perform
    the GenerateSecretKey action at URI
    '/redfish/v1/AccountService/Accounts/service' and retain the secret
    key from the response.",
          "MessageArgs": [
            "/redfish/v1/AccountService/Accounts/service"
          ],
          "MessageId": "Base.1.19.0.GenerateSecretKeyRequired",
          "MessageSeverity": "Critical",
          "Resolution": "Generate secret key for this account by performing
    the `GenerateSecretKey` action on the referenced URI and retaining the
    secret key from the action response to produce a Time-based One-Time
    Password (TOTP) for the `Token` property in future session creation
    requests."
        }
      ],
      "@odata.id": "/redfish/v1/SessionService/Sessions/neSLpiOFJz",
      "@odata.type": "#Session.v1_5_0.Session",
      "ClientOriginIPAddress": <ip>,
      "Description": "Manager User Session",
      "Id": "neSLpiOFJz",
      "Name": "User Session",
      "UserName": <user>
    
    Change-Id: Icfeb7486ae76d436a73afde51b0158b369a62ad0
    Signed-off-by: Jishnu CM <jishnunambiarcm@duck.com>

--- redfish-core/lib/redfish_sessions.hpp
+++ redfish-core/lib/redfish_sessions.hpp
@@ -25,8 +25,11 @@ limitations under the License.
 #include "registries/privilege_registry.hpp"
 #include "utils/json_utils.hpp"
 
+#include <boost/system/error_code.hpp>
 #include <boost/url/format.hpp>
 
+#include <functional>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -243,6 +246,19 @@ inline void processAfterSessionCreation(
                       });
 }
 
+inline void checkGoogleAuthenticatorSecretKeyRequired(
+    const std::string& username,
+    std::function<void(const boost::system::error_code& ec, bool)> callback)
+{
+    sdbusplus::message::object_path userPath("/xyz/openbmc_project/user");
+    crow::connections::systemBus->async_method_call(
+        [callback = std::move(callback)](const boost::system::error_code& ec,
+                                         bool val) { callback(ec, val); },
+        "xyz.openbmc_project.User.Manager", userPath,
+        "xyz.openbmc_project.User.TOTPAuthenticatorManager",
+        "IsGenerateSecretKeyRequired", username);
+}
+
 inline void handleSessionCollectionPost(
     crow::App& app, const crow::Request& req,
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
@@ -295,7 +311,7 @@ inline void handleSessionCollectionPost(
         std::shared_ptr<persistent_data::UserSession> session =
             persistent_data::SessionStore::getInstance().generateUserSession(
                 username, req.ipAddress, clientId,
-                persistent_data::SessionType::Session, true);
+                persistent_data::SessionType::Session, true, false);
         if (!session)
         {
             messages::internalError(asyncResp->res);
@@ -305,19 +321,31 @@ inline void handleSessionCollectionPost(
         processAfterSessionCreation(asyncResp, req, session, false);
         return;
     }
-
-    std::shared_ptr<persistent_data::UserSession> session =
-        persistent_data::SessionStore::getInstance().generateUserSession(
-            username, req.ipAddress, clientId,
-            persistent_data::SessionType::Session, false);
-    if (!session)
-    {
-        messages::internalError(asyncResp->res);
-        return;
-    }
-
-    processAfterSessionCreation(asyncResp, req, session, false);
+    // check if secret key generation is required for the user
+    checkGoogleAuthenticatorSecretKeyRequired(
+        username, [username, asyncResp, req, clientId = std::move(clientId)](
+                      const boost::system::error_code& ec, bool required) {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR("secretKeyRequired check failed = {}",
+                                 ec.message());
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            std::shared_ptr<persistent_data::UserSession> session =
+                persistent_data::SessionStore::getInstance()
+                    .generateUserSession(username, req.ipAddress, clientId,
+                                         persistent_data::SessionType::Session,
+                                         required, required);
+            if (!session)
+            {
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            processAfterSessionCreation(asyncResp, req, session, required);
+        });
 }
+
 inline void handleSessionServiceHead(
     crow::App& app, const crow::Request& req,
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
