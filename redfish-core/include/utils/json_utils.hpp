7fcf2c8b92109bb13ed2e4713abae595d8d610fb
Myung Bae <myungbae@us.ibm.com>
2024-10-12 16:07:12 -0500

    Handling of OutOfRange in ReadJson
    Currently readJsonPatch returns `PropertValueNotInList` in case when an
    input integer is out of range.  This change is to return
    `PropertyValueOutOfRange` for the case out-of-range integer input.
    
    Tested:
    
    - Verify PATCH with an out-of-value integer. e.g.
    ```
    $ curl -k -X PATCH https://${bmc}/redfish/v1/EventService/  -H "Content-Type: application/json" -d '{ "DeliveryRetryIntervalSeconds" : 4294967296}'
    ```
    
    Before the change, its `MessageId` is `PropertyValueNotInList`.
    ```
     "Message": "The value '4294967296' for the property DeliveryRetryIntervalSeconds is not in the list of acceptable values.",
     "MessageId": "Base.1.19.0.PropertyValueNotInList",
     "MessageSeverity": "Warning",
     "Resolution": "Choose a value from the enumeration list that the implementation can support and resubmit the request if the operation failed."
    ```
    
    After the change, its `MessageId` will be `PropertyValueOutOfRange`.
    
    ```
     "Message": "The value '4294967296' for the property DeliveryRetryIntervalSeconds is not in the supported range of acceptable values.",
     "MessageId": "Base.1.19.0.PropertyValueOutOfRange",
     "MessageSeverity": "Warning",
     "Resolution": "Correct the value for the property in the request body and resubmit the request if the operation failed."
    ```
    
    - Redfish Service Validator passes
    
    Change-Id: I0d0c5ecbc9f416b68fa7c0e81a0ea896ec2e50af
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/include/utils/json_utils.hpp
+++ redfish-core/include/utils/json_utils.hpp
@@ -330,7 +330,7 @@ bool unpackValue(nlohmann::json& jsonValue, std::string_view key,
             }
             else if (ec == UnpackErrorCode::outOfRange)
             {
-                messages::propertyValueNotInList(res, jsonValue, key);
+                messages::propertyValueOutOfRange(res, jsonValue, key);
             }
             return false;
         }
@@ -346,7 +346,7 @@ bool unpackValue(nlohmann::json& jsonValue, std::string_view key,
             }
             else if (ec == UnpackErrorCode::outOfRange)
             {
-                messages::propertyValueNotInList(res, jsonValue, key);
+                messages::propertyValueOutOfRange(res, jsonValue, key);
             }
             return false;
         }
