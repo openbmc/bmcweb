98737b713f4ba0d5d79ffd3e81bce01058ab2e76
Ivan Moiseev <moiseev.ivan4w@yandex.com>
2024-10-15 16:18:12 +0300

    Manage user password expiration via REST
    This change add user password expiration date time mgmt via REST
    API provided by bmcweb. Password expiration date time is managed as
    string in 'YYYY-MM-DDTHH:MM:SS±hh:mm' format and internally operates as
    Epoch time. When set password expiration date time can be specified in
    any format supported by 'dateStringToEpoch' function in
    'redfish::time_utils'. Value 'null' is used to make password not to
    expire.
    
    Unit tests checking correct password expiration value conversion were
    added.
    
    This change depends on corresponding change in phosphor-dbus-interfaces
    [1] and in phospor-user-manager [2].
    
    Password expiration management:
    - create user with password expiration
    ```
    curl -k -X POST -H 'Content-Type: application/json' \
      "https://<bmc>/redfish/v1/AccountService/Accounts" \
      -d '{"UserName":"<user>", "Password":"<password>", "RoleId":"<role>", "PasswordExpiration": "<YYYY-MM-DDTHH:MM:SS>"}'
    ```
    
    - modify user password expiration
    ```
    curl -k -X PATCH -H 'Content-Type: application/json' \
      https://<bmc>/redfish/v1/AccountService/Accounts/<user> \
      -d '{"PasswordExpiration": "<YYYY-MM-DDTHH:MM:SS>"}'
    ```
    
    - get user password expiration
    ```
      curl -k -X GET https://<bmc>/redfish/v1/AccountService/Accounts/<user>
    ```
    
    - modify user password not to expire
    ```
    curl -k -X PATCH -H 'Content-Type: application/json' \
      https://<bmc>/redfish/v1/AccountService/Accounts/<user> \
      -d '{"PasswordExpiration": null}'
    ```
    
    Tested:
    Functionality of this change was tested via curl utility. Also, it was
    checked that proper value was set on dbus for'PasswordExpiration'
    attribute of the specified user.
    - create user account without password expiration, verify that password
      expiration is not set
    - create user account with password expiration specified, verify that it
      is correct
    - create user account with null password expiration which makes password
      not to expire, verify that it is correct
    - try to create user account with various invalid password expiration
      values(incorrect type, invalid format), verify that user is not
      created and appropriate error is returned in response
    - modify user password expiration to specific time, verify that is is
      correct
    - make user password not to expiry, verify that it is correct
    - try to set password expiration to an invalid value (incorrect type,
      invalid format), verify that is does not change and appropriate error
      is returned in response
    
    Redfish service validation on /redfish/v1/AccountService/Accounts tree
    containing both user accounts with and without password expiration has
    passed successfully.
    
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/75236
    [2] https://gerrit.openbmc.org/c/openbmc/phosphor-user-manager/+/75237
    
    Change-Id: Idf5e4356eaa8866dd4a10664996117e2cdba0684
    Signed-off-by: Ivan Moiseev <moiseev.ivan4w@yandex.com>
    Signed-off-by: Ivan Mikhaylov <fr0st61te@gmail.com>

---
+++ test/redfish-core/lib/account_service_test.cpp
@@ -0,0 +1,66 @@
+#include "account_service.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <cstdint>
+#include <optional>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using ::testing::Eq;
+using ::testing::Optional;
+
+namespace redfish
+{
+namespace
+{
+
+using json = nlohmann::json;
+
+TEST(Conversion, PositiveToUint64)
+{
+    std::optional<uint64_t> passwordExpiration;
+
+    passwordExpiration = passwordExpirationToUint64(nullptr);
+    EXPECT_THAT(passwordExpiration, Optional(Eq(0)));
+
+    passwordExpiration = passwordExpirationToUint64("1970-01-01T00:00:00");
+    EXPECT_THAT(passwordExpiration, Optional(Eq(0)));
+
+    passwordExpiration = passwordExpirationToUint64("2024-10-17T18:12:04");
+    EXPECT_THAT(passwordExpiration, Optional(Eq(1729188724)));
+
+    passwordExpiration = passwordExpirationToUint64("2024-10-17T18:12:04Z");
+    EXPECT_THAT(passwordExpiration, Optional(Eq(1729188724)));
+
+    passwordExpiration =
+        passwordExpirationToUint64("2024-10-17T18:12:04+03:00");
+    EXPECT_THAT(passwordExpiration, Optional(Eq(1729177924)));
+}
+
+TEST(Conversion, NegativeToUint64)
+{
+    EXPECT_EQ(passwordExpirationToUint64("01-02-03"), std::nullopt);
+
+    EXPECT_EQ(passwordExpirationToUint64("1912-02-03"), std::nullopt);
+
+    EXPECT_EQ(passwordExpirationToUint64("2024-10-17T00:00"), std::nullopt);
+
+    EXPECT_EQ(passwordExpirationToUint64("ABC"), std::nullopt);
+}
+
+TEST(Conversion, PositiveToJson)
+{
+    constexpr uint64_t unexpiringPasswordExpiration = 0;
+
+    json value = passwordExpirationToJson(unexpiringPasswordExpiration);
+    EXPECT_TRUE(value.is_null());
+
+    value = passwordExpirationToJson(1729188724);
+    EXPECT_TRUE(value.is_string());
+    EXPECT_EQ(value, R"("2024-10-17T18:12:04+00:00")"_json);
+}
+
+} // namespace
+} // namespace redfish
