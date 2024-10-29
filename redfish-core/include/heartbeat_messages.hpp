252746cd1fbb6231fd8e22a1ce5631f3084f3653
Myung Bae <myungbae@us.ibm.com>
2024-10-29 10:21:26 -0500

    Implement Subscription Heartbeat Logic
    This implements the subscription heartbeat logic which will send the
    message `RedfishServiceFunctional` periodically with the interval of
    `HeartbeatIntervalMinutes` specified in subscription property [1], if
    `SendHeartbeat` is enabled..
    
    Tested:
    
    - Create a subscription via Redfish Event Listener
    - PATCH `SendHeartbeat=true` and `HeartbeatIntervalMinutes` like
    
    ```
    curl -k -X PATCH https://${bmc}/redfish/v1/EventService/Subscriptions/${SUBID} \
            -H "Content-Type: application/json" \
            -d '{"SendHeartbeat":true, "HeartbeatIntervalMinutes":1}'
    ```
    
    - Monitor the Redfish Event Listener and check the periodic message like
    ```
    response_type: POST
    headers: {'Host': '9.3.62.209', 'Content-Length': '230'}
    
    bodydata: {
      "@odata.type": "#Event.v1_4_0.Event",
      "Events": [
        {
          "@odata.type": "#Message.v1_1_1.Message",
          "EventId": 1,
          "EventTimestamp": "2024-11-13T17:20:14+00:00",
          "MemberId": "0",
          "Message": "Redfish service is functional.",
          "MessageArgs": [],
          "MessageId": "HeartbeatEvent.1.0.1.RedfishServiceFunctional",
          "MessageSeverity": "OK",
          "OriginOfCondition": "",
          "Resolution": "None."
        }
      ],
      "Id": 1,
      "Name": "Event Log"
    }
    ```
    
    - Change `SendHeartbeat` to false and see whether the heartbeat message
      is stopped.
    
    - Redfish Service Validator passes
    
    [1]  https://github.com/openbmc/bmcweb/blob/02ea923f13de196726ac2f022766a6f80bee1c0a/redfish-core/schema/dmtf/json-schema/EventDestination.v1_15_0.json#L356
    
    Change-Id: I8682e05f4459940913ba189f1ed016874e38dd4a
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

---
+++ redfish-core/include/heartbeat_messages.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "registries/heartbeat_event_message_registry.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <array>
+namespace redfish::messages
+{
+
+inline nlohmann::json
+    getLogHeartbeat(redfish::registries::heartbeat_event::Index name,
+                    std::span<const std::string_view> args)
+{
+    size_t index = static_cast<size_t>(name);
+    if (index >= redfish::registries::heartbeat_event::registry.size())
+    {
+        return {};
+    }
+    return getLogFromRegistry(redfish::registries::heartbeat_event::header,
+                              redfish::registries::heartbeat_event::registry,
+                              index, args);
+}
+
+/**
+ * @brief Formats RedfishServiceFunctional message into JSON
+ * Message body: "Redfish service is functional."
+ *
+ * @returns Message RedfishServiceFunctional formatted to JSON */
+inline nlohmann::json redfishServiceFunctional()
+{
+    return getLogHeartbeat(
+        registries::heartbeat_event::Index::redfishServiceFunctional, {});
+}
+
+} // namespace redfish::messages
