6722da7ccec2f438e3a0fccda482b6d90b7cfb77
Ed Tanous <etanous@nvidia.com>
2024-11-11 19:03:55 -0800

    WIP: Dbus cache
    Change-Id: I68149cb2039a2b961f431cfad21b84e73352a21b
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ test/include/property_tree_test.cpp
@@ -0,0 +1,33 @@
+
+#include "property_tree.hpp"
+
+#include <optional>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+TEST(PropertyTree, Doubles)
+{
+    dbus::PropertyTree tree;
+    tree.setDoubleValue("/xyz/openbmc_project/sensors/foo", "Value", 1.0);
+    EXPECT_EQ(tree.getDoubleValue("/xyz/openbmc_project/sensors/foo", "Value"),
+              1.0);
+    EXPECT_EQ(
+        tree.getDoubleValue("/xyz/openbmc_project/sensors/foo", "NoExist"),
+        std::nullopt);
+    tree.setDoubleValue("/a/b", "Value", 1.0);
+    EXPECT_EQ(tree.getDoubleValue("/a/b", "Value"), 1.0);
+}
+
+TEST(PropertyTree, Strings)
+{
+    dbus::PropertyTree tree;
+    tree.setStringValue("/xyz/openbmc_project/sensors/foo", "Value", "MyValue");
+    EXPECT_EQ(tree.getStringValue("/xyz/openbmc_project/sensors/foo", "Value"),
+              "MyValue");
+    EXPECT_EQ(
+        tree.getStringValue("/xyz/openbmc_project/sensors/foo", "NoExist"),
+        std::nullopt);
+    tree.setStringValue("/a/b", "Value", "MyValue");
+    EXPECT_EQ(tree.getStringValue("/a/b", "Value"), "MyValue");
+}
