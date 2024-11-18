openbmc/bmcweb

commit 463a0e3e880340a119bf986bab039d1afbe1a432

Explicitly set verify_none
As reported, there are cases where a valid certificate isn't present,
but a browser still prompts for an MTLS cert.  Fix that by explicitly
setting verify_none if strict tls isn't enabled.  Unclear what impacts
this will have elsewhere:

Tested (not yet done on this patch): with a self-signed certificate,
logging into chrome no longer prompts the certificate screen.

Change-Id: Iaf7d25fec15ad547a6c741c9410995e19ba22016
Signed-off-by: Ed Tanous <etanous@nvidia.com>

src/ssl_key_handler.cpp
@@ -545,20 +545,21 @@ std::shared_ptr<boost::asio::ssl::context> getSslServerContext()
     const persistent_data::AuthConfigMethods& c =
         persistent_data::SessionStore::getInstance().getAuthMethodsConfig();
 
-    boost::asio::ssl::verify_mode mode = boost::asio::ssl::verify_peer;
     if (c.tlsStrict)
     {
         BMCWEB_LOG_DEBUG("Setting verify peer");
-        mode |= boost::asio::ssl::verify_fail_if_no_peer_cert;
+        boost::asio::ssl::verify_mode mode =
+            boost::asio::ssl::verify_peer |
+            boost::asio::ssl::verify_fail_if_no_peer_cert;
+        boost::system::error_code ec;
+        sslCtx.set_verify_mode(mode, ec);
+        if (ec)
+        {
+            BMCWEB_LOG_DEBUG("Failed to set verify mode {}", ec.message());
+            return nullptr;
+        }
     }
 
-    boost::system::error_code ec;
-    sslCtx.set_verify_mode(mode, ec);
-    if (ec)
-    {
-        BMCWEB_LOG_DEBUG("Failed to set verify mode {}", ec.message());
-        return nullptr;
-    }
     SSL_CTX_set_options(sslCtx.native_handle(), SSL_OP_NO_RENEGOTIATION);
 
     if constexpr (BMCWEB_EXPERIMENTAL_HTTP2)

http/http_connection.hpp
@@ -108,6 +108,9 @@ class Connection :
             persistent_data::SessionStore::getInstance().getAuthMethodsConfig();
         if (c.tlsStrict)
         {
+            BMCWEB_LOG_DEBUG(
+                "{} TLS is in strict mode, returning preverified as is.",
+                logPtr(this));
             return preverified;
         }
         // If tls strict mode is disabled

commit 3c4757682294715e6bd8b2ce5710e3c348662b5d

ssl_key_handler: enable verify_peer for Meta TLS mode
Meta does not use TLSStrict, due to wanting optional password
authentication, but does need mTLS support.  463a0e3 broke this
functionality in order to fix asking for client certificates on
the webui side.  Revert to the old behavior only for Meta TLS
mode.

Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: Iae2e62faa5e8341c0422ab0521dea340d4e927b2

src/ssl_key_handler.cpp
@@ -559,6 +559,18 @@ std::shared_ptr<boost::asio::ssl::context> getSslServerContext()
             return nullptr;
         }
     }
+    else if constexpr (BMCWEB_META_TLS_COMMON_NAME_PARSING)
+    {
+        BMCWEB_LOG_DEBUG("Setting verify peer");
+        boost::asio::ssl::verify_mode mode = boost::asio::ssl::verify_peer;
+        boost::system::error_code ec;
+        sslCtx.set_verify_mode(mode, ec);
+        if (ec)
+        {
+            BMCWEB_LOG_DEBUG("Failed to set verify mode {}", ec.message());
+            return nullptr;
+        }
+    }
 
     SSL_CTX_set_options(sslCtx.native_handle(), SSL_OP_NO_RENEGOTIATION);
 