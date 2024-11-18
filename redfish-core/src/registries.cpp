6dccdcc909a5503307006a3eb22fbedb30502c3b
Alexander Hansen <alexander.hansen@9elements.com>
2024-11-18 14:38:06 +0100

    Refactor: move registries functions to their file
    There were same functions from namespace 'registries' in
    event_service_manager.hpp. Move them to registries.cpp/hpp.
    
    Tested:
    - Using Redfish Event Listener, test subscriptions and eventing.
    - Redfish Service Validator passes
    
    Change-Id: Id0912f6581637bb4117e67b138122c355256b561
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- redfish-core/src/registries.cpp
+++ redfish-core/src/registries.cpp
@@ -3,6 +3,7 @@
 #include "registries/base_message_registry.hpp"
 #include "registries/openbmc_message_registry.hpp"
 #include "registries/telemetry_message_registry.hpp"
+#include "registries_selector.hpp"
 #include "str_utility.hpp"
 
 #include <algorithm>
@@ -43,22 +44,9 @@ const Message* getMessage(std::string_view messageID)
     const std::string& messageKey = fields[3];
 
     // Find the right registry and check it for the MessageKey
-    if (std::string(base::header.registryPrefix) == registryName)
-    {
-        return getMessageFromRegistry(
-            messageKey, std::span<const MessageEntry>(base::registry));
-    }
-    if (std::string(openbmc::header.registryPrefix) == registryName)
-    {
-        return getMessageFromRegistry(
-            messageKey, std::span<const MessageEntry>(openbmc::registry));
-    }
-    if (std::string(telemetry::header.registryPrefix) == registryName)
-    {
-        return getMessageFromRegistry(
-            messageKey, std::span<const MessageEntry>(telemetry::registry));
-    }
-    return nullptr;
+    // Find the right registry and check it for the MessageKey
+    return getMessageFromRegistry(messageKey,
+                                  getRegistryFromPrefix(registryName));
 }
 
 } // namespace redfish::registries
