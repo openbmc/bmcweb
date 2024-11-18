4cd3637f7157a98169adbd33eb4742a4f2b6d37d
Alexander Hansen <alexander.hansen@9elements.com>
2024-11-18 12:24:35 +0100

    Refactor: extract src/event_log.cpp
    event_service_manager.hpp contains namespace 'event_log' which is
    confusing. Extract it to a separate header and cpp file to have the
    filename match the namespace.
    
    No functional changes have been made to the code.
    
    Tested:
    - Using Redfish Event Listener, test subscriptions and eventing.
    - Redfish Service Validator passes
    
    Change-Id: Ia0bf658b8b46f92aede059d46e8de48f160e073e
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ redfish-core/include/event_log.hpp
@@ -0,0 +1,43 @@
+/*
+Copyright (c) 2020 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+#include <span>
+#include <string>
+#include <string_view>
+
+namespace redfish
+{
+
+namespace event_log
+{
+
+bool getUniqueEntryID(const std::string& logEntry, std::string& entryID);
+
+int getEventLogParams(const std::string& logEntry, std::string& timestamp,
+                      std::string& messageID,
+                      std::vector<std::string>& messageArgs);
+
+int formatEventLogEntry(
+    const std::string& logEntryID, const std::string& messageID,
+    std::span<std::string_view> messageArgs, std::string timestamp,
+    const std::string& customText, nlohmann::json::object_t& logEntryJson);
+
+} // namespace event_log
+
+} // namespace redfish
