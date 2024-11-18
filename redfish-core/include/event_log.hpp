d24fa1bc27e044d597d779d546311a62e4383182
Alexander Hansen <alexander.hansen@9elements.com>
2024-11-18 12:24:35 +0100

    Refactor: extract src/event_log.cpp
    event_service_manager.hpp contains namespace 'event_log' which is
    confusing. Extract it to a separate header and cpp file to have the
    filename match the namespace.
    
    No functional changes have been made to the code.
    
    Tested:
    - Using Redfish Event Listener, test subscriptions and eventing.
    - Redfish Service Validator passes
    
    Change-Id: Ia0bf658b8b46f92aede059d46e8de48f160e073e
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ redfish-core/include/event_log.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+#include <span>
+#include <string>
+#include <string_view>
+
+namespace redfish
+{
+
+namespace event_log
+{
+
+bool getUniqueEntryID(const std::string& logEntry, std::string& entryID);
+
+int getEventLogParams(const std::string& logEntry, std::string& timestamp,
+                      std::string& messageID,
+                      std::vector<std::string>& messageArgs);
+
+int formatEventLogEntry(
+    const std::string& logEntryID, const std::string& messageID,
+    std::span<std::string_view> messageArgs, std::string timestamp,
+    const std::string& customText, nlohmann::json::object_t& logEntryJson);
+
+} // namespace event_log
+
+} // namespace redfish
