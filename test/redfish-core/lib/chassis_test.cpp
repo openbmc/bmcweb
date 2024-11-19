4564f770dcfcd292192e4586561c4554e065e43d
Joseph-Jonathan Salzano <joseph-jonathan.salzano@hp.com>
2024-11-19 12:00:06 -0700

    Dynamically determine ChassisType for Redfish
    The function handleChassisGetSubTree will now handle properties that
    are part of the xyz.openbmc_project.Inventory.Item.Chassis interface.
    At time of development this is only ChassisType. The new function
    "handleChassisProperties" will attempt to get the Type property from
    the interface, translate it to a Redfish standard string and set it as
    the ChassisType value. If the property cannot be found the default
    "RackMount" will be used.
    
    Tested: Added and ran 4 new unit tests. Ran manual tests with
    ChassisType being exposed via dbus in QEMU emulated environments.
    Tested on ASPEED 2600 eval board.
    
    Change-Id: Ibbd048db5007f5154e88495ec6e651a3a2137b06
    Signed-off-by: Joseph-Jonathan Salzano <joseph-jonathan.salzano@hp.com>

--- test/redfish-core/lib/chassis_test.cpp
+++ test/redfish-core/lib/chassis_test.cpp
@@ -1,10 +1,13 @@
 #include "app.hpp"
 #include "async_resp.hpp"
 #include "chassis.hpp"
+#include "dbus_utility.hpp"
+#include "generated/enums/chassis.hpp"
 #include "http_request.hpp"
 #include "http_response.hpp"
 
 #include <boost/beast/core/string_type.hpp>
+#include <boost/beast/http/status.hpp>
 #include <boost/beast/http/verb.hpp>
 #include <nlohmann/json.hpp>
 
@@ -57,6 +60,86 @@ TEST(HandleChassisResetActionInfoGet, StaticAttributesAreExpected)
 
     crow::App app;
     handleChassisResetActionInfoGet(app, request, response, fakeChassis);
+}
+
+TEST(TranslateChassisTypeToRedfish, TranslationsAreExpected)
+{
+    ASSERT_EQ(
+        chassis::ChassisType::Blade,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.Blade"));
+    ASSERT_EQ(
+        chassis::ChassisType::Component,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.Component"));
+    ASSERT_EQ(
+        chassis::ChassisType::Enclosure,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.Enclosure"));
+    ASSERT_EQ(
+        chassis::ChassisType::Module,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.Module"));
+    ASSERT_EQ(
+        chassis::ChassisType::RackMount,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.RackMount"));
+    ASSERT_EQ(
+        chassis::ChassisType::StandAlone,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.StandAlone"));
+    ASSERT_EQ(
+        chassis::ChassisType::StorageEnclosure,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.StorageEnclosure"));
+    ASSERT_EQ(
+        chassis::ChassisType::Zone,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.Zone"));
+    ASSERT_EQ(
+        chassis::ChassisType::Invalid,
+        translateChassisTypeToRedfish(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.Unknown"));
+}
+
+TEST(HandleChassisProperties, TypeFound)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    auto properties = dbus::utility::DBusPropertiesMap();
+    properties.emplace_back(
+        std::string("Type"),
+        dbus::utility::DbusVariantType(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.RackMount"));
+    handleChassisProperties(response, properties);
+    ASSERT_EQ("RackMount", response->res.jsonValue["ChassisType"]);
+
+    response = std::make_shared<bmcweb::AsyncResp>();
+    properties.clear();
+    properties.emplace_back(
+        std::string("Type"),
+        dbus::utility::DbusVariantType(
+            "xyz.openbmc_project.Inventory.Item.Chassis.ChassisType.StandAlone"));
+    handleChassisProperties(response, properties);
+    ASSERT_EQ("StandAlone", response->res.jsonValue["ChassisType"]);
+}
+
+TEST(HandleChassisProperties, FailToGetProperty)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    auto properties = dbus::utility::DBusPropertiesMap();
+    properties.emplace_back(std::string("Type"),
+                            dbus::utility::DbusVariantType(123));
+    handleChassisProperties(response, properties);
+    ASSERT_EQ(boost::beast::http::status::internal_server_error,
+              response->res.result());
+}
+
+TEST(HandleChassisProperties, TypeNotFound)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    auto properties = dbus::utility::DBusPropertiesMap();
+    handleChassisProperties(response, properties);
+    ASSERT_EQ("RackMount", response->res.jsonValue["ChassisType"]);
 }
 
 } // namespace
