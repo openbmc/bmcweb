openbmc/bmcweb

commit e9f120141c8d2a1404660e46ce2126c83521405d

Add unit test for trailing slashes
Common error #9 requires that most urls end in a trailing slash.  Given
the redfish standard, we know that all redfish routes need to end in a
trailing slash, so write a unit test that verifies that is true.

Despite code review, this appears to have snuck into the codebase in 4
different handlers.  Fix those at the same time so the tests pass.

Tested: Unit tests pass.

Change-Id: I0299a7231662725a7100d5308b3977a549b49253
Signed-off-by: Ed Tanous <etanous@nvidia.com>

redfish-core/lib/metadata.hpp
@@ -115,7 +115,7 @@ inline void
 
 inline void requestRoutesMetadata(App& app)
 {
-    BMCWEB_ROUTE(app, "/redfish/v1/$metadata")
+    BMCWEB_ROUTE(app, "/redfish/v1/$metadat/")
         .methods(boost::beast::http::verb::get)(
             std::bind_front(handleMetadataGet, std::ref(app)));
 }

redfish-core/lib/account_service.hpp
@@ -2277,21 +2277,21 @@ inline void requestAccountServiceRoutes(App& app)
 
     BMCWEB_ROUTE(
         app,
-        "/redfish/v1/AccountService/MultiFactorAuth/ClientCertificate/Certificates")
+        "/redfish/v1/AccountService/MultiFactorAuth/ClientCertificate/Certificates/")
         .privileges(redfish::privileges::headCertificateCollection)
         .methods(boost::beast::http::verb::head)(std::bind_front(
             handleAccountServiceClientCertificatesHead, std::ref(app)));
 
     BMCWEB_ROUTE(
         app,
-        "/redfish/v1/AccountService/MultiFactorAuth/ClientCertificate/Certificates")
+        "/redfish/v1/AccountService/MultiFactorAuth/ClientCertificate/Certificates/")
         .privileges(redfish::privileges::getCertificateCollection)
         .methods(boost::beast::http::verb::get)(std::bind_front(
             handleAccountServiceClientCertificatesGet, std::ref(app)));
 
     BMCWEB_ROUTE(
         app,
-        "/redfish/v1/AccountService/MultiFactorAuth/ClientCertificate/Certificates/<str>")
+        "/redfish/v1/AccountService/MultiFactorAuth/ClientCertificate/Certificates/<str>/")
         .privileges(redfish::privileges::headCertificate)
         .methods(boost::beast::http::verb::head)(std::bind_front(
             handleAccountServiceClientCertificatesInstanceHead, std::ref(app)));

test/redfish-core/include/redfish_test.cpp
@@ -4,13 +4,16 @@
 #include <boost/asio/io_context.hpp>
 
 #include <memory>
+#include <string>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 namespace redfish
 {
 namespace
 {
+using ::testing::EndsWith;
 
 TEST(Redfish, PathsShouldValidate)
 {
@@ -20,6 +23,12 @@ TEST(Redfish, PathsShouldValidate)
     RedfishService redfish(app);
 
     app.validate();
+
+    for (const std::string* route : app.getRoutes())
+    {
+        ASSERT_NE(route, nullptr);
+        EXPECT_THAT(*route, EndsWith("/"));
+    }
 }
 
 } // namespace

commit 68dd055b4ff208651b4df3cc130aa50e865da709

Fix regression on metadata uri
e9f120141c8d2a1404660e46ce2126c83521405d caused a regression on this
that can be seen from the find and replace.  Unfortunately service
validator doesn't catch this, but protocol validator does.

Tested: Run redfish protocol validator and see this test is now passing.

Change-Id: Ie90be9e7dd9332924a33f71ee6125b82cb21bcec
Signed-off-by: Ed Tanous <etanous@nvidia.com>

redfish-core/lib/metadata.hpp
@@ -115,7 +115,7 @@ inline void
 
 inline void requestRoutesMetadata(App& app)
 {
-    BMCWEB_ROUTE(app, "/redfish/v1/$metadat/")
+    BMCWEB_ROUTE(app, "/redfish/v1/$metadata/")
         .methods(boost::beast::http::verb::get)(
             std::bind_front(handleMetadataGet, std::ref(app)));
 }