4e5228bab35e05dc446fbf823c5fc949e7aa858d
Ed Tanous <etanous@nvidia.com>
2024-11-21 15:27:04 -0800

    WIP: Cleanup
    EventServiceManager is already too large.  Make it smaller.
    
    Change-Id: Ib3bbf4b3c481e5c9eaf7268f81cde3ade3629067
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ redfish-core/include/filesystem_log_watcher.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <sys/inotify.h>
+
+#include <boost/asio/posix/stream_descriptor.hpp>
+
+#include <optional>
+#include <string_view>
+namespace redfish
+{
+constexpr const char* eventFormatType = "Event";
+constexpr const char* metricReportFormatType = "MetricReport";
+constexpr const char* eventServiceFile =
+    "/var/lib/bmcweb/eventservice_config.json";
+
+constexpr const char* redfishEventLogDir = "/var/log";
+constexpr const char* redfishEventLogFile = "/var/log/redfish";
+constexpr const size_t iEventSize = sizeof(inotify_event);
+
+class FilesystemLogWatcher
+{
+  private:
+    int inotifyFd = -1;
+    int dirWatchDesc = -1;
+    int fileWatchDesc = -1;
+    boost::asio::posix::stream_descriptor inotifyConn;
+    void onINotify(const boost::system::error_code& ec,
+                   std::size_t bytesTransferred);
+    void watchRedfishEventLogFile();
+
+    std::array<char, 1024> readBuffer;
+
+  public:
+    FilesystemLogWatcher(boost::asio::io_context& iocIn);
+};
+} // namespace redfish
