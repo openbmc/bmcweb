a53269a0514531a5bf364eb4f0d421199e696824
Ed Tanous <etanous@nvidia.com>
2024-11-21 15:27:04 -0800

    Break out Dbus and Filesystem events into classes
    EventServiceManager is already too large.  Implement the TODO from run
    that these should be classes, and fix the issue where events are being
    registered on startup, not on a subscription being created.
    
    To accomplish this, this patch takes global state and breaks them out
    into RAII classes from EventServiceManager, one for monitoring DBus
    matches, and one for monitoring filesystem log events using inotify.
    Each of these connect to static methods on EventService that can send
    the relevant events to the user.
    
    Fundamentally, no code within the two new classes is changed, and the
    only changes to event service are made to support creation and
    destruction of the RAII classes.
    
    Tested: WIP
    No TelemetryService tests exist in Redfish.
    
    Change-Id: Ibc91cd1496edf4a080e2d60bfc1a32e00a6c74b8
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- redfish-core/include/filesystem_log_watcher.hpp
+++ redfish-core/include/filesystem_log_watcher.hpp
@@ -1,9 +1,30 @@
 #pragma once
 
-#include <boost/asio/io_context.hpp>
+#include <sys/inotify.h>
 
+#include <boost/asio/posix/stream_descriptor.hpp>
+
+#include <optional>
+#include <string_view>
 namespace redfish
 {
-int startEventLogMonitor(boost::asio::io_context& ioc);
-void stopEventLogMonitor();
+
+constexpr const char* redfishEventLogFile = "/var/log/redfish";
+
+class FilesystemLogWatcher
+{
+  private:
+    int inotifyFd = -1;
+    int dirWatchDesc = -1;
+    int fileWatchDesc = -1;
+    boost::asio::posix::stream_descriptor inotifyConn;
+    void onINotify(const boost::system::error_code& ec,
+                   std::size_t bytesTransferred);
+    void watchRedfishEventLogFile();
+
+    std::array<char, 1024> readBuffer{};
+
+  public:
+    explicit FilesystemLogWatcher(boost::asio::io_context& iocIn);
+};
 } // namespace redfish
