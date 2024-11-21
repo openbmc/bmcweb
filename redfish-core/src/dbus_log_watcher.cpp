08b919eec14d0332f05d34d1fcdecd5a15b6f8a0
Alexander Hansen <alexander.hansen@9elements.com>
2024-11-21 15:27:04 -0800

    event service: dbus log: enable event subscription
    enable the event subscriptions
    
    /redfish/v1/EventService/Subscriptions/
    
    to work for the dbus event log.
    
    So if you are enabling redfish-dbus-log option,
    event subscriptions should work similar to when
    this option is disabled, with one difference:
    
    - 'MessageArgs' property is currently not implemented and cannot be
    found in the returned json.
    
    Tested:
    - Using Redfish Event Listener, test subscriptions and eventing.
    - Manual Test below with the Redfish Event Listener:
    
    1. Created a maximal Event Log Subscription
    
    redfish
    {
    "@odata.id": "/redfish/v1/EventService/Subscriptions/2023893979",
    "@odata.type": "#EventDestination.v1_8_0.EventDestination",
    "Context": "EventLogSubscription",
    "DeliveryRetryPolicy": "TerminateAfterRetries",
    "Destination": "http://${ip}:5000/event-receiver",
    "EventFormatType": "Event",
    "HttpHeaders": [],
    "Id": "2023893979",
    "MessageIds": [],
    "MetricReportDefinitions": [],
    "Name": "Event Destination 2023893979",
    "Protocol": "Redfish",
    "RegistryPrefixes": [],
    "ResourceTypes": [],
    "SubscriptionType": "RedfishEvent",
    "VerifyCertificate": true
    }
    
    which matches on all registries and all message ids.
    
    2. created a new phosphor-logging entry
    
    busctl call xyz.openbmc_project.Logging \
    /xyz/openbmc_project/logging \
    xyz.openbmc_project.Logging.Create \
    Create 'ssa{ss}' \
    OpenBMC.0.1.PowerButtonPressed \
    xyz.openbmc_project.Logging.Entry.Level.Error 0
    
    3. bmcweb picks up this new entry via the dbus match, this can be
    verified by putting bmcweb in debug logging mode.
    
    4. the event log entry makes it through the filtering code
    
    5. the POST request is sent to the subscribed server as expected,
    and contains the same properties as with the file-based backend.
    
    Change-Id: I122e1121389f72e67a998706aeadd052ae607d60
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- redfish-core/src/dbus_log_watcher.cpp
+++ redfish-core/src/dbus_log_watcher.cpp
@@ -2,21 +2,98 @@
 
 #include "dbus_singleton.hpp"
 #include "dbus_utility.hpp"
+#include "event_logs_object_type.hpp"
 #include "event_service_manager.hpp"
 #include "logging.hpp"
 #include "metric_report.hpp"
+#include "utils/dbus_event_log_entry.hpp"
 
 #include <sdbusplus/bus/match.hpp>
 #include <sdbusplus/message.hpp>
 #include <sdbusplus/message/native_types.hpp>
 
 #include <algorithm>
+#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
 
 namespace redfish
 {
+static bool eventLogObjectFromDBus(const dbus::utility::DBusPropertiesMap& map,
+                                   EventLogObjectsType& event)
+{
+    std::optional<DbusEventLogEntry> optEntry =
+        fillDbusEventLogEntryFromPropertyMap(map);
+
+    if (!optEntry.has_value())
+    {
+        BMCWEB_LOG_ERROR(
+            "Could not construct event log entry from dbus properties");
+        return false;
+    }
+    DbusEventLogEntry& entry = optEntry.value();
+    event.id = std::to_string(entry.Id);
+
+    // The order of 'AdditionalData' is not what's specified in an e.g.
+    // busctl call to create the Event Log Entry. So it cannot be used
+    // to map to the message args. Leaving this branch here for it to be
+    // implemented when the mapping is available
+
+    return true;
+}
+
+static void dbusEventLogMatchHandlerSingleEntry(
+    const dbus::utility::DBusPropertiesMap& map)
+{
+    std::vector<EventLogObjectsType> eventRecords;
+    EventLogObjectsType& event = eventRecords.emplace_back();
+    bool success = eventLogObjectFromDBus(map, event);
+    if (!success)
+    {
+        BMCWEB_LOG_ERROR("Could not parse event log entry from dbus");
+        return;
+    }
+
+    BMCWEB_LOG_DEBUG("Found Event Log Entry Id={}, Timestamp={}, Message={}",
+                     event.id, event.timestamp, event.messageId);
+    EventServiceManager::sendEventsToSubs(eventRecords);
+}
+
+static void onDbusEventLogCreated(sdbusplus::message_t& msg)
+{
+    BMCWEB_LOG_DEBUG("Handling new DBus Event Log Entry");
+
+    sdbusplus::message::object_path objectPath;
+    dbus::utility::DBusInterfacesMap interfaces;
+
+    msg.read(objectPath, interfaces);
+
+    for (auto& pair : interfaces)
+    {
+        BMCWEB_LOG_DEBUG("Found dbus interface {}", pair.first);
+        if (pair.first == "xyz.openbmc_project.Logging.Entry")
+        {
+            const dbus::utility::DBusPropertiesMap& map = pair.second;
+            dbusEventLogMatchHandlerSingleEntry(map);
+        }
+    }
+}
+
+const std::string propertiesMatchString =
+    sdbusplus::bus::match::rules::type::signal() +
+    sdbusplus::bus::match::rules::sender("xyz.openbmc_project.Logging") +
+    sdbusplus::bus::match::rules::interface(
+        "org.freedesktop.DBus.ObjectManager") +
+    sdbusplus::bus::match::rules::path("/xyz/openbmc_project/logging") +
+    sdbusplus::bus::match::rules::member("InterfacesAdded");
+
+DbusEventLogMonitor::DbusEventLogMonitor() :
+    dbusEventLogMonitor(*crow::connections::systemBus, propertiesMatchString,
+                        onDbusEventLogCreated)
+
+{}
+
 static void getReadingsForReport(sdbusplus::message_t& msg)
 {
     if (msg.is_method_error())
