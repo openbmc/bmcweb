0a81185b3e725eec90a44a287dc8855e950de7ca
Ed Tanous <etanous@nvidia.com>
2024-11-22 11:47:08 -0800

    Break out dbus telemetry watcher
    EventServiceManager is too large.  Make it smaller by breaking out the
    dbus log watching mechanisims into a class.
    
    No changes are made, with the exception of the addition of
    sendTelemetryReportToSubs to allow sending events from outside the
    class, without needing the subscription list, which should be private.
    
    Tested: On last patch in series.
    
    Change-Id: Idf17886971ddff5dddeabeeb6ae44e733063b909
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ redfish-core/src/dbus_log_watcher.cpp
@@ -0,0 +1,72 @@
+#include "dbus_log_watcher.hpp"
+
+#include "dbus_singleton.hpp"
+#include "dbus_utility.hpp"
+#include "event_logs_object_type.hpp"
+#include "event_service_manager.hpp"
+#include "logging.hpp"
+#include "metric_report.hpp"
+#include "utils/dbus_event_log_entry.hpp"
+
+#include <sdbusplus/bus/match.hpp>
+#include <sdbusplus/message.hpp>
+#include <sdbusplus/message/native_types.hpp>
+
+#include <algorithm>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace redfish
+{
+static void getReadingsForReport(sdbusplus::message_t& msg)
+{
+    if (msg.is_method_error())
+    {
+        BMCWEB_LOG_ERROR("TelemetryMonitor Signal error");
+        return;
+    }
+
+    sdbusplus::message::object_path path(msg.get_path());
+    std::string id = path.filename();
+    if (id.empty())
+    {
+        BMCWEB_LOG_ERROR("Failed to get Id from path");
+        return;
+    }
+
+    std::string interface;
+    dbus::utility::DBusPropertiesMap props;
+    std::vector<std::string> invalidProps;
+    msg.read(interface, props, invalidProps);
+
+    auto found = std::ranges::find_if(props, [](const auto& x) {
+        return x.first == "Readings";
+    });
+    if (found == props.end())
+    {
+        BMCWEB_LOG_INFO("Failed to get Readings from Report properties");
+        return;
+    }
+
+    const telemetry::TimestampReadings* readings =
+        std::get_if<telemetry::TimestampReadings>(&found->second);
+    if (readings == nullptr)
+    {
+        BMCWEB_LOG_INFO("Failed to get Readings from Report properties");
+        return;
+    }
+    EventServiceManager::sendTelemetryReportToSubs(id, *readings);
+}
+
+const std::string telemetryMatchStr =
+    "type='signal',member='PropertiesChanged',"
+    "interface='org.freedesktop.DBus.Properties',"
+    "arg0=xyz.openbmc_project.Telemetry.Report";
+
+DbusTelemetryMonitor::DbusTelemetryMonitor() :
+    matchTelemetryMonitor(*crow::connections::systemBus, telemetryMatchStr,
+                          getReadingsForReport)
+{}
+} // namespace redfish
