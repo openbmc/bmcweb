86c1ad10432e3740e99429edc6aa4c57565b9e5d
Oliver Brewka <oliver.brewka@9elements.com>
2024-12-02 17:52:17 +0100

    Multi-host support for PostCodes LogService
    Add support for multi-host for all GET and POST method requests under
    
    /redfish/v1/Systems/{computerSystemId}/LogServices/PostCodes/
    
    redfish resource.
    
    Multi-host meson options needed:
    -Dexperimental-redfish-multi-computer-system=enabled
    
    Tested:
    Redfish-service-validation passes on single-host machine as well as on
    yv4 in qemu.
    
    Every postcode entry is displayed in web-ui on single-host and shown
    in the curl output when requested on yv4 qemu.
    
    ```
    curl -w "@curl-format.txt" -c cjar -b cjar -k -X GET
        'https://'"${BMC}"':4443/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_Slot_1_Chassis/LogServices/PostCodes/Entries' \
        -H 'X-Auth-Token: '"$BMCWEB_SESSION_TOKEN"''
    
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_Slot_1_Chassis/LogServices/PostCodes/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of POST Code Log Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_Slot_1_Chassis/LogServices/PostCodes/Entries/B1-1",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "Created": "2025-04-22T18:30:51.528798+00:00",
          "EntryType": "Event",
          "Id": "B1-1",
          "Message": "Boot Count: 1; Time Stamp Offset: 0.0000 seconds; POST Code: 0xA1",
          "MessageArgs": [
            "1",
            "0.0000",
            "0xA1"
          ],
          "MessageId": "OpenBMC.0.2.BIOSPOSTCode",
          "Name": "POST Code Log Entry",
          "Severity": "OK"
        },
    
        ...
    
        "Members@odata.count": 819,
        "Name": "BIOS POST Code Log Entries"
    }
    
    curl -w "@curl-format.txt" -c cjar -b cjar -k -X GET
        'https://'"${BMC}"':4443/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_Slot_1_Chassis/LogServices/PostCodes/Entries/B1-1' \
        -H 'X-Auth-Token: '"$BMCWEB_SESSION_TOKEN"''
    
    {
      "@odata.id": "/redfish/v1/Systems/Yosemite_4_Sentinel_Dome_Slot_1_Chassis/LogServices/PostCodes/Entries/B1-1",
      "@odata.type": "#LogEntry.v1_9_0.LogEntry",
      "Created": "2025-04-22T18:30:51.528798+00:00",
      "EntryType": "Event",
      "Id": "B1-1",
      "Message": "Boot Count: 1; Time Stamp Offset: 0.0000 seconds; POST Code: 0xA1",
      "MessageArgs": [
        "1",
        "0.0000",
        "0xA1"
      ],
      "MessageId": "OpenBMC.0.2.BIOSPOSTCode",
      "Name": "POST Code Log Entry",
      "Severity": "OK"
    }
    ```
    
    Validator
    
    +--------------+--------------+--------------+--------------+
    |     PASS     |     WARN     |     FAIL     |  NOT TESTED  |
    +--------------+--------------+--------------+--------------+
    |     558      |      0       |      0       |     973      |
    +--------------+--------------+--------------+--------------+
    
    POST has been tested on single-host hardware inside web-ui and yv4 qemu
    machine with curl. (Postcodes have been copied over to the qemu machine
    manually from a single-host machine). No regressions observed.
    The postcode dir for the specific host is cleared successfully
    on both single- and multi-host machine after the POST.
    
    Change-Id: Ie04cb160a1f2756a04be68e6675a6cecc5f09117
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -94,7 +94,7 @@ inline void parseCrashdumpParameters(
 
 inline void afterGetPostCodeService(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-    const boost::system::error_code& ec,
+    const std::string& systemName, const boost::system::error_code& ec,
     const dbus::utility::MapperGetSubTreePathsResponse& subtreePath)
 {
     if (ec)
@@ -112,8 +112,7 @@ inline void afterGetPostCodeService(
                 asyncResp->res.jsonValue["Members"];
             nlohmann::json::object_t member;
             member["@odata.id"] = boost::urls::format(
-                "/redfish/v1/Systems/{}/LogServices/PostCodes",
-                BMCWEB_REDFISH_SYSTEM_URI_NAME);
+                "/redfish/v1/Systems/{}/LogServices/PostCodes", systemName);
 
             logServiceArrayLocal.emplace_back(std::move(member));
 
@@ -151,8 +150,8 @@ inline void handleSystemsLogServiceCollectionGet(
     // because it has a duplicate entry for members
     asyncResp->res.jsonValue["@odata.type"] =
         "#LogServiceCollection.LogServiceCollection";
-    asyncResp->res.jsonValue["@odata.id"] = boost::urls::format(
-        "/redfish/v1/Systems/{}/LogServices", BMCWEB_REDFISH_SYSTEM_URI_NAME);
+    asyncResp->res.jsonValue["@odata.id"] =
+        boost::urls::format("/redfish/v1/Systems/{}/LogServices", systemName);
     asyncResp->res.jsonValue["Name"] = "System Log Services Collection";
     asyncResp->res.jsonValue["Description"] =
         "Collection of LogServices for this Computer System";
@@ -201,7 +200,7 @@ inline void handleSystemsLogServiceCollectionGet(
         "xyz.openbmc_project.State.Boot.PostCode"};
     dbus::utility::getSubTreePaths(
         "/", 0, interfaces,
-        std::bind_front(afterGetPostCodeService, asyncResp));
+        std::bind_front(afterGetPostCodeService, asyncResp, systemName));
 }
 
 inline void handleManagersLogServicesCollectionGet(
