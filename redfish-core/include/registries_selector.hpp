af3140bcc01ac94fa639721963b6fe8313555ece
Myung Bae <myungbae@us.ibm.com>
2024-12-04 10:08:59 -0600

    Refactor Message Header and Url Selector
    This is to refactor the access of header and url parts from
    MessageRegistry to reduce the replicated comparison of the registry
    name.
    
    Tested:
    -  GET /redfish/v1/Registries/<str>/<str>  like
    ```
    - /redfish/v1/Registries/Base/Base
    - /redfish/v1/Registries/TaskEvent/TaskEvent
    - /redfish/v1/Registries/ResourceEvent/ResourceEvent
    - /redfish/v1/Registries/OpenBMC/OpenBMC
    - /redfish/v1/Registries/Telemetry/Telemetry
    ```
    
    Change-Id: Id5806343709084292273e8021e0d0b4a114ac06f
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/include/registries_selector.hpp
+++ redfish-core/include/registries_selector.hpp
@@ -1,27 +1,89 @@
 #pragma once
+#include "registries.hpp"
 #include "registries/base_message_registry.hpp"
+#include "registries/heartbeat_event_message_registry.hpp"
+#include "registries/license_message_registry.hpp"
 #include "registries/openbmc_message_registry.hpp"
+#include "registries/resource_event_message_registry.hpp"
 #include "registries/task_event_message_registry.hpp"
+#include "registries/telemetry_message_registry.hpp"
 
+#include <optional>
 #include <span>
 #include <string_view>
 
 namespace redfish::registries
 {
+struct HeaderAndUrl
+{
+    const Header& header;
+    const char* url;
+};
+
+inline std::optional<registries::HeaderAndUrl>
+    getRegistryHeaderAndUrlFromPrefix(std::string_view registryName)
+{
+    if (base::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{base::header, base::url};
+    }
+    if (heartbeat_event::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{heartbeat_event::header, heartbeat_event::url};
+    }
+    if (license::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{license::header, license::url};
+    }
+    if (openbmc::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{openbmc::header, openbmc::url};
+    }
+    if (resource_event::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{resource_event::header, resource_event::url};
+    }
+    if (task_event::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{task_event::header, task_event::url};
+    }
+    if (telemetry::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{telemetry::header, telemetry::url};
+    }
+    return std::nullopt;
+}
+
 inline std::span<const MessageEntry>
     getRegistryFromPrefix(std::string_view registryName)
 {
-    if (task_event::header.registryPrefix == registryName)
+    if (base::header.registryPrefix == registryName)
     {
-        return {task_event::registry};
+        return {base::registry};
+    }
+    if (heartbeat_event::header.registryPrefix == registryName)
+    {
+        return {heartbeat_event::registry};
+    }
+    if (license::header.registryPrefix == registryName)
+    {
+        return {license::registry};
     }
     if (openbmc::header.registryPrefix == registryName)
     {
         return {openbmc::registry};
     }
-    if (base::header.registryPrefix == registryName)
+    if (resource_event::header.registryPrefix == registryName)
     {
-        return {base::registry};
+        return {resource_event::registry};
+    }
+    if (task_event::header.registryPrefix == registryName)
+    {
+        return {task_event::registry};
+    }
+    if (telemetry::header.registryPrefix == registryName)
+    {
+        return {telemetry::registry};
     }
     return {openbmc::registry};
 }
