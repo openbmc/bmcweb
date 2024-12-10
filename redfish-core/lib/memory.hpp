d9bf7d7ad14c2703bf663394b813fc7e45bf2435
Gunnar Mills <gmills@us.ibm.com>
2024-12-10 07:28:11 -0700

    Dimm: Look at Pretty Name (#973) (#1111)
    https://github.com/ibm-openbmc/bmcweb/pull/542 added PrettyName for
    Processors, Assembly, Chassis, Storage but not Memory. I believe this
    was due to not needed it for Memory at the time. We want it now so the
    GUI can use on the inventory page.
    
    Part of 602233.
    
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- redfish-core/lib/memory.hpp
+++ redfish-core/lib/memory.hpp
@@ -34,6 +34,7 @@
 #include <nlohmann/json.hpp>
 #include <sdbusplus/asio/property.hpp>
 #include <sdbusplus/unpack_properties.hpp>
+#include <utils/name_utils.hpp>
 
 #include <array>
 #include <cstdint>
@@ -414,7 +415,6 @@ inline void assembleDimmProperties(
     const nlohmann::json::json_pointer& jsonPtr)
 {
     asyncResp->res.jsonValue[jsonPtr]["Id"] = dimmId;
-    asyncResp->res.jsonValue[jsonPtr]["Name"] = "DIMM Slot";
     asyncResp->res.jsonValue[jsonPtr]["Status"]["State"] =
         resource::State::Enabled;
     asyncResp->res.jsonValue[jsonPtr]["Status"]["Health"] =
@@ -803,6 +803,8 @@ inline void handleGetDimmData(
                     dimmInterface = true;
                     getDimmDataByService(asyncResp, dimmId, serviceName,
                                          objectPath);
+                    name_util::getPrettyName(asyncResp, objectPath, serviceMap,
+                                             "/Name"_json_pointer);
                 }
                 else if (
                     interface ==
