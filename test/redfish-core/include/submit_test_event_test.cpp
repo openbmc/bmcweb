e4b8a630a2b1a02bed5831358dad7364dc130993
Chandramohan Harkude <Chandramohan.harkude@gmail.com>
2024-12-20 19:22:12 +0530

    Update Submit Test event feature to send custom data
    Changes Added : Updated the submit test event feature to send test
    data as per spec
    
    https://www.dmtf.org/sites/default/files/standards/documents/
    DSP2046_2019.1.pdf
    
    SubmitTestEvent section
    
    Testing :
    
    Tested sending custom test data
    and same data received at the event listener
    Change-Id: I2c2363a676aafd39c121c9fe4e16402c0f5961e2
    Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

---
+++ test/redfish-core/include/submit_test_event_test.cpp
@@ -0,0 +1,72 @@
+#include "event_service_store.hpp"
+#include "subscription.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <string_view>
+
+#include <gtest/gtest.h>
+namespace redfish
+{
+static TestEvent createTestEvent()
+{
+    TestEvent testEvent;
+    testEvent.eventGroupId = 1;
+    testEvent.eventId = "dummyEvent";
+    testEvent.eventTimestamp = "2021-01";
+    testEvent.message = "Test Message";
+    testEvent.messageArgs = std::vector<std::string>{"arg1", "arg2"};
+    testEvent.messageId = "Dummy message ID";
+    testEvent.originOfCondition = "/redfish/v1/Chassis/GPU_SXM_1";
+    testEvent.resolution = "custom resolution";
+    testEvent.severity = "whatever";
+    return testEvent;
+}
+
+TEST(Subscription, submitTestEVent)
+{
+    boost::asio::io_context io;
+    boost::urls::url url;
+    auto userSubscription =
+        std::make_shared<persistent_data::UserSubscription>();
+    {
+        auto sub = std::make_shared<Subscription>(userSubscription, url, io);
+        TestEvent testEvent;
+        EXPECT_TRUE(sub->sendTestEventLog(testEvent));
+    }
+    {
+        auto sub = std::make_shared<Subscription>(userSubscription, url, io);
+        TestEvent testEvent;
+        testEvent.eventGroupId = 1;
+        testEvent.eventId = "GPU-RST-RECOMM-EVENT";
+        testEvent.eventTimestamp = "2021-01-01T00:00:00Z";
+        testEvent.message = "Custom Message";
+        testEvent.messageArgs =
+            std::vector<std::string>{"GPU_SXM_1", "Restart Recommended"};
+        testEvent.messageId = "Base.1.13.ResetRecommended";
+        testEvent.originOfCondition = "/redfish/v1/Chassis/GPU_SXM_1";
+        testEvent.resolution =
+            "Reset the GPU at the next service window since the ECC errors are contained";
+        testEvent.severity = "Informational";
+        EXPECT_TRUE(sub->sendTestEventLog(testEvent));
+    }
+    {
+        auto sub = std::make_shared<Subscription>(userSubscription, url, io);
+        TestEvent testEvent = createTestEvent();
+
+        bool result = sub->sendTestEventLog(testEvent);
+
+        EXPECT_TRUE(result);
+
+        EXPECT_EQ(testEvent.eventGroupId.value(), 1);
+        EXPECT_EQ(testEvent.eventId.value(), "dummyEvent");
+        EXPECT_EQ(testEvent.eventTimestamp.value(), "2021-01");
+        EXPECT_EQ(testEvent.message.value(), "Test Message");
+        EXPECT_EQ(testEvent.messageId.value(), "Dummy message ID");
+        EXPECT_EQ(testEvent.originOfCondition.value(),
+                  "/redfish/v1/Chassis/GPU_SXM_1");
+        EXPECT_EQ(testEvent.resolution.value(), "custom resolution");
+        EXPECT_EQ(testEvent.severity.value(), "whatever");
+    }
+}
+} // namespace redfish
