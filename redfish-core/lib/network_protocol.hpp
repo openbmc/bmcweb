openbmc/bmcweb

commit 87c449664e5375abb040af6fad63ef965c311bec

Move to Redfish setProperty call
This commit changes sdbusplus setProperty calls (in various files) to
setDbusProperty method in Redfish namespace that handles all DBus
errors in a consistent manner.

It also handles and translates additional DBus errors to Redfish
specific errors in dbus_utils file.

Tested By:
Not tested yet

Change-Id: If440774879413754f4c24f9b6572c3c9fa1fd033
Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

redfish-core/lib/network_protocol.hpp
@@ -252,16 +252,11 @@ inline void handleNTPProtocolEnabled(
             "xyz.openbmc_project.Time.Synchronization.Method.Manual";
     }
 
-    sdbusplus::asio::setProperty(
-        *crow::connections::systemBus, "xyz.openbmc_project.Settings",
-        "/xyz/openbmc_project/time/sync_method",
-        "xyz.openbmc_project.Time.Synchronization", "TimeSyncMethod",
-        timeSyncMethod, [asyncResp](const boost::system::error_code& ec) {
-        if (ec)
-        {
-            messages::internalError(asyncResp->res);
-        }
-    });
+    setDbusProperty(asyncResp, "xyz.openbmc_project.Settings",
+                    sdbusplus::message::object_path(
+                        "/xyz/openbmc_project/time/sync_method"),
+                    "xyz.openbmc_project.Time.Synchronization",
+                    "TimeSyncMethod", "NTP/ProtocolEnabled", timeSyncMethod);
 }
 
 inline void
@@ -362,16 +357,9 @@ inline void
                         continue;
                     }
 
-                    sdbusplus::asio::setProperty(
-                        *crow::connections::systemBus, service, objectPath,
-                        interface, "StaticNTPServers", currentNtpServers,
-                        [asyncResp](const boost::system::error_code& ec2) {
-                        if (ec2)
-                        {
-                            messages::internalError(asyncResp->res);
-                            return;
-                        }
-                    });
+                    setDbusProperty(asyncResp, service, objectPath, interface,
+                                    "StaticNTPServers", "NTP/NTPServers/",
+                                    currentNtpServers);
                 }
             }
         }
@@ -400,30 +388,14 @@ inline void
         {
             if (entry.first.starts_with(netBasePath))
             {
-                sdbusplus::asio::setProperty(
-                    *crow::connections::systemBus, entry.second.begin()->first,
-                    entry.first,
+                setDbusProperty(
+                    asyncResp, entry.second.begin()->first, entry.first,
                     "xyz.openbmc_project.Control.Service.Attributes", "Running",
-                    protocolEnabled,
-                    [asyncResp](const boost::system::error_code& ec2) {
-                    if (ec2)
-                    {
-                        messages::internalError(asyncResp->res);
-                        return;
-                    }
-                });
-                sdbusplus::asio::setProperty(
-                    *crow::connections::systemBus, entry.second.begin()->first,
-                    entry.first,
+                    "IPMI/ProtocolEnabled", protocolEnabled);
+                setDbusProperty(
+                    asyncResp, entry.second.begin()->first, entry.first,
                     "xyz.openbmc_project.Control.Service.Attributes", "Enabled",
-                    protocolEnabled,
-                    [asyncResp](const boost::system::error_code& ec2) {
-                    if (ec2)
-                    {
-                        messages::internalError(asyncResp->res);
-                        return;
-                    }
-                });
+                    "IPMI/ProtocolEnabled", protocolEnabled);
             }
         }
     });

redfish-core/lib/led.hpp
@@ -138,20 +138,12 @@ inline void
                 ledOn = true;
             }
         }
-        sdbusplus::asio::setProperty(
-            *crow::connections::systemBus,
-            "xyz.openbmc_project.LED.GroupManager",
-            "/xyz/openbmc_project/led/groups/enclosure_identify",
-            "xyz.openbmc_project.Led.Group", "Asserted", ledBlinkng,
-            [asyncResp](const boost::system::error_code& ec2) {
-            if (ec2)
-            {
-                BMCWEB_LOG_DEBUG("DBUS response error {}", ec2);
-                messages::internalError(asyncResp->res);
-                return;
-            }
-            messages::success(asyncResp->res);
-        });
+        setDbusProperty(
+            asyncResp, "xyz.openbmc_project.LED.GroupManager",
+            sdbusplus::message::object_path(
+                "/xyz/openbmc_project/led/groups/enclosure_identify"),
+            "xyz.openbmc_project.Led.Group", "Asserted", "IndicatorLED",
+            ledBlinkng);
     });
 }
 
@@ -236,19 +228,12 @@ inline void setSystemLocationIndicatorActive(
             // Some systems may not have enclosure_identify_blink object so
             // lets set enclosure_identify state also if
             // enclosure_identify_blink failed
-            sdbusplus::asio::setProperty(
-                *crow::connections::systemBus,
-                "xyz.openbmc_project.LED.GroupManager",
-                "/xyz/openbmc_project/led/groups/enclosure_identify",
-                "xyz.openbmc_project.Led.Group", "Asserted", ledState,
-                [asyncResp](const boost::system::error_code& ec2) {
-                if (ec2)
-                {
-                    BMCWEB_LOG_DEBUG("DBUS response error {}", ec2);
-                    messages::internalError(asyncResp->res);
-                    return;
-                }
-            });
+            setDbusProperty(
+                asyncResp, "xyz.openbmc_project.LED.GroupManager",
+                sdbusplus::message::object_path(
+                    "/xyz/openbmc_project/led/groups/enclosure_identify"),
+                "xyz.openbmc_project.Led.Group", "Asserted",
+                "LocationIndicatorActive", ledState);
         }
     });
 }

redfish-core/lib/power.hpp
@@ -53,20 +53,11 @@ inline void afterGetPowerCapEnable(
         return;
     }
 
-    sdbusplus::asio::setProperty(
-        *crow::connections::systemBus, "xyz.openbmc_project.Settings",
-        "/xyz/openbmc_project/control/host0/power_cap",
-        "xyz.openbmc_project.Control.Power.Cap", "PowerCap", valueToSet,
-        [sensorsAsyncResp](const boost::system::error_code& ec2) {
-        if (ec2)
-        {
-            BMCWEB_LOG_DEBUG("Power Limit Set: Dbus error: {}", ec2);
-            messages::internalError(sensorsAsyncResp->asyncResp->res);
-            return;
-        }
-        sensorsAsyncResp->asyncResp->res.result(
-            boost::beast::http::status::no_content);
-    });
+    setDbusProperty(sensorsAsyncResp->asyncResp, "xyz.openbmc_project.Settings",
+                    sdbusplus::message::object_path(
+                        "/xyz/openbmc_project/control/host0/power_cap"),
+                    "xyz.openbmc_project.Control.Power.Cap", "PowerCap",
+                    "PowerControl", valueToSet);
 }
 
 inline void afterGetChassisPath(

redfish-core/lib/processor.hpp
@@ -999,65 +999,6 @@ inline void
 }
 
 /**
- * Handle the D-Bus response from attempting to set the CPU's AppliedConfig
- * property. Main task is to translate error messages into Redfish errors.
- *
- * @param[in,out]   resp    HTTP response.
- * @param[in]       setPropVal  Value which we attempted to set.
- * @param[in]       ec      D-Bus response error code.
- * @param[in]       msg     D-Bus response message.
- */
-inline void
-    handleAppliedConfigResponse(const std::shared_ptr<bmcweb::AsyncResp>& resp,
-                                const std::string& setPropVal,
-                                const boost::system::error_code& ec,
-                                const sdbusplus::message_t& msg)
-{
-    if (!ec)
-    {
-        BMCWEB_LOG_DEBUG("Set Property succeeded");
-        return;
-    }
-
-    BMCWEB_LOG_DEBUG("Set Property failed: {}", ec);
-
-    const sd_bus_error* dbusError = msg.get_error();
-    if (dbusError == nullptr)
-    {
-        messages::internalError(resp->res);
-        return;
-    }
-
-    // The asio error code doesn't know about our custom errors, so we have to
-    // parse the error string. Some of these D-Bus -> Redfish translations are a
-    // stretch, but it's good to try to communicate something vaguely useful.
-    if (strcmp(dbusError->name,
-               "xyz.openbmc_project.Common.Error.InvalidArgument") == 0)
-    {
-        // Service did not like the object_path we tried to set.
-        messages::propertyValueIncorrect(
-            resp->res, "AppliedOperatingConfig/@odata.id", setPropVal);
-    }
-    else if (strcmp(dbusError->name,
-                    "xyz.openbmc_project.Common.Error.NotAllowed") == 0)
-    {
-        // Service indicates we can never change the config for this processor.
-        messages::propertyNotWritable(resp->res, "AppliedOperatingConfig");
-    }
-    else if (strcmp(dbusError->name,
-                    "xyz.openbmc_project.Common.Error.Unavailable") == 0)
-    {
-        // Service indicates the config cannot be changed right now, but maybe
-        // in a different system state.
-        messages::resourceInStandby(resp->res);
-    }
-    else
-    {
-        messages::internalError(resp->res);
-    }
-}
-
-/**
  * Handle the PATCH operation of the AppliedOperatingConfig property. Do basic
  * validation of the input data, and then set the D-Bus property.
  *
@@ -1099,8 +1040,8 @@ inline void patchAppliedOperatingConfig(
     if (!appliedConfigUri.starts_with(expectedPrefix) ||
         expectedPrefix.size() == appliedConfigUri.size())
     {
-        messages::propertyValueIncorrect(
-            resp->res, "AppliedOperatingConfig/@odata.id", appliedConfigUri);
+        messages::propertyValueIncorrect(resp->res, "AppliedOperatingConfig",
+                                         appliedConfigUri);
         return;
     }
 
@@ -1115,14 +1056,10 @@ inline void patchAppliedOperatingConfig(
     BMCWEB_LOG_INFO("Setting config to {}", configPath.str);
 
     // Set the property, with handler to check error responses
-    sdbusplus::asio::setProperty(
-        *crow::connections::systemBus, *controlService, cpuObjectPath,
+    setDbusProperty(
+        resp, *controlService, cpuObjectPath,
         "xyz.openbmc_project.Control.Processor.CurrentOperatingConfig",
-        "AppliedConfig", configPath,
-        [resp, appliedConfigUri](const boost::system::error_code& ec,
-                                 const sdbusplus::message_t& msg) {
-        handleAppliedConfigResponse(resp, appliedConfigUri, ec, msg);
-    });
+        "AppliedConfig", "AppliedOperatingConfig", configPath);
 }
 
 inline void

commit 16ed3c1d1b6e9f8bbcc65fb6a34fca61458e5dc1

Fix PATCH bmc/NetworkProtocol SSH/ProtocolEnabled
This fixes existing function PATCH /redfish/v1/Managers/bmc/NetworkProtocol
property SSH/ProtocolEnabled which appears to have been accidentally broken
by 87c449664e5375abb040af6fad63ef965c311bec.  The operation had no effect.
The fix is to revert the function body to the previous commit 4d69861ff.

Tested:
Patching this property works (note the service configuration manager may
take up to 15 seconds or more to take effect.

Change-Id: I22f443268927c4b94f97379050682bc818304e61
Signed-off-by: Joseph Reynolds <joseph.reynolds1@ibm.com>

redfish-core/lib/network_protocol.hpp
@@ -408,29 +408,43 @@ inline void
         [protocolEnabled, asyncResp,
          netBasePath](const boost::system::error_code& ec,
                       const dbus::utility::MapperGetSubTreeResponse& subtree) {
-            if (ec)
-            {
-                messages::internalError(asyncResp->res);
-                return;
-            }
+        if (ec)
+        {
+            messages::internalError(asyncResp->res);
+            return;
+        }
 
-            for (const auto& entry : subtree)
+        for (const auto& entry : subtree)
+        {
+            if (entry.first.starts_with(netBasePath))
             {
-                if (entry.first.starts_with(netBasePath))
-                {
-                    setDbusProperty(
-                        asyncResp, "IPMI/ProtocolEnabled",
-                        entry.second.begin()->first, entry.first,
-                        "xyz.openbmc_project.Control.Service.Attributes",
-                        "Running", protocolEnabled);
-                    setDbusProperty(
-                        asyncResp, "IPMI/ProtocolEnabled",
-                        entry.second.begin()->first, entry.first,
-                        "xyz.openbmc_project.Control.Service.Attributes",
-                        "Enabled", protocolEnabled);
-                }
+                sdbusplus::asio::setProperty(
+                    *crow::connections::systemBus, entry.second.begin()->first,
+                    entry.first,
+                    "xyz.openbmc_project.Control.Service.Attributes", "Running",
+                    protocolEnabled,
+                    [asyncResp](const boost::system::error_code& ec2) {
+                    if (ec2)
+                    {
+                        messages::internalError(asyncResp->res);
+                        return;
+                    }
+                });
+                sdbusplus::asio::setProperty(
+                    *crow::connections::systemBus, entry.second.begin()->first,
+                    entry.first,
+                    "xyz.openbmc_project.Control.Service.Attributes", "Enabled",
+                    protocolEnabled,
+                    [asyncResp](const boost::system::error_code& ec2) {
+                    if (ec2)
+                    {
+                        messages::internalError(asyncResp->res);
+                        return;
+                    }
+                });
             }
-        });
+        }
+    });
 }
 
 inline std::string getHostName()