ab948703f252cbc8e85335c3c6a9f8293e2febfc
rohitpai <rohitpai77@gmail.com>
2025-01-03 19:13:36 +0530

    OEM Route Handling Infrastructure
    Goal of the MR is to provide infrastructure support in bmcweb to manage
    the OEM fragment handling separately. OEM schema are vendor defined and
    per DMTF resource we could have multiple vendor defined OEM schema to be
    enabled.
    
    The feature allows registration of route handler per schema per OEM
    namespace.
    Example
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/OpenBmc")'
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/Nvidia")'
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/Ibm")'
    
    We can have separate vendor defined route handlers per resource. Each of
    these route handlers can populate their own vendor specific OEM data.
    The OEM code can be better organised and enabled/disabled as per the
    platform needs. The current MR has the code changes related to handling
    GET requests alone. The feature only supports requests
    where the response payload is JSON.
    
    Tests
    - All UT cases passes
    - New UT added for RF OEM router passes
    
    Change-Id: Ic82aa5fe760eda31e2792fbdfb6884ac3ea613dc
    Signed-off-by: rohitpai <rohitpai77@gmail.com>

---
+++ test/redfish-core/include/redfish_oem_routing_test.cpp
@@ -0,0 +1,52 @@
+
+#include "http_request.hpp"
+#include "http_response.hpp"
+#include "redfish_oem_routing.hpp"
+#include "utility.hpp"
+
+#include <memory>
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+TEST(OemRouter, FragmentRoutes)
+{
+    // Callback handler that does nothing
+    bool oemCalled = false;
+    auto oemCallback = [&oemCalled](const crow::Request&,
+                                    const std::shared_ptr<bmcweb::AsyncResp>&,
+                                    const std::string& bar) {
+        oemCalled = true;
+        EXPECT_EQ(bar, "bar");
+    };
+
+    OemRouter router;
+    std::error_code ec;
+    constexpr std::string_view fragment = "/foo/<str>#Oem";
+    router
+        .newOemRule<crow::utility::getParameterTag(fragment)>(
+            std::string(fragment))
+        .setGetHandler(oemCallback);
+    router.validate();
+    {
+        constexpr std::string_view reqUrl = "/foo/bar";
+
+        auto req = std::make_shared<crow::Request>(
+            crow::Request::Body{boost::beast::http::verb::get, reqUrl, 11}, ec);
+
+        std::shared_ptr<bmcweb::AsyncResp> asyncResp =
+            std::make_shared<bmcweb::AsyncResp>();
+
+        router.handleOemGet(req, asyncResp);
+    }
+    EXPECT_TRUE(oemCalled);
+}
+
+} // namespace
+} // namespace redfish
