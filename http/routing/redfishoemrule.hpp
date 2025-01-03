5cf7c251eaec645990e1d85403e42617a0b40063
rohitpai <rohitpai77@gmail.com>
2025-01-03 19:13:36 +0530

    OEM Route Handling Infrastructure
    Goal of the MR is to provide infrastructure support in bmcweb
    to manage the OEM fragment handling separately. OEM schema
    are vendor defined and per DMTF resource we could have
    multiple vendor defined OEM schema to be enabled.
    
    The feature allows registration of route handler per schema
    per OEM type.
    Example
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/OpenBmc")'
    The OEM code can be better organized and enabled/disabled on
    the platform needs.
    The current MR has the code changes related to handling GET
    requests alone. The feature only supports requests where the
    response payload is JSON.
    
    Tests
    - New route handler registered for Manager OEM schema for openbmc
    - GET requested on Manager resource return the OEM data.
    
    Change-Id: Ic82aa5fe760eda31e2792fbdfb6884ac3ea613dc
    Signed-off-by: rohitpai <rohitpai77@gmail.com>

---
+++ http/routing/redfishoemrule.hpp
@@ -0,0 +1,154 @@
+#pragma once
+#include "baserule.hpp"
+#include "dynamicrule.hpp"
+#include "ruleparametertraits.hpp"
+
+#include <boost/beast/http/verb.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace crow
+{
+class RfOemBaseRule : public BaseRule
+{
+  public:
+    explicit RfOemBaseRule(const std::string& thisRule) : BaseRule(thisRule) {}
+    ~RfOemBaseRule() = default;
+    RfOemBaseRule(const RfOemBaseRule&) = delete;
+    RfOemBaseRule(RfOemBaseRule&&) = delete;
+    RfOemBaseRule& operator=(const RfOemBaseRule&) = delete;
+    RfOemBaseRule& operator=(const RfOemBaseRule&&) = delete;
+
+    void handle(const Request& /*req*/,
+                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                const std::vector<std::string>& /*params*/) override
+    {
+        asyncResp->res.result(boost::beast::http::status::not_found);
+    }
+
+    virtual void handle(Request& /*req*/,
+                        const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                        const std::vector<std::string>& /*params*/,
+                        const nlohmann::json& /*payload*/)
+    {
+        asyncResp->res.result(boost::beast::http::status::not_found);
+    }
+};
+
+template <typename... Args>
+class RfOemRule :
+    public RfOemBaseRule,
+    public RuleParameterTraits<RfOemRule<Args...>>
+{
+  public:
+    using self_t = RfOemRule<Args...>;
+
+    explicit RfOemRule(const std::string& ruleIn) : RfOemBaseRule(ruleIn) {}
+
+    void validate() override
+    {
+        if (!getHandler && !patchHandler)
+        {
+            throw std::runtime_error("no handler for url " + rule);
+        }
+    }
+
+    void setGetHandler(
+        std::function<void(const crow::Request&,
+                           const std::shared_ptr<bmcweb::AsyncResp>&, Args...)>
+            newHandler)
+    {
+        getHandler = std::move(newHandler);
+    }
+
+    void setPatchHandler(
+        std::function<void(const crow::Request&,
+                           const std::shared_ptr<bmcweb::AsyncResp>&,
+                           const nlohmann::json::object_t& payload, Args...)>
+            newHandler)
+    {
+        patchHandler = std::move(newHandler);
+    }
+
+    void handle(const Request& req,
+                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                const std::vector<std::string>& params) override
+    {
+        if constexpr (sizeof...(Args) == 0)
+        {
+            getHandler(req, asyncResp);
+        }
+        else if constexpr (sizeof...(Args) == 1)
+        {
+            getHandler(req, asyncResp, params[0]);
+        }
+        else if constexpr (sizeof...(Args) == 2)
+        {
+            getHandler(req, asyncResp, params[0], params[1]);
+        }
+        else if constexpr (sizeof...(Args) == 3)
+        {
+            getHandler(req, asyncResp, params[0], params[1], params[2]);
+        }
+        else if constexpr (sizeof...(Args) == 4)
+        {
+            getHandler(req, asyncResp, params[0], params[1], params[2],
+                       params[3]);
+        }
+        else if constexpr (sizeof...(Args) == 5)
+        {
+            getHandler(req, asyncResp, params[0], params[1], params[2],
+                       params[3], params[4]);
+        }
+        static_assert(sizeof...(Args) <= 5, "More args than are supported");
+    }
+
+    void handle(Request& req,
+                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                const std::vector<std::string>& params,
+                const nlohmann::json& payload) override
+    {
+        BMCWEB_LOG_DEBUG("OEM Patch Fragment JSON {}", payload.dump(4));
+
+        if constexpr (sizeof...(Args) == 0)
+        {
+            patchHandler(req, asyncResp, payload);
+        }
+        else if constexpr (sizeof...(Args) == 1)
+        {
+            patchHandler(req, asyncResp, payload, params[0]);
+        }
+        else if constexpr (sizeof...(Args) == 2)
+        {
+            patchHandler(req, asyncResp, payload, params[0], params[1]);
+        }
+        else if constexpr (sizeof...(Args) == 3)
+        {
+            patchHandler(req, asyncResp, payload, params[0], params[1],
+                         params[2]);
+        }
+        else if constexpr (sizeof...(Args) == 4)
+        {
+            patchHandler(req, asyncResp, payload, params[0], params[1],
+                         params[2], params[3]);
+        }
+        else if constexpr (sizeof...(Args) == 5)
+        {
+            patchHandler(req, asyncResp, payload, params[0], params[1],
+                         params[2], params[3], params[4]);
+        }
+        static_assert(sizeof...(Args) <= 5, "More args than are supported");
+    }
+
+  private:
+    std::function<void(const crow::Request&,
+                       const std::shared_ptr<bmcweb::AsyncResp>&, Args...)>
+        getHandler;
+    std::function<void(const crow::Request&,
+                       const std::shared_ptr<bmcweb::AsyncResp>&,
+                       const nlohmann::json::object_t& payload, Args...)>
+        patchHandler;
+};
+} // namespace crow
