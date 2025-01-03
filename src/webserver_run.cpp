94b986f2130ee1bec885a73c33070d0f01d70fd5
rohitpai <rohitpai77@gmail.com>
2025-01-03 19:13:36 +0530

    OEM Route Handling Infrastructure
    Goal of the MR is to provide infrastructure support in bmcweb to manage
    the OEM fragment handling separately. OEM schema are vendor defined and
    per DMTF resource we could have multiple vendor defined OEM schema to be
    enabled.
    
    The feature allows registration of route handler per schema per OEM
    namespace.
    Example
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/OpenBmc")'
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/Nvidia")'
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/Ibm")'
    
    We can have separate vendor defined route handlers per resource. Each of
    these route handlers can populate their own vendor specific OEM data.
    The OEM code can be better organized and enabled/disabled as per the
    platform needs. The current MR has the code changes related to handling
    GET requests alone. The feature only supports requests
    where the response payload is JSON.
    
    Tests
    - All UT cases passes
    - New UT added for RF OEM router passes
    
    Change-Id: Ic82aa5fe760eda31e2792fbdfb6884ac3ea613dc
    Signed-off-by: Rohit PAI <rohitpai77@gmail.com>
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- src/webserver_run.cpp
+++ src/webserver_run.cpp
@@ -20,6 +20,7 @@
 #include "openbmc_dbus_rest.hpp"
 #include "redfish.hpp"
 #include "redfish_aggregator.hpp"
+#include "redfish_oem_routing.hpp"
 #include "user_monitor.hpp"
 #include "vm_websocket.hpp"
 #include "watchdog.hpp"
@@ -128,6 +129,8 @@ int run()
     bmcweb::registerUserRemovedSignal();
 
     bmcweb::ServiceWatchdog watchdog;
+
+    redfish::oemRouterInit();
 
     app.run();
 
