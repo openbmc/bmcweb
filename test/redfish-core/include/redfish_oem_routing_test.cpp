ff7a1614824314ad99fbe36643413deab8d69ded
rohitpai <rohitpai77@gmail.com>
2025-01-03 19:13:36 +0530

    OEM Route Handling Infrastructure
    Goal of the MR is to provide infrastructure support in bmcweb to manage
    the OEM fragment handling separately. OEM schema are vendor defined and
    per DMTF resource we could have multiple vendor defined OEM schema to be
    enabled.
    
    The feature allows registration of route handler per schema per OEM
    namespace.
    Example
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/OpenBmc")'
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/Nvidia")'
    'BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>#Oem/Ibm")'
    
    We can have separate vendor defined route handlers per resource. Each of
    these route handlers can populate their own vendor specific OEM data.
    The OEM code can be better organized and enabled/disabled as per the
    platform needs. The current MR has the code changes related to handling
    GET requests alone. The feature only supports requests
    where the response payload is JSON.
    
    Tests
    - All UT cases passes
    - New UT added for RF OEM router passes
    - Service Validator passes on qemu
    - GET Response on Manager/bmc resource contains the OEM fragment
    
    ```
    curl -c cjar -b cjar -k -X GET https://127.0.0.1:2443/redfish/v1/Managers/bmc
    {
      "@odata.id": "/redfish/v1/Managers/bmc",
      "@odata.type": "#Manager.v1_14_0.Manager",
    
      "Oem": {
        "OpenBmc": {
          "@odata.id": "/redfish/v1/Managers/bmc#/Oem/OpenBmc",
          "@odata.type": "#OpenBMCManager.v1_0_0.Manager",
          "Certificates": {
            "@odata.id": "/redfish/v1/Managers/bmc/Truststore/Certificates"
          }
        }
      },
    
      "UUID": "40575e98-90d7-4c10-9eb5-8d8a7156c9b9"
    }
    ```
    
    Change-Id: Ic82aa5fe760eda31e2792fbdfb6884ac3ea613dc
    Signed-off-by: Rohit PAI <rohitpai77@gmail.com>

---
+++ test/redfish-core/include/redfish_oem_routing_test.cpp
@@ -0,0 +1,70 @@
+
+#include "app.hpp"
+#include "async_resp.hpp"
+#include "http_request.hpp"
+#include "redfish.hpp"
+#include "verb.hpp"
+
+#include <boost/beast/http/verb.hpp>
+
+#include <memory>
+#include <string>
+#include <string_view>
+#include <system_error>
+
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+TEST(OemRouter, FragmentRoutes)
+{
+    std::error_code ec;
+    App app;
+    RedfishService service(app);
+
+    // Callback handler that does nothing
+    bool oemCalled = false;
+    auto oemCallback = [&oemCalled](const crow::Request&,
+                                    const std::shared_ptr<bmcweb::AsyncResp>&,
+                                    const std::string& bar) {
+        oemCalled = true;
+        EXPECT_EQ(bar, "bar");
+    };
+    bool standardCalled = false;
+    auto standardCallback =
+        [&standardCalled,
+         &service](const crow::Request& req,
+                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& bar) {
+            service.handleSubRoute(req, asyncResp);
+            standardCalled = true;
+            EXPECT_EQ(bar, "bar");
+        };
+    // Need the normal route registered for OEM to work
+    BMCWEB_ROUTE(app, "/foo/<str>/")
+        .methods(boost::beast::http::verb::get)(standardCallback);
+        REDFISH_SUB_ROUTE<"/foo/<str>/#/Oem">(service, HttpVerb::Get)(oemCallback);
+
+    app.validate();
+    service.validate();
+
+    {
+        constexpr std::string_view reqUrl = "/foo/bar";
+
+        std::shared_ptr<crow::Request> req = std::make_shared<crow::Request>(
+            crow::Request::Body{boost::beast::http::verb::get, reqUrl, 11}, ec);
+
+        std::shared_ptr<bmcweb::AsyncResp> asyncResp =
+            std::make_shared<bmcweb::AsyncResp>();
+
+        app.handle(req, asyncResp);
+    }
+    EXPECT_TRUE(oemCalled);
+    EXPECT_TRUE(standardCalled);
+}
+
+} // namespace
+} // namespace redfish
