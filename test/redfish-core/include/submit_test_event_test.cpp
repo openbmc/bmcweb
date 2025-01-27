a785871b4f621304c56e993500987db26a5c6d0e
Ed Tanous <etanous@nvidia.com>
2025-01-27 10:44:15 -0800

    Deduplicate event ids
    Redfish specification states:
    ```
    The value of the id field shall be the same as the Id property in the
    event payload. The value of the Id property in the event payload should
    be the same as the EventId property of the last event record in the
    Events array. The value of the EventId property for an event record
    should be a positive integer value and should be generated in a
    sequential manner.
    ```
    
    The event service code did not implement that correctly.  So:
    1. Add ID fields for all events.
    2. Remove the per-sse connection id field and rely solely on
       EventServiceManager.
    3. Make sure all paths, (including metric report) are generating an
       event id that's based on the eventservice event id
    
    Tested: Redfish event listener now sees events populated.
    LastEventId when sent to the SSE socket now sees a contiguous id.
    
    ```
    uri=$(curl -s --user "root:0penBmc" -k "https://192.168.7.2/redfish/v1/EventService" | jq -r .ServerSentEventUri)
    curl -u root:0penBmc -vvv -k -N -H "Accept: text/event-stream" -H "Last-Event-Id: 0" "https://192.168.7.2$uri"
    ```
    
    Change-Id: Ic32e036f40a53a9b2715639ae384d7891c768260
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- test/redfish-core/include/submit_test_event_test.cpp
+++ test/redfish-core/include/submit_test_event_test.cpp
@@ -1,4 +1,4 @@
-#include "event_service_store.hpp"
+#include "event_service_manager.hpp"
 #include "subscription.hpp"
 
 #include <boost/asio/io_context.hpp>
@@ -15,7 +15,6 @@ static TestEvent createTestEvent()
 {
     TestEvent testEvent;
     testEvent.eventGroupId = 1;
-    testEvent.eventId = "dummyEvent";
     testEvent.eventTimestamp = "2021-01";
     testEvent.message = "Test Message";
     testEvent.messageArgs = std::vector<std::string>{"arg1", "arg2"};
@@ -26,22 +25,17 @@ static TestEvent createTestEvent()
     return testEvent;
 }
 
-TEST(Subscription, submitTestEVent)
+TEST(EventServiceManager, submitTestEVent)
 {
-    boost::asio::io_context io;
     boost::urls::url url;
-    auto userSubscription =
-        std::make_shared<persistent_data::UserSubscription>();
+    EventServiceManager& evt = EventServiceManager::getInstance();
     {
-        auto sub = std::make_shared<Subscription>(userSubscription, url, io);
         TestEvent testEvent;
-        EXPECT_TRUE(sub->sendTestEventLog(testEvent));
+        EXPECT_TRUE(evt.sendTestEventLog(testEvent));
     }
     {
-        auto sub = std::make_shared<Subscription>(userSubscription, url, io);
         TestEvent testEvent;
         testEvent.eventGroupId = 1;
-        testEvent.eventId = "GPU-RST-RECOMM-EVENT";
         testEvent.eventTimestamp = "2021-01-01T00:00:00Z";
         testEvent.message = "Custom Message";
         testEvent.messageArgs =
@@ -51,54 +45,23 @@ TEST(Subscription, submitTestEVent)
         testEvent.resolution =
             "Reset the GPU at the next service window since the ECC errors are contained";
         testEvent.severity = "Informational";
-        EXPECT_TRUE(sub->sendTestEventLog(testEvent));
+        EXPECT_TRUE(evt.sendTestEventLog(testEvent));
     }
     {
-        auto sub = std::make_shared<Subscription>(userSubscription, url, io);
         TestEvent testEvent = createTestEvent();
 
-        bool result = sub->sendTestEventLog(testEvent);
+        bool result = evt.sendTestEventLog(testEvent);
 
         EXPECT_TRUE(result);
-        if (testEvent.eventGroupId.has_value())
-        {
-            EXPECT_EQ(testEvent.eventGroupId.value(), 1);
-        }
-        if (testEvent.eventId.has_value())
-        {
-            EXPECT_EQ(testEvent.eventId.value(), "dummyEvent");
-        }
-        if (testEvent.eventTimestamp.has_value())
-        {
-            EXPECT_EQ(testEvent.eventTimestamp.value(), "2021-01");
-        }
-        if (testEvent.message.has_value())
-        {
-            EXPECT_EQ(testEvent.message.value(), "Test Message");
-        }
-        if (testEvent.messageArgs.has_value())
-        {
-            EXPECT_EQ(testEvent.messageArgs.value().size(), 2);
-            EXPECT_EQ(testEvent.messageArgs.value()[0], "arg1");
-            EXPECT_EQ(testEvent.messageArgs.value()[1], "arg2");
-        }
-        if (testEvent.messageId.has_value())
-        {
-            EXPECT_EQ(testEvent.messageId.value(), "Dummy message ID");
-        }
-        if (testEvent.originOfCondition.has_value())
-        {
-            EXPECT_EQ(testEvent.originOfCondition.value(),
-                      "/redfish/v1/Chassis/GPU_SXM_1");
-        }
-        if (testEvent.resolution.has_value())
-        {
-            EXPECT_EQ(testEvent.resolution.value(), "custom resolution");
-        }
-        if (testEvent.severity.has_value())
-        {
-            EXPECT_EQ(testEvent.severity.value(), "whatever");
-        }
+
+        EXPECT_EQ(testEvent.eventGroupId.value(), 1);
+        EXPECT_EQ(testEvent.eventTimestamp.value(), "2021-01");
+        EXPECT_EQ(testEvent.message.value(), "Test Message");
+        EXPECT_EQ(testEvent.messageId.value(), "Dummy message ID");
+        EXPECT_EQ(testEvent.originOfCondition.value(),
+                  "/redfish/v1/Chassis/GPU_SXM_1");
+        EXPECT_EQ(testEvent.resolution.value(), "custom resolution");
+        EXPECT_EQ(testEvent.severity.value(), "whatever");
     }
 }
 } // namespace redfish
