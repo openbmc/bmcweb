274d77f5c5466bab51f2a33e7a193b4a379c1868
rohitpai <rohitpai77@gmail.com>
2025-01-28 09:23:53 +0530

    Redfish OEM Patch Route Handling
    Extension of OEM route infra to support registration of handlers for OEM
    patch requests. When patch request is made on a redfish resource, first
    the main route handler will be called and if request patch payload
    contains any OEM fragments then, registered OEM patch handler will be
    called.
    
    Tested
    1. UT passes with new test cases added for OEM patch handling
    2. Tested PATCH request on Manager Schema with OEM property
    
    curl -k -u root:0penBmc \
      -H "Content-Type: application/json" \
      -X PATCH \
      -d '{
        "DateTime": "2025-04-10T14:30:00+00:00",
        "Oem": {
          "OpenBmc": {
            "Fan": {
              "StepwiseControllers": {
                "SWTest": {
                  "NegativeHysteresis": 3,
                  "PositiveHysteresis": 4
                }
              }
            }
          }
        }
      }' \
      https://localhost:2443/redfish/v1/Managers/bmc -i
    
    HTTP/1.1 204 No Content
    
    Change-Id: Ib2498b6a4db0343d5d4a405a5a8e4d78f615bed8
    Signed-off-by: Rohit PAI <rohitpai77@gmail.com>

--- test/redfish-core/include/redfish_oem_routing_test.cpp
+++ test/redfish-core/include/redfish_oem_routing_test.cpp
@@ -6,11 +6,13 @@
 #include "verb.hpp"
 
 #include <boost/beast/http/verb.hpp>
+#include <nlohmann/json.hpp>
 
 #include <memory>
 #include <string>
 #include <string_view>
 #include <system_error>
+#include <utility>
 
 #include <gtest/gtest.h>
 
@@ -64,6 +66,102 @@ TEST(OemRouter, FragmentRoutes)
     }
     EXPECT_TRUE(oemCalled);
     EXPECT_TRUE(standardCalled);
+}
+
+TEST(OemRouter, PatchHandlerWithJsonObject)
+{
+    std::error_code ec;
+    App app;
+    RedfishService service(app);
+
+    // Callback handlers
+    bool callback1Called = false;
+    auto patchCallback1 =
+        [&callback1Called](
+            const SubRequest& req,
+            [[maybe_unused]] const std::shared_ptr<bmcweb::AsyncResp>&
+                asyncResp,
+            [[maybe_unused]] const std::string& param) {
+            callback1Called = true;
+
+            const nlohmann::json::object_t& payload = req.payload();
+            auto oemFooIt = payload.find("OemFoo");
+            ASSERT_NE(oemFooIt, payload.end());
+            ASSERT_TRUE(oemFooIt->second.is_object());
+
+            auto keyIt = oemFooIt->second.find("OemFooKey");
+            ASSERT_NE(keyIt, oemFooIt->second.end());
+            EXPECT_EQ(keyIt.value(), "fooValue");
+        };
+
+    bool callback2Called = false;
+    auto patchCallback2 =
+        [&callback2Called](
+            const SubRequest& req,
+            [[maybe_unused]] const std::shared_ptr<bmcweb::AsyncResp>&
+                asyncResp,
+            [[maybe_unused]] const std::string& param) {
+            callback2Called = true;
+
+            const nlohmann::json::object_t& payload = req.payload();
+            auto oemBarIt = payload.find("OemBar");
+            ASSERT_NE(oemBarIt, payload.end());
+            ASSERT_TRUE(oemBarIt->second.is_object());
+
+            auto keyIt = oemBarIt->second.find("OemBarKey");
+            ASSERT_NE(keyIt, oemBarIt->second.end());
+            EXPECT_EQ(keyIt.value(), "barValue");
+        };
+
+    bool standardCalled = false;
+    auto standardCallback =
+        [&standardCalled,
+         &service](const crow::Request& req,
+                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& bar) {
+            service.handleSubRoute(req, asyncResp);
+            standardCalled = true;
+            EXPECT_EQ(bar, "bar");
+        };
+
+    // Need the normal route registered for OEM to work
+    BMCWEB_ROUTE(app, "/foo/<str>/")
+        .methods(boost::beast::http::verb::patch)(standardCallback);
+
+    REDFISH_SUB_ROUTE<"/foo/<str>/#/Oem/OemFoo">(service, HttpVerb::Patch)(
+        patchCallback1);
+    REDFISH_SUB_ROUTE<"/foo/<str>/#/Oem/OemBar">(service, HttpVerb::Patch)(
+        patchCallback2);
+
+    app.validate();
+    service.validate();
+
+    {
+        constexpr std::string_view reqUrl = "/foo/bar";
+        // OEM payload
+        nlohmann::json reqBody = {{"Oem",
+                                   {{"OemFoo", {{"OemFooKey", "fooValue"}}},
+                                    {"OemBar", {{"OemBarKey", "barValue"}}}}}};
+
+        // Create request with the body string
+        std::shared_ptr<crow::Request> req = std::make_shared<crow::Request>(
+            crow::Request::Body{
+                boost::beast::http::verb::patch, reqUrl, 11,
+                reqBody.dump() // Pass body string directly in the constructor
+            },
+            ec);
+
+        req->addHeader("Content-Type", "application/json");
+
+        std::shared_ptr<bmcweb::AsyncResp> asyncResp =
+            std::make_shared<bmcweb::AsyncResp>();
+
+        app.handle(req, asyncResp);
+    }
+
+    EXPECT_TRUE(standardCalled);
+    EXPECT_TRUE(callback1Called);
+    EXPECT_TRUE(callback2Called);
 }
 
 } // namespace
