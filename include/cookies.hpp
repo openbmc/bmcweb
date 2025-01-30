4153b9e7fa648fbeb57a4393781893c455734bdc
Gunnar Mills <gmills@us.ibm.com>
2025-01-30 15:41:08 -0700

    Revert "Change Session Cookie name (#1130)" (#1138)
    This reverts commit 05b61c64f39e58e36cebfca10d52dbb1f6791f64.

--- include/cookies.hpp
+++ include/cookies.hpp
@@ -13,14 +13,14 @@ inline void setSessionCookies(crow::Response& res,
                   "XSRF-TOKEN=" + session.csrfToken +
                       "; Path=/; SameSite=Strict; Secure");
     res.addHeader(boost::beast::http::field::set_cookie,
-                  "BMCWEB-SESSION=" + session.sessionToken +
+                  "SESSION=" + session.sessionToken +
                       "; Path=/; SameSite=Strict; Secure; HttpOnly");
 }
 
 inline void clearSessionCookies(crow::Response& res)
 {
     res.addHeader(boost::beast::http::field::set_cookie,
-                  "BMCWEB-SESSION="
+                  "SESSION="
                   "; Path=/; SameSite=Strict; Secure; HttpOnly; "
                   "expires=Thu, 01 Jan 1970 00:00:00 GMT");
     res.addHeader("Clear-Site-Data", R"("cache","cookies","storage")");
