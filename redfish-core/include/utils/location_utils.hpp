110d2571e70152bf6da435b13837616858f7fe54
Aishwary Joshi <aishwaryjoshi31@gmail.com>
2025-02-04 21:11:05 +0530

    Processor: Add processor Location Type
    - Add processor Location type
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/77944
    Embedded: CPU/Accelerator is embedded
    
    - Schema defining the Location type
    http://redfish.dmtf.org/schemas/v1/Resource.v1_21_0.yaml
    
    Pending action
    - Redfish validator results
    
    Change-Id: I528236ff88da877951cc22efa1a58413cf43f823
    Signed-off-by: Aishwary Joshi <aishwaryjoshi31@gmail.com>

---
+++ redfish-core/include/utils/location_utils.hpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+
+#include "generated/enums/resource.hpp"
+
+#include <string>
+
+namespace redfish
+{
+namespace location_utils
+{
+
+/**
+ * @brief Converts D-Bus interface name to ConnectorType
+ *
+ * @param interface D-Bus interface name
+ * @return ConnectorType enum value
+ */
+inline resource::LocationType getLocationType(std::string_view interface)
+{
+    static const std::unordered_map<std::string_view, resource::LocationType>
+        interfaceMap{{"xyz.openbmc_project.Inventory.Connector.Embedded",
+                      resource::LocationType::Embedded},
+                     {"xyz.openbmc_project.Inventory.Connector.Slot",
+                      resource::LocationType::Slot}};
+
+    auto it = interfaceMap.find(interface);
+    if (it != interfaceMap.end())
+    {
+        return it->second;
+    }
+    return resource::LocationType::Invalid;
+}
+
+} // namespace location_utils
+} // namespace redfish
