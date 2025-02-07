0d34ab54ac62987d4839975fa8dec4fc3e3b26d7
Asmitha Karunanithi <asmitk01@in.ibm.com>
2025-02-07 12:41:09 -0600

    mfa: Add GenerateSecretKey Action for user account
    Tested By:
    '''
    POST https://${bmc}/redfish/v1/AccountService/Accounts/<>/Actions/ManagerAccount.GenerateSecretKey
    
    This returns the secretKey in the redfish response.
    '''
    
    Change-Id: I1e72d79162bac763750a18e0264d115aa4a83cf4
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

---
+++ redfish-core/lib/account_service_mfa_actions.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "app.hpp"
+#include "dbus_utility.hpp"
+#include "error_messages.hpp"
+#include "privileges.hpp"
+#include "registries/privilege_registry.hpp"
+#include "utils/json_utils.hpp"
+
+#include <nlohmann/json.hpp>
+#include <sdbusplus/message.hpp>
+
+namespace redfish
+{
+
+inline void createSecretKeyUtil(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& username, const std::string& userPath)
+{
+    crow::connections::systemBus->async_method_call(
+        [asyncResp, username](const boost::system::error_code& ec,
+                              const std::string& secretKey) {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR("D-Bus response error: {}", ec.value());
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            asyncResp->res.jsonValue["@odata.type"] =
+                "#ManagerAccount.v1_13_0.ManagerAccount";
+            asyncResp->res.jsonValue["Name"] = "User Account";
+            asyncResp->res.jsonValue["Description"] = "User Account";
+            asyncResp->res.jsonValue["SecretKey"] = secretKey;
+        },
+        "xyz.openbmc_project.User.Manager", userPath,
+        "xyz.openbmc_project.User.TOTPAuthenticator", "CreateSecretKey");
+}
+
+inline void
+    handleGenerateSecretKey(App& app, const crow::Request& req,
+                            const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                            const std::string& username)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    if constexpr (BMCWEB_INSECURE_DISABLE_AUTH)
+    {
+        // If authentication is disabled, there are no user accounts
+        messages::resourceNotFound(asyncResp->res, "ManagerAccount", username);
+        return;
+    }
+
+    if (req.session == nullptr)
+    {
+        messages::internalError(asyncResp->res);
+        return;
+    }
+    bool userSelf = (username == req.session->username);
+
+    Privileges effectiveUserPrivileges =
+        redfish::getUserPrivileges(*req.session);
+    Privileges configureUsers = {"ConfigureUsers"};
+    bool userHasConfigureUsers =
+        effectiveUserPrivileges.isSupersetOf(configureUsers);
+
+    if (!userHasConfigureUsers && !userSelf)
+    {
+        BMCWEB_LOG_WARNING("Insufficient Privilege");
+        messages::insufficientPrivilege(asyncResp->res);
+        return;
+    }
+    sdbusplus::message::object_path tempObjPath(rootUserDbusPath);
+    tempObjPath /= username;
+    const std::string userPath(tempObjPath);
+
+    createSecretKeyUtil(asyncResp, username, userPath);
+}
+
+inline void requestAccountServiceMFARoutes(App& app)
+{
+    BMCWEB_ROUTE(
+        app,
+        "/redfish/v1/AccountService/Accounts/<str>/Actions/ManagerAccount.GenerateSecretKey")
+        // TODO this privilege should be using the generated endpoints, but
+        // because of the special handling of ConfigureSelf, it's not able to
+        // yet
+        .privileges({{"ConfigureUsers"}, {"ConfigureSelf"}})
+        .methods(boost::beast::http::verb::post)(
+            std::bind_front(handleGenerateSecretKey, std::ref(app)));
+}
+} // namespace redfish
