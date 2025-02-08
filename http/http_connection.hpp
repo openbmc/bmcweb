1d09c753751f5b5220264bf33fcc1b1adfcb1934
Ed Tanous <etanous@nvidia.com>
2025-02-08 14:29:53 -0800

    Implement http2 WIP
    To support HTTP2 simultaneously on http and https connections, the HTTP
    connection classes formerly took the socket as a template option,
    allowing passing ssl::stream<tcp::socket> or simply tcp socket.  With
    the addition of the multiple-sockets option, this would cause two copies
    of the template to be instantiated, increasing both compile times and
    binary size.
    
    This commit applies the same logic to http2connection as was applied to
    HTTPConnection, adding an http type parameter to the constructor, which
    allows switching between adapter and adapter.next_level() on each read
    or write operation.  In compiled code, this means that the connection
    classes are only specialized once.
    
    Tested:
    When configured for one of each http and https socket and http2
    curl --http2 http://<ip>/redfish/v1
    succeeds
    curl --http2 https://<ip>/redfish/v1 succeeds
    
    Change-Id: I8f33796edd5874d5b93d10a3f253cfadd4f6d7a4
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- http/http_connection.hpp
+++ http/http_connection.hpp
@@ -76,7 +76,6 @@ class Connection :
   public:
     Connection(Handler* handlerIn, HttpType httpTypeIn,
                boost::asio::steady_timer&& timerIn,
-
                std::function<std::string()>& getCachedDateStrF,
                boost::asio::ssl::stream<Adaptor>&& adaptorIn) :
         httpType(httpTypeIn), adaptor(std::move(adaptorIn)), handler(handlerIn),
@@ -280,34 +279,15 @@ class Connection :
 
     void upgradeToHttp2()
     {
-        // TODO HTTP2Connection needs adaptor moved to a similar abstraction as
-        // HTTPConnection
-        if (httpType == HttpType::HTTP)
+        auto http2 = std::make_shared<HTTP2Connection<Adaptor, Handler>>(
+            std::move(adaptor), handler, getCachedDateStr, httpType);
+        if (http2settings.empty())
         {
-            auto http2 = std::make_shared<HTTP2Connection<Adaptor, Handler>>(
-                std::move(adaptor.next_layer()), handler, getCachedDateStr);
-            if (http2settings.empty())
-            {
-                http2->start();
-            }
-            else
-            {
-                http2->startFromSettings(http2settings);
-            }
+            http2->start();
         }
         else
         {
-            auto http2 = std::make_shared<
-                HTTP2Connection<boost::asio::ssl::stream<Adaptor>, Handler>>(
-                std::move(adaptor), handler, getCachedDateStr);
-            if (http2settings.empty())
-            {
-                http2->start();
-            }
-            else
-            {
-                http2->startFromSettings(http2settings);
-            }
+            http2->startFromSettings(http2settings);
         }
     }
 
