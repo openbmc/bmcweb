942796fe6c27131b3bce9ec25413c3be5300bf22
rohitpai <ropai@nvidia.com>
2025-02-24 12:33:59 +0530

    Add support for systemd service watchdog
    Systemd has support for enabling service level watchdog. The MR enables
    this support for bmcweb daemon. Request for watchdog monitor from
    systemd is added in bmcweb.service.in. From the event loop a timer is
    registered to kick the watchdog periodically
    
    The default watchdog timeout is set at 120 seconds and the timer is set
    to kick it at a quarter of the interval (every 30 seconds).
    This timeout is set somewhat arbitrarily based on the longest blocking
    call that could occur and still give a valid HTTP response.  Suspect
    lower values could work equally as well.
    
    Benefits of Service Watchdog
    - Bmcweb route handlers should not make any blocking IO calls which
    block the event loop for considerable amount of time and slowdown the
    response of other URI requests in the queue. Watchdog can help to detect
    such issues.
    - Watchdog can help restart the service if any route handler code has
    uncaught bugs resulting from system API errors (this is in theory,
    currently we don't have any use case).
    
    Tested
    1. UT is passing
    2. Service validator is passing
    3. Fw upgrade POST requests are working
    
    Change-Id: If62397d8836c942fdcbc0618810fe82a8b248df8
    Signed-off-by: rohitpai <ropai@nvidia.com>
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- src/webserver_run.cpp
+++ src/webserver_run.cpp
@@ -22,6 +22,7 @@
 #include "redfish_aggregator.hpp"
 #include "user_monitor.hpp"
 #include "vm_websocket.hpp"
+#include "watchdog.hpp"
 #include "webassets.hpp"
 
 #include <boost/asio/io_context.hpp>
@@ -125,6 +126,8 @@ int run()
     }
 
     bmcweb::registerUserRemovedSignal();
+
+    bmcweb::ServiceWatchdog watchdog;
 
     app.run();
 
