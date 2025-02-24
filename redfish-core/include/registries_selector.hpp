12aa4ef65676b0b92bb8feedbdfb65d07b950c5b
Igor Kanyuka <ifelmail@gmail.com>
2025-02-24 15:47:52 +0000

    Add supported registries to the registry selector
    There are some registries, either DMTF or OpenBMC vendor ones, that have
    code generated, but cannot be used as they are not added to the
    selector. Add them, so the code will be able to use them.
    
    Testing: Unit test
    
    Change-Id: Id68cdd7a4f2f54395ebac51fd6aa5e18d47f9d6d
    Signed-off-by: Igor Kanyuka <ifelmail@gmail.com>

--- redfish-core/include/registries_selector.hpp
+++ redfish-core/include/registries_selector.hpp
@@ -3,9 +3,14 @@
 #pragma once
 #include "registries.hpp"
 #include "registries/base_message_registry.hpp"
+#include "registries/environmental_message_registry.hpp"
 #include "registries/heartbeat_event_message_registry.hpp"
+#include "registries/openbmc_logging_message_registry.hpp"
 #include "registries/openbmc_message_registry.hpp"
+#include "registries/openbmc_state_cable_message_registry.hpp"
+#include "registries/openbmc_state_leak_detector_group_message_registry.hpp"
 #include "registries/resource_event_message_registry.hpp"
+#include "registries/sensor_event_message_registry.hpp"
 #include "registries/task_event_message_registry.hpp"
 #include "registries/telemetry_message_registry.hpp"
 
@@ -28,6 +33,10 @@ inline std::optional<registries::HeaderAndUrl>
     {
         return HeaderAndUrl{base::header, base::url};
     }
+    if (environmental::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{environmental::header, environmental::url};
+    }
     if (heartbeat_event::header.registryPrefix == registryName)
     {
         return HeaderAndUrl{heartbeat_event::header, heartbeat_event::url};
@@ -36,9 +45,28 @@ inline std::optional<registries::HeaderAndUrl>
     {
         return HeaderAndUrl{openbmc::header, openbmc::url};
     }
+    if (openbmc_logging::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{openbmc_logging::header, openbmc_logging::url};
+    }
+    if (openbmc_state_cable::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{openbmc_state_cable::header,
+                            openbmc_state_cable::url};
+    }
+    if (openbmc_state_leak_detector_group::header.registryPrefix ==
+        registryName)
+    {
+        return HeaderAndUrl{openbmc_state_leak_detector_group::header,
+                            openbmc_state_leak_detector_group::url};
+    }
     if (resource_event::header.registryPrefix == registryName)
     {
         return HeaderAndUrl{resource_event::header, resource_event::url};
+    }
+    if (sensor_event::header.registryPrefix == registryName)
+    {
+        return HeaderAndUrl{sensor_event::header, sensor_event::url};
     }
     if (task_event::header.registryPrefix == registryName)
     {
@@ -58,6 +86,10 @@ inline std::span<const MessageEntry> getRegistryFromPrefix(
     {
         return {base::registry};
     }
+    if (environmental::header.registryPrefix == registryName)
+    {
+        return {environmental::registry};
+    }
     if (heartbeat_event::header.registryPrefix == registryName)
     {
         return {heartbeat_event::registry};
@@ -66,9 +98,26 @@ inline std::span<const MessageEntry> getRegistryFromPrefix(
     {
         return {openbmc::registry};
     }
+    if (openbmc_logging::header.registryPrefix == registryName)
+    {
+        return {openbmc_logging::registry};
+    }
+    if (openbmc_state_cable::header.registryPrefix == registryName)
+    {
+        return {openbmc_state_cable::registry};
+    }
+    if (openbmc_state_leak_detector_group::header.registryPrefix ==
+        registryName)
+    {
+        return {openbmc_state_leak_detector_group::registry};
+    }
     if (resource_event::header.registryPrefix == registryName)
     {
         return {resource_event::registry};
+    }
+    if (sensor_event::header.registryPrefix == registryName)
+    {
+        return {sensor_event::registry};
     }
     if (task_event::header.registryPrefix == registryName)
     {
