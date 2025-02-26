34a17b561cf0648f4543d068632236f0d2beeb72
Malik Akbar Hashemi Rafsanjani <malikrafsan@meta.com>
2025-02-26 13:14:30 -0800

    mtls: implement UPN parse mode
    This commit is intended to implement the UserPrincipalName (UPN) parse
    mode on mutual TLS (MTLS). By implementing this we can use the X509
    certificate extension Subject Alternative Name (SAN), specifically UPN
    to be used as the username
    
    In our case, this feature is needed because we have a specific format
    on our Subject CN of X509 certificate. This format cannot directly
    mapped to the username of bmcweb because it contains special
    characters (`/` and `:`), which cannot exist in the username.
    Changing the format of our Subject CN is very risky. By enabling
    this feature we can use other field, which is the SAN extension to
    be used as the username and do not change our Subject CN on the
    X509 certificate
    
    In general, by implementing this feature, we can enable multiple
    options for the system. There might be other cases where we want to
    have the username of the bmcweb is not equal to the Subject CN of the
    certificate, instead the username is added as the UserPrincipalName
    field in the certificate
    
    Tested
    - Query using UPN certificate [1]
        - Check UPN field on X509 certificate
        - Make sure the UPN name exist inside the bmcweb accounts
        - Change the `CertificateMappingAttribute` to use UPN
        - Make sure we are still able to query the device
    - Run unit tests
    
    [1] https://justpaste.it/adq6g
    
    Change-Id: I490da8b95aee9579546971e58ab2c4afd64c5997
    Signed-off-by: Malik Akbar Hashemi Rafsanjani <malikrafsan@meta.com>

--- http/mutual_tls.hpp
+++ http/mutual_tls.hpp
@@ -4,6 +4,8 @@
 
 #include "sessions.hpp"
 
+#include <openssl/crypto.h>
+
 #include <boost/asio/ip/address.hpp>
 #include <boost/asio/ssl/verify_context.hpp>
 
@@ -11,7 +13,15 @@
 #include <string>
 #include <string_view>
 
-std::string getUsernameFromCommonName(std::string_view commonName);
+std::string getCommonNameFromCert(X509* cert);
+
+std::string getUPNFromCert(X509* peerCert, std::string_view hostname);
+
+std::string getMetaUserNameFromCert(X509* cert);
+
+std::string getUsernameFromCert(X509* cert);
+
+bool isUPNMatch(std::string_view upn, std::string_view hostname);
 
 std::shared_ptr<persistent_data::UserSession> verifyMtlsUser(
     const boost::asio::ip::address& clientIp,
