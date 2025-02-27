74462fa782aba63d7ff5f63b56b473954a2e6f1a
Patrick Williams <patrick@stwcx.xyz>
2025-02-27 18:39:58 -0500

    registries: generate registries_selector.hpp
    Enhance parse_registries.py to generate the registries_selector.hpp
    so that it does not need to be generated by hand.
    
    Note that as part of this, additional registries are now emitted for
    Redfish.  These registries were already being generated but they were
    missing the header hook that allowed them to be exposed.
    
    Tested:
    ```
    $ curl -s -k https://localhost:18080/redfish/v1/Registries/ | jq '.Members | map(."@odata.id")'
    [
      "/redfish/v1/Registries/Base",
      "/redfish/v1/Registries/Composition",
      "/redfish/v1/Registries/EthernetFabric",
      "/redfish/v1/Registries/Fabric",
      "/redfish/v1/Registries/HeartbeatEvent",
      "/redfish/v1/Registries/OpenBMC",
      "/redfish/v1/Registries/StorageDevice",
      "/redfish/v1/Registries/Power",
      "/redfish/v1/Registries/Telemetry",
      "/redfish/v1/Registries/LogService",
      "/redfish/v1/Registries/Update",
      "/redfish/v1/Registries/SensorEvent",
      "/redfish/v1/Registries/ResourceEvent",
      "/redfish/v1/Registries/Environmental",
      "/redfish/v1/Registries/TaskEvent",
      "/redfish/v1/Registries/License",
      "/redfish/v1/Registries/NetworkDevice",
      "/redfish/v1/Registries/Platform",
      "/redfish/v1/Registries/JobEvent"
    ]
    ```
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ia243a7bfbb8873384b6679b462914ba8380bf5b3

--- redfish-core/include/registries_selector.hpp
+++ redfish-core/include/registries_selector.hpp
@@ -1,17 +1,24 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 #pragma once
-// NOLINTNEXTLINE(misc-include-cleaner)
-#include "registries.hpp"
-// NOLINTNEXTLINE(misc-include-cleaner)
+// NOLINTBEGIN(misc-include-cleaner)
 #include "registries/base_message_registry.hpp"
-// NOLINTNEXTLINE(misc-include-cleaner)
+#include "registries/composition_message_registry.hpp"
+#include "registries/environmental_message_registry.hpp"
+#include "registries/ethernet_fabric_message_registry.hpp"
+#include "registries/fabric_message_registry.hpp"
 #include "registries/heartbeat_event_message_registry.hpp"
-// NOLINTNEXTLINE(misc-include-cleaner)
+#include "registries/job_event_message_registry.hpp"
+#include "registries/license_message_registry.hpp"
+#include "registries/log_service_message_registry.hpp"
+#include "registries/network_device_message_registry.hpp"
 #include "registries/openbmc_message_registry.hpp"
-// NOLINTNEXTLINE(misc-include-cleaner)
+#include "registries/platform_message_registry.hpp"
+#include "registries/power_message_registry.hpp"
 #include "registries/resource_event_message_registry.hpp"
-// NOLINTNEXTLINE(misc-include-cleaner)
+#include "registries/sensor_event_message_registry.hpp"
+#include "registries/storage_device_message_registry.hpp"
 #include "registries/task_event_message_registry.hpp"
-// NOLINTNEXTLINE(misc-include-cleaner)
 #include "registries/telemetry_message_registry.hpp"
+#include "registries/update_message_registry.hpp"
+// NOLINTEND(misc-include-cleaner)
