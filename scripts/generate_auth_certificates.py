8291e5e420b786564250b3e7ed2fd95249a1f230
Malik Akbar Hashemi Rafsanjani <malikrafsan@meta.com>
2025-03-12 10:43:48 -0700

    extend test script to cover upn testing
    This commit is intended to extend existing `generate_auth_certificate` python script that automatically test the auth functionality of bmcweb. We extend the script by adding test for UserPrincipalName (UPN) feature. This feature [1] allow us to use SubjectAlternativeName (SAN) extension on X509 certificate and enable us to use the different name as username. Previously we can only use CommonName in the certificate as username
    
    By adding this changes, we can test the UPN feature easily using the script. We add a new flag that enable the user to test using UPN feature by specifying the UPN name to be tested
    
    Tested:
    - Regress test on CommonName by running without `--upn` flag
    - Test using correct UPN name
      - There are two requirements for the UPN name (`username@domain`)
      - `username` must exist in the BMC device accounts
      - `domain` must match the domain forest of the device
      - eg: malik@fb.com match macbmc1.abc.fb.com
    - Test using incorrect UPN name
      - Violate one of the requirements and the test should fail
    
    [1] Patch feature: https://gerrit.openbmc.org/c/openbmc/bmcweb/+/78519
    
    Change-Id: I997bea9a6662fa41c3824fde71ea4f20b606ca9c
    Signed-off-by: Malik Akbar Hashemi Rafsanjani <malikrafsan@meta.com>

--- scripts/generate_auth_certificates.py
+++ scripts/generate_auth_certificates.py
@@ -12,18 +12,21 @@ import ipaddress
 import os
 import socket
 import time
+from typing import Optional
 
+import asn1
 import httpx
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.serialization import (
-    load_pem_private_key,
-    pkcs12,
-)
+from cryptography.hazmat.primitives.serialization import load_pem_private_key, pkcs12
 from cryptography.x509.oid import NameOID
 
-replaceCertPath = "/redfish/v1/CertificateService/Actions/CertificateService.ReplaceCertificate"
+replaceCertPath = (
+    "/redfish/v1/CertificateService/Actions/CertificateService.ReplaceCertificate"
+)
+# https://oidref.com/1.3.6.1.4.1.311.20.2.3
+upnObjectIdentifier = "1.3.6.1.4.1.311.20.2.3"
 
 
 class RedfishSessionContext:
@@ -100,9 +103,7 @@ def generateCA():
 
     builder = builder.add_extension(auth_key, critical=False)
 
-    root_cert = builder.sign(
-        private_key=private_key, algorithm=hashes.SHA256()
-    )
+    root_cert = builder.sign(private_key=private_key, algorithm=hashes.SHA256())
 
     return private_key, root_cert
 
@@ -112,7 +113,7 @@ def signCsr(csr, ca_key):
     return
 
 
-def generate_client_key_and_cert(commonName, ca_cert, ca_key):
+def generate_client_key_and_cert(commonName, ca_cert, ca_key, upn: Optional[str]):
     private_key = ec.generate_private_key(ec.SECP256R1())
     public_key = private_key.public_key()
     builder = x509.CertificateBuilder()
@@ -121,9 +122,7 @@ def generate_client_key_and_cert(commonName, ca_cert, ca_key):
         x509.Name(
             [
                 x509.NameAttribute(NameOID.COUNTRY_NAME, "US"),
-                x509.NameAttribute(
-                    NameOID.STATE_OR_PROVINCE_NAME, "California"
-                ),
+                x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "California"),
                 x509.NameAttribute(NameOID.LOCALITY_NAME, "San Francisco"),
                 x509.NameAttribute(NameOID.ORGANIZATION_NAME, "OpenBMC"),
                 x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, "bmcweb"),
@@ -160,6 +159,23 @@ def generate_client_key_and_cert(commonName, ca_cert, ca_key):
 
     auth_key = x509.AuthorityKeyIdentifier.from_issuer_public_key(public_key)
     builder = builder.add_extension(auth_key, critical=False)
+
+    if upn is not None:
+        encoder = asn1.Encoder()
+        encoder.start()
+        encoder.write(upn, asn1.Numbers.UTF8String)
+
+        builder = builder.add_extension(
+            x509.SubjectAlternativeName(
+                [
+                    x509.OtherName(
+                        x509.ObjectIdentifier(upnObjectIdentifier),
+                        encoder.output(),
+                    )
+                ]
+            ),
+            critical=False,
+        )
 
     signed = builder.sign(private_key=ca_key, algorithm=hashes.SHA256())
 
@@ -273,10 +289,7 @@ def install_ca_cert(redfish_session, ca_cert_dump, manager_uri):
 
     response = redfish_session.post(ca_certPath, json=ca_certJSON)
     if response.status_code == 500:
-        print(
-            "An existing CA certificate is likely already installed."
-            " Replacing..."
-        )
+        print("An existing CA certificate is likely already installed." " Replacing...")
         ca_certJSON["CertificateUri"] = {
             "@odata.id": ca_certPath + "/1",
         }
@@ -314,9 +327,7 @@ def install_server_cert(redfish_session, manager_uri, server_cert_dump):
 
     tls_patch_json = {"Oem": {"OpenBMC": {"AuthMethods": {"TLS": True}}}}
     print("Ensuring TLS authentication is enabled.")
-    response = redfish_session.patch(
-        "/redfish/v1/AccountService", json=tls_patch_json
-    )
+    response = redfish_session.patch("/redfish/v1/AccountService", json=tls_patch_json)
     if response.status_code == 200:
         print("Successfully enabled TLS authentication.")
     else:
@@ -336,16 +347,45 @@ def generate_pk12(certs_dir, key, client_cert, username):
         f.write(p12)
 
 
-def test_mtls_auth(url, certs_dir):
-    response = httpx.get(
-        f"https://{url}/redfish/v1/SessionService/Sessions",
+def test_mtls_auth(url, certs_dir, upn: Optional[str]):
+    with httpx.Client(
+        base_url=f"https://{url}/redfish/v1",
         verify=os.path.join(certs_dir, "CA-cert.cer"),
         cert=(
             os.path.join(certs_dir, "client-cert.pem"),
             os.path.join(certs_dir, "client-key.pem"),
         ),
-    )
-    response.raise_for_status()
+    ) as client:
+        print("Testing mTLS auth with CommonName")
+        response = client.get("/SessionService/Sessions")
+        response.raise_for_status()
+
+        if upn is None:
+            return
+
+        print("Changing CertificateMappingAttribute to use UPN")
+        patch_json = {
+            "MultiFactorAuth": {
+                "ClientCertificate": {
+                    "CertificateMappingAttribute": "UserPrincipalName"
+                }
+            }
+        }
+        response = client.patch("/AccountService", json=patch_json)
+        response.raise_for_status()
+
+        print("Retesting mTLS auth with UPN")
+        response = client.get("/SessionService/Sessions")
+        response.raise_for_status()
+
+        print("Changing CertificateMappingAttribute to use CommonName")
+        patch_json = {
+            "MultiFactorAuth": {
+                "ClientCertificate": {"CertificateMappingAttribute": "CommonName"}
+            }
+        }
+        response = client.patch("/AccountService", json=patch_json)
+        response.raise_for_status()
 
 
 def setup_server_cert(
@@ -362,9 +402,7 @@ def setup_server_cert(
     service_root = redfish_session.get("/redfish/v1/")
     service_root.raise_for_status()
 
-    manager_uri = service_root.json()["Links"]["ManagerProvidingService"][
-        "@odata.id"
-    ]
+    manager_uri = service_root.json()["Links"]["ManagerProvidingService"]["@odata.id"]
 
     install_ca_cert(redfish_session, ca_cert_dump, manager_uri)
     generate_pk12(certs_dir, client_key, client_cert, username)
@@ -380,9 +418,7 @@ def setup_server_cert(
         ca_cert,
         csr,
     )
-    server_cert_dump = serverCert.public_bytes(
-        encoding=serialization.Encoding.PEM
-    )
+    server_cert_dump = serverCert.public_bytes(encoding=serialization.Encoding.PEM)
     with open(os.path.join(certs_dir, "server-cert.pem"), "wb") as f:
         f.write(server_cert_dump)
         print("Server cert generated.")
@@ -390,7 +426,7 @@ def setup_server_cert(
     install_server_cert(redfish_session, manager_uri, server_cert_dump)
 
 
-def generate_and_load_certs(url, username, password):
+def generate_and_load_certs(url, username, password, upn: Optional[str]):
     certs_dir = os.path.expanduser("~/certs")
     print(f"Writing certs to {certs_dir}")
     try:
@@ -410,9 +446,7 @@ def generate_and_load_certs(url, username, password):
             format=serialization.PrivateFormat.TraditionalOpenSSL,
             encryption_algorithm=serialization.NoEncryption(),
         )
-        ca_cert_dump = ca_cert.public_bytes(
-            encoding=serialization.Encoding.PEM
-        )
+        ca_cert_dump = ca_cert.public_bytes(encoding=serialization.Encoding.PEM)
 
         with open(ca_cert_filename, "wb") as f:
             f.write(ca_cert_dump)
@@ -430,7 +464,7 @@ def generate_and_load_certs(url, username, password):
     ca_key = load_pem_private_key(ca_key_dump, None)
 
     client_key, client_cert = generate_client_key_and_cert(
-        username, ca_cert, ca_key
+        username, ca_cert, ca_key, upn
     )
     client_key_dump = client_key.private_bytes(
         encoding=serialization.Encoding.PEM,
@@ -441,9 +475,7 @@ def generate_and_load_certs(url, username, password):
     with open(os.path.join(certs_dir, "client-key.pem"), "wb") as f:
         f.write(client_key_dump)
         print("Client key generated.")
-    client_cert_dump = client_cert.public_bytes(
-        encoding=serialization.Encoding.PEM
-    )
+    client_cert_dump = client_cert.public_bytes(encoding=serialization.Encoding.PEM)
 
     with open(os.path.join(certs_dir, "client-cert.pem"), "wb") as f:
         f.write(client_cert_dump)
@@ -453,9 +485,7 @@ def generate_and_load_certs(url, username, password):
     with httpx.Client(
         base_url=f"https://{url}", verify=False, follow_redirects=False
     ) as redfish_session:
-        with RedfishSessionContext(
-            redfish_session, username, password
-        ) as rf_session:
+        with RedfishSessionContext(redfish_session, username, password) as rf_session:
             redfish_session.headers["X-Auth-Token"] = rf_session.x_auth_token
             setup_server_cert(
                 redfish_session,
@@ -472,7 +502,7 @@ def generate_and_load_certs(url, username, password):
     print("Testing redfish TLS authentication with generated certs.")
 
     time.sleep(2)
-    test_mtls_auth(url, certs_dir)
+    test_mtls_auth(url, certs_dir, upn)
     print("Redfish TLS authentication success!")
 
 
@@ -488,10 +518,15 @@ def main():
         help="Password for user in order to install certs over Redfish.",
         default="0penBmc",
     )
+    parser.add_argument(
+        "--upn",
+        help="Use user principal name instead",
+        default=None,
+    )
     parser.add_argument("host", help="Host to connect to")
 
     args = parser.parse_args()
-    generate_and_load_certs(args.host, args.username, args.password)
+    generate_and_load_certs(args.host, args.username, args.password, args.upn)
 
 
 if __name__ == "__main__":
