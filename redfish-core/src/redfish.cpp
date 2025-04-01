d73ccd5513924bad2e4e7d4def92d1a9e071b119
Prithvi Pai <ppai@nvidia.com>
2025-04-01 23:49:48 +0530

    Enable BIOS Password change action
    Added a patch to change bios password remotely.
    The backend support to the same is already implemented
    in `bios-settings-manager`.
    https://github.com/openbmc/bios-settings-mgr.
    
    `bios-settings-manager` exposes a method `ChangePassword`
    which takes following arguments as input
    1) PasswordName(string)
    2) OldPassword(string)
    3) NewPassword(string)
    
    The redfish schema exposing the Action is:
    https://redfish.dmtf.org/schemas/v1/Bios.v1_2_3.json
    
    Tested:
    1) POST request with PasswordName, OldPassword and NewPassword
    is triggered. On successful password update HTTP 200 OK code is
    exposed.
    2) POST request with PasswordName, wrong OldPassword and
    NewPassword is triggered. 400 Bad Request code is exposed with
    [1] "Message": "The value for the parameter \"OldPassword\" in the action ChangePassword is invalid.",
    [2] "MessageArgs": "\"OldPassword\"", "ChangePassword"],
    [3] "MessageId": "Base.1.18.1.ActionParameterValueError",
    [4] "MessageSeverity": "Warning",
    [5] "Resolution": "Correct the value for the parameter in the request body and resubmit the request if the operation failed."
    
    Change-Id: I9fa19553a4b2459d22b398029768a21eed4a8c7a
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>

--- redfish-core/src/redfish.cpp
+++ redfish-core/src/redfish.cpp
@@ -164,6 +164,7 @@ RedfishService::RedfishService(App& app)
 
     requestRoutesBiosService(app);
     requestRoutesBiosReset(app);
+    requestRoutesBiosChangePassword(app);
 
     if constexpr (BMCWEB_VM_NBDPROXY)
     {
