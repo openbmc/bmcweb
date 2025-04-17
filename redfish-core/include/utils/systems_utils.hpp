eb5b7ad92c3889d787984fe887a2bc7ced764322
Janet Adkins <janeta@us.ibm.com>
2025-04-17 10:30:43 -0500

    Systems: Update LocationIndicatorActive property
    Modify get/set of LocationIndicatorActive property for Systems to use
    identifying association instead of hard-coding led group.[1]
    
    History: Almost 5 years ago IBM added support for this property to the
    Systems.[2] That original implementation assumed the system and chassis
    shared the same LED and just looked at the enclosure_identify_blink and
    enclosure_identify like the existing IndicatorLED property did.
    
    IBM renamed these functions getSystemLocationIndicatorActive and
    setSystemLocationIndicatorActive.[3]
    
    The interest from other companies has mostly been around IndicatorLED
    (old deprecated LED property).[4]
    
    Today, LEDs have the association documented above and used elsewhere
    like PowerSupplies, Fans, etc. Switching to this association: 1) follows
    the design 2) allows multiple chassis support 3) doesn't assume your
    system led is your chassis led.
    
    There is a commit with the equivalent change to Chassis
    implementation of LocationIndicatorActive.[5] The use for Systems is the
    only other caller so this commit also removes the
    getSystemLocationIndicatorActive() and
    setSystemLocationIndicatorActive() functions.
    
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/58299
    [2] https://gerrit.openbmc.org/c/openbmc/bmcweb/+/36886
    [3] https://gerrit.openbmc.org/c/openbmc/bmcweb/+/57765
    [4] https://gerrit.openbmc.org/c/openbmc/bmcweb/+/27301
    [5] https://gerrit.openbmc.org/c/openbmc/bmcweb/+/81886
    
    Tested:
     - Redfish Service Validator passes
     - Confirm able to set and get LED
    
    1. Get for Systems
    ```
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system
    {
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_22_0.ComputerSystem",
      ...
      "LocationIndicatorActive": false,
      ...
    }
    ```
    
    2. Set for Systems
    ```
    curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X PATCH -d '{"LocationIndicatorActive": true}' https://${bmc}/redfish/v1/Systems/system
    
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_22_0.ComputerSystem",
      ...
      "LocationIndicatorActive": true,
      ...
    }
    ```
    Change-Id: I1c06621586148d4b299b1f8e1ee1fb0ccdc51f10
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Signed-off-by: Janet Adkins <janeta@us.ibm.com>
    Co-authored-by: George Liu <liuxiwei@inspur.com>

--- redfish-core/include/utils/systems_utils.hpp
+++ redfish-core/include/utils/systems_utils.hpp
@@ -116,4 +116,62 @@ inline void getSystemCollectionMembers(
         "/xyz/openbmc_project/inventory", 0, interfaces,
         std::bind_front(handleSystemCollectionMembers, asyncResp));
 }
+
+namespace systems_utils
+{
+
+inline void afterGetValidSystemsPath(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemId,
+    const std::function<void(const std::optional<std::string>&)>& callback,
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreePathsResponse& systemsPaths)
+{
+    if (ec)
+    {
+        if (ec == boost::system::errc::io_error)
+        {
+            BMCWEB_LOG_DEBUG("No systems found");
+            callback("");
+            return;
+        }
+        BMCWEB_LOG_ERROR("DBUS response error: {}", ec.value());
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    for (const std::string& system : systemsPaths)
+    {
+        sdbusplus::message::object_path path(system);
+        if (path.filename() == systemId)
+        {
+            callback(path);
+            return;
+        }
+    }
+    BMCWEB_LOG_DEBUG("No system named {} found", systemId);
+    callback("");
+}
+
+inline void getValidSystemsPath(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemId,
+    std::function<void(const std::optional<std::string>&)>&& callback)
+{
+    BMCWEB_LOG_DEBUG("Get path for {}", systemId);
+
+    constexpr std::array<std::string_view, 1> interfaces = {
+        "xyz.openbmc_project.Inventory.Item.System"};
+    dbus::utility::getSubTreePaths(
+        "/xyz/openbmc_project/inventory", 0, interfaces,
+        [asyncResp, systemId, callback{std::move(callback)}](
+            const boost::system::error_code& ec,
+            const dbus::utility::MapperGetSubTreePathsResponse& systemsPaths) {
+            afterGetValidSystemsPath(asyncResp, systemId, callback, ec,
+                                     systemsPaths);
+            return;
+        });
+}
+
+} // namespace systems_utils
 } // namespace redfish
