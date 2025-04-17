e8102a37352a5306090b91549a8c02bff25c0104
Chandramohan Harkude <chandramohan.harkude@gmail.com>
2025-04-17 13:25:28 +0530

    Add TypeDefinition support for enumeration generation
    TypeDefinition was not supported by generate_schema_enums.py, this
    change is to support the 'TypeDefinition' generation from schema.
    
    Tested:
    'TypeDefinition' fields in schema can be generated in generated files
    with the change
    
    Change-Id: Ibe61f65b905f2089f9e17a26fbd27e3ff1753166
    Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

--- redfish-core/include/generated/enums/serial_interface.hpp
+++ redfish-core/include/generated/enums/serial_interface.hpp
@@ -13,6 +13,19 @@ enum class SignalType{
     Rs485,
 };
 
+enum class BitRate{
+    Invalid,
+    BitRate1200,
+    BitRate2400,
+    BitRate4800,
+    BitRate9600,
+    BitRate19200,
+    BitRate38400,
+    BitRate57600,
+    BitRate115200,
+    BitRate230400,
+};
+
 enum class Parity{
     Invalid,
     None,
@@ -20,6 +33,20 @@ enum class Parity{
     Odd,
     Mark,
     Space,
+};
+
+enum class DataBits{
+    Invalid,
+    DataBits5,
+    DataBits6,
+    DataBits7,
+    DataBits8,
+};
+
+enum class StopBits{
+    Invalid,
+    StopBits1,
+    StopBits2,
 };
 
 enum class FlowControl{
@@ -36,10 +63,36 @@ enum class PinOut{
     Digi,
 };
 
+enum class ConnectorType{
+    Invalid,
+    RJ45,
+    RJ11,
+    DB9Female,
+    DB9Male,
+    DB25Female,
+    DB25Male,
+    USB,
+    mUSB,
+    uUSB,
+};
+
 NLOHMANN_JSON_SERIALIZE_ENUM(SignalType, {
     {SignalType::Invalid, "Invalid"},
     {SignalType::Rs232, "Rs232"},
     {SignalType::Rs485, "Rs485"},
+});
+
+NLOHMANN_JSON_SERIALIZE_ENUM(BitRate, {
+    {BitRate::Invalid, "Invalid"},
+    {BitRate::BitRate1200, "1200"},
+    {BitRate::BitRate2400, "2400"},
+    {BitRate::BitRate4800, "4800"},
+    {BitRate::BitRate9600, "9600"},
+    {BitRate::BitRate19200, "19200"},
+    {BitRate::BitRate38400, "38400"},
+    {BitRate::BitRate57600, "57600"},
+    {BitRate::BitRate115200, "115200"},
+    {BitRate::BitRate230400, "230400"},
 });
 
 NLOHMANN_JSON_SERIALIZE_ENUM(Parity, {
@@ -49,6 +102,20 @@ NLOHMANN_JSON_SERIALIZE_ENUM(Parity, {
     {Parity::Odd, "Odd"},
     {Parity::Mark, "Mark"},
     {Parity::Space, "Space"},
+});
+
+NLOHMANN_JSON_SERIALIZE_ENUM(DataBits, {
+    {DataBits::Invalid, "Invalid"},
+    {DataBits::DataBits5, "5"},
+    {DataBits::DataBits6, "6"},
+    {DataBits::DataBits7, "7"},
+    {DataBits::DataBits8, "8"},
+});
+
+NLOHMANN_JSON_SERIALIZE_ENUM(StopBits, {
+    {StopBits::Invalid, "Invalid"},
+    {StopBits::StopBits1, "1"},
+    {StopBits::StopBits2, "2"},
 });
 
 NLOHMANN_JSON_SERIALIZE_ENUM(FlowControl, {
@@ -63,6 +130,19 @@ NLOHMANN_JSON_SERIALIZE_ENUM(PinOut, {
     {PinOut::Cisco, "Cisco"},
     {PinOut::Cyclades, "Cyclades"},
     {PinOut::Digi, "Digi"},
+});
+
+NLOHMANN_JSON_SERIALIZE_ENUM(ConnectorType, {
+    {ConnectorType::Invalid, "Invalid"},
+    {ConnectorType::RJ45, "RJ45"},
+    {ConnectorType::RJ11, "RJ11"},
+    {ConnectorType::DB9Female, "DB9 Female"},
+    {ConnectorType::DB9Male, "DB9 Male"},
+    {ConnectorType::DB25Female, "DB25 Female"},
+    {ConnectorType::DB25Male, "DB25 Male"},
+    {ConnectorType::USB, "USB"},
+    {ConnectorType::mUSB, "mUSB"},
+    {ConnectorType::uUSB, "uUSB"},
 });
 
 }
