5069799f9949cc6da00116cf2f9c8b9ad577fcde
Rohit PAI <ropai@nvidia.com>
2025-04-25 23:06:00 +0530

    RFA fix to consider Satellite Info from EM
    Removal of hard coded prefix while starting the aggregation
    
    Tested
    - Tested with multi satmc aggregation
    EM Configs
            {
                "Hostname": "10.0.2.2",
                "Port": "8000",
                "Name": "sat0",
                "Type": "SatelliteController",
                "AuthType": "None"
            },
            ,
            {
                "Hostname": "10.0.2.2",
                "Port": "8001",
                "Name": "sat1",
                "Type": "SatelliteController",
                "AuthType": "None"
            }
    
    Aggregation Responses
    ```
    curl -k -u root:0penBmc https://localhost:2443/redfish/v1/AggregationService/AggregationSources/
    {
      "@odata.id": "/redfish/v1/AggregationService/AggregationSources",
      "@odata.type": "#AggregationSourceCollection.AggregationSourceCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/AggregationService/AggregationSources/sat1"
        },
        {
          "@odata.id": "/redfish/v1/AggregationService/AggregationSources/sat0"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Aggregation Source Collection"
    }
    ```
    
    Resource Aggregation
    ```
    curl -k -u root:0penBmc https://localhost:2443/redfish/v1/Systems
    {
      "@odata.id": "/redfish/v1/Systems",
      "@odata.type": "#ComputerSystemCollection.ComputerSystemCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system"
        },
        {
          "@odata.id": "/redfish/v1/Systems/sat1_system1"
        },
        {
          "@odata.id": "/redfish/v1/Systems/sat0_system1"
        }
      ],
      "Members@odata.count": 3,
      "Name": "Computer System Collection"
    }
    ```
    
    - Tested aggregation schema and RF validator
    
    Change-Id: I748d72087dc981747231615adde0d10eab14ee08
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- redfish-core/include/redfish_aggregator.hpp
+++ redfish-core/include/redfish_aggregator.hpp
@@ -727,6 +727,31 @@ class RedfishAggregator
         messages::resourceNotFound(asyncResp->res, "", nameStr);
     }
 
+    // Check if a collection item matches any satellite prefix
+    static bool isSatelliteItem(std::string_view itemName)
+    {
+        const auto& satelliteInfo = getInstance().cachedSatelliteInfo;
+        if (satelliteInfo.empty())
+        {
+            // If cache is empty, try to refresh it for future use
+            getSatelliteConfigs(callbackUpdateSatelliteConfig);
+            return false;
+        }
+
+        for (const auto& satellite : satelliteInfo)
+        {
+            std::string targetPrefix = satellite.first;
+            targetPrefix += "_";
+            if (itemName.starts_with(targetPrefix))
+            {
+                BMCWEB_LOG_DEBUG("{} matches satellite prefix {}", itemName,
+                                 satellite.first);
+                return true;
+            }
+        }
+        return false;
+    }
+
     // Intended to handle an incoming request based on if Redfish Aggregation
     // is enabled.  Forwards request to satellite BMC if it exists.
     static void aggregateAndHandle(
@@ -1387,14 +1412,7 @@ class RedfishAggregator
             {
                 // We've matched a resource collection so this current segment
                 // might contain an aggregation prefix
-                // TODO: This needs to be rethought when we can support multiple
-                // satellites due to
-                // /redfish/v1/AggregationService/AggregationSources/BMCWEB_REDFISH_SATELLITE_PREFIX
-                // being a local resource describing the satellite
-                std::string targetPrefix =
-                    std::string(BMCWEB_REDFISH_SATELLITE_PREFIX);
-                targetPrefix += "_";
-                if (collectionItem.starts_with(targetPrefix))
+                if (isSatelliteItem(collectionItem))
                 {
                     BMCWEB_LOG_DEBUG("Need to forward a request");
 
