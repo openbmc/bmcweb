455457093a857f06233a5469c387ab95ab747519
Rohit PAI <ropai@nvidia.com>
2025-05-05 11:33:37 +0530

    To test CI
    Change-Id: I8c7844556a2a71a022c764e95170bd900713b9c0
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/webserver_main.cpp
+++ src/webserver_main.cpp
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#include "logging.hpp"
 #include "webserver_run.hpp"
 
 int main(int /*argc*/, char** /*argv*/) noexcept(false)
 {
+    BMCWEB_LOG_INFO("Starting BMC Web Server");
     return run();
 }
