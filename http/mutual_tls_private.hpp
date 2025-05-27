b39f038bff91812ddd06556ee06a566d3595f06d
Malik Akbar Hashemi Rafsanjani <malikrafsan@meta.com>
2025-05-27 13:23:44 -0700

    remove meta mtls parse mode
    Change-Id: Idcf4340a2a9940f035aea41cd30ef4df7bd95530
    Signed-off-by: Malik Akbar Hashemi Rafsanjani <malikrafsan@meta.com>

--- http/mutual_tls_private.hpp
+++ http/mutual_tls_private.hpp
@@ -11,8 +11,6 @@ std::string getCommonNameFromCert(X509* cert);
 
 std::string getUPNFromCert(X509* peerCert, std::string_view hostname);
 
-std::string getMetaUserNameFromCert(X509* cert);
-
 std::string getUsernameFromCert(X509* cert);
 
 bool isUPNMatch(std::string_view upn, std::string_view hostname);
