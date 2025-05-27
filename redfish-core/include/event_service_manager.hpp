960b1100e2f187447d924e4d0a127ae21cb4971e
Ed Tanous <ed@tanous.net>
2025-05-27 16:24:12 -0700

    Remove old load config
    This was created to port configurations forward from old systems 4 years
    ago.  If there are systems that haven't been incermentally moved forward
    at this point, they are few and far between, and likely will never
    update.
    
    Remove the functionality.
    
    For reference, the original commit adding this was
    28afb49c480790e763b8491be0b5a8e35964dbc9
    
    Tested: bmcweb boots.  Basic functions (GET /redfish/v1) work properly.
    
    Change-Id: I42415662cabc540b4b0a65666b755df28034be46
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/include/event_service_manager.hpp
+++ redfish-core/include/event_service_manager.hpp
@@ -98,8 +98,6 @@ class EventServiceManager
 
     void initConfig()
     {
-        loadOldBehavior();
-
         persistent_data::EventServiceConfig eventServiceConfig =
             persistent_data::EventServiceStore::getInstance()
                 .getEventServiceConfig();
@@ -141,105 +139,6 @@ class EventServiceManager
             if (subValue->userSub->sendHeartbeat)
             {
                 subValue->scheduleNextHeartbeatEvent();
-            }
-        }
-    }
-
-    static void loadOldBehavior()
-    {
-        std::ifstream eventConfigFile(eventServiceFile);
-        if (!eventConfigFile.good())
-        {
-            BMCWEB_LOG_DEBUG("Old eventService config not exist");
-            return;
-        }
-        auto jsonData = nlohmann::json::parse(eventConfigFile, nullptr, false);
-        if (jsonData.is_discarded())
-        {
-            BMCWEB_LOG_ERROR("Old eventService config parse error.");
-            return;
-        }
-
-        const nlohmann::json::object_t* obj =
-            jsonData.get_ptr<const nlohmann::json::object_t*>();
-        if (obj == nullptr)
-        {
-            return;
-        }
-        for (const auto& item : *obj)
-        {
-            if (item.first == "Configuration")
-            {
-                persistent_data::EventServiceStore::getInstance()
-                    .getEventServiceConfig()
-                    .fromJson(item.second);
-            }
-            else if (item.first == "Subscriptions")
-            {
-                for (const auto& elem : item.second)
-                {
-                    std::optional<persistent_data::UserSubscription>
-                        newSubscription =
-                            persistent_data::UserSubscription::fromJson(elem,
-                                                                        true);
-                    if (!newSubscription)
-                    {
-                        BMCWEB_LOG_ERROR("Problem reading subscription "
-                                         "from old persistent store");
-                        continue;
-                    }
-                    persistent_data::UserSubscription& newSub =
-                        *newSubscription;
-
-                    std::uniform_int_distribution<uint32_t> dist(0);
-                    bmcweb::OpenSSLGenerator gen;
-
-                    std::string id;
-
-                    int retry = 3;
-                    while (retry != 0)
-                    {
-                        id = std::to_string(dist(gen));
-                        if (gen.error())
-                        {
-                            retry = 0;
-                            break;
-                        }
-                        newSub.id = id;
-                        auto inserted =
-                            persistent_data::EventServiceStore::getInstance()
-                                .subscriptionsConfigMap.insert(std::pair(
-                                    id, std::make_shared<
-                                            persistent_data::UserSubscription>(
-                                            newSub)));
-                        if (inserted.second)
-                        {
-                            break;
-                        }
-                        --retry;
-                    }
-
-                    if (retry <= 0)
-                    {
-                        BMCWEB_LOG_ERROR(
-                            "Failed to generate random number from old "
-                            "persistent store");
-                        continue;
-                    }
-                }
-            }
-
-            persistent_data::getConfig().writeData();
-            std::error_code ec;
-            std::filesystem::remove(eventServiceFile, ec);
-            if (ec)
-            {
-                BMCWEB_LOG_DEBUG(
-                    "Failed to remove old event service file.  Ignoring");
-            }
-            else
-            {
-                BMCWEB_LOG_DEBUG("Remove old eventservice config");
             }
         }
     }
