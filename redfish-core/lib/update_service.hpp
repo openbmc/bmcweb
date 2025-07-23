5d9b61ef82bec6be24f8e8a12e05676f8b2a3a1f
rajeeranjan <ranjan.rajeev1609@gmail.com>
2025-07-23 23:25:52 +0530

    Allow fw update when no targets are specified
    Targets shouldn't be mandated when Multipart firmware update is
    performed. When targets are not provided in multipart firmware updates,
    automatically select the first available software object that implements
    the update interface.
    
    This assumes that on such systems, only one software object will
    implement the StartUpdate D-Bus method. Systems with multiple updateable
    components will receive an error if targets are not specified.
    
    This change is required for PLDM firmware update support where PLDM
    daemon internally handles the firmware distribution to the appropriate
    components. PLDM provides a single StartUpdate API [1] that will manage
    the complexity of updating multiple components internally.
    
    [1] https://gerrit.openbmc.org/c/openbmc/pldm/+/79192
    
    Signed-off-by: Rajeev Ranjan <ranjan.rajeev1609@gmail.com>
    Change-Id: I294b1447b6faf2055419d3659f9c963aeb6d5d0d

--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -688,7 +688,7 @@ struct MultiPartUpdateParameters
 {
     std::optional<std::string> applyTime;
     std::string uploadData;
-    std::vector<std::string> targets;
+    std::optional<std::vector<std::string>> targets;
 };
 
 inline std::optional<std::string> processUrl(
@@ -752,7 +752,6 @@ inline std::optional<MultiPartUpdateParameters>
 
             if (param.second == "UpdateParameters")
             {
-                std::vector<std::string> tempTargets;
                 nlohmann::json content =
                     nlohmann::json::parse(formpart.content, nullptr, false);
                 if (content.is_discarded())
@@ -771,33 +770,34 @@ inline std::optional<MultiPartUpdateParameters>
                 if (!json_util::readJsonObject(                            //
                         *obj, asyncResp->res,                              //
                         "@Redfish.OperationApplyTime", multiRet.applyTime, //
-                        "Targets", tempTargets                             //
+                        "Targets", multiRet.targets                        //
                         ))
                 {
                     return std::nullopt;
                 }
 
-                for (size_t urlIndex = 0; urlIndex < tempTargets.size();
-                     urlIndex++)
+                if (multiRet.targets)
                 {
-                    const std::string& target = tempTargets[urlIndex];
-                    boost::system::result<boost::urls::url_view> url =
-                        boost::urls::parse_origin_form(target);
-                    auto res = processUrl(url);
-                    if (!res.has_value())
+                    if (multiRet.targets->size() > 1)
                     {
                         messages::propertyValueFormatError(
-                            asyncResp->res, target,
-                            std::format("Targets/{}", urlIndex));
+                            asyncResp->res, *multiRet.targets, "Targets");
                         return std::nullopt;
                     }
-                    multiRet.targets.emplace_back(res.value());
-                }
-                if (multiRet.targets.size() != 1)
-                {
-                    messages::propertyValueFormatError(
-                        asyncResp->res, multiRet.targets, "Targets");
-                    return std::nullopt;
+
+                    for (auto& target : *multiRet.targets)
+                    {
+                        boost::system::result<boost::urls::url_view> url =
+                            boost::urls::parse_origin_form(target);
+                        auto res = processUrl(url);
+                        if (!res.has_value())
+                        {
+                            messages::propertyValueFormatError(
+                                asyncResp->res, target, "Targets");
+                            return std::nullopt;
+                        }
+                        target = res.value();
+                    }
                 }
             }
             else if (param.second == "UpdateFile")
@@ -811,11 +811,6 @@ inline std::optional<MultiPartUpdateParameters>
     {
         BMCWEB_LOG_ERROR("Upload data is NULL");
         messages::propertyMissing(asyncResp->res, "UpdateFile");
-        return std::nullopt;
-    }
-    if (multiRet.targets.empty())
-    {
-        messages::propertyMissing(asyncResp->res, "Targets");
         return std::nullopt;
     }
     return multiRet;
@@ -858,14 +853,11 @@ inline void startUpdate(
 
 inline void getSwInfo(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                       task::Payload payload, const MemoryFileDescriptor& memfd,
-                      const std::string& applyTime, const std::string& target,
+                      const std::string& applyTime,
+                      const std::optional<std::string>& target,
                       const boost::system::error_code& ec,
                       const dbus::utility::MapperGetSubTreeResponse& subtree)
 {
-    using SwInfoMap = std::unordered_map<
-        std::string, std::pair<sdbusplus::message::object_path, std::string>>;
-    SwInfoMap swInfoMap;
-
     if (ec)
     {
         BMCWEB_LOG_ERROR("error_code = {}", ec);
@@ -875,6 +867,38 @@ inline void getSwInfo(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     }
     BMCWEB_LOG_DEBUG("Found {} software version paths", subtree.size());
 
+    if (subtree.empty())
+    {
+        BMCWEB_LOG_WARNING("No software objects found");
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    if (!target)
+    {
+        if (subtree.size() > 1)
+        {
+            BMCWEB_LOG_ERROR(
+                "Multiple software objects found ({}) when no target specified",
+                subtree.size());
+            messages::propertyMissing(asyncResp->res, "Targets");
+            return;
+        }
+
+        const auto& firstEntry = subtree[0];
+        BMCWEB_LOG_DEBUG(
+            "No target specified, using first software object: {} serviceName {}",
+            firstEntry.first, firstEntry.second[0].first);
+
+        startUpdate(asyncResp, std::move(payload), memfd, applyTime,
+                    firstEntry.first, firstEntry.second[0].first);
+        return;
+    }
+
+    using SwInfoMap = std::unordered_map<
+        std::string, std::pair<sdbusplus::message::object_path, std::string>>;
+    SwInfoMap swInfoMap;
+
     for (const auto& entry : subtree)
     {
         sdbusplus::message::object_path path(entry.first);
@@ -882,11 +906,11 @@ inline void getSwInfo(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
         swInfoMap.emplace(swId, make_pair(path, entry.second[0].first));
     }
 
-    auto swEntry = swInfoMap.find(target);
+    auto swEntry = swInfoMap.find(*target);
     if (swEntry == swInfoMap.end())
     {
-        BMCWEB_LOG_WARNING("No valid DBus path for Target URI {}", target);
-        messages::propertyValueFormatError(asyncResp->res, target, "Targets");
+        BMCWEB_LOG_WARNING("No valid DBus path for Target URI {}", *target);
+        messages::propertyValueFormatError(asyncResp->res, *target, "Targets");
         return;
     }
 
@@ -925,7 +949,7 @@ inline void handleBMCUpdate(
 inline void processUpdateRequest(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     task::Payload&& payload, std::string_view body,
-    const std::string& applyTime, std::vector<std::string>& targets)
+    const std::string& applyTime, const std::vector<std::string>& targets)
 {
     MemoryFileDescriptor memfd("update-image");
     if (memfd.fd == -1)
@@ -969,8 +993,13 @@ inline void processUpdateRequest(
              applyTime, targets](const boost::system::error_code& ec,
                                  const dbus::utility::MapperGetSubTreeResponse&
                                      subtree) mutable {
+                std::optional<std::string> target;
+                if (!targets.empty())
+                {
+                    target = targets[0];
+                }
                 getSwInfo(asyncResp, std::move(payload), memfd, applyTime,
-                          targets[0], ec, subtree);
+                          target, ec, subtree);
             });
     }
 }
@@ -1000,9 +1029,10 @@ inline void updateMultipartContext(
         }
         task::Payload payload(req);
 
-        processUpdateRequest(asyncResp, std::move(payload),
-                             multipart->uploadData, applyTimeNewVal,
-                             multipart->targets);
+        processUpdateRequest(
+            asyncResp, std::move(payload), multipart->uploadData,
+            applyTimeNewVal,
+            multipart->targets.value_or(std::vector<std::string>{}));
     }
     else
     {
