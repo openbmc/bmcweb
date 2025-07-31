182289a104fb16af7d04e8ec4aebdcfc34dc3dae
Harshit Aghera <haghera@nvidia.com>
2025-07-31 15:24:55 +0530

    sensor_utils: Add PeakReading property
    Add support for PeakReading and PeakReadingTime for sensors. This
    enhancement allows sensor readings to include max observed value
    information in the Redfish API, along with timestamp.
    
    Schema:
    https://redfish.dmtf.org/schemas/v1/Sensor.v1_2_0.yaml (PeakReading)
    
    PDI for reference:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/78348
    
    Change-Id: I8c1ab6ce85f31419db4a1d931bf99722d24afbd7
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

--- redfish-core/include/utils/sensor_utils.hpp
+++ redfish-core/include/utils/sensor_utils.hpp
@@ -463,10 +463,13 @@ inline void objectPropertiesToJson(
         std::optional<std::string> readingBasis;
         std::optional<std::string> implementation;
         std::optional<std::string> prettyName;
+        std::optional <
+            std::unordered_map<std::string, std::pair<double, uint64_t>>
+                statistic;
         const bool success = sdbusplus::unpackPropertiesNoThrow(
             dbus_utils::UnpackErrorPrinter(), propertiesDict, "Available",
             checkAvailable, "ReadingBasis", readingBasis, "Implementation",
-            implementation, "PrettyName", prettyName);
+            implementation, "PrettyName", prettyName, "Statistic", statistic);
         if (!success)
         {
             messages::internalError();
@@ -519,6 +522,16 @@ inline void objectPropertiesToJson(
             if (prettyName.has_value())
             {
                 sensorJson["Description"] = *prettyName;
+            }
+            if (statistic.has_value())
+            {
+                auto peak = statistic->find(
+                    "xyz.openbmc_project.Common.Statistic.StatsType.Peak");
+                if (peak != statistic->end())
+                {
+                    sensorJson["PeakReading"] = peak->second.first;
+                    sensorJson["PeakReadingTime"] = peak->second.second;
+                }
             }
         }
         else if (sensorType == "temperature")
