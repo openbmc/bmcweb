34d383606541a04ea4edc0bb4887e00dc7dda790
Ed Tanous <etanous@nvidia.com>
2025-08-06 18:47:41 +0200

    Clean up CLI naming
    Now that all applications run through one CLI, names like run() don't
    make a lot of sense.  Update names to match the new reality, make bmcweb
    with no arguments launch the webserver once again.
    
    Tested: bmcweb boots.
    Change-Id: I011b57507872a9518a9c470b58779805504c7293

--- src/webserver_cli.hpp
+++ src/webserver_cli.hpp
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 
-int cliMain(int argc, char** argv) noexcept(false);
+int runCLI(int argc, char** argv) noexcept(false);
