f5842e0b43a277b9943d1dfd33fcd42176b75644
Ed Tanous <ed@tanous.net>
2025-08-18 09:35:00 -0700

    WIP: Implement modernize-use-ranges
    This tidy check can transform code to use std::ranges.  I'm not sure if
    we really want the check itself enabled, but it certainly seems like
    something we could go run once a year to make code cleaner.
    
    Looking for feedback.
    
    [1] https://clang.llvm.org/extra/clang-tidy/checks/modernize/use-ranges.html
    
    Change-Id: I3f21b27d3d30277f71b9c8a2c584a22bc16865e9
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- src/webserver_cli.cpp
+++ src/webserver_cli.cpp
@@ -29,7 +29,7 @@ static constexpr std::array<std::string, 7> levels{
 // Check if debug level is valid
 static std::string validateLogLevel(std::string& input)
 {
-    std::transform(input.begin(), input.end(), input.begin(), ::toupper);
+    std::ranges::transform(input, input.begin(), ::toupper);
     const std::string* iter = std::ranges::find(levels, input);
     if (iter == levels.end())
     {
@@ -44,7 +44,7 @@ static std::string helpMsg()
     for (const std::string& prompt : levels)
     {
         std::string level = prompt;
-        std::transform(level.begin(), level.end(), level.begin(), ::tolower);
+        std::ranges::transform(level, level.begin(), ::tolower);
         help.append(level + "\n");
     }
     return help;
@@ -58,8 +58,25 @@ static int setLogLevel(std::string& loglevel)
     std::string iface = "xyz.openbmc_project.bmcweb";
     std::string method = "SetLogLevel";
 
+<<<<<<< PATCH SET (4d0541 WIP: Implement modernize-use-ranges)
+    std::string loglevel;
+    app.require_subcommand(1);
+
+    const CLI::Validator levelValidator =
+        CLI::Validator(validateLogLevel, "valid level");
+
+    CLI::App* sub = app.add_subcommand("loglevel", "Set bmcweb log level");
+    sub->add_option("level", loglevel, helpMsg())
+        ->required()
+        ->check(levelValidator);
+
+    CLI11_PARSE(app, argc, argv)
+
+    std::ranges::transform(loglevel, loglevel.begin(), ::toupper);
+=======
     std::transform(loglevel.begin(), loglevel.end(), loglevel.begin(),
                    ::toupper);
+>>>>>>> BASE      (9ad2c6 Update to boost 1.89)
     // Set up dbus connection:
     boost::asio::io_context io;
     auto conn = std::make_shared<sdbusplus::asio::connection>(io);
