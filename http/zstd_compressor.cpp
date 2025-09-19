16580cbf0775da991bcd4752a6239a4a28087083
Ed Tanous <etanous@nvidia.com>
2025-09-19 15:12:00 -0700

    zstd compression
    zstd compression allows for reducing the size of payloads with repeating
    elements.  Pretty printed json is one very specific case where we would
    prefer to not change the behavior, but would also like to avoid the
    overhead of TLS compression.
    
    When ztd is present, the webserver will look for the Accepts-Encoding
    header to contain zstd, and if it does, compress the payload before
    sending.
    
    Tested:
    Webui loads correctly, and shows zstd being used for download.
    Redfish service validator runs. (does not support zstd)
    Unit tests pass.
    
    Change-Id: Ic575911fea70d218d1efb4df030d3cc098f1ecd0
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ http/zstd_compressor.cpp
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "zstd_compressor.hpp"
+
+#include "logging.hpp"
+
+#include <boost/asio/buffer.hpp>
+
+#include <cstdint>
+#include <limits>
+
+#ifdef HAVE_ZSTD
+#include <zstd.h>
+#endif
+
+#include <cstddef>
+#include <optional>
+#include <span>
+
+namespace bmcweb
+{
+
+bool ZstdCompressor::init([[maybe_unused]] size_t sourceSize)
+{
+#ifdef HAVE_ZSTD
+    if (cctx != nullptr)
+    {
+        BMCWEB_LOG_ERROR("ZstdCompressor already initialized");
+        return false;
+    }
+    cctx = ZSTD_createCCtx();
+    if (cctx == nullptr)
+    {
+        BMCWEB_LOG_ERROR("Failed to create ZstdCompressor");
+        return false;
+    }
+
+    // 3 is the default compression level for zstd, but set it explicitly
+    // so we can tune later if needed]
+    size_t ret = ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, 3);
+    if (ZSTD_isError(ret) != 0U)
+    {
+        BMCWEB_LOG_ERROR("Failed to set compression level {}:{}", ret,
+                         ZSTD_getErrorName(ret));
+        return false;
+    }
+    ret = ZSTD_CCtx_setParameter(cctx, ZSTD_c_checksumFlag, 1);
+    if (ZSTD_isError(ret) != 0U)
+    {
+        BMCWEB_LOG_ERROR("Failed to set checksum flag {}:{}", ret,
+                         ZSTD_getErrorName(ret));
+        return false;
+    }
+    ret = ZSTD_CCtx_setParameter(cctx, ZSTD_c_contentSizeFlag, 1);
+    if (ZSTD_isError(ret) != 0U)
+    {
+        BMCWEB_LOG_ERROR("Failed to set contentsize flag {}:{}", ret,
+                         ZSTD_getErrorName(ret));
+        return false;
+    }
+
+    ret = ZSTD_CCtx_setPledgedSrcSize(cctx, sourceSize);
+    if (ZSTD_isError(ret) != 0U)
+    {
+        BMCWEB_LOG_ERROR("Failed to set pledged src size {}:{}", ret,
+                         ZSTD_getErrorName(ret));
+        return false;
+    }
+    return true;
+#else
+    BMCWEB_LOG_CRITICAL("ZstdCompressor not compiled in");
+    return false;
+#endif
+}
+
+std::optional<std::span<const uint8_t>> ZstdCompressor::compress(
+    [[maybe_unused]] std::span<const uint8_t> buffIn,
+    [[maybe_unused]] bool more)
+{
+#ifdef HAVE_ZSTD
+    if (cctx == nullptr)
+    {
+        BMCWEB_LOG_ERROR("ZstdCompressor not initialized");
+        return std::nullopt;
+    }
+    compressionBuf.clear();
+    ZSTD_inBuffer input = {buffIn.data(), buffIn.size(), 0};
+
+    // Note, this loop is prone to compression bombs, decompressing chunks that
+    // appear very small, but decompress to be very large, given that they're
+    // highly decompressible. This algorithm assumes that at this time, the
+    // whole file will fit in ram.
+    size_t remaining = std::numeric_limits<size_t>::max();
+    while (remaining > 0)
+    {
+        constexpr size_t frameSize = 4096;
+        auto buffer = compressionBuf.prepare(frameSize);
+        ZSTD_outBuffer output = {buffer.data(), buffer.size(), 0};
+        ZSTD_EndDirective dir = ZSTD_e_end;
+        if (more)
+        {
+            dir = ZSTD_e_continue;
+        }
+        remaining = ZSTD_compressStream2(cctx, &output, &input, dir);
+        if (ZSTD_isError(remaining) != 0U)
+        {
+            return std::nullopt;
+        }
+        compressionBuf.commit(output.pos);
+    }
+    boost::asio::const_buffer buf = compressionBuf.cdata();
+    return std::span(static_cast<const uint8_t*>(buf.data()), buf.size());
+#else
+    BMCWEB_LOG_CRITICAL("Attempt to compress, but libzstd not enabled");
+
+    return std::nullopt;
+#endif
+}
+
+ZstdCompressor::~ZstdCompressor()
+{
+#ifdef HAVE_ZSTD
+    ZSTD_freeCCtx(cctx);
+#endif
+}
+} // namespace bmcweb
