6515594857798f07c2c5e7dfa7bb270041150aee
Christopher Meis <christopher.meis@9elements.com>
2025-09-26 08:54:40 +0200

    Unify route setup for fan collection and other fan functions
    Change-Id: Ib09a3b7b667ee551df74f2483babf4d0526e26dd
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- redfish-core/src/redfish.cpp
+++ redfish-core/src/redfish.cpp
@@ -90,7 +90,6 @@ RedfishService::RedfishService(App& app)
         requestRoutesThermalMetrics(app);
         requestRoutesThermalSubsystem(app);
         requestRoutesFan(app);
-        requestRoutesFanCollection(app);
     }
     requestRoutesManager(app);
     requestRoutesManagerResetAction(app);
