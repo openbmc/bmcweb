8ccfa4d37caaaecc5498105a096bbcc0e41a1134
Oliver Brewka <oliver.brewka@9elements.com>
2025-10-13 19:50:14 +0200

    Rename Systems EventLog route handler
    Rename the route handler to clearly indicate, that the
    handler handles the ComputerSystem redfish resource.
    
    Tested: Code compiles.
    
    Change-Id: Ib722427ab53f35b2270fe6e102614f21fe28ffcb
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- redfish-core/lib/systems_logservices_dbus_eventlog.hpp
+++ redfish-core/lib/systems_logservices_dbus_eventlog.hpp
@@ -146,7 +146,7 @@ inline void handleSystemsDBusEventLogEntryDownloadGet(
     eventlog_utils::downloadEventLogEntry(asyncResp, entryId, "System");
 }
 
-inline void requestRoutesDBusEventLogEntryCollection(App& app)
+inline void requestRoutesSystemsDBusEventLogEntryCollection(App& app)
 {
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/LogServices/EventLog/Entries/")
         .privileges(redfish::privileges::getLogEntryCollection)
@@ -154,7 +154,7 @@ inline void requestRoutesDBusEventLogEntryCollection(App& app)
             handleSystemsDBusEventLogEntryCollection, std::ref(app)));
 }
 
-inline void requestRoutesDBusEventLogEntry(App& app)
+inline void requestRoutesSystemsDBusEventLogEntry(App& app)
 {
     BMCWEB_ROUTE(
         app, "/redfish/v1/Systems/<str>/LogServices/EventLog/Entries/<str>/")
@@ -183,7 +183,7 @@ inline void requestRoutesDBusEventLogEntry(App& app)
 /**
  * DBusLogServiceActionsClear class supports POST method for ClearLog action.
  */
-inline void requestRoutesDBusLogServiceActionsClear(App& app)
+inline void requestRoutesSystemsDBusLogServiceActionsClear(App& app)
 {
     /**
      * Function handles POST method request.
@@ -200,7 +200,7 @@ inline void requestRoutesDBusLogServiceActionsClear(App& app)
             handleSystemsDBusLogServiceActionsClear, std::ref(app)));
 }
 
-inline void requestRoutesDBusEventLogEntryDownload(App& app)
+inline void requestRoutesSystemsDBusEventLogEntryDownload(App& app)
 {
     BMCWEB_ROUTE(
         app,
