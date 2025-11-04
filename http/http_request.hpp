f186599bd9db042c7426cc165a7220eb73cac28c
Chandramohan Harkude <chandramohan.harkude@gmail.com>
2025-11-05 00:49:05 +0530

    Filter X-AUTH-TOKEN and HTTP2 headers in aggregation
    When using aggregation X-AUTH-TOKEN and HTTP2 headers need not to be
    passed to HMC.
    
    Added code to clear these headers before sending to satellite
    BMC.
    
    Tested: Unit tests pass.
    
    Change-Id: I68f5fb11e4934c6d34c67f443dc0c933c019d18f
    Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

--- http/http_request.hpp
+++ http/http_request.hpp
@@ -113,6 +113,11 @@ struct Request
         req.erase(key);
     }
 
+    void clearHeader(std::string_view key)
+    {
+        req.erase(key);
+    }
+
     std::string_view methodString() const
     {
         return req.method_string();
@@ -136,6 +141,11 @@ struct Request
     const boost::beast::http::fields& fields() const
     {
         return req.base();
+    }
+
+    void setFields(const boost::beast::http::fields& fields)
+    {
+        req.base() = fields;
     }
 
     const std::string& body() const
