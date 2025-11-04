884ef1bca8ce19b773aed2f99ec704fa6fd64075
Chandramohan Harkude <chandramohan.harkude@gmail.com>
2025-11-05 00:49:05 +0530

    Filter X-AUTH-TOKEN and HTTP2 headers in aggregation
    When using aggregation X-AUTH-TOKEN and HTTP2 headers need not to be
    passed to HMC.
    
    Added code to clear these headers before sending to satellite
    BMC.
    
    Tested: Unit tests pass.
    
    Change-Id: I68f5fb11e4934c6d34c67f443dc0c933c019d18f
    Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

---
+++ redfish-core/include/utils/redfish_aggregator_utils.hpp
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+#include "http_request.hpp"
+
+#include <boost/beast/http/field.hpp>
+#include <boost/beast/http/fields.hpp>
+#include <boost/url/url.hpp>
+
+#include <memory>
+namespace redfish
+{
+// Drop any incoming x-auth-token headers and keep Host and Content-Type. Set
+// Accept.
+inline crow::Request createNewRequest(
+    const std::shared_ptr<crow::Request>& localReq)
+{
+    boost::system::error_code ec;
+    // Note, this is an expensive copy.  It ideally shouldn't be done, but no
+    // option at this point.
+    crow::Request req(localReq.copy(), ec);
+    if (ec)
+    {
+        BMCWEB_LOG_ERROR("Failed to set body.  Continuing");
+    }
+
+    for (const auto& field : req.fields())
+    {
+        // Drop any incoming x-auth-token headers and keep Host and
+        // Content-Type. Set Accept.
+        auto headerName = field.name();
+        if (headerName == boost::beast::http::field::content_type ||
+            headerName == boost::beast::http::field::host)
+        {
+            req.addHeader(headerName, field.value());
+        }
+    }
+    req.addHeader(boost::beast::http::field::accept,
+                  "application/json, application/octet-stream");
+    return req;
+}
+} // namespace redfish
