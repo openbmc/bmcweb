04743b8f532dc45b2cd12097a94549e50a9e1a0a
Chandramohan Harkude <chandramohan.harkude@gmail.com>
2025-11-05 00:49:05 +0530

    Filter X-AUTH-TOKEN and HTTP2 headers in aggregation
    When using aggregation X-AUTH-TOKEN and HTTP2 headers need not to be
    passed to HMC.
    
    Added code to clear these headers before sending to satellite
    BMC.
    
    Tested: Unit tests pass.
    
    Change-Id: I68f5fb11e4934c6d34c67f443dc0c933c019d18f
    Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

---
+++ redfish-core/include/utils/redfish_aggregator_utils.hpp
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+#include "http_request.hpp"
+
+#include <boost/beast/http/field.hpp>
+#include <boost/beast/http/fields.hpp>
+#include <boost/url/url.hpp>
+
+#include <memory>
+namespace redfish
+{
+// Drop any incoming x-auth-token headers and keep Host, Accept, and
+// Content-Type
+boost::beast::http::fields createNewHeaders(
+    const std::shared_ptr<crow::Request>& localReq)
+{
+    boost::beast::http::fields newHeaders;
+    for (const auto& it : localReq->fields())
+    {
+        auto headerName = it.name();
+        if (headerName == boost::beast::http::field::content_type ||
+            headerName == boost::beast::http::field::host)
+        {
+            newHeaders.insert(headerName, it.value());
+        }
+    }
+    newHeaders.insert(boost::beast::http::field::accept,
+                      "application/json, application/octet-stream");
+    return newHeaders;
+}
+} // namespace redfish
