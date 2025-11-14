ad5f0fd6324d5688096db28edc55962d0fb56900
Oliver Brewka <oliver.brewka@9elements.com>
2025-11-14 17:27:35 +0100

    Multi-host support for LogServiceCollection
    As a first step to introduce multi-host support for LogServices, update
    the LogServiceCollection accordingly to allow for request handling when
    redfish-experimental-multi-computer-system is enabled.
    
    Disallow all LogServices not supported for multi-host via meson.
    
    Tested: Validator succeeded.
    
    Change-Id: I619a96bcab6a0c240d93c3c6586d813ccfb43cad
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -27,6 +27,7 @@
 #include "utils/eventlog_utils.hpp"
 #include "utils/json_utils.hpp"
 #include "utils/log_services_utils.hpp"
+#include "utils/systems_utils.hpp"
 #include "utils/time_utils.hpp"
 
 #include <asm-generic/errno.h>
@@ -874,17 +875,9 @@ inline void handleSystemsLogServiceCollectionGet(
     {
         return;
     }
-    if constexpr (BMCWEB_EXPERIMENTAL_REDFISH_MULTI_COMPUTER_SYSTEM)
+
+    if (systems_utils::checkSingleHostSystemNotFound(systemName, asyncResp))
     {
-        // Option currently returns no systems.  TBD
-        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
-                                   systemName);
-        return;
-    }
-    if (systemName != BMCWEB_REDFISH_SYSTEM_URI_NAME)
-    {
-        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
-                                   systemName);
         return;
     }
 
@@ -892,8 +885,8 @@ inline void handleSystemsLogServiceCollectionGet(
     // because it has a duplicate entry for members
     asyncResp->res.jsonValue["@odata.type"] =
         "#LogServiceCollection.LogServiceCollection";
-    asyncResp->res.jsonValue["@odata.id"] = boost::urls::format(
-        "/redfish/v1/Systems/{}/LogServices", BMCWEB_REDFISH_SYSTEM_URI_NAME);
+    asyncResp->res.jsonValue["@odata.id"] =
+        std::format("/redfish/v1/Systems/{}/LogServices", systemName);
     asyncResp->res.jsonValue["Name"] = "System Log Services Collection";
     asyncResp->res.jsonValue["Description"] =
         "Collection of LogServices for this Computer System";
@@ -942,7 +935,7 @@ inline void handleSystemsLogServiceCollectionGet(
         "xyz.openbmc_project.State.Boot.PostCode"};
     dbus::utility::getSubTreePaths(
         "/", 0, interfaces,
-        [asyncResp](
+        [asyncResp, systemName](
             const boost::system::error_code& ec,
             const dbus::utility::MapperGetSubTreePathsResponse& subtreePath) {
             if (ec)
@@ -960,7 +953,7 @@ inline void handleSystemsLogServiceCollectionGet(
                     nlohmann::json::object_t member;
                     member["@odata.id"] = boost::urls::format(
                         "/redfish/v1/Systems/{}/LogServices/PostCodes",
-                        BMCWEB_REDFISH_SYSTEM_URI_NAME);
+                        systemName);
 
                     logServiceArrayLocal.emplace_back(std::move(member));
 
