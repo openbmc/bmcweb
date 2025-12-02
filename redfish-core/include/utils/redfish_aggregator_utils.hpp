openbmc/bmcweb

commit 897e4c80f35b5bd963923f5794a7d3b229dba306

Filter X-AUTH-TOKEN and HTTP2 headers in aggregation
When using aggregation X-AUTH-TOKEN and HTTP2 headers need not to be
passed to HMC.

Added code to clear these headers before sending to satellite
BMC.

Tested: Unit tests pass.

Change-Id: I68f5fb11e4934c6d34c67f443dc0c933c019d18f
Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

redfish-core/include/utils/redfish_aggregator_utils.hpp
@@ -1,0 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+#include "http_request.hpp"
+
+#include <boost/beast/http/field.hpp>
+#include <boost/beast/http/fields.hpp>
+#include <boost/url/url.hpp>
+
+#include <memory>
+namespace redfish
+{
+// Drop any incoming x-auth-token headers and keep Host and Content-Type. Set
+// Accept.
+inline crow::Request createNewRequest(const crow::Request& localReq)
+{
+    boost::system::error_code ec;
+    // Note, this is an expensive copy.  It ideally shouldn't be done, but no
+    // option at this point.
+    crow::Request req(localReq.body(), ec);
+    if (ec)
+    {
+        BMCWEB_LOG_ERROR("Failed to set body.  Continuing");
+    }
+
+    for (const auto& field : req.fields())
+    {
+        // Drop any incoming x-auth-token headers and keep Host and
+        // Content-Type. Set Accept.
+        auto headerName = field.name();
+        if (headerName == boost::beast::http::field::content_type ||
+            headerName == boost::beast::http::field::host)
+        {
+            req.addHeader(headerName, field.value());
+        }
+    }
+    req.addHeader(boost::beast::http::field::accept,
+                  "application/json, application/octet-stream");
+    return req;
+}
+} // namespace redfish

redfish-core/include/redfish_aggregator.hpp
@@ -14,6 +14,7 @@
 #include "parsing.hpp"
 #include "ssl_key_handler.hpp"
 #include "utility.hpp"
+#include "utils/redfish_aggregator_utils.hpp"
 
 #include <boost/beast/http/field.hpp>
 #include <boost/beast/http/status.hpp>
@@ -608,9 +609,10 @@ class RedfishAggregator
             }
         }
 
-        // Create a copy of thisReq so we we can still locally process the req
         std::error_code ec;
-        auto localReq = std::make_shared<crow::Request>(thisReq.copy());
+        // Create a filtered copy of the request
+        auto localReq =
+            std::make_shared<crow::Request>(createNewRequest(thisReq));
         if (ec)
         {
             BMCWEB_LOG_ERROR("Failed to create copy of request");
@@ -621,9 +623,9 @@ class RedfishAggregator
             return;
         }
 
+        boost::urls::url& urlNew = localReq->url();
         if (aggType == AggregationType::Collection)
         {
-            boost::urls::url& urlNew = localReq->url();
             auto paramsIt = urlNew.params().begin();
             while (paramsIt != urlNew.params().end())
             {
@@ -647,9 +649,9 @@ class RedfishAggregator
                 // Pass all other parameters
                 paramsIt++;
             }
-            localReq->target(urlNew.buffer());
         }
-
+        // Filter headers to only allow Host and Content-Type
+        localReq->target(urlNew.buffer());
         getSatelliteConfigs(
             std::bind_front(aggregateAndHandle, aggType, localReq, asyncResp));
     }

commit c40e7ed9254bd31d6f7d6131deb1e685ca1ecb85

Fix RFA aggregation issue
Aggregation was failing because of method and target URI missing
in HTTP request

This was a regression from 897e4c80f35b5bd963923f5794a7d3b229dba306

HTTP/1.1
Accept: application/json, application/octet-stream
Host: 172.31.13.251

Fix : Added the Target URI and method from original request

Testing :

```
curl -k -u root:0penBmc -X GET https://${BMC_IP}/redfish/v1/Chassis

Returned items from both BMC and Satellite BMC
```
Change-Id: Ib446adfeb94d0cce3a272fc39ee5cc60ce3df7f8
Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>
Signed-off-by: Ed Tanous <etanous@nvidia.com>

redfish-core/include/utils/redfish_aggregator_utils.hpp
@@ -23,7 +23,14 @@ inline crow::Request createNewRequest(const crow::Request& localReq)
         BMCWEB_LOG_ERROR("Failed to set body.  Continuing");
     }
 
-    for (const auto& field : req.fields())
+    // Preserve method and target (URI) from the original request
+    req.method(localReq.method());
+    if (!req.target(localReq.target()))
+    {
+        BMCWEB_LOG_ERROR("Failed to set target on aggregated request");
+    }
+
+    for (const auto& field : localReq.fields())
     {
         // Drop any incoming x-auth-token headers and keep Host and
         // Content-Type. Set Accept.
@@ -34,6 +41,7 @@ inline crow::Request createNewRequest(const crow::Request& localReq)
             req.addHeader(headerName, field.value());
         }
     }
+    // Set Accept header to application/json, application/octet-stream
     req.addHeader(boost::beast::http::field::accept,
                   "application/json, application/octet-stream");
     return req;