90984b88b4e9053637c3154938cf1beace390fed
Ed Tanous <etanous@nvidia.com>
2026-01-20 14:23:31 -0800

    Remove old login routines
    bmcweb at one point the bmcweb /login route has supported a bunch of
    different login mechanisms:
    1. Sending username and password via headers, mirroring a non-openbmc
       implementation that has never been used in openbmc, and predates this
       repo being called bmcweb.
    2. Sending username and password under a "data" object, mirroring
       phosphor-rest json webserver.
    3. Sending username and password as an http multipart as part of the
       initial multipart parser patch.
    4. Sending a json payload as {"username": <>, "password"}
    
    This commit removes all but the final login mechanism.  Redfish login
    has been used exclusively for many many years, and only the webui used
    4 above up until a few years ago in commit
    1ff8e89fd2397c468ab0237158e5aeeff2692413
    
    Keeping one viable login mechanism that's not Redfish is useful in the
    future if there are those that want to disable Redfish.  All others
    could potentially be security issues, so keeping the code simple and
    conscise here is ideal.
    
    This commit does not attempt to make a backwards compatibility path,
    under the assumption that other than 4, none of the other code was ever
    used in any real capacity.  If we find that to not be true, we can add
    back the portions where we need compatibility, but this seems unlikely.
    
    Tested: WIP
    
    Change-Id: I04b4968836f0f824f46b3dff180ad92feb16967c
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- features/webui_login/login_routes.hpp
+++ features/webui_login/login_routes.hpp
@@ -71,139 +71,34 @@ inline void handleLogin(const crow::Request& req,
                     password = *passStr;
                 }
             }
-            else
-            {
-                // Openbmc appears to push a data object that contains the
-                // same keys (username and password), attempt to use that
-                auto dataIt = obj->find("data");
-                if (dataIt != obj->end())
-                {
-                    // Some apis produce an array of value ["username",
-                    // "password"]
-                    const nlohmann::json::array_t* arr =
-                        dataIt->second
-                            .get_ptr<const nlohmann::json::array_t*>();
-                    if (arr != nullptr)
-                    {
-                        if (arr->size() == 2)
-                        {
-                            nlohmann::json::array_t::const_iterator userIt2 =
-                                arr->begin();
-                            nlohmann::json::array_t::const_iterator passIt2 =
-                                arr->begin() + 1;
-                            if (userIt2 != arr->end() && passIt2 != arr->end())
-                            {
-                                const std::string* userStr =
-                                    userIt2->get_ptr<const std::string*>();
-                                const std::string* passStr =
-                                    passIt2->get_ptr<const std::string*>();
-                                if (userStr != nullptr && passStr != nullptr)
-                                {
-                                    username = *userStr;
-                                    password = *passStr;
-                                }
-                            }
-                        }
-                    }
-                    const nlohmann::json::object_t* obj2 =
-                        dataIt->second
-                            .get_ptr<const nlohmann::json::object_t*>();
-
-                    if (obj2 != nullptr)
-                    {
-                        nlohmann::json::object_t::const_iterator userIt2 =
-                            obj2->find("username");
-                        nlohmann::json::object_t::const_iterator passIt2 =
-                            obj2->find("password");
-                        if (userIt2 != obj2->end() && passIt2 != obj2->end())
-                        {
-                            const std::string* userStr =
-                                userIt2->second.get_ptr<const std::string*>();
-                            const std::string* passStr =
-                                passIt2->second.get_ptr<const std::string*>();
-                            if (userStr != nullptr && passStr != nullptr)
-                            {
-                                username = *userStr;
-                                password = *passStr;
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
-    else if (contentType.starts_with("multipart/form-data"))
-    {
-        ParserError ec = parser.parse(req);
-        if (ec != ParserError::PARSER_SUCCESS)
-        {
-            // handle error
-            BMCWEB_LOG_ERROR("MIME parse failed, ec : {}",
-                             static_cast<int>(ec));
-            asyncResp->res.result(boost::beast::http::status::bad_request);
-            return;
-        }
 
-        for (const FormPart& formpart : parser.mime_fields)
-        {
-            boost::beast::http::fields::const_iterator it =
-                formpart.fields.find("Content-Disposition");
-            if (it == formpart.fields.end())
-            {
-                BMCWEB_LOG_ERROR("Couldn't find Content-Disposition");
-                asyncResp->res.result(boost::beast::http::status::bad_request);
-                continue;
-            }
-
-            BMCWEB_LOG_INFO("Parsing value {}", it->value());
-
-            if (it->value() == "form-data; name=\"username\"")
-            {
-                username = formpart.content;
-            }
-            else if (it->value() == "form-data; name=\"password\"")
-            {
-                password = formpart.content;
-            }
-            else
-            {
-                BMCWEB_LOG_INFO("Extra format, ignore it.{}", it->value());
-            }
-        }
-    }
-    else
-    {
-        // check if auth was provided as a headers
-        username = req.getHeaderValue("username");
-        password = req.getHeaderValue("password");
-    }
-
-    if (!username.empty() && !password.empty())
-    {
-        int pamrc = pamAuthenticateUser(username, password, std::nullopt);
-        bool isConfigureSelfOnly = pamrc == PAM_NEW_AUTHTOK_REQD;
-        if ((pamrc != PAM_SUCCESS) && !isConfigureSelfOnly)
-        {
-            asyncResp->res.result(boost::beast::http::status::unauthorized);
-        }
-        else
-        {
-            auto session =
-                persistent_data::SessionStore::getInstance()
-                    .generateUserSession(username, req.ipAddress, std::nullopt,
-                                         persistent_data::SessionType::Session,
-                                         isConfigureSelfOnly);
-
-            bmcweb::setSessionCookies(asyncResp->res, *session);
-
-            // if content type is json, assume json token
-            asyncResp->res.jsonValue["token"] = session->sessionToken;
-        }
-    }
-    else
+    if (username.empty() || password.empty())
     {
         BMCWEB_LOG_DEBUG("Couldn't interpret password");
         asyncResp->res.result(boost::beast::http::status::bad_request);
+        return;
+    }
+
+    int pamrc = pamAuthenticateUser(username, password, std::nullopt);
+    bool isConfigureSelfOnly = pamrc == PAM_NEW_AUTHTOK_REQD;
+    if ((pamrc != PAM_SUCCESS) && !isConfigureSelfOnly)
+    {
+        asyncResp->res.result(boost::beast::http::status::unauthorized);
+    }
+    else
+    {
+        auto session =
+            persistent_data::SessionStore::getInstance()
+                .generateUserSession(username, req.ipAddress, std::nullopt,
+                                        persistent_data::SessionType::Session,
+                                        isConfigureSelfOnly);
+
+        bmcweb::setSessionCookies(asyncResp->res, *session);
+
+        // if content type is json, assume json token
+        asyncResp->res.jsonValue["token"] = session->sessionToken;
     }
 }
 
