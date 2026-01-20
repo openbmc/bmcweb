fc33f6cdc21ef561e1d4a47ea8f688cfb68e4734
Gunnar Mills <gmills@us.ibm.com>
2026-01-20 16:07:17 -0600

    Move to Redfish 2025.4
    One line change and rerun the script. 2025.4 includes new properties.
    One use case is for Redundancy. For the complete overview see [1].
    
    Tested: Visual and build only. In the past these have not broken things.
    
    [1]: https://www.dmtf.org/sites/default/files/Redfish_Release_2025.4_Overview.pdf
    
    Change-Id: Icaf710eaa99816264993a964ef9dd8a7f5e7722a
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ redfish-core/include/generated/enums/switch.hpp
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+#include <nlohmann/json.hpp>
+
+namespace switch
+{
+// clang-format off
+
+enum class TargetType{
+    Invalid,
+    FabricPort,
+    HostEdgePort,
+    DownstreamEdgePort,
+};
+
+NLOHMANN_JSON_SERIALIZE_ENUM(TargetType, {
+    {TargetType::Invalid, "Invalid"},
+    {TargetType::FabricPort, "FabricPort"},
+    {TargetType::HostEdgePort, "HostEdgePort"},
+    {TargetType::DownstreamEdgePort, "DownstreamEdgePort"},
+});
+
+// clang-format on
+}
