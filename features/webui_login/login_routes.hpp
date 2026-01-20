285144870bf898412eb0f3f63ec19219a73b7509
Ed Tanous <etanous@nvidia.com>
2026-01-20 14:23:31 -0800

    Remove old login routines
    At one point the bmcweb /login route has supported a bunch of
    different login mechanisms:
    1. Sending username and password via headers, mirroring a non-openbmc
       implementation that has never been used in openbmc, and predates this
       repo being called bmcweb.
    2. Sending username and password under a "data" object, mirroring
       phosphor-rest json webserver.
    3. Sending username and password as an http multipart as part of the
       initial multipart parser patch.
    4. Sending a json payload as {"username": <>, "password": <>}
    
    This commit removes all but the final login mechanism.  Redfish login
    has been used exclusively for many many years, and only the webui used
    4 above up until a few years ago in commit
    1ff8e89fd2397c468ab0237158e5aeeff2692413
    
    Keeping one viable login mechanism that's not Redfish is useful in the
    future if there are those that want to disable Redfish.  All others
    could potentially be security issues, so keeping the code simple and
    conscise here is ideal.
    
    This commit does not attempt to make a backwards compatibility path,
    under the assumption that other than 4, none of the other code was ever
    used in any real capacity.  If we find that to not be true, we can add
    back the portions where we need compatibility, but this seems unlikely.
    
    Tested: WIP
    
    Change-Id: I04b4968836f0f824f46b3dff180ad92feb16967c
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- features/webui_login/login_routes.hpp
+++ features/webui_login/login_routes.hpp
@@ -8,7 +8,6 @@
 #include "http_request.hpp"
 #include "http_response.hpp"
 #include "logging.hpp"
-#include "multipart_parser.hpp"
 #include "pam_authenticate.hpp"
 #include "sessions.hpp"
 
@@ -33,178 +32,70 @@ namespace login_routes
 inline void handleLogin(const crow::Request& req,
                         const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
 {
-    MultipartParser parser;
-    std::string_view contentType = req.getHeaderValue("content-type");
-    std::string_view username;
-    std::string_view password;
-
     // This object needs to be declared at this scope so the strings
     // within it are not destroyed before we can use them
     nlohmann::json loginCredentials;
-    // Check if auth was provided by a payload
-    if (contentType.starts_with("application/json"))
+    if (parseRequestAsJson(req, loginCredentials) != JsonParseResult::Success)
     {
-        loginCredentials = nlohmann::json::parse(req.body(), nullptr, false);
-        if (loginCredentials.is_discarded())
-        {
-            BMCWEB_LOG_DEBUG("Bad json in request");
-            asyncResp->res.result(boost::beast::http::status::bad_request);
-            return;
-        }
-        const nlohmann::json::object_t* obj =
-            loginCredentials.get_ptr<const nlohmann::json::object_t*>();
-        if (obj != nullptr)
-        {
-            // check for username/password in the root object
-            // This method is how intel APIs authenticate
-            auto userIt = obj->find("username");
-            auto passIt = obj->find("password");
-            if (userIt != obj->end() && passIt != obj->end())
-            {
-                const std::string* userStr =
-                    userIt->second.get_ptr<const std::string*>();
-                const std::string* passStr =
-                    passIt->second.get_ptr<const std::string*>();
-                if (userStr != nullptr && passStr != nullptr)
-                {
-                    username = *userStr;
-                    password = *passStr;
-                }
-            }
-            else
-            {
-                // Openbmc appears to push a data object that contains the
-                // same keys (username and password), attempt to use that
-                auto dataIt = obj->find("data");
-                if (dataIt != obj->end())
-                {
-                    // Some apis produce an array of value ["username",
-                    // "password"]
-                    const nlohmann::json::array_t* arr =
-                        dataIt->second
-                            .get_ptr<const nlohmann::json::array_t*>();
-                    if (arr != nullptr)
-                    {
-                        if (arr->size() == 2)
-                        {
-                            nlohmann::json::array_t::const_iterator userIt2 =
-                                arr->begin();
-                            nlohmann::json::array_t::const_iterator passIt2 =
-                                arr->begin() + 1;
-                            if (userIt2 != arr->end() && passIt2 != arr->end())
-                            {
-                                const std::string* userStr =
-                                    userIt2->get_ptr<const std::string*>();
-                                const std::string* passStr =
-                                    passIt2->get_ptr<const std::string*>();
-                                if (userStr != nullptr && passStr != nullptr)
-                                {
-                                    username = *userStr;
-                                    password = *passStr;
-                                }
-                            }
-                        }
-                    }
-                    const nlohmann::json::object_t* obj2 =
-                        dataIt->second
-                            .get_ptr<const nlohmann::json::object_t*>();
-
-                    if (obj2 != nullptr)
-                    {
-                        nlohmann::json::object_t::const_iterator userIt2 =
-                            obj2->find("username");
-                        nlohmann::json::object_t::const_iterator passIt2 =
-                            obj2->find("password");
-                        if (userIt2 != obj2->end() && passIt2 != obj2->end())
-                        {
-                            const std::string* userStr =
-                                userIt2->second.get_ptr<const std::string*>();
-                            const std::string* passStr =
-                                passIt2->second.get_ptr<const std::string*>();
-                            if (userStr != nullptr && passStr != nullptr)
-                            {
-                                username = *userStr;
-                                password = *passStr;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    else if (contentType.starts_with("multipart/form-data"))
-    {
-        ParserError ec = parser.parse(req);
-        if (ec != ParserError::PARSER_SUCCESS)
-        {
-            // handle error
-            BMCWEB_LOG_ERROR("MIME parse failed, ec : {}",
-                             static_cast<int>(ec));
-            asyncResp->res.result(boost::beast::http::status::bad_request);
-            return;
-        }
-
-        for (const FormPart& formpart : parser.mime_fields)
-        {
-            boost::beast::http::fields::const_iterator it =
-                formpart.fields.find("Content-Disposition");
-            if (it == formpart.fields.end())
-            {
-                BMCWEB_LOG_ERROR("Couldn't find Content-Disposition");
-                asyncResp->res.result(boost::beast::http::status::bad_request);
-                continue;
-            }
-
-            BMCWEB_LOG_INFO("Parsing value {}", it->value());
-
-            if (it->value() == "form-data; name=\"username\"")
-            {
-                username = formpart.content;
-            }
-            else if (it->value() == "form-data; name=\"password\"")
-            {
-                password = formpart.content;
-            }
-            else
-            {
-                BMCWEB_LOG_INFO("Extra format, ignore it.{}", it->value());
-            }
-        }
-    }
-    else
-    {
-        // check if auth was provided as a headers
-        username = req.getHeaderValue("username");
-        password = req.getHeaderValue("password");
+        asyncResp->res.result(boost::beast::http::status::bad_request);
+        return;
     }
 
-    if (!username.empty() && !password.empty())
+    const nlohmann::json::object_t* obj =
+        loginCredentials.get_ptr<const nlohmann::json::object_t*>();
+    if (obj == nullptr)
     {
-        int pamrc = pamAuthenticateUser(username, password, std::nullopt);
-        bool isConfigureSelfOnly = pamrc == PAM_NEW_AUTHTOK_REQD;
-        if ((pamrc != PAM_SUCCESS) && !isConfigureSelfOnly)
-        {
-            asyncResp->res.result(boost::beast::http::status::unauthorized);
-        }
-        else
-        {
-            auto session =
-                persistent_data::SessionStore::getInstance()
-                    .generateUserSession(username, req.ipAddress, std::nullopt,
-                                         persistent_data::SessionType::Session,
-                                         isConfigureSelfOnly);
-
-            bmcweb::setSessionCookies(asyncResp->res, *session);
-
-            // if content type is json, assume json token
-            asyncResp->res.jsonValue["token"] = session->sessionToken;
-        }
+        BMCWEB_LOG_DEBUG("Received json was not an object");
+        asyncResp->res.result(boost::beast::http::status::bad_request);
+        return;
     }
-    else
+
+    // check for username/password in the root object
+    auto userIt = obj->find("username");
+    if (userIt == obj->end())
+    {
+        BMCWEB_LOG_DEBUG("Couldn't interpret username");
+        asyncResp->res.result(boost::beast::http::status::bad_request);
+        return;
+    }
+    const std::string* userStr = userIt->second.get_ptr<const std::string*>();
+    if (userStr == nullptr)
+    {
+        BMCWEB_LOG_DEBUG("Couldn't interpret username");
+        asyncResp->res.result(boost::beast::http::status::bad_request);
+        return;
+    }
+    auto passIt = obj->find("password");
+    if (passIt == obj->end())
     {
         BMCWEB_LOG_DEBUG("Couldn't interpret password");
         asyncResp->res.result(boost::beast::http::status::bad_request);
+        return;
     }
+    const std::string* passStr = passIt->second.get_ptr<const std::string*>();
+    if (passStr == nullptr)
+    {
+        BMCWEB_LOG_DEBUG("Couldn't interpret password");
+        asyncResp->res.result(boost::beast::http::status::bad_request);
+        return;
+    }
+
+    int pamrc = pamAuthenticateUser(*userStr, *passStr, std::nullopt);
+    bool isConfigureSelfOnly = pamrc == PAM_NEW_AUTHTOK_REQD;
+    if ((pamrc != PAM_SUCCESS) && !isConfigureSelfOnly)
+    {
+        asyncResp->res.result(boost::beast::http::status::unauthorized);
+        return;
+    }
+    auto session =
+        persistent_data::SessionStore::getInstance().generateUserSession(
+            *userStr, req.ipAddress, std::nullopt,
+            persistent_data::SessionType::Session, isConfigureSelfOnly);
+
+    bmcweb::setSessionCookies(asyncResp->res, *session);
+
+    // if content type is json, assume json token
+    asyncResp->res.jsonValue["token"] = session->sessionToken;
 }
 
 inline void handleLogout(const crow::Request& req,
