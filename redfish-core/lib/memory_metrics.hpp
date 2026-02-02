ef53421b005c1aad608a191d8b9dfaf39004efba
Eric Liu <liuer@nvidia.com>
2026-02-02 13:53:43 +0900

    Memory: add Memory MemoryMetrics ECC support
    Add support for exposing memory ECC error counts
    via Redfish MemoryMetrics endpoint at
    /redfish/v1/Systems/system/Memory/<str>/MemoryMetrics
    
    This follows DMTF Redfish MemoryMetrics v1_7_0
    schema. The MemoryMetrics endpoint is accessible
    for inventory objects implementing
    xyz.openbmc_project.Memory.MemoryECC interface.
    
    The implementation maps D-Bus properties to Redfish
    as follows:
    - ceCount -> LifeTime.CorrectableECCErrorCount
    - ueCount -> LifeTime.UncorrectableECCErrorCount
    
    The ECC data is backed by D-Bus interface
    xyz.openbmc_project.Memory.MemoryECC with
    properties ceCount and ueCount (int64_t).
    
    Key changes:
    - redfish-core/lib/memory_metrics.hpp: New file.
      Implement MemoryMetrics GET and HEAD handlers
      using getSubTree filtered by
      xyz.openbmc_project.Memory.MemoryECC; populate
      LifeTime.CorrectableECCErrorCount and
      LifeTime.UncorrectableECCErrorCount from ceCount
      and ueCount.
    - redfish-core/lib/memory.hpp: Add Metrics link in
      Memory resource.
    - redfish-core/src/redfish.cpp: Register
      MemoryMetrics routes.
    - docs/Redfish.md: Add MemoryMetrics entry and
      Metrics property under Memory.
    
    Tested: Build an image for nvl32-obmc with the
    following patch cherry picked.
    
    ```
    1. Align with upstream u-boot dts tree:
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/89932
    2. U-Boot change phy mode (in-flight on mailing list):
    https://lore.kernel.org/openbmc/20260504044702.2613879-1-andhsieh@nvidia.com/T/#t
    3. Kernel device tree add mac mode (in-flight on mailing list):
    https://lore.kernel.org/linux-aspeed/20260505050541.3031447-1-andhsieh@nvidia.com/T/#t
    4. platform-init enable LCLK and espiCLK:
    https://gerrit.openbmc.org/c/openbmc/platform-init/+/89900
    5. mctpd configuration:
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/87390
    6. Enable nvidia-gpu sensor:
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/89933
    
    $ curl -sk -u <credentials> \
      https://localhost/redfish/v1/Systems/system/\
    Memory/GPU_0_0_DRAM_0/MemoryMetrics
    {
      "@odata.type":
        "#MemoryMetrics.v1_7_0.MemoryMetrics",
      "LifeTime": {
        "CorrectableECCErrorCount": 0,
        "UncorrectableECCErrorCount": 0
      }
    }
    ```
    1. All GPU DRAM objects return valid MemoryMetrics.
    2. Redfish Service Validator: PASS.
    
    Change-Id: I4a3855c37d5a240c627e0414c5d82eeb0dd2dd68
    Signed-off-by: Eric Liu <liuer@nvidia.com>

---
+++ redfish-core/lib/memory_metrics.hpp
@@ -0,0 +1,201 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+
+#include "app.hpp"
+#include "async_resp.hpp"
+#include "dbus_utility.hpp"
+#include "error_messages.hpp"
+#include "logging.hpp"
+#include "query.hpp"
+#include "registries/privilege_registry.hpp"
+#include "utils/dbus_utils.hpp"
+
+#include <boost/beast/http/verb.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/url/format.hpp>
+#include <sdbusplus/message/native_types.hpp>
+#include <sdbusplus/unpack_properties.hpp>
+
+#include <array>
+#include <cerrno>
+#include <format>
+#include <functional>
+#include <memory>
+#include <string>
+#include <string_view>
+
+namespace redfish
+{
+
+inline void afterGetMemoryEccProperties(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec,
+    const dbus::utility::DBusPropertiesMap& properties)
+{
+    if (ec)
+    {
+        if (ec.value() != EBADR)
+        {
+            BMCWEB_LOG_ERROR("D-Bus response error for MemoryECC: {}",
+                             ec.message());
+            messages::internalError(asyncResp->res);
+        }
+        return;
+    }
+
+    const int64_t* ceCount = nullptr;
+    const int64_t* ueCount = nullptr;
+
+    const bool success = sdbusplus::unpackPropertiesNoThrow(
+        dbus_utils::UnpackErrorPrinter(), properties, "ceCount", ceCount,
+        "ueCount", ueCount);
+
+    if (!success)
+    {
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    if (ceCount != nullptr)
+    {
+        asyncResp->res.jsonValue["LifeTime"]["CorrectableECCErrorCount"] =
+            *ceCount;
+    }
+
+    if (ueCount != nullptr)
+    {
+        asyncResp->res.jsonValue["LifeTime"]["UncorrectableECCErrorCount"] =
+            *ueCount;
+    }
+}
+
+inline void afterGetMemoryMetricsSubTree(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& memoryId, const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreeResponse& subtree)
+{
+    if (ec)
+    {
+        if (ec.value() != EBADR)
+        {
+            BMCWEB_LOG_ERROR("D-Bus response error for MemoryMetrics: {}",
+                             ec.message());
+            messages::internalError(asyncResp->res);
+            return;
+        }
+        messages::resourceNotFound(asyncResp->res, "MemoryMetrics", memoryId);
+        return;
+    }
+
+    for (const auto& [objectPath, serviceMap] : subtree)
+    {
+        if (sdbusplus::message::object_path(objectPath).filename() != memoryId)
+        {
+            continue;
+        }
+
+        if (serviceMap.size() != 1)
+        {
+            BMCWEB_LOG_ERROR("Expected exactly one service for {}, got {}",
+                             objectPath, serviceMap.size());
+            messages::internalError(asyncResp->res);
+            return;
+        }
+
+        const std::string& service = serviceMap.begin()->first;
+        dbus::utility::getAllProperties(
+            service, objectPath, "xyz.openbmc_project.Memory.MemoryECC",
+            std::bind_front(afterGetMemoryEccProperties, asyncResp));
+        return;
+    }
+
+    messages::resourceNotFound(asyncResp->res, "MemoryMetrics", memoryId);
+}
+
+inline void handleMemoryMetricsHead(
+    App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemName, const std::string& /*memoryId*/)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    if constexpr (BMCWEB_EXPERIMENTAL_REDFISH_MULTI_COMPUTER_SYSTEM)
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+
+    if (systemName != BMCWEB_REDFISH_SYSTEM_URI_NAME)
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/MemoryMetrics/MemoryMetrics.json>; rel=describedby");
+}
+
+inline void handleMemoryMetricsGet(
+    App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemName, const std::string& memoryId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    if constexpr (BMCWEB_EXPERIMENTAL_REDFISH_MULTI_COMPUTER_SYSTEM)
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+
+    if (systemName != BMCWEB_REDFISH_SYSTEM_URI_NAME)
+    {
+        messages::resourceNotFound(asyncResp->res, "ComputerSystem",
+                                   systemName);
+        return;
+    }
+
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/MemoryMetrics/MemoryMetrics.json>; rel=describedby");
+
+    asyncResp->res.jsonValue["@odata.type"] =
+        "#MemoryMetrics.v1_7_0.MemoryMetrics";
+    asyncResp->res.jsonValue["@odata.id"] = boost::urls::format(
+        "/redfish/v1/Systems/{}/Memory/{}/MemoryMetrics", systemName, memoryId);
+    asyncResp->res.jsonValue["Id"] = "MemoryMetrics";
+    asyncResp->res.jsonValue["Name"] =
+        std::format("{} Memory Metrics", memoryId);
+
+    constexpr std::array<std::string_view, 1> interfaces = {
+        "xyz.openbmc_project.Memory.MemoryECC"};
+    dbus::utility::getSubTree(
+        "/xyz/openbmc_project/inventory", 0, interfaces,
+        std::bind_front(afterGetMemoryMetricsSubTree, asyncResp, memoryId));
+}
+
+inline void requestRoutesMemoryMetrics(App& app)
+{
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/Memory/<str>/MemoryMetrics/")
+        .privileges(redfish::privileges::headMemoryMetrics)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handleMemoryMetricsHead, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/Memory/<str>/MemoryMetrics/")
+        .privileges(redfish::privileges::getMemoryMetrics)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handleMemoryMetricsGet, std::ref(app)));
+}
+
+} // namespace redfish
