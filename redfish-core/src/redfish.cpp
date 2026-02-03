6dd4ccdf3c348398ffb61864978011addf5736ce
Eric Liu <liuer@nvidia.com>
2026-02-03 17:38:59 +0900

    Add Processor MemorySummary MemoryMetrics ECC support
    Add support for exposing combined GPU SRAM and DRAM ECC error
    counts via Redfish MemoryMetrics endpoint at
    /redfish/v1/Systems/system/Processors/<str>/MemorySummary/MemoryMetrics
    
    This implementation provides aggregated ECC error monitoring for
    GPU accelerators following DMTF Redfish MemoryMetrics v1_7_0
    schema specification. The feature combines correctable and
    uncorrectable error counts from both GPU on-chip SRAM/Cache and
    external DRAM/HBM memory through the LifeTime properties.
    
    The SRAM ECC data is backed by D-Bus interface
    xyz.openbmc_project.Memory.MemoryECC on the Processor object.
    The DRAM ECC data is obtained via D-Bus Association mechanism
    using the "all_memory" endpoint to locate associated Memory
    objects with the same MemoryECC interface.
    
    ECC counts mapping follows Redfish schema conventions:
    - SRAM ceCount + DRAM ceCount -> LifeTime.CorrectableECCErrorCount
    - SRAM ueCount + DRAM ueCount -> LifeTime.UncorrectableECCErrorCount
    
    Changes:
    - redfish-core/lib/processor_memory_summary_metrics.hpp: New
      MemorySummary/MemoryMetrics endpoint implementation. Add
      afterGetDramEccProperties() and afterGetSramEccProperties()
      to process ECC data from D-Bus. Add findProcessorMemoryObject()
      to query associations via dbus::utility::getAssociationEndPoints().
      Add afterGetProcessorSubTree() to locate processor objects.
      Add doProcessorMemorySummaryMetricsGet() as main handler
      populating response with @odata.type, @odata.id, and LifeTime
      structure. Add handleProcessorMemorySummaryMetricsHead() and
      handleProcessorMemorySummaryMetricsGet() for route handling.
      Add requestRoutesProcessorMemorySummaryMetrics() for route
      registration.
    - redfish-core/src/redfish.cpp: Include new header and register
      routes via requestRoutesProcessorMemorySummaryMetrics().
    
    Tested:
    1. MemorySummary/MemoryMetrics endpoint query:
    '''
    $ curl -s -k -u <credentials> \
      https://localhost/redfish/v1/Systems/system/\
    Processors/GPU_0_0/MemorySummary/MemoryMetrics | jq
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/\
    GPU_0_0/MemorySummary/MemoryMetrics",
      "@odata.type": "#MemoryMetrics.v1_7_0.MemoryMetrics",
      "Id": "MemoryMetrics",
      "LifeTime": {
        "CorrectableECCErrorCount": 0,
        "UncorrectableECCErrorCount": 0
      },
      "Name": "GPU_0_0 Memory Summary Metrics"
    }
    '''
    
    2. D-Bus Association verification:
    '''
    $ busctl get-property xyz.openbmc_project.ObjectMapper \
      /xyz/openbmc_project/inventory/GPU_0_0/all_memory \
      xyz.openbmc_project.Association endpoints
    as 1 "/xyz/openbmc_project/inventory/GPU_0_0_DRAM_0"
    '''
    
    3. Redfish Service Validator: PASS
    
    Change-Id: Ieac6020c8f4566304c9f6214e6e5ce968c1d05f9
    Signed-off-by: Eric Liu <liuer@nvidia.com>

--- redfish-core/src/redfish.cpp
+++ redfish-core/src/redfish.cpp
@@ -39,6 +39,7 @@
 #include "power_subsystem.hpp"
 #include "power_supply.hpp"
 #include "processor.hpp"
+#include "processor_memory_summary_metrics.hpp"
 #include "processor_operating_config.hpp"
 #include "redfish_sessions.hpp"
 #include "redfish_v1.hpp"
@@ -184,6 +185,7 @@ RedfishService::RedfishService(App& app)
     requestRoutesProcessor(app);
     requestRoutesOperatingConfig(app);
     requestRoutesMemory(app);
+    requestRoutesProcessorMemorySummaryMetrics(app);
 
     requestRoutesSystems(app);
 
