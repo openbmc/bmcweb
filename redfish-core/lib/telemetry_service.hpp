37cec372a3d1505796b668840605f7699f27dd2c
Ramya Sivakumar <sramya@ami.com>
2026-02-06 09:42:11 +0000

    Add unit test for TelemetryService
    Add test case for handleTelemetryServiceGet function to verify the
    static attributes returned by the TelemetryService endpoint
    
    The test validates the following response fields:
    - @odata.type and @odata.id
    - Id and Name
    - Navigation properties: MetricReportDefinitions, MetricReports and
      Triggers
    
    Tested:
    - Built telemetry_service_test successfully
    - All assertions passed
    
    Change-Id: Ibc71ad9ff1796b90fed19d25be1525204919fa04
    Signed-off-by: Ramya Sivakumar <sramya@ami.com>

--- redfish-core/lib/telemetry_service.hpp
+++ redfish-core/lib/telemetry_service.hpp
@@ -29,26 +29,26 @@
 namespace redfish
 {
 
-inline void handleTelemetryServiceGet(
-    crow::App& app, const crow::Request& req,
+inline void setTelemetryServiceStaticAttributes(
+    nlohmann::json& json)
+{
+    json["@odata.type"] = "#TelemetryService.v1_2_1.TelemetryService";
+    json["@odata.id"] = "/redfish/v1/TelemetryService";
+    json["Id"] = "TelemetryService";
+    json["Name"] = "Telemetry Service";
+
+    json["MetricReportDefinitions"]["@odata.id"] =
+        "/redfish/v1/TelemetryService/MetricReportDefinitions";
+    json["MetricReports"]["@odata.id"] =
+        "/redfish/v1/TelemetryService/MetricReports";
+    json["Triggers"]["@odata.id"] =
+        "/redfish/v1/TelemetryService/Triggers";
+}
+
+inline void handleTelemetryServiceGetImpl(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
 {
-    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
-    {
-        return;
-    }
-    asyncResp->res.jsonValue["@odata.type"] =
-        "#TelemetryService.v1_2_1.TelemetryService";
-    asyncResp->res.jsonValue["@odata.id"] = "/redfish/v1/TelemetryService";
-    asyncResp->res.jsonValue["Id"] = "TelemetryService";
-    asyncResp->res.jsonValue["Name"] = "Telemetry Service";
-
-    asyncResp->res.jsonValue["MetricReportDefinitions"]["@odata.id"] =
-        "/redfish/v1/TelemetryService/MetricReportDefinitions";
-    asyncResp->res.jsonValue["MetricReports"]["@odata.id"] =
-        "/redfish/v1/TelemetryService/MetricReports";
-    asyncResp->res.jsonValue["Triggers"]["@odata.id"] =
-        "/redfish/v1/TelemetryService/Triggers";
+    setTelemetryServiceStaticAttributes(asyncResp->res.jsonValue);
 
     dbus::utility::getAllProperties(
         telemetry::service, "/xyz/openbmc_project/Telemetry/Reports",
@@ -104,6 +104,18 @@ inline void handleTelemetryServiceGet(
             asyncResp->res.jsonValue["SupportedCollectionFunctions"] =
                 std::move(supportedCollectionFunctions);
         });
+}
+
+inline void handleTelemetryServiceGet(
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    handleTelemetryServiceGetImpl(asyncResp);
 }
 
 inline void requestRoutesTelemetryService(App& app)
---
+++ test/redfish-core/lib/telemetry_service_test.cpp
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#include "telemetry_service.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+TEST(TelemetryServiceTest, TelemetryServiceStaticAttributesAreExpected)
+{
+    nlohmann::json json;
+
+    setTelemetryServiceStaticAttributes(json);
+
+    EXPECT_EQ(json.size(), 7);
+    EXPECT_EQ(json.at("@odata.type"), "#TelemetryService.v1_2_1.TelemetryService");
+    EXPECT_EQ(json.at("@odata.id"), "/redfish/v1/TelemetryService");
+    EXPECT_EQ(json.at("Id"), "TelemetryService");
+    EXPECT_EQ(json.at("Name"), "Telemetry Service");
+
+    const nlohmann::json& metricReportDefinitions =
+        json.at("MetricReportDefinitions");
+    EXPECT_EQ(metricReportDefinitions.size(), 1);
+    EXPECT_EQ(metricReportDefinitions.at("@odata.id"),
+              "/redfish/v1/TelemetryService/MetricReportDefinitions");
+
+    const nlohmann::json& metricReports = json.at("MetricReports");
+    EXPECT_EQ(metricReports.size(), 1);
+    EXPECT_EQ(metricReports.at("@odata.id"),
+              "/redfish/v1/TelemetryService/MetricReports");
+
+    const nlohmann::json& triggers = json.at("Triggers");
+    EXPECT_EQ(triggers.size(), 1);
+    EXPECT_EQ(triggers.at("@odata.id"), "/redfish/v1/TelemetryService/Triggers");
+}
+
+} // namespace
+} // namespace redfish
