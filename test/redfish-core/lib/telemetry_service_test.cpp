8afebbdafa3a191811575ddc06a77a6c6199d67d
Ramya Sivakumar <sramya@ami.com>
2026-02-06 09:42:11 +0000

    Add unit test for TelemetryService
    Add test case for handleTelemetryServiceGet function to verify the
    static attributes returned by the TelemetryService endpoint
    
    The test validates the following response fields:
    - @odata.type and @odata.id
    - Id and Name
    - Navigation properties: MetricReportDefinitions, MetricReports and
      Triggers
    
    Tested:
    - Built telemetry_service_test successfully
    - All assertions passed
    
    Change-Id: Ibc71ad9ff1796b90fed19d25be1525204919fa04
    Signed-off-by: Ramya Sivakumar <sramya@ami.com>

---
+++ test/redfish-core/lib/telemetry_service_test.cpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#include "app.hpp"
+#include "async_resp.hpp"
+#include "http_request.hpp"
+#include "http_response.hpp"
+#include "telemetry_service.hpp"
+
+#include <boost/beast/http/verb.hpp>
+#include <nlohmann/json.hpp>
+
+#include <memory>
+#include <string>
+#include <system_error>
+
+#include <gtest/gtest.h>
+
+namespace redfish
+{
+namespace
+{
+
+void assertTelemetryServiceGet(crow::Response& res)
+{
+    nlohmann::json& json = res.jsonValue;
+    EXPECT_EQ(json["@odata.type"], "#TelemetryService.v1_2_1.TelemetryService");
+    EXPECT_EQ(json["@odata.id"], "/redfish/v1/TelemetryService");
+    EXPECT_EQ(json["Id"], "TelemetryService");
+    EXPECT_EQ(json["Name"], "Telemetry Service");
+    EXPECT_EQ(json["MetricReportDefinitions"]["@odata.id"],
+              "/redfish/v1/TelemetryService/MetricReportDefinitions");
+    EXPECT_EQ(json["MetricReports"]["@odata.id"],
+              "/redfish/v1/TelemetryService/MetricReports");
+    EXPECT_EQ(json["Triggers"]["@odata.id"],
+              "/redfish/v1/TelemetryService/Triggers");
+}
+
+TEST(TelemetryServiceTest, TelemetryServiceStaticAttributesAreExpected)
+{
+    auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
+    asyncResp->res.setCompleteRequestHandler(assertTelemetryServiceGet);
+
+    crow::App app;
+    crow::Request req{
+        {boost::beast::http::verb::get, "/redfish/v1/TelemetryService", 11}};
+
+    handleTelemetryServiceGet(app, req, asyncResp);
+}
+
+} // namespace
+} // namespace redfish
