f06de88d57256d2af07de991680aecc4168597be
Ed Tanous <etanous@nvidia.com>
2026-02-10 09:21:21 -0800

    Port nlohmann::json::parse uses to sax
    We should have a single entry point where we do json parsing.  There are
    configurations for nlohmmann that we had previously documented, but were
    not well enforced.  Move all uses to using the helper parse functions.
    
    Change-Id: I2a8aed9327b6b15219dc9b4d6db146b69bcd8eb3
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- http/parsing.hpp
+++ http/parsing.hpp
@@ -147,7 +147,7 @@ class bmcweb_sax_parse : public nlohmann::json::json_sax_t
 };
 } // namespace details
 
-inline std::optional<nlohmann::json> parseStringAsJson(const std::string& body)
+inline std::optional<nlohmann::json> parseStringAsJson(std::string_view body)
 {
     nlohmann::json jsonOut;
     // Arbitrarily limit to 1MB payloads
