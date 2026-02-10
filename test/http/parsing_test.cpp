154a31e0fa06649ddfbc2978f4958282abf04e1a
Ed Tanous <etanous@nvidia.com>
2026-02-10 08:32:07 -0800

    Be more paranoid in json parsing
    When taking json directly from a user, we should set some limits on
    parsing depth as well as total number of keys.  This is to prevent
    flooding type attacks.  This commit makes use of the nlohmann sax parse
    to handle this by injecting a new error handler in between.
    
    Currently this sets the depth limit to 10 and the total number of keys
    to 500;  These are intentionally high.
    
    Change-Id: I789543679e22b0b0ce0b2b0b71f31377b0759cd7
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- test/http/parsing_test.cpp
+++ test/http/parsing_test.cpp
@@ -2,10 +2,76 @@
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 #include "http/parsing.hpp"
 
+#include <format>
+#include <string>
+
 #include <gtest/gtest.h>
 
 namespace
 {
+
+// Makes an array of n elements
+std::string makeWideArray(int n)
+{
+    n -= 1; // The array itself counts as one element
+    std::string os;
+    os += "[";
+    for (int i = 0; i < n; ++i)
+    {
+        if (i != 0)
+        {
+            os += ",";
+        }
+        os += std::to_string(i);
+    }
+    os += "]";
+    return os;
+}
+
+std::string makeDeepArray(int n)
+{
+    std::string os;
+    for (int i = 0; i < n; ++i)
+    {
+        os += "[";
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        os += "]";
+    }
+    return os;
+}
+// Makes an object of n elements deep
+std::string makeDeepObject(int n)
+{
+    std::string os;
+    for (int i = 0; i < n; ++i)
+    {
+        os += std::format(R"({{"{}": )", i);
+    }
+    os += "{";
+    for (int i = 0; i < n + 1; ++i)
+    {
+        os += "}";
+    }
+    return os;
+}
+
+std::string makeWideObject(int n)
+{
+    std::string os;
+    os += "{";
+    for (int i = 0; i < n; ++i)
+    {
+        os += std::format(R"("{}": {})", i, i);
+        if (i != n - 1)
+        {
+            os += ",";
+        }
+    }
+    os += "}";
+    return os;
+}
 
 TEST(HttpParsing, isJsonContentType)
 {
@@ -31,4 +97,36 @@ TEST(HttpParsing, isJsonContentType)
     EXPECT_FALSE(isJsonContentType("application/json; "));
     EXPECT_FALSE(isJsonContentType("json"));
 }
+
+TEST(HttpParsing, parseRequestAsJsonLimitsArrayDepth)
+{
+    EXPECT_TRUE(parseStringAsJson(makeDeepArray(10)))
+        << "10 level deep should parse";
+
+    EXPECT_FALSE(parseStringAsJson(makeDeepArray(11)))
+        << "11 level deep should fail to parse";
+}
+
+TEST(HttpParsing, parseRequestAsJsonLimitsObjectDepths)
+{
+    EXPECT_TRUE(parseStringAsJson(makeDeepObject(9)))
+        << "10 level deep should parse";
+    EXPECT_FALSE(parseStringAsJson(makeDeepObject(10)))
+        << "10 level deep should fail to parse";
+}
+
+TEST(HttpParsing, parseStringAsJsonMaxValues)
+{
+    EXPECT_TRUE(parseStringAsJson(makeWideArray(499)))
+        << "499 values should parse";
+    EXPECT_FALSE(parseStringAsJson(makeWideArray(501)))
+        << "500 values should be rejected";
+
+    // Keys and values are each counted separately, so 500/2 = 250 dict elements
+    EXPECT_TRUE(parseStringAsJson(makeWideObject(249)))
+        << "249 keys objects should parse";
+    EXPECT_FALSE(parseStringAsJson(makeWideObject(250)))
+        << "250 keys should be rejected";
+}
+
 } // namespace
