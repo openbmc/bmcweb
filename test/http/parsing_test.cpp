17bdc7446f9682f83ac6d56e3f216c5b5b1140e6
Ed Tanous <etanous@nvidia.com>
2026-02-10 08:32:07 -0800

    Be more paranoid in json parsing
    When taking json directly from a user, we should set some limits on
    parsing depth as well as total number of keys.  This is to prevent
    flooding type attacks.  This commit makes use of the nlohmann sax parse
    to handle this by injecting a new error handler in between.
    
    Currently this sets the depth limit to 10 and the total number of keys
    to 500;  These are intentionally high.
    
    Change-Id: I789543679e22b0b0ce0b2b0b71f31377b0759cd7
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- test/http/parsing_test.cpp
+++ test/http/parsing_test.cpp
@@ -2,6 +2,9 @@
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 #include "http/parsing.hpp"
 
+#include <format>
+#include <string>
+
 #include <gtest/gtest.h>
 
 namespace
@@ -31,4 +34,54 @@ TEST(HttpParsing, isJsonContentType)
     EXPECT_FALSE(isJsonContentType("application/json; "));
     EXPECT_FALSE(isJsonContentType("json"));
 }
+
+TEST(HttpParsing, parseRequestAsJsonLimitsArrayDepth)
+{
+    // 10 levels deep
+    std::string veryDeepArray = "[[[[[[[[[[]]]]]]]]]]";
+
+    EXPECT_TRUE(parseStringAsJson(veryDeepArray));
+
+    veryDeepArray = std::format("[{}]", veryDeepArray);
+    EXPECT_FALSE(parseStringAsJson(veryDeepArray));
+}
+
+TEST(HttpParsing, parseRequestAsJsonLimitsObjectDepths)
+{
+    // 10 levels deep
+    std::string veryDeepObject =
+        R"({"1":{"2":{"3":{"4":{"5":{"6":{"7":{"8":{"9":{}}}}}}}}}})";
+    EXPECT_FALSE(nlohmann::json::parse(veryDeepObject).is_discarded());
+    EXPECT_TRUE(parseStringAsJson(veryDeepObject));
+
+    veryDeepObject = std::format(R"({{"0": {}}})", veryDeepObject);
+    EXPECT_FALSE(parseStringAsJson(veryDeepObject));
+}
+
+// Makes an array of n elements
+std::string makeArray(size_t n)
+{
+    std::string os;
+    os += "[";
+    for (size_t i = 0; i < n; ++i)
+    {
+        if (i != 0)
+        {
+            os += ",";
+        }
+        os += std::to_string(i);
+    }
+    os += "]";
+    return os;
+}
+
+TEST(HttpParsing, parseStringAsJsonMaxValues)
+{
+    std::string json500 = makeArray(500);
+    EXPECT_TRUE(parseStringAsJson(json500)) << "500 values should parse";
+
+    std::string json501 = makeArray(501);
+    EXPECT_FALSE(parseStringAsJson(json501)) << "501 values should be rejected";
+}
+
 } // namespace
