93ed03cea1c55f5f063d3e260931b752746d450b
Eric Liu <liuer@nvidia.com>
2026-03-05 12:27:48 +0900

    processor: add TotalMemorySizeMiB to MemorySummary
    Add TotalMemorySizeMiB to the Processor
    MemorySummary by reading MemorySizeInKB from the
    associated GPU DRAM inventory object at
    /redfish/v1/Systems/system/Processors/{ProcessorId}
    
    This follows the DMTF Redfish Processor v1_18_0
    schema MemorySummary definition. TotalMemorySizeMiB
    reports the total GPU DRAM capacity attached to the
    processor.
    
    The DRAM inventory object is discovered via D-Bus
    Association using the "containing" endpoint on the
    Processor object with Item.Dram interface filter to
    locate the associated Memory object.
    
    The implementation maps D-Bus properties to Redfish
    as follows:
    - Dram.MemorySizeInKB (uint32, KiB) ->
      MemorySummary.TotalMemorySizeMiB (KiB / 1024)
    
    Key changes:
    - redfish-core/lib/processor.hpp: Add
      afterGetMemorySummaryEndpoints() to read
      MemorySizeInKB from Item.Dram. Add
      getProcessorMemorySummary() to query
      association endpoints via containing.
    - docs/Redfish.md: Add
      MemorySummary/TotalMemorySizeMiB.
    
    Tested:
    ```
    $ curl -sk \
      https://localhost/redfish/v1/Systems/system/\
      Processors/GPU_0_0
    {
      "@odata.type": "#Processor.v1_18_0.Processor",
      "MemorySummary": {
        "TotalMemorySizeMiB": 97887
      }
    }
    ```
    1. All GPU processors return valid
       TotalMemorySizeMiB.
    2. Redfish Service Validator: PASS.
    
    Change-Id: I5c619d3bbd17196584ea6e1ebbca591c74fb4f4e
    Signed-off-by: Eric Liu <liuer@nvidia.com>

--- redfish-core/lib/processor.hpp
+++ redfish-core/lib/processor.hpp
@@ -819,6 +819,45 @@ inline void getProcessorObject(
         });
 }
 
+inline void afterGetMemorySummaryEndpoints(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& service, const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreePathsResponse& endpoints)
+{
+    if (ec || endpoints.empty())
+    {
+        return;
+    }
+
+    const std::string& dramPath = endpoints.front();
+    dbus::utility::getProperty<size_t>(
+        service, dramPath, "xyz.openbmc_project.Inventory.Item.Dram",
+        "MemorySizeInKB",
+        [asyncResp](const boost::system::error_code& ec2,
+                    const size_t memorySizeInKB) {
+            if (ec2)
+            {
+                return;
+            }
+            asyncResp->res.jsonValue["MemorySummary"]["TotalMemorySizeMiB"] =
+                memorySizeInKB / 1024;
+        });
+}
+
+inline void getProcessorMemorySummary(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& service, const std::string& objectPath)
+{
+    constexpr std::array<std::string_view, 1> dramInterface = {
+        "xyz.openbmc_project.Inventory.Item.Dram"};
+
+    dbus::utility::getAssociatedSubTreePaths(
+        objectPath + "/containing",
+        sdbusplus::message::object_path("/xyz/openbmc_project/inventory"), 0,
+        dramInterface,
+        std::bind_front(afterGetMemorySummaryEndpoints, asyncResp, service));
+}
+
 inline void getProcessorData(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const std::string& processorId, const std::string& objectPath,
@@ -855,6 +894,7 @@ inline void getProcessorData(
             {
                 getAcceleratorDataByService(asyncResp, processorId, serviceName,
                                             objectPath);
+                getProcessorMemorySummary(asyncResp, serviceName, objectPath);
             }
             else if (
                 interface ==
