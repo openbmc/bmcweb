5b27dc779df85caf24fe195324cbd548a6910251
Ed Tanous <ed@tanous.net>
2026-03-13 16:35:19 -0700

    WIP: Encapsulate sd-journal
    It would be better if the larger compile units didn't have to pull in
    the sd-journal header.  At the same time, there's C handling of a number
    of resources that could be moved to an RAII class.
    
    Change-Id: If578316bcd1d0162b7ecdcca27c1c7818ce2b6fa
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ redfish-core/include/utils/journal_read_state.hpp
@@ -0,0 +1,45 @@
+#include <systemd/sd-journal.h>
+
+#include <optional>
+#include <string_view>
+
+namespace redfish
+{
+
+struct JournalReadState
+{
+    JournalReadState();
+    explicit JournalReadState(const std::string& path);
+
+    // Non copyable.  Movable
+    JournalReadState(const JournalReadState&) = delete;
+    JournalReadState(JournalReadState&& other) noexcept{
+        journal = other.journal;
+        other.journal = nullptr;
+    }
+    JournalReadState& operator=(const JournalReadState&) = delete;
+    JournalReadState& operator=(JournalReadState&& other) noexcept{
+        journal = other.journal;
+        other.journal = nullptr;
+        return *this;
+    }
+
+    int next();
+    int next_skip(size_t skip);
+    int previous();
+    int seek_tail();
+    int seek_head();
+    std::string get_cursor();
+    int seek_cursor(const char* cursor);
+    int test_cursor(const char* cursor);
+    std::optional<uint64_t> get_realtime_usec();
+
+    std::string_view get_data(const char* field);
+
+    int get_seqnum(uint64_t& seqnum);
+
+    ~JournalReadState();
+    sd_journal* journal = nullptr;
+};
+
+} // namespace redfish
