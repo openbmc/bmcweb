b201f4823629455289d8e3c9bcb0539288bda715
Ivan Mikhaylov <fr0st61te@gmail.com>
2026-04-03 17:15:03 +0300

    audit: add simple audit handler for BMCWEB actions
    Add simple sync audit call into set user password for processing of
    audit events.
    
    As example, password set as:
    ```
    curl -k -X PATCH "https://${bmc}/redfish/v1/AccountService/Accounts/someuser11" \
    -H "Content-Type: application/json" \
    -d '{"UserName":"someuser11","Password":"Custom45674~","RoleId":"Operator","Enabled":true,"Locked":false}' \
    -u root:0penBmc
    ```
    which lead to message into auditd subsystem:
    ```
    AUDIT1120 pid=854 uid=0 auid=4294967295 ses=4294967295 msg='Item: type[3] requestId[2] return code[0] user[root] data size[0] sockaddr size[16] Set User Password exe="/tmp/phosphor-auditd" hostname=get_hostname() addr=0.0.0.0/443 terminal=no_tty() res=success'
    ```
    
    [1] ```https://gerrit.openbmc.org/c/openbmc/phosphor-logging/+/76959```
    
    Change-Id: I7e994e10f12ce7c689d6d760ae8932a4651bacfc
    Signed-off-by: Ivan Mikhaylov <fr0st61te@gmail.com>

---
+++ redfish-core/lib/audit.hpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+
+#include "persistent_data.hpp"
+#include "utils/collection.hpp"
+#include "utils/dbus_utils.hpp"
+
+#include <phosphor-logging/audit/audit.hpp>
+
+namespace redfish
+{
+
+static std::optional<std::vector<std::tuple<uint32_t, uint32_t>>>
+    bmcwebAllowList;
+static std::optional<bool> bmcwebAuditEnabled;
+
+void bmcwebAuEvent(uint32_t pathId, int32_t retval,
+                   persistent_data::UserSession session)
+{
+    auto addr = boost::asio::ip::make_address(session.clientIp);
+    boost::asio::ip::tcp::endpoint ep(addr, 443);
+
+    phosphor::logging::audit::bmcwebAuditEvent(
+        pathId, retval, ep, session.username, bmcwebAuditEnabled,
+        bmcwebAllowList);
+}
+
+} // namespace redfish
