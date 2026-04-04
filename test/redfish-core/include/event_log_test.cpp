ee44ff31f84eb5ba06e0eaaca5cff074d5105f88
Archit Mehrotra <architmehrotra@meta.com>
2026-04-04 11:20:55 -0700

    Store Log Entry URI for Redfish event
    Store Log Entry URI for a Redfish event. This is to ensure
    the originating log entry can be tracked from the RF event.
    
    Tested:
      Unit tests pass.
    
    Change-Id: I46de49f47cc3783070a1d207394d5e1f02ff11be
    Signed-off-by: Archit Mehrotra <architmehrotra@meta.com>

--- test/redfish-core/include/event_log_test.cpp
+++ test/redfish-core/include/event_log_test.cpp
@@ -1,5 +1,8 @@
+#include "bmcweb_config.h"
+
 #include "event_log.hpp"
 
+#include <boost/url/format.hpp>
 #include <nlohmann/json.hpp>
 
 #include <cerrno>
@@ -163,6 +166,24 @@ TEST(RedfishEventLog, FormatEventLogEntrySuccess)
 
     ASSERT_TRUE(logEntryJson.contains("Context"));
     ASSERT_EQ(logEntryJson["Context"], "customText");
+
+    ASSERT_TRUE(logEntryJson.contains("LogEntry"));
+    if constexpr (BMCWEB_REDFISH_EVENTLOG_LOCATION == "systems")
+    {
+        ASSERT_EQ(
+            logEntryJson["LogEntry"]["@odata.id"],
+            boost::urls::format(
+                "/redfish/v1/Systems/{}/LogServices/EventLog/Entries/{}",
+                BMCWEB_REDFISH_SYSTEM_URI_NAME, logEntryID));
+    }
+    else if constexpr (BMCWEB_REDFISH_EVENTLOG_LOCATION == "managers")
+    {
+        ASSERT_EQ(
+            logEntryJson["LogEntry"]["@odata.id"],
+            boost::urls::format(
+                "/redfish/v1/Managers/{}/LogServices/EventLog/Entries/{}",
+                BMCWEB_REDFISH_MANAGER_URI_NAME, logEntryID));
+    }
 }
 
 TEST(RedfishEventLog, FormatEventLogEntryFail)
