3b7228c44674d1f62c13945ee25210da185e9fc9
Ed Tanous <ed@tanous.net>
2026-04-08 14:31:51 -0700

    WIP: Update and add unit tests for date string function
    To do this, refactor the date string function to a single method that
    can be unit tested easily.  While we're here, deprecate our usage of
    time_t and move to std::chrono
    
    Change-Id: I67d4fe66d40b06ed0a0b60286adc75394c34ea1e
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ test/http/http_server_test.cpp
@@ -0,0 +1,27 @@
+#include "http/http_server.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using ::testing::MatchesRegex;
+
+namespace crow
+{
+
+TEST(HttpServer, getCachedDateStr)
+{
+    struct FakeHandler
+    {};
+    FakeHandler handler;
+    crow::Server<FakeHandler> server(&handler, {});
+    std::string firstString = server.getCachedDateStr();
+    EXPECT_THAT(
+        firstString,
+        MatchesRegex(
+            "(Mon|Tue|Wed|Thu|Fri|Sat|Sun), (0[1-9]|[12][0-9]|3[01]) "
+            "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{4} "
+            "([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] GMT"));
+
+    EXPECT_EQ(firstString, server.getCachedDateStr());
+}
+} // namespace crow
