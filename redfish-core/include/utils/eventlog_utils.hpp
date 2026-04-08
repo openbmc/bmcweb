1a7f443778f5273b206f0e87c70507396ee00fad
Ed Tanous <ed@tanous.net>
2026-04-08 12:54:34 -0700

    Remove static variables
    These static variables have the potential to cause reentrancy issues.
    In practice, the conditions to cause issues would require someone to
    basically write incorrect code, but it makes sense to wrap this into a
    state tracker anyway to clean up the code.  While we're here, convert to
    using std::chrono.
    
    Change-Id: Icb7cff1d289ae23790a5fb1db6604abd73dd68fd
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/include/utils/eventlog_utils.hpp
+++ redfish-core/include/utils/eventlog_utils.hpp
@@ -339,6 +339,7 @@ inline void handleSystemsAndManagersLogServiceEventLogLogEntryCollection(
 
     // Oldest logs are in the last file, so start there and loop
     // backwards
+    event_log::UniqueEntryIDState state;
     for (auto it = redfishLogFiles.rbegin(); it < redfishLogFiles.rend(); it++)
     {
         std::ifstream logStream(*it);
@@ -351,7 +352,7 @@ inline void handleSystemsAndManagersLogServiceEventLogLogEntryCollection(
         while (std::getline(logStream, logEntry))
         {
             std::string idStr;
-            if (!event_log::getUniqueEntryID(logEntry, idStr))
+            if (!event_log::getUniqueEntryID(state, logEntry, idStr))
             {
                 continue;
             }
@@ -418,6 +419,7 @@ inline void handleSystemsAndManagersLogServiceEventLogEntriesGet(
 
     // Oldest logs are in the last file, so start there and loop
     // backwards
+    event_log::UniqueEntryIDState state;
     for (auto it = redfishLogFiles.rbegin(); it < redfishLogFiles.rend(); it++)
     {
         std::ifstream logStream(*it);
@@ -430,7 +432,7 @@ inline void handleSystemsAndManagersLogServiceEventLogEntriesGet(
         while (std::getline(logStream, logEntry))
         {
             std::string idStr;
-            if (!event_log::getUniqueEntryID(logEntry, idStr))
+            if (!event_log::getUniqueEntryID(state, logEntry, idStr))
             {
                 continue;
             }
