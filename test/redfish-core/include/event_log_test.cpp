44370acb73fc01a5a5ddea155d3a9a509211ca3e
Ed Tanous <ed@tanous.net>
2026-04-14 12:09:03 -0700

    Fix GetUniqueEntryIDUnique unit test
    This should be doing exact matching, not partial matching.  This failed
    to catch a bug in a later change.
    
    Note, this unit test will currently only pass on the UTC timezone.
    Later in the series the backend is fixed to produce consistent results
    regardless of timezone
    
    Tested: unit test
    
    Change-Id: Ie5652c0f9503b4de926addcfc292c76b59682b12
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- test/redfish-core/include/event_log_test.cpp
+++ test/redfish-core/include/event_log_test.cpp
@@ -33,22 +33,16 @@ TEST(RedfishEventLog, GetUniqueEntryIDSuccess)
 
 TEST(RedfishEventLog, GetUniqueEntryIDUnique)
 {
-    bool success = false;
     std::string entryID1;
-    std::string entryID2;
     std::string example = "2000-08-02T03:04:05";
 
-    success = getUniqueEntryID(example, entryID1);
-    ASSERT_EQ(success, true);
-    success = getUniqueEntryID(example, entryID2);
-    ASSERT_EQ(success, true);
+    ASSERT_TRUE(getUniqueEntryID(example, entryID1));
 
-    // when calling a second time with the same argument
-    // there should be an underscore
-    ASSERT_TRUE(entryID2.contains("_"));
+    ASSERT_EQ(entryID1, "965185445");
 
-    // only one '_' allowed
-    ASSERT_THAT(entryID2, testing::MatchesRegex("^[0-9]+_[0-9]+$"));
+    std::string entryID2;
+    ASSERT_TRUE(getUniqueEntryID(example, entryID2));
+    ASSERT_EQ(entryID2, "965185445_1");
 }
 
 TEST(RedfishEventLog, GetUniqueEntryIDIndex)
