6ac014ccd4a88de4feee0ff9e2eb206beb1a0b4f
Ed Tanous <ed@tanous.net>
2026-04-15 10:02:38 -0700

    Refactor Task payload into its own file
    This should've arguably been in its own file from the beginning.  Do it
    now.
    
    Tested: At end of series.
    
    Change-Id: I69a6913432712c0bf1db202aa38b622ce01a06f5
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- test/http/http_server_test.cpp
+++ test/http/http_server_test.cpp
@@ -21,6 +21,5 @@ TEST(HttpServer, getCachedDateStr)
             "(Mon|Tue|Wed|Thu|Fri|Sat|Sun), (0[1-9]|[12][0-9]|3[01]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{4} ([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] GMT"));
 
     EXPECT_EQ(firstString, server.getCachedDateStr());
-
 }
 } // namespace crow
