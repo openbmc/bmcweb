5ce23d112a4740e29de4685314ff0791751c8ed1
Sandeep Akant Patil <sandeepap@ami.com>
2026-04-21 07:19:51 +0000

    Add PrivilegeRegistry Redfish endpoint support
    Enable Redfish clients to discover and retrieve
    operation-to-privilege mappings for BMC role authorization.
    Management tools can query which privileges (Login,
    ConfigureManager, ConfigureUsers, etc.) are required for
    HTTP operations on different resource entities.
    
    Tested: Tested on evb2600
    
    /redfish/v1/Registries
    
    {
        "@odata.id": "/redfish/v1/Registries",
        "@odata.type": "#MessageRegistryFileCollection.MessageRegistryFileCollection",
        "Description": "Collection of MessageRegistryFiles",
        "Members": [
            {
                "@odata.id": "/redfish/v1/Registries/Base"
            },
            {
                "@odata.id": "/redfish/v1/Registries/HeartbeatEvent"
            },
            {
                "@odata.id": "/redfish/v1/Registries/OpenBMC"
            },
            {
                "@odata.id": "/redfish/v1/Registries/ResourceEvent"
            },
            {
                "@odata.id": "/redfish/v1/Registries/TaskEvent"
            },
            {
                "@odata.id": "/redfish/v1/Registries/Telemetry"
            },
            {
                "@odata.id": "/redfish/v1/Registries/PrivilegeRegistry"
            }
        ],
        "Members@odata.count": 7,
        "Name": "MessageRegistryFile Collection"
    }
    
    
    Signed-off-by: Sandeep Akant Patil <sandeepap@ami.com>
    Change-Id: I4b411831dc8f541375bf8895fcddc495ec7be118

--- redfish-core/lib/message_registries.hpp
+++ redfish-core/lib/message_registries.hpp
@@ -9,6 +9,7 @@
 #include "http_request.hpp"
 #include "query.hpp"
 #include "registries.hpp"
+#include "registries/privilege_mapping.hpp"
 #include "registries/privilege_registry.hpp"
 
 #include <boost/beast/http/verb.hpp>
@@ -51,6 +52,11 @@ inline void handleMessageRegistryFileCollectionGet(
             boost::urls::format("/redfish/v1/Registries/{}", memberName);
         members.emplace_back(std::move(member));
     }
+
+    // Privilege registry is served by dedicated handlers and is not in
+    // registries::allRegistries().
+    members.emplace_back(nlohmann::json::object_t{
+        {"@odata.id", "/redfish/v1/Registries/PrivilegeRegistry"}});
     asyncResp->res.jsonValue["Members@odata.count"] = members.size();
 }
 
@@ -65,6 +71,94 @@ inline void requestRoutesMessageRegistryFileCollection(App& app)
             handleMessageRegistryFileCollectionGet, std::ref(app)));
 }
 
+inline void fillPrivilegeRegistry(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const registries::Header& header)
+{
+    asyncResp->res.jsonValue["@Redfish.Copyright"] = header.copyright;
+    asyncResp->res.jsonValue["@odata.type"] = header.type;
+    asyncResp->res.jsonValue["Id"] =
+        std::format("{}.{}.{}.{}", header.registryPrefix, header.versionMajor,
+                    header.versionMinor, header.versionPatch);
+    asyncResp->res.jsonValue["Name"] = header.name;
+    asyncResp->res.jsonValue["Language"] = header.language;
+    asyncResp->res.jsonValue["Description"] = header.description;
+    asyncResp->res.jsonValue["RegistryPrefix"] = header.registryPrefix;
+    asyncResp->res.jsonValue["RegistryVersion"] =
+        std::format("{}.{}.{}", header.versionMajor, header.versionMinor,
+                    header.versionPatch);
+    asyncResp->res.jsonValue["OwningEntity"] = header.owningEntity;
+
+    nlohmann::json& privilegeObj = asyncResp->res.jsonValue["privilegeUsed"];
+    privilegeObj = nlohmann::json::array();
+
+    for (const char* privilegeItem :
+         registries::privilege_registry::privilegeUsed)
+    {
+        if (privilegeItem == nullptr)
+        {
+            break;
+        }
+        privilegeObj.push_back(privilegeItem);
+    }
+
+    nlohmann::json& oemPrivileges =
+        asyncResp->res.jsonValue["OEMPrivilegesUsed"];
+    oemPrivileges = nlohmann::json::array();
+
+    for (const auto& entity : registries::privilege_registry::oemEntities)
+    {
+        std::string entityName = entity.first;
+        const auto& operationMaps = entity.second;
+
+        nlohmann::json oemPrivilegesObj = nlohmann::json::object();
+        oemPrivilegesObj["Entity"] = entityName;
+        oemPrivilegesObj["OperationMap"] = nlohmann::json::object();
+
+        for (const auto& operation : operationMaps)
+        {
+            const std::string& method = operation.first;
+            const auto& privileges = operation.second;
+            oemPrivilegesObj["OperationMap"][method] = nlohmann::json::array();
+
+            for (const auto& privilege : privileges)
+            {
+                oemPrivilegesObj["OperationMap"][method].push_back(
+                    {{"Privilege", nlohmann::json::array({privilege})}});
+            }
+        }
+
+        oemPrivileges.push_back(oemPrivilegesObj);
+    }
+
+    nlohmann::json& mappings = asyncResp->res.jsonValue["Mappings"];
+    mappings = nlohmann::json::array();
+    for (const auto& entity : registries::privilege_registry::entities)
+    {
+        std::string entityName = entity.first;
+        const auto& operationMaps = entity.second;
+
+        nlohmann::json mappingObj = nlohmann::json::object();
+        mappingObj["Entity"] = entityName;
+        mappingObj["OperationMap"] = nlohmann::json::object();
+
+        for (const auto& operation : operationMaps)
+        {
+            const std::string& method = operation.first;
+            const auto& privileges = operation.second;
+
+            mappingObj["OperationMap"][method] = nlohmann::json::array();
+            for (const auto& privilege : privileges)
+            {
+                mappingObj["OperationMap"][method].push_back(
+                    {{"Privilege", nlohmann::json::array({privilege})}});
+            }
+        }
+
+        mappings.push_back(mappingObj);
+    }
+}
+
 inline void handleMessageRoutesMessageRegistryFileGet(
     crow::App& app, const crow::Request& req,
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
@@ -74,6 +168,54 @@ inline void handleMessageRoutesMessageRegistryFileGet(
     {
         return;
     }
+
+    const registries::Header& privilegeHeader =
+        registries::privilege_registry::header;
+    const char* privilegeUrl = registries::privilege_registry::url;
+
+    // Custom flat endpoint for the full PrivilegeRegistry document.
+    if (registry == std::format("{}.json", privilegeHeader.name))
+    {
+        fillPrivilegeRegistry(asyncResp, privilegeHeader);
+        return;
+    }
+
+    if (registry == "PrivilegeRegistry")
+    {
+        asyncResp->res.jsonValue["@odata.id"] =
+            "/redfish/v1/Registries/PrivilegeRegistry";
+        asyncResp->res.jsonValue["@odata.type"] =
+            "#MessageRegistryFile.v1_1_0.MessageRegistryFile";
+        asyncResp->res.jsonValue["Name"] =
+            "PrivilegeRegistry Message Registry File";
+        asyncResp->res.jsonValue["Description"] =
+            "DMTF PrivilegeRegistry Message Registry File Location";
+        asyncResp->res.jsonValue["Id"] = privilegeHeader.registryPrefix;
+        asyncResp->res.jsonValue["Registry"] = std::format(
+            "{}.{}.{}", privilegeHeader.registryPrefix,
+            privilegeHeader.versionMajor, privilegeHeader.versionMinor);
+
+        nlohmann::json::array_t languages;
+        languages.emplace_back(privilegeHeader.language);
+        asyncResp->res.jsonValue["Languages@odata.count"] = languages.size();
+        asyncResp->res.jsonValue["Languages"] = std::move(languages);
+
+        nlohmann::json::array_t locationMembers;
+        nlohmann::json::object_t location;
+        location["Language"] = privilegeHeader.language;
+        location["Uri"] =
+            "/redfish/v1/Registries/Redfish_1.5.0_PrivilegeRegistry.json";
+        if (privilegeUrl != nullptr)
+        {
+            location["PublicationUri"] = privilegeUrl;
+        }
+        locationMembers.emplace_back(std::move(location));
+        asyncResp->res.jsonValue["Location@odata.count"] =
+            locationMembers.size();
+        asyncResp->res.jsonValue["Location"] = std::move(locationMembers);
+        return;
+    }
+
     std::string dmtf = "DMTF ";
     std::optional<registries::RegistryEntryRef> registryEntry =
         registries::getRegistryFromPrefix(registry);
@@ -88,6 +230,7 @@ inline void handleMessageRoutesMessageRegistryFileGet(
     {
         dmtf.clear();
     }
+
     const registries::Header& header = registryEntry->get().header;
     const char* url = registryEntry->get().url;
 
@@ -136,6 +279,20 @@ inline void handleMessageRegistryGet(
 {
     if (!redfish::setUpRedfishRoute(app, req, asyncResp))
     {
+        return;
+    }
+
+    if (registry == "PrivilegeRegistry")
+    {
+        const registries::Header& privilegeHeader =
+            registries::privilege_registry::header;
+        if (registryMatch != privilegeHeader.name)
+        {
+            messages::resourceNotFound(asyncResp->res, privilegeHeader.type,
+                                       registryMatch);
+            return;
+        }
+        fillPrivilegeRegistry(asyncResp, privilegeHeader);
         return;
     }
 
