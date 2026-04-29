7038a05f4ee20ae465d21901e6a73f11e9043ada
Ed Tanous <etanous@nvidia.com>
2026-04-29 15:24:19 -0700

    Port update_schemas script to use git
    DMTF released Redfish-Publications a while ago.  Recently, they've
    started blocking scripts from accessing the website.
    
    This is fully vibe coded with some review by me, but seems to produce
    the same result we had previously, and if there were differences they'd
    show up in review.
    
    Change-Id: Ib3686b610ca6a60b1fae12b575042575b32f6ec5
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- scripts/update_schemas.py
+++ scripts/update_schemas.py
@@ -1,44 +1,27 @@
 #!/usr/bin/env python3
 import os
 import shutil
-import zipfile
+import subprocess
+import tempfile
 from collections import OrderedDict, defaultdict
-from io import BytesIO
 
 import generate_schema_enums
-import requests
 from generate_schema_collections import generate_top_collections
 
-VERSION = "DSP8010_2025.4"
+REPO_URL = "https://github.com/DMTF/Redfish-Publications.git"
+TAG = "2025.4"
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-proxies = {"https": os.environ.get("https_proxy", None)}
-
-r = requests.get(
-    "https://www.dmtf.org/sites/default/files/standards/documents/"
-    + VERSION
-    + ".zip",
-    proxies=proxies,
-)
-
-r.raise_for_status()
-
 redfish_core_path = os.path.join(SCRIPT_DIR, "..", "redfish-core")
-
-cpp_path = os.path.realpath(os.path.join(redfish_core_path, "include"))
 
 schema_path = os.path.join(redfish_core_path, "schema", "dmtf", "csdl")
 json_schema_path = os.path.join(
     redfish_core_path, "schema", "dmtf", "json-schema"
 )
 
-# installed csdl/json symlinks
 schema_installed_path = os.path.join(schema_path, "..", "installed")
 json_schema_installed_path = json_schema_path + "-installed"
-
-zipBytesIO = BytesIO(r.content)
-zip_ref = zipfile.ZipFile(zipBytesIO)
 
 
 class SchemaVersion:
@@ -67,86 +50,77 @@ class SchemaVersion:
         return self.version_pieces < other.version_pieces
 
 
-shutil.rmtree(schema_path)
-os.makedirs(schema_path)
+with tempfile.TemporaryDirectory() as repo_dir:
+    subprocess.run(
+        [
+            "git",
+            "clone",
+            "--depth=1",
+            "--branch",
+            TAG,
+            REPO_URL,
+            repo_dir,
+        ],
+        check=True,
+    )
 
-shutil.rmtree(json_schema_path)
-os.makedirs(json_schema_path)
+    repo_csdl_dir = os.path.join(repo_dir, "csdl")
+    repo_json_dir = os.path.join(repo_dir, "json-schema")
 
-csdl_filenames = []
-json_schema_files = defaultdict(list)
+    shutil.rmtree(schema_path)
+    os.makedirs(schema_path)
 
-for zip_file in zip_ref.infolist():
-    if zip_file.is_dir():
-        continue
-    if zip_file.filename.startswith(VERSION + "/csdl/"):
-        csdl_filenames.append(os.path.basename(zip_file.filename))
-    elif zip_file.filename.startswith(VERSION + "/json-schema/"):
-        filename = os.path.basename(zip_file.filename)
-        filenamesplit = filename.split(".")
-        json_schema_files[filenamesplit[0]].append(filename)
-    elif zip_file.filename.startswith(VERSION + "/openapi/"):
-        pass
-    elif zip_file.filename.startswith(VERSION + "/dictionaries/"):
-        pass
+    shutil.rmtree(json_schema_path)
+    os.makedirs(json_schema_path)
 
-# sort the json files by version
-for key, value in json_schema_files.items():
-    value.sort(key=SchemaVersion, reverse=True)
+    csdl_filenames = sorted(os.listdir(repo_csdl_dir))
 
-# Create a dictionary ordered by schema name
-json_schema_files = OrderedDict(
-    sorted(json_schema_files.items(), key=lambda x: SchemaVersion(x[0]))
-)
+    json_schema_files = defaultdict(list)
+    for filename in os.listdir(repo_json_dir):
+        json_schema_files[filename.split(".")[0]].append(filename)
 
-# Get the currently-installed csdl
-csdl_installed_symlinks = set()
-for csdl_symlink in os.listdir(schema_installed_path):
-    csdl_installed_symlinks.add(csdl_symlink)
+    for value in json_schema_files.values():
+        value.sort(key=SchemaVersion, reverse=True)
 
-shutil.rmtree(schema_installed_path)
-os.makedirs(schema_installed_path)
+    json_schema_files = OrderedDict(
+        sorted(json_schema_files.items(), key=lambda x: SchemaVersion(x[0]))
+    )
 
-# Create csdl files & keep the updated csdl-installed symlinks
-for csdl_file in csdl_filenames:
-    with open(os.path.join(schema_path, csdl_file), "wb") as schema_out:
-        content = zip_ref.read(os.path.join(VERSION + "/csdl", csdl_file))
-        content = content.replace(b"\r\n", b"\n")
-        schema_out.write(content)
+    csdl_installed_symlinks = set(os.listdir(schema_installed_path))
+    shutil.rmtree(schema_installed_path)
+    os.makedirs(schema_installed_path)
 
-    if csdl_file in csdl_installed_symlinks:
-        os.symlink(
-            os.path.join("..", "csdl", csdl_file),
-            os.path.join(schema_installed_path, csdl_file),
+    for csdl_file in csdl_filenames:
+        shutil.copy(
+            os.path.join(repo_csdl_dir, csdl_file),
+            os.path.join(schema_path, csdl_file),
         )
 
-# Get the currently-installed json symlinks
-json_base_symlinks = defaultdict(list)
-for json_symlink in os.listdir(json_schema_installed_path):
-    base_json_name = json_symlink.split(".")[0]
-    json_base_symlinks[base_json_name].append(json_symlink)
+        if csdl_file in csdl_installed_symlinks:
+            os.symlink(
+                os.path.join("..", "csdl", csdl_file),
+                os.path.join(schema_installed_path, csdl_file),
+            )
 
-shutil.rmtree(json_schema_installed_path)
-os.makedirs(json_schema_installed_path)
+    json_base_symlinks = defaultdict(list)
+    for json_symlink in os.listdir(json_schema_installed_path):
+        base_json_name = json_symlink.split(".")[0]
+        json_base_symlinks[base_json_name].append(json_symlink)
 
-# Create json files & keep the installed json symlinks
-for schema_filename, versions in json_schema_files.items():
-    zip_filepath = os.path.join(VERSION + "/json-schema", versions[0])
+    shutil.rmtree(json_schema_installed_path)
+    os.makedirs(json_schema_installed_path)
 
-    with open(
-        os.path.join(json_schema_path, versions[0]), "wb"
-    ) as schema_file:
-        content = zip_ref.read(zip_filepath)
-        content = content.replace(b"\r\n", b"\n")
-        schema_file.write(content)
-
-    if schema_filename in json_base_symlinks:
-        os.symlink(
-            os.path.join("..", "json-schema", versions[0]),
-            os.path.join(json_schema_installed_path, versions[0]),
+    for schema_filename, versions in json_schema_files.items():
+        shutil.copy(
+            os.path.join(repo_json_dir, versions[0]),
+            os.path.join(json_schema_path, versions[0]),
         )
 
-zip_ref.close()
+        if schema_filename in json_base_symlinks:
+            os.symlink(
+                os.path.join("..", "json-schema", versions[0]),
+                os.path.join(json_schema_installed_path, versions[0]),
+            )
 
 generate_schema_enums.main()
 generate_top_collections()
