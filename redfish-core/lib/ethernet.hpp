03c88dd223a3720501caa2046f8e7886505bc885
AKash A007 <mirrorghost007@gmail.com>
2026-05-19 17:57:18 +0530

    ethernet: restrict PATCH to ConfigureManager privilege
    Per the OpenBMC user-management spec, operators are not allowed
    to change network configuration. Only admin-level roles should
    be permitted to PATCH EthernetInterface resources under a Manager.
    
    The PATCH handler for
    /redfish/v1/Managers/<str>/EthernetInterfaces/<str>/
    was using patchEthernetInterface (ConfigureComponents), which the
    operator role possesses. The Redfish PrivilegeRegistry already has
    patchEthernetInterfaceSubOverManagerEthernetInterfaceCollection
    defined for this specific Manager subordinate case.
    
    Change to use the SubOverride privilege which requires ConfigureManager.
    
    Fixes: https://github.com/openbmc/bmcweb/issues/284
    
    Tested:
    - operator user receives 403 Forbidden on PATCH EthernetInterface
    - admin user (root) receives 200 OK on same PATCH
    
    Change-Id: I7d15b369ed04dba0f55807ee86e309be76861765
    Signed-off-by: Akash Arunkumar <mirrorghost007@gmail.com>

--- redfish-core/lib/ethernet.hpp
+++ redfish-core/lib/ethernet.hpp
@@ -2309,7 +2309,9 @@ inline void requestEthernetInterfacesRoutes(App& app)
             });
 
     BMCWEB_ROUTE(app, "/redfish/v1/Managers/<str>/EthernetInterfaces/<str>/")
-        .privileges(redfish::privileges::patchEthernetInterface)
+        .privileges(
+            redfish::privileges::
+                patchEthernetInterfaceSubOverManagerEthernetInterfaceCollection)
         .methods(boost::beast::http::verb::patch)(
             [&app](const crow::Request& req,
                    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
