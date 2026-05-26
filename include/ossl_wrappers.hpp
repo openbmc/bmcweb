5fde3101be5823d4f33b1226f1d1296bba040e23
Ed Tanous <etanous@nvidia.com>
2026-05-26 13:28:59 -0700

    WIP: Allow setting Mozilla modern
    Mozilla publishes recommendations for TLS cipher suites to support.  For
    many years bmcweb selected "intermediate" because of compatibility with
    clients that didn't yet support TLS1.3.
    
    This commit changes to using Mozilla modern recommendations, and
    disables TLS1.2 support.  This should be a noop for almost all uses, as
    any modern OS has had support for this cipher suite for a very long
    time.  Mozilla intermediate is added still as a compile time fallback in
    case some clients are impacted, but expectation is that this is only a
    security increase[1]
    
    Tested:
    
    Loaded on qemu, and verified with testssl.sh[2] that parameters were
    applied.
    
    [1] https://caniuse.com/tls1-3
    [2] https://github.com/testssl/testssl.sh
    
    Change-Id: I38e915b3943b5dbe5fb31e54eb3ebda9bbaeb811
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- include/ossl_wrappers.hpp
+++ include/ossl_wrappers.hpp
@@ -990,11 +990,15 @@ class OpenSSLSSLCtx
 
     static OpenSSLSSLCtx createServerCtx()
     {
-        return OpenSSLSSLCtx(SSL_CTX_new(TLS_server_method()));
+        OpenSSLSSLCtx ctx(SSL_CTX_new(TLS_server_method()));
+        ctx.ptrOwned = true;
+        return ctx;
     }
     static OpenSSLSSLCtx createClientCtx()
     {
-        return OpenSSLSSLCtx(SSL_CTX_new(TLS_client_method()));
+        OpenSSLSSLCtx ctx(SSL_CTX_new(TLS_client_method()));
+        ctx.ptrOwned = true;
+        return ctx;
     }
 
     OpenSSLSSLCtx(const OpenSSLSSLCtx&) = delete;
@@ -1017,7 +1021,10 @@ class OpenSSLSSLCtx
     }
     ~OpenSSLSSLCtx()
     {
-        SSL_CTX_free(ptr);
+        if (ptrOwned)
+        {
+            SSL_CTX_free(ptr);
+        }
     }
 
     bool valid() const
@@ -1050,15 +1057,49 @@ class OpenSSLSSLCtx
         return X509_STORE_add_cert(store, cert.get()) == 1;
     }
 
-  private:
-    explicit OpenSSLSSLCtx(SSL_CTX* ptrIn) : ptr(ptrIn) {}
+    bool setCiphersuites(std::string_view ciphersuites)
+    {
+        std::string ciphersuitesStr(ciphersuites);
+        if (SSL_CTX_set_ciphersuites(ptr, ciphersuitesStr.data()) != 1)
+        {
+            BMCWEB_LOG_ERROR("Error setting TLS ciphersuites");
+            return false;
+        }
+        return true;
+    }
 
+    bool setCurves(std::string_view curves)
+    {
+        std::string curvesStr(curves);
+        if (SSL_CTX_set1_curves_list(ptr, curvesStr.data()) != 1)
+        {
+            BMCWEB_LOG_ERROR("Error setting TLS curve list");
+            return false;
+        }
+        return true;
+    }
+
+    bool setCipherList(std::string_view cipherList)
+    {
+        std::string cipherListStr(cipherList);
+        if (SSL_CTX_set_cipher_list(ptr, cipherListStr.data()) != 1)
+        {
+            BMCWEB_LOG_ERROR("Error setting cipher list");
+            return false;
+        }
+        return true;
+    }
+
+    explicit OpenSSLSSLCtx(SSL_CTX* ptrIn) : ptr(ptrIn), ptrOwned(false) {}
+
+  private:
     SSL_CTX* get() const
     {
         return ptr;
     }
 
     SSL_CTX* ptr;
+    bool ptrOwned = true;
 };
 
 class OpenSSLSSL
