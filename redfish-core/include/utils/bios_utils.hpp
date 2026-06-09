4f24c669f85a1bc27b3111b689fe90af29a175eb
Ravi Teja <rbailapu@qti.qualcomm.com>
2026-06-09 22:50:52 +0530

    Add bios settings Get and Patch support
    Add Redfish GET and PATCH support for the BIOS Settings resource at
    /redfish/v1/Systems/system/Bios/Settings. This exposes the BIOS pending
    configuration so clients can read staged settings and update them
    through standard Redfish BIOS Settings semantics.
    
    The implementation uses the xyz.openbmc_project.BIOSConfig.Manager
    PDI backend. GET reads PendingAttributes from D-Bus and formats the
    values under Redfish Attributes. PATCH accepts Redfish Attributes,
    merges them into the existing pending attribute map, and writes
    PendingAttributes back to D-Bus. The pending attribute payload matches
    PDI as a dictionary of attribute name to {AttributeType enum, value},
    where value is restricted to integer, string, or boolean BIOS
    attribute types.
    
    BIOS Settings support is needed to provide full Redfish-compliant BIOS
    configuration through Redfish interfaces. BIOS Attribute Registry
    support is planned as follow-up work.
    
    Here is design doc for redfish remote bios configuration:
    https://gerrit.openbmc.org/c/openbmc/docs/+/79482
    
    Tested:
    Redfish Service Validator passed.
    Verified GET and PATCH on /redfish/v1/Systems/system/Bios/Settings.
    
    Change-Id: I296bf9a18dfa0ba97c35153f069c3fa3a10e4a30
    Signed-off-by: Ravi Teja <rbailapu@qti.qualcomm.com>

--- redfish-core/include/utils/bios_utils.hpp
+++ redfish-core/include/utils/bios_utils.hpp
@@ -11,10 +11,12 @@
 #include <array>
 #include <cstdint>
 #include <functional>
+#include <map>
 #include <memory>
 #include <string>
 #include <string_view>
-#include <unordered_map>
+#include <tuple>
+#include <utility>
 #include <variant>
 
 namespace redfish
@@ -26,60 +28,42 @@ static constexpr std::string_view biosConfigManagerPath =
 static constexpr std::string_view biosConfigManagerInterface =
     "xyz.openbmc_project.BIOSConfig.Manager";
 
-template <typename Type>
-inline void extractValue(nlohmann::json& attributes, const std::string& name,
-                         const dbus::utility::DbusVariantType& value)
-{
-    const Type* tValue = std::get_if<Type>(&value);
-    if (tValue != nullptr)
-    {
-        attributes[name] = *tValue;
-        return;
-    }
-    attributes[name] = Type{};
-}
+using BiosAttributeType = std::string;
+using BiosAttributeValue = std::variant<int64_t, std::string, bool>;
 
-template <>
-inline void extractValue<bool>(nlohmann::json& attributes,
-                               const std::string& name,
-                               const dbus::utility::DbusVariantType& value)
-{
-    const int64_t* tValue = std::get_if<int64_t>(&value);
-    if (tValue != nullptr)
-    {
-        attributes[name] = (*tValue != 0);
-        return;
-    }
-    attributes[name] = false;
-}
-
-using HandlerType = std::function<void(nlohmann::json&, const std::string&,
-                                       const dbus::utility::DbusVariantType&)>;
+static constexpr std::string_view biosAttributeTypeEnumeration =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Enumeration";
+static constexpr std::string_view biosAttributeTypeString =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.String";
+static constexpr std::string_view biosAttributeTypePassword =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Password";
+static constexpr std::string_view biosAttributeTypeInteger =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Integer";
+static constexpr std::string_view biosAttributeTypeBoolean =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Boolean";
 
 inline void addAttribute(nlohmann::json& attributes, const std::string& name,
-                         const dbus::utility::DbusVariantType& type,
-                         const dbus::utility::DbusVariantType& value)
+                         const BiosAttributeType& type,
+                         const BiosAttributeValue& value)
 {
-    static const std::unordered_map<std::string, HandlerType> typeMap = {
-        {"xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Enumeration",
-         extractValue<std::string>},
-        {"xyz.openbmc_project.BIOSConfig.Manager.AttributeType.String",
-         extractValue<std::string>},
-        {"xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Password",
-         extractValue<std::string>},
-        {"xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Integer",
-         extractValue<int64_t>},
-        {"xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Boolean",
-         extractValue<bool>}};
-
-    const std::string* typeStr = std::get_if<std::string>(&type);
-    if (typeStr != nullptr)
+    if (type == biosAttributeTypeEnumeration ||
+        type == biosAttributeTypeString || type == biosAttributeTypePassword)
     {
-        auto it = typeMap.find(*typeStr);
-        if (it != typeMap.end())
-        {
-            it->second(attributes, name, value);
-        }
+        const std::string* stringValue = std::get_if<std::string>(&value);
+        attributes[name] =
+            stringValue == nullptr ? std::string{} : *stringValue;
+        return;
+    }
+    if (type == biosAttributeTypeInteger)
+    {
+        const int64_t* intValue = std::get_if<int64_t>(&value);
+        attributes[name] = intValue == nullptr ? int64_t{} : *intValue;
+        return;
+    }
+    if (type == biosAttributeTypeBoolean)
+    {
+        const bool* boolValue = std::get_if<bool>(&value);
+        attributes[name] = boolValue == nullptr ? false : *boolValue;
     }
 }
 
@@ -107,6 +91,27 @@ inline void getBIOSManagerProperty(
 }
 
 template <typename CallbackFunc>
+inline void handleBIOSManagerObject(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp, CallbackFunc& callback,
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperGetObject& object)
+{
+    if (ec || object.empty())
+    {
+        BMCWEB_LOG_ERROR("Error finding BIOS Manager object {}", ec);
+        messages::internalError(asyncResp->res);
+        return;
+    }
+    if (object.size() > 1)
+    {
+        BMCWEB_LOG_ERROR("More than one BIOS Manager object found");
+        messages::internalError(asyncResp->res);
+        return;
+    }
+    callback(object.begin()->first);
+}
+
+template <typename CallbackFunc>
 inline void getBIOSManagerObject(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     CallbackFunc&& callback)
@@ -116,20 +121,37 @@ inline void getBIOSManagerObject(
         std::array<std::string_view, 1>{biosConfigManagerInterface},
         [asyncResp, callback = std::forward<CallbackFunc>(callback)](
             const boost::system::error_code& ec,
-            const dbus::utility::MapperGetObject& object) {
-            if (ec || object.empty())
+            const dbus::utility::MapperGetObject& object) mutable {
+            handleBIOSManagerObject(asyncResp, callback, ec, object);
+        });
+}
+
+using PendingAttributeValue = std::tuple<BiosAttributeType, BiosAttributeValue>;
+enum class PendingAttributeValueIndex
+{
+    Type = 0,
+    Value
+};
+
+using PendingAttributes = std::map<std::string, PendingAttributeValue>;
+
+inline void setBIOSManagerProperty(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& propertyName, const PendingAttributes& propertyValue,
+    const std::string& objectPath)
+{
+    sdbusplus::asio::setProperty(
+        *crow::connections::systemBus, objectPath,
+        std::string(biosConfigManagerPath),
+        std::string(biosConfigManagerInterface), propertyName, propertyValue,
+        [asyncResp, propertyName](const boost::system::error_code& ec) {
+            if (ec)
             {
-                BMCWEB_LOG_ERROR("Error finding BIOS Manager object {}", ec);
+                BMCWEB_LOG_ERROR("DBus response error for setting {}: {}",
+                                 propertyName, ec);
                 messages::internalError(asyncResp->res);
                 return;
             }
-            if (object.size() > 1)
-            {
-                BMCWEB_LOG_ERROR("More than one BIOS Manager object found");
-                messages::internalError(asyncResp->res);
-                return;
-            }
-            callback(object.begin()->first);
         });
 }
 
