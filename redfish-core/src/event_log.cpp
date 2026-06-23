openbmc/bmcweb

commit aa2dfd26a514e063d2b62ec32f562cbcd43a21b4

Remove static variables
These static variables have the potential to cause reentrancy issues.
In practice, the conditions to cause issues would require someone to
basically write incorrect code, but it makes sense to wrap this into a
state tracker anyway to clean up the code.  While we're here, convert to
using std::chrono.

Note, this changes the behavior such that the values produced are now no
longer dependent on timezone.  Functionally, Redfish only recently got
the ability to set a timezone, so this is not expected to have any user
facing impact, even though the unit tests need to change.

Tested: RSV Passes

Change-Id: Icb7cff1d289ae23790a5fb1db6604abd73dd68fd
Signed-off-by: Ed Tanous <etanous@nvidia.com>

redfish-core/src/event_log.cpp
@@ -6,18 +6,17 @@
 #include "logging.hpp"
 #include "registries.hpp"
 #include "str_utility.hpp"
+#include "utils/time_utils.hpp"
 
 #include <nlohmann/json.hpp>
 
 #include <cerrno>
+#include <chrono>
 #include <cstddef>
 #include <cstdint>
-#include <ctime>
 #include <format>
-#include <iomanip>
 #include <optional>
 #include <span>
-#include <sstream>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -29,33 +28,31 @@ namespace redfish
 namespace event_log
 {
 
-bool getUniqueEntryID(const std::string& logEntry, std::string& entryID)
+bool getUniqueEntryID(UniqueEntryIDState& state, const std::string& logEntry,
+                      std::string& entryID)
 {
-    static time_t prevTs = 0;
-    static int index = 0;
-
     // Get the entry timestamp
-    std::time_t curTs = 0;
-    std::tm timeStruct = {};
-    std::istringstream entryStream(logEntry);
-    if (entryStream >> std::get_time(&timeStruct, "%Y-%m-%dT%H:%M:%S"))
+    std::optional<time_utils::usSinceEpoch> curTs =
+        time_utils::dateStringToEpoch(logEntry);
+    if (!curTs)
     {
-        curTs = std::mktime(&timeStruct);
-        if (curTs == -1)
-        {
-            return false;
-        }
+        return false;
     }
+    using std::chrono::floor;
+    // Convert to seconds.  Journal doesn't give sub-seconds resolution (yet)
+    // so this should be no op.
+    std::chrono::seconds curTsSec = floor<std::chrono::seconds>(*curTs);
+    std::chrono::sys_seconds curTsSys(curTsSec);
     // If the timestamp isn't unique, increment the index
-    index = (curTs == prevTs) ? index + 1 : 0;
+    state.index = (curTsSys == state.prevTs) ? state.index + 1 : 0;
 
     // Save the timestamp
-    prevTs = curTs;
+    state.prevTs = curTsSys;
 
-    entryID = std::to_string(curTs);
-    if (index > 0)
+    entryID = std::to_string(curTsSys.time_since_epoch().count());
+    if (state.index > 0)
     {
-        entryID += "_" + std::to_string(index);
+        entryID += "_" + std::to_string(state.index);
     }
     return true;
 }

http/http_server.hpp
@@ -20,7 +20,6 @@
 #include <chrono>
 #include <csignal>
 #include <cstddef>
-#include <ctime>
 #include <functional>
 #include <memory>
 #include <string>

http/utility.hpp
@@ -19,7 +19,6 @@
 #include <concepts>
 #include <cstddef>
 #include <cstdint>
-#include <ctime>
 #include <functional>
 #include <initializer_list>
 #include <limits>

redfish-core/include/event_log.hpp
@@ -16,7 +16,14 @@ namespace redfish
 namespace event_log
 {
 
-bool getUniqueEntryID(const std::string& logEntry, std::string& entryID);
+struct UniqueEntryIDState
+{
+    std::chrono::sys_seconds prevTs;
+    int index = 0;
+};
+
+bool getUniqueEntryID(UniqueEntryIDState& state, const std::string& logEntry,
+                      std::string& entryID);
 
 int getEventLogParams(const std::string& logEntry, std::string& timestamp,
                       std::string& messageID,

redfish-core/include/event_service_manager.hpp
@@ -28,7 +28,6 @@
 #include <algorithm>
 #include <cstdint>
 #include <cstdlib>
-#include <ctime>
 #include <memory>
 #include <optional>
 #include <random>

redfish-core/include/utils/eventlog_utils.hpp
@@ -32,7 +32,6 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
-#include <ctime>
 #include <fstream>
 #include <iomanip>
 #include <memory>
@@ -339,6 +338,7 @@ inline void handleSystemsAndManagersLogServiceEventLogLogEntryCollection(
 
     // Oldest logs are in the last file, so start there and loop
     // backwards
+    event_log::UniqueEntryIDState state;
     for (auto it = redfishLogFiles.rbegin(); it < redfishLogFiles.rend(); it++)
     {
         std::ifstream logStream(*it);
@@ -351,7 +351,7 @@ inline void handleSystemsAndManagersLogServiceEventLogLogEntryCollection(
         while (std::getline(logStream, logEntry))
         {
             std::string idStr;
-            if (!event_log::getUniqueEntryID(logEntry, idStr))
+            if (!event_log::getUniqueEntryID(state, logEntry, idStr))
             {
                 continue;
             }
@@ -418,6 +418,7 @@ inline void handleSystemsAndManagersLogServiceEventLogEntriesGet(
 
     // Oldest logs are in the last file, so start there and loop
     // backwards
+    event_log::UniqueEntryIDState state;
     for (auto it = redfishLogFiles.rbegin(); it < redfishLogFiles.rend(); it++)
     {
         std::ifstream logStream(*it);
@@ -430,7 +431,7 @@ inline void handleSystemsAndManagersLogServiceEventLogEntriesGet(
         while (std::getline(logStream, logEntry))
         {
             std::string idStr;
-            if (!event_log::getUniqueEntryID(logEntry, idStr))
+            if (!event_log::getUniqueEntryID(state, logEntry, idStr))
             {
                 continue;
             }

redfish-core/lib/task.hpp
@@ -37,7 +37,6 @@
 #include <array>
 #include <chrono>
 #include <cstddef>
-#include <ctime>
 #include <deque>
 #include <functional>
 #include <memory>

redfish-core/lib/telemetry_service.hpp
@@ -21,7 +21,6 @@
 #include <chrono>
 #include <cstddef>
 #include <cstdint>
-#include <ctime>
 #include <functional>
 #include <memory>
 #include <utility>

commit 55cec32a9c845be66c21fee88139c317dd5cbc99

Fix event log timestamp parse error
The commit aa2dfd26a514 ("Remove static variables") removed extraction
of timestamp data from the whole event log entry, 'dateStringToEpoch()'
will reject this kind of timestamp with trailing text.

Fixes: aa2dfd26a514 ("Remove static variables")

Change-Id: Id59af476e56a913a02a9f56a8c5f38c4ada7ccbf
Signed-off-by: Haiyue Wang <haiyuewa@163.com>

redfish-core/src/event_log.cpp
@@ -31,9 +31,17 @@ namespace event_log
 bool getUniqueEntryID(UniqueEntryIDState& state, const std::string& logEntry,
                       std::string& entryID)
 {
+    // The redfish log format is "<Timestamp> <MessageId>,<MessageArgs>"
+    std::string_view timestamp = logEntry;
+    size_t space = logEntry.find_first_of(' ');
+    if (space != std::string::npos)
+    {
+        timestamp = timestamp.substr(0, space);
+    }
+
     // Get the entry timestamp
     std::optional<time_utils::usSinceEpoch> curTs =
-        time_utils::dateStringToEpoch(logEntry);
+        time_utils::dateStringToEpoch(timestamp);
     if (!curTs)
     {
         return false;