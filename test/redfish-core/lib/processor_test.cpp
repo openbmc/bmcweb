c098190acca0716dd30c5434bbcd333bae219ddc
JY Voon <jvoon@nvidia.com>
2026-07-07 17:47:24 +0900

    bmcweb: add unit tests for processor FirmwareVersion handlers
    Cover the callbacks introduced by the GPU Processor FirmwareVersion
    change. The tests assert each branch's externally visible behavior:
    unexpected D-Bus errors, an ambiguous subtree, and a missing owning
    service set an internal server error, while EBADR, an empty subtree,
    and an empty version leave FirmwareVersion omitted.
    
    Change-Id: Ic00549fe7513651a139ebb8a4e7b36c95b3f453c
    Signed-off-by: JY Voon <jvoon@nvidia.com>

--- test/redfish-core/lib/processor_test.cpp
+++ test/redfish-core/lib/processor_test.cpp
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 #include "async_resp.hpp"
+#include "dbus_utility.hpp"
 #include "http_response.hpp"
 #include "processor.hpp"
 
 #include <boost/asio/error.hpp>
 #include <boost/beast/http/status.hpp>
+#include <boost/system/linux_error.hpp>
 #include <nlohmann/json.hpp>
 
 #include <memory>
@@ -39,6 +41,117 @@ TEST(AfterGetProcessorLocationCode, SuccessSetsServiceLabel)
     EXPECT_EQ(
         response->res.jsonValue["Location"]["PartLocation"]["ServiceLabel"],
         "U3-P0");
+}
+
+TEST(AfterGetProcessorFirmwareVersion, EbadrOmitsFirmwareVersion)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec =
+        boost::system::linux_error::bad_request_descriptor;
+
+    afterGetProcessorFirmwareVersion(response, ec, "");
+
+    EXPECT_EQ(response->res.result(), boost::beast::http::status::ok);
+    EXPECT_FALSE(response->res.jsonValue.contains("FirmwareVersion"));
+}
+
+TEST(AfterGetProcessorFirmwareVersion, ErrorSetsInternalError)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec = boost::asio::error::invalid_argument;
+
+    afterGetProcessorFirmwareVersion(response, ec, "");
+
+    EXPECT_EQ(response->res.result(),
+              boost::beast::http::status::internal_server_error);
+}
+
+TEST(AfterGetProcessorFirmwareVersion, EmptyVersionOmitsFirmwareVersion)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec;
+
+    afterGetProcessorFirmwareVersion(response, ec, "");
+
+    EXPECT_EQ(response->res.result(), boost::beast::http::status::ok);
+    EXPECT_FALSE(response->res.jsonValue.contains("FirmwareVersion"));
+}
+
+TEST(AfterGetProcessorFirmwareVersion, SuccessSetsFirmwareVersion)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec;
+
+    afterGetProcessorFirmwareVersion(response, ec, "1.2.3");
+
+    EXPECT_EQ(response->res.result(), boost::beast::http::status::ok);
+    EXPECT_EQ(response->res.jsonValue["FirmwareVersion"], "1.2.3");
+}
+
+TEST(AfterGetProcessorFirmwareVersionSubTree, EbadrOmitsFirmwareVersion)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec =
+        boost::system::linux_error::bad_request_descriptor;
+
+    afterGetProcessorFirmwareVersionSubTree(response, ec, {});
+
+    EXPECT_EQ(response->res.result(), boost::beast::http::status::ok);
+    EXPECT_FALSE(response->res.jsonValue.contains("FirmwareVersion"));
+}
+
+TEST(AfterGetProcessorFirmwareVersionSubTree, ErrorSetsInternalError)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec = boost::asio::error::invalid_argument;
+
+    afterGetProcessorFirmwareVersionSubTree(response, ec, {});
+
+    EXPECT_EQ(response->res.result(),
+              boost::beast::http::status::internal_server_error);
+}
+
+TEST(AfterGetProcessorFirmwareVersionSubTree, EmptySubTreeOmitsFirmwareVersion)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec;
+
+    afterGetProcessorFirmwareVersionSubTree(response, ec, {});
+
+    EXPECT_EQ(response->res.result(), boost::beast::http::status::ok);
+    EXPECT_FALSE(response->res.jsonValue.contains("FirmwareVersion"));
+}
+
+TEST(AfterGetProcessorFirmwareVersionSubTree,
+     MultipleSoftwareObjectsSetInternalError)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec;
+    dbus::utility::MapperGetSubTreeResponse subtree = {
+        {"/xyz/openbmc_project/software/fw0",
+         {{"xyz.openbmc_project.Service0",
+           {"xyz.openbmc_project.Software.Version"}}}},
+        {"/xyz/openbmc_project/software/fw1",
+         {{"xyz.openbmc_project.Service0",
+           {"xyz.openbmc_project.Software.Version"}}}}};
+
+    afterGetProcessorFirmwareVersionSubTree(response, ec, subtree);
+
+    EXPECT_EQ(response->res.result(),
+              boost::beast::http::status::internal_server_error);
+}
+
+TEST(AfterGetProcessorFirmwareVersionSubTree, EmptyServiceMapSetsInternalError)
+{
+    auto response = std::make_shared<bmcweb::AsyncResp>();
+    boost::system::error_code ec;
+    dbus::utility::MapperGetSubTreeResponse subtree = {
+        {"/xyz/openbmc_project/software/fw0", {}}};
+
+    afterGetProcessorFirmwareVersionSubTree(response, ec, subtree);
+
+    EXPECT_EQ(response->res.result(),
+              boost::beast::http::status::internal_server_error);
 }
 
 } // namespace
