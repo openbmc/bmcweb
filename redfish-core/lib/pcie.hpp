830bb5a152b444637c06c8e3586988c35e120cb3
Crystal Huang <crzhuang1@gmail.com>
2026-07-10 15:38:30 -0500

    Add Location information for PCIeDevice
    This commit is to add location information according to the Redfish
    PCIeDevice schema. If the
    `xyz.openbmc_project.Inventory.Decorator.LocationCode` interface does
    not exist, the location information property is not displayed.
    The Redfish PCIeDevice schema includes a Location property under Slot.
    Get the Location from PCIeSlot.
    
    ref: https://redfish.dmtf.org/schemas/v1/PCIeDevice.v1_9_0.json
    
    Tested: Validator passed
    
    curl -k https://$bmc/redfish/v1/Systems/system/PCIeDevices/pcie_card2
    ```
        {
          "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card2",
          "@odata.type": "#PCIeDevice.v1_9_0.PCIeDevice",
          "Id": "pcie_card2",
          "Name": "PCIe Device",
          "PCIeFunctions": {
            "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/pcie_card2/PCIeFunctions"
          },
          "PCIeInterface": {
            "LanesInUse": -1
          },
          "Slot": {
            "Lanes": 0,
            "Location": {
              "PartLocation": {
                "ServiceLabel": "U78DB.ND0.WZS000G-P0-C2"
              }
            },
            "SlotType": "FullLength"
          },
          "Status": {
            "Health": "OK",
            "State": "Absent"
          }
        }
    ```
    
    Change-Id: Iba1b98734fdfee43c2995f6c9f1f2660e196814a
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>
    Signed-off-by: Crystal Huang <crzhuang1@gmail.com>

--- redfish-core/lib/pcie.hpp
+++ redfish-core/lib/pcie.hpp
@@ -338,6 +338,24 @@ inline void getPCIeDeviceSlotPath(
         });
 }
 
+inline void afterGetLocationCode(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, const std::string& property)
+{
+    if (ec)
+    {
+        if (ec.value() != EBADR)
+        {
+            BMCWEB_LOG_ERROR("DBUS response error for Location {}", ec.value());
+            messages::internalError(asyncResp->res);
+        }
+        return;
+    }
+    asyncResp->res
+        .jsonValue["Slot"]["Location"]["PartLocation"]["ServiceLabel"] =
+        property;
+}
+
 inline void afterGetDbusObject(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const std::string& pcieDeviceSlot, const boost::system::error_code& ec,
@@ -350,24 +368,46 @@ inline void afterGetDbusObject(
         messages::internalError(asyncResp->res);
         return;
     }
-    dbus::utility::getAllProperties(
-        object.begin()->first, pcieDeviceSlot,
-        "xyz.openbmc_project.Inventory.Item.PCIeSlot",
-        [asyncResp](
-            const boost::system::error_code& ec2,
-            const dbus::utility::DBusPropertiesMap& pcieSlotProperties) {
-            addPCIeSlotProperties(asyncResp->res, ec2, pcieSlotProperties);
-        });
+    for (const auto& [service, interfaces] : object)
+    {
+        for (const std::string& interface : interfaces)
+        {
+            if (interface == "xyz.openbmc_project.Inventory.Item.PCIeSlot")
+            {
+                dbus::utility::getAllProperties(
+                    service, pcieDeviceSlot,
+                    "xyz.openbmc_project.Inventory.Item.PCIeSlot",
+                    [asyncResp](
+                        const boost::system::error_code& ec2,
+                        const dbus::utility::DBusPropertiesMap&
+                            pcieSlotProperties) {
+                        addPCIeSlotProperties(asyncResp->res, ec2,
+                                              pcieSlotProperties);
+                    });
+            }
+            else if (
+                interface ==
+                "xyz.openbmc_project.Inventory.Decorator.LocationCode")
+            {
+                dbus::utility::getProperty<std::string>(
+                    service, pcieDeviceSlot,
+                    "xyz.openbmc_project.Inventory.Decorator.LocationCode",
+                    "LocationCode",
+                    std::bind_front(afterGetLocationCode, asyncResp));
+            }
+        }
+    }
 }
 
 inline void afterGetPCIeDeviceSlotPath(
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
     const std::string& pcieDeviceSlot)
 {
-    static constexpr std::array<std::string_view, 1> pcieSlotInterface = {
-        "xyz.openbmc_project.Inventory.Item.PCIeSlot"};
+    static constexpr std::array<std::string_view, 2> pcieSlotInterfaces = {
+        "xyz.openbmc_project.Inventory.Item.PCIeSlot",
+        "xyz.openbmc_project.Inventory.Decorator.LocationCode"};
     dbus::utility::getDbusObject(
-        pcieDeviceSlot, pcieSlotInterface,
+        pcieDeviceSlot, pcieSlotInterfaces,
         [asyncResp,
          pcieDeviceSlot](const boost::system::error_code& ec,
                          const dbus::utility::MapperGetObject& object) {
