e76fecea5cbd0d8f2ac9ac16a77a88f516c341c4
Akshay Gaitonde <a.g@utexas.edu>
2026-08-03 18:31:36 -0500

    Cable: Add Available state and Status.Health
    Extended Cable's Status.State to use Available mapping and added
    Status.Health to Cable which it did not have before
    
    Utilize resource_util's getResourceHealth and getResourceState to
    standardize Redfish Status.State and Status.Health
    
    Tested: on QEMU, which has no host and so no real cable inventory, with
    a cable object injected into phosphor-inventory-manager.
    
      Functional true  -> "Health": "OK", "State": "Enabled"
      Functional false -> "Health": "Critical", "State": "Enabled"
    
    Where State can be "Enabled", "Absent", and "UnavailableOffline" if the
    xyz.openbmc_project.State.Decorator.Availability interface is
    implemented
    
    Change-Id: I9f99f7cfe348545506753cc4eedd81570fbf3fba
    Signed-off-by: Akshay Gaitonde <a.g@utexas.edu>
    Signed-off-by: Justin Nguyen <justinnanguyen@gmail.com>

--- redfish-core/lib/cable.hpp
+++ redfish-core/lib/cable.hpp
@@ -16,6 +16,7 @@
 #include "utils/asset_utils.hpp"
 #include "utils/collection.hpp"
 #include "utils/dbus_utils.hpp"
+#include "utils/resource_utils.hpp"
 
 #include <asm-generic/errno.h>
 
@@ -94,36 +95,6 @@ inline void fillCableProperties(
     }
 }
 
-inline void fillCableHealthState(
-    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-    const std::string& cableObjectPath, const std::string& service)
-{
-    dbus::utility::getProperty<bool>(
-        *crow::connections::systemBus, service, cableObjectPath,
-        "xyz.openbmc_project.Inventory.Item", "Present",
-        // ast-grep-ignore: long-lambda
-        [asyncResp,
-         cableObjectPath](const boost::system::error_code& ec, bool present) {
-            if (ec)
-            {
-                if (ec.value() != EBADR)
-                {
-                    BMCWEB_LOG_ERROR(
-                        "get presence failed for Cable {} with error {}",
-                        cableObjectPath, ec.value());
-                    messages::internalError(asyncResp->res);
-                }
-                return;
-            }
-
-            if (!present)
-            {
-                asyncResp->res.jsonValue["Status"]["State"] =
-                    resource::State::Absent;
-            }
-        });
-}
-
 /**
  * @brief Api to get Cable properties.
  * @param[in,out]   asyncResp       Async HTTP response.
@@ -156,7 +127,14 @@ inline void getCableProperties(
             }
             else if (interface == "xyz.openbmc_project.Inventory.Item")
             {
-                fillCableHealthState(asyncResp, cableObjectPath, service);
+                resource_utils::getResourceState(
+                    asyncResp, service, cableObjectPath, ""_json_pointer);
+            }
+            else if (interface ==
+                     "xyz.openbmc_project.State.Decorator.OperationalStatus")
+            {
+                resource_utils::getResourceHealth(
+                    asyncResp, service, cableObjectPath, ""_json_pointer);
             }
         }
     }
@@ -194,6 +172,7 @@ inline void afterHandleCableGet(
         asyncResp->res.jsonValue["Id"] = cableId;
         asyncResp->res.jsonValue["Name"] = "Cable";
         asyncResp->res.jsonValue["Status"]["State"] = resource::State::Enabled;
+        asyncResp->res.jsonValue["Status"]["Health"] = resource::Health::OK;
 
         getCableProperties(asyncResp, objectPath, serviceMap);
         return;
