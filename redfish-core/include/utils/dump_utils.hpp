206391fc9676a9e84c83c0c8dc5aeea394725ce3
Oliver Brewka <oliver.brewka@9elements.com>
2026-08-18 16:45:57 +0200

    Inline dump util function
    It should be avoided using static for header only functions.
    
    Tested: Code compiles.
    
    Change-Id: Icb944588fc6f64c663960bf40da29e2de9e0b8bd
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- redfish-core/include/utils/dump_utils.hpp
+++ redfish-core/include/utils/dump_utils.hpp
@@ -223,7 +223,7 @@ inline void parseDumpEntryFromDbusObject(
     }
 }
 
-static boost::urls::url getDumpEntriesPath(const std::string& dumpType)
+inline boost::urls::url getDumpEntriesPath(const std::string& dumpType)
 {
     boost::urls::url entriesPath;
 
