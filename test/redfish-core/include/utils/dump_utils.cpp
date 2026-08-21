6ae47df55d1035a73ebaf8c0058c55c9fcbbedd7
Oliver Brewka <oliver.brewka@9elements.com>
2026-08-21 10:45:58 +0200

    Add unit tests for dump_utils
    Extend dump_utils unit testing.
    
    Tested: Code compiles. Introduced unit tests run through.
    
    Change-Id: I254e7b5f7318e847551055f352b70a87d07c5cda
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- test/redfish-core/include/utils/dump_utils.cpp
+++ test/redfish-core/include/utils/dump_utils.cpp
@@ -6,19 +6,22 @@
 #include "bmcweb_config.h"
 
 #include "async_resp.hpp"
+#include "dbus_utility.hpp"
+#include "generated/enums/log_entry.hpp"
 
 #include <boost/beast/http/status.hpp>
 #include <boost/url/format.hpp>
 #include <sdbusplus/message/native_types.hpp>
 
+#include <cstdint>
 #include <memory>
 #include <optional>
+#include <string>
 
 #include <gtest/gtest.h>
 
 namespace redfish::dump_utils
 {
-
 TEST(DumpUtils, GetDumpServiceInfo)
 {
     auto shareAsyncResp = std::make_shared<bmcweb::AsyncResp>();
@@ -61,5 +64,83 @@ TEST(DumpUtils, GetDumpEntriesUrl)
         boost::urls::format("/redfish/v1/Systems/{}/LogServices/Dump/Entries",
                             BMCWEB_REDFISH_SYSTEM_URI_NAME));
     EXPECT_EQ(getDumpEntriesUrl(static_cast<DumpType>(99)), std::nullopt);
+}
+
+TEST(DumpUtils, MapDbusStatusToDumpProgress)
+{
+    EXPECT_EQ(
+        mapDbusStatusToDumpProgress(
+            "xyz.openbmc_project.Common.Progress.OperationStatus.Completed"),
+        DumpCreationProgress::DUMP_CREATE_SUCCESS);
+
+    EXPECT_EQ(mapDbusStatusToDumpProgress(
+                  "xyz.openbmc_project.Common.Progress.OperationStatus.Failed"),
+              DumpCreationProgress::DUMP_CREATE_FAILED);
+
+    EXPECT_EQ(
+        mapDbusStatusToDumpProgress(
+            "xyz.openbmc_project.Common.Progress.OperationStatus.Aborted"),
+        DumpCreationProgress::DUMP_CREATE_FAILED);
+
+    EXPECT_EQ(
+        mapDbusStatusToDumpProgress(
+            "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress"),
+        DumpCreationProgress::DUMP_CREATE_INPROGRESS);
+
+    EXPECT_EQ(mapDbusStatusToDumpProgress(""),
+              DumpCreationProgress::DUMP_CREATE_INPROGRESS);
+}
+
+TEST(DumpUtils, GetDumpCompletionStatus)
+{
+    dbus::utility::DBusPropertiesMap values;
+
+    values.emplace_back(
+        "Status",
+        dbus::utility::DbusVariantType{std::string{
+            "xyz.openbmc_project.Common.Progress.OperationStatus.Completed"}});
+    EXPECT_EQ(getDumpCompletionStatus(values),
+              DumpCreationProgress::DUMP_CREATE_SUCCESS);
+
+    values[0].second =
+        "xyz.openbmc_project.Common.Progress.OperationStatus.Failed";
+    EXPECT_EQ(getDumpCompletionStatus(values),
+              DumpCreationProgress::DUMP_CREATE_FAILED);
+
+    values[0].second =
+        "xyz.openbmc_project.Common.Progress.OperationStatus.Aborted";
+    EXPECT_EQ(getDumpCompletionStatus(values),
+              DumpCreationProgress::DUMP_CREATE_FAILED);
+
+    values[0].second =
+        "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress";
+    EXPECT_EQ(getDumpCompletionStatus(values),
+              DumpCreationProgress::DUMP_CREATE_INPROGRESS);
+
+    values[0].second = dbus::utility::DbusVariantType{uint64_t{1}};
+    EXPECT_EQ(getDumpCompletionStatus(values),
+              DumpCreationProgress::DUMP_CREATE_FAILED);
+
+    values.clear();
+    EXPECT_EQ(getDumpCompletionStatus(values),
+              DumpCreationProgress::DUMP_CREATE_INPROGRESS);
+}
+
+TEST(DumpUtils, MapDbusOriginatorTypeToRedfish)
+{
+    EXPECT_EQ(
+        mapDbusOriginatorTypeToRedfish(
+            "xyz.openbmc_project.Common.OriginatedBy.OriginatorTypes.Client"),
+        log_entry::OriginatorTypes::Client);
+    EXPECT_EQ(
+        mapDbusOriginatorTypeToRedfish(
+            "xyz.openbmc_project.Common.OriginatedBy.OriginatorTypes.Internal"),
+        log_entry::OriginatorTypes::Internal);
+    EXPECT_EQ(
+        mapDbusOriginatorTypeToRedfish(
+            "xyz.openbmc_project.Common.OriginatedBy.OriginatorTypes.SupportingService"),
+        log_entry::OriginatorTypes::SupportingService);
+    EXPECT_EQ(mapDbusOriginatorTypeToRedfish(""),
+              log_entry::OriginatorTypes::Invalid);
 }
 } // namespace redfish::dump_utils
