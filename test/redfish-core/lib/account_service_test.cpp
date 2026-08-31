32bdbdf59af2dd0c7c5d5277744804f70f784e8d
Ivan Mikhaylov <fr0st61te@gmail.com>
2026-08-31 19:55:46 +0300

    Manage user account expiration via REST
    This change allows to manage user account expiration date time via REST
    API provided by bmcweb. Account expiration date time is managed as
    string in 'YYYY-MM-DDTHH:MM:SSZ' format and treated as Epoch time.
    'null' value is used to make account not to expire.
    
    Account expiration management:
    1. Create user with account expiration
    
    ```
    curl -k -X POST -H 'Content-Type: application/json' \
    "https://<bmc>/redfish/v1/AccountService/Accounts" \
    -d '{"UserName":"<user>", "Password":"<password>", "RoleId":"<role>", "AccountExpiration": "<YYYY-MM-DDTHH:MM:SS>"}'
    ```
    
    2. Modify user account expiration
    
    ```
    curl -k -X PATCH -H 'Content-Type: application/json' \
    "https://<bmc>/redfish/v1/AccountService/Accounts/<user>" \
    -d '{"AccountExpiration": "<YYYY-MM-DDTHH:MM:SS>"}'
    ```
    
    3. Get user account expiration
    
    ```
    curl -k -X GET https://<bmc>/redfish/v1/AccountService/Accounts/<user>
    ```
    
    4. Modify user account not to expire
    
    ```
    curl -k -X PATCH -H 'Content-Type: application/json' \
    "https://<bmc>/redfish/v1/AccountService/Accounts/<user> \
    -d '{"AccountExpiration": null}'
    ```
    
    Bump ManagerAccount version to v1.8.0.
    
    TESTED:
    Functionality of this change was tested via curl utility. It was also
    verified that correct values are set as 'AccountExpiration' attribute on
    D-Bus for the specified user:
    
    1. Create user without account expiration, verify that AccountExpiration
       has default value (18446744073709551615)
    2. Create user with account expiration specified, verify that it is
       correct
    3. Try to create user with invalid account expiration (invalid type,
       invalid value), verify that user is not created and appropriate error
       message is returned
    4. Set account expiration for existing user, verify that it is correct
    5. Set AccountExpiration to 'null' value fot existing user, verify that
       it is 'null'
    6. Try to set account expiration for existing user to invalid value,
       verify that operation fails and response contains appropriate error
       message
    
    Redfish service validation on /redfish/v1/AccountService/Accounts tree
    containing user accounts with AccountExpiration set to some time and
    'null' values has passed successfully.
    
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-user-manager/+/94111
    [2] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/92633
    
    Signed-off-by: Konstantin Maskov <sulwirld@gmail.com>
    Signed-off-by: Ivan Mikhaylov <fr0st61te@gmail.com>
    Change-Id: If77a1bfce94a325277963ebcd24231536a26780d

--- test/redfish-core/lib/account_service_test.cpp
+++ test/redfish-core/lib/account_service_test.cpp
@@ -22,42 +22,41 @@ TEST(Conversion, PositiveToUint64)
 {
     std::optional<uint64_t> passwordExpiration;
 
-    passwordExpiration = passwordExpirationToUint64(nullptr);
+    passwordExpiration = expirationToUint64(nullptr);
     EXPECT_THAT(passwordExpiration, Optional(Eq(0)));
 
-    passwordExpiration = passwordExpirationToUint64("1970-01-01T00:00:00");
+    passwordExpiration = expirationToUint64("1970-01-01T00:00:00");
     EXPECT_THAT(passwordExpiration, Eq(std::nullopt));
 
-    passwordExpiration = passwordExpirationToUint64("2024-10-17T18:12:04");
+    passwordExpiration = expirationToUint64("2024-10-17T18:12:04");
     EXPECT_THAT(passwordExpiration, Optional(Eq(1729188724)));
 
-    passwordExpiration = passwordExpirationToUint64("2024-10-17T18:12:04Z");
+    passwordExpiration = expirationToUint64("2024-10-17T18:12:04Z");
     EXPECT_THAT(passwordExpiration, Optional(Eq(1729188724)));
 
-    passwordExpiration =
-        passwordExpirationToUint64("2024-10-17T18:12:04+03:00");
+    passwordExpiration = expirationToUint64("2024-10-17T18:12:04+03:00");
     EXPECT_THAT(passwordExpiration, Optional(Eq(1729177924)));
 }
 
 TEST(Conversion, NegativeToUint64)
 {
-    EXPECT_EQ(passwordExpirationToUint64("01-02-03"), std::nullopt);
+    EXPECT_EQ(expirationToUint64("01-02-03"), std::nullopt);
 
-    EXPECT_EQ(passwordExpirationToUint64("1912-02-03"), std::nullopt);
+    EXPECT_EQ(expirationToUint64("1912-02-03"), std::nullopt);
 
-    EXPECT_EQ(passwordExpirationToUint64("2024-10-17T00:00"), std::nullopt);
+    EXPECT_EQ(expirationToUint64("2024-10-17T00:00"), std::nullopt);
 
-    EXPECT_EQ(passwordExpirationToUint64("ABC"), std::nullopt);
+    EXPECT_EQ(expirationToUint64("ABC"), std::nullopt);
 }
 
 TEST(Conversion, PositiveToJson)
 {
-    constexpr uint64_t unexpiringPasswordExpiration = 0;
+    constexpr uint64_t unexpiringExpiration = 0;
 
-    json value = passwordExpirationToJson(unexpiringPasswordExpiration);
+    json value = expirationToJson(unexpiringExpiration);
     EXPECT_TRUE(value.is_null());
 
-    value = passwordExpirationToJson(1729188724);
+    value = expirationToJson(1729188724);
     EXPECT_TRUE(value.is_string());
     EXPECT_EQ(value, R"("2024-10-17T18:12:04+00:00")"_json);
 }
