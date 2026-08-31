ecdab51cbf524d71cbf12dbcfdc62b5e4a75f244
Dhruv Goyal <dhgoyal@nvidia.com>
2026-08-31 09:56:12 -0700

    secure_boot: add PATCH support
    Allow updating the SecureBoot resource, completing the read-only
    routes added by the parent change:
    
    - SecureBootEnable: the administrator's request to change the
      setting; staged in the BIOSConfig.SecureBoot PendingEnable D-Bus
      property for the host firmware to apply at next boot.
    - SecureBootCurrentBoot, SecureBootMode: facts about the current
      boot reported by the host firmware over the Redfish Host
      Interface (DSP0270); mapped to CurrentBoot and Mode.
    
    Access is gated by the standard patchSecureBoot privilege
    (ConfigureComponents). Platforms using the host-interface bootstrap
    account flow can further restrict the state-report properties to
    the host identity once that support is available.
    
    Tested: host firmware reports SecureBootCurrentBoot/SecureBootMode
    over the host interface (values served back via GET); an
    out-of-band administrator PATCHes SecureBootEnable and the value
    is staged in PendingEnable.
    
    Change-Id: I89a05f2fd78f9305684764040e27f38cd1f7dd04
    Signed-off-by: Dhruv Goyal <dhgoyal@nvidia.com>

--- redfish-core/lib/secure_boot.hpp
+++ redfish-core/lib/secure_boot.hpp
@@ -13,15 +13,18 @@
 #include "logging.hpp"
 #include "query.hpp"
 #include "registries/privilege_registry.hpp"
+#include "utils/json_utils.hpp"
 
 #include <boost/beast/http/field.hpp>
 #include <boost/beast/http/verb.hpp>
 #include <boost/url/format.hpp>
 #include <nlohmann/json.hpp>
+#include <sdbusplus/asio/property.hpp>
 #include <sdbusplus/unpack_properties.hpp>
 
 #include <functional>
 #include <memory>
+#include <optional>
 #include <string>
 #include <unordered_map>
 
@@ -140,6 +143,126 @@ inline void handleSecureBootHead(
         "</redfish/v1/JsonSchemas/SecureBoot/SecureBoot.json>; rel=describedby");
 }
 
+/**
+ * Handle PATCH of the SecureBoot resource.
+ *
+ * SecureBootEnable is the administrator's request to change the setting;
+ * it is staged in the BIOSConfig.SecureBoot PendingEnable property for
+ * the host firmware to apply at next boot. SecureBootCurrentBoot and
+ * SecureBootMode are facts about the current boot reported by the host
+ * firmware over the Redfish Host Interface (DSP0270); they map to the
+ * CurrentBoot and Mode properties.
+ */
+inline void handleSecureBootPatch(
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+    [[maybe_unused]] const std::string& systemName)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    std::optional<std::string> secureBootCurrentBoot;
+    std::optional<std::string> secureBootMode;
+    std::optional<bool> secureBootEnable;
+    if (!json_util::readJsonPatch(                          //
+            req, aResp->res,                                //
+            "SecureBootCurrentBoot", secureBootCurrentBoot, //
+            "SecureBootEnable", secureBootEnable,           //
+            "SecureBootMode", secureBootMode))
+    {
+        return;
+    }
+
+    constexpr const char* sbService = "xyz.openbmc_project.BIOSConfigManager";
+    constexpr const char* sbPath =
+        "/xyz/openbmc_project/bios_config/secure_boot";
+    constexpr const char* sbIface = "xyz.openbmc_project.BIOSConfig.SecureBoot";
+
+    if (secureBootCurrentBoot)
+    {
+        std::string currentBoot;
+        if (*secureBootCurrentBoot == "Enabled")
+        {
+            currentBoot =
+                "xyz.openbmc_project.BIOSConfig.SecureBoot.CurrentBootType.Enabled";
+        }
+        else if (*secureBootCurrentBoot == "Disabled")
+        {
+            currentBoot =
+                "xyz.openbmc_project.BIOSConfig.SecureBoot.CurrentBootType.Disabled";
+        }
+        else
+        {
+            messages::propertyValueNotInList(
+                aResp->res, *secureBootCurrentBoot, "SecureBootCurrentBoot");
+            return;
+        }
+        sdbusplus::asio::setProperty(
+            *crow::connections::systemBus, sbService, sbPath, sbIface,
+            "CurrentBoot", currentBoot,
+            [aResp](const boost::system::error_code& ec) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR("Failed to set CurrentBoot: {}", ec);
+                    messages::internalError(aResp->res);
+                }
+            });
+    }
+
+    if (secureBootMode)
+    {
+        std::string mode;
+        if (*secureBootMode == "SetupMode")
+        {
+            mode = "xyz.openbmc_project.BIOSConfig.SecureBoot.ModeType.Setup";
+        }
+        else if (*secureBootMode == "UserMode")
+        {
+            mode = "xyz.openbmc_project.BIOSConfig.SecureBoot.ModeType.User";
+        }
+        else if (*secureBootMode == "AuditMode")
+        {
+            mode = "xyz.openbmc_project.BIOSConfig.SecureBoot.ModeType.Audit";
+        }
+        else if (*secureBootMode == "DeployedMode")
+        {
+            mode =
+                "xyz.openbmc_project.BIOSConfig.SecureBoot.ModeType.Deployed";
+        }
+        else
+        {
+            messages::propertyValueNotInList(aResp->res, *secureBootMode,
+                                             "SecureBootMode");
+            return;
+        }
+        sdbusplus::asio::setProperty(
+            *crow::connections::systemBus, sbService, sbPath, sbIface, "Mode",
+            mode, [aResp](const boost::system::error_code& ec) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR("Failed to set Mode: {}", ec);
+                    messages::internalError(aResp->res);
+                }
+            });
+    }
+
+    if (secureBootEnable)
+    {
+        sdbusplus::asio::setProperty(
+            *crow::connections::systemBus, sbService, sbPath, sbIface,
+            "PendingEnable", *secureBootEnable,
+            [aResp](const boost::system::error_code& ec) {
+                if (ec)
+                {
+                    BMCWEB_LOG_ERROR("Failed to set PendingEnable: {}", ec);
+                    messages::internalError(aResp->res);
+                }
+            });
+    }
+}
+
 inline void requestRoutesSecureBoot(App& app)
 {
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/SecureBoot/")
@@ -150,5 +273,9 @@ inline void requestRoutesSecureBoot(App& app)
         .privileges(redfish::privileges::getSecureBoot)
         .methods(boost::beast::http::verb::get)(
             std::bind_front(handleSecureBootGet, std::ref(app)));
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/SecureBoot/")
+        .privileges(redfish::privileges::patchSecureBoot)
+        .methods(boost::beast::http::verb::patch)(
+            std::bind_front(handleSecureBootPatch, std::ref(app)));
 }
 } // namespace redfish
